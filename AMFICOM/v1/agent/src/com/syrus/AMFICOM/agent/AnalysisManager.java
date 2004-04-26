93ea1da86da1868c943dad6d3d6c717b581cae0b
Arseniy Tashoyan <tashoyan@gmail.com>
2004-04-26 14:41:04 +0400

    Imported agent sources

---
+++ AMFICOM/v1/agent/src/com/syrus/AMFICOM/agent/AnalysisManager.java
@@ -0,0 +1,57 @@
+package com.syrus.AMFICOM.agent;
+
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import com.syrus.AMFICOM.analysis.dadara.DadaraAnalysisManager;
+import com.syrus.AMFICOM.CORBA.KIS.Parameter_Transferable;
+import com.syrus.AMFICOM.CORBA.KIS.Analysis_Transferable;
+import com.syrus.AMFICOM.CORBA.KIS.Etalon_Transferable;
+import com.syrus.AMFICOM.CORBA.KIS.Result_Transferable;
+import com.syrus.AMFICOM.CORBA.General.AlarmLevel;
+import com.syrus.AMFICOM.server.measurement.Result;
+
+public abstract class AnalysisManager {
+
+  public static AnalysisManager getAnalysisManager(String analysis_type_id) {
+    if (analysis_type_id.equals("dadara"))
+      return new DadaraAnalysisManager();
+    else
+      return null;
+  }
+
+  public Result_Transferable analyse(Analysis_Transferable analysis, Result_Transferable result, Etalon_Transferable etalon) {
+    Hashtable criterias = new Hashtable();
+    for (int i = 0; i < analysis.criterias.length; i++)
+      criterias.put(analysis.criterias[i].name, analysis.criterias[i].value);
+    Hashtable resultparameters = new Hashtable();
+    for (int i = 0; i < result.parameters.length; i++)
+      resultparameters.put(result.parameters[i].name, result.parameters[i].value);
+
+		Hashtable etalonparameters = new Hashtable();
+    for (int i = 0; i < etalon.etalon_arguments.length; i++)
+      etalonparameters.put(etalon.etalon_arguments[i].name, etalon.etalon_arguments[i].value);
+
+    Hashtable analysisresultparameters = this.analyse(criterias, resultparameters, etalonparameters);
+
+    Enumeration names = analysisresultparameters.keys();
+    Enumeration values = analysisresultparameters.elements();
+    LinkedList ll = new LinkedList();
+    String par_name;
+    while (names.hasMoreElements()) {
+      par_name = (String)names.nextElement();
+      ll.add(new Parameter_Transferable(par_name, ParametersDatabase.getAnalysisParameterTypeId(par_name, analysis.analysis_type_id), (byte[])values.nextElement()));
+    }
+    Parameter_Transferable[] arparameters = new Parameter_Transferable[ll.size()];
+    arparameters = (Parameter_Transferable[])ll.toArray(arparameters);
+    return new Result_Transferable(null,
+                                   analysis.id,
+																	 result.test_id,
+                                   Result.RESULT_TYPE_ANALYSIS,
+                                   result.elementary_start_time,
+                                   arparameters,
+																	 AlarmLevel.ALARM_LEVEL_NONE);
+  }
+
+  public abstract Hashtable analyse(Hashtable criterias, Hashtable resultparameters, Hashtable etalonparameters);
+}
---
+++ AMFICOM/v1/agent/src/com/syrus/AMFICOM/agent/EvaluationManager.java
@@ -0,0 +1,67 @@
+package com.syrus.AMFICOM.agent;
+
+import com.syrus.AMFICOM.agent.ParametersDatabase;
+import com.syrus.AMFICOM.analysis.dadara.DadaraEvaluationManager;
+import com.syrus.AMFICOM.CORBA.KIS.Evaluation_Transferable;
+import com.syrus.AMFICOM.CORBA.KIS.Etalon_Transferable;
+import com.syrus.AMFICOM.CORBA.KIS.Result_Transferable;
+import com.syrus.AMFICOM.CORBA.KIS.Parameter_Transferable;
+import com.syrus.AMFICOM.CORBA.General.AlarmLevel;
+import com.syrus.AMFICOM.server.measurement.Result;
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.util.LinkedList;
+
+public abstract class EvaluationManager {
+	protected Hashtable evaluationresultparameters;
+	protected AlarmLevel alarmLevel;
+
+  public static EvaluationManager getEvaluationManager(String evaluation_type_id) {
+    if (evaluation_type_id.equals("dadara"))
+      return new DadaraEvaluationManager();
+    else
+      return null;
+  }
+
+  public Result_Transferable evaluate(Evaluation_Transferable evaluation, Result_Transferable analysisResult, Result_Transferable result) throws Exception {
+    Hashtable thresholds = new Hashtable();
+    for (int i = 0; i < evaluation.thresholds.length; i++)
+      thresholds.put(evaluation.thresholds[i].name, evaluation.thresholds[i].value);
+    Hashtable etalonparameters = new Hashtable();
+    for (int i = 0; i < evaluation.etalon.etalon_arguments.length; i++)
+      etalonparameters.put(evaluation.etalon.etalon_arguments[i].name, evaluation.etalon.etalon_arguments[i].value);
+
+		Hashtable analysisresultparameters = null;
+		if (analysisResult != null) {
+			analysisresultparameters = new Hashtable();
+			for (int i = 0; i < analysisResult.parameters.length; i++)
+				analysisresultparameters.put(analysisResult.parameters[i].name, analysisResult.parameters[i].value);
+		}
+
+    Hashtable resultparameters = new Hashtable();
+    for (int i = 0; i < result.parameters.length; i++)
+      resultparameters.put(result.parameters[i].name, result.parameters[i].value);
+
+    this.evaluate(thresholds, etalonparameters, analysisresultparameters, resultparameters);
+
+    Enumeration names = this.evaluationresultparameters.keys();
+    Enumeration values = this.evaluationresultparameters.elements();
+    LinkedList ll = new LinkedList();
+    String par_name;
+    while (names.hasMoreElements()) {
+      par_name = (String)names.nextElement();
+      ll.add(new Parameter_Transferable(par_name, ParametersDatabase.getEvaluationParameterTypeId(par_name, evaluation.evaluation_type_id), (byte[])values.nextElement()));
+    }
+    Parameter_Transferable[] erparameters = (Parameter_Transferable[])ll.toArray(new Parameter_Transferable[ll.size()]);
+
+    return new Result_Transferable(null,
+																	 evaluation.id,
+																	 result.test_id,
+																	 Result.RESULT_TYPE_EVALUATION,
+																	 result.elementary_start_time,
+																	 erparameters,
+																	 this.alarmLevel);
+  }
+
+  public abstract void evaluate(Hashtable thresholds, Hashtable etalonparameters, Hashtable analysisresultparameters, Hashtable resultparameters) throws Exception;
+}
