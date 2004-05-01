63046e0545fa302a18642a35b3b56a90ed884c13
cvsadmin <cvsadmin@science.syrus.ru>
2004-05-01 18:33:52 +0400

    *** empty log message ***

---
+++ AMFICOM/v2/administration/src/com/syrus/AMFICOM/administration/MCM.java
@@ -0,0 +1,140 @@
+package com.syrus.AMFICOM.administration;
+
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Iterator;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.CreateObjectException;
+import com.syrus.AMFICOM.general.RetrieveObjectException;
+import com.syrus.AMFICOM.general.StorableObject;
+import com.syrus.AMFICOM.general.StorableObject_Database;
+import com.syrus.AMFICOM.general.StorableObject_DatabaseContext;
+import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
+import com.syrus.AMFICOM.administration.corba.MCM_Transferable;
+
+public class MCM extends StorableObject {
+	private Identifier server_id;
+	private String name;
+	private String description;
+	private String location;
+	private String contact;
+	private String hostname;
+	private Date created;
+	private Date modified;
+
+	private ArrayList kiss;
+
+	private StorableObject_Database mcmDatabase;
+
+	public MCM(Identifier id) throws RetrieveObjectException {
+		super(id);
+
+		this.mcmDatabase = StorableObject_DatabaseContext.mcmDatabase;
+		try {
+			this.mcmDatabase.retrieve(this);
+		}
+		catch (Exception e) {
+			throw new RetrieveObjectException(e.getMessage(), e);
+		}
+	}
+
+	public MCM(MCM_Transferable mt) throws CreateObjectException {
+		super(new Identifier(mt.id));
+		this.server_id = new Identifier(mt.server_id);
+		this.name = new String(mt.name);
+		this.description = new String(mt.description);
+		this.location = new String(mt.location);
+		this.contact = new String(mt.contact);
+		this.hostname = new String(mt.hostname);
+		this.created = new Date(mt.created);
+		this.modified = new Date(mt.modified);
+
+		this.kiss = new ArrayList(mt.kis_ids.length);
+		for (int i = 0; i < mt.kis_ids.length; i++)
+			this.kiss.add(new KIS(new Identifier(mt.kis_ids[i])));
+
+		this.mcmDatabase = StorableObject_DatabaseContext.mcmDatabase;
+		try {
+			this.mcmDatabase.insert(this);
+		}
+		catch (Exception e) {
+			throw new CreateObjectException(e.getMessage(), e);
+		}
+	}
+
+	public Object getTransferable() {
+		Identifier_Transferable[] kis_ids = new Identifier_Transferable[this.kiss.size()];
+		int i = 0;
+		for (Iterator iterator = this.kiss.iterator(); iterator.hasNext();)
+			kis_ids[i++] = (Identifier_Transferable)((KIS)iterator.next()).getId().getTransferable();
+
+		return new MCM_Transferable((Identifier_Transferable)super.getId().getTransferable(),
+																(Identifier_Transferable)this.server_id.getTransferable(),
+																new String(this.name),
+																new String(this.description),
+																new String(this.location),
+																new String(this.contact),
+																new String(this.hostname),
+																this.created.getTime(),
+																this.modified.getTime(),
+																kis_ids);
+	}
+
+	public Identifier getServerId() {
+		return this.server_id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public String getLocation() {
+		return this.location;
+	}
+
+	public String getContact() {
+		return this.contact;
+	}
+
+	public String getHostname() {
+		return this.hostname;
+	}
+
+	public Date getCreated() {
+		return this.created;
+	}
+
+	public Date getModified() {
+		return this.modified;
+	}
+
+	public ArrayList getKISs() {
+		return this.kiss;
+	}
+
+	protected synchronized void setAttributes(Identifier server_id,
+																						String name,
+																						String description,
+																						String location,
+																						String contact,
+																						String hostname,
+																						Date created,
+																						Date modified) {
+		this.server_id = server_id;
+		this.name = name;
+		this.description = description;
+		this.location = location;
+		this.contact = contact;
+		this.hostname = hostname;
+		this.created = created;
+		this.modified = modified;
+	}
+
+	protected synchronized void setKISs(ArrayList arraylist) {
+		this.kiss = arraylist;
+	}
+}
---
+++ AMFICOM/v2/administration/src/com/syrus/AMFICOM/administration/User.java
@@ -0,0 +1,133 @@
+package com.syrus.AMFICOM.administration;
+
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Iterator;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.StorableObject;
+import com.syrus.AMFICOM.general.StorableObject_Database;
+import com.syrus.AMFICOM.general.StorableObject_DatabaseContext;
+import com.syrus.AMFICOM.general.TransferableObject;
+import com.syrus.AMFICOM.general.CreateObjectException;
+import com.syrus.AMFICOM.general.RetrieveObjectException;
+import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
+import com.syrus.AMFICOM.administration.corba.User_Transferable;
+
+public class User extends StorableObject {
+	private String login;
+	private String type;
+	private Date last_logged;
+	private Date logged;
+	private int sessions;
+
+	private ArrayList category_ids;
+	private ArrayList group_ids;
+
+	private StorableObject_Database userDatabase;
+
+	public User(Identifier id) throws RetrieveObjectException {
+		super(id);
+
+		this.userDatabase = StorableObject_DatabaseContext.userDatabase;
+		try {
+			this.userDatabase.retrieve(this);
+		}
+		catch (Exception e) {
+			throw new RetrieveObjectException(e.getMessage(), e);
+		}
+	}
+
+	public User(User_Transferable ut) throws CreateObjectException {
+		super(new Identifier(ut.id));
+		this.login = new String(ut.login);
+		this.type = new String(ut.type);
+		this.last_logged = new Date(ut.last_login);
+		this.logged = new Date(ut.logged);
+		this.sessions = ut.sessions;
+
+		this.category_ids = new ArrayList(ut.category_ids.length);
+		for (int i = 0; i < ut.category_ids.length; i++)
+			this.category_ids.add(new Identifier(ut.category_ids[i]));
+
+		this.group_ids = new ArrayList(ut.group_ids.length);
+		for (int i = 0; i < ut.group_ids.length; i++)
+			this.group_ids.add(new Identifier(ut.group_ids[i]));
+
+		this.userDatabase = StorableObject_DatabaseContext.userDatabase;
+		try {
+			this.userDatabase.insert(this);
+		}
+		catch (Exception e) {
+			throw new CreateObjectException(e.getMessage(), e);
+		}
+	}
+
+	public Object getTransferable() {
+		int i = 0;
+		Identifier_Transferable[] cat_ids = new Identifier_Transferable[this.category_ids.size()];
+		for (Iterator iterator = this.category_ids.iterator(); iterator.hasNext();)
+			cat_ids[i++] = (Identifier_Transferable)((Identifier)iterator.next()).getTransferable();
+
+		i = 0;
+		Identifier_Transferable[] gr_ids = new Identifier_Transferable[this.group_ids.size()];
+		for (Iterator iterator = this.group_ids.iterator(); iterator.hasNext();)
+			gr_ids[i++] = (Identifier_Transferable)((Identifier)iterator.next()).getTransferable();
+
+		return new User_Transferable((Identifier_Transferable)this.id.getTransferable(),
+																 new String(this.login),
+																 new String(this.type),
+																 this.last_logged.getTime(),
+																 this.logged.getTime(),
+																 this.sessions,
+																 cat_ids,
+																 gr_ids);
+	}
+
+	public String getLogin() {
+		return this.login;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public Date getLastLogged() {
+		return this.last_logged;
+	}
+
+	public Date getLogged() {
+		return this.logged;
+	}
+
+	public int getSessions() {
+		return this.sessions;
+	}
+
+	public ArrayList getCategoryIds() {
+		return this.category_ids;
+	}
+
+	public ArrayList getGroupIds() {
+		return this.group_ids;
+	}
+
+	protected void setAttributes(String login,
+															 String type,
+															 Date last_logged,
+															 Date logged,
+															 int sessions) {
+		this.login = login;
+		this.type = type;
+		this.last_logged = last_logged;
+		this.logged = logged;
+		this.sessions = sessions;
+	}
+
+	protected void setCategoryIds(ArrayList category_ids) {
+		this.category_ids = category_ids;
+	}
+
+	protected void setGroupIds(ArrayList group_ids) {
+		this.group_ids = group_ids;
+	}
+}
