4f45b3288cb8fe655702cbfcb6fd0ca7243c8bc2
Arseniy Tashoyan <tashoyan@gmail.com>
2004-05-08 22:40:32 +0400

    Imported database_v1 sources

---
+++ AMFICOM/v1/database/schema/measurement/type/DROP.sql
@@ -0,0 +1,13 @@
+DROP TABLE mnttypanatypevatyp;
+DROP TABLE evatyppartyplink;
+DROP TABLE anatyppartyplink;
+DROP TABLE mnttyppartyplink;
+DROP TABLE evaluationtype;
+DROP TABLE analysistype;
+DROP TABLE measurementtype;
+DROP TABLE parametertype;
+
+DROP SEQUENCE evaluationtype_seq;
+DROP SEQUENCE analysistype_seq;
+DROP SEQUENCE measurementtype_seq;
+DROP SEQUENCE parametertype_seq;
