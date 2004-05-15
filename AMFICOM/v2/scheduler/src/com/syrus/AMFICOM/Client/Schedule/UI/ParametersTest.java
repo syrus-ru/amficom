eeffe7f47409040f44262cce51d8f39d730d3a21
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-05-15 09:14:58 +0400

    start cvs

---
+++ AMFICOM/v2/scheduler/src/com/syrus/AMFICOM/Client/Schedule/UI/ParametersTest.java
@@ -0,0 +1,22 @@
+/*
+ * ParametersTestPanel.java
+ * Created on 29.04.2004 11:44:40
+ * 
+ */
+package com.syrus.AMFICOM.Client.Schedule.UI;
+
+import com.syrus.AMFICOM.Client.Resource.Result.*;
+
+/**
+ * @author Vladimir Dolzhenko
+ * 
+ * provide interface for varius parameter test panel 
+ * i.g. ReflectometryTestPanel
+ */
+public interface ParametersTest {
+	//String COMMAND_SEND_TEST = "SendTest";  
+	public TestArgumentSet getParameters();	
+	public String getPanelName();
+	public void setTest(Test test);
+	
+}
