644a3e125733297260cd64443bf1f2c5f977cadf
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-05-16 14:07:46 +0400

    code cleaning,formating

--- AMFICOM/v2/scheduler/src/com/syrus/AMFICOM/Client/Schedule/UI/TimeParametersFrame.java
+++ AMFICOM/v2/scheduler/src/com/syrus/AMFICOM/Client/Schedule/UI/TimeParametersFrame.java
@@ -4,26 +4,23 @@ import java.awt.*;
 
 import javax.swing.*;
 
-import com.syrus.AMFICOM.CORBA.General.*;
 import com.syrus.AMFICOM.Client.General.Event.*;
 import com.syrus.AMFICOM.Client.General.Model.*;
 import com.syrus.AMFICOM.Client.Resource.Result.*;
-import com.syrus.AMFICOM.Client.Resource.Test.TestType;
 import com.syrus.AMFICOM.Client.Resource.*;
 import com.syrus.AMFICOM.Client.Schedule.ScheduleMainFrame;
-import com.syrus.util.*;
 
 public class TimeParametersFrame extends JInternalFrame implements
 		OperationListener {
 
-	private ApplicationContext	aContext;
+//	private ApplicationContext	aContext;
 
 	private Dispatcher			dispatcher;
 
 	private TimeParametersPanel	panel;
 
 	public TimeParametersFrame(ApplicationContext aContext) {
-		this.aContext = aContext;
+//		this.aContext = aContext;
 		initModule(aContext.getDispatcher());
 		setTitle("Временные параметры");
 		setFrameIcon(new ImageIcon(Toolkit.getDefaultToolkit().getImage(
@@ -45,8 +42,8 @@ public class TimeParametersFrame extends JInternalFrame implements
 		if (ScheduleMainFrame.DEBUG)
 				System.out.println(getClass().getName() + " commandName: "
 						+ commandName);
-		int id = ae.getID();
-		Object obj = ae.getSource();
+//		int id = ae.getID();
+//		Object obj = ae.getSource();
 
 		if (commandName.equals(TestUpdateEvent.typ)) {
 			TestUpdateEvent tue = (TestUpdateEvent) ae;
