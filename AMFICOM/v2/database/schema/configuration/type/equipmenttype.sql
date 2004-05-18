592ea4a2fe5e437bce2a3c00004527b800d4b63c
Arseniy Tashoyan <tashoyan@gmail.com>
2004-05-18 15:56:46 +0400

    added eqipment

---
+++ AMFICOM/v2/database/schema/configuration/type/equipmenttype.sql
@@ -0,0 +1,18 @@
+CREATE TABLE EquipmentType (
+ id NUMBER(20, 0),
+ created DATE NOT NULL,
+ modified DATE NOT NULL,
+ creator_id NUMBER(20, 0) NOT NULL,
+ modifier_id NUMBER(20, 0) NOT NULL,
+
+ codename VARCHAR2(32) NOT NULL,
+ description VARCHAR2(256),
+
+ CONSTRAINT eqptype_pk PRIMARY KEY (id) ENABLE,
+ CONSTRAINT eqptype_creator_fk FOREIGN KEY (creator_id)
+  REFERENCES Users (id) ON DELETE CASCADE ENABLE,
+ CONSTRAINT eqptype_modifier_fk FOREIGN KEY (modifier_id)
+  REFERENCES Users (id) ON DELETE CASCADE ENABLE
+);
+
+CREATE SEQUENCE equipmenttype_seq ORDER;
