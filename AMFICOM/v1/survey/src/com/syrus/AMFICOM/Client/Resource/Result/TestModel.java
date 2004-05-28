413adcec307462f3e180bc05fec7f6609d1f3c7c
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-05-28 18:22:56 +0400

    recast to getter/setter accessor, deprecate public fields

--- AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/Resource/Result/TestModel.java
+++ AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/Resource/Result/TestModel.java
@@ -16,24 +16,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Enumeration;
 
-public class TestModel extends ObjectResourceModel
-{
-	public Test test;
-  public TestModel(Test test)
-  {
-	this.test = test;
+public class TestModel extends ObjectResourceModel {
+
+	/**
+	 * @deprecated use setter/getter pair to access this field
+	 */
+	public Test	test;
+
+	public TestModel(Test test) {
+		this.test = test;
 	}
 
-	public String getColumnValue(String col_id)
-	{
+	public String getColumnValue(String col_id) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 		String s = "";
-		try
-		{
-			if(col_id.equals("status"))
-			{
-				switch(test.status.value())
-				{
+		try {
+			if (col_id.equals("status")) {
+				switch (test.status.value()) {
 					case TestStatus._TEST_STATUS_PROCESSING:
 						s = LangModelSurvey.String("labelDoing");
 						break;
@@ -45,44 +44,37 @@ public class TestModel extends ObjectResourceModel
 						break;
 				}
 			}
-			if(col_id.equals("local_id"))
-			{
+			if (col_id.equals("local_id")) {
 				TransmissionPath path;
-				for(Enumeration e = Pool.getHash(TransmissionPath.typ).elements();
-					e.hasMoreElements();)
-				{
-					path = (TransmissionPath )e.nextElement();
-					if(path.monitored_element_id.equals(test.monitored_element_id))
-					{
+				for (Enumeration e = Pool.getHash(TransmissionPath.typ)
+						.elements(); e.hasMoreElements();) {
+					path = (TransmissionPath) e.nextElement();
+					if (path.monitored_element_id
+							.equals(test.monitored_element_id)) {
 						s = path.getName();
 						break;
 					}
 				}
-//				s = test.local_id;
+				//				s = test.local_id;
 			}
-			if(col_id.equals("port_id"))
-			{
+			if (col_id.equals("port_id")) {
 				TransmissionPath path;
-				for(Enumeration e = Pool.getHash(TransmissionPath.typ).elements();
-					e.hasMoreElements();)
-				{
-					path = (TransmissionPath )e.nextElement();
-					if(path.monitored_element_id.equals(test.monitored_element_id))
-					{
+				for (Enumeration e = Pool.getHash(TransmissionPath.typ)
+						.elements(); e.hasMoreElements();) {
+					path = (TransmissionPath) e.nextElement();
+					if (path.monitored_element_id
+							.equals(test.monitored_element_id)) {
 						s = Pool.getName("accessport", path.access_port_id);
 						break;
 					}
 				}
-//				s = test.local_id;
+				//				s = test.local_id;
 			}
-			if(col_id.equals("kis_id"))
-				s = Pool.getName("kis", test.kis_id);
-			if(col_id.equals("start_time"))
-				s = sdf.format(new Date(test.start_time));
-			if(col_id.equals("temporal_type"))
-			{
-				switch(test.temporal_type.value())
-				{
+			if (col_id.equals("kis_id")) s = Pool.getName("kis", test.kis_id);
+			if (col_id.equals("start_time"))
+					s = sdf.format(new Date(test.start_time));
+			if (col_id.equals("temporal_type")) {
+				switch (test.temporal_type.value()) {
 					case TestTemporalType._TEST_TEMPORAL_TYPE_ONETIME:
 						s = LangModelSurvey.String("labelOnetime");
 						break;
@@ -94,40 +86,49 @@ public class TestModel extends ObjectResourceModel
 						break;
 				}
 			}
-			if(col_id.equals("test_type_id"))
-				s = Pool.getName("testtype", test.test_type_id);
-			if(col_id.equals("request_id"))
-				s = Pool.getName("testrequest", test.request_id);
-		}
-		catch(Exception e)
-		{
+			if (col_id.equals("test_type_id"))
+					s = Pool.getName("testtype", test.test_type_id);
+			if (col_id.equals("request_id"))
+					s = Pool.getName("testrequest", test.request_id);
+		} catch (Exception e) {
 			System.out.println("error gettin field value - Test");
 			s = "";
 		}
 		return s;
 	}
 
-	public Component getColumnRenderer(String col_id)
-	{
+	public Component getColumnRenderer(String col_id) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yy HH:mm:ss");
 
-		if(col_id.equals("status"))
-			return new TextFieldEditor(getColumnValue("status"));
-		if(col_id.equals("kis_id"))
-			return new TextFieldEditor(test.kis_id);
-		if(col_id.equals("start_time"))
-			return new TextFieldEditor(sdf.format(new Date(test.start_time)));
-		if(col_id.equals("temporal_type"))
-			return new TextFieldEditor(getColumnValue("temporal_type"));
-		if(col_id.equals("test_type_id"))
-			return new ObjectResourceComboBox("testtype", test.test_type_id);
-		if(col_id.equals("request_id"))
-			return new ObjectResourceComboBox("testrequest", test.request_id);
+		if (col_id.equals("status"))
+				return new TextFieldEditor(getColumnValue("status"));
+		if (col_id.equals("kis_id")) return new TextFieldEditor(test.kis_id);
+		if (col_id.equals("start_time"))
+				return new TextFieldEditor(sdf
+						.format(new Date(test.start_time)));
+		if (col_id.equals("temporal_type"))
+				return new TextFieldEditor(getColumnValue("temporal_type"));
+		if (col_id.equals("test_type_id"))
+				return new ObjectResourceComboBox("testtype", test.test_type_id);
+		if (col_id.equals("request_id"))
+				return new ObjectResourceComboBox("testrequest",
+						test.request_id);
 		return null;
 	}
 
-	public Component getColumnEditor(String col_id)
-	{
+	public Component getColumnEditor(String col_id) {
 		return getColumnRenderer(col_id);
+	}
+	/**
+	 * @return Returns the test.
+	 */
+	public Test getTest() {
+		return test;
+	}
+	/**
+	 * @param test The test to set.
+	 */
+	public void setTest(Test test) {
+		this.test = test;
 	}
 }
