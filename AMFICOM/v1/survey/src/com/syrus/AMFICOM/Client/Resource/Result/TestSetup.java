90a4fcbe4215377cd009ed6af9e1e419a9e949b8
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-05-28 13:21:54 +0400

    *** empty log message ***

--- AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/Resource/Result/TestSetup.java
+++ AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/Resource/Result/TestSetup.java
@@ -10,12 +10,23 @@ import com.syrus.AMFICOM.Client.Resource.Test.*;
 public class TestSetup extends ObjectResource implements Serializable
 {
 	private static final long serialVersionUID = 01L;
+	/**
+	 * @deprecated
+	 */
 	public static final String typ = "testsetup";
 
 	private TestSetup_Transferable transferable;
-
+	/**
+	 * @deprecated
+	 */
 	public String id = "";
+	/**
+	 * @deprecated
+	 */
 	public String name = "";
+	/**
+	 * @deprecated
+	 */	
 	public long created = 0;
 	public long modified = 0;
 	public String created_by = "";
@@ -55,7 +66,7 @@ public class TestSetup extends ObjectResource implements Serializable
 		return id;
 	}
 
-	public String getTyp()
+	public static String getTyp()
 	{
 		return typ;
 	}
@@ -165,6 +176,162 @@ public class TestSetup extends ObjectResource implements Serializable
 
 		transferable = new TestSetup_Transferable();
 		updateLocalFromTransferable();
+	}
+	/**
+	 * @return Returns the analysisTypeId.
+	 */
+	public String getAnalysisTypeId() {
+		return analysis_type_id;
+	}
+	/**
+	 * @param analysisTypeId The analysisTypeId to set.
+	 */
+	public void setAnalysis_type_id(String analysisTypeId) {
+		this.analysis_type_id = analysisTypeId;
+	}
+	/**
+	 * @return Returns the created.
+	 */
+	public long getCreated() {
+		return created;
+	}
+	/**
+	 * @param created The created to set.
+	 */
+	public void setCreated(long created) {
+		this.created = created;
+	}
+	/**
+	 * @return Returns the createdBy.
+	 */
+	public String getCreatedBy() {
+		return created_by;
+	}
+	/**
+	 * @param createdBy The createdBy to set.
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.created_by = createdBy;
+	}
+	/**
+	 * @return Returns the criteriaSetId.
+	 */
+	public String getCriteriaSetId() {
+		return criteria_set_id;
+	}
+	/**
+	 * @param criteriaSetId The criteriaSetId to set.
+	 */
+	public void setCriteriaSetId(String criteriaSetId) {
+		this.criteria_set_id = criteriaSetId;
+	}
+	/**
+	 * @return Returns the description.
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description The description to set.
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return Returns the ethalonId.
+	 */
+	public String getEthalonId() {
+		return etalon_id;
+	}
+	/**
+	 * @param ethalonId The ethalonId to set.
+	 */
+	public void setEthalonId(String ethalonId) {
+		this.etalon_id = ethalonId;
+	}
+	/**
+	 * @return Returns the evaluationTypeId.
+	 */
+	public String getEvaluationTypeId() {
+		return evaluation_type_id;
+	}
+	/**
+	 * @param evaluationTypeId The evaluationTypeId to set.
+	 */
+	public void setEvaluationTypeId(String evaluationTypeId) {
+		this.evaluation_type_id = evaluationTypeId;
+	}
+	/**
+	 * @return Returns the modified.
+	 */
+	public long getModified() {
+		return modified;
+	}
+	/**
+	 * @param modified The modified to set.
+	 */
+	public void setModified(long modified) {
+		this.modified = modified;
+	}
+	/**
+	 * @return Returns the monitoredElementIds.
+	 */
+	public String[] getMonitoredElementIds() {
+		return monitored_element_ids;
+	}
+	/**
+	 * @param monitoredElementIds The monitoredElementIds to set.
+	 */
+	public void setMonitoredElementIds(String[] monitoredElementIds) {
+		this.monitored_element_ids = monitoredElementIds;
+	}
+	/**
+	 * @return Returns the testArgumentSetId.
+	 */
+	public String getTestArgumentSetId() {
+		return test_argument_set_id;
+	}
+	/**
+	 * @param testArgumentSetId The testArgumentSetId to set.
+	 */
+	public void setTestArgumentSetId(String testArgumentSetId) {
+		this.test_argument_set_id = testArgumentSetId;
+	}
+	/**
+	 * @return Returns the testTypeId.
+	 */
+	public String getTestTypeId() {
+		return test_type_id;
+	}
+	/**
+	 * @param testTypeId The testTypeId to set.
+	 */
+	public void settestTypeId(String testTypeId) {
+		this.test_type_id = testTypeId;
+	}
+	/**
+	 * @return Returns the thresholdSetId.
+	 */
+	public String getThresholdSetId() {
+		return threshold_set_id;
+	}
+	/**
+	 * @param thresholdSetId The thresholdSetId to set.
+	 */
+	public void setThresholdSetId(String thresholdSetId) {
+		this.threshold_set_id = thresholdSetId;
+	}
+	/**
+	 * @param id The id to set.
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @param name The name to set.
+	 */
+	public void setName(String name) {
+		this.name = name;
 	}
 }
 
