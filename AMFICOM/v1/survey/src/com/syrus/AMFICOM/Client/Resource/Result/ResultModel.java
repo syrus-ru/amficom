405f89d51e2de2c6979b514ed9d275f04716567b
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-06-02 17:53:04 +0400

    recast with getter/setter accessor,deprecate public fields and removing Vector for List

--- AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/Resource/Result/ResultModel.java
+++ AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/Resource/Result/ResultModel.java
@@ -1,80 +1,94 @@
 package com.syrus.AMFICOM.Client.Resource.Result;
 
-import com.syrus.AMFICOM.CORBA.General.TestStatus;
-import com.syrus.AMFICOM.CORBA.General.TestTemporalType;
-import com.syrus.AMFICOM.Client.General.Lang.*;
 import com.syrus.AMFICOM.Client.General.UI.*;
 import com.syrus.AMFICOM.Client.Resource.*;
-import com.syrus.AMFICOM.Client.Resource.ISM.*;
 import java.awt.*;
 import java.text.*;
 import java.util.*;
 
-public class ResultModel extends ObjectResourceModel
-{
-	public Result r;
+public class ResultModel extends ObjectResourceModel {
 
-	public ResultModel(Result r)
-	{
+	public static final String	COLUMN_NAME_ID			= "id";
+	public static final String	COLUMN_NAME_RESULT_TYPE	= "result_type";
+	public static final String	COLUMN_NAME_CREATED		= "created";
+	public static final String	COLUMN_NAME_USER_ID		= "user_id";
+	public static final String	COLUMN_NAME_ACTION_ID	= "action_id";
+
+	/**
+	 * @deprecated use setter/getter pair to access this field
+	 */
+
+	public Result				r;
+
+	public ResultModel(Result r) {
 		this.r = r;
 	}
 
-	public String getColumnValue(String col_id)
-	{
+	public String getColumnValue(String colId) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yy HH:mm:ss");
 		String s = "";
-		try
-		{
-			if(col_id.equals("id"))
-				s = r.id;
-			if(col_id.equals("result_type"))
-			{
+		try {
+			if (colId.equals(COLUMN_NAME_ID))
+				s = r.getId();
+			else if (colId.equals(COLUMN_NAME_RESULT_TYPE)) {
+				/**
+				 * @todo need to i18n !!!
+				 * XXX:  need to i18n !!!
+				 */
 				Hashtable actions = new Hashtable();
-				actions.put("analysis", "Анализ");
-				actions.put("modeling", "Моделирование");
-				actions.put("evaluation", "Оценка");
-				actions.put("test", "Тестирование");
-				actions.put("testrequest", "Запрос на тестирование");
-				s = (String)actions.get(r.result_type);
-				if (s == null)
-					s = "";
+				actions.put(Analysis.typ, "Анализ");
+				actions.put(Modeling.typ, "Моделирование");
+				actions.put(Evaluation.typ, "Оценка");
+				actions.put(Test.typ, "Тестирование");
+				actions.put(TestRequest.typ, "Запрос на тестирование");
+				s = (String) actions.get(r.getResultType());
+				if (s == null) s = "";
+			} else if (colId.equals(COLUMN_NAME_CREATED))
+				s = sdf.format(new Date(r.getModified()));
+			else if (colId.equals(COLUMN_NAME_USER_ID))
+				s = r.getUserId();
+			else if (colId.equals(COLUMN_NAME_ACTION_ID)) {
+				s = r.getActionId();
 			}
-			if(col_id.equals("created"))
-				s = sdf.format(new Date(r.elementary_start_time));
-			if(col_id.equals("user_id"))
-				s = r.user_id;
-			if(col_id.equals("action_id"))
-			{
-				s = r.action_id;
-			}
-		}
-		catch(Exception e)
-		{
-//			System.out.println("error gettin field value - Result");
+		} catch (Exception e) {
+			//			System.out.println("error gettin field value - Result");
 			s = "";
 		}
 		return s;
 	}
 
-	public Component getColumnRenderer(String col_id)
-	{
+	public Component getColumnRenderer(String colId) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yy HH:mm:ss");
 
-		if(col_id.equals("id"))
+		if (colId.equals(COLUMN_NAME_ID))
 			return new TextFieldEditor(r.getId());
-		if(col_id.equals("result_type"))
-			return new TextFieldEditor(r.result_type);
-		if(col_id.equals("created"))
-			return new TextFieldEditor(sdf.format(new Date(r.elementary_start_time)));
-		if(col_id.equals("user_id"))
-			return new TextFieldEditor(r.user_id);
-		if(col_id.equals("action_id"))
-			return new TextFieldEditor(r.action_id);
+		else if (colId.equals(COLUMN_NAME_RESULT_TYPE))
+			return new TextFieldEditor(r.getResultType());
+		else if (colId.equals(COLUMN_NAME_CREATED))
+			return new TextFieldEditor(sdf.format(new Date(r.getModified())));
+		else if (colId.equals(COLUMN_NAME_USER_ID))
+			return new TextFieldEditor(r.getUserId());
+		else if (colId.equals(COLUMN_NAME_ACTION_ID))
+				return new TextFieldEditor(r.getActionId());
 		return null;
 	}
 
-	public Component getColumnEditor(String col_id)
-	{
-		return getColumnRenderer(col_id);
+	public Component getColumnEditor(String colId) {
+		return getColumnRenderer(colId);
+	}
+
+	/**
+	 * @return Returns the result.
+	 */
+	public Result getResult() {
+		return r;
+	}
+
+	/**
+	 * @param result
+	 *            The result to set.
+	 */
+	public void setResult(Result result) {
+		this.r = result;
 	}
 }
