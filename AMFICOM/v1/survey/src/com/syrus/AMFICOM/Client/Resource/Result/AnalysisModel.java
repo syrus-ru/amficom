fcbf8726d869f8f5948b30cb9e492fbb002e437a
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-06-07 10:17:00 +0400

    recast with getter/setter accessor,deprecate public fields

--- AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/Resource/Result/AnalysisModel.java
+++ AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/Resource/Result/AnalysisModel.java
@@ -1,41 +1,37 @@
 package com.syrus.AMFICOM.Client.Resource.Result;
 
-import java.text.*;
 import java.util.*;
 
 import com.syrus.AMFICOM.Client.Resource.*;
+import com.syrus.AMFICOM.Client.Survey.General.ConstStorage;
 
-public class AnalysisModel extends ObjectResourceModel
-{
-  Analysis a;
+public class AnalysisModel extends ObjectResourceModel {
 
-  public AnalysisModel(Analysis analysis)
-  {
-    a = analysis;
-  }
+	private Analysis	analysis;
 
-  public String getColumnValue(String col_id)
-  {
-    SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yy hh:mm:ss");
-    String s = "";
-    try
-    {
-      if(col_id.equals("id"))
-        s = a.getId();
-      if(col_id.equals("name"))
-        s = a.getName();
-      if(col_id.equals("modified"))
-        s = sdf.format(new Date(a.modified));
-      if(col_id.equals("user_id"))
-        s = a.user_id;
-      if(col_id.equals("description"))
-        s = a.description;
-    }
-    catch(Exception e)
-    {
-//      System.out.println("error gettin field value - Analysis");
-      s = "";
-    }
-    return s;
+	public AnalysisModel(Analysis analysis) {
+		this.analysis = analysis;
+	}
+
+	public String getColumnValue(String colId) {
+
+		String s = null;
+		try {
+			if (colId.equals(ConstStorage.COLUMN_NAME_ID))
+				s = this.analysis.getId();
+			else if (colId.equals(ConstStorage.COLUMN_NAME_NAME))
+				s = this.analysis.getName();
+			else if (colId.equals(ConstStorage.COLUMN_NAME_MODIFIED))
+				s = ConstStorage.SIMPLE_DATE_FORMAT.format(new Date(
+						this.analysis.getModified()));
+			else if (colId.equals(ConstStorage.COLUMN_NAME_USER_ID))
+				s = this.analysis.getUserId();
+			else if (colId.equals(ConstStorage.COLUMN_NAME_DESCRIPTION))
+					s = this.analysis.getDescription();
+		} catch (Exception e) {
+			//      System.out.println("error gettin field value - Analysis");
+			// s = "";
+		}
+		return s;
 	}
 }
