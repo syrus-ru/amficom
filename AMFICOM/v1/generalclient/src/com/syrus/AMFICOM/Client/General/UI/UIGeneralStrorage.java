855ef489a1ea2d263f7f40427295b17f737c9e09
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-06-10 14:22:06 +0400

    start cvs

---
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/General/UI/UIGeneralStrorage.java
@@ -0,0 +1,16 @@
+/*
+ * UIGeneralStrorage.java
+ * Created on 10.06.2004 13:55:54
+ * 
+ */
+package com.syrus.AMFICOM.Client.General.UI;
+
+import java.awt.Font;
+
+
+/**
+ * @author Vladimir Dolzhenko
+ */
+public class UIGeneralStrorage {
+	public static final Font	DIALOG_FONT	= new Font("Dialog", Font.PLAIN, 11);
+}
