29fb59449de4120310c280583320949c17e63230
Arseniy Tashoyan <tashoyan@gmail.com>
2004-06-22 17:20:11 +0400

    *** empty log message ***

---
+++ AMFICOM/v1/database/schema/measurement/type/measurementtype.sql
@@ -0,0 +1,19 @@
+CREATE TABLE MeasurementType (
+ id VARCHAR2(32),
+ created DATE NOT NULL,
+ modified DATE NOT NULL,
+ creator_id VARCHAR2(32) NOT NULL,
+ modifier_id VARCHAR2(32) NOT NULL,
+--
+ codename VARCHAR2(32) NOT NULL,
+ description VARCHAR2(256),
+--
+ CONSTRAINT mnttype_pk PRIMARY KEY (id),
+ CONSTRAINT mnttype_uniq UNIQUE (codename),
+ CONSTRAINT mnttype_creator_fk FOREIGN KEY (creator_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+ CONSTRAINT mnttype_modifier_fk FOREIGN KEY (modifier_id)
+  REFERENCES Users (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE measurementtype_seq ORDER;
