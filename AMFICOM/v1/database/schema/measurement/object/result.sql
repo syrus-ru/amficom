45232d8a1ec2e7b6191f8ee5bb22b9b935eaab09
Arseniy Tashoyan <tashoyan@gmail.com>
2004-06-22 17:48:59 +0400

    *** empty log message ***

---
+++ AMFICOM/v1/database/schema/measurement/object/result.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Result (
+ id VARCHAR2(32),
+ created DATE NOT NULL,
+ modified DATE NOT NULL,
+ creator_id VARCHAR2(32) NOT NULL,
+ modifier_id VARCHAR2(32) NOT NULL,
+--
+ measurement_id VARCHAR2(32) NOT NULL,
+ analysis_id VARCHAR2(32),
+ evaluation_id VARCHAR2(32),
+ sort NUMBER(2, 0) NOT NULL,
+ alarm_level NUMBER(2, 0) NOT NULL,
+--
+ CONSTRAINT res_pk PRIMARY KEY (id),
+ CONSTRAINT res_creator_fk FOREIGN KEY (creator_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+ CONSTRAINT res_modifier_fk FOREIGN KEY (modifier_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+--
+ CONSTRAINT res_mnt_fk FOREIGN KEY (measurement_id)
+  REFERENCES Measurement (id) ON DELETE CASCADE,
+ CONSTRAINT res_ana_fk FOREIGN KEY (analysis_id)
+  REFERENCES Analysis (id) ON DELETE CASCADE,
+ CONSTRAINT res_eva_fk FOREIGN KEY (evaluation_id)
+  REFERENCES Evaluation (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE result_seq ORDER;
