777c9478fba16d08af4a8522c1371dc6f4285a2a
Arseniy Tashoyan <tashoyan@gmail.com>
2004-06-22 17:27:34 +0400

    *** empty log message ***

---
+++ AMFICOM/v1/database/schema/measurement/type/mnttyppartyplink.sql
@@ -0,0 +1,13 @@
+CREATE TABLE MntTypParTypLink (
+ measurement_type_id VARCHAR2(32) NOT NULL,
+ parameter_type_id VARCHAR2(32) NOT NULL,
+ parameter_mode VARCHAR2(3) NOT NULL,
+--
+ CONSTRAINT mnttpartlnk_mntt_fk FOREIGN KEY (measurement_type_id)
+  REFERENCES MeasurementType (id) ON DELETE CASCADE,
+ CONSTRAINT mnttpartlnk_part_fk FOREIGN KEY (parameter_type_id)
+  REFERENCES ParameterType (id) ON DELETE CASCADE,
+--
+ CONSTRAINT mnttpartlnk_chk CHECK (parameter_mode = 'IN'
+  OR parameter_mode = 'OUT')
+);
