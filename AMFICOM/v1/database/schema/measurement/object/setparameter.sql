20321b3ccbb208b64c356553303c8f67f1a34326
Arseniy Tashoyan <tashoyan@gmail.com>
2004-06-22 16:22:02 +0400

    *** empty log message ***

---
+++ AMFICOM/v1/database/schema/measurement/object/setparameter.sql
@@ -0,0 +1,15 @@
+CREATE TABLE SetParameter (
+ id VARCHAR2(32),
+ type_id VARCHAR2(32) NOT NULL,
+ set_id VARCHAR2(32) NOT NULL,
+ value BLOB NOT NULL,
+--
+ CONSTRAINT spar_pk PRIMARY KEY (id),
+--
+ CONSTRAINT spar_partype_fk FOREIGN KEY (type_id)
+  REFERENCES ParameterType (id) ON DELETE CASCADE,
+ CONSTRAINT spar_sett_fk FOREIGN KEY (set_id)
+  REFERENCES Sett (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE setparameter_seq ORDER;
