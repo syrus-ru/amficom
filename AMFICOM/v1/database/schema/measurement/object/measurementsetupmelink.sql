796fd0614ba1951600c51228d4be7bc31e078e46
Arseniy Tashoyan <tashoyan@gmail.com>
2004-06-22 17:48:59 +0400

    Imported database_v1 sources

---
+++ AMFICOM/v1/database/schema/measurement/object/measurementsetupmelink.sql
@@ -0,0 +1,9 @@
+CREATE TABLE MeasurementSetupMELink (
+ measurement_setup_id VARCHAR2(32) NOT NULL,
+ monitored_element_id VARCHAR2(32) NOT NULL,
+--
+ CONSTRAINT mntspmelink_mntsp_fk FOREIGN KEY (measurement_setup_id)
+  REFERENCES MeasurementSetup (id) ON DELETE CASCADE,
+ CONSTRAINT mntspmelink_me_fk FOREIGN KEY (monitored_element_id)
+  REFERENCES MonitoredElement (id) ON DELETE CASCADE
+);
