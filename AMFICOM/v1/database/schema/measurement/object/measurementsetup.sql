b574aaae2a0dbca946d27854ab9d5170b95ea646
Arseniy Tashoyan <tashoyan@gmail.com>
2004-06-22 16:28:16 +0400

    *** empty log message ***

---
+++ AMFICOM/v1/database/schema/measurement/object/measurementsetup.sql
@@ -0,0 +1,31 @@
+CREATE TABLE MeasurementSetup (
+ id VARCHAR2(32),
+ created DATE NOT NULL,
+ modified DATE NOT NULL,
+ creator_id VARCHAR2(32) NOT NULL,
+ modifier_id VARCHAR2(32) NOT NULL,
+--
+ parameter_set_id VARCHAR2(32) NOT NULL,
+ criteria_set_id VARCHAR2(32),
+ threshold_set_id VARCHAR2(32),
+ etalon_id VARCHAR2(32),
+ description VARCHAR2(256),
+ measurement_duration NUMBER(20),
+--
+ CONSTRAINT mntsetup_pk PRIMARY KEY (id),
+ CONSTRAINT mntsetup_creator_fk FOREIGN KEY (creator_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+ CONSTRAINT mntsetup_modifier_fk FOREIGN KEY (modifier_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+--
+ CONSTRAINT mntsetup_parset_fk FOREIGN KEY (parameter_set_id)
+  REFERENCES Sett (id) ON DELETE CASCADE,
+ CONSTRAINT mntsetup_criset_fk FOREIGN KEY (criteria_set_id)
+  REFERENCES Sett (id) ON DELETE CASCADE,
+ CONSTRAINT mntsetup_thrset_fk FOREIGN KEY (threshold_set_id)
+  REFERENCES Sett (id) ON DELETE CASCADE,
+ CONSTRAINT mntsetup_etalon_fk FOREIGN KEY (etalon_id)
+  REFERENCES Sett (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE measurementsetup_seq ORDER;
