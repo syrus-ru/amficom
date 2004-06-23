016a2668873b7afa5f0080b7ac63234c46fd000f
Arseniy Tashoyan <tashoyan@gmail.com>
2004-06-23 11:10:23 +0400

    *** empty log message ***

---
+++ AMFICOM/v1/database/schema/administration/object/CREATE.sql
@@ -0,0 +1,5 @@
+@@users
+@@domain
+@@server
+@@mcm
+@@serverproperty
---
+++ AMFICOM/v1/database/schema/administration/object/DROP.sql
@@ -0,0 +1,10 @@
+DROP TABLE ServerProperty;
+DROP TABLE mcm;
+DROP TABLE Server;
+DROP TABLE Domain;
+DROP TABLE Users;
+
+DROP SEQUENCE mcm_seq;
+DROP SEQUENCE server_seq;
+DROP SEQUENCE domain_seq;
+DROP SEQUENCE users_seq;
