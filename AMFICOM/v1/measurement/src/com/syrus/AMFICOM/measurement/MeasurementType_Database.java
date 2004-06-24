d42edecb77f8ae890ffda4535820bbf4452c0dbc
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-06-24 16:54:55 +0400

    *** empty log message ***

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/MeasurementType_Database.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/MeasurementType_Database.java
@@ -2,6 +2,7 @@ package com.syrus.AMFICOM.measurement;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,6 +17,11 @@ import com.syrus.util.database.DatabaseDate;
 public class MeasurementType_Database extends StorableObject_Database  {
 	public static final String MODE_IN = "IN";
 	public static final String MODE_OUT = "OUT";
+	
+	public static final String	COLUMN_CODENAME					= "codename";
+	public static final String	COLUMN_DESCRIPTION				= "description";
+	
+	public static final String	LINK_COLUMN_MEASUREMENT_TYPE_ID	= "measurement_type_id";
 
 	private MeasurementType fromStorableObject(StorableObject storableObject) throws Exception {
 		if (storableObject instanceof MeasurementType)
@@ -31,16 +37,25 @@ public class MeasurementType_Database extends StorableObject_Database  {
 	}
 
 	private void retrieveMeasurementType(MeasurementType measurementType) throws Exception {
-		String measurement_type_id_str = measurementType.getId().toString();
-		String sql = "SELECT "
-			+ DatabaseDate.toQuerySubString("created") + ", " 
-			+ DatabaseDate.toQuerySubString("modified") + ", "
-			+ "creator_id, "
-			+ "modifier_id, "
-			+ "codename, "
-			+ "description"
-			+ " FROM " + ObjectEntities.MEASUREMENTTYPE_ENTITY
-			+ " WHERE id = " + measurement_type_id_str;
+		String measurement_type_id_str = measurementType.getId().toSQLString();
+		String sql = SQL_SELECT
+			+ DatabaseDate.toQuerySubString(COLUMN_CREATED) 
+			+ COMMA 
+			+ DatabaseDate.toQuerySubString(COLUMN_MODIFIED) 
+			+ COMMA
+			+ COLUMN_CREATOR_ID
+			+ COMMA
+			+ COLUMN_MODIFIER_ID
+			+ COMMA
+			+ COLUMN_CREATED
+			+ COMMA
+			+ COLUMN_DESCRIPTION
+			+ SQL_FROM
+			+ ObjectEntities.MEASUREMENTTYPE_ENTITY
+			+ SQL_WHERE
+			+ COLUMN_ID 
+			+ EQUALS
+			+ measurement_type_id_str;
 		Statement statement = null;
 		ResultSet resultSet = null;
 		try {
@@ -48,12 +63,20 @@ public class MeasurementType_Database extends StorableObject_Database  {
 			Log.debugMessage("MeasurementType_Database.retrieveMeasurementType | Trying: " + sql, Log.DEBUGLEVEL05);
 			resultSet = statement.executeQuery(sql);
 			if (resultSet.next())
-				measurementType.setAttributes(DatabaseDate.fromQuerySubString(resultSet, "created"),
-																			DatabaseDate.fromQuerySubString(resultSet, "modified"),
-																			new Identifier(resultSet.getLong("creator_id")),
-																			new Identifier(resultSet.getLong("modifier_id")),
-																			resultSet.getString("codename"),
-																			resultSet.getString("description"));
+				measurementType.setAttributes(DatabaseDate.fromQuerySubString(resultSet, COLUMN_CREATED),
+																			DatabaseDate.fromQuerySubString(resultSet, COLUMN_MODIFIED),
+																			/**
+																			 * @todo when change DB Identifier model ,change getString() to
+																			 *       getLong()
+																			 */																			
+																			new Identifier(resultSet.getString(COLUMN_CREATOR_ID)),
+																			/**
+																			 * @todo when change DB Identifier model ,change getString() to
+																			 *       getLong()
+																			 */
+																			new Identifier(resultSet.getString(COLUMN_MODIFIER_ID)),
+																			resultSet.getString(COLUMN_CODENAME),
+																			resultSet.getString(COLUMN_DESCRIPTION));
 			else
 				throw new Exception("No such measurement type: " + measurement_type_id_str);
 		}
@@ -78,12 +101,16 @@ public class MeasurementType_Database extends StorableObject_Database  {
 		ArrayList in_par_typs = new ArrayList();
 		ArrayList out_par_typs = new ArrayList();
 
-		String measurement_type_id_str = measurementType.getId().toString();
-		String sql = "SELECT "
-			+ "parameter_type_id, "
-			+ "parameter_mode"
-			+ " FROM " + ObjectEntities.MNTTYPPARTYPLINK_ENTITY
-			+ " WHERE measurement_type_id = " + measurement_type_id_str;
+		String measurement_type_id_str = measurementType.getId().toSQLString();
+		String sql = SQL_SELECT
+			+ LINK_COLUMN_PARAMETER_TYPE_ID
+			+ LINK_COLUMN_PARAMETER_MODE
+			+ SQL_FROM 
+			+ ObjectEntities.MNTTYPPARTYPLINK_ENTITY
+			+ SQL_WHERE
+			+ LINK_COLUMN_MEASUREMENT_TYPE_ID
+			+ EQUALS
+			+ measurement_type_id_str;
 		Statement statement = null;
 		ResultSet resultSet = null;
 		try {
@@ -91,10 +118,17 @@ public class MeasurementType_Database extends StorableObject_Database  {
 			Log.debugMessage("MeasurementType_Database.retrieveParameterTypes | Trying: " + sql, Log.DEBUGLEVEL05);
 			resultSet = statement.executeQuery(sql);
 			String parameter_mode;
-			long parameter_type_id_code;
+			/**
+			 * @todo when change DB Identifier model ,change String to long
+			 */
+			String parameter_type_id_code;
 			while (resultSet.next()) {
-				parameter_mode = resultSet.getString("parameter_mode");
-				parameter_type_id_code = resultSet.getLong("parameter_type_id");
+				parameter_mode = resultSet.getString(LINK_COLUMN_PARAMETER_MODE);
+				/**
+				 * @todo when change DB Identifier model ,change getString() to
+				 *       getLong()
+				 */
+				parameter_type_id_code = resultSet.getString(LINK_COLUMN_PARAMETER_TYPE_ID);
 				if (parameter_mode.equals(MODE_IN))
 					in_par_typs.add(new Identifier(parameter_type_id_code));
 				else
@@ -158,18 +192,44 @@ public class MeasurementType_Database extends StorableObject_Database  {
 	}
 
 	private void insertMeasurementType(MeasurementType measurementType) throws Exception {
-		String measurement_type_id_str = measurementType.getId().toString();
-		String sql = "INSERT INTO " + ObjectEntities.MEASUREMENTTYPE_ENTITY
-			+ " (id, created, modified, creator_id, modifier_id, codename, description)"
-			+ " VALUES ("
-			+ measurement_type_id_str + ", "
-			+ DatabaseDate.toUpdateSubString(measurementType.getCreated()) + ", "
-			+ DatabaseDate.toUpdateSubString(measurementType.getModified()) + ", "
-			+ measurementType.getCreatorId().toString() + ", "
-			+ measurementType.getModifierId().toString() + ", '"
-			+ measurementType.getCodename() + "', '"
+		String measurement_type_id_str = measurementType.getId().toSQLString();
+		String sql = SQL_INSERT_INTO
+			+ ObjectEntities.MEASUREMENTTYPE_ENTITY
+			+ OPEN_BRACKET
+			+ COLUMN_ID
+			+ COMMA
+			+ COLUMN_CREATED
+			+ COMMA
+			+ COLUMN_MODIFIED
+			+ COMMA
+			+ COLUMN_CREATOR_ID
+			+ COMMA
+			+ COLUMN_MODIFIER_ID
+			+ COMMA
+			+ COLUMN_CODENAME
+			+ COMMA
+			+ COLUMN_DESCRIPTION
+			+ CLOSE_BRACKET
+			+ SQL_VALUES
+			+ OPEN_BRACKET
+			+ measurement_type_id_str 
+			+ COMMA
+			+ DatabaseDate.toUpdateSubString(measurementType.getCreated()) 
+			+ COMMA
+			+ DatabaseDate.toUpdateSubString(measurementType.getModified()) 
+			+ COMMA
+			+ measurementType.getCreatorId().toString() 
+			+ COMMA
+			+ measurementType.getModifierId().toString()
+			+ COMMA
+			+ APOSTOPHE
+			+ measurementType.getCodename() 
+			+ APOSTOPHE
+			+ COMMA
+			+ APOSTOPHE
 			+ measurementType.getDescription()
-			+ "')";
+			+ APOSTOPHE
+			+ CLOSE_BRACKET;
 		Statement statement = null;
 		try {
 			statement = connection.createStatement();
@@ -191,30 +251,66 @@ public class MeasurementType_Database extends StorableObject_Database  {
 	}
 
 	private void insertParameterTypes(MeasurementType measurementType) throws Exception {
-		ArrayList in_par_typs = measurementType.getInParameterTypes();
-		ArrayList out_par_typs = measurementType.getOutParameterTypes();
-		long measurement_type_id_code = measurementType.getId().getCode();
-		String sql = "INSERT INTO " + ObjectEntities.MNTTYPPARTYPLINK_ENTITY
-			+ " (measurement_type_id, parameter_type_id, parameter_mode)"
-			+ " VALUES (?, ?, ?)";
+		List in_par_typs = measurementType.getInParameterTypes();
+		List out_par_typs = measurementType.getOutParameterTypes();
+		/**
+		 * @todo when change DB Identifier model ,change String to long
+		 */
+		String measurement_type_id_code = measurementType.getId().getCode();
+		String sql = SQL_INSERT_INTO
+			+ ObjectEntities.MNTTYPPARTYPLINK_ENTITY
+			+ OPEN_BRACKET
+			+ LINK_COLUMN_MEASUREMENT_TYPE_ID
+			+ COMMA
+			+ LINK_COLUMN_PARAMETER_TYPE_ID
+			+ COMMA
+			+ LINK_COLUMN_PARAMETER_MODE
+			+ CLOSE_BRACKET
+			+ SQL_VALUES
+			+ OPEN_BRACKET
+			+ QUESTION
+			+ COMMA
+			+ QUESTION
+			+ COMMA
+			+ QUESTION
+			+ CLOSE_BRACKET;
 		PreparedStatement preparedStatement = null;
-		long parameter_type_id_code = 0;
+		/**
+		 * @todo when change DB Identifier model ,change String to long
+		 */
+		String parameter_type_id_code = null;
 		String parameter_mode = null;
 		try {
 			preparedStatement = connection.prepareStatement(sql);
 			for (Iterator iterator = in_par_typs.iterator(); iterator.hasNext();) {
-				preparedStatement.setLong(1, measurement_type_id_code);
+				/**
+				 * @todo when change DB Identifier model ,change setString() to
+				 *       setLong()
+				 */
+				preparedStatement.setString(1, measurement_type_id_code);
 				parameter_type_id_code = ((Identifier)iterator.next()).getCode();
-				preparedStatement.setLong(2, parameter_type_id_code);
+				/**
+				 * @todo when change DB Identifier model ,change setString() to
+				 *       setLong()
+				 */
+				preparedStatement.setString(2, parameter_type_id_code);
 				parameter_mode = MODE_IN;
 				preparedStatement.setString(3, parameter_mode);
 				Log.debugMessage("MeasurementType_Database.insertParameterTypes | Inserting parameter type " + parameter_type_id_code + " of parameter mode '" + parameter_mode + "' for measurement type " + measurement_type_id_code, Log.DEBUGLEVEL05);
 				preparedStatement.executeUpdate();
 			}
 			for (Iterator iterator = out_par_typs.iterator(); iterator.hasNext();) {
-				preparedStatement.setLong(1, measurement_type_id_code);
+				/**
+				 * @todo when change DB Identifier model ,change setString() to
+				 *       setLong()
+				 */
+				preparedStatement.setString(1, measurement_type_id_code);
 				parameter_type_id_code = ((Identifier)iterator.next()).getCode();
-				preparedStatement.setLong(2, parameter_type_id_code);
+				/**
+				 * @todo when change DB Identifier model ,change setString() to
+				 *       setLong()
+				 */
+				preparedStatement.setString(2, parameter_type_id_code);
 				parameter_mode = MODE_OUT;
 				preparedStatement.setString(3, parameter_mode);
 				Log.debugMessage("MeasurementType_Database.insertParameterTypes | Inserting parameter type " + parameter_type_id_code + " of parameter mode '" + parameter_mode + "' for measurement type " + measurement_type_id_code, Log.DEBUGLEVEL05);
@@ -244,12 +340,22 @@ public class MeasurementType_Database extends StorableObject_Database  {
 	}
 
 	public void delete(MeasurementType measurementType) {
-		String measurement_type_id_str = measurementType.getId().toString();
+		String measurement_type_id_str = measurementType.getId().toSQLString();
 		Statement statement = null;
 		try {
 			statement = connection.createStatement();
-			statement.executeUpdate("DELETE FROM " + ObjectEntities.MNTTYPPARTYPLINK_ENTITY + " WHERE measurement_type_id = " + measurement_type_id_str);
-			statement.executeUpdate("DELETE FROM " + ObjectEntities.MEASUREMENTTYPE_ENTITY + " WHERE id = " + measurement_type_id_str);
+			statement.executeUpdate(SQL_DELETE_FROM
+					+ ObjectEntities.MNTTYPPARTYPLINK_ENTITY
+					+ SQL_WHERE
+					+ LINK_COLUMN_MEASUREMENT_TYPE_ID
+					+ EQUALS
+					+ measurement_type_id_str);
+			statement.executeUpdate(SQL_DELETE_FROM
+					+ ObjectEntities.MEASUREMENTTYPE_ENTITY 
+					+ SQL_WHERE
+					+ COLUMN_ID
+					+ EQUALS 
+					+ measurement_type_id_str);
 			connection.commit();
 		}
 		catch (SQLException sqle1) {
@@ -266,18 +372,29 @@ public class MeasurementType_Database extends StorableObject_Database  {
 	}
 
 	public static MeasurementType retrieveForCodename(String codename) throws Exception {
-		String sql = "SELECT "
-			+ "id"
-			+ " FROM " + ObjectEntities.MEASUREMENTTYPE_ENTITY
-			+ " WHERE codename = '" + codename + "'";
+		String sql = SQL_SELECT
+			+ COLUMN_ID
+			+ SQL_FROM 
+			+ ObjectEntities.MEASUREMENTTYPE_ENTITY
+			+ SQL_WHERE
+			+ COLUMN_CODENAME
+			+ EQUALS
+			+ APOSTOPHE
+			+ codename 
+			+ APOSTOPHE;
 		Statement statement = null;
 		ResultSet resultSet = null;
 		try {
 			statement = connection.createStatement();
 			Log.debugMessage("MeasurementType_Database.retrieveForCodename | Trying: " + sql, Log.DEBUGLEVEL05);
 			resultSet = statement.executeQuery(sql);
-			if (resultSet.next())
-				return new MeasurementType(new Identifier(resultSet.getLong("id")));
+			if (resultSet.next()){
+				/**
+				 * @todo when change DB Identifier model ,change getString() to
+				 *       getLong()
+				 */
+				return new MeasurementType(new Identifier(resultSet.getString(COLUMN_ID)));
+			}
 			else
 				throw new Exception("No measurement type with codename: '" + codename + "'");
 		}
