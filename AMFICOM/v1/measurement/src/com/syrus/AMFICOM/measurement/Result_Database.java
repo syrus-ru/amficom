16c34240cbad0f66008c6fd7fb1ca74cdbdadbb2
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-06-25 14:40:12 +0400

    *** empty log message ***

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/Result_Database.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/Result_Database.java
@@ -18,6 +18,21 @@ import com.syrus.AMFICOM.measurement.corba.ResultSort;
 
 
 public class Result_Database extends StorableObject_Database {
+	
+//	 measurement_id VARCHAR2(32) NOT NULL,
+	public static final String COLUMN_MEASUREMENT_ID		=	"measurement_id";
+//	 analysis_id VARCHAR2(32),
+	public static final String COLUMN_ANALYSIS_ID			=	"analysis_id";
+//	 evaluation_id VARCHAR2(32),
+	public static final String COLUMN_EVALUATION_ID			=	"evaluation_id";
+//	 sort NUMBER(2, 0) NOT NULL,
+	public static final String COLUMN_SORT					=	"sort";
+//	 alarm_level NUMBER(2, 0) NOT NULL,
+	public static final String COLUMN_ALARM_LEVEL			=	"alarm_level";
+	
+	public static final String LINK_COLUMN_TYPE_ID			=	"type_id";
+	public static final String LINK_COLUMN_RESULT_ID		=	"result_id";
+	public static final String LINK_COLUMN_VALUE			=	"value";
 
 	private Result fromStorableObject(StorableObject storableObject) throws Exception {
 		if (storableObject instanceof Result)
@@ -33,19 +48,21 @@ public class Result_Database extends StorableObject_Database {
 	}
 
 	private void retrieveResult(Result result) throws Exception {
-		String result_id_str = result.getId().toString();
-		String sql = "SELECT "
-			+ DatabaseDate.toQuerySubString("created") + ", " 
-			+ DatabaseDate.toQuerySubString("modified") + ", "
-			+ "creator_id, "
-			+ "modifier_id, "
-			+ "measurement_id, "
-			+ "analysis_id, "
-			+ "evaluation_id, "
-			+ "sort, "
-			+ "alarm_level"
-			+ " FROM " + ObjectEntities.RESULT_ENTITY
-			+ " WHERE id = " + result_id_str;
+		String result_id_str = result.getId().toSQLString();
+		String sql = SQL_SELECT
+			+ DatabaseDate.toQuerySubString(COLUMN_CREATED) + COMMA 
+			+ DatabaseDate.toQuerySubString(COLUMN_MODIFIED) + COMMA
+			+ COLUMN_CREATOR_ID + COMMA
+			+ COLUMN_MODIFIER_ID + COMMA
+			+ COLUMN_MEASUREMENT_ID + COMMA
+			+ COLUMN_ANALYSIS_ID + COMMA
+			+ COLUMN_EVALUATION_ID + COMMA
+			+ COLUMN_SORT + COMMA
+			+ COLUMN_ALARM_LEVEL
+			+ SQL_FROM + ObjectEntities.RESULT_ENTITY
+			+ SQL_WHERE 
+			+ COLUMN_ID + EQUALS
+			+ result_id_str;
 		Statement statement = null;
 		ResultSet resultSet = null;
 		try {
@@ -53,30 +70,45 @@ public class Result_Database extends StorableObject_Database {
 			Log.debugMessage("Result_Database.retrieveResult | Trying: " + sql, Log.DEBUGLEVEL05);
 			resultSet = statement.executeQuery(sql);
 			if (resultSet.next()) {
-				Measurement measurement = new Measurement(new Identifier(resultSet.getLong("measurement_id")));
-				int result_sort = resultSet.getInt("sort");
+				/**
+				 * @todo when change DB Identifier model ,change getString() to getLong()
+				 */
+				Measurement measurement = new Measurement(new Identifier(resultSet.getString(COLUMN_MEASUREMENT_ID)));
+				int result_sort = resultSet.getInt(COLUMN_SORT);
 				Action action = null;
 				switch (result_sort) {
 					case ResultSort._RESULT_SORT_MEASUREMENT:
 						action = measurement;
 						break;
 					case ResultSort._RESULT_SORT_ANALYSIS:
-						action = new Analysis(new Identifier(resultSet.getLong("analysis_id")));
+						/**
+						 * @todo when change DB Identifier model ,change getString() to getLong()
+						 */
+						action = new Analysis(new Identifier(resultSet.getString(COLUMN_ANALYSIS_ID)));
 						break;
 					case ResultSort._RESULT_SORT_EVALUATION:
-						action = new Evaluation(new Identifier(resultSet.getLong("evaluation_id")));
+						/**
+						 * @todo when change DB Identifier model ,change getString() to getLong()
+						 */
+						action = new Evaluation(new Identifier(resultSet.getString(COLUMN_EVALUATION_ID)));
 						break;
 					default:
 						Log.errorMessage("Unkown sort: " + result_sort + " of result " + result_id_str);
 				}
-				result.setAttributes(DatabaseDate.fromQuerySubString(resultSet, "created"),
-														 DatabaseDate.fromQuerySubString(resultSet, "modified"),
-														 new Identifier(resultSet.getLong("creator_id")),
-														 new Identifier(resultSet.getLong("modifier_id")),
+				result.setAttributes(DatabaseDate.fromQuerySubString(resultSet, COLUMN_CREATED),
+														 DatabaseDate.fromQuerySubString(resultSet, COLUMN_MODIFIED),
+														 /**
+														  * @todo when change DB Identifier model ,change getString() to getLong()
+														  */
+														 new Identifier(resultSet.getString(COLUMN_CREATOR_ID)),
+														 /**
+														  * @todo when change DB Identifier model ,change getString() to getLong()
+														  */
+														 new Identifier(resultSet.getString(COLUMN_MODIFIER_ID)),
 														 measurement,
 														 action,
 														 result_sort,
-														 resultSet.getInt("alarm_level"));
+														 resultSet.getInt(COLUMN_ALARM_LEVEL));
 			}
 			else
 				throw new Exception("No such result: " + result_id_str);
@@ -99,13 +131,16 @@ public class Result_Database extends StorableObject_Database {
 	}
 
 	private void retrieveResultParameters(Result result) throws Exception {
-		String result_id_str = result.getId().toString();
-		String sql = "SELECT "
-			+ "id, "
-			+ "type_id, "
-			+ "value"
-			+ " FROM " + ObjectEntities.RESULTPARAMETER_ENTITY
-			+ " WHERE result_id = " + result_id_str;
+		String result_id_str = result.getId().toSQLString();
+		String sql = SQL_SELECT
+			+ COLUMN_ID + COMMA
+			+ LINK_COLUMN_TYPE_ID + COMMA
+			+ LINK_COLUMN_VALUE
+			+ SQL_FROM
+			+ ObjectEntities.RESULTPARAMETER_ENTITY
+			+ SQL_WHERE
+			+ LINK_COLUMN_RESULT_ID + EQUALS
+			+ result_id_str;
 		Statement statement = null;
 		ResultSet resultSet = null;
 		ArrayList arraylist = new ArrayList();
@@ -114,9 +149,15 @@ public class Result_Database extends StorableObject_Database {
 			Log.debugMessage("Result_Database.retrieveResultParameters | Trying: " + sql, Log.DEBUGLEVEL05);
 			resultSet = statement.executeQuery(sql);
 			while (resultSet.next())
-				arraylist.add(new SetParameter(new Identifier(resultSet.getLong("id")),
-																			 new Identifier(resultSet.getLong("type_id")),
-																			 ByteArrayDatabase.toByteArray((BLOB)resultSet.getBlob("value"))));
+							/**
+							 * @todo when change DB Identifier model ,change getString() to getLong()
+							 */
+				arraylist.add(new SetParameter(	new Identifier(resultSet.getString(COLUMN_ID)),
+							/**
+							 * @todo when change DB Identifier model ,change getString() to getLong()
+							 */
+												new Identifier(resultSet.getString(LINK_COLUMN_TYPE_ID)),
+												ByteArrayDatabase.toByteArray((BLOB)resultSet.getBlob(LINK_COLUMN_VALUE))));
 		}
 		catch (SQLException sqle) {
 			String mesg = "Result_Database.retrieveResultParameters | Cannot retrieve parameters for result " + result_id_str;
@@ -157,38 +198,85 @@ public class Result_Database extends StorableObject_Database {
 	}
 
 	private void insertResult(Result result) throws Exception {
-		String result_id_str = result.getId().toString();
-		String sql = "INSERT INTO " + ObjectEntities.RESULT_ENTITY
-			+ " (id, created, modified, creator_id, modifier_id, measurement_id, analysis_id, evaluation_id, sort, alarm_level)"
-			+ " VALUES ("
-			+ result_id_str + ", "
-			+ DatabaseDate.toUpdateSubString(result.getCreated()) + ", "
-			+ DatabaseDate.toUpdateSubString(result.getModified()) + ", "
-			+ result.getCreatorId().toString() + ", "
-			+ result.getModifierId().toString() + ", "
-			+ result.getMeasurement().getId().toString() + ", ";
+		String result_id_str = result.getId().toSQLString();
+		String sql;
+		{			
+			StringBuffer buffer = new StringBuffer(SQL_INSERT_INTO);
+			buffer.append(ObjectEntities.RESULT_ENTITY);
+			buffer.append(OPEN_BRACKET);
+			buffer.append(COLUMN_ID);
+			buffer.append(COMMA);
+			buffer.append(COLUMN_CREATED);
+			buffer.append(COMMA);
+			buffer.append(COLUMN_MODIFIED);
+			buffer.append(COMMA);
+			buffer.append(COLUMN_CREATOR_ID);
+			buffer.append(COMMA);
+			buffer.append(COLUMN_MODIFIER_ID);
+			buffer.append(COMMA);
+			buffer.append(COLUMN_MEASUREMENT_ID);
+			buffer.append(COMMA);
+			buffer.append(COLUMN_ANALYSIS_ID);
+			buffer.append(COMMA);
+			buffer.append(COLUMN_EVALUATION_ID);
+			buffer.append(COMMA);
+			buffer.append(COLUMN_SORT);
+			buffer.append(COMMA);
+			buffer.append(COLUMN_ALARM_LEVEL);
+			buffer.append(CLOSE_BRACKET);
+			buffer.append(SQL_VALUES);
+			buffer.append(OPEN_BRACKET);			
+			buffer.append(result_id_str);
+			buffer.append(COMMA);
+			buffer.append(DatabaseDate.toUpdateSubString(result.getCreated()));
+			buffer.append(COMMA);
+			buffer.append(DatabaseDate.toUpdateSubString(result.getModified()));
+			buffer.append(COMMA);
+			buffer.append(result.getCreatorId().toSQLString());
+			buffer.append(COMMA);
+			buffer.append(result.getModifierId().toSQLString());
+			buffer.append(COMMA);
+			buffer.append(result.getMeasurement().getId().toSQLString());
+			buffer.append(COMMA);
 		int result_sort = result.getSort().value();
 		switch (result_sort) {
 			case ResultSort._RESULT_SORT_MEASUREMENT:
-				sql = sql
-					+ "0, "
-					+ "0, ";
+				/**
+				 * @todo when change DB Identifier model ,change '' to 0
+				 */
+				buffer.append("''");
+				buffer.append(COMMA);
+				/**
+				 * @todo when change DB Identifier model ,change '' to 0
+				 */
+				buffer.append("''");
+				buffer.append(COMMA);
 				break;
 			case ResultSort._RESULT_SORT_ANALYSIS:
-				sql = sql
-					+ result.getAction().getId().toString() + ", "
-					+ "0, ";
+				buffer.append(result.getAction().getId().toSQLString());
+				buffer.append(COMMA);
+				/**
+				 * @todo when change DB Identifier model ,change "" to 0
+				 */
+				buffer.append("''");
+				buffer.append(COMMA);
 				break;
 			case ResultSort._RESULT_SORT_EVALUATION:
-				sql = sql
-					+ "0, "
-					+ result.getAction().getId().toString() + ", ";
+				/**
+				 * @todo when change DB Identifier model ,change '' to 0
+				 */
+				buffer.append("''");
+				buffer.append(COMMA);
+				buffer.append(result.getAction().getId().toSQLString());
+				buffer.append(COMMA);
 				break;
 		}
-		sql = sql
-			+ Integer.toString(result_sort) + ", "
-			+ Integer.toString(result.getAlarmLevel().value())
-			+ ")";
+			buffer.append(Integer.toString(result_sort));
+			buffer.append(COMMA);
+			buffer.append(Integer.toString(result.getAlarmLevel().value()));
+			buffer.append(CLOSE_BRACKET);
+			sql = buffer.toString();
+		}
 		Statement statement = null;
 		try {
 			statement = connection.createStatement();
@@ -211,24 +299,50 @@ public class Result_Database extends StorableObject_Database {
 	}
 
 	private void insertResultParameters(Result result) throws Exception {
-		long result_id_code = result.getId().getCode();
+		/**
+		 * @todo when change DB Identifier model ,change String to long
+		 */
+		String result_id_code = result.getId().getCode();
 		SetParameter[] setParameters = result.getParameters();
-		String sql = "INSERT INTO " + ObjectEntities.RESULTPARAMETER_ENTITY
-			+ " (id, type_id, result_id, value)"
-			+ " VALUES (?, ?, ?, ?)";
+		String sql = SQL_INSERT_INTO 
+			+ ObjectEntities.RESULTPARAMETER_ENTITY
+			+ OPEN_BRACKET
+			+ COLUMN_ID + COMMA
+			+ LINK_COLUMN_TYPE_ID + COMMA
+			+ LINK_COLUMN_RESULT_ID + COMMA
+			+ LINK_COLUMN_VALUE + COMMA
+			+ CLOSE_BRACKET
+			+ SQL_VALUES
+			+ OPEN_BRACKET
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION 
+			+ CLOSE_BRACKET;
 		PreparedStatement preparedStatement = null;
 		int i = 0;
 		try {
 			preparedStatement = connection.prepareStatement(sql);
 			for (i = 0; i < setParameters.length; i++) {
-				preparedStatement.setLong(1, setParameters[i].getId().getCode());
-				preparedStatement.setLong(2, setParameters[i].getTypeId().getCode());
-				preparedStatement.setLong(3, result_id_code);
+				/**
+				 * @todo when change DB Identifier model ,change setString() to setLong()
+				 */
+				preparedStatement.setString(1, setParameters[i].getId().getCode());
+				/**
+				 * @todo when change DB Identifier model ,change setString() to setLong()
+				 */
+				preparedStatement.setString(2, setParameters[i].getTypeId().getCode());
+				/**
+				 * @todo when change DB Identifier model ,change setString() to setLong()
+				 */
+				preparedStatement.setString(3, result_id_code);
 				preparedStatement.setBlob(4, BLOB.empty_lob());
 				Log.debugMessage("Result_Database.insertResultParameters | Inserting parameter " + setParameters[i].getTypeId().toString() + " for result " + result_id_code, Log.DEBUGLEVEL05);
 				preparedStatement.executeUpdate();
 				ByteArrayDatabase badb = new ByteArrayDatabase(setParameters[i].getValue());
-				badb.saveAsBlob(connection, ObjectEntities.RESULTPARAMETER_ENTITY, "value", "id = " + setParameters[i].getId().toString());
+				badb.saveAsBlob(connection, 
+								ObjectEntities.RESULTPARAMETER_ENTITY, 
+								LINK_COLUMN_VALUE, COLUMN_ID + EQUALS + setParameters[i].getId().toSQLString());
 			}
 			connection.commit();
 		}
@@ -255,12 +369,22 @@ public class Result_Database extends StorableObject_Database {
 	}
 
 	private void delete(Result result) {
-		String result_id_str = result.getId().toString();
+		String result_id_str = result.getId().toSQLString();
 		Statement statement = null;
 		try {
 			statement = connection.createStatement();
-			statement.executeUpdate("DELETE FROM " + ObjectEntities.RESULTPARAMETER_ENTITY + " WHERE result_id = " + result_id_str);
-			statement.executeUpdate("DELETE FROM " + ObjectEntities.RESULT_ENTITY + " WHERE id = " + result_id_str);
+			statement.executeUpdate(SQL_DELETE_FROM 
+									+ ObjectEntities.RESULTPARAMETER_ENTITY
+									+ SQL_WHERE
+									+ LINK_COLUMN_RESULT_ID
+									+ EQUALS
+									+ result_id_str);
+			statement.executeUpdate(SQL_DELETE_FROM 
+									+ ObjectEntities.RESULT_ENTITY
+									+ SQL_WHERE
+									+ COLUMN_ID
+									+ EQUALS
+									+ result_id_str);
 			connection.commit();
 		}
 		catch (SQLException sqle1) {
