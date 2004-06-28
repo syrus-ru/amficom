19c7487e877cd56487d3cfb744982b14abc9890f
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2004-06-28 17:24:44 +0400

    temporarily remarked prinsStackTrace for older LangModel constanta

--- AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/General/Lang/LangModelSurvey.java
+++ AMFICOM/v1/survey/src/com/syrus/AMFICOM/Client/General/Lang/LangModelSurvey.java
@@ -113,7 +113,7 @@ public class LangModelSurvey {
 								+ key + "' key.");
 				throw new Exception(s);
 			} catch (Exception exc) {
-				exc.printStackTrace();
+//				exc.printStackTrace();
 			}
 			if (key != null) string = LangModelSurvey.getString(key);
 
