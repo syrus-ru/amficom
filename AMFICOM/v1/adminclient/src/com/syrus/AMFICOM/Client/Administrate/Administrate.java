a89a5bbf53a0f56bf6ec5318633ad708b54110cf
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-06-29 15:34:56 +0400

    *** empty log message ***

--- AMFICOM/v1/adminclient/src/com/syrus/AMFICOM/Client/Administrate/Administrate.java
+++ AMFICOM/v1/adminclient/src/com/syrus/AMFICOM/Client/Administrate/Administrate.java
@@ -1,70 +1,79 @@
+/*
+ * $Id: Administrate.java,v 1.2 2004/06/29 11:34:56 bass Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
 package com.syrus.AMFICOM.Client.Administrate;
 
-import java.awt.*;
-import java.awt.event.*;
-
-import javax.swing.*;
-
-import com.syrus.AMFICOM.Client.General.Lang.*;
+import com.syrus.AMFICOM.Client.General.Lang.LangModelAdmin;
 import com.syrus.AMFICOM.Client.General.Model.*;
+import java.awt.*;
+import javax.swing.UIManager;
 
+/**
+ * @version $Revision: 1.2 $, $Date: 2004/06/29 11:34:56 $
+ * @author $Author: bass $
+ * @module admin_v1
+ */
 public class Administrate
 {
-  ApplicationContext aContext = new ApplicationContext();
+	ApplicationContext aContext = new ApplicationContext();
 
-  public Administrate(AdminApplicationModelFactory factory)
-  {
-	if(!Environment.canRun(Environment.MODULE_ADMINISTRATE))
-		return;
+	public Administrate(AdminApplicationModelFactory factory)
+	{
+		if (!Environment.canRun(Environment.MODULE_ADMINISTRATE))
+			return;
 		
-    aContext.setApplicationModel(factory.create());
+		aContext.setApplicationModel(factory.create());
 
-    Frame frame = new AdministrateMDIMain(aContext);
+		Frame frame = new AdministrateMDIMain(aContext);
 /*
-    //Center the window
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    Dimension frameSize = frame.getSize();
+		//Center the window
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension frameSize = frame.getSize();
 
-    frame.setSize(screenSize.width-40, screenSize.height-40);
+		frame.setSize(screenSize.width-40, screenSize.height-40);
 
-//    if (frameSize.height > screenSize.height)
-//    {
-//      frameSize.height = screenSize.height;
-//    }
-//    if (frameSize.width > screenSize.width)
-//    {
-//      frameSize.width = screenSize.width;
-//    }
-    frame.setLocation(20, 0);
-//    frame.addWindowListener(new WindowAdapter() { public void windowClosing(WindowEvent e) { System.exit(0); } });
+//		if (frameSize.height > screenSize.height)
+//		{
+//			frameSize.height = screenSize.height;
+//		}
+//		if (frameSize.width > screenSize.width)
+//		{
+//			frameSize.width = screenSize.width;
+//		}
+		frame.setLocation(20, 0);
+//		frame.addWindowListener(new WindowAdapter() { public void windowClosing(WindowEvent e) { System.exit(0); } });
 */
 //		Environment.addWindow(frame);
-    frame.setIconImage(Toolkit.getDefaultToolkit().getImage("images/main/administrate_mini.gif").getScaledInstance(16, 16, Image.SCALE_SMOOTH));
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("images/main/administrate_mini.gif").getScaledInstance(16, 16, Image.SCALE_SMOOTH));
 
-    frame.setVisible(true);
+		frame.setVisible(true);
+	}
 
-  }
-
-  public static void main(String[] args)
-  {
+	public static void main(String[] args)
+	{
 //		System.out.println(LangModel.resourceBundle + " vs " + LangModelAdmin.resourceBundle);
-    Environment.initialize();
-    try
-    {
-		UIManager.setLookAndFeel(Environment.getLookAndFeel());
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-    }
+		Environment.initialize();
+		try
+		{
+			UIManager.setLookAndFeel(Environment.getLookAndFeel());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 //		System.out.println(LangModel.resourceBundle + " vs " + LangModelAdmin.resourceBundle);
-    LangModelAdmin.initialize();
+		LangModelAdmin.initialize();
 
 //		System.out.println(LangModel.resourceBundle + " vs " + LangModelAdmin.resourceBundle);
 
-    // test!!!
+		// test!!!
 //		new com.syrus.AMFICOM.Client.Test.LoadTestAdmin();
 
-    new Administrate(new DefaultAdminApplicationModelFactory());
-  }
+		new Administrate(new DefaultAdminApplicationModelFactory());
+	}
 }
--- AMFICOM/v1/adminclient/src/com/syrus/AMFICOM/Client/Administrate/AdministrateRun.java
+++ AMFICOM/v1/adminclient/src/com/syrus/AMFICOM/Client/Administrate/AdministrateRun.java
@@ -1,27 +1,40 @@
+/*
+ * $Id: AdministrateRun.java,v 1.2 2004/06/29 11:34:56 bass Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
 package com.syrus.AMFICOM.Client.Administrate;
 
-import java.awt.*;
-import java.awt.event.*;
-
-import javax.swing.*;
-
-import com.syrus.AMFICOM.Client.General.Lang.*;
+import com.syrus.AMFICOM.Client.General.Lang.LangModelAdmin;
 import com.syrus.AMFICOM.Client.General.Model.*;
+import javax.swing.UIManager;
 
+/**
+ * @version $Revision: 1.2 $, $Date: 2004/06/29 11:34:56 $
+ * @author $Author: bass $
+ * @module admin_v1
+ */
 public class AdministrateRun
 {
-  public static void main(String[] args)
-  {
-    Environment.initialize();
-    try
-    {
-		UIManager.setLookAndFeel(Environment.getLookAndFeel());
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-    }
-    LangModelAdmin.initialize();
-    new Administrate(new DefaultAdminApplicationModelFactory());
-  }
+	private AdministrateRun()
+	{
+	}
+
+	public static void main(String[] args)
+	{
+		Environment.initialize();
+		try
+		{
+			UIManager.setLookAndFeel(Environment.getLookAndFeel());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		LangModelAdmin.initialize();
+		new Administrate(new DefaultAdminApplicationModelFactory());
+	}
 }
