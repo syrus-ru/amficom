51d03f8ac6ea0ebc29188f11c290e0cb228734d8
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-06-30 18:24:42 +0400

    added version model

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObject.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObject.java
@@ -3,21 +3,20 @@ package com.syrus.AMFICOM.general;
 import java.util.Date;
 
 public abstract class StorableObject implements Identified, TransferableObject {
-	protected Identifier id;
-	protected Date created;
-	protected Date modified;
-	protected Identifier creatorId;
-	protected Identifier modifierId;
+
+	protected Identifier	id;
+	protected Date			created;
+	protected Date			modified;
+	protected Identifier	creatorId;
+	protected Identifier	modifierId;
+	protected long			version			= 0;
+	protected long			currentVersion	= 0;
 
 	protected StorableObject(Identifier id) {
 		this.id = id;
 	}
 
-	protected StorableObject(Identifier id,
-													 Date created,
-													 Date modified,
-													 Identifier creatorId,
-													 Identifier modifierId) {
+	protected StorableObject(Identifier id, Date created, Date modified, Identifier creatorId, Identifier modifierId) {
 		this.id = id;
 		this.created = created;
 		this.modified = modified;
@@ -45,10 +44,23 @@ public abstract class StorableObject implements Identified, TransferableObject {
 		return this.modifierId;
 	}
 
-	protected synchronized void setAttributes(Date created,
-																						Date modified,
-																						Identifier creatorId,
-																						Identifier modifierId) {
+	public boolean isChanged() throws VersionCollisionException {
+		boolean result = (this.currentVersion != this.version);
+		/**
+		 * @todo: check version at DB, if object was deleted or DB version isn't
+		 *        equal local version throws VersionCollisionException
+		 */
+		return result;
+	}
+	
+	protected long getNextVersion(){
+		/**
+		 * @todo recast to another getting modified version
+		 */
+		return this.version + 1;
+	}
+
+	protected synchronized void setAttributes(Date created, Date modified, Identifier creatorId, Identifier modifierId) {
 		this.created = created;
 		this.modified = modified;
 		this.creatorId = creatorId;
