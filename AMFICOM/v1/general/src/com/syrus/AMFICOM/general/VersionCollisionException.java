e451c7426edd9b5babc5bc0d33ecc2aba09817c1
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-06-30 18:27:00 +0400

    start cvs

---
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/VersionCollisionException.java
@@ -0,0 +1,40 @@
+/*
+ * VersionCollisionException.java
+ * Created on 30.06.2004 12:55:57
+ * 
+ */
+package com.syrus.AMFICOM.general;
+
+
+/**
+ * @author Vladimir Dolzhenko
+ */
+public class VersionCollisionException extends Exception {
+
+	public VersionCollisionException() {
+		super();
+	}
+
+	/**
+	 * @param message
+	 */
+	public VersionCollisionException(String message) {
+		super(message);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public VersionCollisionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public VersionCollisionException(Throwable cause) {
+		super(cause);
+	}
+
+}
