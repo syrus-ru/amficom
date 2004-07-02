374db1a6f99e007128d2bed4cdd25579c980f01f
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-07-02 13:15:58 +0400

    start cvs

---
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/PoolId.java
@@ -0,0 +1,20 @@
+/*
+ * PoolId.java
+ * Created on 02.07.2004 12:59:25
+ * 
+ */
+package com.syrus.AMFICOM.general;
+
+
+/**
+ * @author Vladimir Dolzhenko
+ */
+public class PoolId {
+	
+	public static Identifier getId(String entity){
+		/**
+		 * TODO write code to getId  
+		 */
+		return null;
+	}
+}
