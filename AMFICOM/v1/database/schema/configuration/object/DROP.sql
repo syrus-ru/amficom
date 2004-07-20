6cf7ba7ddb3d006fe42152adaee394f08e119c79
cvs-fast-export <cvs-fast-export>
2004-07-20 12:54:10 +0000

    Synthetic commit for incomplete tag mgts

--- AMFICOM/v1/database/schema/configuration/object/DROP.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE monitoredelement;
-DROP TABLE equipment;
-DROP TABLE mcm;
-DROP TABLE server;
-
-DROP SEQUENCE monitoredelement_seq;
-DROP SEQUENCE equipment_seq;
-DROP SEQUENCE mcm_seq;
-DROP SEQUENCE server_seq;
--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/AnalysisManager.java
+++
@@ -1,7 +0,0 @@
-package com.syrus.AMFICOM.mcm;
-
-import com.syrus.AMFICOM.measurement.SetParameter;
-
-public interface AnalysisManager {
-	SetParameter[] analyse() throws AnalysisException;
-}
