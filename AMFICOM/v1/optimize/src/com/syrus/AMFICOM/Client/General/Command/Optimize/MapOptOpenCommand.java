7a89332e8e750627e74f3e23617c6e608fcf3567
Vitaliy Shiryaev <miptsu@gmail.com>
2004-07-22 11:15:07 +0400

    some old unused class has been deleted

--- AMFICOM/v1/optimize/src/com/syrus/AMFICOM/Client/General/Command/Optimize/MapOptOpenCommand.java
+++ AMFICOM/v1/optimize/src/com/syrus/AMFICOM/Client/General/Command/Optimize/MapOptOpenCommand.java
@@ -1,15 +1,10 @@
 package com.syrus.AMFICOM.Client.General.Command.Optimize;
 
-import java.awt.*;
-import javax.swing.*;
-import java.util.*;
 
 import com.syrus.AMFICOM.Client.Resource.*;
 import com.syrus.AMFICOM.Client.Resource.Map.*;
 import com.syrus.AMFICOM.Client.Resource.Scheme.*;
 import com.syrus.AMFICOM.Client.Optimize.*;
-import com.syrus.AMFICOM.Client.General.UI.*;
-import com.syrus.AMFICOM.Client.General.Lang.*;
 import com.syrus.AMFICOM.Client.General.Event.*;
 import com.syrus.AMFICOM.Client.General.Model.*;
 import com.syrus.AMFICOM.Client.General.Command.*;
@@ -18,8 +13,7 @@ import com.syrus.AMFICOM.Client.General.Command.Map.*;
 import com.syrus.AMFICOM.Client.General.Checker;
 //===============================================================================================================
 public class MapOptOpenCommand extends VoidCommand
-{
-  Dispatcher dispatcher;
+{ Dispatcher dispatcher;
   ApplicationContext aContext;
   OptimizeMDIMain mdiMain;
   //-----------------------------------------------------------------
@@ -48,32 +42,29 @@ public class MapOptOpenCommand extends VoidCommand
   }
   //-----------------------------------------------------------------
   public Object clone()
-  {	return new MapOptOpenCommand(dispatcher, aContext, mdiMain);
+  {	return new MapOptOpenCommand(this.dispatcher, this.aContext,this.mdiMain);
   }
   //-----------------------------------------------------------------
   public void execute()
   {	// проверка прав доступа
-    Checker checker = new Checker( aContext.getDataSourceInterface() );
-    if( !checker.checkCommand(checker.openMapToBeOptimized) )
+    Checker checker = new Checker( this.aContext.getDataSourceInterface() );
+    if( !checker.checkCommand(Checker.openMapToBeOptimized) )
     {
   return;
     }
 
-    MapOpenCommand com = new MapOpenCommand(null, null, aContext);
+    MapOpenCommand com = new MapOpenCommand(null, null, this.aContext);
     com.execute();
     if( com.retCode == 1 )
-    { //закрыть всё, что было открыто, если открытие новой карты подтверждено
-      new CloseAllCommand(dispatcher, mdiMain.desktopPane, aContext, mdiMain).execute();
+    { // закрыть всё, что было открыто, если открытие новой карты подтверждено
+      new CloseAllCommand(this.dispatcher, this.mdiMain.desktopPane, this.aContext, this.mdiMain).execute();
 
-      mdiMain.mapContext = (MapContext )Pool.get("mapcontext", com.retobj_id);
-      dispatcher.notify(new OperationEvent(this, 0, "mapopened"));
-      mdiMain.aContext.getDispatcher().notify(new StatusMessageEvent(""));
-
-      Scheme scheme = (Scheme )Pool.get("scheme", mdiMain.mapContext.scheme_id);//при открытии карты подгружаем и схему
-      new SchemeOpenCommand(dispatcher, aContext, mdiMain).execute(scheme);
-
-      // при открытии карты  открываем всё окна
-      // new ViewShowallCommand(dispatcher, mdiMain.desktopPane, aContext, mdiMain).execute();
+      this.mdiMain.mapContext = (MapContext )Pool.get("mapcontext", com.retobj_id);
+      this.dispatcher.notify(new OperationEvent(this, 0, "mapopened"));
+      this.mdiMain.aContext.getDispatcher().notify(new StatusMessageEvent(""));
+      // при открытии карты подгружаем и схему ( при открытии схемы будут открыты все )
+      Scheme scheme = (Scheme )Pool.get("scheme", this.mdiMain.mapContext.scheme_id);
+      new SchemeOpenCommand(this.dispatcher, this.aContext, this.mdiMain).execute(scheme);  
     }
   }
   //-----------------------------------------------------------------
