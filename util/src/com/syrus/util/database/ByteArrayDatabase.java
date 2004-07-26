cf81d3f287c458f3f5b5b29dc3826d46a28b2f8c
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-07-26 10:07:58 +0400

    *** empty log message ***

--- util/src/com/syrus/util/database/ByteArrayDatabase.java
+++ util/src/com/syrus/util/database/ByteArrayDatabase.java
@@ -1,49 +1,58 @@
+/*
+ * $Id: ByteArrayDatabase.java,v 1.5 2004/07/26 06:07:58 bass Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
 package com.syrus.util.database;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.Statement;
-import oracle.sql.BLOB;
+import com.syrus.util.*;
+import java.io.*;
+import java.sql.*;
 import oracle.jdbc.driver.OracleResultSet;
-import com.syrus.util.ByteArray;
-import com.syrus.util.Log;
+import oracle.sql.BLOB;
 
+/**
+ * @version $Revision: 1.5 $, $Date: 2004/07/26 06:07:58 $
+ * @author $Author: bass $
+ * @module util
+ */
 public class ByteArrayDatabase {
-  private byte[] bar;
+	private byte[] bar;
 
-  public ByteArrayDatabase(byte[] bar) {
-    this.bar = bar;
-  }
+	public ByteArrayDatabase(byte[] bar) {
+		this.bar = bar;
+	}
 
-  public ByteArrayDatabase(ByteArray bArr) {
-    this.bar = bArr.getBytes();
-  }
+	public ByteArrayDatabase(ByteArray bArr) {
+		this.bar = bArr.getBytes();
+	}
 
-  public void saveAsBlob(Connection conn, String table, String column, String where) throws SQLException {
-    boolean oldAutoCommit = conn.getAutoCommit();
-    if (oldAutoCommit)
-      conn.setAutoCommit(false);
+	public void saveAsBlob(Connection conn, String table, String column, String where) throws SQLException {
+		boolean oldAutoCommit = conn.getAutoCommit();
+		if (oldAutoCommit)
+			conn.setAutoCommit(false);
 
-    Statement statement = null;
+		Statement statement = null;
 		OracleResultSet ors = null;
 		String s = "SELECT " + column + " FROM " + table + " WHERE " + where + " FOR UPDATE";
 		try {
-		  statement = conn.createStatement();
+			statement = conn.createStatement();
 			Log.debugMessage("Trying: " + s, Log.DEBUGLEVEL05);
 			ors = (OracleResultSet)statement.executeQuery(s);
 			BLOB blob = null;
-      if (ors.next())
-        blob = ors.getBLOB(column);
-      else
-        throw new SQLException("No record in " + table + " for '" + where + "'");
+			if (ors.next())
+				blob = ors.getBLOB(column);
+			else
+				throw new SQLException("No record in " + table + " for '" + where + "'");
 			OutputStream os = null;
-      os = blob.getBinaryOutputStream();
-      os.write(this.bar);
+			os = blob.getBinaryOutputStream();
+			os.write(this.bar);
 			os.close();
 		}
-		catch (IOException e){
+		catch (IOException e) {
 			throw new SQLException(e.getMessage());
 		}
 		finally {
@@ -53,12 +62,12 @@ public class ByteArrayDatabase {
 				ors.close();
 			statement = null;
 			ors = null;
-	    if (oldAutoCommit)
-		    conn.setAutoCommit(true);
+			if (oldAutoCommit)
+				conn.setAutoCommit(true);
 		}
-  }
+	}
 
-  public static byte[] toByteArray(BLOB blob) throws SQLException{
-    return blob.getBytes(1, (int)blob.length());
-  }
+	public static byte[] toByteArray(BLOB blob) throws SQLException{
+		return blob.getBytes(1, (int)blob.length());
+	}
 }
