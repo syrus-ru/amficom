59ee1b72bcc4c3242235eabc000f3944adf20404
Arseniy Tashoyan <tashoyan@gmail.com>
2004-07-27 20:17:12 +0400

    misc

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/MeasurementException.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/MeasurementException.java
@@ -1,4 +1,18 @@
+/*
+ * $Id: MeasurementException.java,v 1.2 2004/07/27 16:17:12 arseniy Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
 package com.syrus.AMFICOM.mcm;
+
+/**
+ * @version $Revision: 1.2 $, $Date: 2004/07/27 16:17:12 $
+ * @author $Author: arseniy $
+ * @module mcm_v1
+ */
 
 public class MeasurementException extends Exception {
 	private static final long serialVersionUID = 5663146797166499357L;
