a91ea922a2f05c16b48aba96f0e161edbe186b19
Arseniy Tashoyan <tashoyan@gmail.com>
2004-07-28 17:56:28 +0400

    Added string constant 'SQL_IN'

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectDatabase.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectDatabase.java
@@ -14,26 +14,27 @@ public abstract class StorableObjectDatabase {
 	public static final String	LINK_COLUMN_PARAMETER_TYPE_ID	= "parameter_type_id";
 	public static final String	LINK_COLUMN_PARAMETER_MODE		= "parameter_mode";
 
-	public static final String	APOSTOPHE						= "'";
-	public static final String	CLOSE_BRACKET					= " ) ";
-	public static final String	COMMA							= " , ";
-	public static final String	EQUALS							= " = ";
-	public static final String	OPEN_BRACKET					= " ( ";
-	public static final String	QUESTION						= "?";
+	public static final String	APOSTOPHE				= "'";
+	public static final String	CLOSE_BRACKET		= " ) ";
+	public static final String	COMMA						= " , ";
+	public static final String	EQUALS					= " = ";
+	public static final String	OPEN_BRACKET		= " ( ";
+	public static final String	QUESTION				= "?";
 
-	public static final String	SQL_AND							= " AND ";
-	public static final String	SQL_DELETE_FROM					= " DELETE FROM ";
-	public static final String	SQL_FROM						= " FROM ";
-	public static final String	SQL_INSERT_INTO					= " INSERT INTO ";	
-	public static final String	SQL_OR							= " OR ";
-	public static final String	SQL_ORDER_BY					= " ORDER BY ";
-	public static final String	SQL_SELECT						= " SELECT ";
-	public static final String	SQL_SET							= " SET ";
-	public static final String	SQL_UPDATE						= " UPDATE ";
-	public static final String	SQL_VALUES						= " VALUES ";
-	public static final String	SQL_WHERE						= " WHERE ";
-	
-	public static final String	SQL_FUNCTION_MAX		= " MAX ";
+	public static final String	SQL_AND					= " AND ";
+	public static final String	SQL_DELETE_FROM	= " DELETE FROM ";
+	public static final String	SQL_FROM				= " FROM ";
+	public static final String	SQL_INSERT_INTO	= " INSERT INTO ";
+	public static final String	SQL_IN					= " IN ";
+	public static final String	SQL_OR					= " OR ";
+	public static final String	SQL_ORDER_BY		= " ORDER BY ";
+	public static final String	SQL_SELECT			= " SELECT ";
+	public static final String	SQL_SET					= " SET ";
+	public static final String	SQL_UPDATE			= " UPDATE ";
+	public static final String	SQL_VALUES			= " VALUES ";
+	public static final String	SQL_WHERE				= " WHERE ";
+
+	public static final String	SQL_FUNCTION_MAX	= " MAX ";
 
 	protected static Connection	connection;
 
