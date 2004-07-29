3f6b94e8cbdb7f934d6e22f30723a4beb6d1e299
Arseniy Tashoyan <tashoyan@gmail.com>
2004-07-29 17:09:09 +0400

    main class

---
+++ AMFICOM/v1/mserver/src/com/syrus/AMFICOM/mserver/MeasurementServer.java
@@ -0,0 +1,29 @@
+/*
+ * $Id: MeasurementServer.java,v 1.1 2004/07/29 13:09:09 arseniy Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
+package com.syrus.AMFICOM.mserver;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2004/07/29 13:09:09 $
+ * @author $Author: arseniy $
+ * @module mserver_v1
+ */
+
+public class MeasurementServer extends Thread {
+
+	public MeasurementServer() {
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void run() {
+		
+	}
+}
