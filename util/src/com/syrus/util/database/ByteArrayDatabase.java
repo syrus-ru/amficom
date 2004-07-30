4daf6898d6adf73a26316c77d9368abe7eb9122f
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-07-30 16:44:34 +0400

    --- ByteArrayDatabase +++ JdbcBlobManager

--- util/src/com/syrus/util/database/ByteArrayDatabase.java
+++ util/src/com/syrus/util/database/ByteArrayDatabase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ByteArrayDatabase.java,v 1.5 2004/07/26 06:07:58 bass Exp $
+ * $Id: ByteArrayDatabase.java,v 1.6 2004/07/30 12:44:11 bass Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -15,8 +15,11 @@ import oracle.jdbc.driver.OracleResultSet;
 import oracle.sql.BLOB;
 
 /**
- * @version $Revision: 1.5 $, $Date: 2004/07/26 06:07:58 $
  * @author $Author: bass $
+ * @version $Revision: 1.6 $, $Date: 2004/07/30 12:44:11 $
+ * @deprecated This class doesn't realize <i>singleton</i> pattern, so
+ *             eventually we can receive multiple unnecessary instances. Use
+ *             {@link JdbcBlobManager} instead.
  * @module util
  */
 public class ByteArrayDatabase {
@@ -30,6 +33,10 @@ public class ByteArrayDatabase {
 		this.bar = bArr.getBytes();
 	}
 
+	/**
+	 * @deprecated Use {@link JdbcBlobManager#setData(Connection, String,
+	 *             String, String, boolean, byte[])} instead
+	 */
 	public void saveAsBlob(Connection conn, String table, String column, String where) throws SQLException {
 		boolean oldAutoCommit = conn.getAutoCommit();
 		if (oldAutoCommit)
@@ -67,6 +74,9 @@ public class ByteArrayDatabase {
 		}
 	}
 
+	/**
+	 * @deprecated Use {@link JdbcBlobManager#getData(Blob)} instead.
+	 */
 	public static byte[] toByteArray(BLOB blob) throws SQLException{
 		return blob.getBytes(1, (int)blob.length());
 	}
