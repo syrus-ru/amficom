bb451f189cfb2ac88e47b09624bc9a30782de3f9
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-08-03 09:50:49 +0400

    fixes

--- AMFICOM/v2/scheduler/src/com/syrus/AMFICOM/Client/General/UI/ObjectResourceTableCellRenderer.java
+++ AMFICOM/v2/scheduler/src/com/syrus/AMFICOM/Client/General/UI/ObjectResourceTableCellRenderer.java
@@ -19,113 +19,117 @@ import com.syrus.AMFICOM.Client.Resource.ObjectResource;
 /**
  * @author Vladimir Dolzhenko
  */
-public abstract class ObjectResourceTableCellRenderer extends JLabel implements TableCellRenderer{
+public abstract class ObjectResourceTableCellRenderer extends JLabel implements TableCellRenderer {
 
-		//protected static Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
+	public static final double	ALPHA			= 0.3;
+	private static final double	ONE_MINUS_ALPHA	= 1.0 - ALPHA;
 
-		//		 We need a place to store the color the JLabel should be returned
-		// to after its foreground and background colors have been set
-		// to the selection background color.
-		// These ivars will be made protected when their names are finalized.
-		private Color	unselectedForeground;
+	//protected static Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
 
-		//private Color unselectedBackground;
+	//		 We need a place to store the color the JLabel should be returned
+	// to after its foreground and background colors have been set
+	// to the selection background color.
+	// These ivars will be made protected when their names are finalized.
+	private Color				unselectedForeground;
 
-		/**
-		 * Creates a default table cell renderer.
-		 */
-		public ObjectResourceTableCellRenderer() {
-			setOpaque(true);
-			//setBorder(noFocusBorder);
-		}
-		
-		protected abstract void doCustomRendering(JTable table,
+	//private Color unselectedBackground;
+
+	/**
+	 * Creates a default table cell renderer.
+	 */
+	public ObjectResourceTableCellRenderer() {
+		setOpaque(true);
+		//setBorder(noFocusBorder);
+	}
+
+	protected abstract void customRendering(JTable table,
 											ObjectResource objectResource,
 											boolean isSelected,
 											boolean hasFocus,
 											int rowIndex,
 											int vColIndex);
 
-		// This method is called each time a cell in a column
-		// using this renderer needs to be rendered.
-		public Component getTableCellRendererComponent(	JTable table,
-														Object value,
-														boolean isSelected,
-														boolean hasFocus,
-														int rowIndex,
-														int vColIndex) {
-			// 'value' is value contained in the cell located at
-			// (rowIndex, vColIndex)
-			//
-			ObjectResourceTableModel model = (ObjectResourceTableModel) table.getModel();
-			ObjectResourceTableRow line = (ObjectResourceTableRow) model.getRow(rowIndex);
-			
-			ObjectResource objectResource = line.getObjectResource();
-			
-			doCustomRendering(table , objectResource, isSelected, hasFocus , rowIndex, vColIndex);
-			Color color = super.getBackground();
+	// This method is called each time a cell in a column
+	// using this renderer needs to be rendered.
+	public Component getTableCellRendererComponent(	JTable table,
+													Object value,
+													boolean isSelected,
+													boolean hasFocus,
+													int rowIndex,
+													int vColIndex) {
+		// 'value' is value contained in the cell located at
+		// (rowIndex, vColIndex)
+		//
+		ObjectResourceTableModel model = (ObjectResourceTableModel) table.getModel();
+		ObjectResourceTableRow line = (ObjectResourceTableRow) model.getRow(rowIndex);
 
-			if (isSelected) {
-				super.setForeground((this.unselectedForeground != null) ? this.unselectedForeground : table
-						.getForeground());
-				Font font = table.getFont();
-				font = new Font(font.getName(), Font.BOLD | Font.ITALIC, font.getSize());
-				setFont(font);
-				Color c = table.getSelectionBackground();
-				double k = 0.3;
-				super.setBackground(new Color((int) (c.getRed() * (1.0 - k) + k * color.getRed()) % 256, (int) (c
-						.getGreen()
-						* (1.0 - k) + k * color.getGreen()) % 256,
-												(int) (c.getBlue() * (1.0 - k) + k * color.getBlue()) % 256));
-			} else {
-				super.setForeground((this.unselectedForeground != null) ? this.unselectedForeground : table
-						.getForeground());
-				setFont(table.getFont());
-				super.setBackground(color);
+		ObjectResource objectResource = line.getObjectResource();
+
+		customRendering(table, objectResource, isSelected, hasFocus, rowIndex, vColIndex);
+		Color color = super.getBackground();
+
+		if (isSelected) {
+			super
+					.setForeground((this.unselectedForeground != null) ? this.unselectedForeground : table
+							.getForeground());
+			Font font = table.getFont();
+			font = new Font(font.getName(), Font.BOLD | Font.ITALIC, font.getSize());
+			setFont(font);
+			Color c = table.getSelectionBackground();
+			// calculate color with alpha-channel weight alpha
+			super.setBackground(new Color((int) (c.getRed() * ONE_MINUS_ALPHA + ALPHA * color.getRed()) % 256, (int) (c
+					.getGreen()
+					* ONE_MINUS_ALPHA + ALPHA * color.getGreen()) % 256, (int) (c.getBlue() * ONE_MINUS_ALPHA + ALPHA
+					* color.getBlue()) % 256));
+		} else {
+			super
+					.setForeground((this.unselectedForeground != null) ? this.unselectedForeground : table
+							.getForeground());
+			setFont(table.getFont());
+			super.setBackground(color);
+		}
+
+		if (hasFocus) {
+			setBorder(UIManager.getBorder("Table.focusCellHighlightBorder")); //$NON-NLS-1$
+			if (table.isCellEditable(rowIndex, vColIndex)) {
+				super.setForeground(UIManager.getColor("Table.focusCellForeground")); //$NON-NLS-1$
+				super.setBackground(UIManager.getColor("Table.focusCellBackground")); //$NON-NLS-1$
 			}
-
-			if (hasFocus) {
-				setBorder(UIManager.getBorder("Table.focusCellHighlightBorder")); //$NON-NLS-1$
-				if (table.isCellEditable(rowIndex, vColIndex)) {
-					super.setForeground(UIManager.getColor("Table.focusCellForeground")); //$NON-NLS-1$
-					super.setBackground(UIManager.getColor("Table.focusCellBackground")); //$NON-NLS-1$
-				}
-			} else {
-				//setBorder(noFocusBorder);
-			}
-
-			setValue(value);
-
-			return this;
+		} else {
+			//setBorder(noFocusBorder);
 		}
 
-		/**
-		 * Overrides <code>JComponent.setBackground</code> to assign the
-		 * unselected-background color to the specified color.
-		 * 
-		 * @param c
-		 *            set the background color to this value
-		 */
-		public void setBackground(Color c) {
-			super.setBackground(c);
-			//unselectedBackground = c;
-		}
+		setValue(value);
 
-		/**
-		 * Overrides <code>JComponent.setForeground</code> to assign the
-		 * unselected-foreground color to the specified color.
-		 * 
-		 * @param c
-		 *            set the foreground color to this value
-		 */
-		public void setForeground(Color c) {
-			super.setForeground(c);
-			this.unselectedForeground = c;
-		}
+		return this;
+	}
 
-		protected void setValue(Object value) {
-			setText((value == null) ? "" : value.toString()); //$NON-NLS-1$
-		}
+	/**
+	 * Overrides <code>JComponent.setBackground</code> to assign the
+	 * unselected-background color to the specified color.
+	 * 
+	 * @param c
+	 *            set the background color to this value
+	 */
+	public void setBackground(Color c) {
+		super.setBackground(c);
+		//unselectedBackground = c;
+	}
 
-	
+	/**
+	 * Overrides <code>JComponent.setForeground</code> to assign the
+	 * unselected-foreground color to the specified color.
+	 * 
+	 * @param c
+	 *            set the foreground color to this value
+	 */
+	public void setForeground(Color c) {
+		super.setForeground(c);
+		this.unselectedForeground = c;
+	}
+
+	protected void setValue(Object value) {
+		setText((value == null) ? "" : value.toString()); //$NON-NLS-1$
+	}	
+
 }
