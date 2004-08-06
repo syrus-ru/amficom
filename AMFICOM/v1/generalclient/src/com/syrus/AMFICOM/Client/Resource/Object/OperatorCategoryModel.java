92d67c3837c154c8ebc90d968f384ff79f577604
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-08-06 16:14:19 +0400

    Files moved from admin_v1.

---
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/Resource/Object/OperatorCategoryModel.java
@@ -0,0 +1,158 @@
+/*
+ * $Id: OperatorCategoryModel.java,v 1.1 2004/08/06 12:14:19 bass Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
+package com.syrus.AMFICOM.Client.Resource.Object;
+
+import java.awt.*;
+import java.util.*;
+
+
+import com.syrus.AMFICOM.Client.Administrate.Object.UI.*;
+import com.syrus.AMFICOM.Client.General.UI.*;
+import com.syrus.AMFICOM.Client.Resource.*;
+
+/**
+ * This class actually belongs to <tt>admin_v1</tt> module. It was
+ * moved to <tt>generalclient_v1</tt> to resolve cross-module
+ * dependencies between <tt>generalclient_v1</tt> and <tt>admin_1</tt>.
+ *
+ * @author $Author: bass $
+ * @version $Revision: 1.1 $, $Date: 2004/08/06 12:14:19 $
+ * @module generalclient_v1
+ */
+public class OperatorCategoryModel extends ObjectResourceModel
+{
+	public OperatorCategory cat;
+
+	public OperatorCategoryModel(OperatorCategory cat)
+	{
+		this.cat = cat;
+	}
+
+	public Enumeration getChildren(String key)
+	{
+		if(key.equals(User.typ))
+		{
+			return cat.users.elements();
+		}
+		return new Vector().elements();
+	}
+
+	public Enumeration getChildTypes()
+	{
+		Vector ret = new Vector();
+		  ret.add(User.typ);
+		return ret.elements();
+	}
+
+	public Class getChildClass(String type)
+	{
+	  if(type.equals(User.typ))
+		return User.class;
+	  return ObjectResource.class;
+	}
+
+	public PropertiesPanel getPropertyPane()
+	{
+		return new OperatorCategoryPanel(cat);
+	}
+
+	public String getColumnValue(String col_id)
+	{
+		String s = "";
+		try
+		{
+		if(col_id.equals("id"))
+			s = cat.id;
+		if(col_id.equals("name"))
+			s = cat.name;
+//		if(col_id.equals("codename"))
+//			s = cat.codename;
+		}
+		catch(Exception e)
+		{
+//			System.out.println("error gettin field value - OperatorCategory");
+			s = "";
+		}
+
+		return s;
+	}
+
+	public Vector getPropertyColumns()
+	{
+		Vector vec = new Vector();
+		vec.add("id");
+		vec.add("name");
+		vec.add("user");
+		return vec;
+	}
+
+	public String getPropertyName(String col_id)
+	{
+		if(col_id.equals("id"))
+			return "ID";
+		if(col_id.equals("name"))
+			return "NAME";
+		return "leer";
+	}
+
+	public String getPropertyValue(String col_id)
+	{
+		if(col_id.equals("id"))
+			return cat.id;
+		if(col_id.equals("name"))
+			return cat.name;
+		if(col_id.equals("user"))
+		{
+			Hashtable h = Pool.getHash("user");
+			User user = (User )h.elements().nextElement();
+			return user.id;
+		}
+		return "val";
+	}
+
+	public Component getPropertyEditor(String col_id)
+	{
+		if(col_id.equals("user"))
+		{
+			Hashtable h = Pool.getHash("user");
+			User user = (User )h.elements().nextElement();
+			return new ObjectResourceComboBox("user", user);
+		}
+		return super.getPropertyEditor(col_id);
+	}
+
+	public Component getPropertyRenderer(String col_id)
+	{
+		if(col_id.equals("user"))
+		{
+			Hashtable h = Pool.getHash("user");
+			if(h == null)
+				return null;
+			User user = (User )h.elements().nextElement();
+			return new JLabelRenderer(user.getName());
+		}
+		return super.getPropertyRenderer(col_id);
+	}
+
+	public boolean isPropertyEditable(String col_id)
+	{
+		if(col_id.equals("name"))
+			return true;
+		if(col_id.equals("user"))
+			return true;
+		return false;
+	}
+
+	public void setPropertyValue(String col_id, Object val)
+	{
+		if(col_id.equals("name"))
+			cat.name = (String )val;
+	}
+
+}
---
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/Resource/System/Server.java
@@ -0,0 +1,235 @@
+/*
+ * $Id: Server.java,v 1.1 2004/08/06 12:14:19 bass Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
+package com.syrus.AMFICOM.Client.Resource.System;
+
+import java.io.*;
+import java.util.*;
+
+import com.syrus.AMFICOM.CORBA.Admin.*;
+import com.syrus.AMFICOM.Client.Administrate.Object.UI.*;
+import com.syrus.AMFICOM.Client.General.UI.*;
+import com.syrus.AMFICOM.Client.Resource.*;
+import com.syrus.AMFICOM.Client.Resource.Object.*;
+
+/**
+ * This class actually belongs to <tt>admin_v1</tt> module. It was
+ * moved to <tt>generalclient_v1</tt> to resolve cross-module
+ * dependencies between <tt>generalclient_v1</tt> and <tt>admin_1</tt>.
+ *
+ * @author $Author: bass $
+ * @version $Revision: 1.1 $, $Date: 2004/08/06 12:14:19 $
+ * @module generalclient_v1
+ */
+public class Server extends AdminObjectResource implements Serializable
+{
+	private static final long serialVersionUID = 01L;
+	public static final String typ = "server";
+
+	Server_Transferable transferable = new Server_Transferable();
+
+	public String id = "";
+	public String name = "";
+	public String version = "";
+	public String licence_id = "";
+	public String location = "";
+	public String contact = "";
+	public String hostname = "";
+	public long created = 0;
+	public long modified = 0;
+	public int sessions = 0;
+
+	public Server()
+	{
+	}
+
+	public Server(Server_Transferable transferable)
+	{
+		this.transferable = transferable;
+		setLocalFromTransferable();
+	}
+
+	public Server(
+		String id,
+		String name,
+		String version,
+		String licence_id,
+		String location,
+		String contact,
+		String hostname,
+		long created,
+		long modified,
+		int sessions)
+	{
+		this.id = id;
+		this.name = name;
+		this.version = version;
+		this.licence_id = licence_id;
+		this.location = location;
+		this.contact = contact;
+		this.hostname = hostname;
+		this.created = created;
+		this.modified = modified;
+		this.sessions = sessions;
+
+		transferable = new Server_Transferable();
+	}
+
+	public void updateLocalFromTransferable()
+	{
+	}
+
+	public void setTransferableFromLocal()
+	{
+		transferable.id = id;
+		transferable.name = name;
+		transferable.version = version;
+		transferable.licence_id = licence_id;
+		transferable.location = location;
+		transferable.contact = contact;
+		transferable.hostname = hostname;
+		transferable.created = created;
+		transferable.modified = modified;
+		transferable.sessions = sessions;
+	}
+
+	public void setLocalFromTransferable()
+	{
+		this.id = transferable.id;
+		this.name = transferable.name;
+		this.version = transferable.version;
+		this.licence_id = transferable.licence_id;
+		this.location = transferable.location;
+		this.contact = transferable.contact;
+		this.hostname = transferable.hostname;
+		this.created = transferable.created;
+		this.modified = transferable.modified;
+		this.sessions = transferable.sessions;
+	}
+
+	public String getDomainId()
+	{
+		return null;
+	}
+
+	public String getId()
+	{
+		return this.id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public Object getTransferable()
+	{
+		return transferable;
+	}
+
+/*--------------------------------------------------------------------*/
+
+	public Class getChildClass(String type)
+	{
+		return ObjectResource.class;
+	}
+
+	public Enumeration getChildren(String key)
+	{
+		return new Vector().elements();
+	}
+
+  public Vector getChildIds(String key)
+  {
+    return new Vector();
+  }
+
+  public void addChildId(String key, String id)
+  {
+  }
+  public void removeChildId(String key, String id)
+  {
+  }
+
+	public Enumeration getChildTypes()
+	{
+		return new Vector().elements();
+	}
+
+  public static Vector getChildTypes_()
+  {
+    return new Vector();
+  }
+
+	public static ObjectResourceDisplayModel getDefaultDisplayModel()
+	{
+		return new ServerDisplayModel();
+	}
+
+	public ObjectResourceModel getModel()
+	{
+		return new ServerModel(this);
+	}
+
+	public static PropertiesPanel getPropertyPane()
+	{
+		return new ServerPane();
+	}
+
+	private void writeObject(java.io.ObjectOutputStream out) throws IOException
+	{
+		out.writeObject(id);
+		out.writeObject(name);
+		out.writeObject(version);
+		out.writeObject(licence_id);
+		out.writeObject(location);
+		out.writeObject(contact);
+		out.writeObject(hostname);
+		out.writeLong(created);
+		out.writeLong(modified);
+		out.writeInt(sessions);
+	}
+
+	private void readObject(java.io.ObjectInputStream in)
+			throws IOException, ClassNotFoundException
+	{
+		id = (String )in.readObject();
+		name = (String )in.readObject();
+		version = (String )in.readObject();
+		licence_id = (String )in.readObject();
+		location = (String )in.readObject();
+		contact = (String )in.readObject();
+		hostname = (String )in.readObject();
+		created = in.readLong();
+		modified = in.readLong();
+		sessions = in.readInt();
+
+		transferable = new Server_Transferable();
+		updateLocalFromTransferable();
+	}
+
+  public String getTyp()
+  {
+    return typ;
+  }
+
+  public String getOwnerId()
+  {
+    return "";
+  }
+
+  public void setOwnerId(String ownerID)
+  {
+  }
+
+  public  void setModificationTime(long time)
+  {
+    modified = time;
+  }
+
+}
