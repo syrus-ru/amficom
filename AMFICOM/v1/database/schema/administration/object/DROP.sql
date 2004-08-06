b7aecffbf6076855ad5a3f396b0b03ea951cff97
cvs-fast-export <cvs-fast-export>
2004-08-06 07:07:04 +0000

    Synthetic commit for incomplete tag FILTER-0_0_1

--- AMFICOM/v1/database/schema/administration/object/DROP.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE Domain;
-DROP TABLE Users;
-DROP TABLE ServerProperty;
-
-DROP SEQUENCE domain_seq;
-DROP SEQUENCE users_seq;
--- AMFICOM/v1/database/schema/general/DROP.sql
+++
@@ -1,3 +0,0 @@
-@@./object/DROP.sql
-@@./dictionary/DROP.sql
-
