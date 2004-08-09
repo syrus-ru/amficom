79009eca52697500e889c40c56af46ae3ec3bfc4
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-08-09 18:23:06 +0400

    rebuild

--- AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/UserDatabase.java
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/UserDatabase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UserDatabase.java,v 1.3 2004/08/09 12:06:23 arseniy Exp $
+ * $Id: UserDatabase.java,v 1.4 2004/08/09 14:23:06 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,6 +7,11 @@
  */
 
 package com.syrus.AMFICOM.configuration;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.StorableObject;
@@ -17,20 +22,233 @@ import com.syrus.AMFICOM.general.UpdateObjectException;
 import com.syrus.AMFICOM.general.IllegalDataException;
 import com.syrus.AMFICOM.general.ObjectNotFoundException;
 import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.util.Log;
+import com.syrus.util.database.DatabaseDate;
 
 /**
- * @version $Revision: 1.3 $, $Date: 2004/08/09 12:06:23 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.4 $, $Date: 2004/08/09 14:23:06 $
+ * @author $Author: bob $
  * @module configuration_v1
  */
 
 public class UserDatabase extends StorableObjectDatabase {
-
+	// table :: users
+	// description VARCHAR2(256),
+    public static final String COLUMN_DESCRIPTION   = "description";
+    // login VARCHAR2(32) NOT NULL,
+    public static final String COLUMN_LOGIN = "login";
+    // name VARCHAR2(64) not NULL,
+    public static final String COLUMN_NAME  = "name";
+    // sort NUMBER(2, 0) NOT NULL,
+    public static final String COLUMN_SORT  = "sort";
+    
 	private User fromStorableObject(StorableObject storableObject) throws IllegalDataException {
 		if (storableObject instanceof User)
 			return (User) storableObject;
 		throw new IllegalDataException("UserDatabase.fromStorableObject | Illegal Storable Object: " + storableObject.getClass().getName());
 	}
 
+	public void retrieve(StorableObject storableObject) throws IllegalDataException, ObjectNotFoundException, RetrieveObjectException {
+		User user = this.fromStorableObject(storableObject);
+		this.retrieveUser(user);	
+	}
+
+	private void retrieveUser(User user) throws ObjectNotFoundException, RetrieveObjectException {
+		String userIdStr = user.getId().toSQLString();
+		String sql;		
+		{
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(StorableObjectDatabase.SQL_SELECT);
+		buffer.append(DatabaseDate.toQuerySubString(StorableObjectDatabase.COLUMN_CREATED));
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(DatabaseDate.toQuerySubString(StorableObjectDatabase.COLUMN_MODIFIED));
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(StorableObjectDatabase.COLUMN_CREATOR_ID);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(StorableObjectDatabase.COLUMN_MODIFIER_ID);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(COLUMN_LOGIN);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(COLUMN_SORT);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(COLUMN_NAME);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(COLUMN_DESCRIPTION);
+		buffer.append(StorableObjectDatabase.SQL_FROM);
+		buffer.append(ObjectEntities.USER_ENTITY);
+		buffer.append(StorableObjectDatabase.SQL_WHERE);
+		buffer.append(StorableObjectDatabase.COLUMN_ID);
+		buffer.append(StorableObjectDatabase.EQUALS);
+		buffer.append(userIdStr);
+		sql = buffer.toString();
+		}
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+			statement = connection.createStatement();
+			Log.debugMessage("UserDatabase.retrieve | Trying: " + sql, Log.DEBUGLEVEL05);
+			resultSet = statement.executeQuery(sql);
+			if (resultSet.next()) {				
+				user.setAttributes(DatabaseDate.fromQuerySubString(resultSet, COLUMN_CREATED),
+													DatabaseDate.fromQuerySubString(resultSet, COLUMN_MODIFIED),
+													/**
+														* @todo when change DB Identifier model ,change getString() to getLong()
+														*/
+													new Identifier(resultSet.getString(COLUMN_CREATOR_ID)),
+													/**
+														* @todo when change DB Identifier model ,change getString() to getLong()
+														*/
+													new Identifier(resultSet.getString(COLUMN_MODIFIER_ID)),
+													
+													resultSet.getString(COLUMN_LOGIN),
+													resultSet.getInt(COLUMN_SORT),				
+													resultSet.getString(EquipmentDatabase.COLUMN_NAME),
+													resultSet.getString(EquipmentDatabase.COLUMN_DESCRIPTION));
+			}
+			else
+				throw new ObjectNotFoundException("No such user: " + userIdStr);
+		}
+		catch (SQLException sqle) {
+			String mesg = "UserDatabase.retrieve | Cannot retrieve user " + userIdStr;
+			throw new RetrieveObjectException(mesg, sqle);
+		}
+		finally {
+			try {
+				if (statement != null)
+					statement.close();
+				if (resultSet != null)
+					resultSet.close();
+				statement = null;
+				resultSet = null;
+			}
+			catch (SQLException sqle1) {
+				Log.errorException(sqle1);
+			}
+		}
+	}	
 	
+	public Object retrieveObject(StorableObject storableObject, int retrieveKind, Object arg)
+			throws IllegalDataException, ObjectNotFoundException, RetrieveObjectException {
+		User user = this.fromStorableObject(storableObject);
+		switch (retrieveKind) {
+			default:
+				return null;
+		}
+	}
+
+	public void insert(StorableObject storableObject) throws IllegalDataException, CreateObjectException {
+		User user = this.fromStorableObject(storableObject);
+		try {
+			this.insertUser(user);
+		}
+		catch (CreateObjectException e) {
+			try {
+				connection.rollback();
+			}
+			catch (SQLException sqle) {
+				Log.errorMessage("Exception in rolling back");
+				Log.errorException(sqle);
+			}
+			throw e;
+		}
+		try {
+			connection.commit();
+		}
+		catch (SQLException sqle) {
+			Log.errorMessage("Exception in commiting");
+			Log.errorException(sqle);
+		}
+	}
+
+	private void insertUser(User user) throws CreateObjectException {
+		/**
+		 * @todo when change DB Identifier model ,change String to long
+		 */
+		String userIdCode = user.getId().getCode();
+
+
+		
+		String sql = SQL_INSERT_INTO
+			+ ObjectEntities.KIS_ENTITY
+			+ OPEN_BRACKET
+			+ COLUMN_ID + COMMA
+			+ COLUMN_CREATED + COMMA
+			+ COLUMN_MODIFIED + COMMA
+			+ COLUMN_CREATOR_ID + COMMA
+			+ COLUMN_MODIFIER_ID + COMMA
+			+ COLUMN_LOGIN + COMMA
+			+ COLUMN_SORT
+			+ COLUMN_NAME + COMMA
+			+ COLUMN_DESCRIPTION 
+			+ CLOSE_BRACKET
+			+ SQL_VALUES + OPEN_BRACKET
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION
+			+ CLOSE_BRACKET;
+		
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(1, userIdCode);
+			preparedStatement.setDate(2, new java.sql.Date(user.getCreated().getTime()));
+			preparedStatement.setDate(3, new java.sql.Date(user.getModified().getTime()));
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(4, user.getCreatorId().getCode());
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(5, user.getModifierId().getCode());
+			
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(6, user.getLogin());
+
+			preparedStatement.setInt(7, user.getSort().value());
+			
+			preparedStatement.setString(7, user.getName());
+			
+			preparedStatement.setString(8, user.getDescription());			
+			
+										
+			Log.debugMessage("UserDatabase.insert | Trying: " + sql, Log.DEBUGLEVEL05);
+			preparedStatement.executeUpdate();
+			connection.commit();
+		}
+		catch (SQLException sqle) {
+			String mesg = "UserDatabase.insert | Cannot insert user " + userIdCode;
+			throw new CreateObjectException(mesg, sqle);
+		}
+		finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+				preparedStatement = null;
+			}
+			catch (SQLException sqle1) {
+				Log.errorException(sqle1);
+			}
+		}
+	}
+
+	public void update(StorableObject storableObject, int updateKind, Object obj) throws IllegalDataException, UpdateObjectException {
+		User user = this.fromStorableObject(storableObject);
+		switch (updateKind) {
+			default:
+				return;
+		}
+	}
+
 }
