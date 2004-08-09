a541d78afeead7823c6eead41562a5704562edd6
Arseniy Tashoyan <tashoyan@gmail.com>
2004-08-09 17:58:56 +0400

    misc

--- AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/ConfigurationDatabaseContext.java
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/ConfigurationDatabaseContext.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ConfigurationDatabaseContext.java,v 1.16 2004/08/09 13:52:07 arseniy Exp $
+ * $Id: ConfigurationDatabaseContext.java,v 1.17 2004/08/09 13:58:56 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -11,23 +11,23 @@ package com.syrus.AMFICOM.configuration;
 import com.syrus.AMFICOM.general.StorableObjectDatabase;
 
 /**
- * @version $Revision: 1.16 $, $Date: 2004/08/09 13:52:07 $
+ * @version $Revision: 1.17 $, $Date: 2004/08/09 13:58:56 $
  * @author $Author: arseniy $
  * @module configuration_v1
  */
 
 public class ConfigurationDatabaseContext {
-	protected static StorableObjectDatabase characteristicDatabase;
 	protected static StorableObjectDatabase characteristicTypeDatabase;
-	protected static StorableObjectDatabase portDatabase;
-	protected static StorableObjectDatabase monitoredElementDatabase;
-	protected static StorableObjectDatabase kisDatabase;
-	protected static StorableObjectDatabase equipmentDatabase;
 	protected static StorableObjectDatabase equipmentTypeDatabase;
-	protected static StorableObjectDatabase transmissionPathDatabase;
-	protected static StorableObjectDatabase mcmDatabase;
-	protected static StorableObjectDatabase serverDatabase;
+	protected static StorableObjectDatabase characteristicDatabase;
 	protected static StorableObjectDatabase userDatabase;
+	protected static StorableObjectDatabase serverDatabase;
+	protected static StorableObjectDatabase mcmDatabase;
+	protected static StorableObjectDatabase equipmentDatabase;
+	protected static StorableObjectDatabase portDatabase;
+	protected static StorableObjectDatabase transmissionPathDatabase;
+	protected static StorableObjectDatabase kisDatabase;
+	protected static StorableObjectDatabase monitoredElementDatabase;
 	
 	private ConfigurationDatabaseContext() {	
 	}
@@ -41,8 +41,8 @@ public class ConfigurationDatabaseContext {
 													StorableObjectDatabase equipmentDatabase1,
 													StorableObjectDatabase portDatabase1,
 													StorableObjectDatabase transmissionPathDatabase1,
-													StorableObjectDatabase monitoredElementDatabase1,
-													StorableObjectDatabase kisDatabase1) {
+													StorableObjectDatabase kisDatabase1,
+													StorableObjectDatabase monitoredElementDatabase1) {
 		characteristicTypeDatabase = characteristicTypeDatabase1;
 		equipmentTypeDatabase = equipmentTypeDatabase1;
 		characteristicDatabase = characteristicDatabase1;
