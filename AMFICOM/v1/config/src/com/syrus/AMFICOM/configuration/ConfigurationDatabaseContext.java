1e19fe02282abf8fddfd5c138a1234b8543ffab1
Arseniy Tashoyan <tashoyan@gmail.com>
2004-08-09 17:52:07 +0400

    Altered order of arguments in method init

--- AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/ConfigurationDatabaseContext.java
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/ConfigurationDatabaseContext.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ConfigurationDatabaseContext.java,v 1.15 2004/08/09 12:04:23 arseniy Exp $
+ * $Id: ConfigurationDatabaseContext.java,v 1.16 2004/08/09 13:52:07 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -11,7 +11,7 @@ package com.syrus.AMFICOM.configuration;
 import com.syrus.AMFICOM.general.StorableObjectDatabase;
 
 /**
- * @version $Revision: 1.15 $, $Date: 2004/08/09 12:04:23 $
+ * @version $Revision: 1.16 $, $Date: 2004/08/09 13:52:07 $
  * @author $Author: arseniy $
  * @module configuration_v1
  */
@@ -32,27 +32,27 @@ public class ConfigurationDatabaseContext {
 	private ConfigurationDatabaseContext() {	
 	}
 
-	public static void init(StorableObjectDatabase characteristicDatabase1,
-													StorableObjectDatabase characteristicTypeDatabase1,
-													StorableObjectDatabase portDatabase1,
-													StorableObjectDatabase monitoredElementDatabase1,
-													StorableObjectDatabase kisDatabase1,
-													StorableObjectDatabase equipmentDatabase1,
+	public static void init(StorableObjectDatabase characteristicTypeDatabase1,
 													StorableObjectDatabase equipmentTypeDatabase1,
-													StorableObjectDatabase transmissionPathDatabase1,
-													StorableObjectDatabase mcmDatabase1,
+													StorableObjectDatabase characteristicDatabase1,
+													StorableObjectDatabase userDatabase1,
 													StorableObjectDatabase serverDatabase1,
-													StorableObjectDatabase userDatabase1) {
-		characteristicDatabase = characteristicDatabase1;
+													StorableObjectDatabase mcmDatabase1,
+													StorableObjectDatabase equipmentDatabase1,
+													StorableObjectDatabase portDatabase1,
+													StorableObjectDatabase transmissionPathDatabase1,
+													StorableObjectDatabase monitoredElementDatabase1,
+													StorableObjectDatabase kisDatabase1) {
 		characteristicTypeDatabase = characteristicTypeDatabase1;
-		portDatabase = portDatabase1;
-		monitoredElementDatabase = monitoredElementDatabase1;
-		kisDatabase = kisDatabase1;
-		equipmentDatabase = equipmentDatabase1;
 		equipmentTypeDatabase = equipmentTypeDatabase1;
-		transmissionPathDatabase = transmissionPathDatabase1;
-		mcmDatabase = mcmDatabase1;
-		serverDatabase = serverDatabase1;
+		characteristicDatabase = characteristicDatabase1;
 		userDatabase = userDatabase1;
+		serverDatabase = serverDatabase1;
+		mcmDatabase = mcmDatabase1;
+		equipmentDatabase = equipmentDatabase1;
+		portDatabase = portDatabase1;
+		transmissionPathDatabase = transmissionPathDatabase1;
+		kisDatabase = kisDatabase1;
+		monitoredElementDatabase = monitoredElementDatabase1;
 	}
 }
