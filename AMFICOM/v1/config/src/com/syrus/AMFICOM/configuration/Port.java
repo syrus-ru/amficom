dcd8f58d3a798a7d5378dc422256765cb25aac45
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-08-11 14:46:20 +0400

    start cvs

---
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/Port.java
@@ -0,0 +1,149 @@
+/*
+ * $Id: Port.java,v 1.1 2004/08/11 10:46:20 bob Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
+package com.syrus.AMFICOM.configuration;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import com.syrus.AMFICOM.configuration.corba.PortSort;
+import com.syrus.AMFICOM.configuration.corba.Port_Transferable;
+import com.syrus.AMFICOM.general.CreateObjectException;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.IllegalDataException;
+import com.syrus.AMFICOM.general.ObjectNotFoundException;
+import com.syrus.AMFICOM.general.RetrieveObjectException;
+import com.syrus.AMFICOM.general.StorableObjectDatabase;
+import com.syrus.AMFICOM.general.StorableObjectType;
+import com.syrus.AMFICOM.general.TypedObject;
+import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2004/08/11 10:46:20 $
+ * @author $Author: bob $
+ * @module configuration_v1
+ */
+public class Port extends DomainMember implements Characterized, TypedObject {
+
+	private PortType type;	
+	private String description;
+	private Identifier equipmentId;
+	private int sort;
+	
+	private List characteristicIds;
+	
+	private StorableObjectDatabase portDatabase;
+	
+	public Port(Identifier id) throws RetrieveObjectException, ObjectNotFoundException {
+		super(id);
+		
+		this.portDatabase = ConfigurationDatabaseContext.portDatabase;
+		try {
+			this.portDatabase.retrieve(this);
+		}
+		catch (IllegalDataException ide) {
+			throw new RetrieveObjectException(ide.getMessage(), ide);
+		}
+	}
+
+	public Port(Port_Transferable pt) throws CreateObjectException {
+		super(new Identifier(pt.id),
+					new Date(pt.created),
+					new Date(pt.modified),
+					new Identifier(pt.creator_id),
+					new Identifier(pt.modifier_id),
+					new Identifier(pt.domain_id));
+		
+		this.type = (PortType)ConfigurationObjectTypePool.getObjectType(new Identifier(pt.type_id));
+		this.description = new String(pt.description);
+		this.equipmentId = new Identifier(pt.equipment_id);
+
+		this.characteristicIds = new ArrayList(pt.characteristic_ids.length);
+		for (int i = 0; i < pt.characteristic_ids.length; i++)
+			this.characteristicIds.add(new Identifier(pt.characteristic_ids[i]));
+		
+		this.sort = pt.sort.value();
+		
+		this.portDatabase = ConfigurationDatabaseContext.portDatabase;
+		try {
+			this.portDatabase.insert(this);
+		}
+		catch (IllegalDataException ide) {
+			throw new CreateObjectException(ide.getMessage(), ide);
+		}
+	}
+
+	public Object getTransferable() {
+		int i = 0;
+		
+		Identifier_Transferable[] charIds = new Identifier_Transferable[this.characteristicIds.size()];
+		for (Iterator iterator = this.characteristicIds.iterator(); iterator.hasNext();)
+			charIds[i++] = (Identifier_Transferable)((Identifier)iterator.next()).getTransferable();
+		    
+		return new Port_Transferable((Identifier_Transferable)super.id.getTransferable(),
+																			super.created.getTime(),
+																			super.modified.getTime(),
+																			(Identifier_Transferable)super.creatorId.getTransferable(),
+																			(Identifier_Transferable)super.modifierId.getTransferable(),
+																			(Identifier_Transferable)super.domainId.getTransferable(),
+																			
+																			(Identifier_Transferable)this.type.getId().getTransferable(),																			
+																			new String(this.description),
+																			(Identifier_Transferable)this.equipmentId.getTransferable(),																			
+																			PortSort.from_int(this.sort),
+																			charIds);
+	}
+
+	public StorableObjectType getType() {
+		return this.type;
+	}
+
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public List getCharacteristicIds() {
+		return this.characteristicIds;
+	}
+
+	public Identifier getEquipmentId() {
+		return this.equipmentId;
+	}
+	
+	public int getSort(){
+		return this.sort;
+	}
+
+	public void setCharacteristicIds(List characteristicIds) {
+		this.characteristicIds = characteristicIds;
+	}
+
+	protected synchronized void setAttributes(Date created,
+																						Date modified,
+																						Identifier creatorId,
+																						Identifier modifierId,
+																						Identifier domainId,
+																						PortType type,																						
+																						String description,	
+																						Identifier equipmentId,
+																						int sort) {
+		super.setAttributes(created,
+												modified,
+												creatorId,
+												modifierId,
+												domainId);
+		this.type = type;
+		this.description = description;
+		this.equipmentId = equipmentId;
+		this.sort = sort;
+	}
+
+}
---
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/PortDatabase.java
@@ -0,0 +1,294 @@
+/*
+ * $Id: PortDatabase.java,v 1.1 2004/08/11 10:46:20 bob Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
+package com.syrus.AMFICOM.configuration;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.syrus.AMFICOM.general.CreateObjectException;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.IllegalDataException;
+import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.ObjectNotFoundException;
+import com.syrus.AMFICOM.general.RetrieveObjectException;
+import com.syrus.AMFICOM.general.StorableObject;
+import com.syrus.AMFICOM.general.StorableObjectDatabase;
+import com.syrus.AMFICOM.general.UpdateObjectException;
+import com.syrus.util.Log;
+import com.syrus.util.database.DatabaseDate;
+
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2004/08/11 10:46:20 $
+ * @author $Author: bob $
+ * @module configuration_v1
+ */
+public class PortDatabase extends StorableObjectDatabase {
+	// table :: Port
+
+	// domain_id VARCHAR2(32),
+    public static final String COLUMN_DOMAIN_ID     = "domain_id";
+    // type_id VARCHAR2(32) NOT NULL,
+    public static final String COLUMN_TYPE_ID       = "type_id";
+	// description VARCHAR2(256),
+    public static final String COLUMN_DESCRIPTION   = "description";
+    // equipment_id VARCHAR2(32),
+    public static final String COLUMN_EQUIPMENT_ID  = "equipment_id";
+    // sort NUMBER(2) NOT NULL,
+    public static final String COLUMN_SORT  = "sort";
+    
+	private Port fromStorableObject(StorableObject storableObject) throws IllegalDataException {
+		if (storableObject instanceof Port)
+			return (Port)storableObject;
+		throw new IllegalDataException("PortDatabase.fromStorableObject | Illegal Storable Object: " + storableObject.getClass().getName());
+	}
+
+	public void retrieve(StorableObject storableObject) throws IllegalDataException, ObjectNotFoundException, RetrieveObjectException {
+		Port port = this.fromStorableObject(storableObject);
+		this.retrievePort(port);	
+	}
+
+	private void retrievePort(Port port) throws ObjectNotFoundException, RetrieveObjectException{
+		String sql;
+		String eqIdStr = port.getId().toSQLString();
+		{
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(StorableObjectDatabase.SQL_SELECT);
+		buffer.append(DatabaseDate.toQuerySubString(StorableObjectDatabase.COLUMN_CREATED));
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(DatabaseDate.toQuerySubString(StorableObjectDatabase.COLUMN_MODIFIED));
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(StorableObjectDatabase.COLUMN_CREATOR_ID);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(StorableObjectDatabase.COLUMN_MODIFIER_ID);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(DomainMember.COLUMN_DOMAIN_ID);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(COLUMN_TYPE_ID);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(COLUMN_DESCRIPTION);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(COLUMN_EQUIPMENT_ID);
+		buffer.append(StorableObjectDatabase.COMMA);
+		buffer.append(COLUMN_SORT);
+		buffer.append(StorableObjectDatabase.SQL_FROM);
+		buffer.append(ObjectEntities.PORT_ENTITY);
+		buffer.append(StorableObjectDatabase.SQL_WHERE);
+		buffer.append(StorableObjectDatabase.COLUMN_ID);
+		buffer.append(StorableObjectDatabase.EQUALS);
+		buffer.append(eqIdStr);
+		sql = buffer.toString();
+		}
+		
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+			statement = connection.createStatement();
+			Log.debugMessage("PortDatabase.retrievePort | Trying: " + sql, Log.DEBUGLEVEL05);
+			resultSet = statement.executeQuery(sql);
+			if (resultSet.next()) {
+				/**
+				 * @todo when change DB Identifier model ,change String to long
+				 */
+				String portTypeIdCode = resultSet.getString(COLUMN_TYPE_ID);				
+				
+				String description = resultSet.getString(COLUMN_DESCRIPTION);
+				port.setAttributes(DatabaseDate.fromQuerySubString(resultSet, COLUMN_CREATED),
+								  DatabaseDate.fromQuerySubString(resultSet, COLUMN_MODIFIED),								  
+								  
+								  /**
+									* @todo when change DB Identifier model ,change getString() to getLong()
+									*/												
+								  new Identifier(resultSet.getString(COLUMN_CREATOR_ID)),
+								  /**
+									* @todo when change DB Identifier model ,change getString() to getLong()
+									*/
+								  new Identifier(resultSet.getString(COLUMN_MODIFIER_ID)),
+								  /**
+									* @todo when change DB Identifier model ,change getString() to getLong()
+									*/
+								  new Identifier(resultSet.getString(COLUMN_DOMAIN_ID)),
+								  
+								  (portTypeIdCode != null)?((PortType)ConfigurationObjectTypePool.getObjectType(new Identifier(portTypeIdCode))):null,								  
+								  (description != null)?description:"",
+								  /**
+									* @todo when change DB Identifier model ,change getString() to getLong()
+									*/
+								  new Identifier(resultSet.getString(COLUMN_EQUIPMENT_ID)),
+								  resultSet.getInt(COLUMN_SORT));
+			}
+			else
+				throw new ObjectNotFoundException("No such port: " + eqIdStr);
+		}
+		catch (SQLException sqle) {
+			String mesg = "PortDatabase.retrieveEquipment | Cannot retrieve port " + eqIdStr;
+			throw new RetrieveObjectException(mesg, sqle);
+		}
+		finally {
+			try {
+				if (statement != null)
+					statement.close();
+				if (resultSet != null)
+					resultSet.close();
+				statement = null;
+				resultSet = null;
+			}
+			catch (SQLException sqle1) {
+				Log.errorException(sqle1);
+			}
+		}
+	}
+
+	public Object retrieveObject(StorableObject storableObject, int retrieveKind, Object arg) throws IllegalDataException, ObjectNotFoundException, RetrieveObjectException {
+		Port port = this.fromStorableObject(storableObject);
+		switch (retrieveKind) {
+			default:
+				return null;
+		}
+	}
+
+	public void insert(StorableObject storableObject) throws IllegalDataException, CreateObjectException {
+		Port port = this.fromStorableObject(storableObject);
+		
+		try {
+			this.insertPort(port);			
+		} catch (CreateObjectException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException sqle) {
+				Log.errorMessage("Exception in rolling back");
+				Log.errorException(sqle);
+			}
+			throw e;
+		}
+		try {
+			connection.commit();
+		} catch (SQLException sqle) {
+			Log.errorMessage("Exception in commiting");
+			Log.errorException(sqle);
+		}
+		
+	}
+
+	private void insertPort(Port port) throws CreateObjectException {
+		/**
+		 * @todo when change DB Identifier model ,change String to long
+		 */
+		String portIdCode = port.getId().getCode();
+
+		/**
+		 * @todo when change DB Identifier model ,change String to long
+		 */
+		Identifier domainId = port.getDomainId();
+
+		/**
+		 * @todo when change DB Identifier model ,change String to long
+		 */
+		Identifier typeId = port.getType().getId();
+
+		/**
+		 * @todo when change DB Identifier model ,change String to long
+		 */
+		Identifier equipmentId = port.getEquipmentId();
+
+		String sql = SQL_INSERT_INTO
+			+ ObjectEntities.PORT_ENTITY
+			+ OPEN_BRACKET
+			+ COLUMN_ID + COMMA
+			+ COLUMN_CREATED + COMMA
+			+ COLUMN_MODIFIED + COMMA
+			+ COLUMN_CREATOR_ID + COMMA
+			+ COLUMN_MODIFIER_ID + COMMA
+			+ COLUMN_DOMAIN_ID + COMMA
+			+ COLUMN_TYPE_ID + COMMA			
+			+ COLUMN_DESCRIPTION + COMMA
+			+ COLUMN_EQUIPMENT_ID + COMMA
+			+ COLUMN_SORT  			
+			+ CLOSE_BRACKET
+			+ SQL_VALUES + OPEN_BRACKET
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION 
+			+ CLOSE_BRACKET;
+		
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(1, portIdCode);
+			preparedStatement.setDate(2, new java.sql.Date(port.getCreated().getTime()));
+			preparedStatement.setDate(3, new java.sql.Date(port.getModified().getTime()));
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(4, port.getCreatorId().getCode());
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(5, port.getModifierId().getCode());
+			
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(6, (domainId != null)?domainId.getCode():Identifier.getNullSQLString());
+
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(7, (typeId != null)?typeId.getCode():Identifier.getNullSQLString());
+			
+			preparedStatement.setString(8, port.getDescription());
+			
+			/**
+			  * @todo when change DB Identifier model ,change setString() to setLong()
+			  */
+			preparedStatement.setString(9, (equipmentId != null)?equipmentId.getCode():Identifier.getNullSQLString());
+			
+			preparedStatement.setInt(10, port.getSort());			
+										
+			Log.debugMessage("PortDatabase.insertPort | Trying: " + sql, Log.DEBUGLEVEL05);
+			preparedStatement.executeUpdate();
+			connection.commit();
+		}
+		catch (SQLException sqle) {
+			String mesg = "PortDatabase.insertPort | Cannot insert port " + portIdCode;
+			throw new CreateObjectException(mesg, sqle);
+		}
+		finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+				preparedStatement = null;
+			}
+			catch (SQLException sqle1) {
+				Log.errorException(sqle1);
+			}
+		}
+	}
+
+	public void update(StorableObject storableObject, int updateKind, Object obj) throws IllegalDataException, UpdateObjectException {
+		Port port = this.fromStorableObject(storableObject);
+			switch (updateKind) {
+				default:
+					return;
+			}
+	}
+
+}
