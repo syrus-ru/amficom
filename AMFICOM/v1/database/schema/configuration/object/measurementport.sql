45f0250b4b134c5f0f443c1f57552e663ebb8857
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-08-11 16:44:11 +0400

    start cvs

---
+++ AMFICOM/v1/database/schema/configuration/object/measurementport.sql
@@ -0,0 +1,34 @@
+CREATE TABLE MeasurementPort (
+ id VARCHAR2(32),
+ created DATE NOT NULL,
+ modified DATE NOT NULL,
+ creator_id VARCHAR2(32) NOT NULL,
+ modifier_id VARCHAR2(32) NOT NULL,
+--
+ type_id VARCHAR2(32) NOT NULL,
+--
+ name VARCHAR2(64) NOT NULL,
+ description VARCHAR2(256),
+-- 
+ kis_id VARCHAR2(32),
+ port_id VARCHAR2(32),
+--
+ CONSTRAINT mp_pk PRIMARY KEY (id),
+ CONSTRAINT mp_creator_fk FOREIGN KEY (creator_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+ CONSTRAINT mp_modifier_fk FOREIGN KEY (modifier_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+--
+ CONSTRAINT mp_domain_fk FOREIGN KEY (domain_id)
+  REFERENCES Domain (id) ON DELETE CASCADE,
+--
+ CONSTRAINT eqp_kis_fk FOREIGN KEY (kis_id)
+  REFERENCES KIS (id) ON DELETE CASCADE,
+--
+ CONSTRAINT eqp_port_fk FOREIGN KEY (port_id)
+  REFERENCES Port (id) ON DELETE CASCADE
+
+);
+
+CREATE SEQUENCE measurementport_seq ORDER;
+
