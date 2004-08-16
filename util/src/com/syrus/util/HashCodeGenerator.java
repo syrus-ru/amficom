f62fb4137497a5aca090cbbb3d3f5a8425ee0230
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-08-16 20:07:49 +0400

    start cvs

---
+++ util/src/com/syrus/util/HashCodeGenerator.java
@@ -0,0 +1,79 @@
+/*
+ * HashCodeGenerator.java
+ * Created on 16.08.2004 19:24:03
+ * 
+ */
+package com.syrus.util;
+
+/**
+ * @author Vladimir Dolzhenko
+ */
+public class HashCodeGenerator {
+
+	private int	result	= 17;
+
+	public void addInt(int value) {
+		this.result = 37 * this.result + value;
+	}
+
+	public void addBoolean(boolean value) {
+		addInt(value ? 1 : 0);
+	}
+
+	public void addLong(long value) {
+		addInt((int) (value ^ (value >>> 32)));
+	}
+
+	public void addFloat(float value) {
+		addInt(Float.floatToIntBits(value));
+	}
+
+	public void addDouble(double value) {
+		addLong(Double.doubleToLongBits(value));
+	}
+
+	/**
+	 * If the field is an object reference and this class's equals method
+	 * compares the field by recursively invoking equals, recursively invoke
+	 * hashCode on the field. If a more complex comparison is required, compute
+	 * a "canonical representation" for this field and invoke
+	 */
+	public void addObject(Object value) {
+		addInt(value == null ? 0 : value.hashCode());
+	}
+
+	public void addIntArray(int[] array) {
+		for (int i = 0; i < array.length; i++)
+			addInt(array[i]);
+	}
+
+	public void addBooleanArray(boolean[] array) {
+		for (int i = 0; i < array.length; i++)
+			addBoolean(array[i]);
+	}
+
+	public void addLongArray(long[] array) {
+		for (int i = 0; i < array.length; i++)
+			addLong(array[i]);
+	}
+
+	public void addFloatArray(float[] array) {
+		for (int i = 0; i < array.length; i++)
+			addFloat(array[i]);
+	}
+
+	public void addDoubleArray(double[] array) {
+		for (int i = 0; i < array.length; i++)
+			addDouble(array[i]);
+	}
+
+	public void addObjectArray(Object[] array) {
+		for (int i = 0; i < array.length; i++)
+			addObject(array[i]);
+	}
+
+	
+	public int getResult(){
+		return this.result;
+	}
+}
