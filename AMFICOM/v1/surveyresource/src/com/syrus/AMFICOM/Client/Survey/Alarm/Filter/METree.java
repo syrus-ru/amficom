15bdc782f97b30edce899b633566eb76c9da060a
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-08-18 18:26:50 +0400

    start cvs

---
+++ AMFICOM/v1/surveyresource/src/com/syrus/AMFICOM/Client/Survey/Alarm/Filter/METree.java
@@ -0,0 +1,50 @@
+package com.syrus.AMFICOM.Client.Survey.Alarm.Filter;
+
+import com.syrus.AMFICOM.Client.General.Filter.*;
+import com.syrus.AMFICOM.Client.General.Model.*;
+import com.syrus.AMFICOM.Client.General.Lang.*;
+import com.syrus.AMFICOM.Client.General.UI.*;
+import com.syrus.AMFICOM.Client.Resource.*;
+import com.syrus.AMFICOM.Client.Resource.ISM.*;
+import java.util.*;
+import javax.swing.*;
+
+public class METree extends FilterTree
+{
+	FilterTreeNode root = new FilterTreeNode(LangModel.getString("labelRoot"), "root");
+
+	public METree()
+	{
+	}
+
+	public void setTree(ApplicationContext aContext)
+	{
+		Hashtable ht = Pool.getHash(KIS.typ);
+		if(ht != null)
+		for(Enumeration en = ht.elements(); en.hasMoreElements();)
+		{
+			KIS kis = (KIS )en.nextElement();
+			FilterTreeNode kisnode = new FilterTreeNode(kis.getName(), kis.getId());
+			root.add(kisnode);
+
+			for(Enumeration enu = kis.access_ports.elements(); enu.hasMoreElements();)
+			{
+				AccessPort ap = (AccessPort )enu.nextElement();
+
+				Hashtable ht2 = Pool.getHash(MonitoredElement.typ);
+				if(ht2 != null)
+				for(Enumeration en2 = ht2.elements(); en2.hasMoreElements();)
+				{
+					MonitoredElement me = (MonitoredElement )en2.nextElement();
+					if(me.access_port_id.equals(ap.getId()))
+					{
+						kisnode.add(new FilterTreeNode(me.getName(), me.getId()));
+					}
+				}
+			}
+		}
+		TreeModelClone myModel = new TreeModelClone(root);
+		tree = new JTree(myModel);
+	}
+}
+
---
+++ AMFICOM/v1/surveyresource/src/com/syrus/AMFICOM/Client/Survey/Alarm/UI/AlarmDisplayModel.java
@@ -0,0 +1,70 @@
+package com.syrus.AMFICOM.Client.Survey.Alarm.UI;
+
+import java.util.*;
+import java.text.*;
+import java.awt.*;
+
+import com.syrus.AMFICOM.CORBA.*;
+import com.syrus.AMFICOM.CORBA.General.*;
+import com.syrus.AMFICOM.CORBA.Alarm.*;
+import com.syrus.AMFICOM.Client.Resource.*;
+import com.syrus.AMFICOM.Client.Resource.Alarm.*;
+import com.syrus.AMFICOM.Client.General.UI.*;
+import java.util.List;
+
+public class AlarmDisplayModel extends StubDisplayModel
+{
+	public AlarmDisplayModel()
+	{
+		super();
+	}
+
+	List cols = new LinkedList();
+	{
+//		cols.add("id");
+		cols.add("source_name");
+		cols.add("generated");
+		cols.add("alarm_type_name");
+		cols.add("status");
+		cols.add("monitored_element_id");
+	}
+	
+	public List getColumns()
+	{
+		return cols;
+	}
+
+	public String getColumnName(String col_id)
+	{
+		if(col_id.equals("id"))
+			return "Идентификатор";
+		if(col_id.equals("source_name"))
+			return "Источник";
+		if(col_id.equals("status"))
+			return "Статус";
+		if(col_id.equals("generated"))
+			return "Время появления";
+		if(col_id.equals("alarm_type_name"))
+			return "Имя";
+		if(col_id.equals("monitored_element_id"))
+			return "Исследуемый объект";
+		return "leer";
+	}
+
+	public boolean isColumnColored(String col_id)
+	{
+		return true;
+	}
+
+	public Color getColumnColor(ObjectResource or, String col_id)
+	{
+		Alarm alarm = (Alarm )or;
+		if(alarm.status == AlarmStatus.ALARM_STATUS_GENERATED)
+			return new Color(255, 100, 100);
+		else
+		if(alarm.status == AlarmStatus.ALARM_STATUS_ASSIGNED)
+			return new Color(200, 255, 200);
+		return super.getColumnColor(or, col_id);
+	}
+}
+
