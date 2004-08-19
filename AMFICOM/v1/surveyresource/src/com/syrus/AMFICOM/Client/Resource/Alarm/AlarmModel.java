d2cf8b7932fa589e1ef48223f93d27fc4433a8ab
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-08-19 14:24:28 +0400

    *** empty log message ***

--- AMFICOM/v1/surveyresource/src/com/syrus/AMFICOM/Client/Resource/Alarm/AlarmModel.java
+++ AMFICOM/v1/surveyresource/src/com/syrus/AMFICOM/Client/Resource/Alarm/AlarmModel.java
@@ -9,62 +9,48 @@ import com.syrus.AMFICOM.Client.Survey.General.ConstStorage;
 import com.syrus.AMFICOM.Client.General.UI.*;
 import com.syrus.AMFICOM.Client.General.Lang.*;
 
-public class AlarmModel extends ObjectResourceModel
-{
-	Alarm alarm;
-	
+public class AlarmModel extends ObjectResourceModel {
 
-	public AlarmModel(Alarm alarm)
-	{
+	private Alarm	alarm;
+
+	public AlarmModel(Alarm alarm) {
 		this.alarm = alarm;
 	}
 
-	public PropertiesPanel getPropertyPane()
-	{
+	public PropertiesPanel getPropertyPane() {
 		return new GeneralPanel();
 	}
 
-	public String getColumnValue(String col_id)
-	{
-		String s = "";
-		try
-		{
-			if(col_id.equals("id"))
+	public String getColumnValue(String colId) {
+		String s = null;
+		try {
+			if (colId.equals("id"))
 				s = alarm.id;
-			if(col_id.equals("source_name"))
+			if (colId.equals(ObjectResourceModel.COLUMN_SOURCE_NAME))
 				s = Pool.getName(EventSource.typ, alarm.source_id);
-			if(col_id.equals("status"))
-			{
-				if(alarm.status.equals(AlarmStatus.ALARM_STATUS_GENERATED))
+			if (colId.equals(ObjectResourceModel.COLUMN_STATUS)) {
+				if (alarm.status.equals(AlarmStatus.ALARM_STATUS_GENERATED))
 					s = LangModelSurvey.getString("New");
-				else
-				if(alarm.status.equals(AlarmStatus.ALARM_STATUS_ASSIGNED))
+				else if (alarm.status.equals(AlarmStatus.ALARM_STATUS_ASSIGNED))
 					s = LangModelSurvey.getString("Assigned");
-				else
-				if(alarm.status.equals(AlarmStatus.ALARM_STATUS_FIXED))
+				else if (alarm.status.equals(AlarmStatus.ALARM_STATUS_FIXED))
 					s = LangModelSurvey.getString("Fixed");
-//				s = LangModelSurvey.String("label" + String.valueOf(alarm.status));
+				//				s = LangModelSurvey.String("label" +
+				// String.valueOf(alarm.status));
 			}
-			if(col_id.equals("generated"))
+			if (colId.equals(ObjectResourceModel.COLUMN_GENERATED))
 				s = ConstStorage.SIMPLE_DATE_FORMAT.format(new Date(alarm.generated));
-			if(col_id.equals("alarm_type_name"))
+			if (colId.equals(ObjectResourceModel.COLUMN_ALARM_TYPE_NAME))
 				s = Pool.getName(AlarmType.typ, alarm.type_id);
-			if(col_id.equals("monitored_element_id"))
-			{
+			if (colId.equals(ObjectResourceModel.COLUMN_ME_ID)) {
 				//SystemEvent event = alarm.getEvent();
 				//EventSource source = event.getSource();
 				s = Pool.getName(MonitoredElement.typ, alarm.getMonitoredElementId());
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		catch(Exception e)
-		{
-//			System.out.println("error gettin field value - Alarm");
-			s = "";
-		}
-		if(s == null)
-			s = "";
-
-		return s;
+		return (s == null) ? "" : s;
 	}
 }
 
