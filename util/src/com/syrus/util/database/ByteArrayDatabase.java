adf2ffff659988b38464af2d26edd625497da080
Arseniy Tashoyan <tashoyan@gmail.com>
2004-08-22 18:53:11 +0400

    level of debug output

--- util/src/com/syrus/util/database/ByteArrayDatabase.java
+++ util/src/com/syrus/util/database/ByteArrayDatabase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ByteArrayDatabase.java,v 1.6 2004/07/30 12:44:11 bass Exp $
+ * $Id: ByteArrayDatabase.java,v 1.7 2004/08/22 14:53:11 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,15 +8,19 @@
 
 package com.syrus.util.database;
 
-import com.syrus.util.*;
-import java.io.*;
-import java.sql.*;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
 import oracle.jdbc.driver.OracleResultSet;
 import oracle.sql.BLOB;
+import com.syrus.util.Log;
+import com.syrus.util.ByteArray;
 
 /**
- * @author $Author: bass $
- * @version $Revision: 1.6 $, $Date: 2004/07/30 12:44:11 $
+ * @author $Author: arseniy $
+ * @version $Revision: 1.7 $, $Date: 2004/08/22 14:53:11 $
  * @deprecated This class doesn't realize <i>singleton</i> pattern, so
  *             eventually we can receive multiple unnecessary instances. Use
  *             {@link JdbcBlobManager} instead.
@@ -47,7 +51,7 @@ public class ByteArrayDatabase {
 		String s = "SELECT " + column + " FROM " + table + " WHERE " + where + " FOR UPDATE";
 		try {
 			statement = conn.createStatement();
-			Log.debugMessage("Trying: " + s, Log.DEBUGLEVEL05);
+			Log.debugMessage("Trying: " + s, Log.DEBUGLEVEL09);
 			ors = (OracleResultSet)statement.executeQuery(s);
 			BLOB blob = null;
 			if (ors.next())
