31c48996accdcecd0d3e8e04c413bd27be4d7bde
Arseniy Tashoyan <tashoyan@gmail.com>
2004-08-22 19:23:26 +0400

    misc

--- util/src/com/syrus/util/database/ByteArrayDatabase.java
+++ util/src/com/syrus/util/database/ByteArrayDatabase.java
@@ -1,9 +1,9 @@
 /*
- * $Id: ByteArrayDatabase.java,v 1.8 2004/08/22 15:21:53 arseniy Exp $
+ * $Id: ByteArrayDatabase.java,v 1.9 2004/08/22 15:23:26 arseniy Exp $
  *
- * Copyright © 2004 Syrus Systems.
- * Íàó÷íî-òåõíè÷åñêèé öåíòğ.
- * Ïğîåêò: ÀÌÔÈÊÎÌ.
+ * Copyright ¿ 2004 Syrus Systems.
+ * îÁÕŞÎÏ-ÔÅÈÎÉŞÅÓËÉÊ ÃÅÎÔÒ.
+ * ğÒÏÅËÔ: áíæéëïí.
  */
 
 package com.syrus.util.database;
@@ -19,19 +19,10 @@ import com.syrus.util.Log;
 
 /**
  * @author $Author: arseniy $
- * @version $Revision: 1.8 $, $Date: 2004/08/22 15:21:53 $
+ * @version $Revision: 1.9 $, $Date: 2004/08/22 15:23:26 $
  * @module util
  */
 public class ByteArrayDatabase {
-//	private byte[] bar;
-//
-//	public ByteArrayDatabase(byte[] bar) {
-//		this.bar = bar;
-//	}
-//
-//	public ByteArrayDatabase(ByteArray bArr) {
-//		this.bar = bArr.getBytes();
-//	}
 
 	private ByteArrayDatabase() {
 	}
@@ -73,50 +64,6 @@ public class ByteArrayDatabase {
 		}
 	}
 
-//	/**
-//	 * @deprecated Use {@link JdbcBlobManager#setData(Connection, String,
-//	 *             String, String, boolean, byte[])} instead
-//	 */
-//	public void saveAsBlob(Connection conn, String table, String column, String where) throws SQLException {
-//		boolean oldAutoCommit = conn.getAutoCommit();
-//		if (oldAutoCommit)
-//			conn.setAutoCommit(false);
-//
-//		Statement statement = null;
-//		OracleResultSet ors = null;
-//		String s = "SELECT " + column + " FROM " + table + " WHERE " + where + " FOR UPDATE";
-//		try {
-//			statement = conn.createStatement();
-//			Log.debugMessage("Trying: " + s, Log.DEBUGLEVEL09);
-//			ors = (OracleResultSet)statement.executeQuery(s);
-//			BLOB blob = null;
-//			if (ors.next())
-//				blob = ors.getBLOB(column);
-//			else
-//				throw new SQLException("No record in " + table + " for '" + where + "'");
-//			OutputStream os = null;
-//			os = blob.getBinaryOutputStream();
-//			os.write(this.bar);
-//			os.close();
-//		}
-//		catch (IOException e) {
-//			throw new SQLException(e.getMessage());
-//		}
-//		finally {
-//			if (statement != null)
-//				statement.close();
-//			if (ors != null)
-//				ors.close();
-//			statement = null;
-//			ors = null;
-//			if (oldAutoCommit)
-//				conn.setAutoCommit(true);
-//		}
-//	}
-//
-//	/**
-//	 * @deprecated Use {@link JdbcBlobManager#getData(Blob)} instead.
-//	 */
 	public static byte[] toByteArray(BLOB blob) throws SQLException{
 		return blob.getBytes(1, (int)blob.length());
 	}
