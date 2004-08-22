b028b28efaf29554ad3d30ccc3cd2e3007c16734
Arseniy Tashoyan <tashoyan@gmail.com>
2004-08-22 19:21:53 +0400

    singleton

--- util/src/com/syrus/util/database/ByteArrayDatabase.java
+++ util/src/com/syrus/util/database/ByteArrayDatabase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ByteArrayDatabase.java,v 1.7 2004/08/22 14:53:11 arseniy Exp $
+ * $Id: ByteArrayDatabase.java,v 1.8 2004/08/22 15:21:53 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -16,32 +16,27 @@ import java.sql.SQLException;
 import oracle.jdbc.driver.OracleResultSet;
 import oracle.sql.BLOB;
 import com.syrus.util.Log;
-import com.syrus.util.ByteArray;
 
 /**
  * @author $Author: arseniy $
- * @version $Revision: 1.7 $, $Date: 2004/08/22 14:53:11 $
- * @deprecated This class doesn't realize <i>singleton</i> pattern, so
- *             eventually we can receive multiple unnecessary instances. Use
- *             {@link JdbcBlobManager} instead.
+ * @version $Revision: 1.8 $, $Date: 2004/08/22 15:21:53 $
  * @module util
  */
 public class ByteArrayDatabase {
-	private byte[] bar;
+//	private byte[] bar;
+//
+//	public ByteArrayDatabase(byte[] bar) {
+//		this.bar = bar;
+//	}
+//
+//	public ByteArrayDatabase(ByteArray bArr) {
+//		this.bar = bArr.getBytes();
+//	}
 
-	public ByteArrayDatabase(byte[] bar) {
-		this.bar = bar;
+	private ByteArrayDatabase() {
 	}
 
-	public ByteArrayDatabase(ByteArray bArr) {
-		this.bar = bArr.getBytes();
-	}
-
-	/**
-	 * @deprecated Use {@link JdbcBlobManager#setData(Connection, String,
-	 *             String, String, boolean, byte[])} instead
-	 */
-	public void saveAsBlob(Connection conn, String table, String column, String where) throws SQLException {
+	public static void saveAsBlob(byte[] bar, Connection conn, String table, String column, String where) throws SQLException {
 		boolean oldAutoCommit = conn.getAutoCommit();
 		if (oldAutoCommit)
 			conn.setAutoCommit(false);
@@ -60,7 +55,7 @@ public class ByteArrayDatabase {
 				throw new SQLException("No record in " + table + " for '" + where + "'");
 			OutputStream os = null;
 			os = blob.getBinaryOutputStream();
-			os.write(this.bar);
+			os.write(bar);
 			os.close();
 		}
 		catch (IOException e) {
@@ -78,9 +73,50 @@ public class ByteArrayDatabase {
 		}
 	}
 
-	/**
-	 * @deprecated Use {@link JdbcBlobManager#getData(Blob)} instead.
-	 */
+//	/**
+//	 * @deprecated Use {@link JdbcBlobManager#setData(Connection, String,
+//	 *             String, String, boolean, byte[])} instead
+//	 */
+//	public void saveAsBlob(Connection conn, String table, String column, String where) throws SQLException {
+//		boolean oldAutoCommit = conn.getAutoCommit();
+//		if (oldAutoCommit)
+//			conn.setAutoCommit(false);
+//
+//		Statement statement = null;
+//		OracleResultSet ors = null;
+//		String s = "SELECT " + column + " FROM " + table + " WHERE " + where + " FOR UPDATE";
+//		try {
+//			statement = conn.createStatement();
+//			Log.debugMessage("Trying: " + s, Log.DEBUGLEVEL09);
+//			ors = (OracleResultSet)statement.executeQuery(s);
+//			BLOB blob = null;
+//			if (ors.next())
+//				blob = ors.getBLOB(column);
+//			else
+//				throw new SQLException("No record in " + table + " for '" + where + "'");
+//			OutputStream os = null;
+//			os = blob.getBinaryOutputStream();
+//			os.write(this.bar);
+//			os.close();
+//		}
+//		catch (IOException e) {
+//			throw new SQLException(e.getMessage());
+//		}
+//		finally {
+//			if (statement != null)
+//				statement.close();
+//			if (ors != null)
+//				ors.close();
+//			statement = null;
+//			ors = null;
+//			if (oldAutoCommit)
+//				conn.setAutoCommit(true);
+//		}
+//	}
+//
+//	/**
+//	 * @deprecated Use {@link JdbcBlobManager#getData(Blob)} instead.
+//	 */
 	public static byte[] toByteArray(BLOB blob) throws SQLException{
 		return blob.getBytes(1, (int)blob.length());
 	}
