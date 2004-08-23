87bd9ddebc4607cc62db666ca23457fc9ffaac88
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-08-23 17:02:01 +0400

    Fixed compatibility issues.

--- util/src/com/syrus/util/database/ByteArrayDatabase.java
+++ util/src/com/syrus/util/database/ByteArrayDatabase.java
@@ -1,9 +1,9 @@
 /*
- * $Id: ByteArrayDatabase.java,v 1.9 2004/08/22 15:23:26 arseniy Exp $
+ * $Id: ByteArrayDatabase.java,v 1.10 2004/08/23 13:02:01 bass Exp $
  *
- * Copyright ø 2004 Syrus Systems.
- * Ó¡’ﬁŒœ-‘≈»Œ…ﬁ≈”À…  √≈Œ‘“.
- * “œ≈À‘: ·ÌÊÈÎÔÌ.
+ * Copyright © 2004 Syrus Systems.
+ * Õ‡Û˜ÌÓ-ÚÂıÌË˜ÂÒÍËÈ ˆÂÌÚ.
+ * œÓÂÍÚ: ¿Ã‘» ŒÃ.
  */
 
 package com.syrus.util.database;
@@ -16,18 +16,44 @@ import java.sql.SQLException;
 import oracle.jdbc.driver.OracleResultSet;
 import oracle.sql.BLOB;
 import com.syrus.util.Log;
+import com.syrus.util.ByteArray;
 
 /**
- * @author $Author: arseniy $
- * @version $Revision: 1.9 $, $Date: 2004/08/22 15:23:26 $
+ * @author $Author: bass $
+ * @version $Revision: 1.10 $, $Date: 2004/08/23 13:02:01 $
  * @module util
  */
 public class ByteArrayDatabase {
+	/**
+	 * @deprecated
+	 */
+	private byte[] bar;
 
 	private ByteArrayDatabase() {
 	}
 
-	public static void saveAsBlob(byte[] bar, Connection conn, String table, String column, String where) throws SQLException {
+	/**
+	 * @deprecated
+	 */
+	public ByteArrayDatabase(byte[] bar) {
+		this.bar = bar;
+	}
+
+	/**
+	 * @deprecated
+	 */
+	public ByteArrayDatabase(ByteArray bArr) {
+		this.bar = bArr.getBytes();
+	}
+
+	/**
+	 * @deprecated
+	 */
+	public void saveAsBlob(Connection conn, String table, String column, String where) throws SQLException {
+		saveAsBlob(bar, conn, table, column, where);
+	}
+
+	public static void saveAsBlob(byte bar[], Connection conn, String table, String column, String where) throws SQLException {
 		boolean oldAutoCommit = conn.getAutoCommit();
 		if (oldAutoCommit)
 			conn.setAutoCommit(false);
@@ -49,22 +75,26 @@ public class ByteArrayDatabase {
 			os.write(bar);
 			os.close();
 		}
-		catch (IOException e) {
-			throw new SQLException(e.getMessage());
+		catch (IOException ioe) {
+			throw new SQLException(ioe.getMessage());
 		}
 		finally {
-			if (statement != null)
-				statement.close();
-			if (ors != null)
-				ors.close();
-			statement = null;
-			ors = null;
-			if (oldAutoCommit)
-				conn.setAutoCommit(true);
+			try {
+				if (statement != null)
+					statement.close();
+			} finally {
+				try {
+					if (ors != null)
+						ors.close();
+				} finally {
+					if (oldAutoCommit)
+						conn.setAutoCommit(true);
+				}
+			}
 		}
 	}
 
-	public static byte[] toByteArray(BLOB blob) throws SQLException{
-		return blob.getBytes(1, (int)blob.length());
+	public static byte[] toByteArray(BLOB blob) throws SQLException {
+		return blob.getBytes(1, (int) blob.length());
 	}
 }
