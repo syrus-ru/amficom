e25ec2415b3d4d968c9d70700819739edf950e91
Stanislav Kholshin <kholshin@gmail.com>
2004-08-25 14:28:53 +0400

    <No Comment Entered>

--- AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/General/UI/FixedSizeEditableTableModel.java
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/General/UI/FixedSizeEditableTableModel.java
@@ -1,15 +1,15 @@
 package com.syrus.AMFICOM.Client.General.UI;
 
-import javax.swing.table.*;
 import java.util.*;
+
+import javax.swing.table.*;
 
 public class FixedSizeEditableTableModel extends AbstractTableModel
 {
-	String[] columnNames;
-	Object[] p_defaultv;
-	//Object[][] data;
-	protected Vector rows = new Vector();
-	int[] editable;
+	protected String[] columnNames;
+	protected Object[] p_defaultv;
+	protected List rows = new ArrayList();
+	protected int[] editable;
 
 	public FixedSizeEditableTableModel (String[] p_columns, // заголовки столбцов
 																			Object[] p_defaultv,// дефолтные значения
@@ -22,27 +22,17 @@ public class FixedSizeEditableTableModel extends AbstractTableModel
 		for (int i = 0; i < p_columns.length; i++)
 			columnNames[i] = new String(p_columns[i]);
 
-		// устанавливаем дефолтные данные
-		//data = new Object[p_rows.length][p_columns.length];
-
-		if (p_rows != null)
-		for (int i = 0; i < p_rows.length; i++)
-		{
-			Vector row = new Vector(p_columns.length);
-			row.add(p_rows[i]);  // устанавливаем заголовки строк
-			for (int j = 0; j < p_defaultv.length; j++)
-			{
-				row.add(p_defaultv[j]); // устанавливаем дефолтные данные
-				//data[i][j] = (p_defaultv[j]);
-			}
-			rows.add(row);
-		}
-
 		// устанавливаем заголовки строк
-	/*	for (int i = 0; i < p_rows.length; i++)
-		{
-			data[i][0] = p_rows[i];
-		}*/
+		if (p_rows != null)
+			for (int i = 0; i < p_rows.length; i++)
+			{
+				List row = new ArrayList(p_columns.length);
+				row.add(p_rows[i]);  // устанавливаем заголовки строк
+				for (int j = 0; j < p_defaultv.length; j++)
+					row.add(p_defaultv[j]); // устанавливаем дефолтные данные
+				rows.add(row);
+			}
+
 		if (editable == null)
 			this.editable = new int[0];
 		else
@@ -56,8 +46,7 @@ public class FixedSizeEditableTableModel extends AbstractTableModel
 
 	public void clearTable()
 	{
-		//data = new Object[][]{};
-		rows = new Vector();
+		rows = new ArrayList();
 		super.fireTableDataChanged();
 	}
 
@@ -68,7 +57,7 @@ public class FixedSizeEditableTableModel extends AbstractTableModel
 
 	public int getRowCount()
 	{
-		return rows.size();//data.length;
+		return rows.size();
 	}
 
 	public String getColumnName(int col)
@@ -78,8 +67,7 @@ public class FixedSizeEditableTableModel extends AbstractTableModel
 
 	public Object getValueAt(int row, int col)
 	{
-		//return data[row][col];
-		Vector r = (Vector)rows.get(row);
+		List r = (List)rows.get(row);
 		return r.get(col);
 	}
 
@@ -88,37 +76,35 @@ public class FixedSizeEditableTableModel extends AbstractTableModel
 		if (c == 0)
 			return String.class;
 		return p_defaultv[c-1].getClass();
-		//return getValueAt(0, c).getClass();
 	}
 
 	public void setValueAt(Object value, int row, int col)
 	{
-		//data[row][col] = value;
-		Vector r = (Vector)rows.get(row);
+		List r = (List)rows.get(row);
 		r.set(col, value);
 		super.fireTableCellUpdated(row, col);
 	}
 
-	public int addRow (String p_row, Object[] p_defaultv)
+	public int addRow (String p_row, Object[] values)
 	{
-		Vector r = new Vector(p_defaultv.length + 1);
+		List r = new ArrayList(values.length + 1);
 		r.add(p_row);
-		for (int i = 0; i < p_defaultv.length; i++)
-			r.add(p_defaultv[i]);
+		for (int i = 0; i < values.length; i++)
+			r.add(values[i]);
 		rows.add(r);
-		super.fireTableRowsInserted(rows.size()-1, rows.size()-1);
-		return rows.size()-1;
+		super.fireTableRowsInserted(rows.size() - 1, rows.size() - 1);
+		return rows.size() - 1;
 	}
 
 	public int addRow (String p_row)
 	{
-		Vector r = new Vector(p_defaultv.length + 1);
+		List r = new ArrayList(p_defaultv.length + 1);
 		r.add(p_row);
 		for (int i = 0; i < p_defaultv.length; i++)
 			r.add(p_defaultv[i]);
 		rows.add(r);
-		super.fireTableRowsInserted(rows.size()-1, rows.size()-1);
-		return rows.size()-1;
+		super.fireTableRowsInserted(rows.size() - 1, rows.size() - 1);
+		return rows.size() - 1;
 	}
 
 	public void removeRow (int row)
@@ -129,38 +115,31 @@ public class FixedSizeEditableTableModel extends AbstractTableModel
 
 	public void updateRow(Object[] updatedRow, int row)
 	{
-		Vector r = (Vector)rows.get(row);
+		List r = (List)rows.get(row);
 
 		for (int i = 0; i < updatedRow.length; i++)
-			//data[row][i] = updatedRow[i];
-			r.setElementAt(updatedRow[i], i+1);
+			r.set(i + 1, updatedRow[i]);
 		super.fireTableDataChanged();
 	}
 
 	public void updateData(Object[][] updatedData)
 	{
-		/*for (int i = 0; i < data.length; i++)
-			for (int j = 1; j < columnNames.length; j++)
-			{
-				data[i][j] = updatedData[i][j-1];
-			}*/
 		for (int i = 0; i < updatedData.length; i++)
 		{
-			Vector r = (Vector)rows.get(i);
+			List r = (List)rows.get(i);
 			for (int j = 0; j < updatedData[i].length; j++)
-				r.setElementAt(updatedData[i][j], j+1);
+				r.set(j + 1, updatedData[i][j]);
 		}
 		super.fireTableDataChanged();
 	}
 
-	public boolean isCellEditable(int p_row, int p_col)
+	public boolean isCellEditable(int row, int col)
 	{
 		for (int i = 0; i < editable.length; i++)
 		{
-			if (p_col == editable[i])
+			if (col == editable[i])
 				return true;
 		}
 		return false;
 	}
-
 }
