2fd679204cfd3e6234474a3354b1fc74180595d6
Maxim Selivanov <max.selivanov@gmail.com>
2004-09-06 17:47:57 +0400

    fixed to satisfy requirements of StorageObjectDatabase

--- AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/EquipmentTypeDatabase.java
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/EquipmentTypeDatabase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: EquipmentTypeDatabase.java,v 1.6 2004/08/29 10:54:23 bob Exp $
+ * $Id: EquipmentTypeDatabase.java,v 1.7 2004/09/06 13:47:57 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,13 +8,13 @@
 
 package com.syrus.AMFICOM.configuration;
 
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.StorableObject;
 import com.syrus.AMFICOM.general.StorableObjectDatabase;
@@ -24,12 +24,13 @@ import com.syrus.AMFICOM.general.UpdateObjectException;
 import com.syrus.AMFICOM.general.IllegalDataException;
 import com.syrus.AMFICOM.general.ObjectNotFoundException;
 import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.VersionCollisionException;
 import com.syrus.util.Log;
 import com.syrus.util.database.DatabaseDate;
 
 /**
- * @version $Revision: 1.6 $, $Date: 2004/08/29 10:54:23 $
- * @author $Author: bob $
+ * @version $Revision: 1.7 $, $Date: 2004/09/06 13:47:57 $
+ * @author $Author: max $
  * @module configuration_v1
  */
 
@@ -39,6 +40,57 @@ public class EquipmentTypeDatabase extends StorableObjectDatabase {
 
 	public static final int CHARACTER_NUMBER_OF_RECORDS = 1;
 	
+	private String updateColumns;
+	private String updateMultiplySQLValues;
+	
+	protected String getEnityName() {
+		return "EquipmentType";
+	}
+	
+	protected String getTableName() {
+		return ObjectEntities.EQUIPMENTTYPE_ENTITY;
+	}
+	
+	protected String getUpdateMultiplySQLValues() {
+		if (this.updateMultiplySQLValues == null){
+			this.updateMultiplySQLValues = QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION;
+		}
+	return this.updateMultiplySQLValues;
+	}
+	
+	protected String getUpdateColumns() {
+		if (this.updateColumns == null){
+			this.updateColumns = COLUMN_ID + COMMA
+				+ COLUMN_CREATED + COMMA
+				+ COLUMN_MODIFIED + COMMA
+				+ COLUMN_CREATOR_ID + COMMA
+				+ COLUMN_MODIFIER_ID + COMMA
+				+ COLUMN_CODENAME + COMMA
+				+ COLUMN_DESCRIPTION;
+		}
+		return this.updateColumns;
+	}
+	
+	protected String getUpdateSingleSQLValues(StorableObject storableObject)
+			throws IllegalDataException, UpdateObjectException {
+		EquipmentType equipmentType = fromStorableObject(storableObject);
+		String etIdStr = equipmentType.getId().toSQLString();
+		String sql = etIdStr + COMMA
+			+ DatabaseDate.toUpdateSubString(equipmentType.getCreated()) + COMMA
+			+ DatabaseDate.toUpdateSubString(equipmentType.getModified()) + COMMA
+			+ equipmentType.getCreatorId().toSQLString() + COMMA
+			+ equipmentType.getModifierId().toSQLString() + COMMA
+			+ APOSTOPHE + equipmentType.getCodename() + APOSTOPHE + COMMA
+			+ APOSTOPHE + equipmentType.getDescription() + APOSTOPHE;
+		return sql;
+	}
+	
 	private EquipmentType fromStorableObject(StorableObject storableObject) throws IllegalDataException {
 		if (storableObject instanceof EquipmentType)
 			return (EquipmentType)storableObject;
@@ -47,10 +99,10 @@ public class EquipmentTypeDatabase extends StorableObjectDatabase {
 
 	public void retrieve(StorableObject storableObject) throws IllegalDataException, ObjectNotFoundException, RetrieveObjectException {
 		EquipmentType equipmentType = this.fromStorableObject(storableObject);
-		this.retrieveEquipmentType(equipmentType);
+		this.retrieveEntity(equipmentType);
 	}
-
-	private String retrieveEquipmentTypeQuery(String condition){
+	
+	protected String retrieveQuery(String condition){
 		return SQL_SELECT
 		+ COLUMN_ID + COMMA
 		+ DatabaseDate.toQuerySubString(COLUMN_CREATED) + COMMA 
@@ -63,16 +115,40 @@ public class EquipmentTypeDatabase extends StorableObjectDatabase {
 		+ ( ((condition == null) || (condition.length() == 0) ) ? "" : SQL_WHERE + condition);
 
 	}
+	 
+	protected void setEntityForPreparedStatement(StorableObject storableObject,
+			PreparedStatement preparedStatement) throws IllegalDataException,
+			UpdateObjectException {
+		EquipmentType equipmentType = fromStorableObject(storableObject);
+		String etIdStr = equipmentType.getId().getCode();
+		try {
+			preparedStatement.setString( 1, etIdStr);
+			preparedStatement.setTimestamp( 2, new Timestamp(equipmentType.
+					getCreated().getTime()));
+			preparedStatement.setTimestamp( 3, new Timestamp(equipmentType.
+					getModified().getTime()));
+			preparedStatement.setString( 4, equipmentType.getCreatorId().getCode());
+			preparedStatement.setString( 5, equipmentType.getModifierId().getCode());
+			preparedStatement.setString( 6, equipmentType.getCodename());
+			preparedStatement.setString( 7, equipmentType.getDescription());
+			preparedStatement.setString( 8, etIdStr);
+		} catch (SQLException sqle) {
+			throw new UpdateObjectException("EquipmentDatabase." +
+					"setEntityForPreparedStatement | Error " + sqle.getMessage(), sqle);
+		}
+	}
 	
-	private EquipmentType updateEquipmentTypeFromResultSet(EquipmentType equipmentType, ResultSet resultSet) throws SQLException{
-		EquipmentType equipmentType1 = equipmentType;
-		if (equipmentType1 == null){
+	protected StorableObject updateEntityFromResultSet(
+			StorableObject storableObject, ResultSet resultSet)
+			throws IllegalDataException, RetrieveObjectException, SQLException {
+		EquipmentType equipmentType = fromStorableObject(storableObject);
+		if (equipmentType == null){
 			/**
 			 * @todo when change DB Identifier model ,change getString() to getLong()
 			 */
-			equipmentType1 = new EquipmentType(new Identifier(resultSet.getString(COLUMN_ID)), null, null, null);			
+			equipmentType = new EquipmentType(new Identifier(resultSet.getString(COLUMN_ID)), null, null, null);			
 		}
-		equipmentType1.setAttributes(DatabaseDate.fromQuerySubString(resultSet, COLUMN_CREATED),
+		equipmentType.setAttributes(DatabaseDate.fromQuerySubString(resultSet, COLUMN_CREATED),
 									DatabaseDate.fromQuerySubString(resultSet, COLUMN_MODIFIED),
 									/**
 										* @todo when change DB Identifier model ,change getString() to getLong()
@@ -86,43 +162,9 @@ public class EquipmentTypeDatabase extends StorableObjectDatabase {
 									resultSet.getString(COLUMN_DESCRIPTION));
 
 		
-		return equipmentType1;
+		return equipmentType;
 	}
 	
-
-	private void retrieveEquipmentType(EquipmentType equipmentType) throws ObjectNotFoundException, RetrieveObjectException {
-		String etIdStr = equipmentType.getId().toSQLString();
-		String sql = retrieveEquipmentTypeQuery(COLUMN_ID + EQUALS + etIdStr);
-		Statement statement = null;
-		ResultSet resultSet = null;
-		try {
-			statement = connection.createStatement();
-			Log.debugMessage("EquipmentTypeDatabase.retrieve | Trying: " + sql, Log.DEBUGLEVEL09);
-			resultSet = statement.executeQuery(sql);
-			if (resultSet.next())
-				updateEquipmentTypeFromResultSet(equipmentType, resultSet);
-			else
-				throw new ObjectNotFoundException("No such equipment type: " + etIdStr);
-		}
-		catch (SQLException sqle) {
-			String mesg = "EquipmentTypeDatabase.retrieve | Cannot retrieve Equipment type " + etIdStr;
-			throw new RetrieveObjectException(mesg, sqle);
-		}
-		finally {
-			try {
-				if (statement != null)
-					statement.close();
-				if (resultSet != null)
-					resultSet.close();
-				statement = null;
-				resultSet = null;
-			}
-			catch (SQLException sqle1) {
-				Log.errorException(sqle1);
-			}
-		}
-	}
-
 	public Object retrieveObject(StorableObject storableObject, int retrieve_kind, Object arg) throws IllegalDataException, ObjectNotFoundException, RetrieveObjectException {
 		EquipmentType equipmentType = this.fromStorableObject(storableObject);
 		switch (retrieve_kind) {
@@ -134,7 +176,7 @@ public class EquipmentTypeDatabase extends StorableObjectDatabase {
 	public void insert(StorableObject storableObject) throws IllegalDataException, CreateObjectException {
 		EquipmentType equipmentType = this.fromStorableObject(storableObject);
 		try {
-			this.insertEquipmentType(equipmentType);
+			super.insertEntity(equipmentType);
 		}
 		catch (CreateObjectException e) {
 			try {
@@ -155,61 +197,40 @@ public class EquipmentTypeDatabase extends StorableObjectDatabase {
 		}
 	}
 
-	private void insertEquipmentType(EquipmentType equipmentType) throws CreateObjectException {
-		String etIdStr = equipmentType.getId().toSQLString();
-		String sql = SQL_INSERT_INTO
-			+ ObjectEntities.EQUIPMENTTYPE_ENTITY
-			+ OPEN_BRACKET
-			+ COLUMN_ID + COMMA
-			+ COLUMN_CREATED + COMMA
-			+ COLUMN_MODIFIED + COMMA
-			+ COLUMN_CREATOR_ID + COMMA
-			+ COLUMN_MODIFIER_ID + COMMA
-			+ COLUMN_CODENAME + COMMA
-			+ COLUMN_DESCRIPTION
-			+ CLOSE_BRACKET
-			+ SQL_VALUES
-			+ OPEN_BRACKET
-			+ etIdStr + COMMA
-			+ DatabaseDate.toUpdateSubString(equipmentType.getCreated()) + COMMA
-			+ DatabaseDate.toUpdateSubString(equipmentType.getModified()) + COMMA
-			+ equipmentType.getCreatorId().toSQLString() + COMMA
-			+ equipmentType.getModifierId().toSQLString() + COMMA
-			+ APOSTOPHE + equipmentType.getCodename() + APOSTOPHE + COMMA
-			+ APOSTOPHE + equipmentType.getDescription() + APOSTOPHE
-			+ CLOSE_BRACKET;
-		Statement statement = null;
-		try {
-			statement = connection.createStatement();
-			Log.debugMessage("EquipmentTypeDatabase.insert | Trying: " + sql, Log.DEBUGLEVEL09);
-			statement.executeUpdate(sql);
-		}
-		catch (SQLException sqle) {
-			String mesg = "EquipmentTypeDatabase.insert | Cannot insert equipment type " + etIdStr;
-			throw new CreateObjectException(mesg, sqle);
-		}
-		finally {
-			try {
-				if (statement != null)
-					statement.close();
-				statement = null;
-			}
-			catch (SQLException sqle1) {
-				Log.errorException(sqle1);
-			}
+	public void insert(List storableObjects) throws IllegalDataException,
+			CreateObjectException {
+		super.insertEntities(storableObjects);
+	}
+	
+	public void update(StorableObject storableObject, int updateKind, Object obj)
+			throws IllegalDataException, VersionCollisionException, UpdateObjectException {
+		switch (updateKind) {
+		case UPDATE_FORCE:
+			super.checkAndUpdateEntity(storableObject, true);
+			break;
+		case UPDATE_CHECK: 					
+		default:
+			super.checkAndUpdateEntity(storableObject, false);
+		break;
 		}
 	}
 
-	public void update(StorableObject storableObject, int update_kind, Object obj) throws IllegalDataException, UpdateObjectException {
-		EquipmentType equipmentType = this.fromStorableObject(storableObject);
-		switch (update_kind) {
-			default:
-				return;
+	public void update(List storableObjects, int updateKind, Object arg)
+			throws IllegalDataException, VersionCollisionException, UpdateObjectException {
+		switch (updateKind) {
+		case UPDATE_FORCE:
+			super.checkAndUpdateEntities(storableObjects, true);
+			break;
+		case UPDATE_CHECK: 					
+		default:
+			super.checkAndUpdateEntities(storableObjects, false);
+			break;
 		}
 	}
-
-	public List retrieveAll() throws RetrieveObjectException {		
-		return retriveByIdsOneQuery(null);
+	
+		
+	public List retrieveAll() throws IllegalDataException, RetrieveObjectException {		
+		return super.retriveByIdsOneQuery(null, null);
 	}
 	
 	public void delete(EquipmentType equipmentType) {
@@ -241,127 +262,11 @@ public class EquipmentTypeDatabase extends StorableObjectDatabase {
 		}
 	}
 	
-	public List retrieveByIds(List ids) throws RetrieveObjectException {
+	public List retrieveByIds(List ids, String condition) 
+			throws IllegalDataException, RetrieveObjectException {
 		if ((ids == null) || (ids.isEmpty()))
-			return retriveByIdsOneQuery(null);
-		return retriveByIdsOneQuery(ids);	
+			return super.retriveByIdsOneQuery(null, condition);
+		return super.retriveByIdsOneQuery(ids, condition);	
 		//return retriveByIdsPreparedStatement(ids);
 	}
-	
-	private List retriveByIdsOneQuery(List ids) throws RetrieveObjectException {
-		List result = new LinkedList();
-		String sql;
-		{
-			String condition = null;
-			if (ids!=null){
-				StringBuffer buffer = new StringBuffer(COLUMN_ID);
-				int idsLength = ids.size();
-				if (idsLength == 1){
-					buffer.append(EQUALS);
-					buffer.append(((Identifier)ids.iterator().next()).toSQLString());
-				} else{
-					buffer.append(SQL_IN);
-					buffer.append(OPEN_BRACKET);
-					
-					int i = 1;
-					for(Iterator it=ids.iterator();it.hasNext();i++){
-						Identifier id = (Identifier)it.next();
-						buffer.append(id.toSQLString());
-						if (i < idsLength)
-							buffer.append(COMMA);
-					}
-					
-					buffer.append(CLOSE_BRACKET);
-					condition = buffer.toString();
-				}
-			}
-			sql = retrieveEquipmentTypeQuery(condition);
-		}
-		
-		Statement statement = null;
-		ResultSet resultSet = null;
-		try {
-			statement = connection.createStatement();
-			Log.debugMessage("EquipmentTypeDatabase.retriveByIdsOneQuery | Trying: " + sql, Log.DEBUGLEVEL09);
-			resultSet = statement.executeQuery(sql);
-			while (resultSet.next()){
-				result.add(updateEquipmentTypeFromResultSet(null, resultSet));
-			}
-		}
-		catch (SQLException sqle) {
-			String mesg = "EquipmentTypeDatabase.retriveByIdsOneQuery | Cannot execute query " + sqle.getMessage();
-			throw new RetrieveObjectException(mesg, sqle);
-		}
-		finally {
-			try {
-				if (statement != null)
-					statement.close();
-				if (resultSet != null)
-					resultSet.close();
-				statement = null;
-				resultSet = null;
-			}
-			catch (SQLException sqle1) {
-				Log.errorException(sqle1);
-			}
-		}
-		return result;
-	}
-	
-	private List retriveByIdsPreparedStatement(List ids) throws RetrieveObjectException {
-		List result = new LinkedList();
-		String sql;
-		{
-			
-			int idsLength = ids.size();
-			if (idsLength == 1){
-				return retriveByIdsOneQuery(ids);
-			}
-			StringBuffer buffer = new StringBuffer(COLUMN_ID);
-			buffer.append(EQUALS);							
-			buffer.append(QUESTION);
-			
-			sql = retrieveEquipmentTypeQuery(buffer.toString());
-		}
-			
-		PreparedStatement stmt = null;
-		ResultSet resultSet = null;
-		try {
-			stmt = connection.prepareStatement(sql.toString());
-			for(Iterator it = ids.iterator();it.hasNext();){
-				Identifier id = (Identifier)it.next(); 
-				/**
-				 * @todo when change DB Identifier model ,change setString() to setLong()
-				 */
-				String idStr = id.getIdentifierString();
-				stmt.setString(1, idStr);
-				resultSet = stmt.executeQuery();
-				if (resultSet.next()){
-					result.add(updateEquipmentTypeFromResultSet(null, resultSet));
-				} else{
-					Log.errorMessage("EquipmentTypeDatabase.retriveByIdsPreparedStatement | No such equipment type: " + idStr);									
-				}
-				
-			}
-		}catch (SQLException sqle) {
-			String mesg = "EquipmentTypeDatabase.retriveByIdsPreparedStatement | Cannot retrieve equipment type " + sqle.getMessage();
-			throw new RetrieveObjectException(mesg, sqle);
-		}
-		finally {
-			try {
-				if (stmt != null)
-					stmt.close();
-				if (stmt != null)
-					stmt.close();
-				stmt = null;
-				resultSet = null;
-			}
-			catch (SQLException sqle1) {
-				Log.errorException(sqle1);
-			}
-		}			
-		
-		return result;
-	}
-
 }
