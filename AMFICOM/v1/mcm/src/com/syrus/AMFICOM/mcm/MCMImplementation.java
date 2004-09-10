139d0f18791843fbb79314b055e08fc71316f360
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-09-10 15:20:26 +0400

    insert received tests by db driver

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/MCMImplementation.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/MCMImplementation.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MCMImplementation.java,v 1.13 2004/08/31 15:35:23 bob Exp $
+ * $Id: MCMImplementation.java,v 1.14 2004/09/10 11:20:26 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,9 +8,14 @@
 
 package com.syrus.AMFICOM.mcm;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import com.syrus.AMFICOM.mcm.corba.MCMPOA;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.IllegalDataException;
 import com.syrus.AMFICOM.general.RetrieveObjectException;
 import com.syrus.AMFICOM.general.CreateObjectException;
 import com.syrus.AMFICOM.general.ObjectNotFoundException;
@@ -18,11 +23,13 @@ import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
 import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
 import com.syrus.AMFICOM.general.corba.ErrorCode;
 import com.syrus.AMFICOM.general.corba.CompletionStatus;
+import com.syrus.AMFICOM.measurement.MeasurementDatabaseContext;
 import com.syrus.AMFICOM.measurement.MeasurementStorableObjectPool;
 import com.syrus.AMFICOM.measurement.Measurement;
 import com.syrus.AMFICOM.measurement.Analysis;
 import com.syrus.AMFICOM.measurement.Evaluation;
 import com.syrus.AMFICOM.measurement.Test;
+import com.syrus.AMFICOM.measurement.TestDatabase;
 import com.syrus.AMFICOM.measurement.corba.Measurement_Transferable;
 import com.syrus.AMFICOM.measurement.corba.Analysis_Transferable;
 import com.syrus.AMFICOM.measurement.corba.Evaluation_Transferable;
@@ -30,7 +37,7 @@ import com.syrus.AMFICOM.measurement.corba.Test_Transferable;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.13 $, $Date: 2004/08/31 15:35:23 $
+ * @version $Revision: 1.14 $, $Date: 2004/09/10 11:20:26 $
  * @author $Author: bob $
  * @module mcm_v1
  */
@@ -42,12 +49,13 @@ public class MCMImplementation extends MCMPOA {
 
 	public void receiveTests(Test_Transferable[] testsT) throws AMFICOMRemoteException {
 		Log.debugMessage("Received " + testsT.length + " tests", Log.DEBUGLEVEL07);
-		Test test;
+		List testList = new ArrayList(testsT.length);
 		for (int i = 0; i < testsT.length; i++) {
 			try {
-				test = Test.getInstance(testsT[i]);
-				MeasurementControlModule.addTest(test);
-			}
+				Test test = new Test(testsT[i]);
+				testList.add(test);
+			}	
+			
 			catch (CreateObjectException coe) {
 				Log.errorException(coe);
 				throw new AMFICOMRemoteException(ErrorCode.ERROR_SAVE, CompletionStatus.COMPLETED_NO, coe.getMessage());
@@ -56,6 +64,22 @@ public class MCMImplementation extends MCMPOA {
 				Log.errorException(e);
 				throw new AMFICOMRemoteException();
 			}
+		}
+		
+		TestDatabase testDatabase = (TestDatabase) MeasurementDatabaseContext.getTestDatabase();
+		try {
+			testDatabase.insert(testList);
+		} catch (CreateObjectException e) {
+			Log.errorException(e);
+			throw new AMFICOMRemoteException();
+		} catch (IllegalDataException e) {
+			Log.errorException(e);
+			throw new AMFICOMRemoteException();
+		}
+		
+		for (Iterator it = testList.iterator(); it.hasNext();) {
+			Test test = (Test) it.next();
+			MeasurementControlModule.addTest(test);			
 		}
 	}
 
