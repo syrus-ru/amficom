748c7b27a7baf064c1abf2a0ff302ac5e970f2aa
cvs-fast-export <cvs-fast-export>
2004-09-10 11:45:18 +0000

    Synthetic commit for incomplete tag AMFICOM-0_0_1

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectLoader.java
+++
@@ -1,7 +0,0 @@
-package com.syrus.AMFICOM.general;
-
-public interface StorableObjectLoader {
-
-	StorableObject loadStorableObject(Identifier objectId) throws DatabaseException, CommunicationException;
-
-}
