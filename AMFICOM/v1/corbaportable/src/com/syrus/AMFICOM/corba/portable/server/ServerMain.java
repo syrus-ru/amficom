0f1a98237bc051eda4e5ef3003b9a3408058f6a3
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-09-14 17:42:59 +0400

    Now integrates with the new server.

--- AMFICOM/v1/corbaportable/src/com/syrus/AMFICOM/corba/portable/server/ServerMain.java
+++ AMFICOM/v1/corbaportable/src/com/syrus/AMFICOM/corba/portable/server/ServerMain.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ServerMain.java,v 1.1 2004/06/22 12:27:26 bass Exp $
+ * $Id: ServerMain.java,v 1.2 2004/09/14 13:42:59 bass Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,16 +8,24 @@
 
 package com.syrus.AMFICOM.corba.portable.server;
 
+import com.syrus.AMFICOM.CORBA.*;
 import com.syrus.AMFICOM.corba.portable.reflect.*;
+import com.syrus.AMFICOM.server.measurement.AmficomRtuImpl;
+import com.syrus.AMFICOM.server.object.AmficomImpl;
+import com.syrus.AMFICOM.server.prefs.JdbcConnectionManager;
 import com.syrus.util.corba.JavaSoftORBUtil;
 import java.io.IOException;
+import java.net.InetAddress;
+import oracle.jdbc.pool.OracleDataSource;
 import org.omg.CORBA.*;
 import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.AlreadyBound;
 import org.omg.PortableServer.*;
 
 /**
- * @version $Revision: 1.1 $, $Date: 2004/06/22 12:27:26 $
  * @author $Author: bass $
+ * @version $Revision: 1.2 $, $Date: 2004/09/14 13:42:59 $
+ * @module corbaportable_v1
  */
 final class ServerMain {
 	private ServerMain() {
@@ -29,27 +37,38 @@ final class ServerMain {
 		POA rootPOA = POAHelper.narrow(
 			orb.resolve_initial_references("RootPOA"));
 		rootPOA.the_POAManager().activate();
-		NamingContextExt namingContextExt = NamingContextExtHelper.
+		NamingContextExt rootNamingCtx = NamingContextExtHelper.
 			narrow(orb.resolve_initial_references("NameService"));
+			
+		final String hostName = InetAddress.getLocalHost().getCanonicalHostName().replaceAll("\\.", "_");
+		
+		NameComponent childPath[] = rootNamingCtx.to_name(hostName);
+		
+		NamingContextExt childNamingCtx;
+		try {
+			childNamingCtx = NamingContextExtHelper.narrow(rootNamingCtx.bind_new_context(childPath));
+		} catch (AlreadyBound ab) {
+			childNamingCtx = NamingContextExtHelper.narrow(rootNamingCtx.resolve_str(hostName));
+		}
 
 		AlarmTypeUtilities alarmTypeUtilities = (new AlarmTypeUtilitiesPOATie(
 			new AlarmTypeUtilitiesImpl(), rootPOA))._this(orb);
 		NameComponent alarmTypeUtilitiesPath[]
-			= namingContextExt.to_name("AlarmTypeUtilities");
-		namingContextExt.rebind(alarmTypeUtilitiesPath, alarmTypeUtilities);
+			= rootNamingCtx.to_name("AlarmTypeUtilities");
+		childNamingCtx.rebind(alarmTypeUtilitiesPath, alarmTypeUtilities);
 
 		AlertingUtilities alertingUtilities = (new AlertingUtilitiesPOATie(
 			new AlertingUtilitiesImpl(), rootPOA))._this(orb);
 		NameComponent alertingUtilitiesPath[]
-			= namingContextExt.to_name("AlertingUtilities");
-		namingContextExt.rebind(alertingUtilitiesPath, alertingUtilities);
+			= rootNamingCtx.to_name("AlertingUtilities");
+		childNamingCtx.rebind(alertingUtilitiesPath, alertingUtilities);
 
 		AlertingMessageTextUtilities alertingMessageTextUtilities = (
 			new AlertingMessageTextUtilitiesPOATie(
 				new AlertingMessageTextUtilitiesImpl(), rootPOA))._this(orb);
 		NameComponent alertingMessageTextUtilitiesPath[]
-			= namingContextExt.to_name("AlertingMessageTextUtilities");
-		namingContextExt.rebind(alertingMessageTextUtilitiesPath,
+			= rootNamingCtx.to_name("AlertingMessageTextUtilities");
+		childNamingCtx.rebind(alertingMessageTextUtilitiesPath,
 			alertingMessageTextUtilities);
 
 		AlertingMessageUserLinkUtilities alertingMessageUserLinkUtilities = (
@@ -57,65 +76,81 @@ final class ServerMain {
 				new AlertingMessageUserLinkUtilitiesImpl(),
 				rootPOA))._this(orb);
 		NameComponent alertingMessageUserLinkUtilitiesPath[]
-			= namingContextExt.to_name("AlertingMessageUserLinkUtilities");
-		namingContextExt.rebind(alertingMessageUserLinkUtilitiesPath,
+			= rootNamingCtx.to_name("AlertingMessageUserLinkUtilities");
+		childNamingCtx.rebind(alertingMessageUserLinkUtilitiesPath,
 			alertingMessageUserLinkUtilities);
 
 		AlertingTypeUtilities alertingTypeUtilities = (
 			new AlertingTypeUtilitiesPOATie(
 				new AlertingTypeUtilitiesImpl(), rootPOA))._this(orb);
 		NameComponent alertingTypeUtilitiesPath[]
-			= namingContextExt.to_name("AlertingTypeUtilities");
-		namingContextExt.rebind(alertingTypeUtilitiesPath,
+			= rootNamingCtx.to_name("AlertingTypeUtilities");
+		childNamingCtx.rebind(alertingTypeUtilitiesPath,
 			alertingTypeUtilities);
 
 		EventSourceUtilities eventSourceUtilities = (
 			new EventSourceUtilitiesPOATie(
 				new EventSourceUtilitiesImpl(), rootPOA))._this(orb);
 		NameComponent eventSourceUtilitiesPath[]
-			= namingContextExt.to_name("EventSourceUtilities");
-		namingContextExt.rebind(eventSourceUtilitiesPath, eventSourceUtilities);
+			= rootNamingCtx.to_name("EventSourceUtilities");
+		childNamingCtx.rebind(eventSourceUtilitiesPath, eventSourceUtilities);
 
 		EventSourceTypeUtilities eventSourceTypeUtilities = (
 			new EventSourceTypeUtilitiesPOATie(
 				new EventSourceTypeUtilitiesImpl(), rootPOA))._this(orb);
 		NameComponent eventSourceTypeUtilitiesPath[]
-			= namingContextExt.to_name("EventSourceTypeUtilities");
-		namingContextExt.rebind(eventSourceTypeUtilitiesPath,
+			= rootNamingCtx.to_name("EventSourceTypeUtilities");
+		childNamingCtx.rebind(eventSourceTypeUtilitiesPath,
 			eventSourceTypeUtilities);
 
 		EventTypeUtilities eventTypeUtilities = (new EventTypeUtilitiesPOATie(
 			new EventTypeUtilitiesImpl(), rootPOA))._this(orb);
 		NameComponent eventTypeUtilitiesPath[]
-			= namingContextExt.to_name("EventTypeUtilities");
-		namingContextExt.rebind(eventTypeUtilitiesPath, eventTypeUtilities);
+			= rootNamingCtx.to_name("EventTypeUtilities");
+		childNamingCtx.rebind(eventTypeUtilitiesPath, eventTypeUtilities);
 
 		MessageTypeUtilities messageTypeUtilities = (
 			new MessageTypeUtilitiesPOATie(
 				new MessageTypeUtilitiesImpl(), rootPOA))._this(orb);
 		NameComponent messageTypeUtilitiesPath[]
-			= namingContextExt.to_name("MessageTypeUtilities");
-		namingContextExt.rebind(messageTypeUtilitiesPath, messageTypeUtilities);
+			= rootNamingCtx.to_name("MessageTypeUtilities");
+		childNamingCtx.rebind(messageTypeUtilitiesPath, messageTypeUtilities);
 
 		UserUtilities userUtilities = (new UserUtilitiesPOATie(
 			new UserUtilitiesImpl(), rootPOA))._this(orb);
 		NameComponent userUtilitiesPath[]
-			= namingContextExt.to_name("UserUtilities");
-		namingContextExt.rebind(userUtilitiesPath, userUtilities);
+			= rootNamingCtx.to_name("UserUtilities");
+		childNamingCtx.rebind(userUtilitiesPath, userUtilities);
 
-		System.out.println(
-			"The server is now running. Press any key to terminate it...");
+		AMFICOM amficom = (new AMFICOMPOATie(
+			new AmficomImpl(), rootPOA))._this(orb);
+		NameComponent amficomPath[]
+			= rootNamingCtx.to_name("Amficom");
+		childNamingCtx.rebind(amficomPath, amficom);
+
+		AMFICOMKIS amficomRtu = (new AMFICOMKISPOATie(
+			new AmficomRtuImpl(), rootPOA))._this(orb);
+		NameComponent amficomRtuPath[]
+			= rootNamingCtx.to_name("AmficomRtu");
+		childNamingCtx.rebind(amficomRtuPath, amficomRtu);
+
+		System.err.println(
+			"The server is now running. Press enter to terminate it...");
 		System.in.read();
 
-		namingContextExt.unbind(alarmTypeUtilitiesPath);
-		namingContextExt.unbind(alertingUtilitiesPath);
-		namingContextExt.unbind(alertingMessageTextUtilitiesPath);
-		namingContextExt.unbind(alertingMessageUserLinkUtilitiesPath);
-		namingContextExt.unbind(alertingTypeUtilitiesPath);
-		namingContextExt.unbind(eventSourceUtilitiesPath);
-		namingContextExt.unbind(eventSourceTypeUtilitiesPath);
-		namingContextExt.unbind(eventTypeUtilitiesPath);
-		namingContextExt.unbind(messageTypeUtilitiesPath);
-		namingContextExt.unbind(userUtilitiesPath);
+		childNamingCtx.unbind(alarmTypeUtilitiesPath);
+		childNamingCtx.unbind(alertingUtilitiesPath);
+		childNamingCtx.unbind(alertingMessageTextUtilitiesPath);
+		childNamingCtx.unbind(alertingMessageUserLinkUtilitiesPath);
+		childNamingCtx.unbind(alertingTypeUtilitiesPath);
+		childNamingCtx.unbind(eventSourceUtilitiesPath);
+		childNamingCtx.unbind(eventSourceTypeUtilitiesPath);
+		childNamingCtx.unbind(eventTypeUtilitiesPath);
+		childNamingCtx.unbind(messageTypeUtilitiesPath);
+		childNamingCtx.unbind(userUtilitiesPath);
+		childNamingCtx.unbind(amficomPath);
+		childNamingCtx.unbind(amficomRtuPath);
+
+		rootNamingCtx.unbind(childPath);
 	}
 }
