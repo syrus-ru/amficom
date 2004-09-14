7ffe74f62bcb61a62c92cefef9dd31040cfa5b12
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-09-14 18:44:38 +0400

    [no comments]

--- AMFICOM/v1/adminclient/src/com/syrus/AMFICOM/Client/General/Model/AdminApplicationModel.java
+++ AMFICOM/v1/adminclient/src/com/syrus/AMFICOM/Client/General/Model/AdminApplicationModel.java
@@ -1,46 +1,55 @@
+/*
+ * $Id: AdminApplicationModel.java,v 1.2 2004/09/14 14:44:38 bass Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+
 package com.syrus.AMFICOM.Client.General.Model;
 
-import com.syrus.AMFICOM.Client.General.*;
+import com.syrus.AMFICOM.Client.General.SessionInterface;
 import com.syrus.AMFICOM.Client.Resource.*;
 
-public class AdminApplicationModel extends ApplicationModel
-{
-	public AdminApplicationModel()
-	{
-		super();
+/**
+ * @version $Revision: 1.2 $, $Date: 2004/09/14 14:44:38 $
+ * @author $Author: bass $
+ * @module admin_v1
+ */
+public final class AdminApplicationModel extends ApplicationModel {
+	private SessionInterface session = null;
 
+	private DataSourceInterface dataSource = null;
+
+	public AdminApplicationModel() {
 		add("menuSession");
 		add("menuSessionNew");
 		add("menuSessionClose");
 		add("menuSessionOptions");
 		add("menuSessionConnection");
 		add("menuSessionChangePassword");
-//		add("menuSessionSave");
-//		add("menuSessionUndo");
 		add("menuExit");
 
+		add("menuView");
+		add("menuViewNavigator");
+		add("menuViewOpenAll");
+		add("menuViewWhoAmI");
+		add("menuViewOpenObjectsWindow");
 
-    add("menuView");
-    add("menuViewNavigator");
-    add("menuViewOpenAll");
-    add("menuViewWhoAmI");
-    add("menuViewOpenObjectsWindow");
+		add("menuArchitecture");
+		add("menuArchitectureServer");
+		add("menuArchitectureAgent");
+		add("menuArchitectureClient");
 
-    add("menuArchitecture");
-    add("menuArchitectureServer");
-    add("menuArchitectureAgent");
-    add("menuArchitectureClient");
+		add("menuUser");
+		add("menuUserCategory");
+		add("menuUserGroup");
+		add("menuUserProfile");
 
-    add("menuUser");
-    add("menuUserCategory");
-    add("menuUserGroup");
-    add("menuUserProfile");
-
-
-    add("menuAccess");
-    add("menuAccessDomain");
-    add("menuAccessModul");
-    add("menuAccessMaintain");
+		add("menuAccess");
+		add("menuAccessDomain");
+		add("menuAccessModul");
+		add("menuAccessMaintain");
 
 		add("menuHelp");
 		add("menuHelpContents");
@@ -54,16 +63,15 @@ public class AdminApplicationModel extends ApplicationModel
 		add("menuHelpAbout");
 	}
 
-	public DataSourceInterface getDataSource(SessionInterface si)
-	{
-		String connection = Environment.getConnectionType();
-		if(connection.equals("RISD"))
-			return new RISDObjectDataSource(si);
-		else
-		if(connection.equals("Empty"))
-			return new EmptyObjectDataSource(si);
-		return null;
-
+	public DataSourceInterface getDataSource(SessionInterface session) {
+		if ((this.session == null) || (!this.session.equals(session))) {
+			synchronized (this) {
+				if ((this.session == null) || (!this.session.equals(session))) {
+					this.session = session;
+					this.dataSource = new RISDObjectDataSource(this.session);
+				}
+			}
+		}
+		return this.dataSource;
 	}
-
 }
--- AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/General/Report/AMTReport.java
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/General/Report/AMTReport.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AMTReport.java,v 1.3 2004/09/14 12:38:13 bass Exp $
+ * $Id: AMTReport.java,v 1.4 2004/09/14 14:44:18 bass Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -11,8 +11,9 @@ package com.syrus.AMFICOM.Client.General.Report;
 import java.util.Hashtable;
 
 /**
- * @version $Revision: 1.3 $, $Date: 2004/09/14 12:38:13 $
+ * @version $Revision: 1.4 $, $Date: 2004/09/14 14:44:18 $
  * @author $Author: bass $
+ * @module generalclient_v1
  */
 public class AMTReport {
 	public Hashtable data = new Hashtable();
