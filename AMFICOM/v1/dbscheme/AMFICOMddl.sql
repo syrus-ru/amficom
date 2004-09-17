aac1b9d1e3bf1a3a1334c23886e86332b9dc4fab
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2004-09-17 16:33:20 +0400

    initial stable version as of AMFICOM-0_0_1

---
+++ AMFICOM/v1/dbscheme/AMFICOMddl.sql
@@ -0,0 +1,13 @@
+-- D:\My Documents\ISM\model\sept_17_2004\AMFICOMddl.sql
+--
+-- Generated for Oracle 9i on Fri Sep 17  16:08:30 2004 by Server Generator 9.0.4.2.10
+
+
+SPOOL AMFICOMddl.lst
+
+@@AMFICOMddl.tab
+@@AMFICOMddl.con
+@@AMFICOMddl.sqs
+@@AMFICOMddl.trg
+
+SPOOL OFF
