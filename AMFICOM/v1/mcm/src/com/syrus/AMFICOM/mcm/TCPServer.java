6bbe5ea9731405339015adc638d269a6727749a9
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-09-20 10:52:49 +0400

    fix code

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/TCPServer.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/TCPServer.java
@@ -5,17 +5,51 @@ import com.syrus.util.Log;
 import java.util.Collections;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
 
 public class TCPServer implements Runnable
 {
+	private class TCPAcceptingThread implements Runnable
+	{
+	  private TCPServer tcpServer = null;
+	  
+	  public TCPAcceptingThread(TCPServer tcpServer)
+	  {
+	    this.tcpServer = tcpServer;
+	  }
+
+	  public void run()
+	  {
+	    while(true)
+	    {
+	      char[] kisIDChars = new char[TCPServer.MAX_KIS_ID_LENGTH];
+	      int connectedSocket = this.tcpServer.getConnectedSocket(
+	        this.tcpServer.listeningSocket,
+	        kisIDChars);
+	      
+	      if (connectedSocket == -1)
+	      {
+	        Log.errorMessage("Can't establish connection!");
+	        continue;
+	      }
+	      Identifier kisID = new Identifier(new String(kisIDChars));
+	      
+	      TCPServer.kissockets.put(kisID,new Integer(connectedSocket));
+	        
+	      Transceiver transceiver = new Transceiver(kisID);
+	      transceiver.start();
+	      this.tcpServer.transceivers.put(kisID, transceiver);
+	      Log.errorMessage("Started transceiver for kis '" + kisID.toString() + "'");
+	    }
+	  }
+	}
+	
   public static final int MAX_KIS_ID_LENGTH = 32;
   public native int getListeningSocket(byte[] serviceName);
   public native int getConnectedSocket(
     int listeningSocket,
     char[] kis_id);
     
-  public native void shutdown_server(int[] serverSockets);  
+  public native void shutdownServer(int[] serverSockets);  
 
   protected int listeningSocket = -1;
   protected static Map kissockets = new HashMap();
@@ -32,16 +66,16 @@ public class TCPServer implements Runnable
     Collections.synchronizedMap(transceivers);    
   
     byte[] serviceNameChars = serviceName.getBytes();
-    listeningSocket = this.getListeningSocket(serviceNameChars);
-    if (listeningSocket <= 0)
+    this.listeningSocket = this.getListeningSocket(serviceNameChars);
+    if (this.listeningSocket <= 0)
     {
         Log.errorMessage("Can't create listening socket for service (port) " +
           serviceName + "!");
         throw new Exception ("Listening socket required for further work!");
     }  
     
-    acceptingThread = new Thread(new TCPAcceptingThread(this));
-    acceptingThread.start();
+    this.acceptingThread = new Thread(new TCPAcceptingThread(this));
+    this.acceptingThread.start();
   }
 
   public static int getSocketForKisID (Identifier kisID)
@@ -56,11 +90,11 @@ public class TCPServer implements Runnable
   {
     try
     {
-      while (active)
+      while (this.active)
       {
         Thread.sleep(1000);
       }
-      acceptingThread.interrupt();
+      this.acceptingThread.interrupt();
     }
     catch (InterruptedException ie)
     {
@@ -70,43 +104,6 @@ public class TCPServer implements Runnable
   
   public void shutdown()
   {
-    active = false;
-  }
-}
-
-class TCPAcceptingThread implements Runnable
-{
-  private TCPServer tcpServer = null;
-  
-  private boolean active = true;
-  
-  public TCPAcceptingThread(TCPServer tcpServer)
-  {
-    this.tcpServer = tcpServer;
-  }
-
-  public void run()
-  {
-    while(true)
-    {
-      char[] kisIDChars = new char[TCPServer.MAX_KIS_ID_LENGTH];
-      int connectedSocket = tcpServer.getConnectedSocket(
-        tcpServer.listeningSocket,
-        kisIDChars);
-      
-      if (connectedSocket == -1)
-      {
-        Log.errorMessage("Can't establish connection!");
-        continue;
-      }
-      Identifier kisID = new Identifier(new String(kisIDChars));
-      
-      tcpServer.kissockets.put(kisID,new Integer(connectedSocket));
-        
-      Transceiver transceiver = new Transceiver(kisID);
-      transceiver.start();
-      tcpServer.transceivers.put(kisID, transceiver);
-      Log.errorMessage("Started transceiver for kis '" + kisID.toString() + "'");
-    }
+    this.active = false;
   }
 }
