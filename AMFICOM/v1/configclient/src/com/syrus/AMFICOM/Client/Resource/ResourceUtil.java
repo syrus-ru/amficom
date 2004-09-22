ec5d9456cbff68d2e82ec10e2fb3986a92e19fdc
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2004-09-22 17:36:05 +0400

    *** empty log message ***

--- AMFICOM/v1/configclient/src/com/syrus/AMFICOM/Client/Resource/ResourceUtil.java
+++ AMFICOM/v1/configclient/src/com/syrus/AMFICOM/Client/Resource/ResourceUtil.java
@@ -80,5 +80,21 @@ public class ResourceUtil
 		}
 		return n;
 	}
+
+	public static List parseStrings(String s)
+	{
+		List ids = new LinkedList();
+		int i = 0;
+		while(i < s.length())
+		{
+			int space = s.indexOf(" ", i);
+			String s1 = s.substring(i, space);
+			if(s1.length() != 0)
+				ids.add(s1);
+			i = space + 1;
+		}
+		return ids;
+	}
+
 }
 
