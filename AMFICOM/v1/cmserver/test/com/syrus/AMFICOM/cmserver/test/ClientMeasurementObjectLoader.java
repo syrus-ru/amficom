ade644651ca5c6f9a0d39bfd47a3178be04e63c9
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-09-22 16:54:25 +0400

    misc

--- AMFICOM/v1/cmserver/test/com/syrus/AMFICOM/cmserver/test/ClientMeasurementObjectLoader.java
+++ AMFICOM/v1/cmserver/test/com/syrus/AMFICOM/cmserver/test/ClientMeasurementObjectLoader.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ClientMeasurementObjectLoader.java,v 1.3 2004/09/22 10:59:00 bob Exp $
+ * $Id: ClientMeasurementObjectLoader.java,v 1.4 2004/09/22 12:54:25 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -34,12 +34,19 @@ import com.syrus.AMFICOM.measurement.Result;
 import com.syrus.AMFICOM.measurement.Set;
 import com.syrus.AMFICOM.measurement.TemporalPattern;
 import com.syrus.AMFICOM.measurement.Test;
+import com.syrus.AMFICOM.measurement.corba.AnalysisType_Transferable;
+import com.syrus.AMFICOM.measurement.corba.EvaluationType_Transferable;
+import com.syrus.AMFICOM.measurement.corba.MeasurementSetup_Transferable;
 import com.syrus.AMFICOM.measurement.corba.MeasurementType_Transferable;
+import com.syrus.AMFICOM.measurement.corba.Measurement_Transferable;
 import com.syrus.AMFICOM.measurement.corba.ParameterType_Transferable;
+import com.syrus.AMFICOM.measurement.corba.Result_Transferable;
+import com.syrus.AMFICOM.measurement.corba.Set_Transferable;
 import com.syrus.AMFICOM.measurement.corba.TemporalPattern_Transferable;
+import com.syrus.AMFICOM.measurement.corba.Test_Transferable;
 
 /**
- * @version $Revision: 1.3 $, $Date: 2004/09/22 10:59:00 $
+ * @version $Revision: 1.4 $, $Date: 2004/09/22 12:54:25 $
  * @author $Author: bob $
  * @module cmserver_v1
  */
@@ -90,39 +97,78 @@ public final class ClientMeasurementObjectLoader implements MeasurementObjectLoa
 	}
 
 	public AnalysisType loadAnalysisType(Identifier id) throws DatabaseException, CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			return new AnalysisType(this.server.transmitAnalysisType((Identifier_Transferable) id
+					.getTransferable(), accessIdentifierTransferable));
+		} catch (CreateObjectException e) {
+			String msg = "ClientMeasurementObjectLoader.loadAnalysisType | new AnalysisType("
+					+ id.toString() + ")";
+			throw new RetrieveObjectException(msg, e);
+		} catch (AMFICOMRemoteException e) {
+			String msg = "ClientMeasurementObjectLoader.loadAnalysisType | server.transmitAnalysisType("
+					+ id.toString() + ")";
+			throw new CommunicationException(msg, e);
+		}
 	}
 
 	public EvaluationType loadEvaluationType(Identifier id) throws RetrieveObjectException, CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			return new EvaluationType(this.server.transmitEvaluationType((Identifier_Transferable) id
+					.getTransferable(), accessIdentifierTransferable));
+		} catch (CreateObjectException e) {
+			String msg = "ClientMeasurementObjectLoader.loadEvaluationType | new EvaluationType("
+					+ id.toString() + ")";
+			throw new RetrieveObjectException(msg, e);
+		} catch (AMFICOMRemoteException e) {
+			String msg = "ClientMeasurementObjectLoader.loadEvaluationType | server.transmitEvaluationType("
+					+ id.toString() + ")";
+			throw new CommunicationException(msg, e);
+		}
 	}
 
 	public Set loadSet(Identifier id) throws RetrieveObjectException, CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			return new Set(this.server.transmitSet((Identifier_Transferable) id.getTransferable(),
+								accessIdentifierTransferable));
+		} catch (CreateObjectException e) {
+			String msg = "ClientMeasurementObjectLoader.loadSet | new Set(" + id.toString() + ")";
+			throw new RetrieveObjectException(msg, e);
+		} catch (AMFICOMRemoteException e) {
+			String msg = "ClientMeasurementObjectLoader.loadSet | server.transmitSet(" + id.toString()
+					+ ")";
+			throw new CommunicationException(msg, e);
+		}
 	}
 
 	public MeasurementSetup loadMeasurementSetup(Identifier id) throws RetrieveObjectException,
 			CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			return new MeasurementSetup(this.server.transmitMeasurementSetup((Identifier_Transferable) id
+					.getTransferable(), accessIdentifierTransferable));
+		} catch (CreateObjectException e) {
+			String msg = "ClientMeasurementObjectLoader.loadMeasurementSetup | new MeasurementSetup("
+					+ id.toString() + ")";
+			throw new RetrieveObjectException(msg, e);
+		} catch (AMFICOMRemoteException e) {
+			String msg = "ClientMeasurementObjectLoader.loadMeasurementSetup | server.transmitMeasurementSetup("
+					+ id.toString() + ")";
+			throw new CommunicationException(msg, e);
+		}
 	}
 
-	public Measurement loadMeasurement(Identifier id) throws DatabaseException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+	public Measurement loadMeasurement(Identifier id) throws RetrieveObjectException, CommunicationException {
+		try {
+			return new Measurement(this.server.transmitMeasurement((Identifier_Transferable) id
+					.getTransferable(), accessIdentifierTransferable));
+		} catch (CreateObjectException e) {
+			String msg = "ClientMeasurementObjectLoader.loadMeasurement | new Measurement(" + id.toString()
+					+ ")";
+			throw new RetrieveObjectException(msg, e);
+		} catch (AMFICOMRemoteException e) {
+			String msg = "ClientMeasurementObjectLoader.loadMeasurement | server.transmitMeasurement("
+					+ id.toString() + ")";
+			throw new CommunicationException(msg, e);
+		}
 	}
 
 	public Analysis loadAnalysis(Identifier id) throws DatabaseException {
@@ -139,18 +185,33 @@ public final class ClientMeasurementObjectLoader implements MeasurementObjectLoa
 		throw new UnsupportedOperationException();
 	}
 
-	public Test loadTest(Identifier id) throws DatabaseException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+	public Test loadTest(Identifier id) throws RetrieveObjectException, CommunicationException {
+		try {
+			return new Test(this.server.transmitTest((Identifier_Transferable) id.getTransferable(),
+									accessIdentifierTransferable));
+		} catch (CreateObjectException e) {
+			String msg = "ClientTestObjectLoader.loadTest | new Test(" + id.toString() + ")";
+			throw new RetrieveObjectException(msg, e);
+		} catch (AMFICOMRemoteException e) {
+			String msg = "ClientTestObjectLoader.loadTest | server.transmitTest(" + id.toString() + ")";
+			throw new CommunicationException(msg, e);
+		}
 	}
 
-	public Result loadResult(Identifier id) throws DatabaseException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+	public Result loadResult(Identifier id) throws RetrieveObjectException,
+	CommunicationException {
+		try {
+			return new Result(this.server.transmitResult((Identifier_Transferable) id
+					.getTransferable(), accessIdentifierTransferable));
+		} catch (CreateObjectException e) {
+			String msg = "ClientMeasurementObjectLoader.loadResult | new Result("
+					+ id.toString() + ")";
+			throw new RetrieveObjectException(msg, e);
+		} catch (AMFICOMRemoteException e) {
+			String msg = "ClientMeasurementObjectLoader.loadResult | server.transmitResult("
+					+ id.toString() + ")";
+			throw new CommunicationException(msg, e);
+		}
 	}
 
 	public TemporalPattern loadTemporalPattern(Identifier id) throws RetrieveObjectException,
@@ -177,11 +238,26 @@ public final class ClientMeasurementObjectLoader implements MeasurementObjectLoa
 		throw new UnsupportedOperationException();
 	}
 
-	public List loadAnalysisTypes(List ids) throws DatabaseException, CommunicationException {		
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+	public List loadAnalysisTypes(List ids) throws DatabaseException, CommunicationException {
+		try {
+			Identifier_Transferable[] identifier_Transferables = new Identifier_Transferable[ids.size()];
+			int i = 0;
+			for (Iterator it = ids.iterator(); it.hasNext(); i++) {
+				Identifier id = (Identifier) it.next();
+				identifier_Transferables[i] = (Identifier_Transferable) id.getTransferable();
+			}
+			AnalysisType_Transferable[] transferables = this.server
+					.transmitAnalysisTypes(identifier_Transferables, accessIdentifierTransferable);
+			List list = new ArrayList(transferables.length);
+			for (int j = 0; j < transferables.length; j++) {
+				list.add(new AnalysisType(transferables[j]));
+			}
+			return list;
+		} catch (CreateObjectException e) {
+			throw new RetrieveObjectException(e);
+		} catch (AMFICOMRemoteException e) {
+			throw new CommunicationException(e);
+		}
 	}
 
 	public List loadEvaluations(List ids) throws DatabaseException, CommunicationException {
@@ -192,24 +268,70 @@ public final class ClientMeasurementObjectLoader implements MeasurementObjectLoa
 	}
 
 	public List loadEvaluationTypes(List ids) throws DatabaseException, CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			Identifier_Transferable[] identifier_Transferables = new Identifier_Transferable[ids.size()];
+			int i = 0;
+			for (Iterator it = ids.iterator(); it.hasNext(); i++) {
+				Identifier id = (Identifier) it.next();
+				identifier_Transferables[i] = (Identifier_Transferable) id.getTransferable();
+			}
+			EvaluationType_Transferable[] transferables = this.server
+					.transmitEvaluationTypes(identifier_Transferables, accessIdentifierTransferable);
+			List list = new ArrayList(transferables.length);
+			for (int j = 0; j < transferables.length; j++) {
+				list.add(new EvaluationType(transferables[j]));
+			}
+			return list;
+		} catch (CreateObjectException e) {
+			throw new RetrieveObjectException(e);
+		} catch (AMFICOMRemoteException e) {
+			throw new CommunicationException(e);
+		}
 	}
 
 	public List loadMeasurements(List ids) throws DatabaseException, CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			Identifier_Transferable[] identifier_Transferables = new Identifier_Transferable[ids.size()];
+			int i = 0;
+			for (Iterator it = ids.iterator(); it.hasNext(); i++) {
+				Identifier id = (Identifier) it.next();
+				identifier_Transferables[i] = (Identifier_Transferable) id.getTransferable();
+			}
+			Measurement_Transferable[] transferables = this.server
+					.transmitMeasurements(identifier_Transferables, accessIdentifierTransferable);
+			List list = new ArrayList(transferables.length);
+			for (int j = 0; j < transferables.length; j++) {
+				list.add(new Measurement(transferables[j]));
+			}
+			return list;
+		} catch (CreateObjectException e) {
+			throw new RetrieveObjectException(e);
+		} catch (AMFICOMRemoteException e) {
+			throw new CommunicationException(e);
+		}
 	}
 
 	public List loadMeasurementSetups(List ids) throws DatabaseException, CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			Identifier_Transferable[] identifier_Transferables = new Identifier_Transferable[ids.size()];
+			int i = 0;
+			for (Iterator it = ids.iterator(); it.hasNext(); i++) {
+				Identifier id = (Identifier) it.next();
+				identifier_Transferables[i] = (Identifier_Transferable) id.getTransferable();
+			}
+			MeasurementSetup_Transferable[] transferables = this.server
+					.transmitMeasurementSetups(identifier_Transferables,
+									accessIdentifierTransferable);
+			List list = new ArrayList(transferables.length);
+			for (int j = 0; j < transferables.length; j++) {
+				list.add(new MeasurementSetup(transferables[j]));
+			}
+			return list;
+		} catch (CreateObjectException e) {
+			throw new RetrieveObjectException(e);
+		} catch (AMFICOMRemoteException e) {
+			throw new CommunicationException(e);
+		}
 	}
 
 	public List loadMeasurementTypes(List ids) throws DatabaseException, CommunicationException {
@@ -230,7 +352,7 @@ public final class ClientMeasurementObjectLoader implements MeasurementObjectLoa
 			return list;
 		} catch (CreateObjectException e) {
 			throw new RetrieveObjectException(e);
-		} catch (AMFICOMRemoteException e) {			
+		} catch (AMFICOMRemoteException e) {
 			throw new CommunicationException(e);
 		}
 	}
@@ -244,8 +366,7 @@ public final class ClientMeasurementObjectLoader implements MeasurementObjectLoa
 				identifier_Transferables[i] = (Identifier_Transferable) id.getTransferable();
 			}
 			ParameterType_Transferable[] transferables = this.server
-					.transmitParameterTypes(identifier_Transferables,
-									accessIdentifierTransferable);
+					.transmitParameterTypes(identifier_Transferables, accessIdentifierTransferable);
 			List list = new ArrayList(transferables.length);
 			for (int j = 0; j < transferables.length; j++) {
 				list.add(new ParameterType(transferables[j]));
@@ -253,23 +374,53 @@ public final class ClientMeasurementObjectLoader implements MeasurementObjectLoa
 			return list;
 		} catch (CreateObjectException e) {
 			throw new RetrieveObjectException(e);
-		} catch (AMFICOMRemoteException e) {			
+		} catch (AMFICOMRemoteException e) {
 			throw new CommunicationException(e);
 		}
 	}
 
 	public List loadResults(List ids) throws DatabaseException, CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			Identifier_Transferable[] identifier_Transferables = new Identifier_Transferable[ids.size()];
+			int i = 0;
+			for (Iterator it = ids.iterator(); it.hasNext(); i++) {
+				Identifier id = (Identifier) it.next();
+				identifier_Transferables[i] = (Identifier_Transferable) id.getTransferable();
+			}
+			Result_Transferable[] transferables = this.server.transmitResults(identifier_Transferables,
+											accessIdentifierTransferable);
+			List list = new ArrayList(transferables.length);
+			for (int j = 0; j < transferables.length; j++) {
+				list.add(new Result(transferables[j]));
+			}
+			return list;
+		} catch (CreateObjectException e) {
+			throw new RetrieveObjectException(e);
+		} catch (AMFICOMRemoteException e) {
+			throw new CommunicationException(e);
+		}
 	}
 
 	public List loadSets(List ids) throws DatabaseException, CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			Identifier_Transferable[] identifier_Transferables = new Identifier_Transferable[ids.size()];
+			int i = 0;
+			for (Iterator it = ids.iterator(); it.hasNext(); i++) {
+				Identifier id = (Identifier) it.next();
+				identifier_Transferables[i] = (Identifier_Transferable) id.getTransferable();
+			}
+			Set_Transferable[] transferables = this.server.transmitSets(identifier_Transferables,
+											accessIdentifierTransferable);
+			List list = new ArrayList(transferables.length);
+			for (int j = 0; j < transferables.length; j++) {
+				list.add(new Set(transferables[j]));
+			}
+			return list;
+		} catch (CreateObjectException e) {
+			throw new RetrieveObjectException(e);
+		} catch (AMFICOMRemoteException e) {
+			throw new CommunicationException(e);
+		}
 	}
 
 	public List loadTemporalPatterns(List ids) throws DatabaseException, CommunicationException {
@@ -290,15 +441,30 @@ public final class ClientMeasurementObjectLoader implements MeasurementObjectLoa
 			return list;
 		} catch (CreateObjectException e) {
 			throw new RetrieveObjectException(e);
-		} catch (AMFICOMRemoteException e) {			
+		} catch (AMFICOMRemoteException e) {
 			throw new CommunicationException(e);
 		}
 	}
 
 	public List loadTests(List ids) throws DatabaseException, CommunicationException {
-		/**
-		 * FIXME method is not complete !
-		 */
-		throw new UnsupportedOperationException();
+		try {
+			Identifier_Transferable[] identifier_Transferables = new Identifier_Transferable[ids.size()];
+			int i = 0;
+			for (Iterator it = ids.iterator(); it.hasNext(); i++) {
+				Identifier id = (Identifier) it.next();
+				identifier_Transferables[i] = (Identifier_Transferable) id.getTransferable();
+			}
+			Test_Transferable[] transferables = this.server.transmitTests(identifier_Transferables,
+											accessIdentifierTransferable);
+			List list = new ArrayList(transferables.length);
+			for (int j = 0; j < transferables.length; j++) {
+				list.add(new Test(transferables[j]));
+			}
+			return list;
+		} catch (CreateObjectException e) {
+			throw new RetrieveObjectException(e);
+		} catch (AMFICOMRemoteException e) {
+			throw new CommunicationException(e);
+		}
 	}
 }
