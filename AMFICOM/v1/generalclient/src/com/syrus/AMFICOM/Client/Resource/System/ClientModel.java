eaa94aecf5640a8d1cf3476a84f6a0d71790115b
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-09-27 18:03:31 +0400

    [no comments]

--- AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/Resource/System/ClientModel.java
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/Resource/System/ClientModel.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ClientModel.java,v 1.2 2004/08/17 15:02:51 krupenn Exp $
+ * $Id: ClientModel.java,v 1.3 2004/09/27 14:03:31 bass Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,21 +8,19 @@
 
 package com.syrus.AMFICOM.Client.Resource.System;
 
-import java.text.*;
-import java.util.*;
-
-
-import com.syrus.AMFICOM.Client.Administrate.Object.UI.*;
-import com.syrus.AMFICOM.Client.General.UI.*;
+import com.syrus.AMFICOM.Client.Administrate.Object.UI.ClientPane;
+import com.syrus.AMFICOM.Client.General.UI.ObjectResourcePropertiesPane;
 import com.syrus.AMFICOM.Client.Resource.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * This class actually belongs to <tt>admin_v1</tt> module. It was
  * moved to <tt>generalclient_v1</tt> to resolve cross-module
  * dependencies between <tt>generalclient_v1</tt> and <tt>admin_1</tt>.
  *
- * @author $Author: krupenn $
- * @version $Revision: 1.2 $, $Date: 2004/08/17 15:02:51 $
+ * @author $Author: bass $
+ * @version $Revision: 1.3 $, $Date: 2004/09/27 14:03:31 $
  * @module generalclient_v1
  */
 public class ClientModel extends ObjectResourceModel
@@ -34,11 +32,11 @@ public class ClientModel extends ObjectResourceModel
     this.client = client;
   }
 
-
-  public PropertiesPanel getPropertyPane()
-  {
-    return new AgentPane(client);
-  }
+	public ObjectResourcePropertiesPane getPropertyPane() {
+		ClientPane clientPane = ClientPane.getInstance();
+		clientPane.setObjectResource(client);
+		return clientPane;
+	}
 
   public String getColumnValue(String col_id)
   {
