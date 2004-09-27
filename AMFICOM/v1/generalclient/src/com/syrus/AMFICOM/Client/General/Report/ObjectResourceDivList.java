c4fdeb78d85f88931eb04a4a879374473dd429a4
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-09-27 13:20:34 +0400

    [no comments]

--- AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/General/Report/ObjectResourceDivList.java
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/General/Report/ObjectResourceDivList.java
@@ -1,23 +1,22 @@
+/*
+ * $Id: ObjectResourceDivList.java,v 1.2 2004/09/27 09:20:34 bass Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ
+ */
+
 package com.syrus.AMFICOM.Client.General.Report;
 
 import com.syrus.AMFICOM.Client.General.UI.ObjectResourceTableModel;
 import com.syrus.AMFICOM.Client.Resource.ObjectResource;
-import javax.swing.table.TableColumn;
-
-import com.syrus.AMFICOM.Client.General.Model.Environment;
-
-import com.syrus.AMFICOM.Client.General.Report.*;
-import javax.swing.table.TableModel;
+import javax.swing.table.*;
 
 /**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2003</p>
- * <p>Company: </p>
- * @author unascribed
- * @version 1.0
+ * @author $Author: bass $
+ * @version $Revision: 1.2 $, $Date: 2004/09/27 09:20:34 $
+ * @module generalclient_v1
  */
-
 public class ObjectResourceDivList extends ReportData
 {
 	public ObjectResourceDivList(ObjectsReport report,
@@ -27,87 +26,85 @@ public class ObjectResourceDivList extends ReportData
 		columnModel = new ORDTableColumnModel(divisionsNumber,
 			tableModel.getColumnCount(),report);
 	}
-}
 
-class ORDTableColumnModel extends DividableTableColumnModel
-{
-	public ORDTableColumnModel(
-		int divisionsNumber,
-		int columnCount,
-		ObjectsReport report)
+	private class ORDTableColumnModel extends DividableTableColumnModel
 	{
-		super(divisionsNumber);
-
-		for (int j = 0; j < this.getDivisionsNumber(); j++)
-			for (int i = 0; i < columnCount; i++)
-				this.addColumn(new TableColumn(
-					j * columnCount + i,
-					100));
-//					((ReportTable) report.getReserve()).model.)); //Value
-	}
-}
-
-class ORDTableModel extends DividableTableModel
-{
-	int width = 0;
-
-	int length = 0;
-
-//	AMTReportTable reportTable = null;
-  ObjectResourceTableModel model = null;
-
-	boolean viewColumnNames = false;
-
-	public ORDTableModel(int divisionsNumber,
-		ObjectsReport report) throws CreateReportException
-	{
-		super(divisionsNumber,
-			((TableModel) report.getReserve()).getColumnCount());
-
-    model = (ObjectResourceTableModel) report.getReserve();
-
-		width = model.getColumnCount();
-		length = model.getRowCount();
-
-		for (int i = 0; i < width; i++)
-			if (!model.getColumnName(i).equals(""))
-			{
-				viewColumnNames = true;
-				length++;
-				break;
-			}
+		private ORDTableColumnModel(
+			int divisionsNumber,
+			int columnCount,
+			ObjectsReport report)
+		{
+			super(divisionsNumber);
+	
+			for (int j = 0; j < this.getDivisionsNumber(); j++)
+				for (int i = 0; i < columnCount; i++)
+					this.addColumn(new TableColumn(
+						j * columnCount + i,
+						100));
+		}
 	}
 
-	public int getColumnCount()
+	private final class ORDTableModel extends DividableTableModel
 	{
-		return width * this.getDivisionsNumber();
-	}
-
-	public int getRowCount()
-	{
-		// Если данные можно разложить поровну на такое количество столбцов
-		if (length % this.getDivisionsNumber() == 0)
-			return (int) (length / this.getDivisionsNumber());
-		//а если нельзя, то добавляем ещё ряд
-		return (int) (length / this.getDivisionsNumber()) + 1;
-	}
-
-	public Object getValueAt(int row, int col)
-	{
-		if ((row == 0) && viewColumnNames)
-			return model.getColumnName(col % width);
-
-		int index = (this.getRowCount() - 1) * (int) (col / width) + row - 1;
-
-		if (index >= this.length)
-			return "";
-
-		if (!viewColumnNames)
-			index++;
-
-    String column_id = model.getColumnByNumber(col % width);
-    ObjectResource or = (ObjectResource) model.getValueAt(index, col);
-    String text = or.getModel().getColumnValue(column_id);
-		return text;
+		private int width = 0;
+	
+		private int length = 0;
+	
+		private ObjectResourceTableModel model = null;
+	
+		private boolean viewColumnNames = false;
+	
+		private ORDTableModel(int divisionsNumber,
+			ObjectsReport report) throws CreateReportException
+		{
+			super(divisionsNumber,
+				((TableModel) report.getReserve()).getColumnCount());
+	
+			model = (ObjectResourceTableModel) report.getReserve();
+	
+			width = model.getColumnCount();
+			length = model.getRowCount();
+	
+			for (int i = 0; i < width; i++)
+				if (!model.getColumnName(i).equals(""))
+				{
+					viewColumnNames = true;
+					length++;
+					break;
+				}
+		}
+	
+		public int getColumnCount()
+		{
+			return width * this.getDivisionsNumber();
+		}
+	
+		public int getRowCount()
+		{
+			// Если данные можно разложить поровну на такое количество столбцов
+			if (length % this.getDivisionsNumber() == 0)
+				return (int) (length / this.getDivisionsNumber());
+			//а если нельзя, то добавляем ещё ряд
+			return (int) (length / this.getDivisionsNumber()) + 1;
+		}
+	
+		public Object getValueAt(int row, int col)
+		{
+			if ((row == 0) && viewColumnNames)
+				return model.getColumnName(col % width);
+	
+			int index = (this.getRowCount() - 1) * (int) (col / width) + row - 1;
+	
+			if (index >= this.length)
+				return "";
+	
+			if (!viewColumnNames)
+				index++;
+	
+			String column_id = model.getColumnByNumber(col % width);
+			ObjectResource or = (ObjectResource) model.getValueAt(index, col);
+			String text = or.getModel().getColumnValue(column_id);
+			return text;
+		}
 	}
 }
