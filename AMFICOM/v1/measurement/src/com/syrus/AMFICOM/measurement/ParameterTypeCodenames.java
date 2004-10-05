9a76d3a4248c52298ebd25616351aa331468410a
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-10-05 17:48:21 +0400

    add codenames for reflectogram measurement parameters

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/ParameterTypeCodenames.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/ParameterTypeCodenames.java
@@ -1,18 +1,26 @@
 /*
- * $Id: ParameterTypeCodenames.java,v 1.1 2004/10/05 12:16:05 bob Exp $
+ * $Id: ParameterTypeCodenames.java,v 1.2 2004/10/05 13:48:21 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
  * Проект: АМФИКОМ.
  */
+
 package com.syrus.AMFICOM.measurement;
 
-
 /**
- * @version $Revision: 1.1 $, $Date: 2004/10/05 12:16:05 $
+ * @version $Revision: 1.2 $, $Date: 2004/10/05 13:48:21 $
  * @author $Author: bob $
  * @module measurement_v1
  */
 public interface ParameterTypeCodenames {
-	String CODENAME_REFLECTOGRAMMA = "reflectogramma";
+
+	String	CODENAME_REFLECTOGRAMMA		= "reflectogramma";
+
+	String	CODENAME_WAVELENGTH		= "ref_wvlen";
+	String	CODENAME_TRACE_LENGTH		= "ref_trclen";
+	String	CODENAME_RESOLUTION		= "ref_res";
+	String	CODENAME_PULSE_WIDTH		= "ref_pulswd";
+	String	CODENAME_INDEX_OF_REFRACTION	= "ref_ior";
+	String	CODENAME_SCANS			= "ref_scans";
 }
