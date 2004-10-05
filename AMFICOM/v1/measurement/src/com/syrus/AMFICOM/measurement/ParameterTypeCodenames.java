63e37246145059f13cff8aa009d5e117762b742d
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-10-05 16:16:05 +0400

    add ParameterTypeCodenames

---
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/ParameterTypeCodenames.java
@@ -0,0 +1,18 @@
+/*
+ * $Id: ParameterTypeCodenames.java,v 1.1 2004/10/05 12:16:05 bob Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.measurement;
+
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2004/10/05 12:16:05 $
+ * @author $Author: bob $
+ * @module measurement_v1
+ */
+public interface ParameterTypeCodenames {
+	String CODENAME_REFLECTOGRAMMA = "reflectogramma";
+}
