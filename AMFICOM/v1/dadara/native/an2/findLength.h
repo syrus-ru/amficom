26c2ab17bf678d9f971c7447d6a6aed098ab84b0
cvsadmin <cvsadmin@science.syrus.ru>
2004-10-06 10:01:17 +0400

    *** empty log message ***

---
+++ AMFICOM/v1/dadara/native/an2/findLength.h
@@ -0,0 +1,12 @@
+#ifndef _findLength_h
+#define _findLength_h
+
+// определяет длину рефлектограммы до начала шума (т.е. до конца р/граммы)
+// если ничего не может сделать, должно вернуть data_length
+// data - входная р/г
+// data_length - ее длина
+// return: значение >=0, <=data; по возможности не ноль.
+int findReflectogramLength(double *data, int data_lenght);
+
+#endif
+
