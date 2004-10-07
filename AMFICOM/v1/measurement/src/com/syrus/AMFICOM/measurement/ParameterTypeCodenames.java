76fb7c981c600c58b573880927f99ec4af155897
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-10-07 17:54:10 +0400

    add analysis codenames

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/ParameterTypeCodenames.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/ParameterTypeCodenames.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ParameterTypeCodenames.java,v 1.2 2004/10/05 13:48:21 bob Exp $
+ * $Id: ParameterTypeCodenames.java,v 1.3 2004/10/07 13:54:10 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -9,18 +9,36 @@
 package com.syrus.AMFICOM.measurement;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2004/10/05 13:48:21 $
+ * @version $Revision: 1.3 $, $Date: 2004/10/07 13:54:10 $
  * @author $Author: bob $
  * @module measurement_v1
  */
 public interface ParameterTypeCodenames {
 
-	String	CODENAME_REFLECTOGRAMMA		= "reflectogramma";
+	String	REFLECTOGRAMMA		= "reflectogramma";
 
-	String	CODENAME_WAVELENGTH		= "ref_wvlen";
-	String	CODENAME_TRACE_LENGTH		= "ref_trclen";
-	String	CODENAME_RESOLUTION		= "ref_res";
-	String	CODENAME_PULSE_WIDTH		= "ref_pulswd";
-	String	CODENAME_INDEX_OF_REFRACTION	= "ref_ior";
-	String	CODENAME_SCANS			= "ref_scans";
+	String	TRACE_WAVELENGTH		= "ref_wvlen";
+	String	TRACE_LENGTH		= "ref_trclen";
+	String	TRACE_RESOLUTION		= "ref_res";
+	String	TRACE_PULSE_WIDTH		= "ref_pulswd";
+	String	TRACE_INDEX_OF_REFRACTION	= "ref_ior";
+	String	TRACE_AVERAGE_COUNT			= "ref_scans";
+	
+	String TRACE_EVENTS 						= "traceeventarray";
+	String DADARA_EVENTS 					= "dadara_event_array";
+	String DADARA_ETALON_EVENTS		= "dadara_etalon_event_array";
+	String DADARA_THRESHOLDS 			= "dadara_thresholds";
+	String DADARA_MIN_TRACE_LEVEL 	= "dadara_min_trace_level";
+	String DADARA 									= "dadara";
+
+	// analysis params
+	String WAVELET_TYPE	 				= "ref_uselinear";
+	String STRATEGY 							= "ref_strategy";
+	String CONNECTOR_FORM_FACTOR	= "ref_conn_fall_params";
+	String MIN_EVENT_LEVEL 			= "ref_min_level";
+	String MAX_NOISE_LEVEL		 		= "ref_max_level_noise";
+	String MIN_END_LEVEL			 		= "ref_min_level_to_find_end";
+	String MIN_SPLICE 						= "ref_min_weld";
+	String MIN_CONNECTOR 				= "ref_min_connector";
+
 }
