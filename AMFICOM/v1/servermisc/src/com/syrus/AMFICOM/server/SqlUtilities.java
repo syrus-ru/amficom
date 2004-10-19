1157548a43ba6d921386fe3c12a4ed41b5cbfd6e
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-10-19 12:12:16 +0400

    Added primitive type handling.

--- AMFICOM/v1/servermisc/src/com/syrus/AMFICOM/server/SqlUtilities.java
+++ AMFICOM/v1/servermisc/src/com/syrus/AMFICOM/server/SqlUtilities.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SqlUtilities.java,v 1.1 2004/10/18 15:16:58 bass Exp $
+ * $Id: SqlUtilities.java,v 1.2 2004/10/19 08:12:16 bass Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,11 +8,12 @@
 
 package com.syrus.AMFICOM.server;
 
+import java.math.BigDecimal;
 import java.sql.*;
 
 /**
  * @author $Author: bass $
- * @version $Revision: 1.1 $, $Date: 2004/10/18 15:16:58 $
+ * @version $Revision: 1.2 $, $Date: 2004/10/19 08:12:16 $
  * @module servermisc_v1
  */
 public final class SqlUtilities implements SqlConstants {
@@ -42,5 +43,37 @@ public final class SqlUtilities implements SqlConstants {
 	public static long getNonNullDateAsMillis(final ResultSet resultSet, final String columnName) throws SQLException {
 		Date returnValue = resultSet.getDate(columnName);
 		return ((returnValue == null) ? System.currentTimeMillis() : returnValue.getTime());
+	}
+
+	public static int getNonNullInt(final ResultSet resultSet, final String columnName) throws SQLException {
+		BigDecimal bigDecimal = resultSet.getBigDecimal(columnName);
+		boolean bigDecimalNull = (bigDecimal == null);
+		if (DEBUG && bigDecimalNull)
+			System.err.println("WARNING: inserting SQL NULL (column " + columnName + ") into a primitive int type...");
+		return (bigDecimalNull ? 0 : bigDecimal.intValue());
+	}
+
+	public static long getNonNullLong(final ResultSet resultSet, final String columnName) throws SQLException {
+		BigDecimal bigDecimal = resultSet.getBigDecimal(columnName);
+		boolean bigDecimalNull = (bigDecimal == null);
+		if (DEBUG && bigDecimalNull)
+			System.err.println("WARNING: inserting SQL NULL (column " + columnName + ") into a primitive long type...");
+		return (bigDecimalNull ? 0L : bigDecimal.longValue());
+	}
+
+	public static float getNonNullFloat(final ResultSet resultSet, final String columnName) throws SQLException {
+		BigDecimal bigDecimal = resultSet.getBigDecimal(columnName);
+		boolean bigDecimalNull = (bigDecimal == null);
+		if (DEBUG && bigDecimalNull)
+			System.err.println("WARNING: inserting SQL NULL (column " + columnName + ") into a primitive float type...");
+		return (bigDecimalNull ? 0f : bigDecimal.floatValue());
+	}
+
+	public static double getNonNullDouble(final ResultSet resultSet, final String columnName) throws SQLException {
+		BigDecimal bigDecimal = resultSet.getBigDecimal(columnName);
+		boolean bigDecimalNull = (bigDecimal == null);
+		if (DEBUG && bigDecimalNull)
+			System.err.println("WARNING: inserting SQL NULL (column " + columnName + ") into a primitive double type...");
+		return (bigDecimalNull ? 0d : bigDecimal.doubleValue());
 	}
 }
