4e137ffecc87189dfd35517e091e3270b0c3539d
Maxim Selivanov <max.selivanov@gmail.com>
2004-10-20 12:31:40 +0400

    added methods activateMServerReference() and resetMServerConnection() + bugfix

--- AMFICOM/v1/cmserver/src/com/syrus/AMFICOM/cmserver/ClientMeasurementServer.java
+++ AMFICOM/v1/cmserver/src/com/syrus/AMFICOM/cmserver/ClientMeasurementServer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ClientMeasurementServer.java,v 1.9 2004/10/19 15:10:31 max Exp $
+ * $Id: ClientMeasurementServer.java,v 1.10 2004/10/20 08:31:40 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,57 +8,73 @@
 
 package com.syrus.AMFICOM.cmserver;
 
+import java.net.InetAddress;
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextPackage.AlreadyBound;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
 import com.syrus.AMFICOM.cmserver.corba.CMServer;
 import com.syrus.AMFICOM.cmserver.corba.CMServerPOATie;
 import com.syrus.AMFICOM.general.CORBAServer;
 import com.syrus.AMFICOM.general.CommunicationException;
-import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.NewIdentifierPool;
 import com.syrus.AMFICOM.general.SleepButWorkThread;
 import com.syrus.AMFICOM.mserver.corba.MServer;
+import com.syrus.AMFICOM.mserver.corba.MServerHelper;
 import com.syrus.util.Application;
 import com.syrus.util.ApplicationProperties;
 import com.syrus.util.Log;
 import com.syrus.util.corba.JavaSoftORBUtil;
 import com.syrus.util.database.DatabaseConnection;
 
-import java.net.InetAddress;
-import org.omg.CORBA.*;
-import org.omg.CosNaming.*;
-import org.omg.CosNaming.NamingContextPackage.AlreadyBound;
-import org.omg.PortableServer.*;
-
 /**
- * @version $Revision: 1.9 $, $Date: 2004/10/19 15:10:31 $
+ * @version $Revision: 1.10 $, $Date: 2004/10/20 08:31:40 $
  * @author $Author: max $
  * @module cmserver_v1
  */
 public class ClientMeasurementServer extends SleepButWorkThread {
 
-	public static final String	APPLICATION_NAME		= "cmserver";
+	public static final String	APPLICATION_NAME			= "cmserver";
 
-	public static final String	KEY_DB_HOST_NAME		= "DBHostName";
-	public static final String	KEY_DB_SID			= "DBSID";
+	public static final String	KEY_DB_HOST_NAME			= "DBHostName";
+
+	public static final String	KEY_DB_SID					= "DBSID";
+
 	public static final String	KEY_DB_CONNECTION_TIMEOUT	= "DBConnectionTimeout";
-	public static final String	KEY_DB_LOGIN_NAME		= "DBLoginName";
-	public static final String	KEY_TICK_TIME			= "TickTime";
-	public static final String	KEY_MAX_FALLS			= "MaxFalls";
 
-	public static final String	DB_SID				= "amficom";
+	public static final String	KEY_DB_LOGIN_NAME			= "DBLoginName";
+
+	public static final String	KEY_TICK_TIME				= "TickTime";
+
+	public static final String	KEY_MAX_FALLS				= "MaxFalls";
+
+	public static final String	KEY_MSERVER_ID				= "MServerID";
+
+	public static final String	MSERVER_ID					= "Server_1";
+
+	public static final String	DB_SID						= "amficom";
+
 	public static final int		DB_CONNECTION_TIMEOUT		= 120;
-	public static final String	DB_LOGIN_NAME			= "amficom";
 
-	public static final int		TICK_TIME			= 5;
-    
-    protected static MServer mServerRef;    
+	public static final String	DB_LOGIN_NAME				= "amficom";
+
+	public static final int		TICK_TIME					= 5;
+
+	protected static MServer	mServerRef;
 
 	/* CORBA server */
-	private static CORBAServer	iAm;
-    
-	private boolean			running;
+	private static CORBAServer	corbaServer;
+
+	private boolean				running;
 
 	public ClientMeasurementServer() {
-		super(ApplicationProperties.getInt(KEY_TICK_TIME, TICK_TIME) * 1000, ApplicationProperties
-				.getInt(KEY_MAX_FALLS, MAX_FALLS));
+		super(ApplicationProperties.getInt(KEY_TICK_TIME, TICK_TIME) * 1000,
+				ApplicationProperties.getInt(KEY_MAX_FALLS, MAX_FALLS));
 		this.running = true;
 	}
 
@@ -68,12 +84,16 @@ public class ClientMeasurementServer extends SleepButWorkThread {
 	}
 
 	private static void establishDatabaseConnection() {
-		String dbHostName = ApplicationProperties.getString(KEY_DB_HOST_NAME, Application.getInternetAddress());
+		String dbHostName = ApplicationProperties.getString(KEY_DB_HOST_NAME,
+				Application.getInternetAddress());
 		String dbSid = ApplicationProperties.getString(KEY_DB_SID, DB_SID);
-		long dbConnTimeout = ApplicationProperties.getInt(KEY_DB_CONNECTION_TIMEOUT, DB_CONNECTION_TIMEOUT) * 1000;
-		String dbLoginName = ApplicationProperties.getString(KEY_DB_LOGIN_NAME, DB_LOGIN_NAME);
+		long dbConnTimeout = ApplicationProperties.getInt(
+				KEY_DB_CONNECTION_TIMEOUT, DB_CONNECTION_TIMEOUT) * 1000;
+		String dbLoginName = ApplicationProperties.getString(KEY_DB_LOGIN_NAME,
+				DB_LOGIN_NAME);
 		try {
-			DatabaseConnection.establishConnection(dbHostName, dbSid, dbConnTimeout, dbLoginName);
+			DatabaseConnection.establishConnection(dbHostName, dbSid,
+					dbConnTimeout, dbLoginName);
 		} catch (Exception e) {
 			Log.errorException(e);
 			System.exit(-1);
@@ -86,10 +106,10 @@ public class ClientMeasurementServer extends SleepButWorkThread {
 
 		/* Create CORBA server with servant(s) */
 		activateCORBAServer();
+        activateMServerReference();
 
 		DatabaseContextSetup.initDatabaseContext();
 		DatabaseContextSetup.initObjectPools();
-
 		/* Start main loop */
 		final ClientMeasurementServer clientMeasurementServer = new ClientMeasurementServer();
 		clientMeasurementServer.start();
@@ -103,18 +123,19 @@ public class ClientMeasurementServer extends SleepButWorkThread {
 		});
 	}
 
-	private static void activateCORBAServer() {
+	private static void _activateCORBAServer() {
 		/* Create local CORBA server end activate servant */
 		try {
 			ORB orb = JavaSoftORBUtil.getInstance().getORB();
 
-			POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+			POA rootPOA = POAHelper.narrow(orb
+					.resolve_initial_references("RootPOA"));
 			rootPOA.the_POAManager().activate();
 			NamingContextExt rootNamingCtx = NamingContextExtHelper.narrow(orb
 					.resolve_initial_references("NameService"));
 
-			final String hostName = InetAddress.getLocalHost().getCanonicalHostName()
-					.replaceAll("\\.", "_");
+			final String hostName = InetAddress.getLocalHost()
+					.getCanonicalHostName().replaceAll("\\.", "_");
 
 			NameComponent childPath[] = rootNamingCtx.to_name(hostName);
 
@@ -123,10 +144,12 @@ public class ClientMeasurementServer extends SleepButWorkThread {
 				childNamingCtx = NamingContextExtHelper.narrow(rootNamingCtx
 						.bind_new_context(childPath));
 			} catch (AlreadyBound ab) {
-				childNamingCtx = NamingContextExtHelper.narrow(rootNamingCtx.resolve_str(hostName));
+				childNamingCtx = NamingContextExtHelper.narrow(rootNamingCtx
+						.resolve_str(hostName));
 			}
 
-			CMServer server = (new CMServerPOATie(new CMServerImpl(), rootPOA))._this(orb);
+			CMServer server = (new CMServerPOATie(new CMServerImpl(), rootPOA))
+					._this(orb);
 			NameComponent serverPath[] = rootNamingCtx.to_name("CMServer");
 			childNamingCtx.rebind(serverPath, server);
 
@@ -138,10 +161,11 @@ public class ClientMeasurementServer extends SleepButWorkThread {
 		}
 	}
 
-	private static void _activateCORBAServer() {
+	private static void activateCORBAServer() {
 		try {
-			iAm = new CORBAServer();
-			iAm.activateServant(new CMServerPOATie(new CMServerImpl()), "CMServer");
+			corbaServer = new CORBAServer();
+			corbaServer.activateServant(new CMServerPOATie(new CMServerImpl()),
+					"CMServer");
 		} catch (CommunicationException ce) {
 			Log.errorException(ce);
 			System.exit(-1);
@@ -154,13 +178,14 @@ public class ClientMeasurementServer extends SleepButWorkThread {
 		try {
 			ORB orb = JavaSoftORBUtil.getInstance().getORB();
 
-			POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+			POA rootPOA = POAHelper.narrow(orb
+					.resolve_initial_references("RootPOA"));
 			rootPOA.the_POAManager().activate();
 			NamingContextExt rootNamingCtx = NamingContextExtHelper.narrow(orb
 					.resolve_initial_references("NameService"));
 
-			final String hostName = InetAddress.getLocalHost().getCanonicalHostName()
-					.replaceAll("\\.", "_");
+			final String hostName = InetAddress.getLocalHost()
+					.getCanonicalHostName().replaceAll("\\.", "_");
 
 			NameComponent childPath[] = rootNamingCtx.to_name(hostName);
 
@@ -169,7 +194,8 @@ public class ClientMeasurementServer extends SleepButWorkThread {
 				childNamingCtx = NamingContextExtHelper.narrow(rootNamingCtx
 						.bind_new_context(childPath));
 			} catch (AlreadyBound ab) {
-				childNamingCtx = NamingContextExtHelper.narrow(rootNamingCtx.resolve_str(hostName));
+				childNamingCtx = NamingContextExtHelper.narrow(rootNamingCtx
+						.resolve_str(hostName));
 			}
 
 			NameComponent serverPath[] = rootNamingCtx.to_name("CMServer");
@@ -186,10 +212,11 @@ public class ClientMeasurementServer extends SleepButWorkThread {
 
 	protected void processFall() {
 		switch (super.fallCode) {
-			case FALL_CODE_NO_ERROR:
-				break;
-			default:
-				Log.errorMessage("processError | Unknown error code: " + super.fallCode);
+		case FALL_CODE_NO_ERROR:
+			break;
+		default:
+			Log.errorMessage("processError | Unknown error code: "
+					+ super.fallCode);
 		}
 		super.clearFalls();
 	}
@@ -210,11 +237,19 @@ public class ClientMeasurementServer extends SleepButWorkThread {
 		stopCORBAServer();
 	}
 
-    /**
-     * @param serverId
-     */
-    public static void activateMServerReferenceWithId(Identifier serverId) {
-        // TODO Auto-generated method stub
-        
-    }
+	protected static void activateMServerReference() {
+		/* Obtain reference to measurement server */
+		try {
+			mServerRef = MServerHelper.narrow(corbaServer
+					.resolveReference(ApplicationProperties.getString(KEY_MSERVER_ID, MSERVER_ID)));
+		} catch (CommunicationException ce) {
+			Log.errorException(ce);
+			mServerRef = null;
+		}
+	}
+
+	protected static void resetMServerConnection() {
+		activateMServerReference();
+		NewIdentifierPool.setIdentifierGeneratorServer(mServerRef);
+	}	
 }
