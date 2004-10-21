2b83721fa08f4a61f887919cac31b6e780ec40ca
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-10-21 12:00:10 +0400

    add isNeedMore

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectCondition.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectCondition.java
@@ -1,5 +1,5 @@
 /*
- * $Id: StorableObjectCondition.java,v 1.2 2004/10/03 12:44:19 bob Exp $
+ * $Id: StorableObjectCondition.java,v 1.3 2004/10/21 08:00:10 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,15 +7,19 @@
  */
 package com.syrus.AMFICOM.general;
 
+import java.util.List;
+
 
 /**
- * @version $Revision: 1.2 $, $Date: 2004/10/03 12:44:19 $
+ * @version $Revision: 1.3 $, $Date: 2004/10/21 08:00:10 $
  * @author $Author: bob $
  * @module general_v1
  */
 public interface StorableObjectCondition {
 
-	boolean isConditionTrue(Object object) throws ApplicationException;	
+	boolean isConditionTrue(Object object) throws ApplicationException;
+	
+	boolean isNeedMore(List list) throws ApplicationException;
 
 	Short getEntityCode();
 	
