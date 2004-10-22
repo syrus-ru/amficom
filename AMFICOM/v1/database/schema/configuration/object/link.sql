98394e9b74211396cd2239cd996251302bb408e7
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-10-22 14:44:59 +0400

    start

---
+++ AMFICOM/v1/database/schema/configuration/object/link.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Equipment (
+ id VARCHAR2(32),
+ created DATE NOT NULL,
+ modified DATE NOT NULL,
+ creator_id VARCHAR2(32) NOT NULL,
+ modifier_id VARCHAR2(32) NOT NULL,
+--
+ domain_id VARCHAR2(32),
+--
+ type_id VARCHAR2(32) NOT NULL,
+--
+ name VARCHAR2(64) NOT NULL,
+ description VARCHAR2(256),
+--
+ inventory_no VARCHAR2(64),
+ supplier VARCHAR2(64),
+ supplier_code VARCHAR2(64),
+--
+ CONSTRAINT linkp_pk PRIMARY KEY (id),
+ CONSTRAINT linkp_creator_fk FOREIGN KEY (creator_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+ CONSTRAINT linkp_modifier_fk FOREIGN KEY (modifier_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+--
+ CONSTRAINT linkp_domain_fk FOREIGN KEY (domain_id)
+  REFERENCES Domain (id) ON DELETE CASCADE,
+--
+ CONSTRAINT linkp_lnktype_fk FOREIGN KEY (type_id)
+  REFERENCES LinkType (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE link_seq ORDER;
+
---
+++ AMFICOM/v1/database/schema/configuration/type/linktype.sql
@@ -0,0 +1,22 @@
+CREATE TABLE LinkType (
+ id VARCHAR2(32),
+ created DATE NOT NULL,
+ modified DATE NOT NULL,
+ creator_id VARCHAR2(32) NOT NULL,
+ modifier_id VARCHAR2(32) NOT NULL,
+--
+ codename VARCHAR2(32) NOT NULL,
+ description VARCHAR2(256),
+ sort NUMBER(2,0),
+ manufacturer VARCHAR2(64),
+ manufacturer_code VARCHAR2(64),
+ image_id VARCHAR2(32),
+--
+ CONSTRAINT lkptype_pk PRIMARY KEY (id),
+ CONSTRAINT lkptype_creator_fk FOREIGN KEY (creator_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+ CONSTRAINT lkptype_modifier_fk FOREIGN KEY (modifier_id)
+  REFERENCES Users (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE linktype_slk ORDER;
