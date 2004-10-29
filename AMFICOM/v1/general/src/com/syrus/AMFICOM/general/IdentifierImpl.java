835192040bfccacf7eb5549dae650b97a4b24df3
Maxim Selivanov <max.selivanov@gmail.com>
2004-10-29 13:50:40 +0400

    temporal stub

---
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/IdentifierImpl.java
@@ -0,0 +1,19 @@
+/*
+ * $Id: IdentifierImpl.java,v 1.1 2004/10/29 09:50:40 max Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.general;
+
+import java.io.Serializable;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2004/10/29 09:50:40 $
+ * @author $Author: max $
+ * @module module_name
+ */
+public class IdentifierImpl implements Serializable {
+
+}
