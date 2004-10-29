6ea23b4ca22d333f98783e3e8714d6ed94c9d225
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-10-29 16:08:25 +0400

    *_id moved to characterized_id

--- AMFICOM/v1/database/schema/configuration/object/characteristic.sql
+++ AMFICOM/v1/database/schema/configuration/object/characteristic.sql
@@ -14,13 +14,7 @@ CREATE TABLE Characteristic (
  is_visible NUMBER(1) NOT NULL,
 --
  sort NUMBER(2) NOT NULL,
- domain_id VARCHAR2(32),
- server_id VARCHAR2(32),
- mcm_id VARCHAR2(32),
- equipment_id VARCHAR2(32),
- transmission_path_id VARCHAR2(32),
- port_id VARCHAR2(32),
- kis_id VARCHAR2(32), 
+ characterized_id VARCHAR2(32),
 --
  CONSTRAINT chc_pk PRIMARY KEY (id),
  CONSTRAINT chc_creator_fk FOREIGN KEY (creator_id)
@@ -29,23 +23,8 @@ CREATE TABLE Characteristic (
   REFERENCES Users (id) ON DELETE CASCADE,
 --
  CONSTRAINT chc_chctype_fk FOREIGN KEY (type_id)
-  REFERENCES CharacteristicType (id) ON DELETE CASCADE,
---
- CONSTRAINT chc_domain_fk FOREIGN KEY (domain_id)
-  REFERENCES Domain (id) ON DELETE CASCADE,
- CONSTRAINT chc_server_fk FOREIGN KEY (server_id)
-  REFERENCES Server (id) ON DELETE CASCADE,
- CONSTRAINT chc_mcm_fk FOREIGN KEY (mcm_id)
-  REFERENCES MCM (id) ON DELETE CASCADE,
- CONSTRAINT chc_eqp_fk FOREIGN KEY (equipment_id)
-  REFERENCES Equipment (id) ON DELETE CASCADE,
- CONSTRAINT chc_tpath_fk FOREIGN KEY (transmission_path_id)
-  REFERENCES TransmissionPath (id) ON DELETE CASCADE,
- CONSTRAINT chc_port_fk FOREIGN KEY (port_id)
-  REFERENCES Port (id) ON DELETE CASCADE,
- CONSTRAINT chc_kis_fk FOREIGN KEY (kis_id)
-  REFERENCES KIS (id) ON DELETE CASCADE,
- 
+  REFERENCES CharacteristicType (id) ON DELETE CASCADE
+-- 
 );
 
 CREATE SEQUENCE characteristic_seq ORDER;
