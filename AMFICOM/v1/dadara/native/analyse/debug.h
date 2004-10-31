d0153d69d68e538081b46f6bfd96d5e77d1881c5
cvsadmin <cvsadmin@science.syrus.ru>
2004-10-31 15:28:12 +0300

    Imported dadara sources

---
+++ AMFICOM/v1/dadara/native/analyse/debug.h
@@ -0,0 +1,10 @@
+#ifndef _DEBUG_H
+#define _DEBUG_H
+
+//#define DEBUG_DADARA_ANALYSE
+//#define DEBUG_INITIAL_ANALYSIS
+
+#define DEBUG_DADARA_WIN_LOGF "c:\\log\\" "dadara-analyse.log"
+#define DEBUG_INITIAL_WIN_LOGF "c:\\log\\" "dadara-ia.log"
+
+#endif
