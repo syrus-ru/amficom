3f4d6ba0ad674606feadddddeba5f88ecc8bd5c8
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-11-11 12:23:15 +0300

    fix

--- util/src/com/syrus/util/database/DatabaseString.java
+++ util/src/com/syrus/util/database/DatabaseString.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DatabaseString.java,v 1.2 2004/10/27 12:20:17 bob Exp $
+ * $Id: DatabaseString.java,v 1.3 2004/11/11 09:23:15 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -9,7 +9,7 @@ package com.syrus.util.database;
 
 
 /**
- * @version $Revision: 1.2 $, $Date: 2004/10/27 12:20:17 $
+ * @version $Revision: 1.3 $, $Date: 2004/11/11 09:23:15 $
  * @author $Author: bob $
  * @module util
  */
@@ -34,6 +34,8 @@ public class DatabaseString {
  	 * @since j2sdk 1.4
 	 */
 	public static String fromQuerySubString(String string){
-		return (string != null) ? string.replaceAll("('){2}", "$1") : null;
+		// we mustn't unescape from substring because of this work done in sql driver
+		// return (string != null) ? string.replaceAll("(')\\1", "$1") : null;
+		return string;
 	}
 }
