ae367c0558eeed489c140fd50cd248198e4e1144
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-11-19 13:11:43 +0300

    start

---
+++ AMFICOM/v1/database/schema/configuration/type/cablethreadtype.sql
@@ -0,0 +1,26 @@
+CREATE TABLE CableThreadType (
+ id VARCHAR2(32),
+ created DATE NOT NULL,
+ modified DATE NOT NULL,
+ creator_id VARCHAR2(32) NOT NULL,
+ modifier_id VARCHAR2(32) NOT NULL,
+--
+ codename VARCHAR2(32) NOT NULL,
+ description VARCHAR2(256),
+--
+ manufacturer VARCHAR2(64),
+ manufacturer_code VARCHAR2(64),
+ mark VARCHAR2(64),
+ color VARCHAR2(32),
+ cable_link_type_id VARCHAR2(32),
+--
+ CONSTRAINT ctkptype_pk PRIMARY KEY (id),
+ CONSTRAINT ctptype_creator_fk FOREIGN KEY (creator_id)
+  REFERENCES Users (id) ON DELETE CASCADE,
+ CONSTRAINT ctptype_modifier_fk FOREIGN KEY (modifier_id)
+  REFERENCES Users (id) ON DELETE CASCADE
+ CONSTRAINT ctptype_clinktype_fk FOREIGN KEY (cable_link_type_id)
+  REFERENCES LinkType (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE cablethreadtype_seq ORDER;
