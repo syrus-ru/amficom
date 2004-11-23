ac7b3bcf858932ba59829910f8c44c7319ffaf8d
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-11-23 18:15:39 +0300

    start

---
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/corba/ElementAttributeImpl.java
@@ -0,0 +1,71 @@
+/*
+ * $Id: ElementAttributeImpl.java,v 1.1 2004/11/23 15:15:39 bob Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.general.corba;
+
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2004/11/23 15:15:39 $
+ * @author $Author: bob $
+ * @module module
+ */
+public class ElementAttributeImpl extends ElementAttribute {
+
+	/**
+	 * Comment for <code>serialVersionUID</code>
+	 */
+	private static final long	serialVersionUID	= 3258416114416628534L;
+
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.general.corba.StorableObject#created()
+	 */
+	public long created() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.general.corba.StorableObject#modified()
+	 */
+	public long modified() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.general.corba.StorableObject#version()
+	 */
+	public long version() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.general.corba.Namable#name()
+	 */
+	public String name() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.general.corba.Namable#name(java.lang.String)
+	 */
+	public void name(String newName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.general.corba.Identifiable#id()
+	 */
+	public Identifier id() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
