3338bf05034aa96aaceb8034190a6856c3bfa33c
Maxim Selivanov <max.selivanov@gmail.com>
2004-11-24 13:22:23 +0300

    removed KISTypeDatabase

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/DatabaseContextSetup.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/DatabaseContextSetup.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DatabaseContextSetup.java,v 1.12 2004/11/19 23:09:30 arseniy Exp $
+ * $Id: DatabaseContextSetup.java,v 1.13 2004/11/24 10:22:23 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,48 +8,46 @@
 
 package com.syrus.AMFICOM.mcm;
 
-import com.syrus.AMFICOM.configuration.ConfigurationStorableObjectPool;
-import com.syrus.AMFICOM.configuration.ConfigurationDatabaseContext;
+import com.syrus.AMFICOM.configuration.CableThreadTypeDatabase;
+import com.syrus.AMFICOM.configuration.CharacteristicDatabase;
 import com.syrus.AMFICOM.configuration.CharacteristicTypeDatabase;
+import com.syrus.AMFICOM.configuration.ConfigurationDatabaseContext;
+import com.syrus.AMFICOM.configuration.ConfigurationStorableObjectPool;
+import com.syrus.AMFICOM.configuration.DomainDatabase;
+import com.syrus.AMFICOM.configuration.EquipmentDatabase;
 import com.syrus.AMFICOM.configuration.EquipmentTypeDatabase;
-import com.syrus.AMFICOM.configuration.KISTypeDatabase;
+import com.syrus.AMFICOM.configuration.KISDatabase;
 import com.syrus.AMFICOM.configuration.LinkDatabase;
 import com.syrus.AMFICOM.configuration.LinkTypeDatabase;
-import com.syrus.AMFICOM.configuration.CableThreadTypeDatabase;
-import com.syrus.AMFICOM.configuration.PortTypeDatabase;
+import com.syrus.AMFICOM.configuration.MCMDatabase;
+import com.syrus.AMFICOM.configuration.MeasurementPortDatabase;
 import com.syrus.AMFICOM.configuration.MeasurementPortTypeDatabase;
-import com.syrus.AMFICOM.configuration.CharacteristicDatabase;
+import com.syrus.AMFICOM.configuration.MonitoredElementDatabase;
+import com.syrus.AMFICOM.configuration.PortDatabase;
+import com.syrus.AMFICOM.configuration.PortTypeDatabase;
+import com.syrus.AMFICOM.configuration.ServerDatabase;
+import com.syrus.AMFICOM.configuration.TransmissionPathDatabase;
 import com.syrus.AMFICOM.configuration.TransmissionPathTypeDatabase;
 import com.syrus.AMFICOM.configuration.UserDatabase;
-import com.syrus.AMFICOM.configuration.DomainDatabase;
-import com.syrus.AMFICOM.configuration.ServerDatabase;
-import com.syrus.AMFICOM.configuration.MCMDatabase;
-import com.syrus.AMFICOM.configuration.EquipmentDatabase;
-import com.syrus.AMFICOM.configuration.PortDatabase;
-import com.syrus.AMFICOM.configuration.MeasurementPortDatabase;
-import com.syrus.AMFICOM.configuration.TransmissionPathDatabase;
-import com.syrus.AMFICOM.configuration.KISDatabase;
-import com.syrus.AMFICOM.configuration.MonitoredElementDatabase;
-
-import com.syrus.AMFICOM.measurement.MeasurementStorableObjectPool;
-import com.syrus.AMFICOM.measurement.MeasurementDatabaseContext;
-import com.syrus.AMFICOM.measurement.ParameterTypeDatabase;
-import com.syrus.AMFICOM.measurement.MeasurementTypeDatabase;
-import com.syrus.AMFICOM.measurement.AnalysisTypeDatabase;
-import com.syrus.AMFICOM.measurement.EvaluationTypeDatabase;
-import com.syrus.AMFICOM.measurement.SetDatabase;
-import com.syrus.AMFICOM.measurement.MeasurementSetupDatabase;
-import com.syrus.AMFICOM.measurement.MeasurementDatabase;
 import com.syrus.AMFICOM.measurement.AnalysisDatabase;
+import com.syrus.AMFICOM.measurement.AnalysisTypeDatabase;
 import com.syrus.AMFICOM.measurement.EvaluationDatabase;
-import com.syrus.AMFICOM.measurement.TestDatabase;
+import com.syrus.AMFICOM.measurement.EvaluationTypeDatabase;
+import com.syrus.AMFICOM.measurement.MeasurementDatabase;
+import com.syrus.AMFICOM.measurement.MeasurementDatabaseContext;
+import com.syrus.AMFICOM.measurement.MeasurementSetupDatabase;
+import com.syrus.AMFICOM.measurement.MeasurementStorableObjectPool;
+import com.syrus.AMFICOM.measurement.MeasurementTypeDatabase;
+import com.syrus.AMFICOM.measurement.ParameterTypeDatabase;
 import com.syrus.AMFICOM.measurement.ResultDatabase;
+import com.syrus.AMFICOM.measurement.SetDatabase;
 import com.syrus.AMFICOM.measurement.TemporalPatternDatabase;
+import com.syrus.AMFICOM.measurement.TestDatabase;
 
 
 /**
- * @version $Revision: 1.12 $, $Date: 2004/11/19 23:09:30 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.13 $, $Date: 2004/11/24 10:22:23 $
+ * @author $Author: max $
  * @module mcm_v1
  */
 
@@ -64,8 +62,7 @@ public abstract class DatabaseContextSetup {
 										  new PortTypeDatabase(),
 										  new MeasurementPortTypeDatabase(),
 										  new LinkTypeDatabase(),
-											new CableThreadTypeDatabase(),
-										  new KISTypeDatabase(),
+											new CableThreadTypeDatabase(),										  
 										  new CharacteristicDatabase(),
 										  new UserDatabase(),
 										  new DomainDatabase(),
