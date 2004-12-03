6a68e963313d17399d3f45603ea8edc4ca823f75
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2004-12-03 22:24:29 +0300

    Trivial.

--- AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/AbstractLinkType.java
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/AbstractLinkType.java
@@ -1,10 +1,11 @@
 /*
- * $Id: AbstractLinkType.java,v 1.1 2004/11/19 09:00:09 bob Exp $
+ * $Id: AbstractLinkType.java,v 1.2 2004/12/03 19:23:45 bass Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
  * Проект: АМФИКОМ.
  */
+
 package com.syrus.AMFICOM.configuration;
 
 import java.util.Date;
@@ -13,39 +14,38 @@ import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.StorableObjectType;
 import com.syrus.AMFICOM.general.corba.StorableObject_Transferable;
 
-
 /**
- * @version $Revision: 1.1 $, $Date: 2004/11/19 09:00:09 $
- * @author $Author: bob $
+ * @version $Revision: 1.2 $, $Date: 2004/12/03 19:23:45 $
+ * @author $Author: bass $
  * @module configuration_v1
  */
 public abstract class AbstractLinkType extends StorableObjectType {
-	static final long serialVersionUID = 6276017738364160981L;
+	private static final long serialVersionUID = 6276017738364160981L;
 
 	public AbstractLinkType(Identifier id) {
 		super(id);
 	}
-
-	public AbstractLinkType(Identifier id,
-							  Date created,
-							  Date modified,
-							  Identifier creator_id,
-							  Identifier modifier_id,
-							  String codename,
-							  String description) {
-		super(id,
-			  created,
-			  modified,
-			  creator_id,
-			  modifier_id,
-			  codename,
-			  description);		
-	}
 	
-	public AbstractLinkType(StorableObject_Transferable transferable,
-							  String codename,
-							  String description) {
-			super(transferable, codename, description);
+	public AbstractLinkType(final StorableObject_Transferable transferable,
+			final String codename,
+			final String description) {
+		super(transferable, codename, description);
+	}
+
+	protected AbstractLinkType(Identifier id,
+			Date created,
+			Date modified,
+			Identifier creatorId,
+			Identifier modifierId,
+			String codename,
+			String description) {
+		super(id,
+			created,
+			modified,
+			creatorId,
+			modifierId,
+			codename,
+			description);		
 	}
 
 	public String getCodename() {
@@ -56,22 +56,29 @@ public abstract class AbstractLinkType extends StorableObjectType {
 		return super.description;
 	}
 	
-	public void setDescription(String description){
-		super.description = description;
-		super.currentVersion = super.getNextVersion();
+	public void setDescription(final String description){
+		this.currentVersion = getNextVersion();
+		setDescription0(description);
 	}
 
+	/**
+	 * @todo This method is absolutely unnecessary.
+	 */
 	protected synchronized void setAttributes(Date created,
-											  Date modified,
-											  Identifier creator_id,
-											  Identifier modifier_id,
-											  String codename,
-											  String description) {
+			Date modified,
+			Identifier creator_id,
+			Identifier modifier_id,
+			String codename,
+			String description) {
 		super.setAttributes(created,
-							modified,
-							creator_id,
-							modifier_id,
-							codename,
-							description);
+			modified,
+			creator_id,
+			modifier_id,
+			codename,
+			description);
+	}
+
+	protected void setDescription0(final String description) {
+		this.description = description;
 	}
 }
