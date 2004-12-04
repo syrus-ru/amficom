b1510ea8dfed85b25cfbf56df7308dbe28b15a31
Arseniy Tashoyan <tashoyan@gmail.com>
2004-12-04 03:57:17 +0300

    Commented "private" modifier for method copy_string for compatibility with my favorite microsoft compiler

--- hw/trc/BellcoreStructure.h
+++ hw/trc/BellcoreStructure.h
@@ -343,7 +343,7 @@ class BellcoreStructure {
 		void add_field_special(unsigned int size, char* data);
 		unsigned int get_size() const;
 
-	private:
+//	private:
 		static void copy_string(char* copy_str, const char* str);
 };
 
