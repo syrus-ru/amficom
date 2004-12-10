01064fb6fd945d5c7e233c28aa6637ed75288886
Maxim Selivanov <max.selivanov@gmail.com>
2004-12-10 11:16:46 +0300

    mde

---
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/CableThread.java
@@ -0,0 +1,163 @@
+/*
+ * $Id: CableThread.java,v 1.1 2004/12/10 08:16:29 max Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.configuration;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import com.syrus.AMFICOM.configuration.corba.CableThread_Transferable;
+import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.CreateObjectException;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.IdentifierPool;
+import com.syrus.AMFICOM.general.IllegalDataException;
+import com.syrus.AMFICOM.general.IllegalObjectEntityException;
+import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.ObjectNotFoundException;
+import com.syrus.AMFICOM.general.RetrieveObjectException;
+import com.syrus.AMFICOM.general.StorableObjectDatabase;
+import com.syrus.AMFICOM.general.StorableObjectType;
+import com.syrus.AMFICOM.general.TypedObject;
+import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2004/12/10 08:16:29 $
+ * @author $Author: max $
+ * @module config_v1
+ */
+public class CableThread extends DomainMember implements TypedObject {
+	
+    private String name;
+    private String description;
+    private CableThreadType type;
+    
+    private StorableObjectDatabase cableThreadDatabase;
+    
+    public CableThread(Identifier id) throws ObjectNotFoundException, RetrieveObjectException {
+        super(id);
+
+        this.cableThreadDatabase = ConfigurationDatabaseContext.cableThreadDatabase;
+        try {
+            this.cableThreadDatabase.retrieve(this);
+        }
+        catch (IllegalDataException ide) {
+            throw new RetrieveObjectException(ide.getMessage(), ide);
+        }
+    }
+    
+    public CableThread(CableThread_Transferable ctt) throws CreateObjectException {
+        super(ctt.header,
+              new Identifier(ctt.domain_id));
+        
+        this.name = ctt.name;
+        this.description = ctt.description;
+        try {
+            this.type = (CableThreadType) ConfigurationStorableObjectPool.getStorableObject(new Identifier(ctt.type_id), true);
+        }
+        catch (ApplicationException ae) {
+            throw new CreateObjectException(ae);
+        }
+    }
+    
+    protected CableThread(Identifier id,
+              Identifier creatorId,
+              Identifier domainId,
+              String name,
+              String description,
+              CableThreadType type) {
+    super(id,
+                new Date(System.currentTimeMillis()),
+                new Date(System.currentTimeMillis()),
+                creatorId,
+                creatorId,
+                domainId);
+    this.name = name;
+    this.description = description;
+    this.type = type;
+    this.cableThreadDatabase = ConfigurationDatabaseContext.cableThreadDatabase;
+    }
+    
+    public static CableThread createInstance(Identifier creatorId,
+              Identifier domainId,
+              String name,
+              String description,
+              CableThreadType type) throws CreateObjectException {
+    	if (creatorId == null || domainId == null || name == null || description == null 
+                || type == null)
+    		throw new IllegalArgumentException("Argument is 'null'");
+
+		try {
+			return new CableThread(IdentifierPool
+					.getGeneratedIdentifier(ObjectEntities.CABLE_THREAD_ENTITY_CODE),
+					creatorId, domainId, name, description, type);
+		} catch (IllegalObjectEntityException e) {
+			throw new CreateObjectException(
+					"CableThread.createInstance | cannot generate identifier ", e);
+		}
+	}
+    
+    public static CableThread getInstance(CableThread_Transferable ctt) throws CreateObjectException{
+        
+        CableThread cableThread = new CableThread(ctt);
+        
+        cableThread.cableThreadDatabase = ConfigurationDatabaseContext.linkDatabase;
+        try {
+            if (cableThread.cableThreadDatabase != null)
+                cableThread.cableThreadDatabase.insert(cableThread);
+        }
+        catch (IllegalDataException ide) {
+            throw new CreateObjectException(ide.getMessage(), ide);
+        }
+        
+        return cableThread;
+    }
+    
+    public Object getTransferable() {       
+        return new CableThread_Transferable(super.getHeaderTransferable(),
+                                     (Identifier_Transferable)super.domainId.getTransferable(),
+                                     new String(this.name),
+                                     new String(this.description),
+                                     (Identifier_Transferable)this.type.getId().getTransferable());
+    }
+    
+    protected synchronized void setAttributes(Date created,
+            Date modified,
+            Identifier creatorId,
+            Identifier modifierId,
+            Identifier domainId,
+            String name,
+            String description,
+            CableThreadType type) {
+		super.setAttributes(created, modified, creatorId, modifierId, domainId);
+		this.name = name;
+		this.description = description;
+		this.type = type;		
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+        super.currentVersion = super.getNextVersion();
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public StorableObjectType getType() {       
+        return this.type;
+    }
+    
+    public List getDependencies() {
+        return Collections.singletonList(this.type);
+    }
+}
---
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/CableThreadDatabase.java
@@ -0,0 +1,224 @@
+/*
+ * $Id: CableThreadDatabase.java,v 1.1 2004/12/10 08:16:46 max Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.configuration;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.CreateObjectException;
+import com.syrus.AMFICOM.general.DatabaseIdentifier;
+import com.syrus.AMFICOM.general.IllegalDataException;
+import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.ObjectNotFoundException;
+import com.syrus.AMFICOM.general.RetrieveObjectException;
+import com.syrus.AMFICOM.general.StorableObject;
+import com.syrus.AMFICOM.general.StorableObjectCondition;
+import com.syrus.AMFICOM.general.StorableObjectDatabase;
+import com.syrus.AMFICOM.general.UpdateObjectException;
+import com.syrus.AMFICOM.general.VersionCollisionException;
+import com.syrus.util.Log;
+import com.syrus.util.database.DatabaseDate;
+import com.syrus.util.database.DatabaseString;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2004/12/10 08:16:46 $
+ * @author $Author: max $
+ * @module config_v1
+ */
+public class CableThreadDatabase extends StorableObjectDatabase  {
+    
+	//  table :: CableThread
+    public static final String COLUMN_TYPE_ID       = "type_id";
+	//   name VARCHAR2(64) NOT NULL,
+    public static final String COLUMN_NAME          = "name";
+    //  description VARCHAR2(256),
+    public static final String COLUMN_DESCRIPTION   = "description";
+    
+    private static String columns;
+    private static String updateMultiplySQLValues;
+    
+    private CableThread fromStorableObject(StorableObject storableObject) throws IllegalDataException {
+        if (storableObject instanceof CableThread)
+            return (CableThread)storableObject;
+        throw new IllegalDataException("LinkDatabase.fromStorableObject | Illegal Storable Object: " + storableObject.getClass().getName());
+    }
+    
+    protected String getEnityName() {
+        return ObjectEntities.CABLE_THREAD_ENTITY;
+    }
+    
+    protected String getColumns(int mode) {
+        if (columns == null){
+            columns = super.getColumns(mode) + COMMA
+                + DomainMember.COLUMN_DOMAIN_ID + COMMA
+                + COLUMN_TYPE_ID + COMMA
+                + COLUMN_NAME + COMMA
+                + COLUMN_DESCRIPTION;
+        }
+        return columns;
+    }
+    
+    protected String getUpdateMultiplySQLValues(int mode) {
+        if (updateMultiplySQLValues == null){
+            updateMultiplySQLValues = super.getUpdateMultiplySQLValues(mode) + COMMA 
+                + QUESTION + COMMA
+                + QUESTION + COMMA
+                + QUESTION + COMMA
+                + QUESTION;
+        }
+        return updateMultiplySQLValues;
+    }
+    
+    protected String getUpdateSingleSQLValues(StorableObject storableObject)
+    throws IllegalDataException, UpdateObjectException {
+        CableThread CableThread = fromStorableObject(storableObject);
+		
+		String sql = super.getUpdateSingleSQLValues(storableObject) + COMMA
+				+ DatabaseIdentifier.toSQLString(CableThread.getDomainId()) + COMMA
+				+ DatabaseIdentifier.toSQLString(CableThread.getType().getId())
+				+ APOSTOPHE	+ DatabaseString.toQuerySubString(CableThread.getName()) + APOSTOPHE+ COMMA 
+                + APOSTOPHE	+ DatabaseString.toQuerySubString(CableThread.getDescription())	+ APOSTOPHE;
+		return sql;
+    }
+    
+    protected int setEntityForPreparedStatement(StorableObject storableObject,
+            PreparedStatement preparedStatement, int mode) throws IllegalDataException,
+            UpdateObjectException {
+        CableThread cableThread = fromStorableObject(storableObject);
+        int i;
+        try {
+            i = super.setEntityForPreparedStatement(storableObject, preparedStatement, mode);
+            DatabaseIdentifier.setIdentifier(preparedStatement, ++i, cableThread.getDomainId());
+            DatabaseIdentifier.setIdentifier(preparedStatement, ++i, cableThread.getType().getId());
+            preparedStatement.setString( ++i, cableThread.getName());
+            preparedStatement.setString( ++i, cableThread.getDescription());            
+        } catch (SQLException sqle) {
+            throw new UpdateObjectException("LinkDatabase." +
+                    "setEntityForPreparedStatement | Error " + sqle.getMessage(), sqle);
+        }
+        return i;
+    }
+    
+    protected StorableObject updateEntityFromResultSet(
+            StorableObject storableObject, ResultSet resultSet)
+            throws IllegalDataException, RetrieveObjectException, SQLException {
+        CableThread cableThread = fromStorableObject(storableObject);
+        if (cableThread == null){          
+            cableThread = new CableThread(DatabaseIdentifier.getIdentifier(resultSet, COLUMN_ID), null, null, null,
+                                       null, null);            
+        }
+        String name = DatabaseString.fromQuerySubString(resultSet.getString(COLUMN_NAME));
+        String description = DatabaseString.fromQuerySubString(resultSet.getString(COLUMN_DESCRIPTION));        
+        CableThreadType cableThreadType;
+        try {
+            cableThreadType = (CableThreadType)ConfigurationStorableObjectPool.getStorableObject(DatabaseIdentifier.getIdentifier(resultSet, COLUMN_TYPE_ID), true);
+        }
+        catch (ApplicationException ae) {
+            throw new RetrieveObjectException(ae);
+        }
+        cableThread.setAttributes(DatabaseDate.fromQuerySubString(resultSet, COLUMN_CREATED),
+                                                        DatabaseDate.fromQuerySubString(resultSet, COLUMN_MODIFIED),
+                                                        DatabaseIdentifier.getIdentifier(resultSet, COLUMN_CREATOR_ID),
+                                                        DatabaseIdentifier.getIdentifier(resultSet, COLUMN_MODIFIER_ID),
+                                                        DatabaseIdentifier.getIdentifier(resultSet, DomainMember.COLUMN_DOMAIN_ID),
+                                                        (name != null) ? name : "",
+                                                        (description != null) ? description : "",
+                                                        cableThreadType);
+
+        
+        return cableThread;
+    }
+    
+    public void retrieve(StorableObject storableObject) throws IllegalDataException, ObjectNotFoundException, RetrieveObjectException {
+        CableThread cableThread = fromStorableObject(storableObject);
+        super.retrieveEntity(cableThread);        
+    }
+    
+    public Object retrieveObject(StorableObject storableObject, int retrieveKind, Object arg) throws IllegalDataException, ObjectNotFoundException, RetrieveObjectException {
+        CableThread cableThread = this.fromStorableObject(storableObject);
+        switch (retrieveKind) {
+            default:
+                return null;
+        }
+    }
+    
+    public void insert(StorableObject storableObject) throws IllegalDataException, CreateObjectException {
+        CableThread cableThread = fromStorableObject(storableObject);
+        super.insertEntity(cableThread);               
+    }
+    
+    public void insert(List storableObjects) throws IllegalDataException,
+            CreateObjectException {
+		super.insertEntities(storableObjects);		
+	}
+    
+    public void update(StorableObject storableObject, int updateKind, Object obj)
+			throws IllegalDataException, VersionCollisionException,
+			UpdateObjectException {
+		switch (updateKind) {
+		case UPDATE_FORCE:
+			super.checkAndUpdateEntity(storableObject, true);
+			break;
+		case UPDATE_CHECK:
+		default:
+			super.checkAndUpdateEntity(storableObject, false);
+			break;
+		}
+    }
+    
+    public void update(List storableObjects, int updateKind, Object arg)
+			throws IllegalDataException, VersionCollisionException,
+			UpdateObjectException {
+		switch (updateKind) {
+		case UPDATE_FORCE:
+			super.checkAndUpdateEntities(storableObjects, true);
+			break;
+		case UPDATE_CHECK:
+		default:
+			super.checkAndUpdateEntities(storableObjects, false);
+			break;
+		}
+    }
+    
+    public List retrieveAll() throws RetrieveObjectException {
+        List list = null;
+        try {
+            list = retrieveByIds(null, null);
+        }  catch (IllegalDataException ide) {           
+            Log.debugMessage("CableThreadDatabase.retrieveAll | Trying: " + ide, Log.DEBUGLEVEL09);
+            throw new RetrieveObjectException(ide);
+        }
+        return list;
+    }
+    
+    public List retrieveByIds(List ids, String condition)
+			throws IllegalDataException, RetrieveObjectException {
+		List list = null;
+		if ((ids == null) || (ids.isEmpty()))
+			list = super.retrieveByIdsOneQuery(null, condition);
+		else
+			list = super.retrieveByIdsOneQuery(ids, condition);
+		
+		return list;
+	}
+    
+    public List retrieveByCondition(List ids, StorableObjectCondition condition)
+			throws RetrieveObjectException, IllegalDataException {
+		List list;
+		{
+			Log
+					.errorMessage("CableThreadDatabase.retrieveByCondition | Unknown condition class: "
+							+ condition.getClass().getName());
+			list = this.retrieveButIds(ids);
+		}
+		return list;
+	}
+}
