36d3aa614d6ec8bf22d1941fd5a5544e5ba92c27
Arseniy Tashoyan <tashoyan@gmail.com>
2004-12-20 16:22:21 +0300

    Class to generate identifiers locally for internal needs of a client

---
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/general/LocalIdentifierGenerator.java
@@ -0,0 +1,55 @@
+/*
+ * $Id: LocalIdentifierGenerator.java,v 1.1 2004/12/20 13:22:21 arseniy Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.general;
+
+import java.util.List;
+import java.util.LinkedList;
+/**
+ * @version $Revision: 1.1 $, $Date: 2004/12/20 13:22:21 $
+ * @author $Author: arseniy $
+ * @module generalclient_v1
+ */
+
+public class LocalIdentifierGenerator {
+	private static final long MAX_COUNTER = 100000L;
+	private static long counter = 0;
+
+	private LocalIdentifierGenerator() {
+		// singleton constructor
+	}
+
+	public static synchronized Identifier generateIdentifier(short entityCode) throws IllegalObjectEntityException, IdentifierGenerationException {
+		short major = generateMajor(entityCode);
+		long minor = generateMinor(entityCode);
+		return new Identifier(major, minor);
+	}
+
+	public static synchronized Identifier[] generateIdentifierRange(short entityCode, int rangeSize) throws IllegalObjectEntityException, IdentifierGenerationException {
+		List list = new LinkedList();
+		short major = generateMajor(entityCode);
+		long minor;
+		for (int i = 0; i < rangeSize; i++) {
+			minor = generateMinor(entityCode);
+			list.add(new Identifier(major, minor));
+		}
+		return (Identifier[])list.toArray(new Identifier[list.size()]);
+	}
+
+	private static short generateMajor(short entityCode) throws IllegalObjectEntityException {
+		if (ObjectEntities.codeIsValid(entityCode))
+			return entityCode;
+		throw new IllegalObjectEntityException("Illegal or unknown entity code supplied: " + entityCode, IllegalObjectEntityException.ENTITY_NOT_REGISTERED_CODE);
+	}
+
+	private static long generateMinor(short entityCode) throws IdentifierGenerationException {
+		if (counter >= MAX_COUNTER)
+			counter = 0;
+
+		return System.currentTimeMillis() + counter;
+	}
+}
