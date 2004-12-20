ac70a4a8c8e28d7fc3184382dc54c199f353d573
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2004-12-20 16:29:37 +0300

    *** empty log message ***

--- AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/general/LocalIdentifierGenerator.java
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/general/LocalIdentifierGenerator.java
@@ -1,5 +1,5 @@
 /*
- * $Id: LocalIdentifierGenerator.java,v 1.1 2004/12/20 13:22:21 arseniy Exp $
+ * $Id: LocalIdentifierGenerator.java,v 1.2 2004/12/20 13:29:37 krupenn Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -10,8 +10,8 @@ package com.syrus.AMFICOM.general;
 import java.util.List;
 import java.util.LinkedList;
 /**
- * @version $Revision: 1.1 $, $Date: 2004/12/20 13:22:21 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.2 $, $Date: 2004/12/20 13:29:37 $
+ * @author $Author: krupenn $
  * @module generalclient_v1
  */
 
@@ -50,6 +50,6 @@ public class LocalIdentifierGenerator {
 		if (counter >= MAX_COUNTER)
 			counter = 0;
 
-		return System.currentTimeMillis() + counter;
+		return System.currentTimeMillis() + (counter++);
 	}
 }
