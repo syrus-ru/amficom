9aaf1c074148bc0964f2e92c8bb0f5441d3b3224
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-12-21 18:35:35 +0300

    remove domain_id, add dimension_x/y

--- AMFICOM/v1/database/schema/mapletty/type/physicallinktype.sql
+++ AMFICOM/v1/database/schema/mapletty/type/physicallinktype.sql
@@ -6,18 +6,17 @@ CREATE TABLE PhysicalLinkType (
  modifier_id VARCHAR2(32) NOT NULL,
 --
  codename VARCHAR2(32) NOT NULL,
- domain_id VARCHAR2(32) NOT NULL,
  name VARCHAR2(128),
  description VARCHAR2(256),
+ dimension_x NUMBER(6),
+ dimension_y NUMBER(6),
 --
  CONSTRAINT phlinktype_pk PRIMARY KEY (id),
  CONSTRAINT phlinktype_uniq UNIQUE (codename),
  CONSTRAINT phlinktype_creator_fk FOREIGN KEY (creator_id)
   REFERENCES Users (id) ON DELETE CASCADE,
  CONSTRAINT phlinktype_modifier_fk FOREIGN KEY (modifier_id)
-  REFERENCES Users (id) ON DELETE CASCADE,
- CONSTRAINT phlinktype_domain_fk FOREIGN KEY (domain_id)
-  REFERENCES Domain (id) ON DELETE CASCADE
+  REFERENCES Users (id) ON DELETE CASCADE
 );
 
 CREATE SEQUENCE physycallinktype_seq ORDER;
