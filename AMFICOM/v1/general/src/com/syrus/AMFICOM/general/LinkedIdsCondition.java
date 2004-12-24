c3b20348b55e6550636781953e11340cdcccb0c2
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2004-12-24 12:52:21 +0300

    fix

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/LinkedIdsCondition.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/LinkedIdsCondition.java
@@ -1,5 +1,5 @@
 /*
- * $Id: LinkedIdsCondition.java,v 1.3 2004/12/24 09:35:28 bob Exp $
+ * $Id: LinkedIdsCondition.java,v 1.4 2004/12/24 09:52:21 bob Exp $
  *
  * Copyright ¿ 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -62,7 +62,7 @@ import com.syrus.util.Log;
  * </ul>
  * 
  * @author $Author: bob $
- * @version $Revision: 1.3 $, $Date: 2004/12/24 09:35:28 $
+ * @version $Revision: 1.4 $, $Date: 2004/12/24 09:52:21 $
  * @module general_v1
  */
 public class LinkedIdsCondition implements StorableObjectCondition {
@@ -130,10 +130,14 @@ public class LinkedIdsCondition implements StorableObjectCondition {
 		final String className = "com.syrus.AMFICOM." + ObjectGroupEntities.getGroupName(code.shortValue()).toLowerCase().replaceAll("group$", "") + ".LinkedIdsCondition"; //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		try {
 			Constructor ctor;
-			if (id == null)
+			if (id == null){
 				ctor = Class.forName(className).getDeclaredConstructor(new Class[] { List.class, Short.class});
-			else
+				this.delegate = (LinkedIdsCondition) ctor.newInstance(new Object[] { linkIds, code});
+			}
+			else {
 				ctor = Class.forName(className).getDeclaredConstructor(new Class[] { Identifier.class, Short.class});
+				this.delegate = (LinkedIdsCondition) ctor.newInstance(new Object[] { id, code});
+			}
 			ctor.setAccessible(true);
 			this.delegate = (LinkedIdsCondition) ctor.newInstance(new Object[] { linkIds, code});
 		} catch (ClassNotFoundException cnfe) {
@@ -208,12 +212,16 @@ public class LinkedIdsCondition implements StorableObjectCondition {
 		final String className = "com.syrus.AMFICOM." + ObjectGroupEntities.getGroupName(entityCode.shortValue()).toLowerCase().replaceAll("group$", "") + ".LinkedIdsCondition"; //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		try {
 			Constructor ctor;
-			if (identifier == null)
+			if (identifier == null){
 				ctor = Class.forName(className).getDeclaredConstructor(new Class[] { List.class, Short.class});
-			else
+				this.delegate = (LinkedIdsCondition) ctor.newInstance(new Object[] { linkedIds, entityCode});
+			}
+			else {
 				ctor = Class.forName(className).getDeclaredConstructor(new Class[] { Identifier.class, Short.class});
+				this.delegate = (LinkedIdsCondition) ctor.newInstance(new Object[] { identifier, entityCode});
+			}
 			ctor.setAccessible(true);
-			this.delegate = (LinkedIdsCondition) ctor.newInstance(new Object[] { linkedIds, entityCode});
+			
 		} catch (ClassNotFoundException cnfe) {
 			Log.debugMessage(LINKED_IDS_CONDITION_INIT + "Class " + className //$NON-NLS-1$
 					+ " not found on the classpath" //$NON-NLS-1$
