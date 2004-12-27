59890b57596f829583d87e7a3627b2c82dff164d
Arseniy Tashoyan <tashoyan@gmail.com>
2004-12-28 00:05:55 +0300

    porttype.sql - added constraint 'NOT NULL' on field 'sort'

--- AMFICOM/v1/database/schema/configuration/type/porttype.sql
+++ AMFICOM/v1/database/schema/configuration/type/porttype.sql
@@ -9,7 +9,7 @@ CREATE TABLE PortType (
  description VARCHAR2(256),
 --
  name VARCHAR2(64),
- sort NUMBER(2,0),
+ sort NUMBER(2,0) NOT NULL,
 --
  CONSTRAINT porttype_pk PRIMARY KEY (id),
  CONSTRAINT porttype_creator_fk FOREIGN KEY (creator_id)
