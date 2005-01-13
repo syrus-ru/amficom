5882f97b2abf7e03903154a15a750e72d74f8e05
Maxim Selivanov <max.selivanov@gmail.com>
2005-01-13 12:59:32 +0300

    *** empty log message ***

--- tools/test/test/com/syrus/AMFICOM/configuration/CharacteristicTypeTestCase.java
+++ tools/test/test/com/syrus/AMFICOM/configuration/CharacteristicTypeTestCase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CharacteristicTypeTestCase.java,v 1.2 2004/08/31 15:29:12 bob Exp $
+ * $Id: CharacteristicTypeTestCase.java,v 1.3 2005/01/13 09:59:32 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -15,11 +15,13 @@ import junit.framework.Test;
 import com.syrus.AMFICOM.configuration.CharacteristicType;
 import com.syrus.AMFICOM.configuration.CharacteristicTypeDatabase;
 import com.syrus.AMFICOM.configuration.ConfigurationDatabaseContext;
+import com.syrus.AMFICOM.configuration.corba.CharacteristicTypeSort;
 import com.syrus.AMFICOM.configuration.corba.CharacteristicType_Transferable;
 import com.syrus.AMFICOM.general.CreateObjectException;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.IdentifierGenerationException;
 import com.syrus.AMFICOM.general.IdentifierGenerator;
+import com.syrus.AMFICOM.general.IllegalDataException;
 import com.syrus.AMFICOM.general.IllegalObjectEntityException;
 import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.ObjectNotFoundException;
@@ -27,8 +29,8 @@ import com.syrus.AMFICOM.general.RetrieveObjectException;
 import com.syrus.AMFICOM.general.corba.DataType;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2004/08/31 15:29:12 $
- * @author $Author: bob $
+ * @version $Revision: 1.3 $, $Date: 2005/01/13 09:59:32 $
+ * @author $Author: max $
  * @module tools
  */
 public class CharacteristicTypeTestCase extends ConfigureTestCase {
@@ -49,13 +51,16 @@ public class CharacteristicTypeTestCase extends ConfigureTestCase {
 	}
 
 	public void testCreation() throws IdentifierGenerationException, IllegalObjectEntityException,
-			CreateObjectException, ObjectNotFoundException, RetrieveObjectException {
+			CreateObjectException, ObjectNotFoundException, RetrieveObjectException, IllegalDataException {
 		CharacteristicTypeDatabase characteristicTypeDatabase = (CharacteristicTypeDatabase)ConfigurationDatabaseContext.getCharacteristicTypeDatabase();
 		List list = characteristicTypeDatabase.retrieveAll();
 		Identifier id = IdentifierGenerator.generateIdentifier(ObjectEntities.CHARACTERISTICTYPE_ENTITY_CODE);
-		CharacteristicType chType = CharacteristicType.createInstance(id, ConfigureTestCase.creatorId, "CharacteristicTypeTestCase",
-															"characteristicType created by CharacteristicTypeTestCase",
-															DataType._DATA_TYPE_STRING, false,false);
+		CharacteristicType chType = CharacteristicType.createInstance(id, 
+                ConfigureTestCase.creatorId,
+                "CharacteristicTypeTestCase",
+				"characteristicType created by CharacteristicTypeTestCase",
+				DataType._DATA_TYPE_STRING,
+                CharacteristicTypeSort.CHARACTERISTICTYPESORT_ELECTRICAL);
 		CharacteristicType chType2 = CharacteristicType.getInstance((CharacteristicType_Transferable) chType.getTransferable());
 
 		assertEquals(chType.getId(), chType2.getId());
