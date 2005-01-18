7434a32d49153cb21d0364ea6a0770d026d8a3df
Maxim Selivanov <max.selivanov@gmail.com>
2005-01-18 10:51:57 +0300

    added manufacturer VARCHAR2(64) and manufacturer_code VARCHAR2(64)

--- AMFICOM/v1/database/schema/configuration/type/equipmenttype.sql
+++ AMFICOM/v1/database/schema/configuration/type/equipmenttype.sql
@@ -9,6 +9,8 @@ CREATE TABLE EquipmentType (
  description VARCHAR2(256),
 --
  name VARCHAR2(128),
+ manufacturer VARCHAR2(64),
+ manufacturer_code VARCHAR2(64),
 --
  CONSTRAINT eqptype_pk PRIMARY KEY (id),
  CONSTRAINT eqptype_creator_fk FOREIGN KEY (creator_id)
