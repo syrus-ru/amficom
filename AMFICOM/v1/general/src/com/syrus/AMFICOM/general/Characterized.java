e52cc560e79ccca2dd6e3d984ae40c6f4413079d
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-01-28 11:07:22 +0300

    fix argument name at setCharacteristics

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/Characterized.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/Characterized.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Characterized.java,v 1.2 2005/01/17 08:26:35 bob Exp $
+ * $Id: Characterized.java,v 1.3 2005/01/28 08:07:22 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -11,7 +11,7 @@ package com.syrus.AMFICOM.general;
 import java.util.List;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/01/17 08:26:35 $
+ * @version $Revision: 1.3 $, $Date: 2005/01/28 08:07:22 $
  * @author $Author: bob $
  * @module general_v1
  */
@@ -20,5 +20,5 @@ public interface Characterized extends Identified {
 
 	List getCharacteristics();
 
-	void setCharacteristics(List characteristic_ids);
+	void setCharacteristics(List characteristics);
 }
