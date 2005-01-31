bc632be1694fc56ea72c14600cc4450ea0a55686
Arseniy Tashoyan <tashoyan@gmail.com>
2005-01-31 15:27:30 +0300

    Moved from generalclient

---
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/LocalIdentifierGeneratorServer.java
@@ -0,0 +1,55 @@
+package com.syrus.AMFICOM.general;
+
+import com.syrus.AMFICOM.general.corba.IdentifierGeneratorServer;
+import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
+import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
+import com.syrus.AMFICOM.general.corba.ErrorCode;
+import com.syrus.AMFICOM.general.corba.CompletionStatus;
+import com.syrus.util.Log;
+
+public class LocalIdentifierGeneratorServer implements IdentifierGeneratorServer {
+
+	public LocalIdentifierGeneratorServer() {
+	}
+
+	public Identifier_Transferable getGeneratedIdentifier(short entityCode) throws AMFICOMRemoteException {
+		try {
+			Identifier identifier = LocalIdentifierGenerator.generateIdentifier(entityCode);
+			return (Identifier_Transferable)identifier.getTransferable();
+		}
+		catch (IllegalObjectEntityException ioee) {
+			Log.errorException(ioee);
+			throw new AMFICOMRemoteException(ErrorCode.ERROR_ILLEGAL_OBJECT_ENTITY,
+																			 CompletionStatus.COMPLETED_NO,
+																			 "Illegal object entity: '" + ObjectEntities.codeToString(entityCode) + "'");
+		}
+		catch (IdentifierGenerationException ige) {
+			Log.errorException(ige);
+			throw new AMFICOMRemoteException(ErrorCode.ERROR_RETRIEVE,
+																			 CompletionStatus.COMPLETED_NO,
+																			 "Cannot create major/minor entries of identifier for entity: '" + ObjectEntities.codeToString(entityCode) + "' -- " + ige.getMessage());
+		}	
+	}
+
+	public Identifier_Transferable[] getGeneratedIdentifierRange(short entityCode, int size) throws AMFICOMRemoteException {
+		try {
+			Identifier[] identifiers = LocalIdentifierGenerator.generateIdentifierRange(entityCode, size);
+			Identifier_Transferable[] identifiersT = new Identifier_Transferable[identifiers.length];
+			for (int i = 0; i < identifiersT.length; i++)
+				identifiersT[i] = (Identifier_Transferable)identifiers[i].getTransferable();
+			return identifiersT;
+		}
+		catch (IllegalObjectEntityException ioee) {
+			Log.errorException(ioee);
+			throw new AMFICOMRemoteException(ErrorCode.ERROR_ILLEGAL_OBJECT_ENTITY,
+																			 CompletionStatus.COMPLETED_NO,
+																			 "Illegal object entity: '" + ObjectEntities.codeToString(entityCode) + "'");
+		}
+		catch (IdentifierGenerationException ige) {
+			Log.errorException(ige);
+			throw new AMFICOMRemoteException(ErrorCode.ERROR_RETRIEVE,
+																			 CompletionStatus.COMPLETED_NO,
+																			 "Cannot create major/minor entries of identifier for entity: '" + ObjectEntities.codeToString(entityCode) + "' -- " + ige.getMessage());
+		}
+	}
+}
