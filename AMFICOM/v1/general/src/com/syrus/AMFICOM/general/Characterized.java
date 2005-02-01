06c074558375a3cada60ac1447486120ecc1ffaf
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-02-01 10:41:10 +0300

    add add/removeCharacteristic

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/Characterized.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/Characterized.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Characterized.java,v 1.3 2005/01/28 08:07:22 bob Exp $
+ * $Id: Characterized.java,v 1.4 2005/02/01 07:41:10 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -11,7 +11,7 @@ package com.syrus.AMFICOM.general;
 import java.util.List;
 
 /**
- * @version $Revision: 1.3 $, $Date: 2005/01/28 08:07:22 $
+ * @version $Revision: 1.4 $, $Date: 2005/02/01 07:41:10 $
  * @author $Author: bob $
  * @module general_v1
  */
@@ -21,4 +21,8 @@ public interface Characterized extends Identified {
 	List getCharacteristics();
 
 	void setCharacteristics(List characteristics);
+	
+	void addCharacteristic(Characteristic characteristic);
+	
+	void removeCharacteristic(Characteristic characteristic);
 }
