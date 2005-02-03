bd5102e359bbdcf8796e332fd4dd3ce0836f26af
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-02-03 11:52:48 +0300

    fix modificator

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectWrapper.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectWrapper.java
@@ -1,5 +1,5 @@
 /*
- * $Id: StorableObjectWrapper.java,v 1.1 2005/02/03 08:37:26 bob Exp $
+ * $Id: StorableObjectWrapper.java,v 1.2 2005/02/03 08:52:48 bob Exp $
  *
  * Copyright ¿ 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -23,7 +23,7 @@ import com.syrus.util.Wrapper;
  * a static method <code>getInstance()</code>.
  * 
  * @author $Author: bob $
- * @version $Revision: 1.1 $, $Date: 2005/02/03 08:37:26 $
+ * @version $Revision: 1.2 $, $Date: 2005/02/03 08:52:48 $
  * @see <a href =
  *      "http://bass.science.syrus.ru/java/Bitter%20Java.pdf">&laquo;Bitter
  *      Java&raquo; by Bruce A. Tate </a>
@@ -31,21 +31,21 @@ import com.syrus.util.Wrapper;
  */
 public interface StorableObjectWrapper extends Wrapper {
 
-	final String	COLUMN_CREATED					= "created";
-	final String	COLUMN_CREATOR_ID				= "creator_id";
-	final String	COLUMN_ID						= "id";
-	final String	COLUMN_MODIFIED					= "modified";
-	final String	COLUMN_MODIFIER_ID				= "modifier_id";
+	String	COLUMN_CREATED					= "created";
+	String	COLUMN_CREATOR_ID				= "creator_id";
+	String	COLUMN_ID						= "id";
+	String	COLUMN_MODIFIED					= "modified";
+	String	COLUMN_MODIFIER_ID				= "modifier_id";
 
-	final String	COLUMN_CODENAME					= "codename";
-	final String	COLUMN_DESCRIPTION				= "description";
-	final String	COLUMN_NAME						= "name";
+	String	COLUMN_CODENAME					= "codename";
+	String	COLUMN_DESCRIPTION				= "description";
+	String	COLUMN_NAME						= "name";
 
-	final String	COLUMN_TYPE_ID					= "type_id";
+	String	COLUMN_TYPE_ID					= "type_id";
 
-	final String	COLUMN_CHARACTERISTICS			= "characteristics";
+	String	COLUMN_CHARACTERISTICS			= "characteristics";
 
-	final String	LINK_COLUMN_PARAMETER_MODE		= "parameter_mode";
-	final String	LINK_COLUMN_PARAMETER_TYPE_ID	= "parameter_type_id";
+	String	LINK_COLUMN_PARAMETER_MODE		= "parameter_mode";
+	String	LINK_COLUMN_PARAMETER_TYPE_ID	= "parameter_type_id";
 
 }
