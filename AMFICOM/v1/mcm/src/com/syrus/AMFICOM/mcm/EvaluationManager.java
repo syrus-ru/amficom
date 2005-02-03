3e9ce0b6f8fcb6c58b2ffc97459663324fd671a6
Arseniy Tashoyan <tashoyan@gmail.com>
2005-02-03 19:03:08 +0300

    Removed method 'getAlarmLevel'

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/EvaluationManager.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/EvaluationManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: EvaluationManager.java,v 1.6 2004/07/21 18:43:32 arseniy Exp $
+ * $Id: EvaluationManager.java,v 1.7 2005/02/03 16:03:08 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -9,10 +9,9 @@
 package com.syrus.AMFICOM.mcm;
 
 import com.syrus.AMFICOM.measurement.SetParameter;
-import com.syrus.AMFICOM.event.corba.AlarmLevel;
 
 /**
- * @version $Revision: 1.6 $, $Date: 2004/07/21 18:43:32 $
+ * @version $Revision: 1.7 $, $Date: 2005/02/03 16:03:08 $
  * @author $Author: arseniy $
  * @module mcm_v1
  */
@@ -20,6 +19,6 @@ import com.syrus.AMFICOM.event.corba.AlarmLevel;
 public interface EvaluationManager {
 
 	SetParameter[] evaluate() throws EvaluationException;
-
-	AlarmLevel getAlarmLevel();
+//
+//	AlarmLevel getAlarmLevel();
 }
