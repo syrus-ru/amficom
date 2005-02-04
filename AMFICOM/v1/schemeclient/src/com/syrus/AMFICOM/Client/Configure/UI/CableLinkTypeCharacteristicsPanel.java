040f7423d2d44720a97869c4bb54e9660cc5c29c
Stanislav Kholshin <kholshin@gmail.com>
2005-02-04 14:43:08 +0300

    *** empty log message ***

---
+++ AMFICOM/v1/schemeclient/src/com/syrus/AMFICOM/Client/Configure/UI/CableLinkTypeCharacteristicsPanel.java
@@ -0,0 +1,79 @@
+package com.syrus.AMFICOM.Client.Configure.UI;
+
+import java.awt.BorderLayout;
+
+import com.syrus.AMFICOM.Client.General.Lang.LangModelConfig;
+import com.syrus.AMFICOM.Client.General.Model.ApplicationContext;
+import com.syrus.AMFICOM.client_.general.ui_.GeneralPanel;
+import com.syrus.AMFICOM.configuration.CableLinkType;
+import com.syrus.AMFICOM.general.corba.*;
+
+public class CableLinkTypeCharacteristicsPanel extends GeneralPanel
+{
+	protected CableLinkType type;
+	private static CharacteristicTypeSort[] sorts = new CharacteristicTypeSort[] {
+			CharacteristicTypeSort.CHARACTERISTICTYPESORT_ELECTRICAL,
+			CharacteristicTypeSort.CHARACTERISTICTYPESORT_INTERFACE,
+			CharacteristicTypeSort.CHARACTERISTICTYPESORT_OPERATIONAL,
+			CharacteristicTypeSort.CHARACTERISTICTYPESORT_OPTICAL
+	};
+
+	private CharacteristicsPanel charPane;
+
+	protected CableLinkTypeCharacteristicsPanel()
+	{
+		super();
+
+		try {
+			jbInit();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected CableLinkTypeCharacteristicsPanel(CableLinkType l)
+	{
+		this();
+		setObject(l);
+	}
+
+	private void jbInit() throws Exception
+	{
+		charPane = new CharacteristicsPanel();
+		setName(LangModelConfig.getString("label_chars"));
+
+		this.setLayout(new BorderLayout());
+		this.add(charPane, BorderLayout.CENTER);
+	}
+
+	public Object getObject()
+	{
+		return type;
+	}
+
+	public void setContext(ApplicationContext aContext)
+	{
+		super.setContext(aContext);
+		charPane.setContext(aContext);
+	}
+
+	public void setObject(Object or)
+	{
+		this.type = (CableLinkType)or;
+
+		for (int i = 0; i < sorts.length; i++)
+			charPane.setTypeSortMapping(
+					sorts[i],
+					CharacteristicSort.CHARACTERISTIC_SORT_LINKTYPE,
+					type,
+					type.getId(),
+					true);
+		charPane.addCharacteristics(type.getCharacteristics(), type.getId());
+	}
+
+	public boolean modify()
+	{
+		return true;
+	}
+}
