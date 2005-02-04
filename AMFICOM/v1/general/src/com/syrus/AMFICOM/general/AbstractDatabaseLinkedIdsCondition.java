37f7678476f394997664d8f79df8c94e2acd6eca
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-02-04 13:35:31 +0300

    throws expection if entity isn't supported

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/AbstractDatabaseLinkedIdsCondition.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/AbstractDatabaseLinkedIdsCondition.java
@@ -1,10 +1,10 @@
 /*
-* $Id: AbstractDatabaseLinkedIdsCondition.java,v 1.1 2005/02/03 14:56:10 bob Exp $
-*
-* Copyright ¿ 2004 Syrus Systems.
-* Dept. of Science & Technology.
-* Project: AMFICOM.
-*/
+ * $Id: AbstractDatabaseLinkedIdsCondition.java,v 1.2 2005/02/04 10:35:31 bob Exp $
+ *
+ * Copyright ¿ 2004 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
 
 package com.syrus.AMFICOM.general;
 
@@ -12,29 +12,32 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-
 /**
- * @version $Revision: 1.1 $, $Date: 2005/02/03 14:56:10 $
+ * @version $Revision: 1.2 $, $Date: 2005/02/04 10:35:31 $
  * @author $Author: bob $
  * @module general_v1
  */
 public abstract class AbstractDatabaseLinkedIdsCondition implements DatabaseStorableObjectCondition {
 
-	protected LinkedIdsCondition condition;
-	
+	protected LinkedIdsCondition	condition;
+
 	public AbstractDatabaseLinkedIdsCondition(LinkedIdsCondition delegate) {
 		this.condition = delegate;
 	}
 
 	protected abstract String getColumnName();
-	
+
 	public Short getEntityCode() {
 		return this.condition.getEntityCode();
 	}
-	
+
 	public String getSQLQuery() throws IllegalDataException {
+		String columnName = this.getColumnName();
+		if (columnName == null)
+			throw new IllegalDataException("AbstractDatabaseLinkedIdsCondition.getSQLQuery | "
+					+ ObjectEntities.codeToString(this.getEntityCode()) + " isn't supported");
 		StringBuffer buffer = new StringBuffer();
-		buffer.append(this.getColumnName());
+		buffer.append(columnName);
 		buffer.append(StorableObjectDatabase.SQL_IN);
 		buffer.append(StorableObjectDatabase.OPEN_BRACKET);
 		int i = 1;
@@ -46,13 +49,11 @@ public abstract class AbstractDatabaseLinkedIdsCondition implements DatabaseStor
 			Identifier id = null;
 			if (object instanceof Identifier)
 				id = (Identifier) object;
+			else if (object instanceof Identified)
+				id = ((Identified) object).getId();
 			else
-				if (object instanceof Identified)
-					id = ((Identified) object).getId();
-				else
-					throw new IllegalDataException("AbstractDatabaseLinkedIdsCondition.getSQLQuery | Object "
-							+ object.getClass().getName()
-							+ " isn't Identifier or Identified");
+				throw new IllegalDataException("AbstractDatabaseLinkedIdsCondition.getSQLQuery | Object "
+						+ object.getClass().getName() + " isn't Identifier or Identified");
 
 			if (id != null) {
 				buffer.append(DatabaseIdentifier.toSQLString(id));
@@ -62,7 +63,7 @@ public abstract class AbstractDatabaseLinkedIdsCondition implements DatabaseStor
 					else {
 						buffer.append(StorableObjectDatabase.CLOSE_BRACKET);
 						buffer.append(StorableObjectDatabase.SQL_OR);
-						buffer.append(this.getColumnName());
+						buffer.append(columnName);
 						buffer.append(StorableObjectDatabase.SQL_IN);
 						buffer.append(StorableObjectDatabase.OPEN_BRACKET);
 					}
