49d56a6b22880ebc9087d656030d90c6ff7fd049
Arseniy Tashoyan <tashoyan@gmail.com>
2005-02-07 14:57:52 +0300

    Removed fields 'alarmDatabase' and 'alarmTypeDatabase'

--- AMFICOM/v1/event/src/com/syrus/AMFICOM/event/EventDatabaseContext.java
+++ AMFICOM/v1/event/src/com/syrus/AMFICOM/event/EventDatabaseContext.java
@@ -1,5 +1,5 @@
 /*
- * $Id: EventDatabaseContext.java,v 1.2 2004/12/24 18:49:03 arseniy Exp $
+ * $Id: EventDatabaseContext.java,v 1.3 2005/02/07 11:57:52 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -12,38 +12,39 @@ import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectDatabase;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2004/12/24 18:49:03 $
+ * @version $Revision: 1.3 $, $Date: 2005/02/07 11:57:52 $
  * @author $Author: arseniy $
  * @module event_v1
  */
 
 public class EventDatabaseContext {
 	protected static StorableObjectDatabase	eventTypeDatabase;
-	protected static StorableObjectDatabase	alarmTypeDatabase;
+//	protected static StorableObjectDatabase	alarmTypeDatabase;
 
 	protected static StorableObjectDatabase	eventDatabase;
-	protected static StorableObjectDatabase	alarmDatabase;
+//	protected static StorableObjectDatabase	alarmDatabase;
 
 	private EventDatabaseContext() {
 		//singleton
 	}
 
 	public static void init(StorableObjectDatabase eventTypeDatabase1,
-													StorableObjectDatabase alarmTypeDatabase1,
-													StorableObjectDatabase eventDatabase1,
-													StorableObjectDatabase alarmDatabase1) {
+//													StorableObjectDatabase alarmTypeDatabase1,
+													StorableObjectDatabase eventDatabase1
+//													StorableObjectDatabase alarmDatabase1
+													) {
 
 		if (eventTypeDatabase1 != null)
 			eventTypeDatabase = eventTypeDatabase1;
 
-		if (alarmTypeDatabase1 != null)
-			alarmTypeDatabase = alarmTypeDatabase1;
+//		if (alarmTypeDatabase1 != null)
+//			alarmTypeDatabase = alarmTypeDatabase1;
 
 		if (eventDatabase1 != null)
 			eventDatabase = eventDatabase1;
 
-		if (alarmDatabase1 != null)
-			alarmDatabase = alarmDatabase1;
+//		if (alarmDatabase1 != null)
+//			alarmDatabase = alarmDatabase1;
 	}
 
 	public static StorableObjectDatabase getDatabase(short entityCode) {
@@ -55,11 +56,11 @@ public class EventDatabaseContext {
 			case ObjectEntities.EVENT_ENTITY_CODE:
 				return eventDatabase;
 
-			case ObjectEntities.ALARMTYPE_ENTITY_CODE:
-				return alarmTypeDatabase;
-
-			case ObjectEntities.ALARM_ENTITY_CODE:
-				return alarmDatabase;
+//			case ObjectEntities.ALARMTYPE_ENTITY_CODE:
+//				return alarmTypeDatabase;
+//
+//			case ObjectEntities.ALARM_ENTITY_CODE:
+//				return alarmDatabase;
 
 			default:
 				return null;
@@ -74,11 +75,11 @@ public class EventDatabaseContext {
 		return eventDatabase;
 	}
 
-	public static StorableObjectDatabase getAlarmTypeDatabase() {
-		return alarmTypeDatabase;
-	}
-
-	public static StorableObjectDatabase getAlarmDatabase() {
-		return alarmDatabase;
-	}
+//	public static StorableObjectDatabase getAlarmTypeDatabase() {
+//		return alarmTypeDatabase;
+//	}
+//
+//	public static StorableObjectDatabase getAlarmDatabase() {
+//		return alarmDatabase;
+//	}
 }
