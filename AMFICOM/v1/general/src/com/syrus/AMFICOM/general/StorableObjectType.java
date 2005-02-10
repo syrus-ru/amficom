64f9f47747f9dee453894fff9e26b8445de569f5
Arseniy Tashoyan <tashoyan@gmail.com>
2005-02-10 15:51:18 +0300

    Version support Changes in methods and constructors due to changes in StorableObject

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectType.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectType.java
@@ -1,5 +1,5 @@
 /*
- * $Id: StorableObjectType.java,v 1.13 2005/02/03 14:26:18 arseniy Exp $
+ * $Id: StorableObjectType.java,v 1.14 2005/02/10 12:51:18 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -13,7 +13,7 @@ import java.util.Date;
 import com.syrus.AMFICOM.general.corba.StorableObject_Transferable;
 
 /**
- * @version $Revision: 1.13 $, $Date: 2005/02/03 14:26:18 $
+ * @version $Revision: 1.14 $, $Date: 2005/02/10 12:51:18 $
  * @author $Author: arseniy $
  * @module general_v1
  */
@@ -23,71 +23,64 @@ public abstract class StorableObjectType extends StorableObject {
 
 	protected String codename;
 	protected String description;
-	
+
 	public StorableObjectType(Identifier id) {
 		super(id);
 	}
 
 	public StorableObjectType(Identifier id,
-							  Date created,
-							  Date modified,
-							  Identifier creator_id,
-							  Identifier modifier_id,
-							  String codename,
-							  String description) {
-		super(id,
-			  created,
-			  modified,
-			  creator_id,
-			  modifier_id);
+			Date created,
+			Date modified,
+			Identifier creator_id,
+			Identifier modifier_id,
+			long version,
+			String codename,
+			String description) {
+		super(id, created, modified, creator_id, modifier_id, version);
 		this.codename = codename;
 		this.description = description;
 	}
-	
-	public StorableObjectType(StorableObject_Transferable transferable,
-							  String codename,
-							  String description) {
-			super(transferable);
-			this.codename = codename;
-			this.description = description;
+
+	public StorableObjectType(StorableObject_Transferable transferable, String codename, String description) {
+		super(transferable);
+		this.codename = codename;
+		this.description = description;
 	}
 
 	public String getCodename() {
 		return this.codename;
 	}
-	
-	protected void setCodename0(String codename){
+
+	protected void setCodename0(String codename) {
 		this.codename = codename;
 	}
-	
-	public void setCodename(String codename){
+
+	public void setCodename(String codename) {
 		this.setCodename0(codename);
-		super.currentVersion = super.getNextVersion();
+		super.changed = true;
 	}
 
 	public String getDescription() {
 		return this.description;
 	}
-	
-	public void setDescription(String description){
-		this.setDescription0(description);
-		super.currentVersion = super.getNextVersion();
-	}
-	
-	protected void setDescription0(String description){
+
+	protected void setDescription0(String description) {
 		this.description = description;
 	}
 
+	public void setDescription(String description) {
+		this.setDescription0(description);
+		super.changed = true;
+	}
+
 	protected synchronized void setAttributes(Date created,
-											  Date modified,
-											  Identifier creator_id,
-											  Identifier modifier_id,
-											  String codename,
-											  String description) {
-		super.setAttributes(created,
-							modified,
-							creator_id,
-							modifier_id);
+			Date modified,
+			Identifier creator_id,
+			Identifier modifier_id,
+			long version,
+			String codename,
+			String description) {
+		super.setAttributes(created, modified, creator_id, modifier_id, version);
 		this.codename = codename;
 		this.description = description;
 	}
