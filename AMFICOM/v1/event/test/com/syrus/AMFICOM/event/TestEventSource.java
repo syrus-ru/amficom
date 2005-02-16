0e4dedcb265d0df6a23237d43b47bd7af6f2d408
Arseniy Tashoyan <tashoyan@gmail.com>
2005-02-16 16:40:17 +0300

    misc

--- AMFICOM/v1/event/test/com/syrus/AMFICOM/event/TestEventSource.java
+++ AMFICOM/v1/event/test/com/syrus/AMFICOM/event/TestEventSource.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestEventSource.java,v 1.2 2005/02/14 13:19:56 arseniy Exp $
+ * $Id: TestEventSource.java,v 1.3 2005/02/16 13:40:17 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,18 +7,19 @@
  */
 package com.syrus.AMFICOM.event;
 
+import java.util.Collection;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
 import junit.framework.Test;
 
 import com.syrus.AMFICOM.event.corba.EventSource_Transferable;
 import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.EquivalentCondition;
 import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.ObjectEntities;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/02/14 13:19:56 $
+ * @version $Revision: 1.3 $, $Date: 2005/02/16 13:40:17 $
  * @author $Author: arseniy $
  * @module event_v1
  */
@@ -32,31 +33,31 @@ public class TestEventSource extends CommonEventTest {
 		return suiteWrapper(TestEventSource.class);
 	}
 
-	public void testCreateInstance() throws ApplicationException {
-		Identifier creatorId = new Identifier("Users_58");
-		Identifier sourceEntityId;
-		List eventSources = new LinkedList();
-
-		sourceEntityId = new Identifier("MCM_19");
-		eventSources.add(createAndTestEventSource(creatorId, sourceEntityId));
-
-		sourceEntityId = new Identifier("Port_38");
-		eventSources.add(createAndTestEventSource(creatorId, sourceEntityId));
-
-		sourceEntityId = new Identifier("Equipment_19");
-		eventSources.add(createAndTestEventSource(creatorId, sourceEntityId));
-
-		sourceEntityId = new Identifier("TransmissionPath_19");
-		eventSources.add(createAndTestEventSource(creatorId, sourceEntityId));
-
-		EventSource eventSource;
-		for (Iterator it = eventSources.iterator(); it.hasNext();) {
-			eventSource = (EventSource) it.next();
-			System.out.println("id: "+ eventSource.getId());
-			EventStorableObjectPool.putStorableObject(eventSource);
-		}
-		EventStorableObjectPool.flush(false);
-	}
+//	public void testCreateInstance() throws ApplicationException {
+//		Identifier creatorId = new Identifier("Users_58");
+//		Identifier sourceEntityId;
+//		List eventSources = new LinkedList();
+//
+//		sourceEntityId = new Identifier("MCM_19");
+//		eventSources.add(createAndTestEventSource(creatorId, sourceEntityId));
+//
+//		sourceEntityId = new Identifier("Port_38");
+//		eventSources.add(createAndTestEventSource(creatorId, sourceEntityId));
+//
+//		sourceEntityId = new Identifier("Equipment_19");
+//		eventSources.add(createAndTestEventSource(creatorId, sourceEntityId));
+//
+//		sourceEntityId = new Identifier("TransmissionPath_19");
+//		eventSources.add(createAndTestEventSource(creatorId, sourceEntityId));
+//
+//		EventSource eventSource;
+//		for (Iterator it = eventSources.iterator(); it.hasNext();) {
+//			eventSource = (EventSource) it.next();
+//			System.out.println("id: "+ eventSource.getId());
+//			EventStorableObjectPool.putStorableObject(eventSource);
+//		}
+//		EventStorableObjectPool.flush(false);
+//	}
 
 	private EventSource createAndTestEventSource(Identifier creatorId, Identifier sourceEntityId) throws ApplicationException {
 		EventSource eventSource = EventSource.createInstance(creatorId, sourceEntityId);
@@ -74,14 +75,15 @@ public class TestEventSource extends CommonEventTest {
 		return eventSource;
 	}
 
-//	public void testDelete() throws ApplicationException {
-//		EquivalentCondition ec = new EquivalentCondition(ObjectEntities.EVENTSOURCE_ENTITY_CODE);
-//		List eventSources = EventStorableObjectPool.getStorableObjectsByCondition(ec, true);
-//		EventSource eventSource;
-//		for (Iterator it = eventSources.iterator(); it.hasNext();) {
-//			eventSource = (EventSource) it.next();
-//			System.out.println("Event source: " + eventSource.getId());
-//		}
-//		EventStorableObjectPool.delete(eventSources);
-//	}
+	public void testDelete() throws ApplicationException {
+		EquivalentCondition ec = new EquivalentCondition(ObjectEntities.EVENTSOURCE_ENTITY_CODE);
+		Collection eventSources = EventStorableObjectPool.getStorableObjectsByCondition(ec, true);
+		EventSource eventSource;
+		for (Iterator it = eventSources.iterator(); it.hasNext();) {
+			eventSource = (EventSource) it.next();
+			System.out.println("Event source: " + eventSource.getId());
+		}
+		EventStorableObjectPool.delete(eventSources);
+		EventStorableObjectPool.flush(true);
+	}
 }
