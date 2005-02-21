b51651e4480918117ce551d3fcc7d9229fb25651
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-02-21 11:52:51 +0300

    Scheme implications...

--- AMFICOM/v1/database/schema/mapview/object/mapviewscheme.sql
+++ AMFICOM/v1/database/schema/mapview/object/mapviewscheme.sql
@@ -1,7 +1,11 @@
+-- $Id: mapviewscheme.sql,v 1.2 2005/02/21 08:52:51 bass Exp $
+
 CREATE TABLE MapViewScheme (
- mapview_id VARCHAR2(32),
- scheme_id VARCHAR2(32),
+ mapview_id VARCHAR2(32) NOT NULL,
+ scheme_id VARCHAR2(32) NOT NULL,
 --
  CONSTRAINT mvs_mapview_fk FOREIGN KEY (mapview_id)
-  REFERENCES MapView (id) ON DELETE CASCADE
+  REFERENCES MapView (id) ON DELETE CASCADE,
+ CONSTRAINT mvs_scheme_fk FOREIGN KEY (scheme_id)
+  REFERENCES "Scheme" (id) ON DELETE CASCADE
 );
