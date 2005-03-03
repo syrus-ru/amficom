33ea35a83f58ced9f72f560942d37936fc6bc6d9
Alexey Stratonnikov <alexsaa@gmail.com>
2005-03-03 18:57:31 +0300

    BUGFIX: corrected asympY1; was abs. wrong

--- AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/ComplexReflectogramEvent.java
+++ AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/ComplexReflectogramEvent.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ComplexReflectogramEvent.java,v 1.6 2005/03/03 15:10:55 saa Exp $
+ * $Id: ComplexReflectogramEvent.java,v 1.7 2005/03/03 15:57:31 saa Exp $
  * 
  * Copyright © Syrus Systems.
  * Dept. of Science & Technology.
@@ -11,7 +11,7 @@ import com.syrus.AMFICOM.analysis.dadara.SimpleReflectogramEvent;
 
 /**
  * @author $Author: saa $
- * @version $Revision: 1.6 $, $Date: 2005/03/03 15:10:55 $
+ * @version $Revision: 1.7 $, $Date: 2005/03/03 15:57:31 $
  * @module dadara
  * 
  * Класс предназначен для хранения расширенной информации о
@@ -50,7 +50,7 @@ public class ComplexReflectogramEvent implements SimpleReflectogramEvent
 		int N = end - begin + 1;
 		double []yArr = mt.getYArray(begin, N);
 		asympY0 = yArr[0];
-		asympY1 = yArr[1];
+		asympY1 = yArr[N - 1];
 		mLoss = asympY1 - asympY0;
 		aLet = ReflectogramMath.getArrayMax(yArr) - asympY0;
 	}
