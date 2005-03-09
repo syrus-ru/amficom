bde3e8db0f1c7e13aedae355c2f6bd471c3ad874
Alexey Stratonnikov <alexsaa@gmail.com>
2005-03-09 14:21:43 +0300

    MINOR PERFORMANCE: caching added for getYArray(). It is expected to become useless when native code will not need to unpack BreakL

--- AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/ModelTraceImplMF.java
+++ AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/ModelTraceImplMF.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ModelTraceImplMF.java,v 1.2 2005/02/15 06:51:48 saa Exp $
+ * $Id: ModelTraceImplMF.java,v 1.3 2005/03/09 11:21:43 saa Exp $
  * 
  * Copyright © Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,13 +9,14 @@ package com.syrus.AMFICOM.analysis.dadara;
 
 /**
  * @author $Author: saa $
- * @version $Revision: 1.2 $, $Date: 2005/02/15 06:51:48 $
+ * @version $Revision: 1.3 $, $Date: 2005/03/09 11:21:43 $
  * @module
  */
 public class ModelTraceImplMF extends ModelTrace
 {
 	private int length;
 	private ModelFunction mf;
+	private double[] cachedTrace = null;
 
 	public int getLength()
 	{
@@ -24,9 +25,17 @@ public class ModelTraceImplMF extends ModelTrace
 
 	public double[] getYArray(int x0, int N)
 	{
-//		Throwable st = new Throwable("getYArray: stack trace:");
-//		st.printStackTrace();
-		return mf.funFillArray(x0, 1.0, N);
+		//return mf.funFillArray(x0, 1.0, N);
+		if (x0 >= 0 && x0 + N <= length && cachedTrace != null)
+		{
+			double[] out = new double[N];
+			System.arraycopy(cachedTrace, x0, out, 0, N);
+			return out;
+		}
+		else
+		{
+			return mf.funFillArray(x0, 1.0, N);
+		}
 	}
 
 	public double getY(int x)
@@ -34,9 +43,17 @@ public class ModelTraceImplMF extends ModelTrace
 		return mf.fun(x);
 	}
 
+	/**
+	 * Creates ModelTrace based on ModelFunction object (wrapper).
+	 * Assumes input ModelFunction object be not modified anywhere else.
+	 * May perform caching.
+	 * @param mf ModelFunction object to be wrapped
+	 * @param length trace length; x will be allowed to range from 0 to length-1
+	 */
 	public ModelTraceImplMF(ModelFunction mf, int length)
 	{
 		this.mf = mf;
 		this.length = length;
+		this.cachedTrace = mf.funFillArray(0, 1.0, length);
 	}
 }
