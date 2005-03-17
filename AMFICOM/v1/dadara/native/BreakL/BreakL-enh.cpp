164ea8e63fc9f4feb4c11eabd5b6d62ff5f1b486
Alexey Stratonnikov <alexsaa@gmail.com>
2005-03-17 18:47:35 +0300

    MINOR: ChangeByThresh() reorganized. The target is to get ready to track the data necessary for automatic Thresh generation

--- AMFICOM/v1/dadara/native/BreakL/BreakL-enh.cpp
+++ AMFICOM/v1/dadara/native/BreakL/BreakL-enh.cpp
@@ -6,8 +6,57 @@
 #include "../common/ModelF.h"
 #include "../common/ArrList.h"
 
+inline int imax(int a, int b) { return a > b ? a : b; }
+inline int imin(int a, int b) { return a < b ? a : b; }
+
+// 1: умеем отслеживать перемещение DX&DY-порогов
+// 0: не умеем (при этом ничего не работает)
+// этот define нужен лишь для того, чтобы пометить те части кода, которые
+// отслеживают ttdx/ttdy.
+#define CANTTDXDY 1
+
+#if CANTTDXDY
+
+struct TTDX
+{
+	int thId; // номер порога либо -1 если порог не повлиял
+	void set(int thId)
+	{
+		this->thId = thId;
+	}
+	int get()
+	{
+		return thId;
+	}
+/*	void operator= (TTDX &that)
+	{
+		this->thId = that.thId;
+	}*/
+};
+
+struct TTDY
+{
+	int thId; // номер левого порога (-1: отчего-то не определено)
+	double nextWei; // вес соседа справа (должен быть строго 0 для самого правого порога)
+	void set(int thId, double nextWei)
+	{
+		this->thId = thId;
+		this->nextWei = nextWei;
+	}
+	int getNearest()
+	{
+		return nextWei > 0.5 ? thId + 1 : thId;
+	}
+/*	void operator= (TTDY &that)
+	{
+		this->thId = that.thId;
+		this->nextWei = that.nextWei;
+	}*/
+};
+#endif
+
 /////////////////////////////////////////////////
-// алгоритм enhance  в двух вариантах - без отслеживания и с отслеживанием
+// алгоритм enhance  в двух вариантах - без отслеживания TTDY и с отслеживанием TTDY
 
 inline void imaxeq(double &a, double b)
 {
@@ -31,7 +80,23 @@ static void enh0m(double *data, int size, int width)
 	for (i = size - width - 1; i >= 0; i--)
 	   imineq(data[i + width], data[i]);
 }
-inline void imaxeqtid(double &a, double b, int &aid, int bid)
+static void enhanceSimple(double *data, int size, int width, int isUpper)
+{
+	int i;
+	for (i = 1; i <= width; i = i * 2)
+	{
+		isUpper ? enh0p(data, size, i) : enh0m(data, size, i);
+		width -= i;
+	}
+	while (i)
+	{
+		if (i & width)
+			isUpper ? enh0p(data, size, i) : enh0m(data, size, i);
+		i /= 2;
+	}
+}
+#if CANTTDXDY
+inline void imaxeqtid(double &a, double b, TTDY &aid, TTDY &bid)
 {
 	if (b > a)
 	{
@@ -39,7 +104,7 @@ inline void imaxeqtid(double &a, double b, int &aid, int bid)
 		aid = bid;
 	}
 }
-inline void imineqtid(double &a, double b, int &aid, int bid)
+inline void imineqtid(double &a, double b, TTDY &aid, TTDY &bid)
 {
 	if (b < a)
 	{
@@ -47,19 +112,19 @@ inline void imineqtid(double &a, double b, int &aid, int bid)
 		aid = bid;
 	}
 }
-static void enh0ptid(double *data, int size, int width, int *id)
+static void enh0ptid(double *data, int size, int width, TTDY *id)
 {
 	int i;
 	for (i = size - width - 1; i >= 0; i--)
 	   imaxeqtid(data[i + width], data[i], id[i + width], id[i]);
 }
-static void enh0mtid(double *data, int size, int width, int *id)
+static void enh0mtid(double *data, int size, int width, TTDY *id)
 {
 	int i;
 	for (i = size - width - 1; i >= 0; i--)
 	   imineqtid(data[i + width], data[i], id[i + width], id[i]);
 }
-static void enhance(double *data, int size, int width, int isUpper, int *id = 0)
+static void enhanceWithTTDY(double *data, int size, int width, int isUpper, TTDY *id = 0)
 {
 	int i;
 	for (i = 1; i <= width; i = i * 2)
@@ -82,6 +147,7 @@ static void enhance(double *data, int size, int width, int isUpper, int *id = 0)
 		i /= 2;
 	}
 }
+#endif
 /////////////////////////////////////////////////
 
 // "размазывает" (для образования порога) BreakL
@@ -133,7 +199,7 @@ void BreakL_Enh (ModelF &mf, int x0, int x1, int width, int isUpper)
 	}
 
 	// формируем порог
-	enhance(tmp, len, width, isUpper);
+	enhanceWithTTDY(tmp, len, width, isUpper);
 
 	// преобразуем к ломаной
 	BreakL_FitI (mf, tmp, width, x0, len - width);
@@ -192,19 +258,15 @@ struct THY
 	int typeL;
 };
 
-// 1: умеем отслеживать перемещение DY-порогов (следовательно)
-// 0: не умеем
-#define CANTTDY 1
-
 struct UPDATE_REGION
 {
 	int x0;
 	int N;
 	int dxthid;
 	double *data;
-#if CANTTDY
-	int *tid;
-	UPDATE_REGION(int x0, int N, int dxthid, double *data, int *tid)
+#if CANTTDXDY
+	TTDY *tid;
+	UPDATE_REGION(int x0, int N, int dxthid, double *data, TTDY *tid)
 	{
 		this->x0 = x0;
 		this->N = N;
@@ -232,17 +294,36 @@ static int isat(int x, int xMin, int xMax)
 	return x;
 }
 
-// Основное назначение функции соответствует названию - преобразовать м.ф. к ее порогу.
-// Вспомогательное назначение - поиск соответствующего координате DX/DY-порога thCheckPosition.
-// thCheckPosition: x-координата, для которой надо провести поиск ответственного DX/DY-порога
-// wannaDXDY: 0: ничего не надо, 1: нужен DX-порог, 2: нужен DY-порог
-// возвращаемое значение будет либо -1 (не запрошен либо не найден), либо найденным номером в массиве taX или taY
-int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, int key, int thCheckPosition, int wannaDXDY)
+//autoThresh - режим при автоматической генерации порогов (0,1)
+// ttdyOut - null если не нужен,
+// ttdxOut - null если не нужен
+// NB: упадёт при autoThresh != 0 && ttdyOut == 0
+static void ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, int key,
+						   int xMin, int xMax, int autoThresh,
+						   TTDY *ttdyOut, TTDX *ttdxOut)
 {
 	prf_b("BreakL_ChangeByThresh: entered");
 
-#if CANTTDY
-	int needTTDY = wannaDXDY == 2; // need to track ThreshDY
+	int Nx = xMax - xMin + 1;
+	int j;
+
+#if CANTTDXDY
+	// режим отслеживания TTDY.
+	// 0: не требуется. Обычная генерация порогов (со сглаживанием)
+	// 1: требуется выбора порога при захвате мышью в GUI. Обычная генерация порогов (со сглаживанием)
+	// 2: требуется таблица весов при автоматической генерации порогов. генерация порогов без сглаживания.
+	// Сглаживание может делать пороги чуть шире, но делает невозможным представление весов в виде TTDY.
+	// Отключение сглаживания (режим 2) упрощает веса до TTDY, тем самым позволяя простой алгоритм расставления порогов
+
+	// результирующие TTDY полностью заполнятся при наложении AL-порогов, сейчас инициализировать не надо
+	
+	// результирующие TTDX надо сначала инициализировать
+	if (ttdxOut)
+	{
+		for (j = 0; j < Nx; j++)
+			ttdxOut[j].set(-1);
+	}
+
 #endif
 
 	const int thNpX = taX.getLength();
@@ -264,7 +345,6 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 		thY = new THY[thNpY];
 		assert(thY);
 	}
-	int j;
 	for (j = 0; j < thNpX; j++)
 	{
 		int isRising = taX.getIsRise(j);
@@ -284,20 +364,7 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 	}
 
 	prf_b("BreakL_ChangeByThresh: unpacking BreakL");
-	// FIXME: предполагаем, что начальный и конечный узлы mf соответствуют длине рефлектограммы
-	int xMin = (int )mf.getP()[0];
-	int xMax = (int )mf.getP()[mf.getNPars() - 2];
-	int Nx = xMax - xMin + 1;
 	double *unpk = BreakLToArray(mf.getP(), mf.getNPars(), xMin, Nx);
-
-#if CANTTDY
-	int *TTDY = 0;
-	if (needTTDY)
-	{
-		TTDY = new int[Nx];
-		assert(TTDY);
-	}
-#endif
 
 	prf_b("BreakL_ChangeByThresh: process AL threshs");
 
@@ -332,9 +399,9 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 			// пространство внутри порога
 			if (curX <= thY[curLTn].x1 || curLTn == thNpY - 1)
 			{
-#if CANTTDY
-				if (needTTDY)
-					TTDY[i] = curLTn;
+#if CANTTDXDY
+				if (ttdyOut)
+					ttdyOut[i].set(curLTn, 0);
 #endif
 				unpk[i] += thY[curLTn].dy;
 				continue;
@@ -365,16 +432,15 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 			//	curLTn, curMn, curX, xL, xR, dyL, dyR, typeL, typeR, wei);
 
 			unpk[i] += dyL + (dyR - dyL) * wei;
-#if CANTTDY
-			if (needTTDY)
-				TTDY[i] = wei > .5 ? curLTn + 1 : curLTn;
+#if CANTTDXDY
+			if (ttdyOut)
+				//TTDY[i] = wei > .5 ? curLTn + 1 : curLTn;
+				ttdyOut[i].set(curLTn, wei);
 #endif
 		}
 	}
 
 	prf_b("BreakL_ChangeByThresh: process DXLR threshs");
-
-	int rcID = -1;
 
 	// apply dx-thresholds
 	{
@@ -386,10 +452,15 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 			int xEnd = thX[curT].x1;
 
 			// начальное значение DX-порога (без учета расширения)
-			if (wannaDXDY == 1 && xBegin <= thCheckPosition && xEnd >= thCheckPosition)
+#if CANTTDXDY
+			if (ttdxOut)
 			{
-				rcID = curT;
+				int jMin = imax(xBegin, xMin);
+				int jMax = imin(xEnd, xMax);
+				for (j = jMin; j <= jMax; j++)
+					ttdxOut[j - xMin].set(curT);
 			}
+#endif
 
 			int dxL = thX[curT].dxL;
 			int dxR = thX[curT].dxR;
@@ -421,11 +492,11 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 			// XXX: надо ли копировать?
 			double *arr2io = new double[nEnh2];
 			assert(arr2io);
-#if CANTTDY
-			int *tid2io = 0;
-			if (needTTDY)
+#if CANTTDXDY
+			TTDY *tid2io = 0;
+			if (ttdyOut)
 			{
-				tid2io = new int[nEnh2];
+				tid2io = new TTDY[nEnh2];
 				assert(tid2io);
 			}
 #endif
@@ -435,25 +506,25 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 				for (j = 0; j + xL < xMin; j++)
 				{
 					arr2io[j] = unpk[0];
-#if CANTTDY
-					if (needTTDY)
-						tid2io[j] = TTDY[0];
+#if CANTTDXDY
+					if (ttdyOut)
+						tid2io[j] = ttdyOut[0];
 #endif
 				}
 				for (; j + xL < xR && j + xL <= xMax; j++)
 				{
 					arr2io[j] = unpk[j + xL - xMin];
-#if CANTTDY
-					if (needTTDY)
-						tid2io[j] = TTDY[j + xL - xMin];
+#if CANTTDXDY
+					if (ttdyOut)
+						tid2io[j] = ttdyOut[j + xL - xMin];
 #endif
 				}
 				for (; j + xL < xR; j++)
 				{
 					arr2io[j] = unpk[xMax - xMin - 1];
-#if CANTTDY
-					if (needTTDY)
-						tid2io[j] = TTDY[xMax - xMin - 1];
+#if CANTTDXDY
+					if (ttdyOut)
+						tid2io[j] = ttdyOut[xMax - xMin - 1];
 #endif
 				}
 			}
@@ -461,35 +532,38 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 			// расширяем nBase0->nEnh2, забивая справа гориз. прямой; исходный вар-т не меняем
 			double *arr1t = new double[nEnh1];
 			assert(arr1t);
-#if CANTTDY
-			int *tid1t = 0;
-			if (needTTDY)
+#if CANTTDXDY
+			TTDY *tid1t = 0;
+			if (ttdyOut)
 			{
-				tid1t = new int[nEnh1];
+				tid1t = new TTDY[nEnh1];
 				assert(tid1t);
 			}
 #endif
 			for (j = 0; j < nBase0; j++)
 			{
 				arr1t[j] = arr2io[j + dxL * 2];
-#if CANTTDY
-				if (needTTDY)
+#if CANTTDXDY
+				if (ttdyOut)
 					tid1t[j] = tid2io[j + dxL * 2];
 #endif
 			}
 			for (j = nBase0; j < nEnh1; j++)
 			{
 				arr1t[j] = arr1t[nBase0 - 1];
-#if CANTTDY
-				if (needTTDY)
-					tid1t[j] = tid1t[nBase0 - 1];
+#if CANTTDXDY
+				if (ttdyOut)
+					tid1t[j] = tid2io[nBase0 - 1];
 #endif
 			}
 
 			// собственно расширяем
-			enhance(arr1t, nEnh1, W, isUpper, needTTDY ? tid1t : 0);
-
-			// сглаживаем стык
+#if CANTTDXDY
+			enhanceWithTTDY(arr1t, nEnh1, W, isUpper, ttdyOut ? tid1t : 0);
+#else
+			enhanceSimple(arr1t, nEnh1, W, isUpper);
+#endif
+			// переносим расширение из arr1t в arr2io; также сглаживаем стык
 			{
 				double sign = isUpper ? 1.0 : -1.0;
 				int leftMode = thX[curT].leftMode;
@@ -498,66 +572,72 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 				for (j = x0; j < x1; j++)
 				{
 					arr2io[dxL + j] = arr1t[j];
-#if CANTTDY
-					if (needTTDY)
+#if CANTTDXDY
+					if (ttdyOut)
 						tid2io[dxL + j] = tid1t[j];
 #endif
 				}
-				if (leftMode)
-				{
-					double y0 = arr1t[0];
-					double y1 = arr2io[0];
-					if (y1 * sign > y0 * sign) // XXX: в некоторых случаях лучше продолжить горизонтально до пересечения
-						y1 = y0;
-					for (j = 1; j < dxL; j++)
-					{
-						double yt = y0 + (y1 - y0) * (double )j / dxL;
-						if (arr2io[dxL - j] * sign < yt * sign)
-						{
-							arr2io[dxL - j] = yt;
-#if CANTTDY
-							if (needTTDY)
-								tid2io[dxL - j] = j > dxL * 0.5 ? tid2io[0] : tid1t[0];
+				// сглаживаем края, чтобы не было разрывов
+#if CANTTDXDY
+				if (!autoThresh) // в автоматическом режиме генерации порогов сглаживание не делаем
 #endif
+				{
+					if (leftMode)
+					{
+						double y0 = arr1t[0];
+						double y1 = arr2io[0];
+						if (y1 * sign > y0 * sign) // XXX: в некоторых случаях лучше продолжить горизонтально до пересечения
+							y1 = y0;
+						for (j = 1; j < dxL; j++)
+						{
+							double yt = y0 + (y1 - y0) * (double )j / dxL;
+							if (arr2io[dxL - j] * sign < yt * sign)
+							{
+								arr2io[dxL - j] = yt;
+#if CANTTDXDY
+								if (ttdyOut)
+									tid2io[dxL - j] = j > dxL * 0.5 ? tid2io[0] : tid1t[0];
+#endif
+							}
+							else
+								break;
 						}
-						else
-							break;
 					}
-				}
-				else
-				{
-					double y0 = arr1t[nEnh1 - 1];
-					double y1 = arr2io[nEnh2 - 1];
-					if (y1 * sign > y0 * sign) // XXX
-						y1 = y0;
-					for (j = 1; j < dxR; j++)
+					else
 					{
-						double yt = y0 + (y1 - y0) * (double )j / dxR;
-						if (arr2io[nEnh2 - 1 - dxR + j] * sign < yt * sign)
+						double y0 = arr1t[nEnh1 - 1];
+						double y1 = arr2io[nEnh2 - 1];
+						if (y1 * sign > y0 * sign) // XXX
+							y1 = y0;
+						for (j = 1; j < dxR; j++)
 						{
-							arr2io[nEnh2 - 1 - dxR + j] = yt;
-#if CANTTDY
-							if (needTTDY)
-								tid2io[nEnh2 - 1 - dxR + j] = j > dxR * 0.5 ? tid2io[nEnh2 - 1] : tid1t[nEnh1 - 1];
+							double yt = y0 + (y1 - y0) * (double )j / dxR;
+							if (arr2io[nEnh2 - 1 - dxR + j] * sign < yt * sign)
+							{
+								arr2io[nEnh2 - 1 - dxR + j] = yt;
+#if CANTTDXDY
+								if (ttdyOut)
+									tid2io[nEnh2 - 1 - dxR + j] = j > dxR * 0.5 ? tid2io[nEnh2 - 1] : tid1t[nEnh1 - 1];
 #endif
+							}
+							else
+								break;
 						}
-						else
-							break;
 					}
 				}
 			}
 
 			// планируем сохранение
-#if CANTTDY
-			UPDATE_REGION *ur = new UPDATE_REGION(xBegin - dxL * 2, nEnh2, curT, arr2io, needTTDY ? tid2io : 0);
+#if CANTTDXDY
+			UPDATE_REGION *ur = new UPDATE_REGION(xBegin - dxL * 2, nEnh2, curT, arr2io, ttdyOut ? tid2io : 0);
 #else
 			UPDATE_REGION *ur = new UPDATE_REGION(xBegin - dxL * 2, nEnh2, curT, arr2io);
 #endif
 			assert(ur);
 			updateRegions.add(ur);
 			delete[] arr1t;
-#if CANTTDY
-			if (needTTDY)
+#if CANTTDXDY
+			if (ttdyOut)
 				delete[] tid1t;
 #endif
 			//fprintf(stderr, "#6\n"); fflush(stderr);
@@ -582,33 +662,34 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 			int x0 = urp->x0;
 			int N = urp->N;
 			double *upd = urp->data;
-#if CANTTDY
-			int *updtid = urp->tid;
+#if CANTTDXDY
+			TTDY *updtid = urp->tid;
 #endif
-			int rc;
 			{
 				assert(x0 + N <= XRmax);
 				double *data = unpk - xMin + x0;
-#if CANTTDY
-				int *tids = needTTDY ? TTDY - xMin + x0 : 0;
+#if CANTTDXDY
+				// XXX: сами указатели могут указывать не-пойми-куда, а данные начнутся только с индекса iMin
+				// XXX: т.о., предполагается комутативность прибавления int к указателю: p+i+j==p+j+i
+				TTDY *tids = ttdyOut ? ttdyOut - xMin + x0 : 0;
+				TTDX *tidx = ttdxOut ? ttdxOut - xMin + x0 : 0;
 #endif
 				int i;
 				int iMin = x0 < xMin ? xMin - x0 : 0;
 				int iMax = x0 + N >= xMax + 1 ? xMax + 1 - x0 : N;
 
-				rc = 0;
 				if (isUpper)
 				{
 					for (i = iMin; i < iMax; i++)
 						if (upd[i] > data[i])
 						{
 							data[i] = upd[i];
-#if CANTTDY
-							if (needTTDY)
+#if CANTTDXDY
+							if (ttdyOut)
 								tids[i] = updtid[i];
+							if (ttdxOut)
+								tidx[i].set(urp->dxthid);
 #endif
-							if (i == thCheckPosition - x0)
-								rc = 1;
 						}
 				}
 				else
@@ -617,21 +698,18 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 						if (upd[i] < data[i])
 						{
 							data[i] = upd[i];
-#if CANTTDY
-							if (needTTDY)
+#if CANTTDXDY
+							if (ttdyOut)
 								tids[i] = updtid[i];
+							if (ttdxOut)
+								tidx[i].set(urp->dxthid);
 #endif
-							if (i == thCheckPosition - x0)
-								rc = 1;
 						}
 				}
-
 			}
-			if (rc != 0 && wannaDXDY == 1)
-				rcID = urp->dxthid;
 			delete[] urp->data;
-#if CANTTDY
-			if (needTTDY)
+#if CANTTDXDY
+			if (ttdyOut)
 				delete[] urp->tid;
 #endif
 			delete urp;
@@ -642,12 +720,6 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 	prf_b("BreakL_ChangeByThresh: committing thresholds: BreakLFromArray");
 	BreakLFromArray(mf, xMin, Nx, unpk);
 	delete[] unpk;
-#if CANTTDY
-	if (wannaDXDY == 2 && thCheckPosition >= xMin && thCheckPosition <= xMax)
-		rcID = TTDY[thCheckPosition];
-	if (needTTDY)
-		delete[] TTDY;
-#endif
 
 	prf_b("BreakL_ChangeByThresh: done");
 
@@ -657,6 +729,56 @@ int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, i
 		delete[] thX;
 	if (thNpY > 0)
 		delete[] thY;
-
-	return rcID;
 }
+
+#if CANTTDXDY
+// Основное назначение функции соответствует названию - преобразовать м.ф. к ее порогу.
+// Вспомогательное назначение - поиск соответствующего координате DX/DY-порога thCheckPosition.
+// thCheckPosition: x-координата, для которой надо провести поиск ответственного DX/DY-порога
+// wannaDXDY: 0: ничего не надо, 1: нужен DX-порог, 2: нужен DY-порог
+// возвращаемое значение будет либо -1 (не запрошен либо не найден), либо найденным номером в массиве taX или taY
+int BreakL_ChangeByThresh (ModelF &mf, ThreshDXArray &taX, ThreshDYArray &taY, int key, int thCheckPosition, int wannaDXDY)
+{
+	prf_b("Outer BreakL_ChangeByThresh: enter");
+
+	// FIXME: предполагаем, что начальный и конечный узлы mf соответствуют длине рефлектограммы
+	int xMin = (int )mf.getP()[0];
+	int xMax = (int )mf.getP()[mf.getNPars() - 2];
+	int Nx = xMax - xMin + 1;
+
+	TTDX *ttdxOut = 0;
+	TTDY *ttdyOut = 0;
+	if (wannaDXDY == 1)
+	{
+		ttdxOut = new TTDX[Nx];
+		assert(ttdxOut);
+	}
+	if (wannaDXDY == 2)
+	{
+		ttdyOut = new TTDY[Nx];
+		assert(ttdyOut);
+	}
+
+	prf_b("Outer BreakL_ChangeByThresh: go inside");
+	ChangeByThresh (mf, taX, taY, key, xMin, xMax, 0, ttdyOut, ttdxOut);
+	prf_b("Outer BreakL_ChangeByThresh: back from inside");
+
+	int ret = -1;
+	if (thCheckPosition >= xMin && thCheckPosition <= xMax)
+	{
+		if (wannaDXDY == 1)
+			ret = ttdxOut[thCheckPosition - xMin].get();
+		if (wannaDXDY == 2)
+			ret = ttdyOut[thCheckPosition - xMin].getNearest();
+	}
+	prf_b("Outer BreakL_ChangeByThresh: make ret code");
+
+	if (ttdxOut)
+		delete[] ttdxOut;
+	if (ttdyOut)
+		delete[] ttdyOut;
+
+	prf_b("Outer BreakL_ChangeByThresh: done");
+	return ret;
+}
+#endif
