c17ffb632bca4605f4feb5b7c733489a81987165
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-03-21 20:14:37 +0300

    commonly used resources key

---
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Model/AnalysisResourceKeys.java
@@ -0,0 +1,33 @@
+/*
+ * $Id: AnalysisResourceKeys.java,v 1.1 2005/03/21 17:14:37 bob Exp $
+ *
+ * Copyright ¿ 2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
+package com.syrus.AMFICOM.Client.General.Model;
+
+import com.syrus.AMFICOM.Client.Resource.ResourceKeys;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/03/21 17:14:37 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module analysis_v1
+ */
+public interface AnalysisResourceKeys extends ResourceKeys {
+
+	String	ICON_ANALYSIS_MINI				= "com.syrus.AMFICOM.icon.analisys.mainmini";
+	String	ICON_SURVEY_MINI				= "com.syrus.AMFICOM.icon.survey.mainmini";
+	String	ICON_EVALUATE_MINI				= "com.syrus.AMFICOM.icon.evaluation.mainmini";
+	String	ICON_ANALYSIS_CENTER_A			= "com.syrus.AMFICOM.icon.analisys.centerA";
+	String	ICON_ANALYSIS_CENTER_B			= "com.syrus.AMFICOM.icon.analisys.centerB";
+	String	ICON_ANALYSIS_LOSS				= "com.syrus.AMFICOM.icon.analisys.loss";
+	String	ICON_ANALYSIS_REFLECT			= "com.syrus.AMFICOM.icon.analisys.reflect";
+	String	ICON_ANALYSIS_NOANALYSIS		= "com.syrus.AMFICOM.icon.analisys.noanalysis";
+	String	ICON_ANALYSIS_PERFORM_ANALYSIS	= "com.syrus.AMFICOM.icon.analisys.perform";
+	String	ICON_ANALYSIS_INITIAL_ANALYSIS	= "com.syrus.AMFICOM.icon.analisys.initailAnalysis";
+	Object	ICON_ANALYSIS_DEFAULT_ANALYSIS	= "com.syrus.AMFICOM.icon.analisys.defaultAnalysis";
+
+}
