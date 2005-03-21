edf50105fd46a38b2c16b29388142c47c20cadad
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-03-21 20:05:08 +0300

    commonly used resources keys

---
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/Resource/ResourceKeys.java
@@ -0,0 +1,28 @@
+/*
+ * $Id: ResourceKeys.java,v 1.1 2005/03/21 17:05:08 bob Exp $
+ *
+ * Copyright © 2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
+package com.syrus.AMFICOM.Client.Resource;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/03/21 17:05:08 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module generalclient_v1
+ */
+public interface ResourceKeys {
+
+	String	SIMPLE_DATE_FORMAT	= "simpleDateFormat";
+
+	String	INSETS_NULL			= "insets.null";
+
+	String	ICON_OPEN_SESSION	= "com.syrus.AMFICOM.icon.general.opensession";
+	String	ICON_GENERAL		= "com.syrus.AMFICOM.icon.general";
+	String	ICON_OPEN_FILE	= "com.syrus.AMFICOM.icon.general.openfile";
+	String	ICON_ADD_FILE	= "com.syrus.AMFICOM.icon.general.addfile";
+	String	ICON_REMOVE_FILE	= "com.syrus.AMFICOM.icon.general.removefile";
+}
