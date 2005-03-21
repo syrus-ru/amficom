54fcbb0205d07b42f28438b00c23371dac28219a
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-03-21 11:41:01 +0300

    really service item

---
+++ AMFICOM/v1/filter/src/com/syrus/AMFICOM/logic/ServiceItem.java
@@ -0,0 +1,52 @@
+/*
+ * 
+ * Created on 19.03.2005 14:51:04
+ *
+ */
+package com.syrus.AMFICOM.logic;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ServiceItem extends AbstractItem {
+
+	private static int	serviceCount	= 0;
+	private String		name;
+	private Object		object;
+
+	public ServiceItem() {
+		this.name = "service" + (serviceCount++);
+	}
+
+	public int getMaxChildrenCount() {
+		return Integer.MAX_VALUE;
+	}
+
+	public boolean isParentAllow() {
+		return true;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public Object getObject() {
+		return this.object;
+	}
+
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+	public void removeChild(Item childItem) {
+		if (this.children != null) {
+			this.children.remove(childItem);
+		}
+
+	}
+
+	public boolean isService() {
+		return true;
+	}
+
+}
