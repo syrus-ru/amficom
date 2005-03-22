163f5ee2946dcd8b505cccea45802e6df6b7e8a2
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-03-22 13:42:11 +0300

    add table no focus border key

--- AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/Resource/ResourceKeys.java
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/Resource/ResourceKeys.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ResourceKeys.java,v 1.2 2005/03/22 09:34:53 bob Exp $
+ * $Id: ResourceKeys.java,v 1.3 2005/03/22 10:42:11 bob Exp $
  *
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,20 +9,22 @@
 package com.syrus.AMFICOM.Client.Resource;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/03/22 09:34:53 $
+ * @version $Revision: 1.3 $, $Date: 2005/03/22 10:42:11 $
  * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module generalclient_v1
  */
 public interface ResourceKeys {
 
-	String	SIMPLE_DATE_FORMAT	= "simpleDateFormat";
+	String	SIMPLE_DATE_FORMAT		= "simpleDateFormat";
 
-	String	INSETS_NULL			= "insets.null";
+	String	INSETS_NULL				= "insets.null";
 
-	String	ICON_OPEN_SESSION	= "com.syrus.AMFICOM.icon.general.opensession";
-	String	ICON_GENERAL		= "com.syrus.AMFICOM.icon.general";
-	String	ICON_OPEN_FILE		= "com.syrus.AMFICOM.icon.general.openfile";
-	String	ICON_ADD_FILE		= "com.syrus.AMFICOM.icon.general.addfile";
-	String	ICON_REMOVE_FILE	= "com.syrus.AMFICOM.icon.general.removefile";
+	String	TABLE_NO_FOCUS_BORDER	= "Table.nofocusBorder";
+
+	String	ICON_OPEN_SESSION		= "com.syrus.AMFICOM.icon.general.opensession";
+	String	ICON_GENERAL			= "com.syrus.AMFICOM.icon.general";
+	String	ICON_OPEN_FILE			= "com.syrus.AMFICOM.icon.general.openfile";
+	String	ICON_ADD_FILE			= "com.syrus.AMFICOM.icon.general.addfile";
+	String	ICON_REMOVE_FILE		= "com.syrus.AMFICOM.icon.general.removefile";
 }
