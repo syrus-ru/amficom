11c50cfb4da41ca0d04c981e2be78e1cb6d1f2cb
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-03-22 12:34:53 +0300

    COSMETICAL

--- AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/Resource/ResourceKeys.java
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/Resource/ResourceKeys.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ResourceKeys.java,v 1.1 2005/03/21 17:05:08 bob Exp $
+ * $Id: ResourceKeys.java,v 1.2 2005/03/22 09:34:53 bob Exp $
  *
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,7 +9,7 @@
 package com.syrus.AMFICOM.Client.Resource;
 
 /**
- * @version $Revision: 1.1 $, $Date: 2005/03/21 17:05:08 $
+ * @version $Revision: 1.2 $, $Date: 2005/03/22 09:34:53 $
  * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module generalclient_v1
@@ -22,7 +22,7 @@ public interface ResourceKeys {
 
 	String	ICON_OPEN_SESSION	= "com.syrus.AMFICOM.icon.general.opensession";
 	String	ICON_GENERAL		= "com.syrus.AMFICOM.icon.general";
-	String	ICON_OPEN_FILE	= "com.syrus.AMFICOM.icon.general.openfile";
-	String	ICON_ADD_FILE	= "com.syrus.AMFICOM.icon.general.addfile";
+	String	ICON_OPEN_FILE		= "com.syrus.AMFICOM.icon.general.openfile";
+	String	ICON_ADD_FILE		= "com.syrus.AMFICOM.icon.general.addfile";
 	String	ICON_REMOVE_FILE	= "com.syrus.AMFICOM.icon.general.removefile";
 }
