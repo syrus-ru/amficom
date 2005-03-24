4faf494d741be48057b895e7edab593a7919e78c
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-03-24 19:55:42 +0300

    MINOR: new constants added.

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ErrorMessages.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ErrorMessages.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ErrorMessages.java,v 1.5 2005/03/24 09:43:19 bass Exp $
+ * $Id: ErrorMessages.java,v 1.6 2005/03/24 16:55:42 bass Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -11,7 +11,7 @@ package com.syrus.AMFICOM.general;
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.5 $, $Date: 2005/03/24 09:43:19 $
+ * @version $Revision: 1.6 $, $Date: 2005/03/24 16:55:42 $
  * @module general_v1
  */
 public interface ErrorMessages {
@@ -21,7 +21,7 @@ public interface ErrorMessages {
 	String NON_VOID_EXPECTED = "both non-null and non-void identifier expected"; //$NON-NLS-1$
 	String CIRCULAR_DEPS_PROHIBITED = "circular dependencies not allowed"; //$NON-NLS-1$
 	String MULTIPLE_PARENTS_PROHIBITED = "multiple parents not allowed"; //$NON-NLS-1$
-	String HEADLESS_CHILD_PROHIBITED = "at least one parent required"; //$NON-NLS-1$
+	String PARENTLESS_CHILD_PROHIBITED = "exactly one parent required"; //$NON-NLS-1$
 	String COLLECTION_IS_A_SET = "logically, this collection must be a set; double-addition of elements not allowed"; //$NON-NLS-1$
 	String REMOVAL_OF_AN_ABSENT_PROHIBITED = "this collection is missing the element to be removed; removal of an absent member not allowed"; //$NON-NLS-1$
 	String OUT_OF_LIBRARY_HIERARCHY = "object doesn't belong to library hierarchy"; //$NON-NLS-1$
