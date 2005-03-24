c9ad08379c513b5ab5d1cb91672aaf20f38a8781
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-03-24 12:43:19 +0300

    MINOR: new constants added.

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ErrorMessages.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ErrorMessages.java
@@ -1,7 +1,7 @@
-/*
- * $Id: ErrorMessages.java,v 1.4 2005/03/23 14:59:34 bass Exp $
+/*-
+ * $Id: ErrorMessages.java,v 1.5 2005/03/24 09:43:19 bass Exp $
  *
- * Copyright ¿ 2004 Syrus Systems.
+ * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
  * Project: AMFICOM.
  */
@@ -11,17 +11,20 @@ package com.syrus.AMFICOM.general;
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.4 $, $Date: 2005/03/23 14:59:34 $
+ * @version $Revision: 1.5 $, $Date: 2005/03/24 09:43:19 $
  * @module general_v1
  */
 public interface ErrorMessages {
-	String OBJECT_NOT_INITIALIZED = "object hasn't been initialized yet"; //$NON-NLS-1$
+	String OBJECT_NOT_INITIALIZED = "object not initialized yet"; //$NON-NLS-1$
 	String NON_NULL_EXPECTED = "non-null value expected"; //$NON-NLS-1$
 	String NON_EMPTY_EXPECTED = "both non-null and non-empty string value expected"; //$NON-NLS-1$
 	String NON_VOID_EXPECTED = "both non-null and non-void identifier expected"; //$NON-NLS-1$
-	String CIRCULAR_DEPS_PROHIBITED = "circular dependencies are not allowed"; //$NON-NLS-1$
-	String MULTIPLE_PARENTS_PROHIBITED = "multiple parents are not allowed"; //$NON-NLS-1$
+	String CIRCULAR_DEPS_PROHIBITED = "circular dependencies not allowed"; //$NON-NLS-1$
+	String MULTIPLE_PARENTS_PROHIBITED = "multiple parents not allowed"; //$NON-NLS-1$
 	String HEADLESS_CHILD_PROHIBITED = "at least one parent required"; //$NON-NLS-1$
-	String COLLECTION_IS_A_SET = "logically, this collection must be a set; double-addition of elements is not allowed"; //$NON-NLS-1$
-	String REMOVAL_OF_AN_ABSENT_PROHIBITED = "this collection is missing the element to be removed; removal of an absent member is not allowed"; //$NON-NLS-1$
+	String COLLECTION_IS_A_SET = "logically, this collection must be a set; double-addition of elements not allowed"; //$NON-NLS-1$
+	String REMOVAL_OF_AN_ABSENT_PROHIBITED = "this collection is missing the element to be removed; removal of an absent member not allowed"; //$NON-NLS-1$
+	String OUT_OF_LIBRARY_HIERARCHY = "object doesn't belong to library hierarchy"; //$NON-NLS-1$
+	String UNSUPPORTED_CHILD_TYPE = "object doesn't realize any of interfaces supported within this library hierarchy"; //$NON-NLS-1$
+	String CHILDREN_PROHIBITED = "library hierarchy entry sterile"; //$NON-NLS-1$
 }
