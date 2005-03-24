4d413433a5dccb4691fec2ba8d085333b0076507
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-03-24 17:00:28 +0300

    FIX: due to contract changes

--- AMFICOM/v1/mapview/src/com/syrus/AMFICOM/mapview/LinkedIdsConditionImpl.java
+++ AMFICOM/v1/mapview/src/com/syrus/AMFICOM/mapview/LinkedIdsConditionImpl.java
@@ -1,5 +1,5 @@
 /**
- * $Id: LinkedIdsConditionImpl.java,v 1.1 2005/03/01 15:35:12 krupenn Exp $
+ * $Id: LinkedIdsConditionImpl.java,v 1.2 2005/03/24 14:00:28 bob Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -18,11 +18,12 @@ import com.syrus.AMFICOM.administration.Domain;
 import com.syrus.AMFICOM.administration.DomainMember;
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.IllegalObjectEntityException;
 import com.syrus.AMFICOM.general.ObjectEntities;
 
 /**
- * @version $Revision: 1.1 $
- * @author $Author: krupenn $
+ * @version $Revision: 1.2 $
+ * @author $Author: bob $
  * @module map_v1
  */
 class LinkedIdsConditionImpl extends com.syrus.AMFICOM.general.LinkedIdsCondition {
@@ -37,24 +38,29 @@ class LinkedIdsConditionImpl extends com.syrus.AMFICOM.general.LinkedIdsConditio
 		this.entityCode = entityCode;
 	}
 
-	private boolean checkDomain(DomainMember domainMember) throws ApplicationException {
-		Domain dmDomain = (Domain) AdministrationStorableObjectPool.getStorableObject(domainMember.getDomainId(), true);
-		boolean condition = false;
-		/* if linked ids is domain id */
-		for (Iterator it = this.linkedIds.iterator(); it.hasNext() && !condition;) {
-			Identifier id = (Identifier) it.next();
-			if (id.getMajor() == ObjectEntities.DOMAIN_ENTITY_CODE) {
-				Domain domain = (Domain) AdministrationStorableObjectPool.getStorableObject(id, true);
-				if (dmDomain.isChild(domain))
-					condition = true;
+	private boolean checkDomain(DomainMember domainMember) throws IllegalObjectEntityException {
+		boolean condition;
+		try {
+			Domain dmDomain = (Domain) AdministrationStorableObjectPool.getStorableObject(domainMember.getDomainId(), true);
+			condition = false;
+			/* if linked ids is domain id */
+			for (Iterator it = this.linkedIds.iterator(); it.hasNext() && !condition;) {
+				Identifier id = (Identifier) it.next();
+				if (id.getMajor() == ObjectEntities.DOMAIN_ENTITY_CODE) {
+					Domain domain = (Domain) AdministrationStorableObjectPool.getStorableObject(id, true);
+					if (dmDomain.isChild(domain))
+						condition = true;
+
+				}
 
 			}
-
+		} catch (ApplicationException e) {
+			throw new IllegalObjectEntityException(e.getMessage(), IllegalObjectEntityException.UNKNOWN_ENTITY_CODE);
 		}
 		return condition;
 	}
 
-	public boolean isConditionTrue(Object object) throws ApplicationException {
+	public boolean isConditionTrue(Object object) throws IllegalObjectEntityException {
 		boolean condition = false;
 		DomainMember domainMember;
 		switch (this.entityCode.shortValue()) {
@@ -70,14 +76,14 @@ class LinkedIdsConditionImpl extends com.syrus.AMFICOM.general.LinkedIdsConditio
 		return condition;
 	}
 
-	public void setEntityCode(Short entityCode) {
+	public void setEntityCode(Short entityCode) throws IllegalObjectEntityException {
 		switch (entityCode.shortValue()) {
 			case ObjectEntities.MAPVIEW_ENTITY_CODE:
 				this.entityCode = entityCode;
 				break;
 			default:
-				throw new UnsupportedOperationException("LinkedIdsConditionImpl.setEntityCode | entityCode "
-						+ ObjectEntities.codeToString(entityCode.shortValue()) + " is unknown for this condition");
+				throw new IllegalObjectEntityException("LinkedIdsConditionImpl.setEntityCode | entityCode "
+						+ ObjectEntities.codeToString(entityCode.shortValue()) + " is unknown for this condition", IllegalObjectEntityException.UNKNOWN_ENTITY_CODE);
 		}
 	}
 
