0347e30b03c8cdd0a1c31cbffaa1988778ddbab9
Alexey Stratonnikov <alexsaa@gmail.com>
2005-03-25 20:31:18 +0300

    BUGFIX: getDerivative() fixed. Now using new class 'Wavelet'

--- AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/ReflectogramMath.java
+++ AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/ReflectogramMath.java
@@ -1,5 +1,6 @@
 package com.syrus.AMFICOM.analysis.dadara;
 
+import com.syrus.AMFICOM.analysis.CoreAnalysisManager;
 import com.syrus.AMFICOM.analysis.dadara.MathRef;
 
 public class ReflectogramMath
@@ -326,37 +327,10 @@ public class ReflectogramMath
 		return (int)eventSize;
 	}
 
-	/*
-	public static double[] waveletTransform(double[] y, int freq, double[] norma, int wLet)
-	{
-		double[] trans = new double[y.length];
-		for(int i = 0; i < y.length; i++)
-		{
-			double tmp = 0.;
-			for(int j = Math.max(i - freq, 0); j < Math.min(i + freq + 1, y.length); j++)
-				tmp = tmp + y[j] * wLet(j - i, freq, norma, wLet);
-			trans[i] = tmp;
-		}
-		return trans;
-	}
-	*/
-	public static double[] waveletTransform(double[] y, int freq, double norma, int wLet, int start, int end)
-	{
-		double[] trans = new double[end - start];
-		for(int i = start; i < end; i++)
-		{
-			double tmp = 0.;
-			for(int j = Math.max(i - freq, 0); j < Math.min(i + freq + 1, end); j++)
-				tmp = tmp + y[j] * wLet(j - i, freq, norma, wLet);
-			trans[i] = tmp;
-		}
-		return trans;
-	}
-
 	public static double[] getDerivative(double[] y, int freq, int wLet)
 	{
-		double norma = getWLetNorma(freq, wLet);
-		return waveletTransform(y, freq, norma, wLet, 0, y.length);
+		double norma = Wavelet.getNormMx(wLet, freq);
+		return Wavelet.makeTransform(wLet, freq, y, 0, y.length - 1, norma);
 	}
 
 	public static double getMaximalDifference(double[] etalon, double[] data, int begin, int end)
@@ -368,183 +342,6 @@ public class ReflectogramMath
 				shift = data[i] - etalon[i];
 
 		return shift;
-	}
-	/*
-	public static double[] getWLetNorma(int freq)
-	{
-		double[] n = new double[10];
-		for (int i = 0; i < 10; i++)
-			n[i] = 0;
-
-		for(int i = -freq; i <= freq; i++)
-		{
-			n[0] = n[0] + Math.abs(wLet1(i, freq, 1.));
-			n[1] = n[1] + Math.abs(wLet2(i, freq, 1.));
-			n[2] = n[2] + Math.abs(wLet3(i, freq, 1.));
-			n[3] = n[3] + Math.abs(wLet4(i, freq, 1.));
-			n[4] = n[4] + Math.abs(wLet5(i, freq, 1.));
-
-			n[5] = n[5] + Math.abs(wLet6(i, freq, 1.));
-			n[6] = n[6] + Math.abs(wLet7(i, freq, 1.));
-			n[7] = n[7] + Math.abs(wLet8(i, freq, 1.));
-			n[8] = n[8] + Math.abs(wLet9(i, freq, 1.));
-			n[9] = n[9] + Math.abs(wLet10(i, freq, 1.));
-		}
-		for (int i = 0; i < 10; i++)
-			n[i] /= 2.;
-
-		return n;
-	}
-	*/
-
-	static double getWLetNorma(int freq, int waveletType)
-	{
-		double n = 0;
-		switch (waveletType)
-		{
-			case  0:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet1(i, freq, 1.));
-				break;
-			case  1:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet2(i, freq, 1.));
-				break;
-			case  2:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet3(i, freq, 1.));
-				break;
-			case  3:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet4(i, freq, 1.));
-				break;
-			case  4:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet5(i, freq, 1.));
-				break;
-			case  5:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet6(i, freq, 1.));
-				break;
-			case  6:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet7(i, freq, 1.));
-				break;
-			case  7:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet8(i, freq, 1.));
-				break;
-			case  8:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet9(i, freq, 1.));
-				break;
-			case  9:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet10(i, freq, 1.));
-				break;
-			default:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet1(i, freq, 1.));
-				break;
-		}
-		n /= 2.;
-		return n;
-	}
-
-	/*
-	public static double wLet(int arg, int freq, double[] norma, int waveletType)
-	{
-		switch (waveletType)
-		{
-			case  0: return  wLet1(arg, freq, norma[0]);
-			case  1: return  wLet2(arg, freq, norma[1]);
-			case  2: return  wLet3(arg, freq, norma[2]);
-			case  3: return  wLet4(arg, freq, norma[3]);
-			case  4: return  wLet5(arg, freq, norma[4]);
-			case  5: return  wLet6(arg, freq, norma[5]);
-			case  6: return  wLet7(arg, freq, norma[6]);
-			case  7: return  wLet8(arg, freq, norma[7]);
-			case  8: return  wLet9(arg, freq, norma[8]);
-			case  9: return wLet10(arg, freq, norma[9]);
-			default: return  wLet1(arg, freq, norma[0]);
-		}
-	}
-	*/
-
-	public static double wLet(int arg, int freq, double norma, int waveletType)
-	{
-		switch (waveletType)
-		{
-			case  0: return  wLet1(arg, freq, norma);
-			case  1: return  wLet2(arg, freq, norma);
-			case  2: return  wLet3(arg, freq, norma);
-			case  3: return  wLet4(arg, freq, norma);
-			case  4: return  wLet5(arg, freq, norma);
-			case  5: return  wLet6(arg, freq, norma);
-			case  6: return  wLet7(arg, freq, norma);
-			case  7: return  wLet8(arg, freq, norma);
-			case  8: return  wLet9(arg, freq, norma);
-			case  9: return wLet10(arg, freq, norma);
-			default: return  wLet1(arg, freq, norma);
-		}
-	}
-
-	static double wLet1(int arg, int freq, double norma)
-	{
-		return (Math.sin(arg * Math.PI / freq)) / norma;
-	}
-
-	static double wLet2(int arg, int freq, double norma)
-	{
-		return (Math.sin(arg * Math.PI / freq) * Math.abs((double)arg)) / norma;
-	}
-
-	static double wLet3(int arg, int freq, double norma)
-	{
-		return (Math.sin(arg * Math.PI / freq) / (1. + Math.abs((double)arg))) / norma;
-	}
-
-	static double wLet4(int arg, int freq, double norma)
-	{
-		return (Math.sin(arg * Math.PI / freq) / (1. + Math.sqrt(Math.abs((double)arg)))) / norma;
-	}
-
-	static double wLet5(int arg, int freq, double norma)
-	{
-		if(arg < 0)
-			return -1. / norma;
-		if(arg > 0)
-			return 1. / norma;
-		return 0.;
-	}
-
-	static double wLet6(int arg, int freq, double norma)
-	{
-		return (((double)arg) / freq) / norma;
-	}
-
-	static double wLet7(int arg, int freq, double norma)
-	{
-		if(arg > 0)
-			return ((double)(freq - arg)) / norma;
-		if(arg < 0)
-			return -((double)(freq + arg)) / norma;
-		return 0.;
-	}
-
-	static double wLet8(int arg, int freq, double norma)
-	{
-		return (Math.sin(arg * Math.PI / freq) + ((double)arg) / freq / 2.) / norma;
-	}
-
-	static double wLet9(int arg, int freq, double norma)
-	{
-		return (Math.sin(arg * Math.PI / freq / 2.))/norma;
-	}
-
-	static double wLet10(int arg, int freq, double norma)
-	{
-		return (Math.sin(arg * Math.PI / freq / 2.) - ((double)arg) / freq / 2.) / norma;
 	}
 }
 
---
+++ AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/Wavelet.java
@@ -0,0 +1,196 @@
+/*-
+ * $Id: Wavelet.java,v 1.1 2005/03/25 17:31:18 saa Exp $
+ * 
+ * Copyright © 2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
+package com.syrus.AMFICOM.analysis.dadara;
+
+/**
+ * Класс предоставляет самый минимальный набор функций работы с вейвлетами,
+ * которые нужны в GUI анализа. Введен для того, чтобы можно было заменить
+ * медленный Java-код на native-вызов.
+ * @author $Author: saa $
+ * @version $Revision: 1.1 $, $Date: 2005/03/25 17:31:18 $
+ * @module
+ */
+public class Wavelet
+{
+	public static final int TYPE_SINX = 0;
+	public static final int TYPE_ABSXSINX = 1;
+	public static double getNormStep(int type, int scale)
+	{
+		return getWLetNorma(scale, type);
+	}
+	public static double getNormMx(int type, int scale)
+	{
+		return getWLetNormMx(scale, type);
+	}
+	public static double[] makeTransform(int type, int scale, double[] input, int iFrom, int iTo, double norm)
+	{
+		return waveletTransform(input, scale, norm, type, iFrom, iTo + 1);
+	}
+
+	private static double[] waveletTransform(double[] y, int freq, double norma, int wLet, int start, int end)
+	{
+		double[] trans = new double[end - start];
+		for(int i = start; i < end; i++)
+		{
+			double tmp = 0.;
+			for(int j = Math.max(i - freq, 0); j < Math.min(i + freq + 1, end); j++)
+				tmp = tmp + y[j] * wLet(j - i, freq, norma, wLet);
+			trans[i] = tmp;
+		}
+		return trans;
+	}
+
+	private static double getWLetNorma(int freq, int waveletType)
+	{
+		double n = 0;
+		switch (waveletType)
+		{
+			case  0:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet1(i, freq, 1.));
+				break;
+			case  1:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet2(i, freq, 1.));
+				break;
+			case  2:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet3(i, freq, 1.));
+				break;
+			case  3:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet4(i, freq, 1.));
+				break;
+			case  4:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet5(i, freq, 1.));
+				break;
+			case  5:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet6(i, freq, 1.));
+				break;
+			case  6:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet7(i, freq, 1.));
+				break;
+			case  7:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet8(i, freq, 1.));
+				break;
+			case  8:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet9(i, freq, 1.));
+				break;
+			case  9:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet10(i, freq, 1.));
+				break;
+			default:
+				for(int i = -freq; i <= freq; i++)
+					n = n + Math.abs(wLet1(i, freq, 1.));
+				break;
+		}
+		n /= 2.;
+		return n;
+	}
+
+	private static double getWLetNormMx(int freq, int waveletType)
+	{
+		double sum = 0;
+		switch (waveletType)
+		{
+			case  0:
+				for(int i = -freq; i <= freq; i++)
+					sum = sum + i * wLet1(i, freq, 1.);
+				break;
+			case  1:
+				for(int i = -freq; i <= freq; i++)
+					sum = sum + i * wLet2(i, freq, 1.);
+				break;
+		}
+		return sum;
+	}
+
+	private static double wLet(int arg, int freq, double norma, int waveletType)
+	{
+		switch (waveletType)
+		{
+			case  0: return  wLet1(arg, freq, norma);
+			case  1: return  wLet2(arg, freq, norma);
+			case  2: return  wLet3(arg, freq, norma);
+			case  3: return  wLet4(arg, freq, norma);
+			case  4: return  wLet5(arg, freq, norma);
+			case  5: return  wLet6(arg, freq, norma);
+			case  6: return  wLet7(arg, freq, norma);
+			case  7: return  wLet8(arg, freq, norma);
+			case  8: return  wLet9(arg, freq, norma);
+			case  9: return wLet10(arg, freq, norma);
+			default: return  wLet1(arg, freq, norma);
+		}
+	}
+
+	private static double wLet1(int arg, int freq, double norma)
+	{
+		return (Math.sin(arg * Math.PI / freq)) / norma;
+	}
+
+	private static double wLet2(int arg, int freq, double norma)
+	{
+		return (Math.sin(arg * Math.PI / freq) * Math.abs((double)arg)) / norma;
+	}
+
+	private static double wLet3(int arg, int freq, double norma)
+	{
+		return (Math.sin(arg * Math.PI / freq) / (1. + Math.abs((double)arg))) / norma;
+	}
+
+	private static double wLet4(int arg, int freq, double norma)
+	{
+		return (Math.sin(arg * Math.PI / freq) / (1. + Math.sqrt(Math.abs((double)arg)))) / norma;
+	}
+
+	private static double wLet5(int arg, int freq, double norma)
+	{
+		if(arg < 0)
+			return -1. / norma;
+		if(arg > 0)
+			return 1. / norma;
+		return 0.;
+	}
+
+	private static double wLet6(int arg, int freq, double norma)
+	{
+		return (((double)arg) / freq) / norma;
+	}
+
+	private static double wLet7(int arg, int freq, double norma)
+	{
+		if(arg > 0)
+			return ((double)(freq - arg)) / norma;
+		if(arg < 0)
+			return -((double)(freq + arg)) / norma;
+		return 0.;
+	}
+
+	private static double wLet8(int arg, int freq, double norma)
+	{
+		return (Math.sin(arg * Math.PI / freq) + ((double)arg) / freq / 2.) / norma;
+	}
+
+	private static double wLet9(int arg, int freq, double norma)
+	{
+		return (Math.sin(arg * Math.PI / freq / 2.))/norma;
+	}
+
+	private static double wLet10(int arg, int freq, double norma)
+	{
+		return (Math.sin(arg * Math.PI / freq / 2.) - ((double)arg) / freq / 2.) / norma;
+	}
+}
+
