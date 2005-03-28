5d63fe5cc71f915650ffeeeecd3ac7402316111b
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-03-28 11:47:06 +0400

    FIX: header comment

--- AMFICOM/v1/filter/src/com/syrus/AMFICOM/logic/Populatable.java
+++ AMFICOM/v1/filter/src/com/syrus/AMFICOM/logic/Populatable.java
@@ -1,11 +1,18 @@
 /*
- * 
- * Created on 27.03.2005 19:07:39
+ * $Id: Populatable.java,v 1.2 2005/03/28 07:47:06 bob Exp $
  *
+ * Copyright ¿ 2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
  */
 package com.syrus.AMFICOM.logic;
 
-
+/**
+ * @version $Revision: 1.2 $, $Date: 2005/03/28 07:47:06 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module filter_v1
+ */
 public interface Populatable {
 
 	void populate();
