82db2815eaaf7f6a35949e1411771e2d031f9fda
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-03-28 11:42:34 +0400

    ADD: populating routines

---
+++ AMFICOM/v1/filter/src/com/syrus/AMFICOM/logic/ChildrenFactory.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ * Created on 27.03.2005 19:10:45
+ *
+ */
+package com.syrus.AMFICOM.logic;
+
+
+public interface ChildrenFactory {
+
+	void populate(Item item);
+
+}
---
+++ AMFICOM/v1/filter/src/com/syrus/AMFICOM/logic/Populatable.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ * Created on 27.03.2005 19:07:39
+ *
+ */
+package com.syrus.AMFICOM.logic;
+
+
+public interface Populatable {
+
+	void populate();
+	
+}
