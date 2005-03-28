4d9cbef8d2fade72167e03ecf65287ce2a45b1a5
Alexey Stratonnikov <alexsaa@gmail.com>
2005-03-28 15:34:37 +0400

    MINOR: removed some outdated code

--- AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/Wavelet.java
+++ AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/Wavelet.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: Wavelet.java,v 1.2 2005/03/28 08:55:11 saa Exp $
+ * $Id: Wavelet.java,v 1.3 2005/03/28 11:34:37 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -13,28 +13,24 @@ package com.syrus.AMFICOM.analysis.dadara;
  * которые нужны в GUI анализа. ¬веден дл€ того, чтобы можно было заменить
  * медленный Java-код на native-вызов.
  * @author $Author: saa $
- * @version $Revision: 1.2 $, $Date: 2005/03/28 08:55:11 $
+ * @version $Revision: 1.3 $, $Date: 2005/03/28 11:34:37 $
  * @module
  */
 public class Wavelet
 {
-	// native declarations
-
-	private static native double[] nMakeTransform(int type, int scale, double[] input, int iFrom, int iTo, double norm);
-	private static native double nGetNormStep(int type, int scale);
-	private static native double nGetNormMx(int type, int scale);
-
-	// export declarations
-
 	public static final int TYPE_SINX = 0;
 	public static final int TYPE_ABSXSINX = 1;
+
+	private static native double[] nMakeTransform(int type, int scale, double[] input, int iFrom, int iTo, double norm);
+	private static native double nGetNormStep(int type, int scale); 
+	private static native double nGetNormMx(int type, int scale);
+
 	public static double getNormStep(int type, int scale)
 	{
-		return getWLetNorma(scale, type);
+		return nGetNormStep(type, scale);
 	}
 	public static double getNormMx(int type, int scale)
 	{
-		//return getWLetNormMx(scale, type);
 		return nGetNormMx(type, scale);
 	}
 	public static double[] makeTransform(int type, int scale, double[] input, int iFrom, int iTo, double norm)
@@ -47,92 +43,8 @@ public class Wavelet
 	{ // empty
 	}
 
-	// the code that should be removed
-
-	private static double[] waveletTransform(double[] y, int freq, double norma, int wLet, int start, int end)
-	{
-		double[] trans = new double[end - start];
-		for(int i = start; i < end; i++)
-		{
-			double tmp = 0.;
-			for(int j = Math.max(i - freq, 0); j < Math.min(i + freq + 1, end); j++)
-				tmp = tmp + y[j] * wLet(j - i, freq, norma, wLet);
-			trans[i] = tmp;
-		}
-		return trans;
-	}
-
-	private static double getWLetNorma(int freq, int waveletType)
-	{
-		double n = 0;
-		switch (waveletType)
-		{
-			case  0:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet1(i, freq, 1.));
-				break;
-			case  1:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet2(i, freq, 1.));
-				break;
-			case  2:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet3(i, freq, 1.));
-				break;
-			case  3:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet4(i, freq, 1.));
-				break;
-			case  4:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet5(i, freq, 1.));
-				break;
-			case  5:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet6(i, freq, 1.));
-				break;
-			case  6:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet7(i, freq, 1.));
-				break;
-			case  7:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet8(i, freq, 1.));
-				break;
-			case  8:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet9(i, freq, 1.));
-				break;
-			case  9:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet10(i, freq, 1.));
-				break;
-			default:
-				for(int i = -freq; i <= freq; i++)
-					n = n + Math.abs(wLet1(i, freq, 1.));
-				break;
-		}
-		n /= 2.;
-		return n;
-	}
-
-	private static double getWLetNormMx(int freq, int waveletType)
-	{
-		double sum = 0;
-		switch (waveletType)
-		{
-			case  0:
-				for(int i = -freq; i <= freq; i++)
-					sum = sum + i * wLet1(i, freq, 1.);
-				break;
-			case  1:
-				for(int i = -freq; i <= freq; i++)
-					sum = sum + i * wLet2(i, freq, 1.);
-				break;
-		}
-		return sum;
-	}
-
+	// outdated code that should be removed
+/*
 	private static double wLet(int arg, int freq, double norma, int waveletType)
 	{
 		switch (waveletType)
@@ -208,5 +120,5 @@ public class Wavelet
 	{
 		return (Math.sin(arg * Math.PI / freq / 2.) - ((double)arg) / freq / 2.) / norma;
 	}
+*/
 }
-
