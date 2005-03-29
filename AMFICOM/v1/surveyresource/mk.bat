d945200074e8281ae7ad1801239752e332e5d1df
cvs-fast-export <cvs-fast-export>
2005-03-29 09:05:21 +0000

    Synthetic commit for incomplete tag NEWREPAINT

--- AMFICOM/v1/surveyresource/mk.bat
+++
@@ -1,1 +0,0 @@
-nmake /f makesurveyresource %1
--- AMFICOM/v2/database/schema/measurement/dictionary/measurementstatus.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE MeasurementStatus (
- id NUMBER(2, 0),
- name VARCHAR2(32) NOT NULL,
- comments VARCHAR2(64),
- CONSTRAINT measurementstatus_pk PRIMARY KEY (id) ENABLE
-);
