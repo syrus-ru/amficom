0ba7e6ce429803782be916b39059e682b9783398
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-03-29 12:21:32 +0400

    Import/Export routines for UniCableMap

---
+++ AMFICOM/v1/importUCM/src/com/syrus/impexp/unicablemap/UniCableMapBuf.java
@@ -0,0 +1,12 @@
+package com.syrus.impexp.unicablemap;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UniCableMapBuf {
+	public Collection params;
+	
+	public UniCableMapBuf() {
+		this.params = new ArrayList();
+	}
+}
