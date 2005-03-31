c93953c1611a7122e4396b3077ede029b312482a
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-03-31 13:05:38 +0400

    FIX: setting delegate linkedEntityCode when change linkedIds

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/LinkedIdsCondition.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/LinkedIdsCondition.java
@@ -1,5 +1,5 @@
 /*
- * $Id: LinkedIdsCondition.java,v 1.23 2005/03/24 12:52:07 arseniy Exp $
+ * $Id: LinkedIdsCondition.java,v 1.24 2005/03/31 09:05:38 bob Exp $
  *
  * Copyright ¿ 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -15,6 +15,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
@@ -64,8 +65,8 @@ import com.syrus.util.Log;
  * {@link #isNeedMore(Collection)}and {@link #setEntityCode(Short)}.</li>
  * </ul>
  * 
- * @author $Author: arseniy $
- * @version $Revision: 1.23 $, $Date: 2005/03/24 12:52:07 $
+ * @author $Author: bob $
+ * @version $Revision: 1.24 $, $Date: 2005/03/31 09:05:38 $
  * @module general_v1
  */
 public class LinkedIdsCondition implements StorableObjectCondition {
@@ -361,6 +362,13 @@ public class LinkedIdsCondition implements StorableObjectCondition {
 	}
 
 	public void setLinkedIds(Collection linkedIds) {
+		try {
+			this.delegate.linkedEntityCode = getOnlyOneLinkedEntityCode(linkedIds);
+		}
+		catch (IllegalDataException ide) {
+			this.delegate.linkedEntityCode = ObjectEntities.UNKNOWN_ENTITY_CODE;
+			Log.errorException(ide);
+		}
 		this.delegate.linkedIds = linkedIds;
 	}
 
@@ -369,7 +377,15 @@ public class LinkedIdsCondition implements StorableObjectCondition {
 	}
 
 	public void setLinkedId(final Identifier linkedId) {
-		this.delegate.linkedIds = Collections.singletonList(linkedId);
+		List singletonIdList = Collections.singletonList(linkedId);
+		try {
+			this.delegate.linkedEntityCode = getOnlyOneLinkedEntityCode(singletonIdList);
+		}
+		catch (IllegalDataException ide) {
+			this.delegate.linkedEntityCode = ObjectEntities.UNKNOWN_ENTITY_CODE;
+			Log.errorException(ide);
+		}
+		this.delegate.linkedIds = singletonIdList;
 	}
 
 	protected Map sort(Collection linkIds) {
