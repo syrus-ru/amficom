de8b627cae4d1ec19e448577b8411496e202ca1f
Alexey Stratonnikov <alexsaa@gmail.com>
2005-04-01 15:57:33 +0400

    TRIVIAL: RefUpdateEvent of type ModelFunctionChanged is never generated: this type removed

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Event/RefUpdateEvent.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Event/RefUpdateEvent.java
@@ -23,8 +23,6 @@ public class RefUpdateEvent extends OperationEvent
 	    { return (flags & THRESHOLDS_UPDATED_EVENT) != 0; }
 	public boolean minTraceLevelChanged()
 	    { return (flags & MIN_TRACE_LEVEL_CHANGED_EVENT) != 0; }
-	public boolean modelFunctionChanged()
-	{ return (flags & MODEL_CHANGED_EVENT) != 0; }
 
 	public static final long MARKER_MOVED_EVENT 			= 0x00000001;
 	public static final long ANALYSIS_PERFORMED_EVENT 		= 0x00000100; // source is always and is always assumed to be Heap.PRIMARY_TRACE
@@ -32,7 +30,6 @@ public class RefUpdateEvent extends OperationEvent
 	public static final long THRESHOLD_CHANGED_EVENT 		= 0x00100000;
 	public static final long THRESHOLDS_UPDATED_EVENT 		= 0x00200000; // source doesn't matter
 	public static final long MIN_TRACE_LEVEL_CHANGED_EVENT 	= 0x01000000;
-	public static final long MODEL_CHANGED_EVENT 			= 0x00000002; // never generated?
 
 	public static final String typ = "refupdate";
 
