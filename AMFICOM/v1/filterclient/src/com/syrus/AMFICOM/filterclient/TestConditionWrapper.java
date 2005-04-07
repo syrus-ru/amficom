061d543ebf1a00dd12abac16e690620728390ea2
Maxim Selivanov <max.selivanov@gmail.com>
2005-04-07 18:49:50 +0400

    bugfix

--- AMFICOM/v1/filterclient/src/com/syrus/AMFICOM/filterclient/TestConditionWrapper.java
+++ AMFICOM/v1/filterclient/src/com/syrus/AMFICOM/filterclient/TestConditionWrapper.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestConditionWrapper.java,v 1.4 2005/04/01 17:06:06 max Exp $
+ * $Id: TestConditionWrapper.java,v 1.5 2005/04/07 14:49:50 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -24,7 +24,7 @@ import com.syrus.AMFICOM.measurement.Test;
 import com.syrus.AMFICOM.measurement.TestWrapper;
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/04/01 17:06:06 $
+ * @version $Revision: 1.5 $, $Date: 2005/04/07 14:49:50 $
  * @author $Author: max $
  * @module filterclient_v1
  */
@@ -40,13 +40,17 @@ public class TestConditionWrapper implements ConditionWrapper {
 	private Map storableObjectInitialName = new HashMap();
 	
 	private static final String STATUS = "filter by status";
+	private static final String NUMBER = "filter by number";
+	private static final String STRING = "filter by string";
+	private static final String START_TIME = "filter by start time";
+	private static final String END_TIME = "filter by end time";
 	private static final String ME = "filter by monitored elements";
-	private static final String MT = "filter by measurementTypes";
+	private static final String MP = "filter by measurement ports";
 	private static final String MCM = "filter by MCMs";
 		
-	private static String[] keys = {TestWrapper.COLUMN_STATUS, ME, MT, MCM};
-	private static String[] keyNames = {STATUS, ME, MT, MCM};
-	private static byte[] keyTypes = {ConditionWrapper.CONSTRAINT, ConditionWrapper.LIST, ConditionWrapper.LIST, ConditionWrapper.LIST};
+	private static String[] keys = {TestWrapper.COLUMN_STATUS, NUMBER , STRING, TestWrapper.COLUMN_START_TIME, TestWrapper.COLUMN_END_TIME, ME, MP, MCM};
+	private static String[] keyNames = {STATUS, NUMBER, STRING, START_TIME, END_TIME, ME, MP, MCM};
+	private static byte[] keyTypes = {ConditionWrapper.CONSTRAINT, ConditionWrapper.INT, ConditionWrapper.STRING, ConditionWrapper.DATE, ConditionWrapper.DATE,ConditionWrapper.LIST, ConditionWrapper.LIST, ConditionWrapper.LIST};
 	
 	public TestConditionWrapper(Collection initialTests,
 			Collection monitoredElements, Collection measurementPorts, Collection mcms) {
@@ -57,7 +61,7 @@ public class TestConditionWrapper implements ConditionWrapper {
 		
 		this.keyLinkedNames = new HashMap();
 		String[] testStatusNames = new String[]{"NEW", "SCHEDULED", "PROCESSING", "COMPLETED", "ABORTED" }; 
-		this.keyLinkedNames.put(keys[0], testStatusNames);
+		this.keyLinkedNames.put(TestWrapper.COLUMN_STATUS, testStatusNames);
 		
 		String[] meNames = new String[this.monitoredElements.size()];
 		int i=0;
@@ -65,23 +69,23 @@ public class TestConditionWrapper implements ConditionWrapper {
 			MonitoredElement me = (MonitoredElement) iter.next();
 			meNames[i] = me.getName();			
 		}
-		this.keyLinkedNames.put(keys[1], meNames);
+		this.keyLinkedNames.put(ME, meNames);
 				
 		String[] mpNames = new String[this.measurementPorts.size()];
 		i=0;
-		for (Iterator iter = this.measurementPorts.iterator(); iter.hasNext();) {
+		for (Iterator iter = this.measurementPorts.iterator(); iter.hasNext();i++) {
 			MeasurementPort mp = (MeasurementPort) iter.next();
 			mpNames[i] = mp.getName();
 		}
-		this.keyLinkedNames.put(keys[2], mpNames);
+		this.keyLinkedNames.put(MP, mpNames);
 		
 		String[] mcmNames = new String[this.mcms.size()];
 		i=0;
-		for (Iterator iter = this.mcms.iterator(); iter.hasNext();) {
+		for (Iterator iter = this.mcms.iterator(); iter.hasNext();i++) {
 			MCM mcm = (MCM) iter.next();
 			mcmNames[i] = mcm.getName();			
 		}
-		this.keyLinkedNames.put(keys[3], mcmNames);
+		this.keyLinkedNames.put(MCM, mcmNames);
 		
 		this.storableObjectInitialName = new HashMap();
 		for (Iterator iter = initialTests.iterator(); iter.hasNext();) {
@@ -95,11 +99,13 @@ public class TestConditionWrapper implements ConditionWrapper {
 	}
 	
 	public Object getLinkedObject(String key, int indexNumber) throws IllegalDataException {
-		if (key.equals(keys[1])) {
+		if (key.equals(TestWrapper.COLUMN_STATUS)) {
 			return ((MonitoredElement)this.monitoredElements.get(indexNumber)).getId();
-		} else if (key.equals(keys[2])) {
+		} else if (key.equals(ME)) {
+			return ((MonitoredElement)this.monitoredElements.get(indexNumber)).getId();
+		} else if (key.equals(MP)) {
 			return ((MeasurementPort)this.measurementPorts.get(indexNumber)).getId();
-		} else if (key.equals(keys[3])) {
+		} else if (key.equals(MCM)) {
 			return ((MCM)this.mcms.get(indexNumber)).getId();
 		} else {
 			throw new IllegalDataException("TestConditionWrapper.getLinkedObject | Wrong key");
