1a3542bbb2b73c684866d8031664dc560615158e
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-04-07 14:53:59 +0400

    add setObjectNameChanged

--- AMFICOM/v1/filter/src/com/syrus/AMFICOM/logic/ItemListener.java
+++ AMFICOM/v1/filter/src/com/syrus/AMFICOM/logic/ItemListener.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ItemListener.java,v 1.3 2005/03/23 15:04:49 bass Exp $
+ * $Id: ItemListener.java,v 1.4 2005/04/07 10:53:59 bob Exp $
  *
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,12 +9,14 @@
 package com.syrus.AMFICOM.logic;
 
 /**
- * @version $Revision: 1.3 $, $Date: 2005/03/23 15:04:49 $
- * @author $Author: bass $
+ * @version $Revision: 1.4 $, $Date: 2005/04/07 10:53:59 $
+ * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module filter_v1
  */
 public interface ItemListener {
 
 	void setParentPerformed(final Item item, final Item oldParent, final Item newParent);
+	
+	void setObjectNameChanged(Item item, String oldName, String newName);
 }
