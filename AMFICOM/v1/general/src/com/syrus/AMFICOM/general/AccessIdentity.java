3a72dff8170da0f3a2ec15194ec337ce821b3522
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-08 19:28:34 +0400

    MAJOR: String sessionCode instead of Identifier

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/AccessIdentity.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/AccessIdentity.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AccessIdentity.java,v 1.4 2005/04/04 13:07:03 bass Exp $
+ * $Id: AccessIdentity.java,v 1.5 2005/04/08 15:28:34 arseniy Exp $
  *
  * Copyright ¿ 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -15,38 +15,38 @@ import com.syrus.AMFICOM.general.corba.AccessIdentifier_Transferable;
 import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/04/04 13:07:03 $
- * @author $Author: bass $
+ * @version $Revision: 1.5 $, $Date: 2005/04/08 15:28:34 $
+ * @author $Author: arseniy $
  * @module general_v1
  */
 public class AccessIdentity implements TransferableObject {
 
-	private Identifier domainId;
-	private Identifier sessionId;
 
 	private Date start;
+	private Identifier domainId;
 	private Identifier userId;
+	private String sessionCode;
 
 	public AccessIdentity(AccessIdentifier_Transferable transferable) {
 		this.start = new Date(transferable.started);
 		this.domainId = new Identifier(transferable.domain_id);
 		this.userId = new Identifier(transferable.user_id);
-		this.sessionId = new Identifier(transferable.session_id);
+		this.sessionCode = transferable.session_code;
 	}
 
-	public AccessIdentity(Date start, Identifier domainId, Identifier userId, Identifier sessionId) {
+	public AccessIdentity(Date start, Identifier domainId, Identifier userId, String sessionCode) {
 		this.start = start;
 		this.domainId = domainId;
 		this.userId = userId;
-		this.sessionId = sessionId;
+		this.sessionCode = sessionCode;
 	}
 
 	public Identifier getDomainId() {
 		return this.domainId;
 	}
 
-	public Identifier getSessionId() {
-		return this.sessionId;
+	public String getSessionCode() {
+		return this.sessionCode;
 	}
 
 	public Date getStart() {
@@ -54,9 +54,10 @@ public class AccessIdentity implements TransferableObject {
 	}
 
 	public IDLEntity getTransferable() {
-		return new AccessIdentifier_Transferable(this.start.getTime(), (Identifier_Transferable) this.domainId
-				.getTransferable(), (Identifier_Transferable) this.userId.getTransferable(),
-													(Identifier_Transferable) this.sessionId.getTransferable());
+		return new AccessIdentifier_Transferable(this.start.getTime(),
+				(Identifier_Transferable) this.domainId.getTransferable(),
+				(Identifier_Transferable) this.userId.getTransferable(),
+				this.sessionCode);
 	}
 
 	public Identifier getUserId() {
