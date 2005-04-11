e0928da7ce4cc2fa176cb113ef293cc09292e83f
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-04-11 16:50:08 +0400

    FIX using UNITS_PREFIX

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/CharacteristicTypeCodenames.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/CharacteristicTypeCodenames.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: CharacteristicTypeCodenames.java,v 1.4 2005/04/08 14:35:37 bob Exp $
+ * $Id: CharacteristicTypeCodenames.java,v 1.5 2005/04/11 12:50:08 bob Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,7 +9,7 @@
 package com.syrus.AMFICOM.general;
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/04/08 14:35:37 $
+ * @version $Revision: 1.5 $, $Date: 2005/04/11 12:50:08 $
  * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module general_v1
@@ -28,11 +28,12 @@ public final class CharacteristicTypeCodenames {
 	public static final String TRACE_LENGTH_SUFFIX = "_ref_trclen";
 	public static final String TRACE_PULSE_WIDTH_SUFFIX = "_ref_pulswd";
 	
-	public static final String UNITS_RESOLUTION = "units_res";
-	public static final String UNITS_WAVELENGTH = "units_wvlen";
-	public static final String UNITS_AVERAGE_COUNT = "units_scans";
-	public static final String UNITS_TRACE_LENGTH = "units_trclen";
-	public static final String UNITS_PULSE_WIDTH = "units_pulswd";
+	public static final String UNITS_PREFIX = "units_";
+	public static final String UNITS_RESOLUTION = UNITS_PREFIX + "res";
+	public static final String UNITS_WAVELENGTH = UNITS_PREFIX + "wvlen";
+	public static final String UNITS_AVERAGE_COUNT = UNITS_PREFIX + "scans";
+	public static final String UNITS_TRACE_LENGTH = UNITS_PREFIX + "trclen";
+	public static final String UNITS_PULSE_WIDTH = UNITS_PREFIX + "pulswd";
 
 	private CharacteristicTypeCodenames() {
 		//singleton
