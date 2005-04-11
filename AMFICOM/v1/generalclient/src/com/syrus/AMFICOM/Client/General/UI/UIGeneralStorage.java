53b1e4943e7d31b437766e58c0b16bc9c5da9bee
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-04-11 15:17:37 +0400

    ADD arrangeTableColumns

--- AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/General/UI/UIGeneralStorage.java
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/Client/General/UI/UIGeneralStorage.java
@@ -6,13 +6,20 @@
 
 package com.syrus.AMFICOM.Client.General.UI;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Rectangle;
 
 import javax.swing.JComponent;
+import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/04/08 11:30:57 $
+ * @version $Revision: 1.5 $, $Date: 2005/04/11 11:17:37 $
  * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module generalclient_v1
@@ -37,5 +44,33 @@ public final class UIGeneralStorage {
 		Dimension preferredSize = component.getPreferredSize();
 		preferredSize.width = (int) (SCALE_FACTOR * minimumSize.width);
 		component.setPreferredSize(preferredSize);
+	}
+	
+	public static void arrangeTableColumns(JTable table) {
+
+		TableModel model = table.getModel();
+		int[] columnsWidth = new int[model.getColumnCount()];
+		{
+			JTableHeader tableHeader = table.getTableHeader();
+			TableCellRenderer headerRenderer = table.getTableHeader().getDefaultRenderer();
+			TableColumnModel columnModel = tableHeader.getColumnModel();
+			for (int i = 0; i < columnModel.getColumnCount(); i++) {
+				Component component = headerRenderer.getTableCellRendererComponent(null, columnModel.getColumn(i)
+						.getHeaderValue(), false, false, 0, 0);
+				columnsWidth[i] = component.getPreferredSize().width;
+			}
+		}
+		for (int i = 0; i < model.getRowCount(); i++) {
+			for (int j = 0; j < columnsWidth.length; j++) {
+				Rectangle cellRect = table.getCellRect(i, j, true);
+				int width = cellRect.width - cellRect.x;
+				if (width > columnsWidth[j])
+					columnsWidth[j] = width;
+			}
+		}
+
+		for (int j = 0; j < columnsWidth.length; j++) {
+			table.getColumnModel().getColumn(j).setPreferredWidth(columnsWidth[j]);
+		}
 	}
 }
