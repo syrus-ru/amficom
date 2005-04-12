2297b505ca731b0c70c4d6af71aa92a5331f7ef0
Alexey Stratonnikov <alexsaa@gmail.com>
2005-04-12 18:15:41 +0400

    CSOMETIC: removed unused import

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/Analysis/Heap.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/Analysis/Heap.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: Heap.java,v 1.16 2005/04/11 10:36:56 saa Exp $
+ * $Id: Heap.java,v 1.17 2005/04/12 14:15:41 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -25,7 +25,6 @@ import com.syrus.AMFICOM.Client.General.Event.PrimaryMTMListener;
 import com.syrus.AMFICOM.Client.General.Event.bsHashChangeListener;
 import com.syrus.AMFICOM.Client.General.Event.PrimaryTraceListener;
 import com.syrus.AMFICOM.Client.General.Model.AnalysisResourceKeys;
-import com.syrus.AMFICOM.analysis.dadara.ModelTraceAndEvents;
 import com.syrus.AMFICOM.analysis.dadara.ModelTraceAndEventsImpl;
 import com.syrus.AMFICOM.analysis.dadara.ModelTraceManager;
 import com.syrus.AMFICOM.analysis.dadara.RefAnalysis;
@@ -39,7 +38,7 @@ import com.syrus.io.BellcoreStructure;
  * использование остальных методов работы с BS
  * 
  * @author $Author: saa $
- * @version $Revision: 1.16 $, $Date: 2005/04/11 10:36:56 $
+ * @version $Revision: 1.17 $, $Date: 2005/04/12 14:15:41 $
  * @module
  */
 public class Heap
