ef196419f38d1df14cfe25b0218a56a7d331aca1
Alexey Stratonnikov <alexsaa@gmail.com>
2005-04-12 17:55:33 +0400

    COSMETIC: added a fixme to rename the class

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Event/bsHashChangeListener.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Event/bsHashChangeListener.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: bsHashChangeListener.java,v 1.1 2005/03/31 16:58:28 saa Exp $
+ * $Id: bsHashChangeListener.java,v 1.2 2005/04/12 13:55:33 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -12,10 +12,10 @@ import com.syrus.io.BellcoreStructure;
 
 /**
  * @author $Author: saa $
- * @version $Revision: 1.1 $, $Date: 2005/03/31 16:58:28 $
+ * @version $Revision: 1.2 $, $Date: 2005/04/12 13:55:33 $
  * @module
  */
-public interface bsHashChangeListener
+public interface bsHashChangeListener // FIXME: rename to BSHashChangeListener
 {
 	void bsHashAdded(String key, BellcoreStructure bs);
 	void bsHashRemoved(String key);
