c5d87cbd172d122ed8b4116cb98f3072fc58a57e
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-04-12 17:07:25 +0400

    FIX dadara codenames

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ParameterTypeCodenames.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ParameterTypeCodenames.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ParameterTypeCodenames.java,v 1.9 2005/04/12 12:28:40 bob Exp $
+ * $Id: ParameterTypeCodenames.java,v 1.10 2005/04/12 13:07:25 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -9,7 +9,7 @@
 package com.syrus.AMFICOM.general;
 
 /**
- * @version $Revision: 1.9 $, $Date: 2005/04/12 12:28:40 $
+ * @version $Revision: 1.10 $, $Date: 2005/04/12 13:07:25 $
  * @author $Author: bob $
  * @module general_v1
  */
@@ -37,6 +37,8 @@ public final class ParameterTypeCodenames {
 	public static final String	DADARA_MIN_TRACE_LEVEL		= "dadara_min_trace_level"; // double
 
 	// dadara analysis params
+	public static final String	DADARA						= "dadara";
+	public static final String	DADARA_ALARMS				= "dadara_alarm_array";
 	public static final String	DADARA_NOISE_FACTOR			= "dadara_noise_factor";	// double
 	public static final String	MIN_EVENT					= "dadara_min_event";		// double
 	public static final String	MIN_SPLICE					= "dadara_min_splice";		// double
