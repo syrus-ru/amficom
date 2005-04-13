1a50042876459e7a6254c60a3e4f4ccaf739c8ae
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-04-13 13:21:39 +0400

    ADD: search nor nodelinks by reference to physicallink ADD: search for nodelinks by reference to abstractnode (startnode or endnode)

--- AMFICOM/v1/map/src/com/syrus/AMFICOM/map/LinkedIdsConditionImpl.java
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/map/LinkedIdsConditionImpl.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: LinkedIdsConditionImpl.java,v 1.5 2005/04/04 13:15:58 bass Exp $
+ * $Id: LinkedIdsConditionImpl.java,v 1.6 2005/04/13 09:21:39 krupenn Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -23,8 +23,8 @@ import com.syrus.AMFICOM.general.StorableObject;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.5 $
- * @author $Author: bass $
+ * @version $Revision: 1.6 $
+ * @author $Author: krupenn $
  * @module map_v1
  */
 final class LinkedIdsConditionImpl extends LinkedIdsCondition {
@@ -54,9 +54,54 @@ final class LinkedIdsConditionImpl extends LinkedIdsCondition {
 		return condition;
 	}
 
+	private boolean checkPhysicalLink(final NodeLink nodeLink) {
+		boolean condition = false;
+		for (final Iterator it = this.linkedIds.iterator(); it.hasNext() && !condition;) {
+			final Identifier id = (Identifier) it.next();
+			if (id.getMajor() == ObjectEntities.PHYSICAL_LINK_ENTITY_CODE) {
+				if (nodeLink.getPhysicalLink().getId().equals(id))
+					condition = true;
+			}
+		}
+		return condition;
+	}
+
+	private boolean checkNode(final NodeLink nodeLink) {
+		boolean condition = false;
+		for (final Iterator it = this.linkedIds.iterator(); it.hasNext() && !condition;) {
+			final Identifier id = (Identifier) it.next();
+			if (id.getMajor() == ObjectEntities.TOPOLOGICAL_NODE_ENTITY_CODE
+					|| id.getMajor() == ObjectEntities.SITE_NODE_ENTITY_CODE) {
+				if (nodeLink.getStartNode().getId().equals(id)
+						|| nodeLink.getEndNode().getId().equals(id))
+					condition = true;
+			}
+		}
+		return condition;
+	}
+
 	public boolean isConditionTrue(final StorableObject storableObject) throws IllegalObjectEntityException {
 		boolean condition = false;
 		switch (this.entityCode.shortValue()) {
+			case ObjectEntities.NODE_LINK_ENTITY_CODE:
+				NodeLink nodeLink = (NodeLink) storableObject;
+				switch (this.linkedEntityCode) {
+					case ObjectEntities.PHYSICAL_LINK_ENTITY_CODE:
+						condition = this.checkPhysicalLink(nodeLink);
+						break;
+					case ObjectEntities.TOPOLOGICAL_NODE_ENTITY_CODE:
+						// fall through.
+						// finding a node link by endpoint is the same for
+						// topological node and site node
+					case ObjectEntities.SITE_NODE_ENTITY_CODE:
+						condition = this.checkNode(nodeLink);
+						break;
+					default:
+						throw new IllegalObjectEntityException(LINKED_ENTITY_CODE_NOT_REGISTERED + this.linkedEntityCode
+								+ ", " + ObjectEntities.codeToString(this.linkedEntityCode),
+								IllegalObjectEntityException.ENTITY_NOT_REGISTERED_CODE);
+				}
+				break;
 			case ObjectEntities.MAP_ENTITY_CODE:
 				Map map = (Map) storableObject;
 				switch (this.linkedEntityCode) {
