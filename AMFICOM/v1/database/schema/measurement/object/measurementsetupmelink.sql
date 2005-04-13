9ae62f77887f9895b1ab10f66dc5a06f3b152b9a
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-13 13:45:15 +0400

    MINOR: Constraint name

--- AMFICOM/v1/database/schema/measurement/object/measurementsetupmelink.sql
+++ AMFICOM/v1/database/schema/measurement/object/measurementsetupmelink.sql
@@ -2,7 +2,7 @@ CREATE TABLE MeasurementSetupMELink (
  measurement_setup_id VARCHAR2(32) NOT NULL,
  monitored_element_id VARCHAR2(32) NOT NULL,
 --
- CONSTRAINT mntspmelink_mntsp_fk FOREIGN KEY (measurement_setup_id)
+ CONSTRAINT mntspmelink_mntsetup_fk FOREIGN KEY (measurement_setup_id)
   REFERENCES MeasurementSetup (id) ON DELETE CASCADE,
  CONSTRAINT mntspmelink_me_fk FOREIGN KEY (monitored_element_id)
   REFERENCES MonitoredElement (id) ON DELETE CASCADE
