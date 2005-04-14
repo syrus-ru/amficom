0d32cac853d43d9a8075bdec419c1543a68258de
Alexey Stratonnikov <alexsaa@gmail.com>
2005-04-14 19:56:04 +0400

    COSMETIC: javadoc added

--- AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/DataStreamable.java
+++ AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/DataStreamable.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: DataStreamable.java,v 1.1 2005/04/11 14:46:07 saa Exp $
+ * $Id: DataStreamable.java,v 1.2 2005/04/14 15:56:04 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -13,18 +13,40 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 /**
+ * Declares things required to save/load objects to/from
+ * DataOutputStream/DataInputStream via DataStreamableUtil.
+ * To save an object, writeToDOS() method is used;
+ * to create object from stream, a factory is required.
+ * <p>
+ * Although you are not obliged to implement anything else,
+ * you are expected to create getReader() that will return a factory object
+ * <code>
+ * private static DataStreamable.Reader DS_READER = null;
+ * public static DataStreamable.Reader getReader() {
+ *     if (DS_READER == null)
+ *         DS_READER = new DSReader() {
+ *             public DataStreamable readFromDIS { ... } // a factory method 
+ *         };
+ *     return DS_READER;
+ * }
+ * </code>
+ * This will make use of
+ * {@link DataStreamableUtil#readDataStreamableFromBA}.
  * @author $Author: saa $
- * @version $Revision: 1.1 $, $Date: 2005/04/11 14:46:07 $
+ * @version $Revision: 1.2 $, $Date: 2005/04/14 15:56:04 $
  * @module
  */
 public interface DataStreamable
 {
+    /**
+     * Saves the object state to {@link DataOutputStream}
+     * @param dos stream to save to
+     * @throws IOException an error occured during writing to dos
+     */
 	void writeToDOS(DataOutputStream dos) throws IOException;
+
 	static interface Reader
 	{
 		DataStreamable readFromDIS(DataInputStream dis) throws IOException, SignatureMismatchException;
 	}
-	// you are also expected to implement factory method:
-	// static DataStreamable.Reader getReader()
-	// and use this for readDataStreamableFromBA
 }
