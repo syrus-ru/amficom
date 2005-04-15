58b741398dea59f026a68ed9d07fd460660f320a
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-04-15 21:48:30 +0400

    ADD: almost fully implemented PathElement.

--- AMFICOM/v1/scheme/cntlines.sh
+++ AMFICOM/v1/scheme/cntlines.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "`find src -name '*\.java' -exec cat \{\} \; | wc -l` lines of code (LOC)"
-echo "`find src -name '*\.java' -exec grep -H 'throw new UnsupportedOperationException();' \{\} \; | grep -vE 'Loader|Database' | wc -l` unimplemented methods (entities)"
+echo "`find src -name '*\.java' -exec grep -H 'throw new UnsupportedOperationException();' \{\} \; | grep -vE 'Loader|Database|ConditionImpl' | wc -l` unimplemented methods (entities)"
 echo "`find src -name '*\.java' -exec grep -H 'throw new UnsupportedOperationException();' \{\} \; | grep -E 'Loader|Database' | wc -l` unimplemented methods (loaders and databases)"
+echo "`find src -name '*\.java' -exec grep -H 'throw new UnsupportedOperationException();' \{\} \; | grep 'ConditionImpl' | wc -l` unimplemented methods (conditions)"
