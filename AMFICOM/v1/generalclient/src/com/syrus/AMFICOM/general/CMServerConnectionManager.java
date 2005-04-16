30cc9b6d1a849859dc67250bccc9a1036ad5f0aa
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-17 01:29:09 +0400

    MAJOR: Extends VerifiedConnectionManager and implements IGSConnectionManager

--- AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/general/CMServerConnectionManager.java
+++ AMFICOM/v1/generalclient/src/com/syrus/AMFICOM/general/CMServerConnectionManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CMServerConnectionManager.java,v 1.1 2005/04/15 22:18:12 arseniy Exp $
+ * $Id: CMServerConnectionManager.java,v 1.2 2005/04/16 21:29:09 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,25 +7,58 @@
  */
 package com.syrus.AMFICOM.general;
 
+import java.util.Collections;
+
 import com.syrus.AMFICOM.cmserver.corba.CMServer;
 import com.syrus.AMFICOM.general.corba.IdentifierGeneratorServer;
+import com.syrus.AMFICOM.general.corba.Verifiable;
+import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.1 $, $Date: 2005/04/15 22:18:12 $
+ * @version $Revision: 1.2 $, $Date: 2005/04/16 21:29:09 $
  * @author $Author: arseniy $
  * @module generalclient_v1
  */
-public final class CMServerConnectionManager extends VerifiedReferenceSource implements IGServerReferenceSource {
+public final class CMServerConnectionManager extends VerifiedConnectionManager implements IGSConnectionManager {
+	private String cmServerServantName;
 
 	public CMServerConnectionManager(CORBAServer corbaServer, String cmServerServantName) {
-		super(corbaServer, cmServerServantName);
+		super(corbaServer, Collections.singleton(cmServerServantName));
+
+		this.cmServerServantName = cmServerServantName;
 	}
 
-	public IdentifierGeneratorServer getVerifiedIGServerReference() throws CommunicationException {
-		return (IdentifierGeneratorServer) super.getVerifiedReference();
+	public IdentifierGeneratorServer getVerifiedIGSReference() throws CommunicationException {
+		Verifiable reference = null;
+		try {
+			reference = super.getVerifiableReference(this.cmServerServantName);
+		}
+		catch (IllegalDataException ide) {
+			// Never
+			Log.errorException(ide);
+		}
+		return (IdentifierGeneratorServer) reference;
 	}
 
 	public CMServer getVerifiedCMServerReference() throws CommunicationException {
-		return (CMServer) super.getVerifiedReference();
+		Verifiable reference = null;
+		try {
+			reference = super.getVerifiableReference(this.cmServerServantName);
+		}
+		catch (IllegalDataException ide) {
+			// Never
+			Log.errorException(ide);
+		}
+		return (CMServer) reference;
 	}
+
+	protected void onLoseConnection(String servantName) {
+		Log.debugMessage("CMServerConnectionManager.onLoseConnection | Connection with '" + servantName + "' lost", Log.DEBUGLEVEL08);
+	}
+
+	protected void onRestoreConnection(String servantName) {
+		Log.debugMessage("CMServerConnectionManager.onRestoreConnection | Connection with '" + servantName + "' restored",
+				Log.DEBUGLEVEL08);
+	}
+
 }
