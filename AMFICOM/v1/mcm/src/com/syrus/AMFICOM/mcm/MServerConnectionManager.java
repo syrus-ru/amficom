6b0dcc503ee08a95488c3a8176e12b8a179bd087
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-17 01:17:19 +0400

    MAJOR: Extends RunnableVerifiedConnectionManager and implements IGSConnectionManager

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/MServerConnectionManager.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/MServerConnectionManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MServerConnectionManager.java,v 1.2 2005/04/15 22:15:09 arseniy Exp $
+ * $Id: MServerConnectionManager.java,v 1.3 2005/04/16 21:17:19 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,89 +7,67 @@
  */
 package com.syrus.AMFICOM.mcm;
 
+import java.util.Collections;
+
 import com.syrus.AMFICOM.general.CORBAServer;
 import com.syrus.AMFICOM.general.CommunicationException;
-import com.syrus.AMFICOM.general.IGServerReferenceSource;
-import com.syrus.AMFICOM.general.VerifiedReferenceSource;
+import com.syrus.AMFICOM.general.IGSConnectionManager;
+import com.syrus.AMFICOM.general.IllegalDataException;
+import com.syrus.AMFICOM.general.RunnableVerifiedConnectionManager;
 import com.syrus.AMFICOM.general.corba.IdentifierGeneratorServer;
 import com.syrus.AMFICOM.general.corba.Verifiable;
 import com.syrus.AMFICOM.mserver.corba.MServer;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/04/15 22:15:09 $
+ * @version $Revision: 1.3 $, $Date: 2005/04/16 21:17:19 $
  * @author $Author: arseniy $
  * @module mcm_v1
  */
-public class MServerConnectionManager extends VerifiedReferenceSource implements Runnable, IGServerReferenceSource {
-	private long mServerCheckTimeout;
-
-	private boolean connectionLost;
+public class MServerConnectionManager extends RunnableVerifiedConnectionManager implements IGSConnectionManager {
+	private String mServerServantName;
 
 	public MServerConnectionManager(CORBAServer corbaServer, String mServerServantName, long mServerCheckTimeout) {
-		super(corbaServer, mServerServantName);
+		super(corbaServer, Collections.singleton(mServerServantName), mServerCheckTimeout);
+
+		this.mServerServantName = mServerServantName;
+
 		assert mServerCheckTimeout >= 10 * 60 * 1000 : "Too low timeToSleep"; //not less then 10 min
-
-		this.mServerCheckTimeout = mServerCheckTimeout;
-
-		this.connectionLost = false;
 	}
 
-	public void run() {
-		while (true) {
-
-			try {
-				Verifiable reference = super.getVerifiedReference();
-				Log.debugMessage("MServerConnectionManager.run | Verified reference to Measurement Server", Log.DEBUGLEVEL08);
-				Log.debugMessage(reference.toString(), Log.DEBUGLEVEL08);
-				this.checkIfConnectionRestored();
-			}
-			catch (CommunicationException ce) {
-				this.connectionLost = true;
-				//@todo Generate event "Cannot resolve Measurement Server"
-			}
-
-			try {
-				Thread.sleep(this.mServerCheckTimeout);
-			}
-			catch (InterruptedException ie) {
-				Log.errorException(ie);
-			}
-
-		}
-	}
-
-	public IdentifierGeneratorServer getVerifiedIGServerReference() throws CommunicationException {
+	public IdentifierGeneratorServer getVerifiedIGSReference() throws CommunicationException {
+		Verifiable reference = null;
 		try {
-			Verifiable reference = super.getVerifiedReference();
-			this.checkIfConnectionRestored();
-			return (IdentifierGeneratorServer) reference;
+			reference = super.getVerifiableReference(this.mServerServantName);
 		}
-		catch (CommunicationException ce) {
-			this.connectionLost = true;
-			//@todo Generate event "Cannot resolve Measurement Server"
-			throw ce;
+		catch (IllegalDataException ide) {
+			// Never
+			Log.errorException(ide);
 		}
+		return (IdentifierGeneratorServer) reference;
 	}
 
 	protected MServer getVerifiedMServerReference() throws CommunicationException {
+		Verifiable reference = null;
 		try {
-			Verifiable reference = super.getVerifiedReference();
-			this.checkIfConnectionRestored();
-			return (MServer) reference;
+			reference = super.getVerifiableReference(this.mServerServantName);
 		}
-		catch (CommunicationException ce) {
-			this.connectionLost = true;
-			//@todo Generate event "Cannot resolve Measurement Server"
-			throw ce;
+		catch (IllegalDataException ide) {
+			// Never
+			Log.errorException(ide);
 		}
+		return (MServer) reference;
 	}
 
-	private void checkIfConnectionRestored() {
-		if (this.connectionLost) {
-			this.connectionLost = false;
-			//@todo Generate event "Connection with Measurement Server restored"
-		}
+	protected void onLoseConnection(String servantName) {
+		Log.debugMessage("MServerConnectionManager.onLoseConnection | Connection with '" + servantName + "' lost", Log.DEBUGLEVEL08);
+		//@todo Generate event "Connection with Measurement Server lost"
+	}
+
+	protected void onRestoreConnection(String servantName) {
+		Log.debugMessage("MServerConnectionManager.onRestoreConnection | Connection with '" + servantName + "' restored",
+				Log.DEBUGLEVEL08);
+		//@todo Generate event "Connection with Measurement Server restored"
 	}
 
 }
