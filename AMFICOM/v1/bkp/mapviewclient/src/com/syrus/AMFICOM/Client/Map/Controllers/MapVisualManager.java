6ce26d36c316a812f20b0b5484ebfae2cab4a254
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-04-18 20:15:15 +0400

    ADD: sitenode visual manager

--- AMFICOM/v1/bkp/mapviewclient/src/com/syrus/AMFICOM/Client/Map/Controllers/MapVisualManager.java
+++ AMFICOM/v1/bkp/mapviewclient/src/com/syrus/AMFICOM/Client/Map/Controllers/MapVisualManager.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapVisualManager.java,v 1.2 2005/04/18 11:14:34 krupenn Exp $
+ * $Id: MapVisualManager.java,v 1.3 2005/04/18 16:15:15 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -11,13 +11,14 @@ import java.util.HashMap;
 
 import com.syrus.AMFICOM.Client.Map.Props.CablePathVisualManager;
 import com.syrus.AMFICOM.Client.Map.Props.PhysicalLinkVisualManager;
+import com.syrus.AMFICOM.Client.Map.Props.SiteNodeVisualManager;
 import com.syrus.AMFICOM.client_.general.ui_.StorableObjectEditor;
 import com.syrus.AMFICOM.client_.general.ui_.VisualManager;
 import com.syrus.AMFICOM.client_.resource.ObjectResourceController;
 import com.syrus.AMFICOM.map.MapElement;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/04/18 11:14:34 $
+ * @version $Revision: 1.3 $, $Date: 2005/04/18 16:15:15 $
  * @author $Author: krupenn $
  * @module mapviewclient
  */
@@ -33,6 +34,8 @@ public class MapVisualManager implements VisualManager {
 			CablePathVisualManager.getInstance());
 		vmMap.put(com.syrus.AMFICOM.map.PhysicalLink.class,
 			PhysicalLinkVisualManager.getInstance());
+		vmMap.put(com.syrus.AMFICOM.map.SiteNode.class,
+				SiteNodeVisualManager.getInstance());
 	}
 
 	public static VisualManager getVisualManager(MapElement mapElement) {
