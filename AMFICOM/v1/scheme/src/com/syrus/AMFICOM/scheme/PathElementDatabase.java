62fe5db162e57cee4571ac3fc93f7b75d5c4380f
Maxim Selivanov <max.selivanov@gmail.com>
2005-04-19 15:21:58 +0400

    Major: implemented all methods

--- AMFICOM/v1/scheme/src/com/syrus/AMFICOM/scheme/PathElementDatabase.java
+++ AMFICOM/v1/scheme/src/com/syrus/AMFICOM/scheme/PathElementDatabase.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: PathElementDatabase.java,v 1.1 2005/04/01 13:59:07 bass Exp $
+ * $Id: PathElementDatabase.java,v 1.2 2005/04/19 11:21:58 max Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,36 +9,50 @@
 package com.syrus.AMFICOM.scheme;
 
 import com.syrus.AMFICOM.general.*;
+import com.syrus.AMFICOM.scheme.corba.PathElement_TransferablePackage.DataPackage.Kind;
+import com.syrus.util.Log;
+import com.syrus.util.database.DatabaseDate;
+
 import java.sql.*;
+import java.util.Date;
 import java.util.Set;
 
 /**
  * @author Andrew ``Bass'' Shcheglov
- * @author $Author: bass $
- * @version $Revision: 1.1 $, $Date: 2005/04/01 13:59:07 $
+ * @author $Author: max $
+ * @version $Revision: 1.2 $, $Date: 2005/04/19 11:21:58 $
  * @module scheme_v1
  */
 public final class PathElementDatabase extends StorableObjectDatabase {
+	
+	private static String columns;
+	private static String updateMultipleSQLValues;
+	
+	private PathElement fromStorableObject(StorableObject storableObject) throws IllegalDataException {
+		if (storableObject instanceof PathElement)
+			return (PathElement) storableObject;
+		throw new IllegalDataException("PathElementDatabase.fromStorableObject | Illegal Storable Object: " + storableObject.getClass().getName());
+	}
+	
 	/**
 	 * @param storableObjects
 	 * @throws IllegalDataException
 	 * @throws CreateObjectException
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#insert(Set)
 	 */
 	public void insert(final Set storableObjects)
 			throws IllegalDataException, CreateObjectException {
-		throw new UnsupportedOperationException();
+		super.insertEntities(storableObjects);
 	}
 
 	/**
 	 * @param storableObject
 	 * @throws IllegalDataException
 	 * @throws CreateObjectException
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#insert(com.syrus.AMFICOM.general.StorableObject)
 	 */
 	public void insert(StorableObject storableObject)
 			throws IllegalDataException, CreateObjectException {
-		throw new UnsupportedOperationException();
+		PathElement pe = fromStorableObject(storableObject);
+		super.insertEntity(pe);
 	}
 
 	/**
@@ -46,12 +60,12 @@ public final class PathElementDatabase extends StorableObjectDatabase {
 	 * @throws IllegalDataException
 	 * @throws ObjectNotFoundException
 	 * @throws RetrieveObjectException
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#retrieve(com.syrus.AMFICOM.general.StorableObject)
 	 */
 	public void retrieve(StorableObject storableObject)
 			throws IllegalDataException, ObjectNotFoundException,
 			RetrieveObjectException {
-		throw new UnsupportedOperationException();
+		PathElement pe = fromStorableObject(storableObject);
+		super.retrieveEntity(pe);
 	}
 
 	/**
@@ -61,45 +75,66 @@ public final class PathElementDatabase extends StorableObjectDatabase {
 	 * @throws IllegalDataException
 	 * @throws ObjectNotFoundException
 	 * @throws RetrieveObjectException
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#retrieveObject(com.syrus.AMFICOM.general.StorableObject, int, java.lang.Object)
 	 */
 	public Object retrieveObject(StorableObject storableObject,
 			int retrieveKind, Object arg)
 			throws IllegalDataException, ObjectNotFoundException,
 			RetrieveObjectException {
-		throw new UnsupportedOperationException();
+		PathElement pe = fromStorableObject(storableObject);
+		switch (retrieveKind) {
+		default:
+			Log.errorMessage("Unknown retrieve kind: " + retrieveKind + " for " + this.getEnityName()
+					+ " '" + pe.getId() + "'; argument: " + arg);
+			return null;
+		}
 	}
-
-	/**
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#getColumnsTmpl()
-	 */
+	
 	protected String getColumnsTmpl() {
-		throw new UnsupportedOperationException();
+		if (columns == null) {
+			columns = PathElementWrapper.COLUMN_PARENT_SCHEME_PATH_ID + COMMA
+					+ PathElementWrapper.COLUMN_SEQUENTIAL_NUMBER + COMMA
+					+ PathElementWrapper.COLUMN_KIND + COMMA
+					+ PathElementWrapper.COLUMN_START_ABSTRACT_SCHEME_PORT_ID + COMMA
+					+ PathElementWrapper.COLUMN_END_ABSTRACT_SCHEME_PORT_ID + COMMA
+					+ PathElementWrapper.COLUMN_SCHEME_CABLE_THREAD_ID + COMMA
+					+ PathElementWrapper.COLUMN_SCHEME_LINK_ID;					
+		}
+		return columns;
 	}
 
-	/**
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#getEnityName()
-	 */
 	protected String getEnityName() {
-		throw new UnsupportedOperationException();
+		return ObjectEntities.PATH_ELEMENT_ENTITY;
 	}
 
-	/**
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#getUpdateMultipleSQLValuesTmpl()
-	 */
 	protected String getUpdateMultipleSQLValuesTmpl() {
-		throw new UnsupportedOperationException();
+		if (updateMultipleSQLValues == null) {
+			updateMultipleSQLValues = QUESTION + COMMA
+					+ QUESTION + COMMA
+					+ QUESTION + COMMA
+					+ QUESTION + COMMA
+					+ QUESTION + COMMA
+					+ QUESTION + COMMA
+					+ QUESTION;
+		}
+		return updateMultipleSQLValues;
 	}
 
 	/**
 	 * @param storableObject
 	 * @throws IllegalDataException
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#getUpdateSingleSQLValuesTmpl(com.syrus.AMFICOM.general.StorableObject)
 	 */
 	protected String getUpdateSingleSQLValuesTmpl(
 			StorableObject storableObject)
 			throws IllegalDataException {
-		throw new UnsupportedOperationException();
+		PathElement pe = fromStorableObject(storableObject);
+		String sql = DatabaseIdentifier.toSQLString(pe.getParentSchemePath().getId()) + COMMA
+				+ APOSTOPHE + pe.getSequentialNumber() + APOSTOPHE + COMMA
+				+ APOSTOPHE + pe.getKind().value() + APOSTOPHE + COMMA
+				+ DatabaseIdentifier.toSQLString(pe.getStartAbstractSchemePort().getId()) + COMMA
+				+ DatabaseIdentifier.toSQLString(pe.getEndAbstractSchemePort().getId()) + COMMA
+				+ DatabaseIdentifier.toSQLString(pe.getSchemeCableLink().getId()) + COMMA
+				+ DatabaseIdentifier.toSQLString(pe.getSchemeLink().getId());
+		return sql;
 	}
 
 	/**
@@ -108,14 +143,21 @@ public final class PathElementDatabase extends StorableObjectDatabase {
 	 * @param startParameterNumber
 	 * @throws IllegalDataException
 	 * @throws SQLException
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#setEntityForPreparedStatementTmpl(com.syrus.AMFICOM.general.StorableObject, java.sql.PreparedStatement, int)
 	 */
 	protected int setEntityForPreparedStatementTmpl(
 			StorableObject storableObject,
 			PreparedStatement preparedStatement,
 			int startParameterNumber) throws IllegalDataException,
 			SQLException {
-		throw new UnsupportedOperationException();
+		PathElement pe = fromStorableObject(storableObject);
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, pe.getParentSchemePath().getId());
+		preparedStatement.setInt(++startParameterNumber, pe.getSequentialNumber());
+		preparedStatement.setInt(++startParameterNumber, pe.getKind().value());
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, pe.getStartAbstractSchemePort().getId());
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, pe.getEndAbstractSchemePort().getId());
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, pe.getSchemeCableLink().getId());
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, pe.getSchemeLink().getId());
+		return startParameterNumber;
 	}
 
 	/**
@@ -124,12 +166,31 @@ public final class PathElementDatabase extends StorableObjectDatabase {
 	 * @throws IllegalDataException
 	 * @throws RetrieveObjectException
 	 * @throws SQLException
-	 * @see com.syrus.AMFICOM.general.StorableObjectDatabase#updateEntityFromResultSet(com.syrus.AMFICOM.general.StorableObject, java.sql.ResultSet)
 	 */
 	protected StorableObject updateEntityFromResultSet(
 			StorableObject storableObject, ResultSet resultSet)
 			throws IllegalDataException, RetrieveObjectException,
 			SQLException {
-		throw new UnsupportedOperationException();
+		PathElement pathElement;
+		if (storableObject == null) {
+			Date created = new Date(); 
+			pathElement = new PathElement(DatabaseIdentifier.getIdentifier(resultSet, StorableObjectWrapper.COLUMN_ID),
+					created, created, null, null, 0L, null, 0, null, null, null, null, null);
+		} else {
+			pathElement = fromStorableObject(storableObject);
+		}
+		pathElement.setAttributes(DatabaseDate.fromQuerySubString(resultSet, StorableObjectWrapper.COLUMN_CREATED),
+				DatabaseDate.fromQuerySubString(resultSet, StorableObjectWrapper.COLUMN_MODIFIED),
+				DatabaseIdentifier.getIdentifier(resultSet, StorableObjectWrapper.COLUMN_CREATOR_ID),
+				DatabaseIdentifier.getIdentifier(resultSet, StorableObjectWrapper.COLUMN_MODIFIER_ID),
+				resultSet.getLong(StorableObjectWrapper.COLUMN_VERSION),
+				DatabaseIdentifier.getIdentifier(resultSet, PathElementWrapper.COLUMN_PARENT_SCHEME_PATH_ID),
+				resultSet.getInt(PathElementWrapper.COLUMN_SEQUENTIAL_NUMBER),
+				Kind.from_int(resultSet.getInt(PathElementWrapper.COLUMN_KIND)),
+				DatabaseIdentifier.getIdentifier(resultSet, PathElementWrapper.COLUMN_START_ABSTRACT_SCHEME_PORT_ID),
+				DatabaseIdentifier.getIdentifier(resultSet, PathElementWrapper.COLUMN_END_ABSTRACT_SCHEME_PORT_ID),
+				DatabaseIdentifier.getIdentifier(resultSet, PathElementWrapper.COLUMN_SCHEME_CABLE_THREAD_ID),
+				DatabaseIdentifier.getIdentifier(resultSet, PathElementWrapper.COLUMN_SCHEME_LINK_ID));
+		return pathElement;
 	}
 }
