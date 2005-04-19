c4d59dde16beabf05d246e88a9ed024a4624feab
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-04-19 14:44:16 +0400

    MINOR: removed unused constant.

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ErrorMessages.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ErrorMessages.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ErrorMessages.java,v 1.10 2005/03/29 15:57:50 bass Exp $
+ * $Id: ErrorMessages.java,v 1.11 2005/04/19 10:44:16 bass Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -11,7 +11,7 @@ package com.syrus.AMFICOM.general;
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.10 $, $Date: 2005/03/29 15:57:50 $
+ * @version $Revision: 1.11 $, $Date: 2005/04/19 10:44:16 $
  * @module general_v1
  */
 public interface ErrorMessages {
@@ -22,7 +22,6 @@ public interface ErrorMessages {
 	String CIRCULAR_DEPS_PROHIBITED = "circular dependencies not allowed"; //$NON-NLS-1$
 	String MULTIPLE_PARENTS_PROHIBITED = "multiple parents not allowed"; //$NON-NLS-1$
 	String PARENTLESS_CHILD_PROHIBITED = "exactly one parent required"; //$NON-NLS-1$
-	String COLLECTION_IS_A_SET = "logically, this collection must be a set; double-addition of elements not allowed"; //$NON-NLS-1$
 	String REMOVAL_OF_AN_ABSENT_PROHIBITED = "this collection is missing the element to be removed; removal of an absent member not allowed"; //$NON-NLS-1$
 	String OUT_OF_LIBRARY_HIERARCHY = "object doesn't belong to library hierarchy"; //$NON-NLS-1$
 	String UNSUPPORTED_CHILD_TYPE = "object doesn't realize any of interfaces supported within this library hierarchy"; //$NON-NLS-1$
