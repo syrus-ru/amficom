f2df96680d2697cc8b27475634e022397fb0274a
cvs-fast-export <cvs-fast-export>
2005-04-19 15:41:17 +0000

    Synthetic commit for incomplete tag LAST_SLICE_CACHE

--- AMFICOM/v2/database/population/measurement/dictionary/testtemporaltype.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO testtemporaltype (id, name, comments)
- VALUES (0, 'TEST_TEMPORAL_TYPE_ONETIME', 'Одноразовый');
-INSERT INTO testtemporaltype (id, name, comments)
- VALUES (1, 'TEST_TEMPORAL_TYPE_PERIODICAL', 'Периодический');
-INSERT INTO testtemporaltype (id, name, comments)
- VALUES (2, 'TEST_TEMPORAL_TYPE_TIMETABLE', 'По расписанию');
-
-COMMIT;
