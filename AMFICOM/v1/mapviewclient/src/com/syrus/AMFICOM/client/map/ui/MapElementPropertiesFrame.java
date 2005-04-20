63021df8688668e2cf982e86e19374fa18b236ae
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-04-20 20:16:37 +0400

    ADD: display Map amd MapView properties

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapElementPropertiesFrame.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapElementPropertiesFrame.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapElementPropertiesFrame.java,v 1.3 2005/04/19 15:50:12 krupenn Exp $
+ * $Id: MapElementPropertiesFrame.java,v 1.4 2005/04/20 16:16:37 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -17,14 +17,17 @@ import com.syrus.AMFICOM.Client.General.Event.MapEvent;
 import com.syrus.AMFICOM.Client.General.Event.MapNavigateEvent;
 import com.syrus.AMFICOM.Client.General.Event.OperationEvent;
 import com.syrus.AMFICOM.Client.General.Model.ApplicationContext;
+import com.syrus.AMFICOM.Client.Map.Props.MapViewVisualManager;
 import com.syrus.AMFICOM.Client.Map.Props.MapVisualManager;
 import com.syrus.AMFICOM.client_.general.ui_.VisualManager;
 import com.syrus.AMFICOM.client_.scheme.ui.GeneralPropertiesFrame;
+import com.syrus.AMFICOM.map.Map;
 import com.syrus.AMFICOM.map.MapElement;
+import com.syrus.AMFICOM.mapview.MapView;
 
 /**
  *  Окно отображения свойств элемента карты
- * @version $Revision: 1.3 $, $Date: 2005/04/19 15:50:12 $
+ * @version $Revision: 1.4 $, $Date: 2005/04/20 16:16:37 $
  * @author $Author: krupenn $
  * @module mapviewclient_v1
  */
@@ -32,8 +35,15 @@ public final class MapElementPropertiesFrame extends GeneralPropertiesFrame
 {
 	public void stateChanged(ChangeEvent e) {
 		super.stateChanged(e);
-		if(this.aContext.getDispatcher() != null)
-			this.aContext.getDispatcher().notify(new MapEvent(this, MapEvent.MAP_CHANGED));
+		if(this.aContext.getDispatcher() != null) {
+			Object object = e.getSource();
+			if(object instanceof MapElement)
+				this.aContext.getDispatcher().notify(new MapEvent(this, MapEvent.MAP_CHANGED));
+			else if(object instanceof Map)
+				this.aContext.getDispatcher().notify(new MapEvent(this, MapEvent.MAP_SELECTED));
+			else if(object instanceof MapView)
+				this.aContext.getDispatcher().notify(new MapEvent(this, MapEvent.MAP_VIEW_CHANGED));
+		}
 	}
 
 	public MapElementPropertiesFrame(String title, ApplicationContext aContext) {
@@ -44,10 +54,15 @@ public final class MapElementPropertiesFrame extends GeneralPropertiesFrame
 		if(this.aContext != null)
 			if(this.aContext.getDispatcher() != null) {
 				this.aContext.getDispatcher().unregister(this, MapEvent.MAP_NAVIGATE);
+				this.aContext.getDispatcher().unregister(this, MapEvent.MAP_SELECTED);
+				this.aContext.getDispatcher().unregister(this, MapEvent.MAP_VIEW_SELECTED);
 			}
 		super.setContext(aContext);
-		if(aContext.getDispatcher() != null)
+		if(aContext.getDispatcher() != null) {
 			aContext.getDispatcher().register(this, MapEvent.MAP_NAVIGATE);
+			aContext.getDispatcher().register(this, MapEvent.MAP_SELECTED);
+			aContext.getDispatcher().register(this, MapEvent.MAP_VIEW_SELECTED);
+		}
 	}
 	
 	public void operationPerformed(OperationEvent e) {
@@ -62,6 +77,21 @@ public final class MapElementPropertiesFrame extends GeneralPropertiesFrame
 					this.editor.setObject(mapElement);
 			}
 		}
+		else if (e.getActionCommand().equals(MapEvent.MAP_SELECTED)) {
+			Map map = (Map )e.getSource();
+			VisualManager vm = MapVisualManager.getInstance();
+			super.setVisualManager(vm);				
+			if (this.editor != null)
+				this.editor.setObject(map);
+		}
+		else if (e.getActionCommand().equals(MapEvent.MAP_VIEW_SELECTED)) {
+			MapView mapView = (MapView )e.getSource();
+			VisualManager vm = MapViewVisualManager.getInstance();
+			super.setVisualManager(vm);				
+			if (this.editor != null)
+				this.editor.setObject(mapView);
+		}
+
 	}
 	
 }
