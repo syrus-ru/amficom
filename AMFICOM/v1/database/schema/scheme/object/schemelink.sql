788b46e2ba73c39bf4491f3ba8d45ad582f287fe
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-04-21 14:42:38 +0400

    ADD: full implementation.

--- AMFICOM/v1/database/schema/scheme/object/schemelink.sql
+++ AMFICOM/v1/database/schema/scheme/object/schemelink.sql
@@ -1,4 +1,4 @@
--- $Id: schemelink.sql,v 1.3 2005/03/22 13:36:03 bass Exp $
+-- $Id: schemelink.sql,v 1.4 2005/04/21 10:42:38 bass Exp $
 
 CREATE TABLE "SchemeLink" (
 	id VARCHAR2(32 CHAR) NOT NULL,
@@ -14,8 +14,17 @@ CREATE TABLE "SchemeLink" (
 --
 	physical_length BINARY_DOUBLE NOT NULL,
 	optical_length BINARY_DOUBLE NOT NULL,
+--
 	link_type_id VARCHAR2(32 CHAR),
 	link_id VARCHAR2(32 CHAR),
+--
+	site_node_id VARCHAR2(32 CHAR),
+	source_scheme_port_id VARCHAR2(32 CHAR),
+	target_scheme_port_id VARCHAR2(32 CHAR),
+--
+	parent_scheme_id VARCHAR2(32 CHAR),
+	parent_scheme_element_id VARCHAR2(32 CHAR),
+	parent_scheme_proto_element_id VARCHAR2(32 CHAR),
 --
 	CONSTRAINT schmlnk_pk PRIMARY KEY(id),
 --
@@ -27,9 +36,42 @@ CREATE TABLE "SchemeLink" (
 	CONSTRAINT schmlnk_lnk_tp_fk FOREIGN KEY(link_type_id)
 		REFERENCES LinkType(id) ON DELETE CASCADE,
 	CONSTRAINT schmlnk_lnk_fk FOREIGN KEY(link_id)
-		REFERENCES Link(id) ON DELETE CASCADE
+		REFERENCES Link(id) ON DELETE CASCADE,
+--
+	CONSTRAINT schmlnk_site_node_fk FOREIGN KEY(site_node_id)
+		REFERENCES SiteNode(id) ON DELETE CASCADE,
+	CONSTRAINT schmlnk_source_scheme_port_fk FOREIGN KEY(source_scheme_port_id)
+		REFERENCES "SchemePort"(id) ON DELETE CASCADE,
+	CONSTRAINT schmlnk_target_scheme_port_fk FOREIGN KEY(target_scheme_port_id)
+		REFERENCES "SchemePort"(id) ON DELETE CASCADE,
+--
+	CONSTRAINT schmlnk_prnt_schm_fk FOREIGN KEY(parent_scheme_id)
+		REFERENCES "Scheme"(id) ON DELETE CASCADE,
+	CONSTRAINT schmlnk_prnt_schm_elmnt_fk FOREIGN KEY(parent_scheme_element_id)
+		REFERENCES "SchemeElement"(id) ON DELETE CASCADE,
+	CONSTRAINT schmlnk_prnt_schm_prt_elmnt_fk FOREIGN KEY(parent_scheme_proto_element_id)
+		REFERENCES "SchemeProtoElement"(id) ON DELETE CASCADE,
+--
+	-- Boolean XOR: only one of link_type_id and link_id may be defined,
+	-- and only one may be null.
+	CONSTRAINT schmlnk_link_chk CHECK
+		((link_type_id IS NULL
+		AND link_id IS NOT NULL)
+		OR (link_type_id IS NOT NULL
+		AND link_id IS NULL)),
+	-- Exactly one parent allowed.
+	CONSTRAINT schmlnk_prnt_chk CHECK
+		((parent_scheme_id IS NOT NULL
+		AND parent_scheme_element_id IS NULL
+		AND parent_scheme_proto_element_id IS NULL)
+		OR (parent_scheme_id IS NULL
+		AND parent_scheme_element_id IS NOT NULL
+		AND parent_scheme_proto_element_id IS NULL)
+		OR (parent_scheme_id IS NULL
+		AND parent_scheme_element_id IS NULL
+		AND parent_scheme_proto_element_id IS NOT NULL))
 );
 
-COMMENT ON TABLE "SchemeLink" IS '$Id: schemelink.sql,v 1.3 2005/03/22 13:36:03 bass Exp $';
+COMMENT ON TABLE "SchemeLink" IS '$Id: schemelink.sql,v 1.4 2005/04/21 10:42:38 bass Exp $';
 
 CREATE SEQUENCE "SchemeLink_Seq" ORDER;
