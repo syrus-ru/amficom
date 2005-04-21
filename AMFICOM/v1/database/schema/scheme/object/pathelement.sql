2bf6ffdd111d7d0108a6415eff6d5ceaf34f26a1
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-04-21 14:42:21 +0400

    BUGFIX: now valid SQL.

--- AMFICOM/v1/database/schema/scheme/object/pathelement.sql
+++ AMFICOM/v1/database/schema/scheme/object/pathelement.sql
@@ -1,4 +1,4 @@
--- $Id: pathelement.sql,v 1.2 2005/04/19 09:10:13 max Exp $
+-- $Id: pathelement.sql,v 1.3 2005/04/21 10:42:21 bass Exp $
 
 CREATE TABLE "PathElement" (
 	id VARCHAR2(32 CHAR) NOT NULL,
@@ -9,13 +9,13 @@ CREATE TABLE "PathElement" (
 	modifier_id VARCHAR2(32 CHAR) NOT NULL,
 	version NUMBER(19) NOT NULL,
 --
+	sequential_number NUMBER(10) NOT NULL,
+	kind NUMBER(1) NOT NULL,
+	start_abstract_scheme_port_id VARCHAR2(32 CHAR),
+        end_abstract_scheme_port_id VARCHAR2(32 CHAR),
+	scheme_cable_thread_id VARCHAR2(32 CHAR),
+	scheme_link_id VARCHAR2(32 CHAR),
 	parent_scheme_path_id VARCHAR2(32 CHAR),
-	sequential_number NUMBER(10),
-	kind NUMBER(1); NOT NULL,
-	start_abstract_scheme_port_id VARCHAR(32 CHAR),
-        end_abstract_scheme_port_id VARCHAR(32 CHAR),
-	scheme_cable_thread_id VARCHAR(32 CHAR),
-	scheme_link_id VARCHAR(32 CHAR),
 --
 	CONSTRAINT pathelement_pk PRIMARY KEY(id),
 --
@@ -24,31 +24,29 @@ CREATE TABLE "PathElement" (
 	CONSTRAINT pathelement_modifier_fk FOREIGN KEY(modifier_id)
 		REFERENCES "User"(id) ON DELETE CASCADE,
 --
-	CONSTRAINT pathelement_parschpathid FOREIGN KEY(parent_scheme_path_id)
-		REFERENCE SchemePath(id) ON DELETE CASCADE,
-	CONSTRAINT pathelement_stabsschportid FOREIGN KEY(start_abstract_scheme_port_id);
-		REFERENCE SchemePort(id) ON DELETE CASCADE,
-	CONSTRAINT pathelement_endabsschportid FOREIGN KEY(end_abstract_scheme_port_id);
-		REFERENCE SchemePort(id) ON DELETE CASCADE,
-	CONSTRAINT pathelement_cablthreadid ROREIGN KEY(scheme_cable_thread_id);
-		REFERENCE CableThread(id) ON DELETE CASCADE,
-	CONSTRAINT pathelement_linkid ROREIGN KEY(scheme_link_id);
-		REFERENCE Link(id) ON DELETE CASCADE,
+	CONSTRAINT pathelement_schm_cbl_thread_fk ROREIGN KEY(scheme_cable_thread_id)
+		REFERENCES "SchemeCableThread"(id) ON DELETE CASCADE,
+	CONSTRAINT pathelement_scheme_link_fk ROREIGN KEY(scheme_link_id)
+		REFERENCES "SchemeLink"(id) ON DELETE CASCADE,
+	CONSTRAINT pathelement_prnt_schm_path_fk FOREIGN KEY(parent_scheme_path_id)
+		REFERENCES "SchemePath"(id) ON DELETE CASCADE,
 --
-	CONSTRAINT pathelement_param_chk CHECK
+	-- TODO: check for kind value, too.
+	CONSTRAINT pathelement_kind_chk CHECK
 		((start_abstract_scheme_port_id IS NOT NULL 
-			AND end_abstract_scheme_port_id IS NOT NULL
-			AND scheme_cable_thread_id IS NULL 
-			AND scheme_link_id IS NULL)
+		AND end_abstract_scheme_port_id IS NOT NULL
+		AND scheme_cable_thread_id IS NULL 
+		AND scheme_link_id IS NULL)
 		OR (start_abstract_scheme_port_id IS NULL
-			AND end_abstract_scheme_port_id IS NULL
-			AND scheme_cable_thread_id IS NOT NULL
-			AND scheme_link_id IS NULL)
+		AND end_abstract_scheme_port_id IS NULL
+		AND scheme_cable_thread_id IS NOT NULL
+		AND scheme_link_id IS NULL)
 		OR (start_abstract_scheme_port_id IS NULL
-			AND end_abstract_scheme_port_id IS NULL
-			AND scheme_cable_thread_id IS NULL
-			AND scheme_link_id IS NOT NULL))
+		AND end_abstract_scheme_port_id IS NULL
+		AND scheme_cable_thread_id IS NULL
+		AND scheme_link_id IS NOT NULL))
+);
 
-COMMENT ON TABLE "PathElement" IS '$Id: pathelement.sql,v 1.2 2005/04/19 09:10:13 max Exp $';
+COMMENT ON TABLE "PathElement" IS '$Id: pathelement.sql,v 1.3 2005/04/21 10:42:21 bass Exp $';
 
 CREATE SEQUENCE "PathElement_Seq" ORDER;
