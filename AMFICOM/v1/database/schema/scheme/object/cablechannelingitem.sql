40016f84a985e3608368f4320afe146609a5f708
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-04-22 18:45:49 +0400

    REMOVE: name & description removed.

--- AMFICOM/v1/database/schema/scheme/object/cablechannelingitem.sql
+++ AMFICOM/v1/database/schema/scheme/object/cablechannelingitem.sql
@@ -1,4 +1,4 @@
--- $Id: cablechannelingitem.sql,v 1.1 2005/02/21 08:30:18 bass Exp $
+-- $Id: cablechannelingitem.sql,v 1.2 2005/04/22 14:45:49 bass Exp $
 
 CREATE TABLE "CableChannelingItem" (
 	id VARCHAR2(32 CHAR) NOT NULL,
@@ -9,9 +9,6 @@ CREATE TABLE "CableChannelingItem" (
 	modifier_id VARCHAR2(32 CHAR) NOT NULL,
 	version NUMBER(19) NOT NULL,
 --
-	name VARCHAR2(32 CHAR) NOT NULL,
-	description VARCHAR2(256 CHAR),
---
         CONSTRAINT cablchannelingitem_pk PRIMARY KEY(id),
 --
 	CONSTRAINT cablchannelingitem_creator_fk FOREIGN KEY(creator_id)
@@ -20,6 +17,6 @@ CREATE TABLE "CableChannelingItem" (
 		REFERENCES "User"(id) ON DELETE CASCADE
 );
 
-COMMENT ON TABLE "CableChannelingItem" IS '$Id: cablechannelingitem.sql,v 1.1 2005/02/21 08:30:18 bass Exp $';
+COMMENT ON TABLE "CableChannelingItem" IS '$Id: cablechannelingitem.sql,v 1.2 2005/04/22 14:45:49 bass Exp $';
 
 CREATE SEQUENCE "CableChannelingItem_Seq" ORDER;
