2b7a31a08e1cecadbd6c6a4651cc2d168db4868f
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-23 01:09:13 +0400

    MAJOR: Init servant connections, pools. Not completed yet

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/ClientSession.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/ClientSession.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ClientSession.java,v 1.2 2005/04/22 17:18:28 arseniy Exp $
+ * $Id: ClientSession.java,v 1.3 2005/04/22 21:09:13 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,24 +7,77 @@
  */
 package com.syrus.AMFICOM.general;
 
+import com.syrus.AMFICOM.administration.AdministrationObjectLoader;
+import com.syrus.AMFICOM.administration.AdministrationStorableObjectPool;
+import com.syrus.AMFICOM.administration.CORBAAdministrationObjectLoader;
+import com.syrus.AMFICOM.configuration.CORBAConfigurationObjectLoader;
+import com.syrus.AMFICOM.configuration.ConfigurationObjectLoader;
+import com.syrus.AMFICOM.configuration.ConfigurationStorableObjectPool;
 import com.syrus.AMFICOM.general.corba.AccessIdentifier_Transferable;
+import com.syrus.AMFICOM.measurement.CORBAMeasurementObjectLoader;
+import com.syrus.AMFICOM.measurement.MeasurementObjectLoader;
+import com.syrus.AMFICOM.measurement.MeasurementStorableObjectPool;
+import com.syrus.util.Application;
+import com.syrus.util.ApplicationProperties;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/04/22 17:18:28 $
+ * @version $Revision: 1.3 $, $Date: 2005/04/22 21:09:13 $
  * @author $Author: arseniy $
  * @module csbridge_v1
  */
 public final class ClientSession {
+	private static final String KEY_SERVER_HOSTNAME = "ServerHostname";
 
-	public static void init() {
-		
+	private static final String SERVER_HOSTNAME = Application.getInternetAddress();
+
+	private static ServantConnectionManager servantConnectionManager;
+
+	private static AccessIdentity accessIdentity;
+	private static AccessIdentifier_Transferable accessIdentityT;
+
+	private ClientSession() {
+		//singleton
 	}
 
+	public static void init() throws CommunicationException {
+		initServantConnection();
+		/*@todo here, before other operations, perform login*/
+		initObjectPools();
+	}
+
+	private static void initServantConnection() throws CommunicationException {
+		final String serverHostName = ApplicationProperties.getString(KEY_SERVER_HOSTNAME, SERVER_HOSTNAME);
+		servantConnectionManager = new ServantConnectionManager(serverHostName);
+	}
+
+	//???
+//	private static void initSessionContext() {
+//		SessionContext.init();
+//	}
+
 	private static void initObjectPools() {
-		
+		/*	General	*/
+		GeneralObjectLoader generalObjectLoader = new CORBAGeneralObjectLoader(servantConnectionManager);
+		GeneralStorableObjectPool.init(generalObjectLoader, StorableObjectResizableLRUMap.class);
+
+		/*	Administration	*/
+		AdministrationObjectLoader administrationObjectLoader = new CORBAAdministrationObjectLoader(servantConnectionManager);
+		AdministrationStorableObjectPool.init(administrationObjectLoader, StorableObjectResizableLRUMap.class);
+
+		/*	Configuration	*/
+		ConfigurationObjectLoader configurationObjectLoader = new CORBAConfigurationObjectLoader(servantConnectionManager);
+		ConfigurationStorableObjectPool.init(configurationObjectLoader, StorableObjectResizableLRUMap.class);
+
+		/*	Measurement	*/
+		MeasurementObjectLoader measurementObjectLoader = new CORBAMeasurementObjectLoader(servantConnectionManager);
+		MeasurementStorableObjectPool.init(measurementObjectLoader, StorableObjectResizableLRUMap.class);
+
+		/*	Something more pools...	*/
 	}
 
 	public static AccessIdentifier_Transferable getAccessIdentifierTransferable() {
-		return  (AccessIdentifier_Transferable) SessionContext.getAccessIdentity().getTransferable();
+		if (accessIdentityT == null)
+			accessIdentityT = (AccessIdentifier_Transferable) accessIdentity.getTransferable();
+		return accessIdentityT;
 	}
 }
