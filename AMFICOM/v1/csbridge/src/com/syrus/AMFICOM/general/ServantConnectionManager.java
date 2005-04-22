31560bdb9347a2959c757a7d866bbd053d186a6e
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-23 01:08:12 +0400

    MINOR: Renamed ServerConnectionManager to ServantConnectionManager. Takes hostname of server in constructor

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/ServantConnectionManager.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/ServantConnectionManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ServerConnectionManager.java,v 1.2 2005/04/22 17:18:16 arseniy Exp $
+ * $Id: ServantConnectionManager.java,v 1.1 2005/04/22 21:08:12 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,7 +7,6 @@
  */
 package com.syrus.AMFICOM.general;
 
-import com.syrus.AMFICOM.CORBA.AMFICOM;
 import com.syrus.AMFICOM.cmserver.corba.CMServer;
 import com.syrus.AMFICOM.general.corba.IdentifierGeneratorServer;
 import com.syrus.AMFICOM.mshserver.corba.MSHServer;
@@ -15,42 +14,39 @@ import com.syrus.util.Application;
 import com.syrus.util.ApplicationProperties;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/04/22 17:18:16 $
+ * @version $Revision: 1.1 $, $Date: 2005/04/22 21:08:12 $
  * @author $Author: arseniy $
  * @module csbridge_v1
  */
-public class ServerConnectionManager  implements IGSConnectionManager, CMServerConnectionManager {
-	/** @deprecated*/
-	private static final String KEY_SERVANT_NAME_AMFICOM = "AMFICOMServantName";
+public class ServantConnectionManager  implements IGSConnectionManager, CMServerConnectionManager {
+//	/** @deprecated*/
+//	private static final String KEY_SERVANT_NAME_AMFICOM = "AMFICOMServantName";
 
 	private static final String KEY_SERVANT_NAME_CMSERVER = "CMServerServantName";
 	private static final String KEY_SERVANT_NAME_MSHSERVER = "MSHServerServantName";
-	private static final String KEY_SERVER_HOSTNAME = "ServerHostname";
 
-	/** @deprecated*/
-	private static final String SERVANT_NAME_AMFICOM = "AMFICOM";
+//	/** @deprecated*/
+//	private static final String SERVANT_NAME_AMFICOM = "AMFICOM";
 
 	private static final String SERVANT_NAME_CMSERVER = "CMServer";
 	private static final String SERVANT_NAME_MSHSERVER = "MSHServer";
-	private static final String SERVER_HOSTNAME = Application.getInternetAddress();
 
-	/** @deprecated*/
-	private String amficomServantName;
+//	/** @deprecated*/
+//	private String amficomServantName;
 
 	private String cmServerServantName;
 	private String mshServerServantName;
 
 	private VerifiedConnectionManager verifiedConnectionManager;
 
-	public ServerConnectionManager() throws CommunicationException {
-		this.amficomServantName = ApplicationProperties.getString(KEY_SERVANT_NAME_AMFICOM, SERVANT_NAME_AMFICOM);
+	public ServantConnectionManager(final String serverHostName) throws CommunicationException {
+//		this.amficomServantName = ApplicationProperties.getString(KEY_SERVANT_NAME_AMFICOM, SERVANT_NAME_AMFICOM);
 		this.cmServerServantName = ApplicationProperties.getString(KEY_SERVANT_NAME_CMSERVER, SERVANT_NAME_CMSERVER);
 		this.mshServerServantName = ApplicationProperties.getString(KEY_SERVANT_NAME_MSHSERVER, SERVANT_NAME_MSHSERVER);
 
-		final String serverHostName = ApplicationProperties.getString(KEY_SERVER_HOSTNAME, SERVER_HOSTNAME);
 		CORBAServer corbaServer = new CORBAServer(serverHostName);
 		this.verifiedConnectionManager = new VerifiedConnectionManager(corbaServer,
-				new String[] {this.amficomServantName,
+				new String[] {/*this.amficomServantName,*/
 					this.cmServerServantName,
 					this.mshServerServantName});
 
@@ -67,22 +63,22 @@ public class ServerConnectionManager  implements IGSConnectionManager, CMServerC
 			return null;
 		}
 	}
-
-	/**
-	 * @deprecated
-	 * @return
-	 * @throws CommunicationException
-	 */
-	public AMFICOM getAMFICOMReference() throws CommunicationException {
-		try {
-			return (AMFICOM) this.verifiedConnectionManager.getVerifiableReference(this.amficomServantName);
-		}
-		catch (IllegalDataException e) {
-			// Never
-			assert false;
-			return null;
-		}
-	}
+//
+//	/**
+//	 * @deprecated
+//	 * @return
+//	 * @throws CommunicationException
+//	 */
+//	public AMFICOM getAMFICOMReference() throws CommunicationException {
+//		try {
+//			return (AMFICOM) this.verifiedConnectionManager.getVerifiableReference(this.amficomServantName);
+//		}
+//		catch (IllegalDataException e) {
+//			// Never
+//			assert false;
+//			return null;
+//		}
+//	}
 
 	public CMServer getCMServerReference() throws CommunicationException {
 		try {
