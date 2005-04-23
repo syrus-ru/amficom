d3fc6d08d51b6248ddacae1a34a63b04a74b2472
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-23 18:09:23 +0400

    MINOR: Removed unused commented code

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/ServantConnectionManager.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/ServantConnectionManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ServantConnectionManager.java,v 1.2 2005/04/22 21:09:33 arseniy Exp $
+ * $Id: ServantConnectionManager.java,v 1.3 2005/04/23 14:09:23 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -13,25 +13,17 @@ import com.syrus.AMFICOM.mshserver.corba.MSHServer;
 import com.syrus.util.ApplicationProperties;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/04/22 21:09:33 $
+ * @version $Revision: 1.3 $, $Date: 2005/04/23 14:09:23 $
  * @author $Author: arseniy $
  * @module csbridge_v1
  */
 public class ServantConnectionManager  implements IGSConnectionManager, CMServerConnectionManager {
-//	/** @deprecated*/
-//	private static final String KEY_SERVANT_NAME_AMFICOM = "AMFICOMServantName";
 
 	private static final String KEY_SERVANT_NAME_CMSERVER = "CMServerServantName";
 	private static final String KEY_SERVANT_NAME_MSHSERVER = "MSHServerServantName";
 
-//	/** @deprecated*/
-//	private static final String SERVANT_NAME_AMFICOM = "AMFICOM";
-
 	private static final String SERVANT_NAME_CMSERVER = "CMServer";
 	private static final String SERVANT_NAME_MSHSERVER = "MSHServer";
-
-//	/** @deprecated*/
-//	private String amficomServantName;
 
 	private String cmServerServantName;
 	private String mshServerServantName;
@@ -39,17 +31,14 @@ public class ServantConnectionManager  implements IGSConnectionManager, CMServer
 	private VerifiedConnectionManager verifiedConnectionManager;
 
 	public ServantConnectionManager(final String serverHostName) throws CommunicationException {
-//		this.amficomServantName = ApplicationProperties.getString(KEY_SERVANT_NAME_AMFICOM, SERVANT_NAME_AMFICOM);
 		this.cmServerServantName = ApplicationProperties.getString(KEY_SERVANT_NAME_CMSERVER, SERVANT_NAME_CMSERVER);
 		this.mshServerServantName = ApplicationProperties.getString(KEY_SERVANT_NAME_MSHSERVER, SERVANT_NAME_MSHSERVER);
 
 		CORBAServer corbaServer = new CORBAServer(serverHostName);
 		this.verifiedConnectionManager = new VerifiedConnectionManager(corbaServer,
-				new String[] {/*this.amficomServantName,*/
-					this.cmServerServantName,
-					this.mshServerServantName});
+				new String[] {this.cmServerServantName, this.mshServerServantName});
 
-		//@todo Add here some code to startup client's own servant
+		// @todo Add here some code to startup client's own servant
 	}
 
 	public IdentifierGeneratorServer getVerifiedIGSReference() throws CommunicationException {
@@ -62,22 +51,6 @@ public class ServantConnectionManager  implements IGSConnectionManager, CMServer
 			return null;
 		}
 	}
-//
-//	/**
-//	 * @deprecated
-//	 * @return
-//	 * @throws CommunicationException
-//	 */
-//	public AMFICOM getAMFICOMReference() throws CommunicationException {
-//		try {
-//			return (AMFICOM) this.verifiedConnectionManager.getVerifiableReference(this.amficomServantName);
-//		}
-//		catch (IllegalDataException e) {
-//			// Never
-//			assert false;
-//			return null;
-//		}
-//	}
 
 	public CMServer getCMServerReference() throws CommunicationException {
 		try {
