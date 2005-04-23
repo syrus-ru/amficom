a13900dbe935250aa99d97958605fd03d1633953
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-23 17:31:41 +0400

    ADD: Method 'getAccessIdentityTransferable' and field 'accessIdentityT' for caching

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/SessionContext.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/SessionContext.java
@@ -1,5 +1,5 @@
 /*
-* $Id: SessionContext.java,v 1.4 2005/04/08 13:00:07 arseniy Exp $
+* $Id: SessionContext.java,v 1.5 2005/04/23 13:31:41 arseniy Exp $
 *
 * Copyright ¿ 2004 Syrus Systems.
 * Dept. of Science & Technology.
@@ -8,9 +8,11 @@
 
 package com.syrus.AMFICOM.general;
 
+import com.syrus.AMFICOM.general.corba.AccessIdentity_Transferable;
+
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/04/08 13:00:07 $
+ * @version $Revision: 1.5 $, $Date: 2005/04/23 13:31:41 $
  * @author $Author: arseniy $
  * @module general_v1
  */
@@ -18,6 +20,8 @@ public final class SessionContext {
 
 	private static AccessIdentity accessIdentity;
 	private static String serverHostName;
+
+	private static AccessIdentity_Transferable accessIdentityT;
 	
 	private SessionContext() {
 		// singleton
@@ -31,6 +35,12 @@ public final class SessionContext {
 	
 	public static AccessIdentity getAccessIdentity() {
 		return SessionContext.accessIdentity;
+	}
+
+	public static AccessIdentity_Transferable getAccessIdentityTransferable() {
+		if (accessIdentityT == null)
+			accessIdentityT = (AccessIdentity_Transferable) accessIdentity.getTransferable();
+		return accessIdentityT;
 	}
 
 	public static String getServerHostName() {
