a52ce39953e2ac82bc770b0024f044b484bb35ee
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-04-26 20:07:31 +0400

    COSMETIC: formatting

--- AMFICOM/v1/mapview/src/com/syrus/AMFICOM/mapview/Selection.java
+++ AMFICOM/v1/mapview/src/com/syrus/AMFICOM/mapview/Selection.java
@@ -1,12 +1,10 @@
 /**
- * $Id: Selection.java,v 1.7 2005/04/13 10:05:56 krupenn Exp $
+ * $Id: Selection.java,v 1.8 2005/04/26 16:07:31 krupenn Exp $ 
  *
  * Syrus Systems
- * Научно-технический центр
- * Проект: АМФИКОМ Автоматизированный МногоФункциональный
- *         Интеллектуальный Комплекс Объектного Мониторинга
- *
- * Платформа: java 1.4.1
+ * Научно-технический центр 
+ * Проект: АМФИКОМ Автоматизированный
+ * МногоФункциональный Интеллектуальный Комплекс Объектного Мониторинга
  */
 
 package com.syrus.AMFICOM.mapview;
@@ -30,13 +28,11 @@ import java.util.Set;
  * Набор выбранных элементов.
  * 
  * @author $Author: krupenn $
- * @version $Revision: 1.7 $, $Date: 2005/04/13 10:05:56 $
+ * @version $Revision: 1.8 $, $Date: 2005/04/26 16:07:31 $
  * @module mapviewclient_v1
  * @todo copy/paste, properties
  */
-public final class Selection
-    implements MapElement 
-{
+public final class Selection implements MapElement {
 	/**
 	 * Список выбранных элементов.
 	 */
@@ -46,32 +42,37 @@ public final class Selection
 	 * Общий центр выбранных элементов.
 	 */
 	protected DoublePoint location = new DoublePoint(0, 0);
-	
-	/** 
-	 * Выбраны только топологические узлы. Используется для
-	 * вставки сетевых узлов вместо топологических. 
+
+	/**
+	 * Выбраны только топологические узлы. Используется для вставки сетевых
+	 * узлов вместо топологических.
 	 */
 	private boolean physicalNodeSelection = true;
-	/** 
-	 * Выбраны только непривязанные узлы. Используется для генерации
-	 * узлов топологический схемы по непривязанным элементам.
+
+	/**
+	 * Выбраны только непривязанные узлы. Используется для генерации узлов
+	 * топологический схемы по непривязанным элементам.
 	 */
 	private boolean unboundNodeSelection = true;
-	/** 
+
+	/**
 	 * Выбраны только непривязанные линии. Используется для генерации
 	 * топологических линий по непривязанным.
 	 */
 	private boolean unboundLinkSelection = true;
-	/** 
+
+	/**
 	 * Выбраны только непривязанные кабельные пути. Используется для генерации
 	 * топологических линий по непривязанным с привязкой каделя к ним.
 	 */
 	private boolean unboundCableSelection = true;
+
 	/**
 	 * Выбраны только непривязанные элементы. Используется для генерации
 	 * топологических эоементов по непривязанным.
 	 */
 	private boolean unboundSelection = true;
+
 	/**
 	 * Выбраны только линии. Используется для работы с колодцами.
 	 */
@@ -79,50 +80,45 @@ public final class Selection
 
 	/**
 	 * Конструктор
+	 * 
 	 * @param map топологическая схема
 	 */
-	public Selection(Map map)
-	{
+	public Selection(Map map) {
+		// empty
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public boolean isRemoved()
-	{
+	public boolean isRemoved() {
 		return false;
 	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public void setRemoved(boolean removed)
-	{// cannot be removed
-	}
 
 	/**
 	 * {@inheritDoc}
-	 * Suppress since this class is transient 
 	 */
-	public void setAlarmState(boolean alarmState)
-	{
+	public void setRemoved(boolean removed) {
+		// cannot be removed
+	}
+
+	/**
+	 * {@inheritDoc} Suppress since this class is transient
+	 */
+	public void setAlarmState(boolean alarmState) {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public boolean getAlarmState()
-	{
+	public boolean getAlarmState() {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
 	 * Очистить список выделенных элементов.
 	 */
-	public void clear()
-	{
+	public void clear() {
 		this.elements.clear();
 		recalcLocation();
 		recalcType();
@@ -130,10 +126,10 @@ public final class Selection
 
 	/**
 	 * Добавить в список новый выделенный элемент карты.
+	 * 
 	 * @param mapElement элемент карты
 	 */
-	public void add(MapElement mapElement)
-	{
+	public void add(MapElement mapElement) {
 		this.elements.add(mapElement);
 		recalcLocation();
 		recalcType();
@@ -141,10 +137,10 @@ public final class Selection
 
 	/**
 	 * Добавить в список множество выделенных элементов.
+	 * 
 	 * @param coll набор элементов
 	 */
-	public void addAll(Set coll)
-	{
+	public void addAll(Set coll) {
 		this.elements.addAll(coll);
 		recalcLocation();
 		recalcType();
@@ -152,10 +148,10 @@ public final class Selection
 
 	/**
 	 * Убрать из списка элемент карты.
+	 * 
 	 * @param mapElement элемент карты
 	 */
-	public void remove(MapElement mapElement)
-	{
+	public void remove(MapElement mapElement) {
 		this.elements.remove(mapElement);
 		recalcLocation();
 		recalcType();
@@ -164,22 +160,19 @@ public final class Selection
 	/**
 	 * {@inheritDoc}
 	 */
-	public DoublePoint getLocation()
-	{
+	public DoublePoint getLocation() {
 		return this.location;
 	}
 
 	/**
 	 * Пересчитать ГМТ центров выделенных элементов.
 	 */
-	protected void recalcLocation()
-	{
+	protected void recalcLocation() {
 		MapElement me;
 		double x = 0.0D;
 		double y = 0.0D;
-		for(Iterator it = this.elements.iterator(); it.hasNext();)
-		{
-			me = (MapElement)it.next();
+		for(Iterator it = this.elements.iterator(); it.hasNext();) {
+			me = (MapElement )it.next();
 			DoublePoint pt = me.getLocation();
 
 			x += pt.getX();
@@ -187,15 +180,14 @@ public final class Selection
 		}
 		x /= this.elements.size();
 		y /= this.elements.size();
-		
+
 		this.location.setLocation(x, y);
 	}
 
 	/**
 	 * определить флаги выборки.
 	 */
-	private void recalcType()
-	{
+	private void recalcType() {
 		this.physicalNodeSelection = true;
 		this.unboundNodeSelection = true;
 		this.unboundLinkSelection = true;
@@ -203,22 +195,20 @@ public final class Selection
 		this.unboundSelection = true;
 		this.physicalLinkSelection = true;
 
-		for(Iterator it = this.elements.iterator(); it.hasNext();)
-		{
-			MapElement me = (MapElement)it.next();
-			if(! (me instanceof TopologicalNode))
+		for(Iterator it = this.elements.iterator(); it.hasNext();) {
+			MapElement me = (MapElement )it.next();
+			if(!(me instanceof TopologicalNode))
 				this.physicalNodeSelection = false;
-			if(! (me instanceof UnboundNode))
+			if(!(me instanceof UnboundNode))
 				this.unboundNodeSelection = false;
-			if(! (me instanceof UnboundLink))
+			if(!(me instanceof UnboundLink))
 				this.unboundLinkSelection = false;
-			if(! (me instanceof CablePath))
+			if(!(me instanceof CablePath))
 				this.unboundCableSelection = false;
-			if(	! (me instanceof UnboundNode) &&
-				! (me instanceof UnboundLink) &&
-				! (me instanceof CablePath))
+			if(!(me instanceof UnboundNode) && !(me instanceof UnboundLink)
+					&& !(me instanceof CablePath))
 				this.unboundSelection = false;
-			if(! (me instanceof PhysicalLink))
+			if(!(me instanceof PhysicalLink))
 				this.physicalLinkSelection = false;
 		}
 	}
@@ -226,183 +216,148 @@ public final class Selection
 	/**
 	 * {@inheritDoc}
 	 */
-	public void setLocation(DoublePoint aLocation)
-	{
-		throw new UnsupportedOperationException("Cannot set location to selection");
+	public void setLocation(DoublePoint aLocation) {
+		throw new UnsupportedOperationException(
+				"Cannot set location to selection");
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public boolean isSelected()
-	{
+	public boolean isSelected() {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public MapElementState getState()
-	{
+	public MapElementState getState() {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * cannnot be selected
+	 * {@inheritDoc} cannnot be selected
 	 */
-	public void setSelected(boolean selected)
-	{
+	public void setSelected(boolean selected) {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public Identifier getId()
-	{
+	public Identifier getId() {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public String getName()
-	{
+	public String getName() {
 		return "selection";
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public Set getCharacteristics() 
-	{
+	public Set getCharacteristics() {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public void addCharacteristic(Characteristic ch)
-	{
+	public void addCharacteristic(Characteristic ch) {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public void removeCharacteristic(Characteristic ch)
-	{
+	public void removeCharacteristic(Characteristic ch) {
 		throw new UnsupportedOperationException();
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public String getToolTipText()
-	{
+	public String getToolTipText() {
 		return this.getName();
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public void revert(MapElementState state)
-	{
+	public void revert(MapElementState state) {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	/**
 	 * Получить список выделенных элементов.
 	 * @return список выделенных элементов
 	 */
-	public Set getElements()
-	{
+	public Set getElements() {
 		return Collections.unmodifiableSet(this.elements);
 	}
-
 
 	/**
 	 * Get {@link #physicalNodeSelection}.
 	 * @return this.physicalNodeSelection
 	 */
-	public boolean isPhysicalNodeSelection()
-	{
+	public boolean isPhysicalNodeSelection() {
 		return this.physicalNodeSelection;
 	}
-
 
 	/**
 	 * Get {@link #unboundNodeSelection}.
 	 * @return this.unboundNodeSelection
 	 */
-	public boolean isUnboundNodeSelection()
-	{
+	public boolean isUnboundNodeSelection() {
 		return this.unboundNodeSelection;
 	}
-
 
 	/**
 	 * Get {@link #unboundLinkSelection}.
 	 * @return this.unboundLinkSelection
 	 */
-	public boolean isUnboundLinkSelection()
-	{
+	public boolean isUnboundLinkSelection() {
 		return this.unboundLinkSelection;
 	}
-
 
 	/**
 	 * Get {@link #unboundSelection}.
 	 * @return this.unboundSelection
 	 */
-	public boolean isUnboundSelection()
-	{
+	public boolean isUnboundSelection() {
 		return this.unboundSelection;
 	}
-
 
 	/**
 	 * Get {@link #physicalLinkSelection}.
 	 * @return this.physicalLinkSelection
 	 */
-	public boolean isPhysicalLinkSelection()
-	{
+	public boolean isPhysicalLinkSelection() {
 		return this.physicalLinkSelection;
 	}
-
 
 	/**
 	 * Get {@link #unboundCableSelection}.
 	 * @return this.unboundCableSelection
 	 */
-	public boolean isUnboundCableSelection()
-	{
+	public boolean isUnboundCableSelection() {
 		return this.unboundCableSelection;
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * Suppress since this class is transient 
+	 * {@inheritDoc} Suppress since this class is transient
 	 */
-	public java.util.Map getExportMap()
-	{
+	public java.util.Map getExportMap() {
 		throw new UnsupportedOperationException();
 	}
 
