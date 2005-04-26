8944fedf70e25a96cd36c5e10e039a09eaa34174
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-04-26 20:02:55 +0400

    ADD: use externalNodes

--- AMFICOM/v1/map/src/com/syrus/AMFICOM/map/MapWrapper.java
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/map/MapWrapper.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MapWrapper.java,v 1.9 2005/04/11 11:51:24 bob Exp $
+ * $Id: MapWrapper.java,v 1.10 2005/04/26 16:02:55 krupenn Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -17,8 +17,8 @@ import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.StorableObjectWrapper;
 
 /**
- * @version $Revision: 1.9 $, $Date: 2005/04/11 11:51:24 $
- * @author $Author: bob $
+ * @version $Revision: 1.10 $, $Date: 2005/04/26 16:02:55 $
+ * @author $Author: krupenn $
  * @module map_v1
  */
 public class MapWrapper extends StorableObjectWrapper {
@@ -41,6 +41,8 @@ public class MapWrapper extends StorableObjectWrapper {
 	public static final String	LINK_COLUMN_SITE_NODE_ID		= "site_node_id";
 	// topological_node_id VARCHAR2(32),
 	public static final String	LINK_COLUMN_TOPOLOGICAL_NODE_ID	= "topological_node_id";
+	// external_node_id VARCHAR2(32),
+	public static final String	LINK_COLUMN_EXTERNAL_NODE_ID		= "external_node_id";
 
 	protected static MapWrapper	instance;
 
@@ -50,7 +52,7 @@ public class MapWrapper extends StorableObjectWrapper {
 		// empty private constructor
 		String[] keysArray = new String[] { COLUMN_NAME, COLUMN_DESCRIPTION, COLUMN_DOMAIN_ID,
 				LINK_COLUMN_COLLECTOR_ID, LINK_COLUMN_MARK_ID, LINK_COLUMN_NODE_LINK_ID, LINK_COLUMN_PHYSICAL_LINK_ID,
-				LINK_COLUMN_SITE_NODE_ID, LINK_COLUMN_TOPOLOGICAL_NODE_ID, LINK_COLUMN_MAP_ID };
+				LINK_COLUMN_SITE_NODE_ID, LINK_COLUMN_TOPOLOGICAL_NODE_ID, LINK_COLUMN_MAP_ID, LINK_COLUMN_EXTERNAL_NODE_ID };
 
 		this.keys = Collections.unmodifiableList(Arrays.asList(keysArray));
 
@@ -111,7 +113,8 @@ public class MapWrapper extends StorableObjectWrapper {
 				return map.getTopologicalNodes();
 			else if (key.equals(LINK_COLUMN_MAP_ID))
 				return map.getMaps();
-
+			else if (key.equals(LINK_COLUMN_EXTERNAL_NODE_ID))
+				return map.getExternalNodes();
 		}
 		return null;
 	}
@@ -147,6 +150,8 @@ public class MapWrapper extends StorableObjectWrapper {
 				map.setTopologicalNodes((Set) value);
 			else if (key.equals(LINK_COLUMN_MAP_ID))
 				map.setMaps((Set) value);
+			else if (key.equals(LINK_COLUMN_EXTERNAL_NODE_ID))
+				map.setExternalNodes((Set) value);
 		}
 	}
 
