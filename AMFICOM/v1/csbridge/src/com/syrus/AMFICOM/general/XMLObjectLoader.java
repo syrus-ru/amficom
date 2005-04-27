0b0dcc72c2ae4ff76ab1508cc50a4633919a589a
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-27 17:40:44 +0400

    ADD: Superclass for all XML loaders

---
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/XMLObjectLoader.java
@@ -0,0 +1,19 @@
+/*
+ * $Id: XMLObjectLoader.java,v 1.1 2005/04/27 13:40:44 arseniy Exp $
+ * 
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.general;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/04/27 13:40:44 $
+ * @author $Author: arseniy $
+ * @module csbridge_v1
+ */
+public abstract class XMLObjectLoader {
+	/*
+	 * Currently -- nothing
+	 * */ 
+}
