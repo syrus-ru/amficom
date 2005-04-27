fbf293ca6aecb2d0640b6353061f7450928d76cb
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-27 19:10:06 +0400

    MINOR: Removed init pools

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/DatabaseContextSetup.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/DatabaseContextSetup.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DatabaseContextSetup.java,v 1.22 2005/04/25 10:52:38 bob Exp $
+ * $Id: DatabaseContextSetup.java,v 1.23 2005/04/27 15:10:06 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -9,7 +9,6 @@
 package com.syrus.AMFICOM.mcm;
 
 import com.syrus.AMFICOM.administration.AdministrationDatabaseContext;
-import com.syrus.AMFICOM.administration.AdministrationStorableObjectPool;
 import com.syrus.AMFICOM.administration.DomainDatabase;
 import com.syrus.AMFICOM.administration.MCMDatabase;
 import com.syrus.AMFICOM.administration.ServerDatabase;
@@ -18,7 +17,6 @@ import com.syrus.AMFICOM.configuration.CableLinkTypeDatabase;
 import com.syrus.AMFICOM.configuration.CableThreadDatabase;
 import com.syrus.AMFICOM.configuration.CableThreadTypeDatabase;
 import com.syrus.AMFICOM.configuration.ConfigurationDatabaseContext;
-import com.syrus.AMFICOM.configuration.ConfigurationStorableObjectPool;
 import com.syrus.AMFICOM.configuration.EquipmentDatabase;
 import com.syrus.AMFICOM.configuration.EquipmentTypeDatabase;
 import com.syrus.AMFICOM.configuration.KISDatabase;
@@ -34,28 +32,25 @@ import com.syrus.AMFICOM.configuration.TransmissionPathTypeDatabase;
 import com.syrus.AMFICOM.general.CharacteristicDatabase;
 import com.syrus.AMFICOM.general.CharacteristicTypeDatabase;
 import com.syrus.AMFICOM.general.GeneralDatabaseContext;
-import com.syrus.AMFICOM.general.GeneralStorableObjectPool;
 import com.syrus.AMFICOM.general.ParameterTypeDatabase;
-import com.syrus.AMFICOM.general.StorableObjectResizableLRUMap;
 import com.syrus.AMFICOM.measurement.AnalysisDatabase;
 import com.syrus.AMFICOM.measurement.AnalysisTypeDatabase;
+import com.syrus.AMFICOM.measurement.CronTemporalPatternDatabase;
 import com.syrus.AMFICOM.measurement.EvaluationDatabase;
 import com.syrus.AMFICOM.measurement.EvaluationTypeDatabase;
 import com.syrus.AMFICOM.measurement.MeasurementDatabase;
 import com.syrus.AMFICOM.measurement.MeasurementDatabaseContext;
 import com.syrus.AMFICOM.measurement.MeasurementSetupDatabase;
-import com.syrus.AMFICOM.measurement.MeasurementStorableObjectPool;
 import com.syrus.AMFICOM.measurement.MeasurementTypeDatabase;
 import com.syrus.AMFICOM.measurement.PeriodicalTemporalPatternDatabase;
 import com.syrus.AMFICOM.measurement.ResultDatabase;
 import com.syrus.AMFICOM.measurement.SetDatabase;
-import com.syrus.AMFICOM.measurement.CronTemporalPatternDatabase;
 import com.syrus.AMFICOM.measurement.TestDatabase;
 
 
 /**
- * @version $Revision: 1.22 $, $Date: 2005/04/25 10:52:38 $
- * @author $Author: bob $
+ * @version $Revision: 1.23 $, $Date: 2005/04/27 15:10:06 $
+ * @author $Author: arseniy $
  * @module mcm_v1
  */
 
@@ -106,12 +101,5 @@ class DatabaseContextSetup {
 				new CronTemporalPatternDatabase(),
 				/* TODO new IntervalsTemporalPatternDatabase(),*/
 				new PeriodicalTemporalPatternDatabase());
-	}
-	
-	public static void initObjectPools() {
-		GeneralStorableObjectPool.init(new MCMGeneralObjectLoader(), StorableObjectResizableLRUMap.class);
-		AdministrationStorableObjectPool.init(new MCMAdministrationObjectLoader(), StorableObjectResizableLRUMap.class);
-		ConfigurationStorableObjectPool.init(new MCMConfigurationObjectLoader(), StorableObjectResizableLRUMap.class);
-		MeasurementStorableObjectPool.init(new MCMMeasurementObjectLoader(), StorableObjectResizableLRUMap.class);
 	}
 }
