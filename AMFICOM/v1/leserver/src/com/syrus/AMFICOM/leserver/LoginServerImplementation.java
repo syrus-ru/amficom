fbb95897c7458f4eade961ebcffd889e9eb9a344
cvsadmin <cvsadmin@science.syrus.ru>
2005-04-28 11:35:33 +0400

    Imported login/event server sources

---
+++ AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/LoginServerImplementation.java
@@ -0,0 +1,50 @@
+/*
+ * $Id: LoginServerImplementation.java,v 1.1.1.1 2005/04/28 07:35:33 cvsadmin Exp $
+ * 
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.loginserver;
+
+import com.syrus.AMFICOM.administration.corba.Domain_Transferable;
+import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
+import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
+import com.syrus.AMFICOM.general.corba.Identifier_TransferableHolder;
+import com.syrus.AMFICOM.general.corba.SecurityKey;
+import com.syrus.AMFICOM.loginserver.corba.LoginServerPOA;
+import com.syrus.util.Log;
+
+/**
+ * @version $Revision: 1.1.1.1 $, $Date: 2005/04/28 07:35:33 $
+ * @author $Author: cvsadmin $
+ * @module loginserver_v1
+ */
+public final class LoginServerImplementation extends LoginServerPOA {
+	private static final long serialVersionUID = -7190112124735462314L;
+
+	public SecurityKey login(String login, String password, Identifier_TransferableHolder user_id) throws AMFICOMRemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void logout(SecurityKey security_key) throws AMFICOMRemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Domain_Transferable[] transmitAvailableDomains(SecurityKey security_key) throws AMFICOMRemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void selectDomain(Identifier_Transferable domain_id) throws AMFICOMRemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void verify(byte i) {
+		Log.debugMessage("Verify value: " + i, Log.DEBUGLEVEL10);
+	}
+
+}
---
+++ AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/LoginServerServantManager.java
@@ -0,0 +1,67 @@
+/*
+ * $Id: LoginServerServantManager.java,v 1.1.1.1 2005/04/28 07:35:33 cvsadmin Exp $
+ * 
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.loginserver;
+
+import java.util.Collections;
+
+import com.syrus.AMFICOM.eventserver.corba.EventServer;
+import com.syrus.AMFICOM.general.CORBAServer;
+import com.syrus.AMFICOM.general.CommunicationException;
+import com.syrus.AMFICOM.general.ContextNameFactory;
+import com.syrus.AMFICOM.general.EventServerConnectionManager;
+import com.syrus.AMFICOM.general.IllegalDataException;
+import com.syrus.AMFICOM.general.RunnableVerifiedConnectionManager;
+import com.syrus.util.ApplicationProperties;
+
+/**
+ * @version $Revision: 1.1.1.1 $, $Date: 2005/04/28 07:35:33 $
+ * @author $Author: cvsadmin $
+ * @module loginserver_v1
+ */
+public final class LoginServerServantManager extends RunnableVerifiedConnectionManager implements EventServerConnectionManager {
+	private static final String KEY_SERVANT_NAME_EVENTSERVER = "EventServerServantName";
+	private static final String KEY_SERVANT_CHECK_TIMEOUT = "ServantCheckTimeout";
+
+	private static final String SERVANT_NAME_EVENTSERVER = "EventServer";
+	public static final int SERVANT_CHECK_TIMEOUT = 10;		//min
+
+	private String eventServerServantName;
+
+	public LoginServerServantManager(CORBAServer corbaServer, String eventServerServantName, long timeout) {
+		super(corbaServer, Collections.singleton(eventServerServantName), timeout);
+
+		this.eventServerServantName = eventServerServantName;
+
+		assert timeout >= 10 * 60 * 1000 : "Too low timeout"; //not less then 10 min
+	}
+
+	public EventServer getEventServerReference() throws CommunicationException {
+		try {
+			return (EventServer) super.getVerifiableReference(this.eventServerServantName);
+		}
+		catch (IllegalDataException e) {
+			// Never
+			assert false;
+			return null;
+		}
+	}
+
+	public static LoginServerServantManager createAndStart(String serverHostName) throws CommunicationException {
+		String contextName = ContextNameFactory.generateContextName(serverHostName);
+		CORBAServer corbaServer = new CORBAServer(contextName);
+
+		String eventServerServantName = ApplicationProperties.getString(KEY_SERVANT_NAME_EVENTSERVER, SERVANT_NAME_EVENTSERVER);
+
+		long timeout = ApplicationProperties.getInt(KEY_SERVANT_CHECK_TIMEOUT, SERVANT_CHECK_TIMEOUT) * 60 * 1000;
+
+		LoginServerServantManager loginServerServantManager =  new LoginServerServantManager(corbaServer, eventServerServantName, timeout);
+		(new Thread(loginServerServantManager)).start();
+		return loginServerServantManager;
+	}
+
+}
