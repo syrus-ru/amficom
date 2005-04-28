6dfbf03d806a78825e032a3b3df2a15ad6ae5cc6
Maxim Selivanov <max.selivanov@gmail.com>
2005-04-28 20:19:48 +0400

    merjet to the table intervalstemporalpatternoffsetiddur.sql

--- AMFICOM/v1/database/schema/measurement/object/intervalstemporalpatternduration.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IntervalsTemporalPatternDuration (
- inter_temp_patt_id VARCHAR2(32) NOT NULL,
--- 
- offset NUMBER(19) NOT NULL,
- duration VARCHAR2(32) NOT NULL,
---
- CONSTRAINT itp_id_fk FOREIGN KEY (inter_temp_patt_id)
-  REFERENCES IntervalsTemporalPattern (id) ON DELETE CASCADE
-)
