237546f7c545d84c9f4436b2049618beb6a53bc0
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-28 18:11:04 +0400

    MINOR: Continuation

--- AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/LoginEventServer.java
+++ AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/LoginEventServer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: LoginEventServer.java,v 1.1 2005/04/28 10:33:36 arseniy Exp $
+ * $Id: LoginEventServer.java,v 1.2 2005/04/28 14:11:04 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -11,42 +11,43 @@ import com.syrus.AMFICOM.administration.AdministrationDatabaseContext;
 import com.syrus.AMFICOM.administration.Server;
 import com.syrus.AMFICOM.administration.ServerProcess;
 import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.CORBAServer;
 import com.syrus.AMFICOM.general.CommunicationException;
-import com.syrus.AMFICOM.general.SleepButWorkThread;
+import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.util.Application;
 import com.syrus.util.ApplicationProperties;
 import com.syrus.util.Log;
 import com.syrus.util.database.DatabaseConnection;
 
 /**
- * @version $Revision: 1.1 $, $Date: 2005/04/28 10:33:36 $
+ * @version $Revision: 1.2 $, $Date: 2005/04/28 14:11:04 $
  * @author $Author: arseniy $
  * @module loginserver_v1
  */
-public final class LoginEventServer extends SleepButWorkThread {
+public final class LoginEventServer {
 	public static final String APPLICATION_NAME = "leserver";
 
 	public static final String KEY_DB_HOST_NAME = "DBHostName";
 	public static final String KEY_DB_SID = "DBSID";
 	public static final String KEY_DB_CONNECTION_TIMEOUT = "DBConnectionTimeout";
 	public static final String KEY_DB_LOGIN_NAME = "DBLoginName";
-	public static final String KEY_PROCESS_CODENAME = "ProcessCodename";
-	public static final String KEY_TICK_TIME = "TickTime";
-	public static final String KEY_MAX_FALLS = "MaxFalls";
+	public static final String KEY_LOGIN_PROCESS_CODENAME = "LoginProcessCodename";
+	public static final String KEY_EVENT_PROCESS_CODENAME = "EventProcessCodename";
 
 	public static final String DB_SID = "amficom";
 	public static final int DB_CONNECTION_TIMEOUT = 120;	//sec
 	public static final String DB_LOGIN_NAME = "amficom";
-	public static final String PROCESS_CODENAME = "LEServer";
-	public static final int TICK_TIME = 5;	//sec
+	public static final String LOGIN_PROCESS_CODENAME = "LoginServer";
+	public static final String EVENT_PROCESS_CODENAME = "EventServer";
 
-	private static String codename;
+	private static String loginProcessCodename;
+	private static String eventProcessCodename;
 
 	private boolean running;
 
-	public LoginEventServer() {
-		super(ApplicationProperties.getInt(KEY_TICK_TIME, TICK_TIME) * 1000, ApplicationProperties.getInt(KEY_MAX_FALLS, MAX_FALLS));
-		this.running = true;
+	private LoginEventServer() {
+		// singleton
+		assert false;
 	}
 
 	public static void main(String[] args) {
@@ -55,11 +56,13 @@ public final class LoginEventServer extends SleepButWorkThread {
 		/*	All preparations on startup*/
 		startup();
 
-		/*	Start main loop	*/
-		//TODO
+		/*	Start Event Processor*/
+		EventProcessor eventProcessor = new EventProcessor();
+		eventProcessor.start();
 
-		/*	Add shutdown hook	*/
-		//TODO
+		/*	Start Login Processor*/
+		LoginProcessor loginProcessor = new LoginProcessor();
+		loginProcessor.start();
 	}
 
 	private static void startup() {
@@ -70,24 +73,32 @@ public final class LoginEventServer extends SleepButWorkThread {
 		 * 	for work with database*/
 		DatabaseContextSetup.initDatabaseContext();
 
-		/*	Retrieve info about process*/
-		ServerProcess serverProcess = null;
-		codename = ApplicationProperties.getString(KEY_PROCESS_CODENAME, PROCESS_CODENAME);
+		/*	Retrieve info about processes*/
+		ServerProcess loginServerProcess = null;
+		ServerProcess eventServerProcess = null;
+		loginProcessCodename = ApplicationProperties.getString(KEY_LOGIN_PROCESS_CODENAME, LOGIN_PROCESS_CODENAME);
+		eventProcessCodename = ApplicationProperties.getString(KEY_EVENT_PROCESS_CODENAME, EVENT_PROCESS_CODENAME);
 		try {
-			serverProcess = AdministrationDatabaseContext.getServerProcessDatabase().retrieveForCodename(codename);
+			loginServerProcess = AdministrationDatabaseContext.getServerProcessDatabase().retrieveForCodename(loginProcessCodename);
+			eventServerProcess = AdministrationDatabaseContext.getServerProcessDatabase().retrieveForCodename(eventProcessCodename);
 		}
 		catch (ApplicationException ae) {
 			Log.errorException(ae);
+			System.exit(1);
+		}
+		Identifier loginServerId = loginServerProcess.getServerId();
+		if (!loginServerId.equals(eventServerProcess.getServerId())) {
+			Log.errorMessage("Server id of Login Server -- '" + loginServerId
+					+ "' not the same as server id of Event Server -- '" + eventServerProcess.getServerId() + "'");
+			Log.errorMessage("Login Server and Event Server must run on the same server!");
 			System.exit(1);
 		}
 
 		/*	Retrieve info about server, on which to run
 		 * and user, on behave of which to run*/
 		Server server = null;
-//		User user = null;
 		try {
-			server = new Server(serverProcess.getServerId());
-//			user = new User(serverProcess.getUserId());
+			server = new Server(loginServerId);
 		}
 		catch (ApplicationException ae) {
 			Log.errorException(ae);
@@ -106,12 +117,21 @@ public final class LoginEventServer extends SleepButWorkThread {
 
 		/*	Activate servant*/
 		try {
-			SessionEnvironment.getLEServerServantManager().getCORBAServer().activateServant(new LoginServerImplementation(), codename);
+			CORBAServer corbaServer = SessionEnvironment.getLEServerServantManager().getCORBAServer();
+			corbaServer.activateServant(new LoginServerImplementation(), loginProcessCodename);
+			corbaServer.activateServant(new EventServerImplementation(), eventProcessCodename);
 		}
 		catch (CommunicationException ce) {
 			Log.errorException(ce);
 			System.exit(1);
 		}
+
+		/*	Add shutdown hook*/
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				shutdown();
+			}
+		});
 	}
 
 	private static void establishDatabaseConnection() {
@@ -134,20 +154,11 @@ public final class LoginEventServer extends SleepButWorkThread {
 		}
 	}
 
-	protected void processFall() {
-		switch (super.fallCode) {
-			case FALL_CODE_NO_ERROR:
-				break;
-			default:
-				Log.errorMessage("processError | Unknown error code: " + super.fallCode);
-		}
-	}
-
-	protected void shutdown() {
-		this.running = false;
-
+	protected static void shutdown() {
 		try {
-			SessionEnvironment.getLEServerServantManager().getCORBAServer().deactivateServant(codename);
+			CORBAServer corbaServer = SessionEnvironment.getLEServerServantManager().getCORBAServer();
+			corbaServer.deactivateServant(loginProcessCodename);
+			corbaServer.deactivateServant(eventProcessCodename);
 		}
 		catch (CommunicationException ce) {
 			Log.errorException(ce);
