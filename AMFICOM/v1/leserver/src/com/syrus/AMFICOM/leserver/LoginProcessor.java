80a55c852e465573d8da7ed50a108fb39cc54ab2
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-28 14:31:53 +0400

    MAJOR: Replaced separate process LoginServer with thread LoginProcessor

---
+++ AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/LoginProcessor.java
@@ -0,0 +1,59 @@
+/*
+ * $Id: LoginProcessor.java,v 1.1 2005/04/28 10:31:53 arseniy Exp $
+ * 
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.leserver;
+
+import com.syrus.AMFICOM.general.SleepButWorkThread;
+import com.syrus.util.ApplicationProperties;
+import com.syrus.util.Log;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/04/28 10:31:53 $
+ * @author $Author: arseniy $
+ * @module leserver_v1
+ */
+final class LoginProcessor extends SleepButWorkThread {
+	public static final String KEY_LOGIN_PROCESSOR_TICK_TIME = "LoginProcessorTickTime";
+	public static final String KEY_LOGIN_PROCESSOR_MAX_FALLS = "LoginProcessorMaxFalls";
+
+	public static final int LOGIN_PROCESSOR_TICK_TIME = 5;	//sec
+
+	private boolean running;
+
+	public LoginProcessor() {
+		super(ApplicationProperties.getInt(KEY_LOGIN_PROCESSOR_TICK_TIME, LOGIN_PROCESSOR_TICK_TIME) * 1000,
+				ApplicationProperties.getInt(KEY_LOGIN_PROCESSOR_MAX_FALLS, MAX_FALLS));
+		this.running = true;
+	}
+
+	public void run() {
+		while (this.running) {
+			//TODO Implement
+
+			try {
+				sleep(super.initialTimeToSleep);
+			}
+			catch (InterruptedException ie) {
+				Log.errorException(ie);
+			}
+		}
+	}
+
+	protected void processFall() {
+		switch (super.fallCode) {
+			case FALL_CODE_NO_ERROR:
+				break;
+			default:
+				Log.errorMessage("processError | Unknown error code: " + super.fallCode);
+		}
+	}
+
+	protected void shutdown() {
+		this.running = false;
+	}
+
+}
