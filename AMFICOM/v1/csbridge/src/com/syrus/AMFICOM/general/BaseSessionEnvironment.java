b4445372ff5309fcd100283fa608a4f02ab0a93a
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-29 16:30:31 +0400

    BUGFIX: In method 'logout' serialize pool. ADD: Method 'getPoolContext', but commented as not needed now

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/BaseSessionEnvironment.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/BaseSessionEnvironment.java
@@ -1,5 +1,5 @@
 /*
- * $Id: BaseSessionEnvironment.java,v 1.1 2005/04/29 12:13:14 arseniy Exp $
+ * $Id: BaseSessionEnvironment.java,v 1.2 2005/04/29 12:30:31 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,7 +8,7 @@
 package com.syrus.AMFICOM.general;
 
 /**
- * @version $Revision: 1.1 $, $Date: 2005/04/29 12:13:14 $
+ * @version $Revision: 1.2 $, $Date: 2005/04/29 12:30:31 $
  * @author $Author: arseniy $
  * @module csbridge_v1
  */
@@ -28,6 +28,10 @@ public class BaseSessionEnvironment {
 	public BaseConnectionManager getConnectionManager() {
 		return this.baseConnectionManager;
 	}
+//
+//	public PoolContext getPoolContext() {
+//		return this.poolContext;
+//	}
 
 	public void login(String login, String password) throws CommunicationException, LoginException {
 		LoginManager.login(login, password);
@@ -36,6 +40,6 @@ public class BaseSessionEnvironment {
 
 	public void logout() throws CommunicationException, LoginException {
 		LoginManager.logout();
-		this.poolContext.deserialize();
+		this.poolContext.serialize();
 	}
 }
