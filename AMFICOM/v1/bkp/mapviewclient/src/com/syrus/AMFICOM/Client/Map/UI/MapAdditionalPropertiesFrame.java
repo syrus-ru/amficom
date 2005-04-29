6b8c98ad20be5ee0dd4e6c545e03967feb53122f
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-04-29 18:10:19 +0400

    TRIVIAL: pass logical net layer to additional properties panel for SiteNode and PhysicalLink

--- AMFICOM/v1/bkp/mapviewclient/src/com/syrus/AMFICOM/Client/Map/UI/MapAdditionalPropertiesFrame.java
+++ AMFICOM/v1/bkp/mapviewclient/src/com/syrus/AMFICOM/Client/Map/UI/MapAdditionalPropertiesFrame.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapAdditionalPropertiesFrame.java,v 1.1 2005/04/28 13:04:47 krupenn Exp $
+ * $Id: MapAdditionalPropertiesFrame.java,v 1.2 2005/04/29 14:10:19 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -11,13 +11,18 @@
 
 package com.syrus.AMFICOM.Client.Map.UI;
 
+import javax.swing.JDesktopPane;
+
 import com.syrus.AMFICOM.Client.General.Model.ApplicationContext;
+import com.syrus.AMFICOM.Client.Map.Command.MapDesktopCommand;
+import com.syrus.AMFICOM.Client.Map.Props.PhysicalLinkAddEditor;
+import com.syrus.AMFICOM.Client.Map.Props.SiteNodeAddEditor;
 import com.syrus.AMFICOM.client_.general.ui_.StorableObjectEditor;
 import com.syrus.AMFICOM.client_.general.ui_.VisualManager;
 
 /**
  *  Окно отображения свойств элемента карты
- * @version $Revision: 1.1 $, $Date: 2005/04/28 13:04:47 $
+ * @version $Revision: 1.2 $, $Date: 2005/04/29 14:10:19 $
  * @author $Author: krupenn $
  * @module mapviewclient_v1
  */
@@ -28,6 +33,21 @@ public final class MapAdditionalPropertiesFrame extends MapAbstractPropertiesFra
 	}
 	
 	public StorableObjectEditor getEditor(VisualManager manager) {
-		return manager.getAdditionalPropertiesPanel();
+		StorableObjectEditor addEditor = 
+			manager.getAdditionalPropertiesPanel();
+		
+		if(addEditor != null ) {
+			if(addEditor instanceof SiteNodeAddEditor) {
+				SiteNodeAddEditor siteEditor = (SiteNodeAddEditor )addEditor;
+				MapFrame mapFrame = MapDesktopCommand.findMapFrame((JDesktopPane )this.getParent());
+				siteEditor.setLogicalNetLayer(mapFrame.getMapViewer().getLogicalNetLayer());
+			}
+			else if(addEditor instanceof PhysicalLinkAddEditor) {
+				PhysicalLinkAddEditor linkEditor = (PhysicalLinkAddEditor )addEditor;
+				MapFrame mapFrame = MapDesktopCommand.findMapFrame((JDesktopPane )this.getParent());
+				linkEditor.setLogicalNetLayer(mapFrame.getMapViewer().getLogicalNetLayer());
+			}
+		}
+		return addEditor;
 	}
 }
