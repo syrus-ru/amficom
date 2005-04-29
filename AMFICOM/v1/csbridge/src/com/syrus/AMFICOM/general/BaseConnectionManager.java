d2d0c4990434a701498936b0b899b65efabbbe0c
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-29 16:12:14 +0400

    ADD: Base connection manager - can provide connections to LoginServer, EventServer, and IGSServer

---
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/BaseConnectionManager.java
@@ -0,0 +1,17 @@
+/*
+ * $Id: BaseConnectionManager.java,v 1.1 2005/04/29 12:12:14 arseniy Exp $
+ * 
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.general;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/04/29 12:12:14 $
+ * @author $Author: arseniy $
+ * @module csbridge_v1
+ */
+public interface BaseConnectionManager extends LoginServerConnectionManager, EventServerConnectionManager, IGSConnectionManager {
+
+}
