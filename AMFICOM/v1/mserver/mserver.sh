36bb83558664929afbf4dffe159c2a6f9fc596c5
Arseniy Tashoyan <tashoyan@gmail.com>
2005-04-29 21:06:03 +0400

    MINOR: Renamed script rc to mserver.sh

--- AMFICOM/v1/mserver/mserver.sh
+++ AMFICOM/v1/mserver/mserver.sh
@@ -4,6 +4,6 @@
 JAVA=$JAVA_HOME/bin/java
 LIB=../lib
 ORACLECLASSPATH=$ORACLE_HOME/jdbc/lib/ojdbc14_g.jar:$ORACLE_HOME/jdbc/lib/orai18n.jar:$ORACLE_HOME/jdbc/lib/classes12.jar:$ORACLE_HOME/jdbc/lib/nls_charset12.jar
-APPCLASSPATH=$LIB/mserver.jar:$LIB/mcm_interface.jar:$LIB/measurement.jar:$LIB/configuration.jar:$LIB/administration.jar:$LIB/event.jar:$LIB/general.jar:$LIB/util.jar
+APPCLASSPATH=$LIB/mserver.jar:$LIB/general.jar:$LIB/administration.jar:$LIB/configuration.jar:$LIB/measurement.jar:$LIB/csbridge.jar:$LIB/leserver_interface.jar:$LIB/mcm_interface.jar:$LIB/util.jar
 
 exec $JAVA -server -classpath $APPCLASSPATH:$ORACLECLASSPATH com.syrus.AMFICOM.mserver.MeasurementServer
