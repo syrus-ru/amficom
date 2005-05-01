26aba0e453f09f1131277f0e11f4646b53cc1ef7
Arseniy Tashoyan <tashoyan@gmail.com>
2005-05-01 21:26:31 +0400

    MAJOR: Removed methods, operating with single object. MAJOR: Use session environment

--- AMFICOM/v1/cmserver/src/com/syrus/AMFICOM/cmserver/CMServerMeasurementObjectLoader.java
+++ AMFICOM/v1/cmserver/src/com/syrus/AMFICOM/cmserver/CMServerMeasurementObjectLoader.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CMServerMeasurementObjectLoader.java,v 1.40 2005/04/12 17:17:29 arseniy Exp $
+ * $Id: CMServerMeasurementObjectLoader.java,v 1.41 2005/05/01 17:26:31 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -18,15 +18,12 @@ import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.CommunicationException;
 import com.syrus.AMFICOM.general.CreateObjectException;
 import com.syrus.AMFICOM.general.DatabaseException;
-import com.syrus.AMFICOM.general.Identifier;
-import com.syrus.AMFICOM.general.ObjectNotFoundException;
 import com.syrus.AMFICOM.general.RetrieveObjectException;
 import com.syrus.AMFICOM.general.StorableObject;
 import com.syrus.AMFICOM.general.StorableObjectCondition;
 import com.syrus.AMFICOM.general.StorableObjectConditionBuilder;
 import com.syrus.AMFICOM.general.StorableObjectDatabase;
 import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
-import com.syrus.AMFICOM.general.corba.ErrorCode;
 import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
 import com.syrus.AMFICOM.general.corba.StorableObjectCondition_Transferable;
 import com.syrus.AMFICOM.measurement.Analysis;
@@ -44,7 +41,7 @@ import com.syrus.AMFICOM.mserver.corba.MServer;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.40 $, $Date: 2005/04/12 17:17:29 $
+ * @version $Revision: 1.41 $, $Date: 2005/05/01 17:26:31 $
  * @author $Author: arseniy $
  * @module cmserver_v1
  */
@@ -62,157 +59,9 @@ public final class CMServerMeasurementObjectLoader extends DatabaseMeasurementOb
 		this.lastRefesh = new HashMap();
 	}
 
-	public Set refresh(Set storableObjects) throws DatabaseException, CommunicationException {
-		/* refresh no often than one in refreshTimeout ms */
-		if (storableObjects.isEmpty())
-			return Collections.EMPTY_SET;
-
-		StorableObject firstStorableObject = (StorableObject) storableObjects.iterator().next();
-		Short entityCode = new Short(firstStorableObject.getId().getMajor());
-		
-		Date lastRefreshDate = (Date) this.lastRefesh.get(entityCode);
-		
-		if (lastRefreshDate != null && System.currentTimeMillis() - lastRefreshDate.getTime() < this.refreshTimeout)
-			return Collections.EMPTY_SET;
-
-		/* put current date*/
-		this.lastRefesh.put(entityCode, new Date()); 
-
-		try {
-			StorableObjectDatabase database = MeasurementDatabaseContext.getDatabase(entityCode.shortValue());
-			if (database != null)
-				return database.refresh(storableObjects);
-
-			return Collections.EMPTY_SET;
-		}
-		catch (DatabaseException e) {
-			Log.errorMessage("CMServerMeasurementObjectLoader.refresh | DatabaseException: " + e.getMessage());
-			throw new DatabaseException("CMServerMeasurementObjectLoader.refresh | DatabaseException: " + e.getMessage());
-		}
-
-	}
 
 
-
-
-	public Measurement loadMeasurement(Identifier id)
-			throws RetrieveObjectException, CommunicationException, ObjectNotFoundException, CreateObjectException {
-		try {
-			return new Measurement(id);
-		}
-		catch (ObjectNotFoundException e) {
-			Log.debugMessage("CMServerMeasurementObjectLoader.loadMeasurement | Measurement '" + id
-					+ "' not found in database; trying to load from Measurement Server", Log.DEBUGLEVEL08);
-			Measurement measurement = null;
-
-			MServer mServerRef = ClientMeasurementServer.getVerifiedMServerReference();
-			try {
-				Measurement_Transferable transferable = mServerRef.transmitMeasurement((Identifier_Transferable) id.getTransferable());
-				measurement = new Measurement(transferable);
-				Log.debugMessage("CMServerMeasurementObjectLoader.loadMeasurement | Measurement '" + id
-						+ "' loaded from MeasurementServer", Log.DEBUGLEVEL08);
-			}
-			catch (AMFICOMRemoteException are) {
-				if (are.error_code.value() == ErrorCode._ERROR_NOT_FOUND)
-					throw new ObjectNotFoundException("Measurement '" + id + "' not found on Measurement Server -- " + are.message);
-				throw new RetrieveObjectException("Cannot retrieve measurement '" + id + "' from Measurement Server -- " + are.message);
-			}
-			catch (Throwable throwable) {
-				Log.errorException(throwable);
-			}
-
-			if (measurement != null) {
-				try {
-					MeasurementDatabaseContext.getMeasurementDatabase().insert(measurement);
-				}
-				catch (ApplicationException ae) {
-					Log.errorException(ae);
-				}
-				return measurement;
-			}
-			throw new ObjectNotFoundException("Measurement '" + id + "' not found on Measurement Server");
-		}	//catch (ObjectNotFoundException e)
-	}
-
-	public Analysis loadAnalysis(Identifier id)
-			throws RetrieveObjectException, CommunicationException, ObjectNotFoundException, CreateObjectException {
-		try {
-			return new Analysis(id);
-		}
-		catch (ObjectNotFoundException e) {
-			Log.debugMessage("CMServerMeasurementObjectLoader.loadAnalysis | Analysis '" + id + "' not found in database; trying to load from Measurement Server", Log.DEBUGLEVEL08);
-			Analysis analysis = null;
-
-			MServer mServerRef = ClientMeasurementServer.getVerifiedMServerReference();
-			try {
-				Analysis_Transferable transferable = mServerRef.transmitAnalysis((Identifier_Transferable) id.getTransferable());
-				analysis = new Analysis(transferable);
-				Log.debugMessage("CMServerMeasurementObjectLoader.loadAnalysis | Analysis '" + id + "' loaded from MeasurementServer",
-						Log.DEBUGLEVEL08);
-			}
-			catch (AMFICOMRemoteException are) {
-				if (are.error_code.value() == ErrorCode._ERROR_NOT_FOUND)
-					throw new ObjectNotFoundException("Analysis '" + id + "' not found on Measurement Server -- " + are.message);
-				throw new RetrieveObjectException("Cannot retrieve analysis '" + id + "' from Measurement Server -- " + are.message);
-			}
-			catch (Throwable throwable) {
-				Log.errorException(throwable);
-			}
-
-			if (analysis != null) {
-				try {
-					MeasurementDatabaseContext.getAnalysisDatabase().insert(analysis);
-				}
-				catch (ApplicationException ae) {
-					Log.errorException(ae);
-				}
-				return analysis;
-			}
-			throw new ObjectNotFoundException("Analysis '" + id + "' not found on Measurement Server");
-		}	//catch (ObjectNotFoundException e)
-	}
-
-	public Evaluation loadEvaluation(Identifier id)
-			throws RetrieveObjectException, CommunicationException, ObjectNotFoundException, CreateObjectException {
-		try {
-			return new Evaluation(id);
-		}
-		catch (ObjectNotFoundException e) {
-			Log.debugMessage("CMServerMeasurementObjectLoader.loadEvaluation | Evaluation '" + id
-					+ "' not found in database; trying to load from Measurement Server", Log.DEBUGLEVEL08);
-			Evaluation evaluation = null;
-
-			MServer mServerRef = ClientMeasurementServer.getVerifiedMServerReference();
-			try {
-				Evaluation_Transferable transferable = mServerRef.transmitEvaluation((Identifier_Transferable) id.getTransferable());
-				evaluation = new Evaluation(transferable);
-				Log.debugMessage("CMServerMeasurementObjectLoader.loadEvaluation | Evaluation '" + id + "' loaded from MeasurementServer",
-						Log.DEBUGLEVEL08);
-			}
-			catch (AMFICOMRemoteException are) {
-				if (are.error_code.value() == ErrorCode._ERROR_NOT_FOUND)
-					throw new ObjectNotFoundException("Evaluation '" + id + "' not found on Measurement Server -- " + are.message);
-				throw new RetrieveObjectException("Cannot retrieve evaluation '" + id + "' from Measurement Server -- " + are.message);
-			}
-			catch (Throwable throwable) {
-				Log.errorException(throwable);
-			}
-
-			if (evaluation != null) {
-				try {
-					MeasurementDatabaseContext.getEvaluationDatabase().insert(evaluation);
-				}
-				catch (ApplicationException ae) {
-					Log.errorException(ae);
-				}
-				return evaluation;
-			}
-			throw new ObjectNotFoundException("Evaluation '" + id + "' not found on Measurement Server");
-		}	//catch (ObjectNotFoundException e)
-	}
-
-
-
+	/* Load multiple objects*/
 
 	public Set loadMeasurements(Set ids) throws RetrieveObjectException {
 		MeasurementDatabase database = MeasurementDatabaseContext.getMeasurementDatabase();
@@ -224,7 +73,7 @@ public final class CMServerMeasurementObjectLoader extends DatabaseMeasurementOb
 		Set loadedObjects = new HashSet();
 
 		try {
-			MServer mServerRef = ClientMeasurementServer.getVerifiedMServerReference();
+			MServer mServerRef = CMServerSessionEnvironment.getInstance().getCMServerServantManager().getMServerReference();
 			Measurement_Transferable[] transferables = mServerRef.transmitMeasurements(loadIdsT);
 			for (int i = 0; i < transferables.length; i++) {
 				try {
@@ -269,7 +118,7 @@ public final class CMServerMeasurementObjectLoader extends DatabaseMeasurementOb
 		Set loadedObjects = new HashSet();
 
 		try {
-			MServer mServerRef = ClientMeasurementServer.getVerifiedMServerReference();
+			MServer mServerRef = CMServerSessionEnvironment.getInstance().getCMServerServantManager().getMServerReference();
 			Analysis_Transferable[] transferables = mServerRef.transmitAnalyses(loadIdsT);
 			for (int i = 0; i < transferables.length; i++) {
 				try {
@@ -314,7 +163,7 @@ public final class CMServerMeasurementObjectLoader extends DatabaseMeasurementOb
 		Set loadedObjects = new HashSet();
 
 		try {
-			MServer mServerRef = ClientMeasurementServer.getVerifiedMServerReference();
+			MServer mServerRef = CMServerSessionEnvironment.getInstance().getCMServerServantManager().getMServerReference();
 			Evaluation_Transferable[] transferables = mServerRef.transmitEvaluations(loadIdsT);
 			for (int i = 0; i < transferables.length; i++) {
 				try {
@@ -362,7 +211,7 @@ public final class CMServerMeasurementObjectLoader extends DatabaseMeasurementOb
 		Set loadedObjects = new HashSet();
 
 		try {
-			MServer mServerRef = ClientMeasurementServer.getVerifiedMServerReference();
+			MServer mServerRef = CMServerSessionEnvironment.getInstance().getCMServerServantManager().getMServerReference();
 			Measurement_Transferable[] transferables = mServerRef.transmitMeasurementsButIdsByCondition(loadButIdsT, conditionT);
 			for (int i = 0; i < transferables.length; i++) {
 				try {
@@ -406,7 +255,7 @@ public final class CMServerMeasurementObjectLoader extends DatabaseMeasurementOb
 		Set loadedObjects = new HashSet();
 
 		try {
-			MServer mServerRef = ClientMeasurementServer.getVerifiedMServerReference();
+			MServer mServerRef = CMServerSessionEnvironment.getInstance().getCMServerServantManager().getMServerReference();
 			Analysis_Transferable[] transferables = mServerRef.transmitAnalysesButIdsByCondition(loadButIdsT, conditionT);
 			for (int i = 0; i < transferables.length; i++) {
 				try {
@@ -450,7 +299,7 @@ public final class CMServerMeasurementObjectLoader extends DatabaseMeasurementOb
 		Set loadedObjects = new HashSet();
 
 		try {
-			MServer mServerRef = ClientMeasurementServer.getVerifiedMServerReference();
+			MServer mServerRef = CMServerSessionEnvironment.getInstance().getCMServerServantManager().getMServerReference();
 			Evaluation_Transferable[] transferables = mServerRef.transmitEvaluationsButIdsByCondition(loadButIdsT, conditionT);
 			for (int i = 0; i < transferables.length; i++) {
 				try {
@@ -483,6 +332,40 @@ public final class CMServerMeasurementObjectLoader extends DatabaseMeasurementOb
 		}
 
 		return objects;
+	}
+
+
+
+	/*	Refresh*/
+
+	public Set refresh(Set storableObjects) throws DatabaseException, CommunicationException {
+		/* refresh no often than one in refreshTimeout ms */
+		if (storableObjects.isEmpty())
+			return Collections.EMPTY_SET;
+
+		StorableObject firstStorableObject = (StorableObject) storableObjects.iterator().next();
+		Short entityCode = new Short(firstStorableObject.getId().getMajor());
+		
+		Date lastRefreshDate = (Date) this.lastRefesh.get(entityCode);
+		
+		if (lastRefreshDate != null && System.currentTimeMillis() - lastRefreshDate.getTime() < this.refreshTimeout)
+			return Collections.EMPTY_SET;
+
+		/* put current date*/
+		this.lastRefesh.put(entityCode, new Date()); 
+
+		try {
+			StorableObjectDatabase database = MeasurementDatabaseContext.getDatabase(entityCode.shortValue());
+			if (database != null)
+				return database.refresh(storableObjects);
+
+			return Collections.EMPTY_SET;
+		}
+		catch (DatabaseException e) {
+			Log.errorMessage("CMServerMeasurementObjectLoader.refresh | DatabaseException: " + e.getMessage());
+			throw new DatabaseException("CMServerMeasurementObjectLoader.refresh | DatabaseException: " + e.getMessage());
+		}
+
 	}
 
 }
