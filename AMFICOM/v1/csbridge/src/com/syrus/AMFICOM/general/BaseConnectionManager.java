e0fa336fa4765441a9e1c1a4bdf551686534c947
Arseniy Tashoyan <tashoyan@gmail.com>
2005-05-01 20:52:50 +0400

    ADD: Common for all implementations constants

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/BaseConnectionManager.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/BaseConnectionManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: BaseConnectionManager.java,v 1.2 2005/04/29 17:35:25 arseniy Exp $
+ * $Id: BaseConnectionManager.java,v 1.3 2005/05/01 16:52:50 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,10 +8,13 @@
 package com.syrus.AMFICOM.general;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/04/29 17:35:25 $
+ * @version $Revision: 1.3 $, $Date: 2005/05/01 16:52:50 $
  * @author $Author: arseniy $
  * @module csbridge_v1
  */
 public interface BaseConnectionManager extends LoginServerConnectionManager, EventServerConnectionManager, IGSConnectionManager {
+	String KEY_SERVANT_CHECK_TIMEOUT = "ServantCheckTimeout";
+	int SERVANT_CHECK_TIMEOUT = 10;		//min
+
 	// Just the sum of the extended interfaces
 }
