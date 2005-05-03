36620add5a085d04b92e6052412c3fbd2052a2a7
Arseniy Tashoyan <tashoyan@gmail.com>
2005-05-03 17:41:04 +0400

    MINOR: Renamed column 'security_key' to 'session_key'

--- AMFICOM/v1/database/schema/security/userlogin.sql
+++ AMFICOM/v1/database/schema/security/userlogin.sql
@@ -1,13 +1,13 @@
 CREATE TABLE UserLogin (
- security_key VARCHAR2(128) NOT NULL,
+ session_key VARCHAR2(128) NOT NULL,
  user_id VARCHAR2(32) NOT NULL,
  domain_id VARCHAR2(32),
  login_date DATE NOT NULL,
  last_activity_date DATE NOT NULL,
 --
- CONSTRAINT ul_pk PRIMARY KEY (security_key),
+ CONSTRAINT ul_pk PRIMARY KEY (session_key),
  CONSTRAINT ul_user_fk FOREIGN KEY (user_id)
-  REFERENCES "User" (id) ON DELETE CASCADE
+  REFERENCES "User" (id) ON DELETE CASCADE,
  CONSTRAINT ul_domain_fk FOREIGN KEY (domain_id)
   REFERENCES Domain (id) ON DELETE CASCADE
 );
