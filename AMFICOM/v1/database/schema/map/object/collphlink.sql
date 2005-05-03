5b510d00a2ab3a5e5f62a23d1539cc0754cd2abe
cvs-fast-export <cvs-fast-export>
2005-05-03 13:33:27 +0000

    Synthetic commit for incomplete tag OLD_AMFICOM_GONE

--- AMFICOM/v1/database/schema/map/object/collphlink.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE CollPhLink (
- collector_id VARCHAR2(32),
- physical_link_id VARCHAR2(32),
---
- CONSTRAINT collphlink_coll_fk FOREIGN KEY (collector_id)
-  REFERENCES Collector (id) ON DELETE CASCADE,
- CONSTRAINT collphlink_phln_fk FOREIGN KEY (physical_link_id)
-  REFERENCES PhysicalLink (id) ON DELETE CASCADE
-);
