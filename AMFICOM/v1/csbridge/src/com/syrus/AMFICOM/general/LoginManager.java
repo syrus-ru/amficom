ef6fbb05d63f9d045b094c41b14e3675516f3ec8
Arseniy Tashoyan <tashoyan@gmail.com>
2005-05-04 15:54:58 +0400

    COSMETIC

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/LoginManager.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/LoginManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: LoginManager.java,v 1.7 2005/05/04 11:12:32 arseniy Exp $
+ * $Id: LoginManager.java,v 1.8 2005/05/04 11:54:58 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -22,7 +22,7 @@ import com.syrus.AMFICOM.security.corba.SessionKey_Transferable;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.7 $, $Date: 2005/05/04 11:12:32 $
+ * @version $Revision: 1.8 $, $Date: 2005/05/04 11:54:58 $
  * @author $Author: arseniy $
  * @module csbridge_v1
  */
@@ -147,6 +147,13 @@ public final class LoginManager {
 	}
 
 
+	/**
+	 * 
+	 * @return true, only when LoginRestorer.restoreLogin() returned true,
+	 * i.e., when user or application decided to restore login
+	 * @throws LoginException
+	 * @throws CommunicationException
+	 */
 	public static boolean restoreLogin() throws LoginException, CommunicationException {
 		if (loginRestorer != null && loginRestorer.restoreLogin()) {
 			login(loginRestorer.getLogin(), loginRestorer.getPassword());
