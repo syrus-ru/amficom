ea03ce7bc1e57330404e6af12b59ceff25ad6d45
Arseniy Tashoyan <tashoyan@gmail.com>
2005-05-04 11:49:31 +0400

    misc

--- AMFICOM/v1/mserver/mserver.sh
+++ AMFICOM/v1/mserver/mserver.sh
@@ -2,7 +2,7 @@
 
 
 JAVA=$JAVA_HOME/bin/java
-LIB=../lib
+LIB=$HOME/lib
 ORACLECLASSPATH=$ORACLE_HOME/jdbc/lib/ojdbc14_g.jar:$ORACLE_HOME/jdbc/lib/orai18n.jar:$ORACLE_HOME/jdbc/lib/classes12.jar:$ORACLE_HOME/jdbc/lib/nls_charset12.jar
 APPCLASSPATH=$LIB/mserver.jar:$LIB/general.jar:$LIB/administration.jar:$LIB/configuration.jar:$LIB/measurement.jar:$LIB/csbridge.jar:$LIB/leserver_interface.jar:$LIB/mcm_interface.jar:$LIB/util.jar
 
