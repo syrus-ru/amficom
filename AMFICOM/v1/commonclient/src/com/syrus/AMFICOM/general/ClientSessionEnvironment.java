1cc96ece6ab92cb1dd37971204730535a3d5ba29
Arseniy Tashoyan <tashoyan@gmail.com>
2005-05-16 19:56:28 +0400

    ADD: XML Session environment

--- AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/ClientSessionEnvironment.java
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/ClientSessionEnvironment.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ClientSessionEnvironment.java,v 1.2 2005/05/05 10:58:36 arseniy Exp $
+ * $Id: ClientSessionEnvironment.java,v 1.3 2005/05/16 15:56:28 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -10,12 +10,13 @@ package com.syrus.AMFICOM.general;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/05/05 10:58:36 $
+ * @version $Revision: 1.3 $, $Date: 2005/05/16 15:56:28 $
  * @author $Author: arseniy $
  * @module generalclient_v1
  */
 public final class ClientSessionEnvironment extends BaseSessionEnvironment {
 	public static final int SESSION_KIND_MEASUREMENT = 1;
+	//Other kinds -- Map, Scheme, Resources, etc.
 
 	private static ClientSessionEnvironment instance;
 
