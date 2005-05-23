7a21c197d28b545aae0e0f61e4c4999e0c249177
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-05-23 12:34:38 +0400

    MINOR: serialize/deserialize made final

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/PoolContext.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/PoolContext.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: PoolContext.java,v 1.4 2005/05/21 19:55:49 arseniy Exp $
+ * $Id: PoolContext.java,v 1.5 2005/05/23 08:34:38 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,19 +9,19 @@
 package com.syrus.AMFICOM.general;
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/05/21 19:55:49 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.5 $, $Date: 2005/05/23 08:34:38 $
+ * @author $Author: bass $
  * @module csbridge_v1
  */
 public abstract class PoolContext {
 
 	public abstract void init();
 
-	public void deserialize() {
+	public final void deserialize() {
 		StorableObjectPool.deserialize();
 	}
 
-	public void serialize() {
+	public final void serialize() {
 		StorableObjectPool.serialize();
 	}
 }
