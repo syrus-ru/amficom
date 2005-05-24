73c78ff7594caacfa2362594be8bdbe2a5ef6c58
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-05-24 16:38:14 +0400

    ADD deleteTests(Identifier_Transferable[])

--- AMFICOM/v1/mserver/src/com/syrus/AMFICOM/mserver/MServerImplementation.java
+++ AMFICOM/v1/mserver/src/com/syrus/AMFICOM/mserver/MServerImplementation.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MServerImplementation.java,v 1.53 2005/05/01 19:17:31 arseniy Exp $
+ * $Id: MServerImplementation.java,v 1.54 2005/05/24 12:38:14 bob Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,11 +8,15 @@
 
 package com.syrus.AMFICOM.mserver;
 
+import java.util.Set;
+
+import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.IdentifierGenerationException;
 import com.syrus.AMFICOM.general.IdentifierGenerator;
 import com.syrus.AMFICOM.general.IllegalObjectEntityException;
 import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.StorableObjectPool;
 import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
 import com.syrus.AMFICOM.general.corba.CompletionStatus;
 import com.syrus.AMFICOM.general.corba.ErrorCode;
@@ -20,8 +24,8 @@ import com.syrus.AMFICOM.general.corba.Identifier_Transferable;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.53 $, $Date: 2005/05/01 19:17:31 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.54 $, $Date: 2005/05/24 12:38:14 $
+ * @author $Author: bob $
  * @module mserver_v1
  */
 
@@ -73,7 +77,19 @@ public class MServerImplementation extends MServerMeasurementTransmit {
 							+ "' -- " + ige.getMessage());
 		}
 	}
-
+	
+	public void deleteTests(Identifier_Transferable[] ids) throws AMFICOMRemoteException {
+		Set testIds = Identifier.fromTransferables(ids);
+		StorableObjectPool.delete(testIds);
+		try {
+			StorableObjectPool.flush(ObjectEntities.TEST_ENTITY_CODE, true);
+		} catch (ApplicationException ae) {
+			Log.errorException(ae);
+			throw new AMFICOMRemoteException(ErrorCode.ERROR_DELETE,
+					CompletionStatus.COMPLETED_NO,
+					"Cannot delete '" + ObjectEntities.codeToString(ObjectEntities.TEST_ENTITY_CODE) + "'");
+		}
+	}
 
 
 	/* Connection verificaton */
