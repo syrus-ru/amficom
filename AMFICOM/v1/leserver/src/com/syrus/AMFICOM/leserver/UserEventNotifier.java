7faf87f1cd8214978aee02353622a2bdce417743
Arseniy Tashoyan <tashoyan@gmail.com>
2005-05-30 18:52:50 +0400

    MINOR: Method 'run'

--- AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/UserEventNotifier.java
+++ AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/UserEventNotifier.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UserEventNotifier.java,v 1.1 2005/05/30 14:40:07 arseniy Exp $
+ * $Id: UserEventNotifier.java,v 1.2 2005/05/30 14:52:50 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,17 +8,20 @@
 package com.syrus.AMFICOM.leserver;
 
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import com.syrus.AMFICOM.event.Event;
+import com.syrus.AMFICOM.event.EventType;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.SleepButWorkThread;
 import com.syrus.util.ApplicationProperties;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.1 $, $Date: 2005/05/30 14:40:07 $
+ * @version $Revision: 1.2 $, $Date: 2005/05/30 14:52:50 $
  * @author $Author: arseniy $
  * @module leserver_v1
  */
@@ -45,7 +48,16 @@ final class UserEventNotifier extends SleepButWorkThread {
 	}
 
 	public void run() {
-		
+		synchronized (this.eventQueue) {
+			for (final Iterator it1 = this.eventQueue.iterator(); it1.hasNext();) {
+				final Event event = (Event) it1.next();
+				final EventType eventType = (EventType) event.getType();
+				final Set userAlertKinds = eventType.getUserAlertKinds(this.userId);
+				for (final Iterator it2 = this.eventQueue.iterator(); it2.hasNext();) {
+					
+				}
+			}
+		}
 	}
 
 	protected void processFall() {
@@ -56,6 +68,5 @@ final class UserEventNotifier extends SleepButWorkThread {
 				Log.errorMessage("processError | Unknown error code: " + super.fallCode);
 		}
 	}
-}
 
 }
