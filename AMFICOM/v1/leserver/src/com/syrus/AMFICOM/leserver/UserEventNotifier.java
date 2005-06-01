30398c4d18ee2d127f8756195e39877e89c4a046
Arseniy Tashoyan <tashoyan@gmail.com>
2005-06-01 20:03:56 +0400

    MINOR: temporally implemented method 'run'

--- AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/UserEventNotifier.java
+++ AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/UserEventNotifier.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UserEventNotifier.java,v 1.2 2005/05/30 14:52:50 arseniy Exp $
+ * $Id: UserEventNotifier.java,v 1.3 2005/06/01 16:03:56 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -15,13 +15,14 @@ import java.util.Set;
 
 import com.syrus.AMFICOM.event.Event;
 import com.syrus.AMFICOM.event.EventType;
+import com.syrus.AMFICOM.event.corba.AlertKind;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.SleepButWorkThread;
 import com.syrus.util.ApplicationProperties;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/05/30 14:52:50 $
+ * @version $Revision: 1.3 $, $Date: 2005/06/01 16:03:56 $
  * @author $Author: arseniy $
  * @module leserver_v1
  */
@@ -53,9 +54,27 @@ final class UserEventNotifier extends SleepButWorkThread {
 				final Event event = (Event) it1.next();
 				final EventType eventType = (EventType) event.getType();
 				final Set userAlertKinds = eventType.getUserAlertKinds(this.userId);
-				for (final Iterator it2 = this.eventQueue.iterator(); it2.hasNext();) {
-					
+				boolean delivered = false;
+				for (final Iterator it2 = userAlertKinds.iterator(); it2.hasNext();) {
+					final AlertKind alertKind = (AlertKind) it2.next();
+					final Alerter alerter = Alerter.getAlerter(AlertKind.ALERT_KIND_EMAIL);
+					if (alerter != null) {
+						try {
+							alerter.notifyUser(this.userId);
+							delivered = true;
+						}
+						catch (EventNotificationException ene) {
+							Log.errorException(ene);
+						}
+					}
+					else {
+						Log.errorMessage("Unknown alert kind -- " + alertKind.value()
+								+ " for event type '" + eventType.getId()
+								+ "' and user '" + this.userId + "'");
+					}
 				}
+				if (delivered)
+					it1.remove();
 			}
 		}
 	}
