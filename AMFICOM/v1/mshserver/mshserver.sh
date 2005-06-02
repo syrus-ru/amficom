0bc257aae39e571735942acceec3be9c459e3411
Maxim Selivanov <max.selivanov@gmail.com>
2005-06-02 13:44:53 +0400

    additional jars for mapinfo

--- AMFICOM/v1/mshserver/mshserver.sh
+++ AMFICOM/v1/mshserver/mshserver.sh
@@ -7,7 +7,7 @@ EXTLIB=$AMFICOM_HOME/lib/external
 
 ORACLECLASSPATH=$ORACLE_HOME/jdbc/lib/ojdbc14_g.jar:$ORACLE_HOME/jdbc/lib/orai18n.jar:$ORACLE_HOME/jdbc/lib/classes12.jar:$ORACLE_HOME/jdbc/lib/nls_charset12.jar
 
-EXTCLASSPATH=$EXTLIB/trove.jar:$EXTLIB/jdom.jar:$EXTLIB/micsys.jar:$EXTLIB/miutil.jar:$EXTLIB/mxj.jar
+EXTCLASSPATH=$EXTLIB/trove.jar:$EXTLIB/jdom.jar:$EXTLIB/micsys.jar:$EXTLIB/miutil.jar:$EXTLIB/mxj.jar:$EXTLIB/commons-logging.jar:$EXTLIB/mxjgeom.jar:$EXTLIB/miappletsup.jar:$EXTLIB/xercesImpl.jar:$EXTLIB/mxjtabdp.jar
 
 APPCLASSPATH=$LIB/filter.jar:$LIB/mshserver.jar:$LIB/general.jar:$LIB/administration.jar:$LIB/configuration.jar:$LIB/csbridge.jar:$LIB/leserver_interface.jar:$LIB/map.jar:$LIB/scheme.jar:$LIB/util.jar
 
