4d6269f8facbe38b675abd074fff6719b1de9903
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-06-07 22:01:50 +0400

    COSMETIC:

--- tools/test/test/com/syrus/util/snippets/AssertionTest.java
+++ tools/test/test/com/syrus/util/snippets/AssertionTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AssertionTest.java,v 1.1 2005/03/17 15:58:03 bass Exp $
+ * $Id: AssertionTest.java,v 1.2 2005/06/07 18:01:50 bass Exp $
  *
  * Copyright ¿ 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -13,7 +13,7 @@ import java.lang.reflect.*;
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.1 $, $Date: 2005/03/17 15:58:03 $
+ * @version $Revision: 1.2 $, $Date: 2005/06/07 18:01:50 $
  * @module tools
  */
 final class AssertionTest {
@@ -22,13 +22,13 @@ final class AssertionTest {
 	}
 
 	static void doSmth() {
-		assert false: "We're definitely screwed..."; //$NON-NLS-1$
+		assert false: "We're definitely screwed...";
 	}
 
 	public static void main(final String args[]) {
 		final Class clazz = AssertionTest.class;
 		try {
-			final Method method = clazz.getDeclaredMethod("doSmth", new Class[0]); //$NON-NLS-1$
+			final Method method = clazz.getDeclaredMethod("doSmth", new Class[0]);
 			method.invoke(clazz, new Object[0]);
 		} catch (final InvocationTargetException ite) {
 			final Throwable cause = ite.getCause();
