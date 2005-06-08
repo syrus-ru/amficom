7cc9f801d3bef2e3fe149b3365e503900851c2a0
Arseniy Tashoyan <tashoyan@gmail.com>
2005-06-08 13:54:24 +0400

    COSMETIC

--- AMFICOM/v1/scheduler/src/com/syrus/AMFICOM/Client/Schedule/ConstStorage.java
+++ AMFICOM/v1/scheduler/src/com/syrus/AMFICOM/Client/Schedule/ConstStorage.java
@@ -12,26 +12,26 @@ package com.syrus.AMFICOM.Client.Schedule;
  */
 public interface ConstStorage {
 
-	final String			COLUMN_NAME_ACTION_ID		= "action_id";
-	final String			COLUMN_NAME_ACTIVE			= "active";
-	final String			COLUMN_NAME_CREATED			= "created";
-	final String			COLUMN_NAME_DESCRIPTION		= "description";
-	final String			COLUMN_NAME_DOMAIN_ID		= "domain_id";
-	final String			COLUMN_NAME_END_TIME		= "end_time";
-	final String			COLUMN_NAME_ID				= "id";
-	final String			COLUMN_NAME_KIS_ID			= "kis_id";
-	final String			COLUMN_NAME_LOCAL_ID		= "local_id";
-	final String			COLUMN_NAME_MODIFIED		= "modified";
-	final String			COLUMN_NAME_NAME			= "name";
-	final String			COLUMN_NAME_PORT_ID			= "port_id";
-	final String			COLUMN_NAME_REQUEST_ID		= "request_id";
-	final String			COLUMN_NAME_RESULT_TYPE		= "result_type";
-	final String			COLUMN_NAME_START_TIME		= "start_time";
-	final String			COLUMN_NAME_STATUS			= "status";
-	final String			COLUMN_NAME_TEMPORAL_TYPE	= "temporal_type";
-	final String			COLUMN_NAME_TEST_TYPE_ID	= "test_type_id";
-	final String			COLUMN_NAME_USER_ID			= "user_id";
-	final String			SYS_DOMAIN					= "sysdomain";
+	String			COLUMN_NAME_ACTION_ID		= "action_id";
+	String			COLUMN_NAME_ACTIVE			= "active";
+	String			COLUMN_NAME_CREATED			= "created";
+	String			COLUMN_NAME_DESCRIPTION		= "description";
+	String			COLUMN_NAME_DOMAIN_ID		= "domain_id";
+	String			COLUMN_NAME_END_TIME		= "end_time";
+	String			COLUMN_NAME_ID				= "id";
+	String			COLUMN_NAME_KIS_ID			= "kis_id";
+	String			COLUMN_NAME_LOCAL_ID		= "local_id";
+	String			COLUMN_NAME_MODIFIED		= "modified";
+	String			COLUMN_NAME_NAME			= "name";
+	String			COLUMN_NAME_PORT_ID			= "port_id";
+	String			COLUMN_NAME_REQUEST_ID		= "request_id";
+	String			COLUMN_NAME_RESULT_TYPE		= "result_type";
+	String			COLUMN_NAME_START_TIME		= "start_time";
+	String			COLUMN_NAME_STATUS			= "status";
+	String			COLUMN_NAME_TEMPORAL_TYPE	= "temporal_type";
+	String			COLUMN_NAME_TEST_TYPE_ID	= "test_type_id";
+	String			COLUMN_NAME_USER_ID			= "user_id";
+	String			SYS_DOMAIN					= "sysdomain";
 
 }
 
