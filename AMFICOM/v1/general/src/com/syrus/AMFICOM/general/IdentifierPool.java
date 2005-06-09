97e28518e3ee2eb59eaf7c7ac29aa3bb247be79e
Arseniy Tashoyan <tashoyan@gmail.com>
2005-06-09 13:36:56 +0400

    BUGFIX: Wait while identifiers are loading not more then MAX_TIME_WAIT ms; throw IdentifierGenerationException if fifo of identifiers is exhausted. MINOR: Use trove map

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/IdentifierPool.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/IdentifierPool.java
@@ -1,5 +1,5 @@
 /*
- * $Id: IdentifierPool.java,v 1.20 2005/05/18 11:07:38 bass Exp $
+ * $Id: IdentifierPool.java,v 1.21 2005/06/09 09:36:56 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,33 +8,33 @@
 
 package com.syrus.AMFICOM.general;
 
-import java.util.Collections;
-import java.util.HashMap;
+import gnu.trove.TShortObjectHashMap;
+import gnu.trove.TShortObjectIterator;
+
 import java.util.Iterator;
 import java.util.Map;
 
-import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.corba.IdentifierGeneratorServer;
 import com.syrus.io.FIFOSaver;
 import com.syrus.util.Fifo;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.20 $, $Date: 2005/05/18 11:07:38 $
- * @author $Author: bass $
+ * @version $Revision: 1.21 $, $Date: 2005/06/09 09:36:56 $
+ * @author $Author: arseniy $
  * @module general_v1
  */
 public class IdentifierPool {
 	private static final int DEFAULT_CAPACITY = 10;
 	private static final int MAX_CAPACITY = 100;
-	private static final long TIME_TO_SLEEP = 500;
 	private static final double MIN_FILL_FACTOR = 0.2;
+	private static final long MAX_TIME_WAIT = 5 * 1000; /* Maximim time to wait while identifiers are loading*/
 
 	private static IGSConnectionManager igsConnectionMananger;
 	private static int capacity;
 
-	/* Map <Short objectEntity, LRUMap idPool> */
-	private static Map idPoolMap;
+	/* Map <short objectEntity, Fifo idPool> */
+	private static TShortObjectHashMap idPoolMap;
 
 	private IdentifierPool() {
 		// empty private construcor
@@ -43,7 +43,7 @@ public class IdentifierPool {
 	public static void init(IGSConnectionManager igsConnectionMananger1, int capacity1) {
 		igsConnectionMananger = igsConnectionMananger1;
 		capacity = (capacity1 <= MAX_CAPACITY) ? capacity1 : MAX_CAPACITY;
-		idPoolMap = Collections.synchronizedMap(new HashMap());
+		idPoolMap = new TShortObjectHashMap();
 	}
 
 	public static void init(IGSConnectionManager igsConnectionMananger1) {
@@ -53,34 +53,31 @@ public class IdentifierPool {
 	public static Identifier getGeneratedIdentifier(final short entityCode) throws IdentifierGenerationException {
 		assert ObjectEntities.isEntityCodeValid(entityCode) : "Illegal or unknown entity code supplied: " + entityCode;
 
-		final Short entityCodeShort = new Short(entityCode);
-		Fifo fifo = (Fifo) idPoolMap.get(entityCodeShort);
+		Fifo fifo = (Fifo) idPoolMap.get(entityCode);
 
-		// Add new fifo if needed
+		/* Add new fifo if needed */
 		if (fifo == null) {
 			fifo = new Fifo(capacity);
 			fillFifo(fifo, entityCode);
-			idPoolMap.put(entityCodeShort, fifo);
+			synchronized(idPoolMap) {
+				idPoolMap.put(entityCode, fifo);
+			}
 		}
 
-		// Transfer ids when fifo filling minimum than minFillFactor
+		/* Transfer ids when fifo filling minimum than minFillFactor */
 		if (fifo.getNumber() < MIN_FILL_FACTOR * fifo.capacity())
 			fillFifo(fifo, entityCode);
 
-		// Wait if fifo is empty yet
-		while (fifo.getNumber() < 1)
-			try {
-				Log.debugMessage("IdentifierPool.getGeneratedIdentifier | Wait for fetching ids", Log.DEBUGLEVEL10);
-				Thread.sleep(TIME_TO_SLEEP);
-			}
-			catch (final InterruptedException ie) {
-				Log.errorException(ie);
-			}
+		/* If identifiers available -- return one*/
+		if (fifo.getNumber() >= 1)
+			return (Identifier) fifo.remove();
 
-		return (Identifier) fifo.remove();
+		/* Else - throw IdentifierGenerationException*/
+		throw new IdentifierGenerationException("No more identifiers for entity '"
+				+ ObjectEntities.codeToString(entityCode) + "'/" + entityCode);
 	}
 
-	private static void fillFifo(Fifo fifo, final short entityCode) throws IdentifierGenerationException {
+	private static void fillFifo(final Fifo fifo, final short entityCode) throws IdentifierGenerationException {
 		IdentifierGeneratorServer igServer = null;
 		try {
 			igServer = igsConnectionMananger.getIGSReference();
@@ -89,31 +86,33 @@ public class IdentifierPool {
 			throw new IdentifierGenerationException("Cannot obtain reference on identifier generator server", ce);
 		}
 
-		IdentifierLoader il = new IdentifierLoader(igServer, fifo, entityCode);
-		il.start();
+		IdentifierLoader identifierLoader = new IdentifierLoader(igServer, fifo, entityCode);
+		identifierLoader.start();
+		/*	Do not wait more than MAX_TIME_WAIT*/
 		try {
-			il.join();
+			identifierLoader.join(MAX_TIME_WAIT);
 		}
 		catch (InterruptedException ie) {
 			Log.errorException(ie);
 		}
 	}
 
-	public static void serialize() {
-		for (Iterator it = idPoolMap.keySet().iterator(); it.hasNext();) {
-			Short code = (Short) it.next();
-			String entityName = ObjectEntities.codeToString(code);
-			Fifo fifo = (Fifo) idPoolMap.get(code);
+	protected static void serialize() {
+		for (final TShortObjectIterator iterator = idPoolMap.iterator(); iterator.hasNext();) {
+			iterator.advance();
+			final short entityCode = iterator.key();
+			final String entityName = ObjectEntities.codeToString(entityCode);
+			final Fifo fifo = (Fifo) iterator.value();
 			FIFOSaver.save(fifo, entityName);
 		}
 	}
 
-	public static void deserialize() {
-		Map codeNameFifo = FIFOSaver.load();
-		for (Iterator it = codeNameFifo.keySet().iterator(); it.hasNext();) {
-			String codeName = (String) it.next();
-			Short code = new Short(ObjectEntities.stringToCode(codeName));
-			idPoolMap.put(code, codeNameFifo.get(codeName));
+	protected static void deserialize() {
+		final Map codeNameFifo = FIFOSaver.load();
+		for (final Iterator it = codeNameFifo.keySet().iterator(); it.hasNext();) {
+			final String entityName = (String) it.next();
+			final short entityCode = ObjectEntities.stringToCode(entityName);
+			idPoolMap.put(entityCode, codeNameFifo.get(entityName));
 		}
 	}
 }
