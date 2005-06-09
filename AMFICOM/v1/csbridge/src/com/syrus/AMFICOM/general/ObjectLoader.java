76753f7c3c7e91b78cc7076b5b69ae8e999f1588
Arseniy Tashoyan <tashoyan@gmail.com>
2005-06-09 17:58:42 +0400

    MINOR: Use method 'Identifier.getIdentifiers'. MINOR: Javadocs

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/ObjectLoader.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/ObjectLoader.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ObjectLoader.java,v 1.3 2005/06/06 14:35:24 arseniy Exp $
+ * $Id: ObjectLoader.java,v 1.4 2005/06/09 13:58:42 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -14,7 +14,7 @@ import java.util.Set;
 import com.syrus.AMFICOM.general.corba.StorableObject_Transferable;
 
 /**
- * @version $Revision: 1.3 $, $Date: 2005/06/06 14:35:24 $
+ * @version $Revision: 1.4 $, $Date: 2005/06/09 13:58:42 $
  * @author $Author: arseniy $
  * @module csbridge_v1
  */
@@ -42,51 +42,67 @@ public abstract class ObjectLoader {
 		}
 	}
 
+	/**
+	 * Creates a set of identifiers as substract between identifiers from set <code>ids</code>
+	 * and identifiers (of identifiables) from set <code>butIdentifiables</code>
+	 * @param ids
+	 * @param butIdentifiables
+	 * @return Set of identifiers
+	 */
 	protected static final Set createLoadIds(final Set ids, final Set butIdentifiables) {
 		assert ids != null && !ids.isEmpty() : ErrorMessages.NON_EMPTY_EXPECTED;
 		assert butIdentifiables != null : ErrorMessages.NON_NULL_EXPECTED;
 
 		final Set loadIds = new HashSet(ids);
-		for (final Iterator it = butIdentifiables.iterator(); it.hasNext();) {
-			final Identifiable identifiable = (StorableObject) it.next();
-			final Identifier id = identifiable.getId();
-			loadIds.remove(id);
-		}
+		final Set butIds = Identifier.getIdentifiers(butIdentifiables);
+		loadIds.removeAll(butIds);
 		return loadIds;
 	}
 
+	/**
+	 * Removes from set of identifiers <code>loadIds</code> those,
+	 * which contained in set of identifiables <code>butIdentifiables</code>.
+	 * (I. e., parameter <code>loadIds</code> is passed as &quot;inout&quot; argument.
+	 * @param loadIds
+	 * @param butIdentifiables
+	 */
 	protected static final void removeFromLoadIds(final Set loadIds, final Set butIdentifiables) {
 		assert loadIds != null && !loadIds.isEmpty() : ErrorMessages.NON_EMPTY_EXPECTED;
 		assert butIdentifiables != null : ErrorMessages.NON_NULL_EXPECTED;
 
-		for (final Iterator it = butIdentifiables.iterator(); it.hasNext();) {
-			final Identifiable identifiable = (Identifiable) it.next();
-			final Identifier id = identifiable.getId();
-			loadIds.remove(id);
-		}
+		final Set butIds = Identifier.getIdentifiers(butIdentifiables);
+		loadIds.removeAll(butIds);
 	}
 
+	/**
+	 * Creates a set of identifiers as sum of identifiers from set <code>butIds</code>
+	 * and identifiers (of identifiables) from set <code>alsoButIdentifiables</code>
+	 * @param butIds
+	 * @param alsoButIdentifiables
+	 * @return
+	 */
 	protected static final Set createLoadButIds(final Set butIds, final Set alsoButIdentifiables) {
 		assert butIds != null : ErrorMessages.NON_NULL_EXPECTED;
 		assert alsoButIdentifiables != null : ErrorMessages.NON_NULL_EXPECTED;
 
 		final Set loadButIds = new HashSet(butIds);
-		for (final Iterator it = alsoButIdentifiables.iterator(); it.hasNext();) {
-			final Identifiable identifiable = (Identifiable) it.next();
-			final Identifier id = identifiable.getId();
-			loadButIds.add(id);
-		}
+		final Set alsoButIds = Identifier.getIdentifiers(alsoButIdentifiables);
+		loadButIds.addAll(alsoButIds);
 		return loadButIds;
 	}
 
+	/**
+	 * Adds to set of identifiers <code>loadButIds</code>
+	 * identifiers from set of identifiables <code>alsoButIdentifiables</code>.
+	 * (I. e., parameter <code>loadButIds</code> is passed as &quot;inout&quot; argument.
+	 * @param loadButIds
+	 * @param alsoButIdentifiables
+	 */
 	protected static final void addToLoadButIds(final Set loadButIds, final Set alsoButIdentifiables) {
 		assert loadButIds != null : ErrorMessages.NON_NULL_EXPECTED;
 		assert alsoButIdentifiables != null : ErrorMessages.NON_NULL_EXPECTED;
 
-		for (final Iterator it = alsoButIdentifiables.iterator(); it.hasNext();) {
-			final Identifiable identifiable = (Identifiable) it.next();
-			final Identifier id = identifiable.getId();
-			loadButIds.add(id);
-		}
+		final Set alsoButIds = Identifier.getIdentifiers(alsoButIdentifiables);
+		loadButIds.addAll(alsoButIds);
 	}
 }
