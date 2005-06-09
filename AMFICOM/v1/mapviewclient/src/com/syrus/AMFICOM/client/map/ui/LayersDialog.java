5844c659cbacbe3bc7a90bc0bd742f598fa36cfa
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-06-09 15:30:46 +0400

    COSMETIC: extract LayerVisibility to a separate file from LayersDialog

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/LayersDialog.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/LayersDialog.java
@@ -1,5 +1,5 @@
 /**
- * $Id: LayersDialog.java,v 1.4 2005/06/06 12:57:02 krupenn Exp $
+ * $Id: LayersDialog.java,v 1.5 2005/06/09 11:30:46 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -412,72 +412,3 @@ public class LayersDialog extends JDialog {
 		this.allLinksVisibility.setLabelPartial(islabelpartial);
 	}
 }
-	class LayerVisibility {
-		Object type;
-		Boolean visible;
-		Boolean labelVisible;
-		private boolean partial;
-		private boolean labelPartial;
-
-		public LayerVisibility(
-				Object type,
-				boolean visible,
-				boolean partial,
-				boolean labelVisible,
-				boolean labelPartial) {
-			this.type = type;
-			this.partial = partial;
-			this.labelPartial = labelPartial;
-			this.visible = new Boolean(visible);
-			this.labelVisible = new Boolean(labelVisible);
-		}
-
-		public LayerVisibility(
-				Object type,
-				boolean visible,
-				boolean labelVisible) {
-			this(type, visible, false, labelVisible, false);
-		}
-
-		public Boolean getLabelVisible() {
-			return this.labelVisible;
-		}
-
-		public void setLabelVisible(boolean labelVisible) {
-			this.labelVisible = new Boolean(labelVisible);
-		}
-
-		public Object getType() {
-			return this.type;
-		}
-
-		public void setType(Object type) {
-			this.type = type;
-		}
-
-		public Boolean getVisible() {
-			return this.visible;
-		}
-
-		public void setVisible(boolean visible) {
-			this.visible = new Boolean(visible);
-		}
-
-		public boolean isLabelPartial() {
-			return this.labelPartial;
-		}
-
-		public void setLabelPartial(boolean labelPartial) {
-			this.labelPartial = labelPartial;
-		}
-
-		public boolean isPartial() {
-			return this.partial;
-		}
-
-		public void setPartial(boolean partial) {
-			this.partial = partial;
-		}
-	}
-	
-
