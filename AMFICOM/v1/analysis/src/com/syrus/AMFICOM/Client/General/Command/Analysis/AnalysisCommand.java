a9c89abb05b4a9cbca1efaedc3f02078add569b6
Alexey Stratonnikov <alexsaa@gmail.com>
2005-06-10 14:13:43 +0400

    REFACTOR: AnalysisCommand.analyse() moved to Heap.makeAnalysis()

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Command/Analysis/AnalysisCommand.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Command/Analysis/AnalysisCommand.java
@@ -1,21 +1,11 @@
 package com.syrus.AMFICOM.Client.General.Command.Analysis;
 
 import com.syrus.AMFICOM.Client.Analysis.Heap;
-import com.syrus.AMFICOM.analysis.dadara.RefAnalysis;
 import com.syrus.AMFICOM.client.model.AbstractCommand;
-import com.syrus.io.BellcoreStructure;
 
 public class AnalysisCommand extends AbstractCommand {
-	public static void analyse() {
-		BellcoreStructure bs = Heap.getBSPrimaryTrace();
-		if (bs != null)
-		{
-	        RefAnalysis a = new RefAnalysis(bs);
-			Heap.setRefAnalysisPrimary(a);
-		}
-	}
 	public void execute()
 	{
-		analyse();
+		Heap.analyse();
 	}
 }
