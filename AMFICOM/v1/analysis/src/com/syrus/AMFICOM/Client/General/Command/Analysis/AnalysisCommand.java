5b75c21a25d5edfc678cd015013a08aa3719e0bc
Alexey Stratonnikov <alexsaa@gmail.com>
2005-06-10 14:00:03 +0400

    ADD: static void analyse() method

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Command/Analysis/AnalysisCommand.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Command/Analysis/AnalysisCommand.java
@@ -1,19 +1,21 @@
 package com.syrus.AMFICOM.Client.General.Command.Analysis;
 
 import com.syrus.AMFICOM.Client.Analysis.Heap;
-import com.syrus.AMFICOM.analysis.dadara.*;
+import com.syrus.AMFICOM.analysis.dadara.RefAnalysis;
 import com.syrus.AMFICOM.client.model.AbstractCommand;
 import com.syrus.io.BellcoreStructure;
 
 public class AnalysisCommand extends AbstractCommand {
-
-	public void execute()
-	{
+	public static void analyse() {
 		BellcoreStructure bs = Heap.getBSPrimaryTrace();
 		if (bs != null)
 		{
 	        RefAnalysis a = new RefAnalysis(bs);
 			Heap.setRefAnalysisPrimary(a);
 		}
+	}
+	public void execute()
+	{
+		analyse();
 	}
 }
