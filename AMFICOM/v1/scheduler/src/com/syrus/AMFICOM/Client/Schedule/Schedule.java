3afddca5788cea5c9a7537aca01f4c8bd0bcd162
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-06-14 16:24:06 +0400

    COMPAT extends AbstractApplication

--- AMFICOM/v1/scheduler/src/com/syrus/AMFICOM/Client/Schedule/Schedule.java
+++ AMFICOM/v1/scheduler/src/com/syrus/AMFICOM/Client/Schedule/Schedule.java
@@ -1,26 +1,17 @@
 package com.syrus.AMFICOM.Client.Schedule;
 
 import com.syrus.AMFICOM.Client.Scheduler.General.UIStorage;
-import com.syrus.AMFICOM.client.event.Dispatcher;
-import com.syrus.AMFICOM.client.model.ApplicationContext;
+import com.syrus.AMFICOM.client.model.AbstractApplication;
 import com.syrus.AMFICOM.client.model.Environment;
 import com.syrus.util.Application;
 
-public class Schedule {
+public class Schedule extends AbstractApplication {
 	private static final String APPLICATION_NAME = "scheduler";
 
-	private ApplicationContext	aContext;
-
 	public Schedule() {
-		if (Environment.canRun(Environment.MODULE_SCHEDULE)) {
-
-			this.aContext = new ApplicationContext();
-			this.aContext.setDispatcher(new Dispatcher());
-			this.aContext.setApplicationModel(new SchedulerModel(this.aContext));		
-			
-			ScheduleMainFrame frame = new ScheduleMainFrame(this.aContext);
-			frame.setIconImage(UIStorage.SCHEDULING_ICON_MINI);
-			frame.setVisible(true);
+		if (Environment.canRun(Environment.MODULE_SCHEDULE)) {			
+			super.aContext.setApplicationModel(new SchedulerModel(super.aContext));				
+			super.startMainFrame(new ScheduleMainFrame(super.aContext), UIStorage.SCHEDULING_ICON_MINI);
 		}
 	}
 
