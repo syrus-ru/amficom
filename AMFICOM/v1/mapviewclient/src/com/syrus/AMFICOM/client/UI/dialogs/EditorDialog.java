06fc9127d475492dad4ccccdca20837648632a84
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-06-14 14:10:40 +0400

    MAJOR: use JOptionPane to display dialog

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/UI/dialogs/EditorDialog.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/UI/dialogs/EditorDialog.java
@@ -1,5 +1,5 @@
 /**
- * $Id: EditorDialog.java,v 1.1 2005/06/08 13:44:06 krupenn Exp $
+ * $Id: EditorDialog.java,v 1.2 2005/06/14 10:10:40 krupenn Exp $
  * Syrus Systems.
  * Научно-технический центр.
  * Проект: АМФИКОМ.
@@ -17,100 +17,64 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import com.syrus.AMFICOM.client.UI.StorableObjectEditor;
 import com.syrus.AMFICOM.client.model.Environment;
+import com.syrus.AMFICOM.client.resource.LangModel;
 import com.syrus.AMFICOM.client.resource.LangModelGeneral;
 
 /**
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * @author $Author: krupenn $
  * @module commonclient_v1
  */
-public class EditorDialog extends JDialog
+public class EditorDialog
 {
-	protected StorableObjectEditor editor = null;
-	protected JPanel buttonPanel = new JPanel();
-	protected JButton acceptButton = new JButton();
-	protected JButton cancelButton = new JButton();
-	protected JButton helpButton = new JButton();
-
 	protected boolean accept = false;
-	protected BorderLayout borderLayout1 = new BorderLayout();
-	protected  FlowLayout flowLayout1 = new FlowLayout();
 
-	public EditorDialog(
+	public static boolean showEditorDialog(
 			String title,
-			boolean modal,
 			Object object,
 			StorableObjectEditor editor) {
-		super(Environment.getActiveWindow(), title, modal);
-
-		this.editor = editor;
+		
 		if(editor != null)
 			editor.setObject(object);
 
-		try {
-			jbInit();
-			pack();
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-
-	void jbInit() throws Exception {
-		this.setResizable(false);
-		this.setSize(new Dimension(590, 400));
-
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Dimension frameSize = this.getSize();
-		this.setLocation(
-				(screenSize.width - frameSize.width) / 2,
-				(screenSize.height - frameSize.height) / 2);
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.add(editor.getGUI(), BorderLayout.CENTER);
 		
-		JComponent uiComponent = this.editor.getGUI();
-		uiComponent.setPreferredSize(new Dimension(590, 400));
-		this.buttonPanel.setBorder(BorderFactory.createEtchedBorder());
-		this.getContentPane().setLayout(this.borderLayout1);
-		this.buttonPanel.setLayout(this.flowLayout1);
+		final String okButton = LangModelGeneral.getString("Ok");
+		final String cancelButton = LangModelGeneral.getString("Cancel");
+		final JOptionPane optionPane = new JOptionPane(
+				mainPanel, 
+				JOptionPane.PLAIN_MESSAGE,
+				JOptionPane.OK_CANCEL_OPTION, 
+				null, 
+				new Object[] { okButton, cancelButton });
 
-		this.acceptButton.setText(LangModelGeneral.getString("Ok"));
-		this.acceptButton
-				.addActionListener(new java.awt.event.ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						accept();
-					}
-				});
-		this.cancelButton.setText(LangModelGeneral.getString("Cancel"));
-		this.cancelButton
-				.addActionListener(new java.awt.event.ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						cancel();
-					}
-				});
-		this.helpButton.setText(LangModelGeneral.getString("Help"));
+		final JDialog dialog = optionPane.createDialog(
+				Environment.getActiveWindow(), title);
+		Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = Math.min(screenDim.width / 2, 590);
+		int height = Math.min(screenDim.height / 2, 400);
+		dialog.setSize(new Dimension(width, height));
 
-		this.flowLayout1.setAlignment(2);
+		dialog.show();
+		dialog.dispose();
 
-		getContentPane().add(uiComponent, BorderLayout.CENTER);
-		getContentPane().add(this.buttonPanel, BorderLayout.SOUTH);
-		this.buttonPanel.add(this.acceptButton, null);
-		this.buttonPanel.add(this.cancelButton, null);
+		final Object selectedValue = optionPane.getValue();
+
+		if (selectedValue == okButton) {
+			editor.commitChanges();
+			return true;
+		}
+		return false;
 	}
-
-	public boolean ifAccept() {
-		return this.accept;
-	}
-
-	void accept() {
-		this.editor.commitChanges();
-		this.accept = true;
-		this.dispose();
-	}
-
-	void cancel() {
-		this.accept = false;
-		this.dispose();
+	
+	private EditorDialog() {
+		// empty
 	}
 }
