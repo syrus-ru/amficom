d941446e5956d30315b9808ffab07bb4a28ec3e8
Stanislav Kholshin <kholshin@gmail.com>
2005-06-14 16:33:51 +0400

    FIX: imports

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/Analysis/Reflectometry/Analyse.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/Analysis/Reflectometry/Analyse.java
@@ -5,8 +5,11 @@ import java.awt.Image;
 import javax.swing.UIManager;
 
 import com.syrus.AMFICOM.Client.Analysis.Reflectometry.UI.AnalyseMainFrameSimplified;
-import com.syrus.AMFICOM.Client.General.Model.*;
-import com.syrus.AMFICOM.client.model.*;
+import com.syrus.AMFICOM.Client.General.Model.AnalyseApplicationModelFactory;
+import com.syrus.AMFICOM.Client.General.Model.AnalysisResourceKeys;
+import com.syrus.AMFICOM.Client.General.Model.ReflectometryAnalyseApplicationModelFactory;
+import com.syrus.AMFICOM.client.model.AbstractApplication;
+import com.syrus.AMFICOM.client.model.Environment;
 import com.syrus.util.Application;
 
 public class Analyse extends AbstractApplication
