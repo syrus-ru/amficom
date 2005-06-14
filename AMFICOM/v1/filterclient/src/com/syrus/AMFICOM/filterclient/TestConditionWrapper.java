86506fd8c564198ecbf13cbbebed9320ce64e3c8
Arseniy Tashoyan <tashoyan@gmail.com>
2005-06-14 11:01:11 +0400

    COSMETIC

--- AMFICOM/v1/filterclient/src/com/syrus/AMFICOM/filterclient/TestConditionWrapper.java
+++ AMFICOM/v1/filterclient/src/com/syrus/AMFICOM/filterclient/TestConditionWrapper.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestConditionWrapper.java,v 1.7 2005/06/06 11:52:43 bob Exp $
+ * $Id: TestConditionWrapper.java,v 1.8 2005/06/14 07:01:11 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -16,8 +16,8 @@ import com.syrus.AMFICOM.measurement.TestWrapper;
 import com.syrus.AMFICOM.newFilter.ConditionKey;
 
 /**
- * @version $Revision: 1.7 $, $Date: 2005/06/06 11:52:43 $
- * @author $Author: bob $
+ * @version $Revision: 1.8 $, $Date: 2005/06/14 07:01:11 $
+ * @author $Author: arseniy $
  * @module filterclient_v1
  */
 public class TestConditionWrapper implements ConditionWrapper {
@@ -28,12 +28,12 @@ public class TestConditionWrapper implements ConditionWrapper {
 	private static final String START_TIME = "Start time";
 	private static final String END_TIME = "End time";
 	
-	private static final String[] testStatusNames = new String[]{"NEW", "SCHEDULED", "PROCESSING", "COMPLETED", "ABORTED" }; 
+	private static final String[] TEST_STATUS_NAMES = new String[]{"NEW", "SCHEDULED", "PROCESSING", "COMPLETED", "ABORTED" }; 
 	
 	private static ArrayList keys = new ArrayList();
 	
 	static {
-		keys.add(new ConditionKey(TestWrapper.COLUMN_STATUS, STATUS, testStatusNames));
+		keys.add(new ConditionKey(TestWrapper.COLUMN_STATUS, STATUS, TEST_STATUS_NAMES));
 		keys.add(new ConditionKey(TestWrapper.COLUMN_START_TIME, START_TIME, ConditionWrapper.DATE));
 		keys.add(new ConditionKey(TestWrapper.COLUMN_END_TIME, END_TIME, ConditionWrapper.DATE));
 		keys.add(new ConditionKey(ObjectEntities.MONITOREDELEMENT_ENTITY_CODE));
