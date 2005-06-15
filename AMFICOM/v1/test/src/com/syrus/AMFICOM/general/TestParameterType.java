53e47c0cd1cd9c5e71f42939b39275478b7d4acd
Arseniy Tashoyan <tashoyan@gmail.com>
2005-06-15 13:54:13 +0400

    misc

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/general/TestParameterType.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/general/TestParameterType.java
@@ -1,19 +1,21 @@
 /*
- * $Id: TestParameterType.java,v 1.9 2005/06/02 14:31:02 arseniy Exp $ Copyright © 2004 Syrus Systems. Научно-технический центр. Проект:
+ * $Id: TestParameterType.java,v 1.10 2005/06/15 09:54:13 arseniy Exp $ Copyright © 2004 Syrus Systems. Научно-технический центр. Проект:
  * АМФИКОМ.
  */
 package com.syrus.AMFICOM.general;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import junit.framework.Test;
 
 import com.syrus.AMFICOM.general.corba.DataType;
-import com.syrus.AMFICOM.general.corba.OperationSort;
 import com.syrus.AMFICOM.general.corba.ParameterType_Transferable;
+import com.syrus.AMFICOM.general.corba.StorableObjectCondition_TransferablePackage.CompoundCondition_TransferablePackage.CompoundConditionSort;
+import com.syrus.AMFICOM.general.corba.StorableObjectCondition_TransferablePackage.TypicalCondition_TransferablePackage.OperationSort;
 
 /**
- * @version $Revision: 1.9 $, $Date: 2005/06/02 14:31:02 $
+ * @version $Revision: 1.10 $, $Date: 2005/06/15 09:54:13 $
  * @author $Author: arseniy $
  * @module test
  */
@@ -27,11 +29,11 @@ public class TestParameterType extends CommonTest {
 		return suiteWrapper(TestParameterType.class);
 	}
 
-	public void t1estCreateInstance() throws ApplicationException {
-		String codename = ParameterTypeCodenames.TRACE_FLAG_GAIN_SPLICE_ON;
-		String name = "Gain splice";
-		String description = "Gain splice flag";
-		DataType dataType = DataType.DATA_TYPE_BOOLEAN;
+	public void testCreateInstance() throws ApplicationException {
+		final String codename = ParameterTypeCodenames.REFLECTOGRAMMA_ETALON;
+		final String name = "Reflectogramma-etalon";
+		String description = "Raflectogramma, marked as etalon";
+		DataType dataType = DataType.DATA_TYPE_RAW;
 		ParameterType parameterType = ParameterType.createInstance(creatorUser.getId(), codename, description, name, dataType);
 
 		ParameterType_Transferable ptt = (ParameterType_Transferable) parameterType.getTransferable();
@@ -46,30 +48,10 @@ public class TestParameterType extends CommonTest {
 		assertEquals(parameterType.getDescription(), parameterType1.getDescription());
 		assertEquals(parameterType.getDataType(), parameterType1.getDataType());
 
-
-		codename = ParameterTypeCodenames.TRACE_FLAG_LIVE_FIBER_DETECT;
-		name = "Live fiber detect";
-		description = "Live fiber detect flag";
-		dataType = DataType.DATA_TYPE_BOOLEAN;
-		parameterType = ParameterType.createInstance(creatorUser.getId(), codename, description, name, dataType);
-
-		ptt = (ParameterType_Transferable) parameterType.getTransferable();
-		parameterType1 = new ParameterType(ptt);
-		assertEquals(parameterType.getId(), parameterType1.getId());
-		assertEquals(parameterType.getCreated(), parameterType1.getCreated());
-		assertEquals(parameterType.getModified(), parameterType1.getModified());
-		assertEquals(parameterType.getCreatorId(), parameterType1.getCreatorId());
-		assertEquals(parameterType.getModifierId(), parameterType1.getModifierId());
-		assertEquals(parameterType.getCodename(), parameterType1.getCodename());
-		assertEquals(parameterType.getName(), parameterType1.getName());
-		assertEquals(parameterType.getDescription(), parameterType1.getDescription());
-		assertEquals(parameterType.getDataType(), parameterType1.getDataType());
-
-
 		StorableObjectPool.flush(ObjectEntities.PARAMETERTYPE_ENTITY_CODE, true);
 	}
 
-	public void testUpdate() throws ApplicationException {
+	public void _testUpdate() throws ApplicationException {
 		TypicalCondition tc = new TypicalCondition("ref_pulswd",
 				OperationSort.OPERATION_EQUALS,
 				ObjectEntities.PARAMETERTYPE_ENTITY_CODE,
@@ -89,34 +71,35 @@ public class TestParameterType extends CommonTest {
 		StorableObjectPool.flush(ObjectEntities.PARAMETERTYPE_ENTITY_CODE, false);
 	}
 
-//	public void testRetrieveByCondition() throws ApplicationException {
-//		TypicalCondition tc1 = new TypicalCondition(ParameterTypeCodenames.ALARM_STATUS,
-//				OperationSort.OPERATION_EQUALS,
-//				new Short(ObjectEntities.PARAMETERTYPE_ENTITY_CODE),
-//				StorableObjectWrapper.COLUMN_CODENAME);
-//		TypicalCondition tc2 = new TypicalCondition(ParameterTypeCodenames.HZ_CHO,
-//				OperationSort.OPERATION_EQUALS,
-//				new Short(ObjectEntities.PARAMETERTYPE_ENTITY_CODE),
-//				StorableObjectWrapper.COLUMN_CODENAME);
-//		CompoundCondition cc = new CompoundCondition(tc1, CompoundConditionSort.OR, tc2);
-//		List parameterTypes = GeneralStorableObjectPool.getStorableObjectsByCondition(cc, true);
-//		for (Iterator it = parameterTypes.iterator(); it.hasNext();) {
-//			ParameterType parameterType = (ParameterType) it.next();
-//			System.out.println("id: '" + parameterType.getId() + "' codename: '" + parameterType.getCodename() + "', description: '" + parameterType.getDescription() + "'");
-//		}
-//	}
+	public void _testRetrieveByCondition() throws ApplicationException {
+		TypicalCondition tc1 = new TypicalCondition(ParameterTypeCodenames.ALARM_STATUS,
+				OperationSort.OPERATION_EQUALS,
+				new Short(ObjectEntities.PARAMETERTYPE_ENTITY_CODE),
+				StorableObjectWrapper.COLUMN_CODENAME);
+		TypicalCondition tc2 = new TypicalCondition(ParameterTypeCodenames.HZ_CHO,
+				OperationSort.OPERATION_EQUALS,
+				new Short(ObjectEntities.PARAMETERTYPE_ENTITY_CODE),
+				StorableObjectWrapper.COLUMN_CODENAME);
+		CompoundCondition cc = new CompoundCondition(tc1, CompoundConditionSort.OR, tc2);
+		Set parameterTypes = StorableObjectPool.getStorableObjectsByCondition(cc, true);
+		for (Iterator it = parameterTypes.iterator(); it.hasNext();) {
+			ParameterType parameterType = (ParameterType) it.next();
+			System.out.println("id: '" + parameterType.getId() + "' codename: '" + parameterType.getCodename() + "', description: '" + parameterType.getDescription() + "'");
+		}
+	}
 
-//	public void testDelete() throws ApplicationException {
-//		TypicalCondition tc1 = new TypicalCondition(ParameterTypeCodenames.ALARM_STATUS,
-//				OperationSort.OPERATION_EQUALS,
-//				new Short(ObjectEntities.PARAMETERTYPE_ENTITY_CODE),
-//				StorableObjectWrapper.COLUMN_CODENAME);
-//		TypicalCondition tc2 = new TypicalCondition(ParameterTypeCodenames.HZ_CHO,
-//				OperationSort.OPERATION_EQUALS,
-//				new Short(ObjectEntities.PARAMETERTYPE_ENTITY_CODE),
-//				StorableObjectWrapper.COLUMN_CODENAME);
-//		CompoundCondition cc = new CompoundCondition(tc1, CompoundConditionSort.OR, tc2);
-//		List parameterTypes = GeneralStorableObjectPool.getStorableObjectsByCondition(cc, true);
-//		GeneralStorableObjectPool.delete(parameterTypes);
-//	}
+	public void _testDelete() throws ApplicationException {
+		TypicalCondition tc1 = new TypicalCondition(ParameterTypeCodenames.ALARM_STATUS,
+				OperationSort.OPERATION_EQUALS,
+				new Short(ObjectEntities.PARAMETERTYPE_ENTITY_CODE),
+				StorableObjectWrapper.COLUMN_CODENAME);
+		TypicalCondition tc2 = new TypicalCondition(ParameterTypeCodenames.HZ_CHO,
+				OperationSort.OPERATION_EQUALS,
+				new Short(ObjectEntities.PARAMETERTYPE_ENTITY_CODE),
+				StorableObjectWrapper.COLUMN_CODENAME);
+		CompoundCondition cc = new CompoundCondition(tc1, CompoundConditionSort.OR, tc2);
+		Set parameterTypes = StorableObjectPool.getStorableObjectsByCondition(cc, true);
+		StorableObjectPool.delete(parameterTypes);
+	}
+
 }
