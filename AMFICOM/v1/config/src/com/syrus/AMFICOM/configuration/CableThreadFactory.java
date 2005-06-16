b6eb86abcd51dc2d18713ea7d9b9822fb22a4d7a
Arseniy Tashoyan <tashoyan@gmail.com>
2005-06-16 16:35:14 +0400

    ADD: Factory

---
+++ AMFICOM/v1/config/src/com/syrus/AMFICOM/configuration/CableThreadFactory.java
@@ -0,0 +1,49 @@
+/*
+ * $Id: CableThreadFactory.java,v 1.1 2005/06/16 12:35:14 arseniy Exp $
+ * 
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.configuration;
+
+import org.omg.CORBA.portable.IDLEntity;
+
+import com.syrus.AMFICOM.configuration.corba.CableThread_Transferable;
+import com.syrus.AMFICOM.general.CreateObjectException;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.StorableObject;
+import com.syrus.AMFICOM.general.StorableObjectFactory;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/06/16 12:35:14 $
+ * @author $Author: arseniy $
+ * @module config_v1
+ */
+final class CableThreadFactory extends StorableObjectFactory {
+
+	/**
+	 * @param transferable
+	 * @throws CreateObjectException
+	 * @see StorableObjectFactory#newInstance(IDLEntity)
+	 */
+	protected StorableObject newInstance(final IDLEntity transferable) throws CreateObjectException {
+		return new CableThread((CableThread_Transferable) transferable);
+	}
+
+	/**
+	 * @param transferable
+	 * @see com.syrus.AMFICOM.general.StorableObjectFactory#getId(org.omg.CORBA.portable.IDLEntity)
+	 */
+	protected Identifier getId(final IDLEntity transferable) {
+		return new Identifier(((CableThread_Transferable) transferable).header.id);
+	}
+
+	/**
+	 * @param length
+	 * @see com.syrus.AMFICOM.general.StorableObjectFactory#allocateArrayOfTransferables(int)
+	 */
+	protected IDLEntity[] allocateArrayOfTransferables(final int length) {
+		return new CableThread_Transferable[length];
+	}
+}
