f70107654edc6dc3bc4654e81b73eff107b2a000
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-06-17 17:34:08 +0400

    COMPATIBILITY: MapEdotir constructor

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/editor/MapEditorRun.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/editor/MapEditorRun.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapEditorRun.java,v 1.8 2005/06/06 12:57:02 krupenn Exp $
+ * $Id: MapEditorRun.java,v 1.9 2005/06/17 13:34:08 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -15,7 +15,7 @@ import com.syrus.AMFICOM.client.model.DefaultMapEditorApplicationModelFactory;
  * 
  * 
  * 
- * @version $Revision: 1.8 $, $Date: 2005/06/06 12:57:02 $
+ * @version $Revision: 1.9 $, $Date: 2005/06/17 13:34:08 $
  * @module mapviewclient_v1
  * @author $Author: krupenn $
  */
@@ -25,6 +25,6 @@ public class MapEditorRun {
 	}
 
 	public static void main(String[] args) {
-		new MapEditor(new DefaultMapEditorApplicationModelFactory());
+		new MapEditor();
 	}
 }
