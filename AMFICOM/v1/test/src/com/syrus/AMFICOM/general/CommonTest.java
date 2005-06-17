2e7945624469442735bef87aa6cf4c1c6ae9468a
Arseniy Tashoyan <tashoyan@gmail.com>
2005-06-17 20:36:12 +0400

    COMPATIBILITY

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/general/CommonTest.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/general/CommonTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CommonTest.java,v 1.7 2005/06/16 13:26:44 arseniy Exp $
+ * $Id: CommonTest.java,v 1.8 2005/06/17 16:36:12 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -66,7 +66,7 @@ import com.syrus.util.Log;
 import com.syrus.util.database.DatabaseConnection;
 
 /**
- * @version $Revision: 1.7 $, $Date: 2005/06/16 13:26:44 $
+ * @version $Revision: 1.8 $, $Date: 2005/06/17 16:36:12 $
  * @author $Author: arseniy $
  * @module test
  */
@@ -181,7 +181,7 @@ public abstract class CommonTest extends TestCase {
 
 	private static void initStorableObjectPools() {
 		try {
-			SystemUserDatabase userDatabase = (SystemUserDatabase) DatabaseContext.getDatabase(ObjectEntities.SYSTEM_USER_ENTITY_CODE);
+			SystemUserDatabase userDatabase = (SystemUserDatabase) DatabaseContext.getDatabase(ObjectEntities.SYSTEMUSER_CODE);
 			SystemUser sysUser = userDatabase.retrieveForLogin(SystemUserWrapper.SYS_LOGIN);
 			creatorUser = sysUser;
 			DatabaseObjectLoader.init(sysUser.getId());
