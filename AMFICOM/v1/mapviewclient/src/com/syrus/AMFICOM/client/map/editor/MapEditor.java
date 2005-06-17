02ddd25c214783a729c418a1fbd0a8e3c44eb96f
Peter Peskovsky <peskovsky@gmail.com>
2005-06-17 17:21:55 +0400

    TRIVIAL: MapEditor extends AbstractApplication now.

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/editor/MapEditor.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/editor/MapEditor.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapEditor.java,v 1.8 2005/06/14 12:07:14 krupenn Exp $
+ * $Id: MapEditor.java,v 1.9 2005/06/17 13:21:55 peskovsky Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -9,41 +9,58 @@
 
 package com.syrus.AMFICOM.client.map.editor;
 
-import java.awt.Frame;
-import java.awt.Toolkit;
+import java.awt.Image;
 
-import com.syrus.AMFICOM.client.event.Dispatcher;
-import com.syrus.AMFICOM.client.model.ApplicationContext;
+import javax.swing.UIManager;
+
+import com.syrus.AMFICOM.client.model.AbstractApplication;
 import com.syrus.AMFICOM.client.model.DefaultMapEditorApplicationModelFactory;
-import com.syrus.AMFICOM.client.model.Environment;
-import com.syrus.AMFICOM.client.model.MapEditorApplicationModelFactory;
-import com.syrus.util.Application;
 
 /**
  * Запуск основного окна модуля "Редактор топологических схем".
  * 
- * @author $Author: krupenn $
- * @version $Revision: 1.8 $, $Date: 2005/06/14 12:07:14 $
+ * @author $Author: peskovsky $
+ * @version $Revision: 1.9 $, $Date: 2005/06/17 13:21:55 $
  * @module mapviewclient_v1
  */
-public class MapEditor {
-	ApplicationContext aContext = new ApplicationContext();
+//public class MapEditor {
+//	ApplicationContext aContext = new ApplicationContext();
+//
+//	public MapEditor(MapEditorApplicationModelFactory factory) {
+//		if(!Environment.canRun(Environment.MODULE_MAP))
+//			return;
+//
+//		Application.init("mapviewclient");
+//		this.aContext.setApplicationModel(factory.create());
+//		this.aContext.setDispatcher(new Dispatcher());
+//		Frame frame = new MapEditorMainFrame(this.aContext);
+//		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(
+//				"images/main/map_mini.gif"));
+//		frame.setVisible(true);
+//	}
+//
+//	public static void main(String[] args) {
+//		new MapEditor(new DefaultMapEditorApplicationModelFactory());
+//	}
+//}
 
-	public MapEditor(MapEditorApplicationModelFactory factory) {
-		if(!Environment.canRun(Environment.MODULE_MAP))
-			return;
+public class MapEditor extends AbstractApplication
+{
+        public static final String APPLICATION_NAME = "mapeditor";
 
-		Application.init("mapviewclient");
-		this.aContext.setApplicationModel(factory.create());
-		this.aContext.setDispatcher(new Dispatcher());
-		Frame frame = new MapEditorMainFrame(this.aContext);
-		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(
-				"images/main/map_mini.gif"));
-		frame.setVisible(true);
-	}
+        public MapEditor() {
+                super(MapEditor.APPLICATION_NAME);
+        }
+        
+        protected void init() {
+                super.init();           
+                super.aContext.setApplicationModel(new DefaultMapEditorApplicationModelFactory().create());
+                super.startMainFrame(new MapEditorMainFrame(this.aContext), (Image)
+UIManager.get("images/main/map_mini.gif"));
+        }
 
-	public static void main(String[] args) {
-		new MapEditor(new DefaultMapEditorApplicationModelFactory());
-	}
+        public static void main(String[] args) {
+                new MapEditor();
+        }
 }
 
