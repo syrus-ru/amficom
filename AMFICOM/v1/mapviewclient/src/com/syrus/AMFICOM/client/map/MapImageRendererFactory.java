cda8e600bc9ce9eae0f0f6984ff6226df7f6a782
Peter Peskovsky <peskovsky@gmail.com>
2005-06-21 16:32:44 +0400

    BUGFIX: MapImageRender is now constructed correctly - by three parameters, not by one.

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/MapImageRendererFactory.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/MapImageRendererFactory.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapImageRendererFactory.java,v 1.1 2005/06/15 07:42:28 krupenn Exp $
+ * $Id: MapImageRendererFactory.java,v 1.2 2005/06/21 12:32:44 peskovsky Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -13,51 +13,65 @@ import java.lang.reflect.InvocationTargetException;
 import com.syrus.AMFICOM.client.model.Environment;
 
 /**
- * @version $Revision: 1.1 $, $Date: 2005/06/15 07:42:28 $
- * @author $Author: krupenn $
+ * @version $Revision: 1.2 $, $Date: 2005/06/21 12:32:44 $
+ * @author $Author: peskovsky $
  * @module mapviewclient
  */
 public final class MapImageRendererFactory {
 	private MapImageRendererFactory() {
-		//empty
+		// empty
 	}
 
-	public static MapImageRenderer create(String className, MapImageLoader loader)
-		throws MapDataException
-		{
-			Environment.log(Environment.LOG_LEVEL_FINER, "method call MapImageRendererFactory.create()");
+	public static MapImageRenderer create(String className,
+			MapCoordinatesConverter coordsConverter,
+			MapContext mapContext, 
+			MapImageLoader loader) throws MapDataException {
+		Environment.log(Environment.LOG_LEVEL_FINER,
+				"method call MapImageRendererFactory.create()");
 
-			try
-			{
-				Class clazz = Class.forName(className);
-				Constructor[] constructors = clazz.getDeclaredConstructors();
-				for (int i = 0; i < constructors.length; i++) {
-					Class[] parameterTypes = constructors[i].getParameterTypes();
-					if (parameterTypes.length == 1 && parameterTypes[0].equals(MapImageLoader.class)) {
-						Constructor constructor = constructors[i];
-						constructor.setAccessible(true);
-						Object[] initArgs = new Object[1];
-						initArgs[0] = loader;
-						return (MapImageRenderer)constructor.newInstance(initArgs);
-					}
+		try {
+			Class clazz = Class.forName(className);
+			Constructor[] constructors = clazz.getDeclaredConstructors();
+			for (int i = 0; i < constructors.length; i++) {
+				Class[] parameterTypes = constructors[i].getParameterTypes();
+				if (parameterTypes.length == 3
+						&& parameterTypes[0].equals(MapCoordinatesConverter.class)
+						&& parameterTypes[1].equals(MapContext.class)
+						&& parameterTypes[2].equals(MapImageLoader.class)) {
+					Constructor constructor = constructors[i];
+					constructor.setAccessible(true);
+					Object[] initArgs = new Object[3];
+					initArgs[0] = coordsConverter;
+					initArgs[1] = mapContext;
+					initArgs[2] = loader;
+					return (MapImageRenderer) constructor.newInstance(initArgs);
 				}
-//				MapImageRenderer renderer = (MapImageRenderer )Class.forName(className).newInstance();
-			} catch(ClassNotFoundException cnfe) {
-				cnfe.printStackTrace();
-				throw new MapDataException("MapImageRendererFactory.create() throws ClassNotFoundException");
-			} catch(InstantiationException ie) {
-				ie.printStackTrace();
-				throw new MapDataException("MapImageRendererFactory.create() throws InstantiationException");
-			} catch(IllegalAccessException iae) {
-				iae.printStackTrace();
-				throw new MapDataException("MapImageRendererFactory.create() throws IllegalAccessException");
-			} catch(IllegalArgumentException iae) {
-				iae.printStackTrace();
-				throw new MapDataException("MapImageRendererFactory.create() throws IllegalArgumentException");
-			} catch(InvocationTargetException ite) {
-				ite.printStackTrace();
-				throw new MapDataException("MapImageRendererFactory.create() throws InvocationTargetException");
 			}
-			throw new MapDataException("MapImageRendererFactory.create() cannot find constructor with arguments (MapImageLoader) for class " + className);
+			// MapImageRenderer renderer = (MapImageRenderer
+			// )Class.forName(className).newInstance();
+		} catch (ClassNotFoundException cnfe) {
+			cnfe.printStackTrace();
+			throw new MapDataException(
+					"MapImageRendererFactory.create() throws ClassNotFoundException");
+		} catch (InstantiationException ie) {
+			ie.printStackTrace();
+			throw new MapDataException(
+					"MapImageRendererFactory.create() throws InstantiationException");
+		} catch (IllegalAccessException iae) {
+			iae.printStackTrace();
+			throw new MapDataException(
+					"MapImageRendererFactory.create() throws IllegalAccessException");
+		} catch (IllegalArgumentException iae) {
+			iae.printStackTrace();
+			throw new MapDataException(
+					"MapImageRendererFactory.create() throws IllegalArgumentException");
+		} catch (InvocationTargetException ite) {
+			ite.printStackTrace();
+			throw new MapDataException(
+					"MapImageRendererFactory.create() throws InvocationTargetException");
 		}
+		throw new MapDataException(
+				"MapImageRendererFactory.create() cannot find constructor with arguments (MapImageLoader) for class "
+						+ className);
+	}
 }
