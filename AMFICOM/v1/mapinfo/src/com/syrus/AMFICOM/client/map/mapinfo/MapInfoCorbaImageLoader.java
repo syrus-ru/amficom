70d8248b270c4758b2cfa3999592378ea506dbce
Peter Peskovsky <peskovsky@gmail.com>
2005-06-21 17:17:06 +0400

    Using IdlSessionKey instead of SessionKey_Transferrable

--- AMFICOM/v1/mapinfo/src/com/syrus/AMFICOM/client/map/mapinfo/MapInfoCorbaImageLoader.java
+++ AMFICOM/v1/mapinfo/src/com/syrus/AMFICOM/client/map/mapinfo/MapInfoCorbaImageLoader.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: MapInfoCorbaImageLoader.java,v 1.1.2.1 2005/06/20 15:31:23 peskovsky Exp $
+ * $Id: MapInfoCorbaImageLoader.java,v 1.1.2.2 2005/06/21 13:17:06 peskovsky Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -21,11 +21,11 @@ import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
 import com.syrus.AMFICOM.map.TopologicalImageQuery;
 import com.syrus.AMFICOM.map.corba.RenderedImage_Transferable;
 import com.syrus.AMFICOM.map.corba.TopologicalImageQuery_Transferable;
-import com.syrus.AMFICOM.security.corba.SessionKey_Transferable;
+import com.syrus.AMFICOM.security.corba.IdlSessionKey;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.1.2.1 $, $Date: 2005/06/20 15:31:23 $
+ * @version $Revision: 1.1.2.2 $, $Date: 2005/06/21 13:17:06 $
  * @author $Author: peskovsky $
  * @module map_v1
  */
@@ -45,7 +45,7 @@ public class MapInfoCorbaImageLoader implements MapImageLoader
 	 */
 	public void stopRendering()
 	{
-		SessionKey_Transferable keyt = LoginManager.getSessionKeyTransferable();
+		IdlSessionKey keyt = LoginManager.getSessionKeyTransferable();
 		try {
 			this.connection.getMscharServer().stopRenderTopologicalImage(LoginManager.getSessionKey().hashCode(), keyt);
 		} catch (AMFICOMRemoteException e) {
@@ -61,7 +61,7 @@ public class MapInfoCorbaImageLoader implements MapImageLoader
 		query.setUserID(LoginManager.getSessionKey().hashCode());
 		
 		TopologicalImageQuery_Transferable transf = query.getTransferable();
-		SessionKey_Transferable keyt = LoginManager.getSessionKeyTransferable();
+		IdlSessionKey keyt = LoginManager.getSessionKeyTransferable();
 		RenderedImage_Transferable rit;
 		try {
 			rit = this.connection.getMscharServer().transmitTopologicalImage(transf,keyt);
