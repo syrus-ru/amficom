892a1697985046f8591a27055108438823a8fd2c
Peter Peskovsky <peskovsky@gmail.com>
2005-06-22 16:20:56 +0400

    BUGFIX: In constructor scale and center parameters are initialized not only for MapContext but for MapImageRenderer too.

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapFrame.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapFrame.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapFrame.java,v 1.50 2005/06/22 08:43:49 krupenn Exp $
+ * $Id: MapFrame.java,v 1.51 2005/06/22 12:20:56 peskovsky Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -82,8 +82,8 @@ import com.syrus.AMFICOM.scheme.Scheme;
  * 
  * 
  * 
- * @version $Revision: 1.50 $, $Date: 2005/06/22 08:43:49 $
- * @author $Author: krupenn $
+ * @version $Revision: 1.51 $, $Date: 2005/06/22 12:20:56 $
+ * @author $Author: peskovsky $
  * @module mapviewclient_v1
  */
 public class MapFrame extends JInternalFrame 
@@ -181,6 +181,8 @@ public class MapFrame extends JInternalFrame
 
 		mapContext.setCenter(MapPropertiesManager.getCenter());
 		mapContext.setScale(MapPropertiesManager.getZoom());
+		renderer.setScale(mapContext.getScale());
+		renderer.setCenter(mapContext.getCenter());		
 
 //		this.logicalNetLayer.repaint(true);
 	}
