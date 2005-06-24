21e3a6acfaf4c4630696781628fb94de838fbe8b
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-06-24 17:04:05 +0400

    COMPATIBILITY: editors get NetMapViewer instead of LogicalNetLayer

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapAdditionalPropertiesFrame.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapAdditionalPropertiesFrame.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapAdditionalPropertiesFrame.java,v 1.5 2005/06/22 08:43:49 krupenn Exp $
+ * $Id: MapAdditionalPropertiesFrame.java,v 1.6 2005/06/24 13:04:05 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -20,7 +20,7 @@ import com.syrus.AMFICOM.client.model.ApplicationContext;
 
 /**
  *  Окно отображения свойств элемента карты
- * @version $Revision: 1.5 $, $Date: 2005/06/22 08:43:49 $
+ * @version $Revision: 1.6 $, $Date: 2005/06/24 13:04:05 $
  * @author $Author: krupenn $
  * @module mapviewclient_v1
  */
@@ -38,12 +38,12 @@ public final class MapAdditionalPropertiesFrame extends MapAbstractPropertiesFra
 			if(addEditor instanceof SiteNodeAddEditor) {
 				SiteNodeAddEditor siteEditor = (SiteNodeAddEditor )addEditor;
 				MapFrame mapFrame = MapDesktopCommand.findMapFrame((JDesktopPane )this.getParent());
-				siteEditor.setLogicalNetLayer(mapFrame.getMapViewer().getLogicalNetLayer());
+				siteEditor.setNetMapViewer(mapFrame.getMapViewer());
 			}
 			else if(addEditor instanceof PhysicalLinkAddEditor) {
 				PhysicalLinkAddEditor linkEditor = (PhysicalLinkAddEditor )addEditor;
 				MapFrame mapFrame = MapDesktopCommand.findMapFrame((JDesktopPane )this.getParent());
-				linkEditor.setLogicalNetLayer(mapFrame.getMapViewer().getLogicalNetLayer());
+				linkEditor.setNetMapViewer(mapFrame.getMapViewer());
 			}
 		}
 		return addEditor;
