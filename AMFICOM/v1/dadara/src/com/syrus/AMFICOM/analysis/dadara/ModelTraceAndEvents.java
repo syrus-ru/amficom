4a9808a94c65e41a8e94e7ec6cbe87b909cf7087
Alexey Stratonnikov <alexsaa@gmail.com>
2005-06-24 16:50:57 +0400

    COSMETIC: c.o.o.c. removed; comment clarified

--- AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/ModelTraceAndEvents.java
+++ AMFICOM/v1/dadara/src/com/syrus/AMFICOM/analysis/dadara/ModelTraceAndEvents.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ModelTraceAndEvents.java,v 1.5 2005/05/24 10:00:05 saa Exp $
+ * $Id: ModelTraceAndEvents.java,v 1.6 2005/06/24 12:50:57 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -20,7 +20,7 @@ import com.syrus.AMFICOM.analysis.dadara.events.DetailedEvent;
  * 
  * @author $Author: saa $
  * @author saa
- * @version $Revision: 1.5 $, $Date: 2005/05/24 10:00:05 $
+ * @version $Revision: 1.6 $, $Date: 2005/06/24 12:50:57 $
  * @module
  */
 public interface ModelTraceAndEvents
@@ -29,8 +29,7 @@ public interface ModelTraceAndEvents
 	ModelTrace getModelTrace();
 	SimpleReflectogramEvent[] getSimpleEvents();
 	SimpleReflectogramEvent getSimpleEvent(int nEvent);
-//  ComplexReflectogramEvent[] getComplexEvents();
-	DetailedEvent[] getDetailedEvents(); // @todo: add getDetailedEvent(int),it's more efficient
+	DetailedEvent[] getDetailedEvents(); // @todo: add getDetailedEvent(int), that would be more efficient
 	int getEventByCoord(int x);
 	int getNEvents();
 }
