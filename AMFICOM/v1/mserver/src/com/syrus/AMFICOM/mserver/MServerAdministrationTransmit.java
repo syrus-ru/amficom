7bc426e5fe725c0de9dde539a09f3db7fd7280f6
Arseniy Tashoyan <tashoyan@gmail.com>
2005-06-25 19:12:18 +0400

    ADD: Methods 'transmitSystemUsers' and 'transmitDomains'

--- AMFICOM/v1/mserver/src/com/syrus/AMFICOM/mserver/MServerAdministrationTransmit.java
+++ AMFICOM/v1/mserver/src/com/syrus/AMFICOM/mserver/MServerAdministrationTransmit.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MServerAdministrationTransmit.java,v 1.8 2005/06/21 14:13:36 bass Exp $
+ * $Id: MServerAdministrationTransmit.java,v 1.9 2005/06/25 15:12:18 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -9,15 +9,17 @@ package com.syrus.AMFICOM.mserver;
 
 import org.omg.CORBA.portable.IDLEntity;
 
+import com.syrus.AMFICOM.administration.corba.IdlDomain;
 import com.syrus.AMFICOM.administration.corba.IdlMCM;
 import com.syrus.AMFICOM.administration.corba.IdlServer;
+import com.syrus.AMFICOM.administration.corba.IdlSystemUser;
 import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
 import com.syrus.AMFICOM.general.corba.IdlIdentifier;
 import com.syrus.AMFICOM.security.corba.IdlSessionKey;
 
 /**
- * @version $Revision: 1.8 $, $Date: 2005/06/21 14:13:36 $
- * @author $Author: bass $
+ * @version $Revision: 1.9 $, $Date: 2005/06/25 15:12:18 $
+ * @author $Author: arseniy $
  * @module mserver_v1
  */
 abstract class MServerAdministrationTransmit extends MServerGeneralTransmit {
@@ -25,6 +27,24 @@ abstract class MServerAdministrationTransmit extends MServerGeneralTransmit {
 	private static final long serialVersionUID = 76258492809647849L;
 
 	/* Transmit multiple objects*/
+
+	public IdlSystemUser[] transmitSystemUsers(final IdlIdentifier[] idsT, final IdlSessionKey sessionKeyT)
+			throws AMFICOMRemoteException {
+		final IDLEntity[] storableObjectsT = super.transmitStorableObjects(idsT, sessionKeyT);
+		final int length = storableObjectsT.length;
+		final IdlSystemUser[] ret = new IdlSystemUser[length];
+		System.arraycopy(storableObjectsT, 0, ret, 0, length);
+		return ret;
+	}
+
+	public IdlDomain[] transmitDomains(final IdlIdentifier[] idsT, final IdlSessionKey sessionKeyT)
+			throws AMFICOMRemoteException {
+		final IDLEntity[] storableObjectsT = super.transmitStorableObjects(idsT, sessionKeyT);
+		final int length = storableObjectsT.length;
+		final IdlDomain[] ret = new IdlDomain[length];
+		System.arraycopy(storableObjectsT, 0, ret, 0, length);
+		return ret;
+	}
 
 	public IdlServer[] transmitServers(final IdlIdentifier[] idsT, final IdlSessionKey sessionKeyT)
 			throws AMFICOMRemoteException {
