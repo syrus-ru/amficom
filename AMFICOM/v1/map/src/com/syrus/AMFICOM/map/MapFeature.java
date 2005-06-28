43ef0d1b2f32a70f63c3ddd0448faa661b2c5ec7
Maxim Selivanov <max.selivanov@gmail.com>
2005-06-28 12:09:46 +0400

    class for the method findFeature(String featureName) MscharServer

---
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/map/MapFeature.java
@@ -0,0 +1,49 @@
+/*-
+ * $Id: MapFeature.java,v 1.1 2005/06/28 08:09:46 max Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.AMFICOM.map;
+
+import com.syrus.AMFICOM.general.ErrorMessages;
+import com.syrus.AMFICOM.map.corba.IdlMapFeature;
+
+public class MapFeature {
+	
+	private double centerX;
+	private double centerY;
+	
+	private String name;
+	
+	public MapFeature (double centerX, double centerY, String name) {
+		assert name == null : ErrorMessages.NON_NULL_EXPECTED;
+		this.centerX = centerX;
+		this.centerY = centerY;
+		this.name = name;
+	}
+	
+	MapFeature (IdlMapFeature ft) {
+		this.centerX = ft.centerX;
+		this.centerY = ft.centerY;
+		this.name = ft.name;
+	}
+	
+	public IdlMapFeature getTransferable() {
+		return new IdlMapFeature(this.centerX, this.centerY, this.name);
+	}
+
+	public double getCenterX() {
+		return this.centerX;
+	}
+
+	public double getCenterY() {
+		return this.centerY;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+}
