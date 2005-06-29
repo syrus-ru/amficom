670a56017dc31e892888cacaeee6ebb9b5083ae6
Peter Peskovsky <peskovsky@gmail.com>
2005-06-29 14:57:58 +0400

    MAJOR: The class is modified to suite new MapJLocalRenderer.

--- AMFICOM/v1/mapinfo/src/com/syrus/AMFICOM/client/map/mapinfo/MapInfoLocalStubImageLoader.java
+++ AMFICOM/v1/mapinfo/src/com/syrus/AMFICOM/client/map/mapinfo/MapInfoLocalStubImageLoader.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MapInfoLocalStubImageLoader.java,v 1.1.2.1 2005/06/20 15:31:23 peskovsky Exp $
+ * $Id: MapInfoLocalStubImageLoader.java,v 1.1.2.2 2005/06/29 10:57:58 peskovsky Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -20,13 +20,13 @@ import com.syrus.AMFICOM.map.TopologicalImageQuery;
 
 /**
  * @author $Author: peskovsky $
- * @version $Revision: 1.1.2.1 $, $Date: 2005/06/20 15:31:23 $
+ * @version $Revision: 1.1.2.2 $, $Date: 2005/06/29 10:57:58 $
  * @module mapinfo_v1
  */
 public class MapInfoLocalStubImageLoader implements MapImageLoader {
 
-    private final MapJLocalRenderer renderer;
-		private final MapInfoConnection connection;
+    private MapJLocalRenderer renderer;
+		private MapInfoConnection connection;
     
     public MapInfoLocalStubImageLoader(MapInfoConnection connection)
         throws MapConnectionException
@@ -36,62 +36,62 @@ public class MapInfoLocalStubImageLoader implements MapImageLoader {
         //Setting logger to log nothing.
         System.setProperty("org.apache.commons.logging.Log","com.syrus.AMFICOM.Client.Map.EmptyLog");
 
-        try {
-        	this.renderer = new MapJLocalRenderer(this.connection.getPath() + this.connection.getView());
-        } catch (IOException e) {
-				e.printStackTrace();
-				throw new MapConnectionException(e);
-			}
-    }
-    
-    /* (non-Javadoc)
-     * @see com.syrus.AMFICOM.Client.Map.MapImageLoader#stopRenderingAtServer()
-     */
-    public void stopRendering() {
-        try {
-            this.renderer.cancelRendering();
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }        
+       	try {
+       		this.renderer = new MapJLocalRenderer(this.connection.getPath() + this.connection.getView());
+       	} catch (IOException e) {
+       		throw new MapConnectionException ("Failed initializing MapJLocalRenderer");
+       	}
     }
 
-    /* (non-Javadoc)
-     * @see com.syrus.AMFICOM.Client.Map.MapImageLoader#renderMapImageAtServer(com.syrus.AMFICOM.Client.Map.TopologicalRequest)
-     */
-    public Image renderMapImage(TopologicalImageQuery query)
-        throws MapConnectionException,MapDataException {
-        //Здесь должен формироваться запрос serverQuery к пулу
-        byte[] imageArray = null;        
-        try {
-        	imageArray = this.renderer.renderToStream(query);
-        } catch (Exception e) {
-            throw new MapDataException("Failed rendering to stream");
-        }
-        
-        if (imageArray == null)
-        	return null;
-        
-        Image imageReceived = Toolkit.getDefaultToolkit().createImage(imageArray);
-        
-        return imageReceived;
-    }
-
-		/* (non-Javadoc)
-		 * @see com.syrus.AMFICOM.client.map.MapImageLoader#getMapConnection()
-		 */
-		public MapConnection getMapConnection() throws MapConnectionException
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.Client.Map.MapImageLoader#stopRenderingAtServer()
+	 */
+	public void stopRendering()
+	{
+		try
 		{
-			// TODO Auto-generated method stub
-			return null;
+			this.renderer.cancelRendering();
+		} catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.Client.Map.MapImageLoader#renderMapImageAtServer(com.syrus.AMFICOM.Client.Map.TopologicalRequest)
+	 */
+	public Image renderMapImage(TopologicalImageQuery query)
+			throws MapConnectionException, MapDataException
+	{
+		//Здесь должен формироваться запрос serverQuery к пулу
+		Image image = null;
+		try
+		{
+			image = this.renderer.renderImage(query);
+		} catch (Exception e)
+		{
+			throw new MapDataException("Failed rendering image");
 		}
 
-		/* (non-Javadoc)
-		 * @see com.syrus.AMFICOM.client.map.MapImageLoader#findSpatialObjects(java.lang.String)
-		 */
-		public List findSpatialObjects(String searchText) throws MapConnectionException, MapDataException
-		{
-			// TODO Auto-generated method stub
-			return null;
-		}
+		return image;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.client.map.MapImageLoader#getMapConnection()
+	 */
+	public MapConnection getMapConnection() throws MapConnectionException
+	{
+		return this.connection;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.syrus.AMFICOM.client.map.MapImageLoader#findSpatialObjects(java.lang.String)
+	 */
+	public List findSpatialObjects(String searchText)
+			throws MapConnectionException, MapDataException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
