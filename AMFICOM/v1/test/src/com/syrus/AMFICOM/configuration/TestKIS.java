939a2027e85b73cf17c2bc3b1eab7a562e1ce097
Arseniy Tashoyan <tashoyan@gmail.com>
2005-07-01 17:27:29 +0400

    misc

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/configuration/TestKIS.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/configuration/TestKIS.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestKIS.java,v 1.1 2005/06/30 16:14:17 arseniy Exp $
+ * $Id: TestKIS.java,v 1.2 2005/07/01 13:27:29 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -14,6 +14,7 @@ import com.syrus.AMFICOM.administration.MCM;
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.DatabaseCommonTest;
 import com.syrus.AMFICOM.general.EquivalentCondition;
+import com.syrus.AMFICOM.general.LinkedIdsCondition;
 import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectPool;
 
@@ -32,7 +33,7 @@ public final class TestKIS extends TestCase {
 		return commonTest.createTestSetup();
 	}
 
-	public void testCreateInstance() throws ApplicationException {
+	public void _testCreateInstance() throws ApplicationException {
 		EquivalentCondition ec = new EquivalentCondition(ObjectEntities.EQUIPMENT_CODE);
 		Iterator it = StorableObjectPool.getStorableObjectsByCondition(ec, true).iterator();
 		final Equipment equipment = (Equipment) it.next();
@@ -53,6 +54,20 @@ public final class TestKIS extends TestCase {
 				(short) 7501,
 				equipment.getId(),
 				mcm.getId());
+		StorableObjectPool.flush(kis, false);
+	}
+
+	public void testUpdate() throws ApplicationException {
+		final EquivalentCondition ec = new EquivalentCondition(ObjectEntities.MCM_CODE);
+		Iterator it = StorableObjectPool.getStorableObjectsByCondition(ec, true).iterator();
+		final MCM mcm = (MCM) it.next();
+
+		final LinkedIdsCondition lic = new LinkedIdsCondition(mcm.getId(), ObjectEntities.KIS_CODE);
+		it = StorableObjectPool.getStorableObjectsByCondition(lic, true).iterator();
+		final KIS kis = (KIS) it.next();
+		System.out.println("Loaded: " + kis.getName() + ", host: " + kis.getHostName());
+
+		kis.setHostName("rtu-2");
 		StorableObjectPool.flush(kis, false);
 	}
 }
--- AMFICOM/v1/test/src/com/syrus/AMFICOM/configuration/TestMonitoredElement.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/configuration/TestMonitoredElement.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestMonitoredElement.java,v 1.6 2005/06/28 15:28:24 arseniy Exp $
+ * $Id: TestMonitoredElement.java,v 1.7 2005/07/01 13:27:29 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,7 +7,6 @@
  */
 package com.syrus.AMFICOM.configuration;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 
@@ -15,18 +14,15 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 
 import com.syrus.AMFICOM.administration.Domain;
-import com.syrus.AMFICOM.configuration.corba.IdlMonitoredElement;
 import com.syrus.AMFICOM.configuration.corba.IdlMonitoredElementPackage.MonitoredElementSort;
 import com.syrus.AMFICOM.general.ApplicationException;
-import com.syrus.AMFICOM.general.CreateObjectException;
 import com.syrus.AMFICOM.general.DatabaseCommonTest;
 import com.syrus.AMFICOM.general.EquivalentCondition;
-import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectPool;
 
 /**
- * @version $Revision: 1.6 $, $Date: 2005/06/28 15:28:24 $
+ * @version $Revision: 1.7 $, $Date: 2005/07/01 13:27:29 $
  * @author $Author: arseniy $
  * @module config_v1
  */
@@ -51,13 +47,13 @@ public class TestMonitoredElement extends TestCase {
 		it = StorableObjectPool.getStorableObjectsByCondition(ec, true).iterator();
 		final Domain domain = (Domain) it.next();
 
-		String localAddress = "SW=01:06";
+		final String localAddress = "SW=01:01";
 
 		ec.setEntityCode(ObjectEntities.TRANSPATH_CODE);
 		it = StorableObjectPool.getStorableObjectsByCondition(ec, true).iterator();
 		final TransmissionPath transmissionPath = (TransmissionPath) it.next();
 
-		MonitoredElement monitoredElement = MonitoredElement.createInstance(DatabaseCommonTest.getSysUser().getId(),
+		MonitoredElement.createInstance(DatabaseCommonTest.getSysUser().getId(),
 				domain.getId(),
 				"monitored element",
 				measurementPort.getId(),
@@ -65,36 +61,7 @@ public class TestMonitoredElement extends TestCase {
 				localAddress,
 				Collections.singleton(transmissionPath.getId()));
 
-		this.checkMonitoredElement(monitoredElement);
-
 		StorableObjectPool.flush(ObjectEntities.MONITOREDELEMENT_CODE, false);
-	}
-
-	private void checkMonitoredElement(MonitoredElement monitoredElement) {
-		IdlMonitoredElement met = monitoredElement.getTransferable();
-
-		MonitoredElement monitoredElement1 = null;
-		try {
-			monitoredElement1 = new MonitoredElement(met);
-		}
-		catch (CreateObjectException e) {
-			e.printStackTrace();
-		}
-		assertEquals(monitoredElement.getId(), monitoredElement1.getId());
-		assertEquals(monitoredElement.getCreated(), monitoredElement1.getCreated());
-		assertEquals(monitoredElement.getModified(), monitoredElement1.getModified());
-		assertEquals(monitoredElement.getCreatorId(), monitoredElement1.getCreatorId());
-		assertEquals(monitoredElement.getModifierId(), monitoredElement1.getModifierId());
-		assertEquals(monitoredElement.getVersion(), monitoredElement1.getVersion());
-		assertEquals(monitoredElement.getDomainId(), monitoredElement1.getDomainId());
-		assertEquals(monitoredElement.getName(), monitoredElement1.getName());
-		assertEquals(monitoredElement.getMeasurementPortId(), monitoredElement1.getMeasurementPortId());
-		assertEquals(monitoredElement.getSort(), monitoredElement1.getSort());
-		assertEquals(monitoredElement.getLocalAddress(), monitoredElement1.getLocalAddress());
-
-		Collection monitoredDomainMemberIds = monitoredElement.getMonitoredDomainMemberIds();
-		for (Iterator it = monitoredDomainMemberIds.iterator(); it.hasNext();)
-			System.out.println("monitored entity: " + ((Identifier) it.next()).toString());
 	}
 
 
