f74d81fd8ab78f73d15de5603bac3013ee6a6b51
Peter Peskovsky <peskovsky@gmail.com>
2005-07-04 18:36:32 +0400

    MAJOR: The image is processed through Raster.elementContents() to the client side. Searching map text' option is added.

--- AMFICOM/v1/mapinfo/src/com/syrus/AMFICOM/client/map/mapinfo/MapInfoCorbaImageLoader.java
+++ AMFICOM/v1/mapinfo/src/com/syrus/AMFICOM/client/map/mapinfo/MapInfoCorbaImageLoader.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: MapInfoCorbaImageLoader.java,v 1.1.2.4 2005/06/28 11:23:09 peskovsky Exp $
+ * $Id: MapInfoCorbaImageLoader.java,v 1.1.2.5 2005/07/04 14:36:32 peskovsky Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,7 +8,8 @@
 package com.syrus.AMFICOM.client.map.mapinfo;
 
 import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.syrus.AMFICOM.client.map.MapConnection;
@@ -17,15 +18,18 @@ import com.syrus.AMFICOM.client.map.MapDataException;
 import com.syrus.AMFICOM.client.map.MapImageLoader;
 import com.syrus.AMFICOM.general.LoginManager;
 import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
+import com.syrus.AMFICOM.map.DoublePoint;
 import com.syrus.AMFICOM.map.TopologicalImageQuery;
+import com.syrus.AMFICOM.map.corba.IdlMapFeature;
 import com.syrus.AMFICOM.map.corba.IdlRenderedImage;
 import com.syrus.AMFICOM.map.corba.IdlTopologicalImageQuery;
 import com.syrus.AMFICOM.mscharserver.corba.MscharServer;
 import com.syrus.AMFICOM.security.corba.IdlSessionKey;
+import com.syrus.io.ImageToByte;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.1.2.4 $, $Date: 2005/06/28 11:23:09 $
+ * @version $Revision: 1.1.2.5 $, $Date: 2005/07/04 14:36:32 $
  * @author $Author: peskovsky $
  * @module map_v1
  */
@@ -79,7 +83,7 @@ public class MapInfoCorbaImageLoader implements MapImageLoader
 			return null;
 		}
 
-		Image imageReceived = Toolkit.getDefaultToolkit().createImage(image);
+		BufferedImage bufferedImage = ImageToByte.byteToImqge(image);
 		t4 = System.currentTimeMillis();
 		Log.debugMessage("MapInfoCorbaImageLoader.renderMapImage | "
 				+ (t1 - t0) + " (creating transf and session key)"
@@ -88,7 +92,7 @@ public class MapInfoCorbaImageLoader implements MapImageLoader
 				+ (t4 - t3) + "creating image",
 				Log.FINEST);
 
-		return imageReceived;
+		return bufferedImage;
 	}
 
 	/* (non-Javadoc)
@@ -104,7 +108,38 @@ public class MapInfoCorbaImageLoader implements MapImageLoader
 	 */
 	public List findSpatialObjects(String searchText) throws MapConnectionException, MapDataException
 	{
-		// TODO Auto-generated method stub
-		return null;
+		List<MapInfoSpatialObject> resultList = new ArrayList<MapInfoSpatialObject>();		
+		
+		IdlSessionKey keyt = LoginManager.getSessionKeyTransferable();
+		IdlMapFeature[] objectsFound ;
+		
+		long t1 = 0,t2 = 0;
+		
+		try {
+			MscharServer mscharServer= this.connection.getMscharServer();
+			t1 = System.currentTimeMillis();
+			objectsFound = mscharServer.findFeature(searchText,keyt);
+			t2 = System.currentTimeMillis();
+		} catch (AMFICOMRemoteException e) {
+			Log.errorMessage("TopologicalImageLoader.loadTopologicalImage |" + e.getMessage());
+			return null;
+		}
+		Log.debugMessage("MapInfoCorbaImageLoader.findSpatialObjects | "
+				+ "searched for " + (t2 - t1) + " ms.",
+				Log.FINEST);
+
+		if (	(objectsFound.length == 1)
+				&&(objectsFound[0].name.equals("")))
+			return resultList;
+		
+		for (IdlMapFeature mapFeature : objectsFound)
+		{
+			MapInfoSpatialObject spatialObject = new MapInfoSpatialObject(
+					new DoublePoint(mapFeature.centerX,mapFeature.centerY),
+					mapFeature.name);
+			resultList.add(spatialObject);
+		}
+		
+		return resultList;
 	}
 }
