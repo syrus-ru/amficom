632254aa0f4a7ef673cd5dfc55e8fce0dc52107a
Alexey Stratonnikov <alexsaa@gmail.com>
2005-07-05 19:34:45 +0400

    FIX: mismatch distance: units were inconsistent with precision. Now set to km

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/analysis/OverallStats.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/analysis/OverallStats.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: OverallStats.java,v 1.4 2005/07/04 14:15:45 saa Exp $
+ * $Id: OverallStats.java,v 1.5 2005/07/05 15:34:45 saa Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -22,7 +22,7 @@ import com.syrus.io.BellcoreStructure;
 
 /**
  * @author $Author: saa $
- * @version $Revision: 1.4 $, $Date: 2005/07/04 14:15:45 $
+ * @version $Revision: 1.5 $, $Date: 2005/07/05 15:34:45 $
  * @module analysis_v1
  */
 
@@ -78,7 +78,7 @@ public class OverallStats {
 		if (alarm == null) {
 			setMismatch(LangModelAnalyse.getString(("mismatchNo")));
 		} else {
-			setMismatch(String.valueOf(MathRef.round_3(alarm.getDistance()))
+			setMismatch(String.valueOf(MathRef.round_3(alarm.getDistance() / 1000))
 					+ " "
 					+ LangModelAnalyse.getString(("mismatchDistance")));
 		}
