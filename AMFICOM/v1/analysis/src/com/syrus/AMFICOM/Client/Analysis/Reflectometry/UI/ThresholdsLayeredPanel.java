1c38289c562867f14192c42c6aaeb53b1324ae78
Alexey Stratonnikov <alexsaa@gmail.com>
2005-07-08 15:27:48 +0400

    REFACTOR: class ThresholdsToolBar move to separate file

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/Analysis/Reflectometry/UI/ThresholdsLayeredPanel.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/Analysis/Reflectometry/UI/ThresholdsLayeredPanel.java
@@ -1,23 +1,13 @@
 package com.syrus.AMFICOM.Client.Analysis.Reflectometry.UI;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.Map;
-
-import javax.swing.JButton;
-import javax.swing.JToggleButton;
-import javax.swing.UIManager;
 
 import com.syrus.AMFICOM.Client.Analysis.Heap;
 import com.syrus.AMFICOM.Client.General.Event.CurrentEventChangeListener;
 import com.syrus.AMFICOM.Client.General.Event.EtalonMTMListener;
 import com.syrus.AMFICOM.Client.General.Event.RefUpdateEvent;
-import com.syrus.AMFICOM.Client.General.Lang.LangModelAnalyse;
-import com.syrus.AMFICOM.Client.General.Model.AnalysisResourceKeys;
 import com.syrus.AMFICOM.client.event.Dispatcher;
-import com.syrus.AMFICOM.client.resource.ResourceKeys;
 
 public class ThresholdsLayeredPanel extends TraceEventsLayeredPanel
 implements PropertyChangeListener,
@@ -160,84 +150,5 @@ implements PropertyChangeListener,
 	public void etalonMTMRemoved()
 	{
 		etalonUpdated();
-	}
-}
-
-class ThresholdsToolBar extends TraceEventsToolBar
-{
-	protected static final String FIT_EVENT = "fit2event";
-	protected static final String ALL_TRESHOLDS = "allTresholds";
-
-	private JButton fitEvButton = new JButton();
-	protected JToggleButton showThresholdButton = new JToggleButton();
-
-	protected static String[] buttons = new String[]
-	{
-		FIT_EVENT, ALL_TRESHOLDS, SEPARATOR, EX, DX, EY, DY, FIX, SEPARATOR, modeled
-	};
-
-	public ThresholdsToolBar (ThresholdsLayeredPanel panel)
-	{
-		super(panel);
-	}
-
-	protected String[] getButtons()
-	{
-		return buttons;
-	}
-
-	protected Map createGraphButtons()
-	{
-		Map buttons = super.createGraphButtons();
-
-		buttons.put(
-				FIT_EVENT,
-				createToolButton(
-				fitEvButton,
-				null,
-				UIManager.getInsets(ResourceKeys.INSETS_ICONED_BUTTON),
-				null,
-				LangModelAnalyse.getString("fittoevent"),
-				UIManager.getIcon(AnalysisResourceKeys.ICON_ANALYSIS_ZOOM_BOX),
-				new ActionListener()
-				{
-					public void actionPerformed(ActionEvent e)
-					{
-						fitEvButton_actionPerformed(e);
-					}
-				},
-				true));
-		buttons.put(
-				ALL_TRESHOLDS,
-				createToolButton(
-				showThresholdButton,
-				null,
-				UIManager.getInsets(ResourceKeys.INSETS_ICONED_BUTTON),
-				null,
-				LangModelAnalyse.getString("allThresholds"),
-				UIManager.getIcon(AnalysisResourceKeys.ICON_ANALYSIS_THRESHOLD),
-				new ActionListener()
-				{
-					public void actionPerformed(ActionEvent e)
-					{
-						showThresholdButton_actionPerformed(e);
-					}
-				},
-				true));
-		
-		if (!modeledTButton.isSelected())
-			modeledTButton.doClick();
-
-		return buttons;
-	}
-
-	void fitEvButton_actionPerformed(ActionEvent e)
-	{
-		((ThresholdsLayeredPanel)panel).updScale2fitCurrentEv (.2, 1.);
-	}
-
-	void showThresholdButton_actionPerformed(ActionEvent e)
-	{
-		panel.jLayeredPane.repaint();
 	}
 }
