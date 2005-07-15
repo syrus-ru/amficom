ad91423b2b405bff423bdd167539b5b67a023264
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-07-15 13:49:24 +0400

    MINOR:

--- AMFICOM/v1/scheme/test/com/syrus/AMFICOM/scheme/SchemePathTestCase.java
+++ AMFICOM/v1/scheme/test/com/syrus/AMFICOM/scheme/SchemePathTestCase.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: SchemePathTestCase.java,v 1.1 2005/07/15 08:47:17 bass Exp $
+ * $Id: SchemePathTestCase.java,v 1.2 2005/07/15 09:49:24 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -17,21 +17,23 @@ import com.syrus.AMFICOM.configuration.ConfigurationStorableObjectPool;
 import com.syrus.AMFICOM.configuration.EmptyConfigurationObjectLoader;
 import com.syrus.AMFICOM.configuration.LinkType;
 import com.syrus.AMFICOM.configuration.corba.IdlAbstractLinkTypePackage.LinkTypeSort;
+import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.CommunicationException;
-import com.syrus.AMFICOM.general.CreateObjectException;
 import com.syrus.AMFICOM.general.IGSConnectionManager;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.IdentifierPool;
 import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.StorableObjectPool;
 import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
 import com.syrus.AMFICOM.general.corba.IdentifierGeneratorServer;
 import com.syrus.AMFICOM.general.corba.IdlIdentifier;
+import com.syrus.AMFICOM.scheme.corba.IdlAbstractSchemePortPackage.DirectionType;
 import com.syrus.AMFICOM.scheme.corba.IdlSchemePackage.Kind;
 
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.1 $, $Date: 2005/07/15 08:47:17 $
+ * @version $Revision: 1.2 $, $Date: 2005/07/15 09:49:24 $
  * @module scheme_v1
  */
 public class SchemePathTestCase extends TestCase {
@@ -39,7 +41,7 @@ public class SchemePathTestCase extends TestCase {
 		TestRunner.run(SchemePathTestCase.class);
 	}
 
-	public void testSchemePath() throws CreateObjectException {
+	public void testSchemePath() throws ApplicationException {
 		ConfigurationStorableObjectPool.init(new EmptyConfigurationObjectLoader());
 		SchemeStorableObjectPool.init(new EmptySchemeObjectLoader());
 		final IdentifierGeneratorServer identifierGeneratorServer = new IdentifierGeneratorServer() {
@@ -82,12 +84,31 @@ public class SchemePathTestCase extends TestCase {
 		final SchemeLink schemeLink = SchemeLink.createInstance(domainId, "a scheme link", scheme);
 		final SchemeCableLink schemeCableLink = SchemeCableLink.createInstance(domainId, "a scheme cable link", scheme); 
 		final SchemeCableThread schemeCableThread = SchemeCableThread.createInstance(domainId, "a scheme cable thread", cableThreadType, schemeCableLink);
+		final SchemeElement schemeElement = SchemeElement.createInstance(userId, "a scheme element", scheme);
+		final SchemeDevice schemeDevice = SchemeDevice.createInstance(userId, "a scheme device", schemeElement);
+		final SchemePort startSchemePort = SchemePort.createInstance(userId, "starting schem port", DirectionType._IN, schemeDevice);
+		final SchemePort endSchemePort = SchemePort.createInstance(userId, "ending schem port", DirectionType._OUT, schemeDevice);
 
-		PathElement.createInstance(domainId, schemePath, schemeLink);
-		PathElement.createInstance(userId, schemePath, schemeCableThread);		
+		final PathElement pathElement1 = PathElement.createInstance(domainId, schemePath, schemeLink);
+		final PathElement pathElement2 = PathElement.createInstance(userId, schemePath, schemeCableThread);
+		final PathElement pathElement3 = PathElement.createInstance(userId, schemePath, startSchemePort, endSchemePort);
+
+		final Identifier pathElementId1 = pathElement1.getId();
+		final Identifier pathElementId2 = pathElement2.getId();
+		final Identifier pathElementId3 = pathElement3.getId();
 
 		for (final PathElement pathElement : schemePath.getPathElements()) {
-			System.out.println("" + pathElement.getSequentialNumber() + '\t' + pathElement.getParentSchemePath().getName() + '\t' + pathElement.getAbstractSchemeElement().getName());
+			System.out.println("Sequential number: " + pathElement.getSequentialNumber() + ";\t parent scheme path: " + pathElement.getParentSchemePath().getName() + ";\t abstract scheme element: " + pathElement.getAbstractSchemeElement().getName() + ";\t kind: " + pathElement.getKind().value());
 		}
+
+		schemePath.removePathElement(pathElement2);
+
+		for (final PathElement pathElement : schemePath.getPathElements()) {
+			System.out.println("Sequential number: " + pathElement.getSequentialNumber() + ";\t parent scheme path: " + pathElement.getParentSchemePath().getName() + ";\t abstract scheme element: " + pathElement.getAbstractSchemeElement().getName() + ";\t kind: " + pathElement.getKind().value());
+		}
+
+		System.out.println(StorableObjectPool.getStorableObject(pathElementId1, true));
+		System.out.println(StorableObjectPool.getStorableObject(pathElementId2, true));
+		System.out.println(StorableObjectPool.getStorableObject(pathElementId3, true));
 	}
 }
