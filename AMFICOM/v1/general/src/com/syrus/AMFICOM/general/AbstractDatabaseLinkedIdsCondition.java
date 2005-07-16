4721b59fcafad6d8dc004aa2de0102648ab392d7
Arseniy Tashoyan <tashoyan@gmail.com>
2005-07-16 22:51:58 +0400

    MINOR: New functions to process illegal entity code and illegal linked entity code exceptions

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/AbstractDatabaseLinkedIdsCondition.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/AbstractDatabaseLinkedIdsCondition.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AbstractDatabaseLinkedIdsCondition.java,v 1.20 2005/07/12 14:32:44 arseniy Exp $
+ * $Id: AbstractDatabaseLinkedIdsCondition.java,v 1.21 2005/07/16 18:51:58 arseniy Exp $
  *
  * Copyright ¿ 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -10,7 +10,7 @@ package com.syrus.AMFICOM.general;
 
 
 /**
- * @version $Revision: 1.20 $, $Date: 2005/07/12 14:32:44 $
+ * @version $Revision: 1.21 $, $Date: 2005/07/16 18:51:58 $
  * @author $Author: arseniy $
  * @module general_v1
  */
@@ -61,12 +61,23 @@ public abstract class AbstractDatabaseLinkedIdsCondition implements DatabaseStor
 		return query.toString();
 	}
 
-	protected IllegalObjectEntityException newIllegalObjectEntityException() {
+	protected IllegalObjectEntityException newExceptionEntityIllegal() {
 		final short entityCode = this.getEntityCode().shortValue();
 		final short linkedEntityCode = this.condition.getLinkedEntityCode();
-		return new IllegalObjectEntityException("Unsupported entity -- '" + ObjectEntities.codeToString(entityCode) + "'/" + entityCode
-				+ ", linked entity: '" + ObjectEntities.codeToString(linkedEntityCode) + "'/" + linkedEntityCode,
+		return new IllegalObjectEntityException("Unsupported entity '"
+				+ ObjectEntities.codeToString(entityCode) + "'/" + entityCode
+				+ "; linked entity '"+ ObjectEntities.codeToString(linkedEntityCode) + "'/" + linkedEntityCode,
 				IllegalObjectEntityException.ENTITY_NOT_REGISTERED_CODE);
 	}
+
+	protected IllegalObjectEntityException newExceptionLinkedEntityIllegal() {
+		final short entityCode = this.getEntityCode().shortValue();
+		final short linkedEntityCode = this.condition.getLinkedEntityCode();
+		return new IllegalObjectEntityException("Unsupported linked entity '"
+				+ ObjectEntities.codeToString(linkedEntityCode) + "'/" + linkedEntityCode
+				+ " for entity '" + ObjectEntities.codeToString(entityCode) + "'/" + entityCode,
+				IllegalObjectEntityException.ENTITY_NOT_REGISTERED_CODE);
+	}
+	
 
 }
