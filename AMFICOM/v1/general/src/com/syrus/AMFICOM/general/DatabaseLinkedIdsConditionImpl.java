9a0997031afcb39896d1e966de313b343381dd96
Arseniy Tashoyan <tashoyan@gmail.com>
2005-07-16 22:52:02 +0400

    COSMETIC

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/DatabaseLinkedIdsConditionImpl.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/DatabaseLinkedIdsConditionImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DatabaseLinkedIdsConditionImpl.java,v 1.11 2005/06/27 10:17:14 arseniy Exp $
+ * $Id: DatabaseLinkedIdsConditionImpl.java,v 1.12 2005/07/16 18:52:02 arseniy Exp $
  *
  * Copyright ¿ 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,10 +8,13 @@
 
 package com.syrus.AMFICOM.general;
 
+import static com.syrus.AMFICOM.general.ObjectEntities.CHARACTERISTIC_CODE;
+import static com.syrus.AMFICOM.general.ObjectEntities.PARAMETER_TYPE_CODE;
+
 /**
- * @version $Revision: 1.11 $, $Date: 2005/06/27 10:17:14 $
+ * @version $Revision: 1.12 $, $Date: 2005/07/16 18:52:02 $
  * @author $Author: arseniy $
- * @module general_v1
+ * @module general
  */
 final class DatabaseLinkedIdsConditionImpl extends AbstractDatabaseLinkedIdsCondition {
 
@@ -22,16 +25,17 @@ final class DatabaseLinkedIdsConditionImpl extends AbstractDatabaseLinkedIdsCond
 
 	public String getSQLQuery() throws IllegalObjectEntityException {
 		switch (super.condition.getEntityCode().shortValue()) {
-			case ObjectEntities.CHARACTERISTIC_CODE:
+			case CHARACTERISTIC_CODE:
 				switch (super.condition.getLinkedEntityCode()) {
-					case ObjectEntities.CHARACTERISTIC_TYPE_CODE:
-						return super.getQuery(StorableObjectWrapper.COLUMN_TYPE_ID);
-					default:
+					case PARAMETER_TYPE_CODE:
 						return super.getQuery(CharacteristicWrapper.COLUMN_CHARACTERIZABLE_ID);
+//					case ObjectEntities.CHARACTERISTIC_TYPE_CODE:
+//						return super.getQuery(StorableObjectWrapper.COLUMN_TYPE_ID);
+					default:
+						throw super.newExceptionLinkedEntityIllegal();
 				}
 			default:
-				throw new IllegalObjectEntityException("Unsupported entity type -- "
-						+ super.condition.getEntityCode(), IllegalObjectEntityException.ENTITY_NOT_REGISTERED_CODE);
+				throw super.newExceptionEntityIllegal();
 		}
 	}
 
