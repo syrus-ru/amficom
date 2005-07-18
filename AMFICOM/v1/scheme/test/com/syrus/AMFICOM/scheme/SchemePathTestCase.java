5c6e6e8127e5fd0e4a3d46f7004217b6ed1f50b2
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-07-18 20:58:43 +0400

    MINOR:

--- AMFICOM/v1/scheme/test/com/syrus/AMFICOM/scheme/SchemePathTestCase.java
+++ AMFICOM/v1/scheme/test/com/syrus/AMFICOM/scheme/SchemePathTestCase.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: SchemePathTestCase.java,v 1.2 2005/07/15 09:49:24 bass Exp $
+ * $Id: SchemePathTestCase.java,v 1.3 2005/07/18 16:58:43 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -7,6 +7,9 @@
  */
 
 package com.syrus.AMFICOM.scheme;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import junit.awtui.TestRunner;
 import junit.framework.TestCase;
@@ -19,6 +22,7 @@ import com.syrus.AMFICOM.configuration.LinkType;
 import com.syrus.AMFICOM.configuration.corba.IdlAbstractLinkTypePackage.LinkTypeSort;
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.CommunicationException;
+import com.syrus.AMFICOM.general.CreateObjectException;
 import com.syrus.AMFICOM.general.IGSConnectionManager;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.IdentifierPool;
@@ -33,15 +37,16 @@ import com.syrus.AMFICOM.scheme.corba.IdlSchemePackage.Kind;
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.2 $, $Date: 2005/07/15 09:49:24 $
+ * @version $Revision: 1.3 $, $Date: 2005/07/18 16:58:43 $
  * @module scheme_v1
  */
-public class SchemePathTestCase extends TestCase {
+public final class SchemePathTestCase extends TestCase {
 	public static void main(String[] args) {
 		TestRunner.run(SchemePathTestCase.class);
 	}
 
-	public void testSchemePath() throws ApplicationException {
+	@Override
+	protected void setUp() {
 		ConfigurationStorableObjectPool.init(new EmptyConfigurationObjectLoader());
 		SchemeStorableObjectPool.init(new EmptySchemeObjectLoader());
 		final IdentifierGeneratorServer identifierGeneratorServer = new IdentifierGeneratorServer() {
@@ -66,6 +71,18 @@ public class SchemePathTestCase extends TestCase {
 			}
 			
 		});
+	}
+
+	public void testAssertionStatus() {
+		try {
+			assert false;
+			fail();
+		} catch (final AssertionError ae) {
+			assertTrue(true);
+		}
+	}
+
+	public void testSchemePath() throws ApplicationException {
 		final Identifier userId = new Identifier("User_0");
 		final Identifier domainId = new Identifier("Domain_0");
 		final Identifier imageId = new Identifier("ImageResource_0");
@@ -86,8 +103,8 @@ public class SchemePathTestCase extends TestCase {
 		final SchemeCableThread schemeCableThread = SchemeCableThread.createInstance(domainId, "a scheme cable thread", cableThreadType, schemeCableLink);
 		final SchemeElement schemeElement = SchemeElement.createInstance(userId, "a scheme element", scheme);
 		final SchemeDevice schemeDevice = SchemeDevice.createInstance(userId, "a scheme device", schemeElement);
-		final SchemePort startSchemePort = SchemePort.createInstance(userId, "starting schem port", DirectionType._IN, schemeDevice);
-		final SchemePort endSchemePort = SchemePort.createInstance(userId, "ending schem port", DirectionType._OUT, schemeDevice);
+		final SchemePort startSchemePort = SchemePort.createInstance(userId, "starting scheme port", DirectionType._IN, schemeDevice);
+		final SchemePort endSchemePort = SchemePort.createInstance(userId, "ending scheme port", DirectionType._OUT, schemeDevice);
 
 		final PathElement pathElement1 = PathElement.createInstance(domainId, schemePath, schemeLink);
 		final PathElement pathElement2 = PathElement.createInstance(userId, schemePath, schemeCableThread);
@@ -110,5 +127,55 @@ public class SchemePathTestCase extends TestCase {
 		System.out.println(StorableObjectPool.getStorableObject(pathElementId1, true));
 		System.out.println(StorableObjectPool.getStorableObject(pathElementId2, true));
 		System.out.println(StorableObjectPool.getStorableObject(pathElementId3, true));
+	}
+
+	/**
+	 * @see Scheme#setSchemePaths(java.util.Set)
+	 */
+	public void testSetSchemePaths() throws CreateObjectException {
+		final Identifier userId = new Identifier("User_0");
+		final Identifier domainId = new Identifier("Domain_0");
+
+		final Scheme scheme0 = Scheme.createInstance(userId, "scheme0", Kind.BAY, domainId);
+		final Scheme scheme1 = Scheme.createInstance(userId, "scheme1", Kind.BAY, domainId);
+
+		@SuppressWarnings("unused")
+		final SchemePath schemePath0 = SchemePath.createInstance(userId, "0", scheme0);
+		@SuppressWarnings("unused")
+		final SchemePath schemePath1 = SchemePath.createInstance(userId, "1", scheme0);
+		@SuppressWarnings("unused")
+		final SchemePath schemePath2 = SchemePath.createInstance(userId, "2", scheme0);
+		final SchemePath schemePath3 = SchemePath.createInstance(userId, "3", scheme0);
+		final SchemePath schemePath4 = SchemePath.createInstance(userId, "4", scheme0);
+		final SchemePath schemePath5 = SchemePath.createInstance(userId, "5", scheme0);
+		final SchemePath schemePath6 = SchemePath.createInstance(userId, "6", scheme0);
+		final SchemePath schemePath7 = SchemePath.createInstance(userId, "7", scheme1);
+		final SchemePath schemePath8 = SchemePath.createInstance(userId, "8", scheme1);
+		final SchemePath schemePath9 = SchemePath.createInstance(userId, "9", scheme1);
+
+		for (final SchemePath schemePath : scheme0.getSchemePaths()) {
+			System.out.println(scheme0.getName() + ": " + schemePath.getName());
+		}
+		for (final SchemePath schemePath : scheme1.getSchemePaths()) {
+			System.out.println(scheme1.getName() + ": " + schemePath.getName());
+		}
+		
+		final Set<SchemePath> schemePaths = new HashSet<SchemePath>();
+		schemePaths.add(schemePath3);
+		schemePaths.add(schemePath4);
+		schemePaths.add(schemePath5);
+		schemePaths.add(schemePath6);
+		schemePaths.add(schemePath7);
+		schemePaths.add(schemePath8);
+		schemePaths.add(schemePath9);
+		
+		scheme0.setSchemePaths(schemePaths);
+
+		for (final SchemePath schemePath : scheme0.getSchemePaths()) {
+			System.out.println(scheme0.getName() + ": " + schemePath.getName());
+		}
+		for (final SchemePath schemePath : scheme1.getSchemePaths()) {
+			System.out.println(scheme1.getName() + ": " + schemePath.getName());
+		}
 	}
 }
