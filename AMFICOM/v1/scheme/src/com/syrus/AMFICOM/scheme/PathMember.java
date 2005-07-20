0c5fde9501f9e28b0d42cc3f8d0f0cb5819a1f1d
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-07-20 19:15:15 +0400

    COSMETIC:

--- AMFICOM/v1/scheme/src/com/syrus/AMFICOM/scheme/PathMember.java
+++ AMFICOM/v1/scheme/src/com/syrus/AMFICOM/scheme/PathMember.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: PathMember.java,v 1.2 2005/07/20 15:10:50 bass Exp $
+ * $Id: PathMember.java,v 1.3 2005/07/20 15:15:15 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -11,10 +11,10 @@ package com.syrus.AMFICOM.scheme;
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.2 $, $Date: 2005/07/20 15:10:50 $
+ * @version $Revision: 1.3 $, $Date: 2005/07/20 15:15:15 $
  * @module scheme_v1
  */
-public interface PathMember<T extends PathOwner<U>, U extends PathMember> {
+public interface PathMember<T extends PathOwner<U>, U extends PathMember<T, U>> {
 	void insertSelfBefore(final U sibling);
 
 	void insertSelfAfter(final U sibling);
--- AMFICOM/v1/scheme/src/com/syrus/AMFICOM/scheme/PathOwner.java
+++ AMFICOM/v1/scheme/src/com/syrus/AMFICOM/scheme/PathOwner.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: PathOwner.java,v 1.2 2005/07/20 15:10:50 bass Exp $
+ * $Id: PathOwner.java,v 1.3 2005/07/20 15:15:15 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -13,10 +13,10 @@ import java.util.SortedSet;
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.2 $, $Date: 2005/07/20 15:10:50 $
+ * @version $Revision: 1.3 $, $Date: 2005/07/20 15:15:15 $
  * @module scheme_v1
  */
-public interface PathOwner<U extends PathMember> {
+public interface PathOwner<U extends PathMember<? extends PathOwner<U>, U>> {
 	void addPathMember(final U child, final boolean processSubsequentSiblings);
 
 	void removePathMember(final U child, final boolean processSubsequentSiblings);
