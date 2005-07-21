75290378f557da3c36258d765906b3d227186afd
Alexey Stratonnikov <alexsaa@gmail.com>
2005-07-21 18:06:46 +0400

    DEBUG: cosmetic (debug code moved a little); JAVA1.5

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Command/Analysis/CheckMismatchCommand.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/General/Command/Analysis/CheckMismatchCommand.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: CheckMismatchCommand.java,v 1.3 2005/07/12 08:07:25 saa Exp $
+ * $Id: CheckMismatchCommand.java,v 1.4 2005/07/21 14:06:46 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,7 +8,6 @@
 
 package com.syrus.AMFICOM.Client.General.Command.Analysis;
 
-import java.util.Iterator;
 import java.util.List;
 
 import com.syrus.AMFICOM.Client.Analysis.Heap;
@@ -17,28 +16,28 @@ import com.syrus.AMFICOM.analysis.dadara.ReflectogramMismatch;
 import com.syrus.AMFICOM.client.model.AbstractCommand;
 
 public class CheckMismatchCommand extends AbstractCommand {
+	@Override
 	public void execute() {
 	    if (Heap.getBSPrimaryTrace() != null
 	    		&& Heap.getMinuitAnalysisParams() != null
 	    		&& Heap.hasEtalon()) {
-	    	List alarms = CoreAnalysisManager.compareAndMakeAlarms(
+	    	List<ReflectogramMismatch> alarms =
+	    		CoreAnalysisManager.compareAndMakeAlarms(
 	    			Heap.getRefAnalysisPrimary().getAR(),
 	    			Heap.getEtalon());
+	    	if (alarms.size() > 0) {
+	    		ReflectogramMismatch first = alarms.iterator().next();
+	    		Heap.setRefMismatch(first);
+	    	} else {
+	    		Heap.setRefMismatch(null);
+	    	}
 	    	// FIXME: debug-time console alarm listing
 	    	if (alarms.size() == 0)
 	    		System.out.println("No alarms");
 	    	else
 	    		System.out.println(alarms.size() + " alarms:");
-	    	for (Iterator it = alarms.iterator(); it.hasNext(); ) {
-	    		ReflectogramMismatch al = (ReflectogramMismatch)it.next();
+	    	for (ReflectogramMismatch al: alarms) {
 	    		System.out.println("- " + al);
-	    	}
-	    	if (alarms.size() > 0) {
-	    		ReflectogramMismatch first = (ReflectogramMismatch)
-	    				alarms.iterator().next();
-	    		Heap.setRefMismatch(first);
-	    	} else {
-	    		Heap.setRefMismatch(null);
 	    	}
 	    }
 	}
