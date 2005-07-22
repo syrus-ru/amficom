9c4ea2546c1f3191062c2650f126830dae2dbec8
Alexey Stratonnikov <alexsaa@gmail.com>
2005-07-22 12:35:06 +0400

    COSMETIC: comment, XXX added

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/analysis/SimpleApplicationException.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/analysis/SimpleApplicationException.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: SimpleApplicationException.java,v 1.1 2005/07/20 11:09:36 saa Exp $
+ * $Id: SimpleApplicationException.java,v 1.2 2005/07/22 08:35:06 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -15,18 +15,20 @@ import com.syrus.AMFICOM.Client.General.Lang.LangModelAnalyse;
  * ѕредназначено дл€ тривиального вывода в GUI фиксированного локализованного
  * текста.
  * ¬ качестве сообщени€ используетс€ ключ дл€ локализации.
- * ћетод getLocalizedMessage() возвращает локализованную строку по этому ключу. 
+ * ћетод getLocalizedMessage() возвращает локализованную строку по этому ключу.
  * @author $Author: saa $
  * @author saa (все шишки за неправильный подход к локализации адресовать сюда)
- * @version $Revision: 1.1 $, $Date: 2005/07/20 11:09:36 $
+ * @version $Revision: 1.2 $, $Date: 2005/07/22 08:35:06 $
  * @module
  */
 public class SimpleApplicationException extends Exception {
+	// XXX: строка KEY_NULL_REFLECTOGRAMMA дублирует по смыслу строку GUIUtil.MSG_ERROR_NO_ONE_RESULT_HAS_TRACE
 	public static final String KEY_NULL_REFLECTOGRAMMA = "errorNoReflectogramma";
 	public SimpleApplicationException(String key) {
 		super(key);
 	}
 
+	// XXX: пока не используетс€
 	@Override
 	public String getLocalizedMessage() {
 		return LangModelAnalyse.getString(getMessage());
