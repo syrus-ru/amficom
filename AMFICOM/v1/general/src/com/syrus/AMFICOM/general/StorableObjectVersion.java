65e2578d111c47102044df30641e5cc4dd23e9c9
Arseniy Tashoyan <tashoyan@gmail.com>
2005-07-26 00:25:55 +0400

    ADD: Implementation of version control for storable objects

---
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectVersion.java
@@ -0,0 +1,73 @@
+/*-
+ * $Id: StorableObjectVersion.java,v 1.1 2005/07/25 20:25:55 arseniy Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.AMFICOM.general;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/07/25 20:25:55 $
+ * @author $Author: arseniy $
+ * @module general
+ */
+public final class StorableObjectVersion implements Cloneable {
+	public static final StorableObjectVersion ILLEGAL_VERSION = new StorableObjectVersion(-1L);
+
+	private long version;
+
+	public StorableObjectVersion(final long version) {
+		this.version = version;
+	}
+
+	public boolean isNewer(final StorableObjectVersion storableObjectVersion) {
+		if (Math.abs(this.version - storableObjectVersion.version) < (Long.MAX_VALUE >> 1 - Long.MIN_VALUE >> 1))
+			return (this.version > storableObjectVersion.version);
+		return (this.version < storableObjectVersion.version);
+	}
+
+	public final boolean isOlder(final StorableObjectVersion storableObjectVersion) {
+		if (Math.abs(this.version - storableObjectVersion.version) < (Long.MAX_VALUE >> 1 - Long.MIN_VALUE >> 1))
+			return (this.version < storableObjectVersion.version);
+		return (this.version > storableObjectVersion.version);
+	}
+
+	public void increment() {
+		if (this.version < Long.MAX_VALUE) {
+			this.version++;
+			if (this.equals(ILLEGAL_VERSION))
+				this.version++;
+		}
+		else {
+			this.version = Long.MIN_VALUE;
+		}
+	}
+
+	public long longValue() {
+		return this.version;
+	}
+
+	@Override
+	public boolean equals(final Object object) {
+		if (super.equals(object)) {
+			return true;
+		}
+
+		if (object instanceof StorableObjectVersion) {
+			final StorableObjectVersion storableObjectVersion = (StorableObjectVersion) object;
+			return this.version == storableObjectVersion.version;
+		}
+		return false;
+	}
+
+	@Override
+	public StorableObjectVersion clone() {
+		return new StorableObjectVersion(this.version);
+	}
+
+	public static StorableObjectVersion createInitial() {
+		return new StorableObjectVersion(0L);
+	}
+
+}
