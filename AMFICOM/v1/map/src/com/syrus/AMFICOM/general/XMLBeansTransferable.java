90d12e3a9415eaa174cbbbade44f6c274e0dd753
Arseniy Tashoyan <tashoyan@gmail.com>
2005-07-26 15:47:02 +0400

    COSMETIC

--- AMFICOM/v1/map/src/com/syrus/AMFICOM/general/XMLBeansTransferable.java
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/general/XMLBeansTransferable.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: XMLBeansTransferable.java,v 1.4 2005/07/26 11:46:32 arseniy Exp $
+ * $Id: XMLBeansTransferable.java,v 1.5 2005/07/26 11:47:02 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -10,13 +10,13 @@ package com.syrus.AMFICOM.general;
 import org.apache.xmlbeans.XmlObject;
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/07/26 11:46:32 $
+ * @version $Revision: 1.5 $, $Date: 2005/07/26 11:47:02 $
  * @author $Author: arseniy $
  * @module map
  */
 public interface XMLBeansTransferable {
 
-	void fromXMLTransferable(XmlObject xmlObject, ClonedIdsPool clonedIdsPool) throws ApplicationException;
+	void fromXMLTransferable(final XmlObject xmlObject, final ClonedIdsPool clonedIdsPool) throws ApplicationException;
 
 	XmlObject getXMLTransferable();
 }
