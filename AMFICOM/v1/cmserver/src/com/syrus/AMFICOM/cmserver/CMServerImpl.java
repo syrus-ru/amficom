a24ea21309a6718cf3746cd5890c3d9cf6184c3e
Arseniy Tashoyan <tashoyan@gmail.com>
2005-07-28 19:15:43 +0400

    MINOR: Changed visibility of class

--- AMFICOM/v1/cmserver/src/com/syrus/AMFICOM/cmserver/CMServerImpl.java
+++ AMFICOM/v1/cmserver/src/com/syrus/AMFICOM/cmserver/CMServerImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CMServerImpl.java,v 1.115 2005/07/28 10:22:45 arseniy Exp $
+ * $Id: CMServerImpl.java,v 1.116 2005/07/28 15:15:43 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -13,12 +13,12 @@ import com.syrus.AMFICOM.cmserver.corba.CMServerOperations;
 import com.syrus.AMFICOM.general.ServerCore;
 
 /**
- * @version $Revision: 1.115 $, $Date: 2005/07/28 10:22:45 $
+ * @version $Revision: 1.116 $, $Date: 2005/07/28 15:15:43 $
  * @author $Author: arseniy $
  * @module cmserver_v1
  */
 
-public final class CMServerImpl extends ServerCore implements CMServerOperations {
+final class CMServerImpl extends ServerCore implements CMServerOperations {
 	private static final long serialVersionUID = 3760563104903672628L;
 
 	CMServerImpl() {
