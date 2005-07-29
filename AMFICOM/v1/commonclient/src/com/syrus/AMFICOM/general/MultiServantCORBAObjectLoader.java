d67bfbdadec19a63d80291813ff32c1c924612fd
Arseniy Tashoyan <tashoyan@gmail.com>
2005-07-29 17:57:23 +0400

    MINOR: Additional checks

--- AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/MultiServantCORBAObjectLoader.java
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/MultiServantCORBAObjectLoader.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: MultiServantCORBAObjectLoader.java,v 1.2 2005/07/29 13:53:21 arseniy Exp $
+ * $Id: MultiServantCORBAObjectLoader.java,v 1.3 2005/07/29 13:57:23 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -7,12 +7,13 @@
  */
 package com.syrus.AMFICOM.general;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/07/29 13:53:21 $
+ * @version $Revision: 1.3 $, $Date: 2005/07/29 13:57:23 $
  * @author $Author: arseniy $
  * @module commonclient
  */
@@ -54,29 +55,51 @@ public final class MultiServantCORBAObjectLoader implements ObjectLoader {
 		return corbaObjectLoader;
 	}
 
+
 	public final <T extends StorableObject> Set<T> loadStorableObjects(final Set<Identifier> ids) throws ApplicationException {
+		assert ids != null: ErrorMessages.NON_NULL_EXPECTED;
+		if (ids.isEmpty()) {
+			return Collections.emptySet();
+		}
 		return this.getCORBAObjectLoader(ids).loadStorableObjects(ids);
 	}
 
 	public final <T extends StorableObject> Set<T> loadStorableObjectsButIdsByCondition(final Set<Identifier> ids,
 			final StorableObjectCondition condition) throws ApplicationException {
+		assert ids != null && condition != null: ErrorMessages.NON_NULL_EXPECTED;
 		return this.getCORBAObjectLoader(condition.getEntityCode()).loadStorableObjectsButIdsByCondition(ids, condition);
 	}
 
 	public final Map<Identifier, StorableObjectVersion> getRemoteVersions(final Set<Identifier> ids) throws ApplicationException {
+		assert ids != null: ErrorMessages.NON_NULL_EXPECTED;
+		if (ids.isEmpty()) {
+			return Collections.emptyMap();
+		}
 		return this.getCORBAObjectLoader(ids).getRemoteVersions(ids);
 	}
 
 	public final void saveStorableObjects(final Set<StorableObject> storableObjects) throws ApplicationException {
+		assert storableObjects != null : ErrorMessages.NON_NULL_EXPECTED;
+		if (storableObjects.isEmpty()) {
+			return;
+		}
 		this.getCORBAObjectLoader(storableObjects).saveStorableObjects(storableObjects);
 	}
 
 	public final Set<Identifier> getOldVersionIds(final Map<Identifier, StorableObjectVersion> versionsMap)
 			throws ApplicationException {
+		assert versionsMap != null : ErrorMessages.NON_NULL_EXPECTED;
+		if (versionsMap.isEmpty()) {
+			return Collections.emptySet();
+		}
 		return this.getCORBAObjectLoader(versionsMap.keySet()).getOldVersionIds(versionsMap);
 	}
 
 	public final void delete(final Set<? extends Identifiable> identifiables) throws ApplicationException {
+		assert identifiables != null : ErrorMessages.NON_NULL_EXPECTED;
+		if (identifiables.isEmpty()) {
+			return;
+		}
 		this.getCORBAObjectLoader(identifiables).delete(identifiables);
 	}
 }
