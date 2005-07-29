ae22208f2d0f17490019cf1b7748fd45ae034643
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-07-29 10:00:42 +0400

    ADD possibility to compare null values

--- util/src/com/syrus/util/WrapperComparator.java
+++ util/src/com/syrus/util/WrapperComparator.java
@@ -1,5 +1,5 @@
 /*-
-* $Id: WrapperComparator.java,v 1.3 2005/06/15 13:57:49 bob Exp $
+* $Id: WrapperComparator.java,v 1.4 2005/07/29 06:00:42 bob Exp $
 *
 * Copyright ¿ 2005 Syrus Systems.
 * Dept. of Science & Technology.
@@ -13,7 +13,7 @@ import java.util.Comparator;
 
 /**
  * Wrapper comparator
- * @version $Revision: 1.3 $, $Date: 2005/06/15 13:57:49 $
+ * @version $Revision: 1.4 $, $Date: 2005/07/29 06:00:42 $
  * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module util
@@ -30,7 +30,9 @@ public final class WrapperComparator implements Comparator {
 	 * @param key key for wrapper
 	 * @param ascend ascend compare
 	 */
-	public WrapperComparator(final Wrapper wrapper, final String key, final boolean ascend) {
+	public WrapperComparator(final Wrapper wrapper,
+	                         final String key,
+	                         final boolean ascend) {
 		this.wrapper = wrapper;
 		this.key = key;
 		this.ascend = ascend;
@@ -41,7 +43,8 @@ public final class WrapperComparator implements Comparator {
 	 * @param wrapper
 	 * @param key
 	 */
-	public WrapperComparator(final Wrapper wrapper, final String key) {
+	public WrapperComparator(final Wrapper wrapper,
+	                         final String key) {
 		this(wrapper, key, true);
 	}
 
@@ -53,14 +56,16 @@ public final class WrapperComparator implements Comparator {
 		Comparable comparable1;
 		if (value1 instanceof Comparable) {
 			comparable1 = (Comparable)value1;
-		} else
-			comparable1 = value1.toString();		
+		} else {
+			comparable1 = value1 != null ? value1.toString() : "";
+		}		
 
 		Comparable comparable2;
 		if (value2 instanceof Comparable) {
 			comparable2 = (Comparable)value2;
-		} else
-			comparable2 = value2.toString();		
+		} else {
+			comparable2 = value2 != null ? value2.toString() : "";
+		}		
 
 		return (this.ascend ? 1 : -1) * comparable1.compareTo(comparable2);
 	}
