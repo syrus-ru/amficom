8876aa052942a430ea5996b978bd561dee96ae1f
Maxim Selivanov <max.selivanov@gmail.com>
2005-08-02 16:09:21 +0400

    new Entity

---
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/map/MapLibraryWrapper.java
@@ -0,0 +1,103 @@
+/*-
+ * $Id: MapLibraryWrapper.java,v 1.1 2005/08/02 12:09:21 max Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.AMFICOM.map;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.syrus.AMFICOM.general.StorableObjectWrapper;
+
+/**
+ * @author max
+ * @author $Author: max $
+ * @version $Revision: 1.1 $, $Date: 2005/08/02 12:09:21 $
+ * @module map
+ */
+
+public class MapLibraryWrapper extends StorableObjectWrapper{
+	
+	public static final String COLUMN_PARENT_MAP_LIBRARY_ID = "parentMapLibraryId";
+
+	protected static MapLibraryWrapper instance;
+
+	protected List<String> keys;
+	
+	protected MapLibraryWrapper() {
+		// empty private constructor
+		final String[] keysArray = new String[] { COLUMN_NAME,
+				COLUMN_CODENAME,
+				COLUMN_DESCRIPTION,
+				COLUMN_PARENT_MAP_LIBRARY_ID};
+		this.keys = Collections.unmodifiableList(Arrays.asList(keysArray));
+	}
+	
+	public static MapLibraryWrapper getInstance() {
+		if (instance == null)
+			instance = new MapLibraryWrapper();
+		return instance;
+	}
+
+	public String getKey(final int index) {
+		return this.keys.get(index);
+	}
+
+	public List<String> getKeys() {
+		return this.keys;
+	}
+
+	public String getName(final String key) {
+		/* there is no reason rename it */
+		return key;
+	}
+
+	public Object getPropertyValue(final String key) {
+		/* there is no properties */
+		return null;
+	}
+
+	@Override
+	public Object getValue(final Object object, final String key) {
+		if (object instanceof MapLibrary) {
+			final MapLibrary mapLibrary = (MapLibrary) object;
+			if (key.equals(COLUMN_NAME)) {
+				return mapLibrary.getName();
+			} else if (key.equals(COLUMN_CODENAME)) {
+				return mapLibrary.getCodename();
+			} else if (key.equals(COLUMN_DESCRIPTION)) {
+				return mapLibrary.getDescription();
+			} else if (key.equals(COLUMN_PARENT_MAP_LIBRARY_ID)) {
+				return mapLibrary.getParent();
+			}
+		}
+		return null;
+	}
+
+	public boolean isEditable(final String key) {
+		return false;
+	}
+
+	public void setPropertyValue(final String key, final Object objectKey, final Object objectValue) {
+		/* there is no properties */
+	}
+
+	public void setValue(final Object object, final String key, final Object value) {
+		if (object instanceof MapLibrary) {
+			final MapLibrary mapLibrary = (MapLibrary) object;
+			if (key.equals(COLUMN_NAME)) {
+				mapLibrary.setName((String) value);
+			} else if (key.equals(COLUMN_CODENAME)) {
+				mapLibrary.setCodename((String) value);
+			} else if (key.equals(COLUMN_DESCRIPTION)) {
+				mapLibrary.setDescription(COLUMN_DESCRIPTION);
+			} else if (key.equals(COLUMN_PARENT_MAP_LIBRARY_ID)) {
+				mapLibrary.setParent((MapLibrary) value);
+			}
+		}
+	}
+}
