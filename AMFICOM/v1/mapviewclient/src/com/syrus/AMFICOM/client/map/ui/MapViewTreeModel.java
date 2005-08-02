66f9374aecbf2399e68285b128b1a95cf0198a66
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-02 12:15:06 +0400

    ADD: linked map libraries branch

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapViewTreeModel.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapViewTreeModel.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapViewTreeModel.java,v 1.18 2005/07/25 12:44:09 bass Exp $ Syrus
+ * $Id: MapViewTreeModel.java,v 1.19 2005/08/02 08:15:06 krupenn Exp $ Syrus
  * Systems Научно-технический центр Проект: АМФИКОМ Автоматизированный
  * МногоФункциональный Интеллектуальный Комплекс Объектного Мониторинга
  * Платформа: java 1.4.1
@@ -12,6 +12,7 @@ import java.awt.Toolkit;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -23,15 +24,13 @@ import com.syrus.AMFICOM.client.UI.tree.PopulatableIconedNode;
 import com.syrus.AMFICOM.client.resource.LangModelMap;
 import com.syrus.AMFICOM.logic.ChildrenFactory;
 import com.syrus.AMFICOM.logic.Item;
-import com.syrus.AMFICOM.map.Map;
-import com.syrus.AMFICOM.map.MapElement;
-import com.syrus.AMFICOM.map.SiteNodeType;
+import com.syrus.AMFICOM.map.MapLibrary;
 import com.syrus.AMFICOM.mapview.MapView;
 import com.syrus.AMFICOM.scheme.Scheme;
 
 /**
- * @version $Revision: 1.18 $, $Date: 2005/07/25 12:44:09 $
- * @author $Author: bass $
+ * @version $Revision: 1.19 $, $Date: 2005/08/02 08:15:06 $
+ * @author $Author: krupenn $
  * @module mapviewclient_v1
  */
 public class MapViewTreeModel implements ChildrenFactory {
@@ -39,6 +38,8 @@ public class MapViewTreeModel implements ChildrenFactory {
 	public static final String MAP_BRANCH = "Map";
 
 	public static final String SCHEMES_BRANCH = "schemes";
+
+	public static final String LIBRARIES_BRANCH = "libraries";
 
 	public static final String NONAME_BRANCH = "noname";
 
@@ -55,6 +56,14 @@ public class MapViewTreeModel implements ChildrenFactory {
 					IMG_SIZE,
 					IMG_SIZE,
 					Image.SCALE_SMOOTH));
+
+	static ImageIcon libraryIcon = new ImageIcon(Toolkit.getDefaultToolkit()
+			.getImage("images/maplibrary.gif").getScaledInstance(
+					IMG_SIZE,
+					IMG_SIZE,
+					Image.SCALE_SMOOTH));
+
+	public static MapLibraryComparator libraryComparator = new MapLibraryComparator();
 
 	MapSchemeTreeModel schemeTreeModel;
 	MapTreeModel mapTreeModel;
@@ -98,52 +107,15 @@ public class MapViewTreeModel implements ChildrenFactory {
 		}
 	}
 
-	public String getObjectName(Object object) {
-		if(object instanceof String)
-			return LangModelMap.getString((String )object);
-		else
-		if(object instanceof MapView) {
-			MapView mapView = (MapView )object;
-			return mapView.getName();
-		}
-		return LangModelMap.getString(NONAME_BRANCH);
-	}
-
-	public String getNodeName(Item node) {
-		if(node.getObject() instanceof String)
-			return LangModelMap.getString((String )(node.getObject()));
-		else
-		if(node.getObject() instanceof MapView) {
-			MapView mapView = (MapView )node.getObject();
-			return mapView.getName();
-		}
-		else
-		if(node.getObject() instanceof Map) {
-			Map map = (Map )node.getObject();
-			return map.getName();
-		}
-		else
-		if(node.getObject() instanceof SiteNodeType) {
-			SiteNodeType type = (SiteNodeType )node.getObject();
-			return type.getName();
-		}
-		else
-		if(node.getObject() instanceof MapElement) {
-			MapElement mapElement = (MapElement )node
-					.getObject();
-			return mapElement.getName();
-		}
-		return LangModelMap.getString(NONAME_BRANCH);
-	}
-
 	public void populate(Item node) {
 		if (node.getObject() instanceof String) {
 			String s = (String) node.getObject();
 			if (s.equals(MapViewTreeModel.MAP_BRANCH)) {
 				populateMapNode((PopulatableIconedNode )node);
-			}
-			else if (s.equals(MapViewTreeModel.SCHEMES_BRANCH)) {
+			} else if (s.equals(MapViewTreeModel.SCHEMES_BRANCH)) {
 				populateSchemesNode((PopulatableIconedNode )node);
+			} else if (s.equals(MapViewTreeModel.LIBRARIES_BRANCH)) {
+				populateLibrariesNode((PopulatableIconedNode )node);
 			}
 		}
 		else if(node.getObject() instanceof MapView) {
@@ -154,12 +126,13 @@ public class MapViewTreeModel implements ChildrenFactory {
 	void populateMapViewNode(PopulatableIconedNode node) {
 		PopulatableIconedNode mapNode;
 		PopulatableIconedNode schemesNode;
+		PopulatableIconedNode librariesNode;
 
 		if(node.getChildren().size() == 0) {
 			mapNode = new PopulatableIconedNode(
 					this,
 					MapViewTreeModel.MAP_BRANCH,
-					getObjectName(MapViewTreeModel.MAP_BRANCH),
+					LangModelMap.getString(MapViewTreeModel.MAP_BRANCH),
 					folderIcon,
 					true);
 //			mapNode.populate();
@@ -168,11 +141,20 @@ public class MapViewTreeModel implements ChildrenFactory {
 			schemesNode = new PopulatableIconedNode(
 					this,
 					MapViewTreeModel.SCHEMES_BRANCH,
-					getObjectName(MapViewTreeModel.SCHEMES_BRANCH),
+					LangModelMap.getString(MapViewTreeModel.SCHEMES_BRANCH),
 					folderIcon,
 					true);
 //			schemesNode.populate();
 			node.addChild(schemesNode);
+
+			librariesNode = new PopulatableIconedNode(
+					this,
+					MapViewTreeModel.LIBRARIES_BRANCH,
+					LangModelMap.getString(MapViewTreeModel.LIBRARIES_BRANCH),
+					folderIcon,
+					true);
+//			mapNode.populate();
+			node.addChild(librariesNode);
 		}
 		else {
 			for(Iterator iter = node.getChildren().iterator(); iter.hasNext();) {
@@ -256,4 +238,58 @@ public class MapViewTreeModel implements ChildrenFactory {
 		}
 	}
 
+	void populateLibrariesNode(PopulatableIconedNode node) {
+		Item parentNode = node.getParent();
+		MapView mapView = (MapView )parentNode.getObject();
+
+		List librariesChildren = new ArrayList();
+		librariesChildren.addAll(mapView.getMap().getMapLibraries());
+		Collections.sort(librariesChildren, libraryComparator);
+
+		java.util.Map nodePresense = new HashMap();
+
+		List toRemove = new LinkedList();
+
+		for(Iterator iter = node.getChildren().iterator(); iter.hasNext();) {
+			PopulatableIconedNode childNode = (PopulatableIconedNode )iter.next();
+			MapLibrary library = (MapLibrary )childNode.getObject();
+			if(librariesChildren.contains(library)) {
+				if(childNode.isPopulated())
+					childNode.populate();
+				nodePresense.put(library, childNode);
+			}
+			else
+				toRemove.add(childNode);
+		}
+		for(Iterator it = toRemove.iterator(); it.hasNext();) {
+			Item childItem = (Item )it.next();
+			childItem.setParent(null);
+		}
+
+		for(Iterator it = librariesChildren.iterator(); it.hasNext();) {
+			MapLibrary library = (MapLibrary )it.next();
+			Item childNode = (Item )nodePresense.get(library);
+			if(childNode == null) {
+				PopulatableIconedNode newItem = new PopulatableIconedNode(
+						MapLibraryTreeModel.getInstance(),
+						library,
+						libraryIcon, 
+						true);
+//				newItem.populate();
+				node.addChild(newItem);
+			}
+		}
+	}
+}
+
+final class MapLibraryComparator implements Comparator {
+	public int compare(Object o1, Object o2) {
+		Scheme scheme1 = (Scheme )o1;
+		Scheme scheme2 = (Scheme )o2;
+		return scheme1.getName().compareTo(scheme2.getName());
+	}
+
+	public boolean equals(Object obj) {
+		return (obj instanceof SchemeComparator);
+	}
 }
