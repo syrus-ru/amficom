26838d3af1c27e55ac1c93e899e0f7e9fa0114fa
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-02 20:47:44 +0400

    ADD: parametrize getChildren with <LibraryEntry>

--- AMFICOM/v1/map/src/com/syrus/AMFICOM/general/logic/Library.java
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/general/logic/Library.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: Library.java,v 1.2 2005/08/02 07:15:11 krupenn Exp $
+ * $Id: Library.java,v 1.3 2005/08/02 16:47:44 krupenn Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -13,12 +13,12 @@ import java.util.List;
 /**
  * @author Andrei Kroupennikov
  * @author $Author: krupenn $
- * @version $Revision: 1.2 $, $Date: 2005/08/02 07:15:11 $
+ * @version $Revision: 1.3 $, $Date: 2005/08/02 16:47:44 $
  * @module map
  */
 public interface Library extends LibraryEntry {
 
-	List getChildren();
+	List<LibraryEntry> getChildren();
 	void addChild(final LibraryEntry libraryEntry);
 	void removeChild(final LibraryEntry libraryEntry);
 }
