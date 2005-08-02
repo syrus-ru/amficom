b6420c09578e984cde6d59d1c7cf93f965f022f6
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-02 11:25:31 +0400

    tree model for MapLibrary branch

---
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapLibraryTreeModel.java
@@ -0,0 +1,226 @@
+/**
+ * $Id: MapLibraryTreeModel.java,v 1.1 2005/08/02 07:25:31 krupenn Exp $
+ *
+ * Syrus Systems
+ * Научно-технический центр
+ * Проект: АМФИКОМ
+ */
+package com.syrus.AMFICOM.client.map.ui;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import com.syrus.AMFICOM.client.UI.tree.IconedNode;
+import com.syrus.AMFICOM.client.UI.tree.PopulatableIconedNode;
+import com.syrus.AMFICOM.client.map.controllers.NodeTypeController;
+import com.syrus.AMFICOM.client.resource.LangModelMap;
+import com.syrus.AMFICOM.general.logic.LibraryEntry;
+import com.syrus.AMFICOM.logic.ChildrenFactory;
+import com.syrus.AMFICOM.logic.Item;
+import com.syrus.AMFICOM.map.MapLibrary;
+import com.syrus.AMFICOM.map.PhysicalLinkType;
+import com.syrus.AMFICOM.map.SiteNodeType;
+
+public class MapLibraryTreeModel implements ChildrenFactory {
+
+	private static MapLibraryTreeModel instance = null;
+
+	private static final String SITENODETYPE_BRANCH = "sitenodetype";
+	private static final String PHYSICALLINKTYPE_BRANCH = "physicallinktype";
+	static final int IMG_SIZE = 16;
+
+	public static NodeTypeComparator nodeTypeComparator = new NodeTypeComparator();
+	public static LinkTypeComparator linkTypeComparator = new LinkTypeComparator();
+
+	static final Icon linkTypeIcon = new ImageIcon(Toolkit.getDefaultToolkit()
+			.getImage("images/linkmode.gif").getScaledInstance(
+					IMG_SIZE,
+					IMG_SIZE,
+					Image.SCALE_SMOOTH));
+
+	protected MapLibraryTreeModel() {
+		// empty
+	}
+	
+	public static ChildrenFactory getInstance() {
+		if(instance == null) {
+			instance = new MapLibraryTreeModel();
+		}
+		return instance;
+	}
+
+	public void populate(Item node) {
+		if (node.getObject() instanceof String) {
+			String s = (String) node.getObject();
+			if (s.equals(MapLibraryTreeModel.SITENODETYPE_BRANCH)) {
+				populateSiteNodeTypesNode((PopulatableIconedNode )node);
+			} else if (s.equals(MapLibraryTreeModel.PHYSICALLINKTYPE_BRANCH)) {
+				populatePhysicalLinkTypesNode((PopulatableIconedNode )node);
+			}
+		}
+		else if(node.getObject() instanceof MapLibrary) {
+			populateMapLibraryNode((PopulatableIconedNode )node);
+		}
+	}
+
+	void populateMapLibraryNode(PopulatableIconedNode node) {
+		PopulatableIconedNode siteNodeTypesNode;
+		PopulatableIconedNode physicalLinkTypesNode;
+
+		if(node.getChildren().size() == 0) {
+			siteNodeTypesNode = new PopulatableIconedNode(
+					this,
+					MapLibraryTreeModel.SITENODETYPE_BRANCH,
+					LangModelMap.getString(MapLibraryTreeModel.SITENODETYPE_BRANCH),
+					MapViewTreeModel.folderIcon,
+					true);
+			node.addChild(siteNodeTypesNode);
+
+			physicalLinkTypesNode = new PopulatableIconedNode(
+					this,
+					MapLibraryTreeModel.PHYSICALLINKTYPE_BRANCH,
+					LangModelMap.getString(MapLibraryTreeModel.PHYSICALLINKTYPE_BRANCH),
+					MapViewTreeModel.folderIcon,
+					true);
+			node.addChild(physicalLinkTypesNode);
+		}
+		else {
+			for(Iterator iter = node.getChildren().iterator(); iter.hasNext();) {
+				PopulatableIconedNode childNode = (PopulatableIconedNode )iter.next();
+//				if(childNode.isPopulated())
+					childNode.populate();
+			}
+		}
+	}
+
+	void populateSiteNodeTypesNode(PopulatableIconedNode node) {
+		Item parentNode = node.getParent();
+		MapLibrary library = (MapLibrary )parentNode.getObject();
+
+		List siteNodeTypeChildren = new ArrayList();
+		for(Iterator iter = library.getChildren().iterator(); iter.hasNext();) {
+			LibraryEntry entry = (LibraryEntry )iter.next();
+			if(entry instanceof SiteNodeType) {
+				siteNodeTypeChildren.add(entry);
+			}
+		}
+		Collections.sort(siteNodeTypeChildren, MapLibraryTreeModel.nodeTypeComparator);
+
+		java.util.Map nodePresense = new HashMap();
+
+		List toRemove = new LinkedList();
+
+		for(Iterator iter = node.getChildren().iterator(); iter.hasNext();) {
+			PopulatableIconedNode childNode = (PopulatableIconedNode )iter.next();
+			SiteNodeType type = (SiteNodeType )childNode.getObject();
+			if(siteNodeTypeChildren.contains(type)) {
+				if(childNode.isPopulated())
+					childNode.populate();
+				nodePresense.put(type, childNode);
+			}
+			else
+				toRemove.add(childNode);
+		}
+		for(Iterator it = toRemove.iterator(); it.hasNext();) {
+			Item childItem = (Item )it.next();
+			childItem.setParent(null);
+		}
+
+		for(Iterator it = siteNodeTypeChildren.iterator(); it.hasNext();) {
+			SiteNodeType type = (SiteNodeType )it.next();
+			Item childNode = (Item )nodePresense.get(type);
+			if(childNode == null) {
+				Item newItem = new IconedNode(
+						type,
+						new ImageIcon(NodeTypeController.getImage(type)
+								.getScaledInstance(
+										IMG_SIZE,
+										IMG_SIZE,
+										Image.SCALE_SMOOTH)),
+						false); 
+				node.addChild(newItem);
+			}
+		}
+	}
+
+	void populatePhysicalLinkTypesNode(PopulatableIconedNode node) {
+		Item parentNode = node.getParent();
+		MapLibrary library = (MapLibrary )parentNode.getObject();
+
+		List physicalLinkTypeChildren = new ArrayList();
+		for(Iterator iter = library.getChildren().iterator(); iter.hasNext();) {
+			LibraryEntry entry = (LibraryEntry )iter.next();
+			if(entry instanceof PhysicalLinkType) {
+				physicalLinkTypeChildren.add(entry);
+			}
+		}
+		Collections.sort(physicalLinkTypeChildren, MapLibraryTreeModel.linkTypeComparator);
+
+		java.util.Map nodePresense = new HashMap();
+
+		List toRemove = new LinkedList();
+
+		for(Iterator iter = node.getChildren().iterator(); iter.hasNext();) {
+			PopulatableIconedNode childNode = (PopulatableIconedNode )iter.next();
+			PhysicalLinkType type = (PhysicalLinkType )childNode.getObject();
+			if(physicalLinkTypeChildren.contains(type)) {
+				if(childNode.isPopulated())
+					childNode.populate();
+				nodePresense.put(type, childNode);
+			}
+			else
+				toRemove.add(childNode);
+		}
+		for(Iterator it = toRemove.iterator(); it.hasNext();) {
+			Item childItem = (Item )it.next();
+			childItem.setParent(null);
+		}
+
+		for(Iterator it = physicalLinkTypeChildren.iterator(); it.hasNext();) {
+			PhysicalLinkType type = (PhysicalLinkType )it.next();
+			Item childNode = (Item )nodePresense.get(type);
+			if(childNode == null) {
+				Item newItem = new IconedNode(
+						type,
+						linkTypeIcon,
+						false); 
+				node.addChild(newItem);
+			}
+		}
+	}
+
+}
+
+final class NodeTypeComparator implements Comparator {
+	public int compare(Object o1, Object o2) {
+		SiteNodeType type1 = (SiteNodeType )o1;
+		SiteNodeType type2 = (SiteNodeType )o2;
+		return type1.getName().compareTo(type2.getName());
+	}
+
+	public boolean equals(Object obj) {
+		return (obj instanceof NodeTypeComparator);
+	}
+}
+
+final class LinkTypeComparator implements Comparator {
+	public int compare(Object o1, Object o2) {
+		PhysicalLinkType type1 = (PhysicalLinkType )o1;
+		PhysicalLinkType type2 = (PhysicalLinkType )o2;
+		return type1.getName().compareTo(type2.getName());
+	}
+
+	public boolean equals(Object obj) {
+		return (obj instanceof LinkTypeComparator);
+	}
+}
