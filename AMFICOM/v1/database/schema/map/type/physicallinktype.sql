b38f0419af6e0c64b700d3cd82996d725b43fa5c
Maxim Selivanov <max.selivanov@gmail.com>
2005-08-03 15:47:19 +0400

    ADD: fields sort, is_topologial and map_Library_id

--- AMFICOM/v1/database/schema/map/type/physicallinktype.sql
+++ AMFICOM/v1/database/schema/map/type/physicallinktype.sql
@@ -1,4 +1,4 @@
--- $Id: physicallinktype.sql,v 1.8 2005/06/15 17:03:09 bass Exp $
+-- $Id: physicallinktype.sql,v 1.9 2005/08/03 11:46:32 max Exp $
 
 CREATE TABLE PhysicalLinkType (
  id NUMBER(19),
@@ -8,20 +8,25 @@ CREATE TABLE PhysicalLinkType (
  modifier_id NOT NULL,
  version NUMBER(19) NOT NULL,
 --
+ sort NUMBER(1),
  codename VARCHAR2(32 CHAR) NOT NULL,
- name VARCHAR2(128 CHAR),
+ name VARCHAR2(64 CHAR),
  description VARCHAR2(256 CHAR),
  dimension_x NUMBER(6),
  dimension_y NUMBER(6),
+ is_topological NUMBER(1),
+ map_library_id,
 --
  CONSTRAINT phlinktype_pk PRIMARY KEY (id),
  CONSTRAINT phlinktype_uniq UNIQUE (codename),
  CONSTRAINT phlinktype_creator_fk FOREIGN KEY (creator_id)
   REFERENCES SystemUser (id) ON DELETE CASCADE,
  CONSTRAINT phlinktype_modifier_fk FOREIGN KEY (modifier_id)
-  REFERENCES SystemUser (id) ON DELETE CASCADE
+  REFERENCES SystemUser (id) ON DELETE CASCADE,
+ CONSTRAINT phlinktype_maplib_id_fk GOREIGN KEY (map_library_id)
+  REFERENCES MapLibrary (id) ON DELETE CASCADE
 );
 
-COMMENT ON TABLE PhysicalLinkType IS '$Id: physicallinktype.sql,v 1.8 2005/06/15 17:03:09 bass Exp $';
+COMMENT ON TABLE PhysicalLinkType IS '$Id: physicallinktype.sql,v 1.9 2005/08/03 11:46:32 max Exp $';
 
 CREATE SEQUENCE PhysicalLinkType_Seq ORDER;
--- AMFICOM/v1/database/schema/map/type/sitenodetype.sql
+++ AMFICOM/v1/database/schema/map/type/sitenodetype.sql
@@ -1,4 +1,4 @@
--- $Id: sitenodetype.sql,v 1.8 2005/06/15 17:03:09 bass Exp $
+-- $Id: sitenodetype.sql,v 1.9 2005/08/03 11:46:32 max Exp $
 
 CREATE TABLE SiteNodeType (
  id NUMBER(19),
@@ -8,11 +8,13 @@ CREATE TABLE SiteNodeType (
  modifier_id NOT NULL,
  version NUMBER(19) NOT NULL,
 --
+ sort NUMBER(1) NOT NULL,
  codename VARCHAR2(32 CHAR) NOT NULL,
- name VARCHAR2(128 CHAR),
+ name VARCHAR2(64 CHAR),
  description VARCHAR2(256 CHAR),
  image_id NOT NULL,
- topological NUMBER(1),
+ is_topological NUMBER(1) NOT NULL,
+ map_library_id
 --
  CONSTRAINT sitnodetype_pk PRIMARY KEY (id),
  CONSTRAINT sitnodetype_uniq UNIQUE (codename),
@@ -21,9 +23,11 @@ CREATE TABLE SiteNodeType (
  CONSTRAINT sitnodetype_modifier_fk FOREIGN KEY (modifier_id)
   REFERENCES SystemUser (id) ON DELETE CASCADE,
  CONSTRAINT sitnodetype_image_fk FOREIGN KEY (image_id)
-  REFERENCES ImageResource (id) ON DELETE CASCADE
+  REFERENCES ImageResource (id) ON DELETE CASCADE,
+  CONSTRAINT sitnodetype_ml_id_fk GOREIGN KEY (map_library_id)
+  REFERENCES MapLibrary (id) ON DELETE CASCADE
 );
 
-COMMENT ON TABLE SiteNodeType IS '$Id: sitenodetype.sql,v 1.8 2005/06/15 17:03:09 bass Exp $';
+COMMENT ON TABLE SiteNodeType IS '$Id: sitenodetype.sql,v 1.9 2005/08/03 11:46:32 max Exp $';
 
 CREATE SEQUENCE SiteNodeType_Seq ORDER;
