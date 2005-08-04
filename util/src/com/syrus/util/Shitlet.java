84948b7d5d15e19f6ed9435f24dc27d157b94758
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-08-04 17:49:41 +0400

    ADD annotation for greeting 'balbes' to Arseniy

---
+++ util/src/com/syrus/util/Shitlet.java
@@ -0,0 +1,30 @@
+/*-
+* $Id: Shitlet.java,v 1.1 2005/08/04 13:49:41 bob Exp $
+*
+* Copyright © 2005 Syrus Systems.
+* Dept. of Science & Technology.
+* Project: AMFICOM.
+*/
+
+package com.syrus.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/08/04 13:49:41 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @author Andrew `Bass' Scheglov
+ * @module util
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Shitlet {
+	
+ // Дружественный прЮвет главным хуйатарам
+	
+}
+
