eb534c25a83c24c265f5aa4d920fa6b176fb5875
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-08-04 18:04:16 +0400

    ADD PermissionAttributes

---
+++ AMFICOM/v1/admin/src/com/syrus/AMFICOM/administration/PermissionAttributesDatabase.java
@@ -0,0 +1,97 @@
+/*
+ * $Id: PermissionAttributesDatabase.java,v 1.1 2005/08/04 14:04:16 bob Exp $
+ *
+ * Copyright ¿ 2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
+package com.syrus.AMFICOM.administration;
+
+import static com.syrus.AMFICOM.administration.PermissionAttributesWrapper.COLUMN_PERMISSION_MASK;
+import static com.syrus.AMFICOM.administration.PermissionAttributesWrapper.COLUMN_USER_ID;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.syrus.AMFICOM.general.DatabaseIdentifier;
+import com.syrus.AMFICOM.general.IllegalDataException;
+import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.StorableObjectDatabase;
+import com.syrus.AMFICOM.general.StorableObjectVersion;
+import com.syrus.AMFICOM.general.StorableObjectWrapper;
+import com.syrus.util.database.DatabaseDate;
+
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/08/04 14:04:16 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module admin
+ */
+
+public final class PermissionAttributesDatabase extends StorableObjectDatabase<PermissionAttributes> {
+	private static String columns;
+	private static String updateMultipleSQLValues;
+
+	@Override
+	protected short getEntityCode() {		
+		return ObjectEntities.PERMATTR_CODE;
+	}
+
+	@Override
+	protected String getColumnsTmpl() {
+		if (columns == null) {
+			columns = DomainMember.COLUMN_DOMAIN_ID + COMMA
+			+ COLUMN_USER_ID + COMMA
+			+ COLUMN_PERMISSION_MASK;
+		}
+		return columns;
+	}
+
+	@Override
+	protected String getUpdateMultipleSQLValuesTmpl() {
+		if (updateMultipleSQLValues == null) {
+			updateMultipleSQLValues = QUESTION + COMMA
+			+ QUESTION + COMMA
+			+ QUESTION;
+		}
+		return updateMultipleSQLValues;
+	}
+
+	@Override
+	protected String getUpdateSingleSQLValuesTmpl(final PermissionAttributes storableObject) throws IllegalDataException {
+		final String sql = DatabaseIdentifier.toSQLString(storableObject.getDomainId()) + COMMA
+			+ DatabaseIdentifier.toSQLString(storableObject.getUserId()) + COMMA
+			+ storableObject.getPermissionMask();
+		return sql;
+	}
+
+	@Override
+	protected PermissionAttributes updateEntityFromResultSet(final PermissionAttributes storableObject, final ResultSet resultSet)
+			throws IllegalDataException, SQLException {
+		final PermissionAttributes permissionAttributes = (storableObject == null) ? new PermissionAttributes(DatabaseIdentifier.getIdentifier(resultSet,
+				StorableObjectWrapper.COLUMN_ID), null, StorableObjectVersion.ILLEGAL_VERSION, null, null, 0L) : storableObject;
+		permissionAttributes.setAttributes(DatabaseDate.fromQuerySubString(resultSet, StorableObjectWrapper.COLUMN_CREATED),
+				DatabaseDate.fromQuerySubString(resultSet, StorableObjectWrapper.COLUMN_MODIFIED),
+				DatabaseIdentifier.getIdentifier(resultSet, StorableObjectWrapper.COLUMN_CREATOR_ID),
+				DatabaseIdentifier.getIdentifier(resultSet, StorableObjectWrapper.COLUMN_MODIFIER_ID),
+				new StorableObjectVersion(resultSet.getLong(StorableObjectWrapper.COLUMN_VERSION)),
+				DatabaseIdentifier.getIdentifier(resultSet, DomainMember.COLUMN_DOMAIN_ID),
+				DatabaseIdentifier.getIdentifier(resultSet, COLUMN_USER_ID),
+				resultSet.getLong(COLUMN_PERMISSION_MASK));
+		return permissionAttributes;
+	}
+
+	@Override
+	protected int setEntityForPreparedStatementTmpl(final PermissionAttributes storableObject,
+			final PreparedStatement preparedStatement,
+			int startParameterNumber) throws IllegalDataException, SQLException {
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getDomainId());
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getUserId());
+		preparedStatement.setLong(++startParameterNumber, storableObject.getPermissionMask());
+		return startParameterNumber;
+	}
+
+}
---
+++ AMFICOM/v1/admin/src/com/syrus/AMFICOM/administration/PermissionAttributesWrapper.java
@@ -0,0 +1,128 @@
+/*
+ * $Id: PermissionAttributesWrapper.java,v 1.1 2005/08/04 14:04:16 bob Exp $
+ *
+ * Copyright ¿ 2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
+package com.syrus.AMFICOM.administration;
+
+import static com.syrus.AMFICOM.administration.DomainMember.COLUMN_DOMAIN_ID;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.StorableObjectWrapper;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/08/04 14:04:16 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module admin
+ */
+public class PermissionAttributesWrapper extends StorableObjectWrapper {
+
+	public static final String	COLUMN_USER_ID			= "user_id";
+	public static final String	COLUMN_PERMISSION_MASK	= "permission_mask";
+	
+	private static PermissionAttributesWrapper instance;
+
+	private List<String> keys;
+
+	private PermissionAttributesWrapper() {
+		// empty private constructor
+		final String[] keysArray = new String[] { COLUMN_DOMAIN_ID, 
+				COLUMN_USER_ID, 
+				COLUMN_PERMISSION_MASK };
+
+		this.keys = Collections.unmodifiableList(Arrays.asList(keysArray));
+	}
+
+	public static PermissionAttributesWrapper getInstance() {
+		if (instance == null)
+			instance = new PermissionAttributesWrapper();
+		return instance;
+	}
+
+	public List<String> getKeys() {
+		return this.keys;
+	}
+
+	public String getName(final String key) {
+		/* there is no reason to rename it */
+		return key;
+	}
+
+	@Override
+	public Object getValue(final Object object, final String key) {
+		final Object value = super.getValue(object, key);
+		if (value == null && object instanceof PermissionAttributes) {
+			final PermissionAttributes permissionAttributes = 
+				(PermissionAttributes) object;
+			if (key.equals(COLUMN_DOMAIN_ID)) {
+				return permissionAttributes.getDomainId();
+			}
+			if (key.equals(COLUMN_USER_ID)) {
+				return permissionAttributes.getUserId();
+			}
+			if (key.equals(COLUMN_PERMISSION_MASK)) {
+				return permissionAttributes.getPermissionMask();
+			}
+		}
+		return value;
+	}
+
+	public boolean isEditable(final String key) {
+		return false;
+	}
+
+	public void setValue(Object object, final String key, final Object value) {
+		if (object instanceof PermissionAttributes) {
+			final PermissionAttributes permissionAttributes = 
+				(PermissionAttributes) object;
+			if (key.equals(COLUMN_DOMAIN_ID)) {
+				permissionAttributes.setDomainId((Identifier) value);
+			} else if (key.equals(COLUMN_USER_ID)) {
+				permissionAttributes.setUserId((Identifier) value);
+			} else if (key.equals(COLUMN_PERMISSION_MASK)) {
+				permissionAttributes.setPermissionMask((Long) value);
+			}
+			
+		}
+	}
+
+	public String getKey(final int index) {
+		return this.keys.get(index);
+	}
+
+	public Object getPropertyValue(final String key) {
+		/* there is no properties */
+		return null;
+	}
+
+	public void setPropertyValue(final String key, 
+	                             final Object objectKey, 
+	                             final Object objectValue) {
+		/* there is no properties */
+	}
+
+	@Override
+	public Class getPropertyClass(final String key) {
+		final Class clazz = super.getPropertyClass(key); 
+		if (clazz != null) {
+			return clazz;
+		}
+		if (key.equals(COLUMN_DOMAIN_ID)
+				|| key.equals(COLUMN_USER_ID)) {
+			return Identifier.class;
+		}
+		if (key.equals(COLUMN_PERMISSION_MASK)) {
+			return Long.class;
+		}
+		return null;
+	}
+
+}
