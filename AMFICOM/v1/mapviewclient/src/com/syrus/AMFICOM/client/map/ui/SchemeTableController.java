cdb2edc3a2b8a9ff144218712e92bfdaf46293d6
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-05 17:27:14 +0400

    COMPATIBILITY: parametrized superclass PERFORMANCE: remove uneeded instantiation of ArrayList

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/SchemeTableController.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/SchemeTableController.java
@@ -6,7 +6,6 @@
 
 package com.syrus.AMFICOM.client.map.ui;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -20,129 +19,110 @@ import com.syrus.AMFICOM.general.StorableObjectPool;
 import com.syrus.AMFICOM.general.StorableObjectWrapper;
 import com.syrus.AMFICOM.scheme.Scheme;
 
-public final class SchemeTableController extends StorableObjectWrapper 
-{
-	public static final String KEY_NAME = "Name";
+public final class SchemeTableController extends StorableObjectWrapper<Scheme> {
 	public static final String KEY_DOMAIN = "Domain";
-	public static final String KEY_USER = "User_id";
-	public static final String KEY_CREATED = "Created";
-	public static final String KEY_MODIFIED = "Modified";
 
 	private static SchemeTableController instance;
 
 	private List keys;
 	private String[] keysArray;
 
-	private SchemeTableController() 
-	{
+	private SchemeTableController() {
 		// empty private constructor
-		this.keysArray = new String[] { 
-				KEY_NAME, 
-				KEY_DOMAIN, 
-				KEY_USER, 
-				KEY_CREATED, 
-				KEY_MODIFIED };
-	
-		this.keys = Collections.unmodifiableList(new ArrayList(Arrays.asList(this.keysArray)));
+		this.keysArray = new String[] {
+			COLUMN_NAME,
+			KEY_DOMAIN,
+			COLUMN_CREATOR_ID,
+			COLUMN_CREATED,
+			COLUMN_MODIFIED
+		};
+
+		this.keys = Collections.unmodifiableList(Arrays.asList(this.keysArray));
 	}
 
-	public static SchemeTableController getInstance() 
-	{
-		if (instance == null)
+	public static SchemeTableController getInstance() {
+		if(instance == null)
 			instance = new SchemeTableController();
 		return instance;
 	}
-	
-	public List getKeys() 
-	{
+
+	public List getKeys() {
 		return this.keys;
 	}
 
-	public String[] getKeysArray() 
-	{
+	public String[] getKeysArray() {
 		return this.keysArray;
 	}
 
-	public String getName(final String key)
-	{
+	public String getName(final String key) {
 		String name = LangModelMap.getString(key);
 		return name;
 	}
 
-	public Object getValue(final Object object, final String key)
-	{
-		Object result = null;
-		Scheme sc = (Scheme )object;
-		if (key.equals(KEY_NAME))
-		{
-			result = sc.getName();
-		}
-		else
-		if (key.equals(KEY_DOMAIN))
-		{
-			try
-			{
-				Domain domain = StorableObjectPool.getStorableObject(sc.getDomainId(), false);
-				result = domain.getName();
+	public Object getValue(final Scheme scheme, final String key) {
+		Object result = super.getValue(scheme, key);
+		if(result == null && scheme != null) {
+			if(key.equals(COLUMN_NAME)) {
+				result = scheme.getName();
 			}
-			catch (final ApplicationException ae)
-			{
-				ae.printStackTrace();
-				result = null;
+			else if(key.equals(KEY_DOMAIN)) {
+				try {
+					Domain domain = StorableObjectPool.getStorableObject(
+							scheme.getDomainId(), false);
+					result = domain.getName();
+				} catch(final ApplicationException ae) {
+					ae.printStackTrace();
+					result = null;
+				}
 			}
-		}
-		else
-		if (key.equals(KEY_USER))
-		{
-			try
-			{
-				SystemUser user = StorableObjectPool.getStorableObject(sc.getCreatorId(), false);
-				result = user.getName();
+			else if(key.equals(COLUMN_CREATOR_ID)) {
+				try {
+					SystemUser user = StorableObjectPool.getStorableObject(
+							scheme.getCreatorId(),
+							false);
+					result = user.getName();
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
 			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
+			else if(key.equals(COLUMN_CREATED)) {
+				result = MapPropertiesManager.getDateFormat().format(
+						scheme.getCreated());
 			}
-		}
-		else
-		if (key.equals(KEY_CREATED))
-		{
-			result = MapPropertiesManager.getDateFormat().format(sc.getCreated());
-		}
-		else
-		if (key.equals(KEY_MODIFIED))
-		{
-			result = MapPropertiesManager.getDateFormat().format(sc.getModified());
+			else if(key.equals(COLUMN_MODIFIED)) {
+				result = MapPropertiesManager.getDateFormat().format(
+						scheme.getModified());
+			}
+			return result;
 		}
 		return result;
 	}
 
-	public boolean isEditable(final String key)
-	{
+	public boolean isEditable(final String key) {
 		return false;
 	}
 
-	public void setValue(Object object, final String key, final Object value)
-	{//empty
+	public void setValue(Scheme object, final String key, final Object value) {
+		// empty
 	}
 
-	public String getKey(final int index) 
-	{
+	public String getKey(final int index) {
 		return (String )this.keys.get(index);
 	}
 
-	public Object getPropertyValue(final String key) 
-	{
+	public Object getPropertyValue(final String key) {
 		Object result = "";
 		return result;
 	}
 
-	public void setPropertyValue(String key, Object objectKey, Object objectValue) 
-	{//empty
+	public void setPropertyValue(
+			String key,
+			Object objectKey,
+			Object objectValue) {
+		// empty
 	}
 
-	public Class getPropertyClass(String key) 
-	{
+	public Class getPropertyClass(String key) {
 		Class clazz = String.class;
 		return clazz;
 	}
