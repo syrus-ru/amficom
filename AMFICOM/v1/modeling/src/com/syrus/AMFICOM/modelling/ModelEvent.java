a77e695c9156964f3d80c44b4aef00ab57eccf8e
Alexey Stratonnikov <alexsaa@gmail.com>
2005-08-08 17:34:38 +0400

    FIX(DEBUG): ModelEvent.addEventsByString: added prevLinLen parameter to link lin. parts of trace events

--- AMFICOM/v1/modeling/src/com/syrus/AMFICOM/modelling/ModelEvent.java
+++ AMFICOM/v1/modeling/src/com/syrus/AMFICOM/modelling/ModelEvent.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ModelEvent.java,v 1.3 2005/08/04 18:01:45 saa Exp $
+ * $Id: ModelEvent.java,v 1.4 2005/08/08 13:34:38 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -14,7 +14,7 @@ import java.util.regex.Pattern;
 
 /**
  * @author $Author: saa $
- * @version $Revision: 1.3 $, $Date: 2005/08/04 18:01:45 $
+ * @version $Revision: 1.4 $, $Date: 2005/08/08 13:34:38 $
  * @module
  */
 public class ModelEvent {
@@ -59,27 +59,32 @@ public class ModelEvent {
 	/**
 	 * Читает из моего убогого формата (сейчас нет ни времени, ни необходимости
 	 *  придумывать нормальный формат - он используется только для отладки).
-	 * Создает ModelEvent, добавляет их в данный на входе список
 	 * //saa
-	 * @param s строка недокументированного формата.
+	 * Создает ModelEvent, добавляет их в данный на входе список,
+	 * возвращая длину, которую надо отнести к след. лин. соб.
+	 * @param s строка-описатель события (недокументировано)
+	 * @param prevLinLen перенесенная длина с предыдущего участка
+	 * @return перенесенная длина на следующий участок
 	 */
 	@Deprecated
-	public static void addEventsByString(List<ModelEvent> events, String s) {
+	public static double addEventsByString(List<ModelEvent> events, String s,
+			double prevLinLen) {
 		// LINEAR ?
 		Pattern patLinear = Pattern.compile("[LU] ([-0-9.Ee+]+) ([-0-9.Ee+]+)");
 		Matcher m = patLinear.matcher(s);
 		if (m.matches()) {
-			events.add(createLinear(Double.parseDouble(m.group(1)),
+			events.add(createLinear(prevLinLen + Double.parseDouble(m.group(1)),
 					Double.parseDouble(m.group(2))));
-			return;
+			return 0.0;
 		}
+		// not linear: immediately insert lin. region of prevLinLen length 
+		events.add(createLinear(prevLinLen, 1e-8)); // XXX: 1e-8: затухание 0.0 не допустимо 
 		// SPLICE ? 
 		Pattern patSplice = Pattern.compile("S ([-0-9.Ee+]+) ([-0-9.Ee+]+)");
 		m = patSplice.matcher(s);
 		if (m.matches()) {
 			events.add(createSlice(Double.parseDouble(m.group(2))));
-			events.add(createLinear(Double.parseDouble(m.group(1)), 1e-8)); // XXX: не учитываем затухание, которое должно быть отнесено к след. участку 
-			return;
+			return Double.parseDouble(m.group(1));
 		}
 		// CONNECTOR ?
 		Pattern patConn = Pattern.compile("C ([-0-9.Ee+]+) ([-0-9.Ee+]+) ([-0-9.Ee+]+)");
@@ -87,8 +92,7 @@ public class ModelEvent {
 		if (m.matches()) {
 			events.add(createReflective(Double.parseDouble(m.group(2)),
 					Double.parseDouble(m.group(3))));
-			events.add(createLinear(Double.parseDouble(m.group(1)), 1e-8)); // XXX: не учитываем затухание, которое должно быть отнесено к след. участку
-			return;
+			return Double.parseDouble(m.group(1));
 		}
 		// parse error
 		throw new IllegalArgumentException(
