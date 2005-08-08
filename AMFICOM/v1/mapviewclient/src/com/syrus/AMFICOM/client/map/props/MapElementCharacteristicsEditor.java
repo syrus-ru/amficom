b4a05b5ea62681d8a5f1b8df8a71db2a623e11d4
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-08 14:31:40 +0400

    ADD: can display characteristics not of MapElement only, but also of SiteNodeType and PhysicalLinkType

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/props/MapElementCharacteristicsEditor.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/props/MapElementCharacteristicsEditor.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MapElementCharacteristicsEditor.java,v 1.14 2005/07/18 09:39:29 krupenn Exp $
+ * $Id: MapElementCharacteristicsEditor.java,v 1.15 2005/08/08 10:31:40 krupenn Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -14,17 +14,19 @@ import com.syrus.AMFICOM.client.UI.CharacteristicsPanel;
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.corba.IdlCharacteristicTypePackage.CharacteristicTypeSort;
 import com.syrus.AMFICOM.map.MapElement;
+import com.syrus.AMFICOM.map.PhysicalLinkType;
+import com.syrus.AMFICOM.map.SiteNodeType;
 import com.syrus.AMFICOM.mapview.Selection;
 import com.syrus.util.Log;
 
 /**
  * @author $Author: krupenn $
- * @version $Revision: 1.14 $, $Date: 2005/07/18 09:39:29 $
+ * @version $Revision: 1.15 $, $Date: 2005/08/08 10:31:40 $
  * @module mapviewclient_v1
  */
 
 public class MapElementCharacteristicsEditor extends CharacteristicsPanel {
-	protected MapElement mapElement;
+	protected Object object;
 
 	protected MapElementCharacteristicsEditor() {
 		super();
@@ -36,45 +38,58 @@ public class MapElementCharacteristicsEditor extends CharacteristicsPanel {
 	}
 
 	public Object getObject() {
-		return this.mapElement;
+		return this.object;
 	}
 
-	public void setObject(Object or) {
-		this.mapElement = (MapElement) or;
+	public void setObject(Object object) {
+		this.object = object;
 		super.clear();
-		
-		if (this.mapElement != null
-				&& !(this.mapElement instanceof Selection)) {
-				super.setTypeSortMapping(
-						CharacteristicTypeSort.CHARACTERISTICTYPESORT_VISUAL,
-						this.mapElement,
-						this.mapElement.getId(), 
-						true);
+
+		if(object instanceof MapElement) {
+			MapElement mapElement = (MapElement) object;
+			
+			if (mapElement != null
+					&& !(mapElement instanceof Selection)) {
+					super.setTypeSortMapping(
+							CharacteristicTypeSort.CHARACTERISTICTYPESORT_VISUAL,
+							mapElement,
+							mapElement.getId(), 
+							true);
+				try {
+					super.addCharacteristics(mapElement.getCharacteristics(), mapElement.getId());
+				} catch(ApplicationException e) {
+					Log.debugException(e, Level.WARNING);
+				}
+			}
+			else
+				super.showNoSelection();
+		}
+		else if(object instanceof SiteNodeType) {
+			SiteNodeType siteNodeType = (SiteNodeType)object;
+			super.setTypeSortMapping(
+					CharacteristicTypeSort.CHARACTERISTICTYPESORT_VISUAL,
+					siteNodeType,
+					siteNodeType.getId(), 
+					true);
 			try {
-				super.addCharacteristics(this.mapElement.getCharacteristics(), this.mapElement.getId());
+				super.addCharacteristics(siteNodeType.getCharacteristics(), siteNodeType.getId());
 			} catch(ApplicationException e) {
 				Log.debugException(e, Level.WARNING);
 			}
 		}
-		else
-			super.showNoSelection();
+		else if(object instanceof PhysicalLinkType) {
+			PhysicalLinkType physicalLinkType = (PhysicalLinkType)object;
+			super.setTypeSortMapping(
+					CharacteristicTypeSort.CHARACTERISTICTYPESORT_VISUAL,
+					physicalLinkType,
+					physicalLinkType.getId(), 
+					true);
+			try {
+				super.addCharacteristics(physicalLinkType.getCharacteristics(), physicalLinkType.getId());
+			} catch(ApplicationException e) {
+				Log.debugException(e, Level.WARNING);
+			}
+		}
 	}
-
-//	void setCharacteristicValue(Collection characteristics, String name,
-//			String value) {
-//		for (Iterator it = characteristics.iterator(); it.hasNext();) {
-//			Characteristic ch = (Characteristic) it.next();
-//			if (ch.getName().equals(name)) {
-//				if(ch.getCharacterizableId().equals(this.mapElement.getId()))
-//					ch.setValue(value);
-//				else {
-//					PhysicalLinkController controller = (PhysicalLinkController )PhysicalLinkController.getInstance();
-//					if(ch.getType().getCodename().equals(controller.ATTRIBUTE_COLOR))
-//						controller.setColor(this.mapElement, null);
-//				}
-//				break;
-//			}
-//		}
-//	}
 
 }
