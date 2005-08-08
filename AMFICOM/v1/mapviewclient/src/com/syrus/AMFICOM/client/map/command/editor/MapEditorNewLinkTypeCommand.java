b17e599de97e9e0d6bc1cc1f8f8df6286e223c36
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-08 14:14:05 +0400

    BUGFIX: use correct codename (which defaults to getID().toString()) and name

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/command/editor/MapEditorNewLinkTypeCommand.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/command/editor/MapEditorNewLinkTypeCommand.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapEditorNewLinkTypeCommand.java,v 1.2 2005/08/03 15:33:01 krupenn Exp $
+ * $Id: MapEditorNewLinkTypeCommand.java,v 1.3 2005/08/08 10:14:05 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -48,10 +48,11 @@ public class MapEditorNewLinkTypeCommand extends AbstractCommand {
 					new IntDimension(1, 1),
 					true,
 					MapLibraryController.getDefaultMapLibrary());
+			physicalLinkType.setCodename(physicalLinkType.getId().toString());
 			PhysicalLinkTypeEditor physicalLinkTypeEditor = new PhysicalLinkTypeEditor();
 			physicalLinkTypeEditor.setNetMapViewer(mapFrame.getMapViewer());
 			if(EditorDialog.showEditorDialog(
-					LangModelMap.getString("type"),
+					LangModelMap.getString("physicallinktype"),
 					physicalLinkType,
 					physicalLinkTypeEditor) ) {
 				StorableObjectPool.flush(physicalLinkType, LoginManager.getUserId(), true);
