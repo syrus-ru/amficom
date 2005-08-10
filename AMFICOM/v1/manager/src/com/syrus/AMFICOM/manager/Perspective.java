e5865de3d8d2928c79f43181762ecc0302512626
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-08-10 18:01:51 +0400

    ADD perspective

---
+++ AMFICOM/v1/manager/src/com/syrus/AMFICOM/manager/Perspective.java
@@ -0,0 +1,23 @@
+/*-
+* $Id: Perspective.java,v 1.1 2005/08/10 14:01:51 bob Exp $
+*
+* Copyright ¿ 2005 Syrus Systems.
+* Dept. of Science & Technology.
+* Project: AMFICOM.
+*/
+
+package com.syrus.AMFICOM.manager;
+
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/08/10 14:01:51 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module manager
+ */
+public interface Perspective {
+
+	// nothing ?
+	
+}
+
