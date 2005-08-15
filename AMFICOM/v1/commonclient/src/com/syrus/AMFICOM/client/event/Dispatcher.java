8b121e41acd7eb1df577969b3291f83ff90a52ed
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-08-15 12:55:35 +0400

    new Dispatcher realization

--- AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/client/event/Dispatcher.java
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/client/event/Dispatcher.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: Dispatcher.java,v 1.11 2005/08/02 13:03:22 arseniy Exp $
+ * $Id: Dispatcher.java,v 1.12 2005/08/15 08:55:35 bob Exp $
  *
  * Copyright ї 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -10,20 +10,19 @@ package com.syrus.AMFICOM.client.event;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 
 import com.syrus.util.Log;
 
 /**
  * 
- * @version $Revision: 1.11 $, $Date: 2005/08/02 13:03:22 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.12 $, $Date: 2005/08/15 08:55:35 $
+ * @author $Author: bob $
  * @author Kholshin Stanislav
  * @author Vladimir Dolzhenko
  * @module commonclient
@@ -42,36 +41,32 @@ public class Dispatcher {
 				+ ", listener: " + listener.getClass().getName(), Log.DEBUGLEVEL10);
 		List<PropertyChangeListener> listeners = this.events.get(propertyName);
 		if (listeners == null) {
-			listeners = new LinkedList<PropertyChangeListener>();
+			listeners = new CopyOnWriteArrayList<PropertyChangeListener>();
 			this.events.put(propertyName, listeners);
 		}
 
-		synchronized (listeners) {
-			if (!listeners.contains(listener)) {
-				listeners.add(listener);
-			} else {
-				Log.debugMessage("Dispatcher.addPropertyChangeListener | already added listener: " + listener.getClass().getName(),
-						Level.WARNING);
-			}
+		if (!listeners.contains(listener)) {
+			listeners.add(listener);
+		} else {
+			Log.debugMessage("Dispatcher.addPropertyChangeListener | already added listener: " + listener.getClass().getName(),
+					Level.WARNING);
 		}
 	}
 
 	// унрегистрация убирает связь подписчика с определенным событием
-	public synchronized void removePropertyChangeListener(final String propertyName, final PropertyChangeListener listener) {
+	public void removePropertyChangeListener(final String propertyName, final PropertyChangeListener listener) {
 		final List<PropertyChangeListener> listeners = this.events.get(propertyName);
 		Log.debugMessage("Dispatcher.removePropertyChangeListener | propertyName:" + propertyName
 				+ ", listener: " + listener.getClass().getName(), Log.DEBUGLEVEL10);
 		if (listeners != null) {
-			synchronized (listeners) {
-				if (!listeners.remove(listener)) {
-					Log.debugMessage("Dispatcher.removePropertyChangeListener | there is no added listener: "
-							+ listener.getClass().getName(), Level.WARNING);
-				}
+			if (!listeners.remove(listener)) {
+				Log.debugMessage("Dispatcher.removePropertyChangeListener | there is no added listener: "
+						+ listener.getClass().getName(), Level.WARNING);
 			}
 		}
 	}
 
-	public synchronized void firePropertyChange(final PropertyChangeEvent event) {
+	public void firePropertyChange(final PropertyChangeEvent event) {
 		this.firePropertyChange(event, false);
 	}
 
@@ -81,37 +76,32 @@ public class Dispatcher {
 			final String propertyName = (String) iterator.next();
 			List<PropertyChangeListener> listeners = this.events.get(propertyName);
 			Log.debugMessage("Dispatcher.printListeners | propertyName: " + propertyName, Level.FINEST);
-			synchronized (listeners) {
-				for (PropertyChangeListener changeListener : listeners) {
-					Log.debugMessage("Dispatcher.printListeners | changeListener : " + changeListener.getClass().getName(), Level.FINEST);
-				}
+			for (PropertyChangeListener changeListener : listeners) {
+				Log.debugMessage("Dispatcher.printListeners | changeListener : " + changeListener.getClass().getName(), Level.FINEST);
 			}
 		}
 	}
 
-	public synchronized void firePropertyChange(final PropertyChangeEvent event, final boolean canSendToSelf) {
+	public void firePropertyChange(final PropertyChangeEvent event, final boolean canSendToSelf) {
 		final String propertyName = event.getPropertyName();
 
 		final List<PropertyChangeListener> listeners = this.events.get(propertyName);
 
 		if (listeners != null && !listeners.isEmpty()) {
 			final Object source = event.getSource();
-			final List<PropertyChangeListener> newListeners = new ArrayList<PropertyChangeListener>(listeners);
-			synchronized (newListeners) {
-				for (final PropertyChangeListener listener : newListeners) {
-					/*
-					 * yeah, really compare references, skip sending message to source
-					 */
-					if (!canSendToSelf && listener == source) {
-						Log.debugMessage("Dispatcher.firePropertyChange | propertyName: " + propertyName
-								+ ", listener == source (" + source.getClass().getName() + ")", Log.DEBUGLEVEL10);
-						continue;
-					}
-
+			for (final PropertyChangeListener listener : listeners) {
+				/*
+				 * yeah, really compare references, skip sending message to source
+				 */
+				if (!canSendToSelf && listener == source) {
 					Log.debugMessage("Dispatcher.firePropertyChange | propertyName: " + propertyName
-							+ ", listener: " + listener.getClass().getName(), Log.DEBUGLEVEL10);
-					listener.propertyChange(event);
+							+ ", listener == source (" + source.getClass().getName() + ")", Log.DEBUGLEVEL10);
+					continue;
 				}
+
+				Log.debugMessage("Dispatcher.firePropertyChange | propertyName: " + propertyName
+						+ ", listener: " + listener.getClass().getName(), Log.DEBUGLEVEL10);
+				listener.propertyChange(event);
 			}
 		} else {
 			Log.debugMessage("Dispatcher.firePropertyChange | listener for '" + propertyName
