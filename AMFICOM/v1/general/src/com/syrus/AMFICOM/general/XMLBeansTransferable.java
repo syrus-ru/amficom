9e6a868d3f255df4798d31063d3db38e0163adf4
Arseniy Tashoyan <tashoyan@gmail.com>
2005-08-16 14:52:03 +0400

    ADD: Argument 'importType' to method 'fromXMLTransferable'

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/XMLBeansTransferable.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/XMLBeansTransferable.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: XMLBeansTransferable.java,v 1.2 2005/08/12 12:26:28 arseniy Exp $
+ * $Id: XMLBeansTransferable.java,v 1.3 2005/08/16 10:52:03 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -10,13 +10,13 @@ package com.syrus.AMFICOM.general;
 import org.apache.xmlbeans.XmlObject;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/08/12 12:26:28 $
+ * @version $Revision: 1.3 $, $Date: 2005/08/16 10:52:03 $
  * @author $Author: arseniy $
  * @module general
  */
 public interface XMLBeansTransferable {
 
-	void fromXMLTransferable(final XmlObject xmlObject, final ClonedIdsPool clonedIdsPool) throws ApplicationException;
+	void fromXMLTransferable(final XmlObject xmlObject, final ClonedIdsPool clonedIdsPool, final String importType) throws ApplicationException;
 
 	XmlObject getXMLTransferable();
 }
