827946d2c46845b344a16c018fe1191d391945e9
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-18 17:53:51 +0400

    ADD: new popup menu for tree panel - allows for 'cloning' SiteNodeType and PhysicalLinkType. sends map event COPY_TYPE which should be caught by mapFrame COMPATIBILITY: single propertyName "mapevent", distinguish map event type by new field mapEventType

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapViewTreePanel.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/MapViewTreePanel.java
@@ -1,6 +1,8 @@
 package com.syrus.AMFICOM.client.map.ui;
 
 import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.Collection;
@@ -10,8 +12,10 @@ import java.util.List;
 import java.util.logging.Level;
 
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.SwingUtilities;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 
@@ -24,6 +28,8 @@ import com.syrus.AMFICOM.client.model.ApplicationContext;
 import com.syrus.AMFICOM.client.resource.LangModelGeneral;
 import com.syrus.AMFICOM.logic.Item;
 import com.syrus.AMFICOM.logic.ItemTreeModel;
+import com.syrus.AMFICOM.map.PhysicalLinkType;
+import com.syrus.AMFICOM.map.SiteNodeType;
 import com.syrus.AMFICOM.mapview.MapView;
 import com.syrus.util.Log;
 
@@ -69,19 +75,28 @@ public final class MapViewTreePanel extends JPanel
 		this.scroll.getViewport().add(this.tree);
 		this.tree.addTreeSelectionListener(new MapViewTreeSelectionListener(this));
 		this.tree.getSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
+		this.tree.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if(SwingUtilities.isRightMouseButton(e)) {
+					TreePath treePath = MapViewTreePanel.this.tree.getPathForLocation(e.getX(), e.getY());
+					Item item = (Item )treePath.getLastPathComponent();
+					Object object = item.getObject();
+					if(object instanceof SiteNodeType
+							|| object instanceof PhysicalLinkType) {
+						MapViewTreePanel.this.tree.setSelectionPath(treePath);
+						JPopupMenu popupMenu = new TreePopupMenu(object, MapViewTreePanel.this.aContext);
+						popupMenu.show(MapViewTreePanel.this.tree, e.getX(), e.getY());
+					}
+				}
+			}
+		});
 	}
 
 	public void setContext(ApplicationContext aContext) {
 		if(this.aContext != null)
 			if(this.aContext.getDispatcher() != null) {
 				Dispatcher disp = this.aContext.getDispatcher();
-				disp.removePropertyChangeListener(MapEvent.LIBRARY_SET_CHANGED, this);
-				disp.removePropertyChangeListener(MapEvent.SELECTION_CHANGED, this);
-				disp.removePropertyChangeListener(MapEvent.MAP_SELECTED, this);
-				disp.removePropertyChangeListener(MapEvent.MAP_CHANGED, this);
-				disp.removePropertyChangeListener(MapEvent.MAP_VIEW_CHANGED, this);
-				disp.removePropertyChangeListener(MapEvent.MAP_VIEW_SELECTED, this);
-				disp.removePropertyChangeListener(MapEvent.MAP_VIEW_CLOSED, this);
+				disp.removePropertyChangeListener(MapEvent.MAP_EVENT_TYPE, this);
 			}
 		this.aContext = aContext;
 		if(aContext == null)
@@ -90,13 +105,7 @@ public final class MapViewTreePanel extends JPanel
 		Dispatcher disp = aContext.getDispatcher();
 		if(disp == null)
 			return;
-		disp.addPropertyChangeListener(MapEvent.LIBRARY_SET_CHANGED, this);
-		disp.addPropertyChangeListener(MapEvent.SELECTION_CHANGED, this);
-		disp.addPropertyChangeListener(MapEvent.MAP_SELECTED, this);
-		disp.addPropertyChangeListener(MapEvent.MAP_CHANGED, this);
-		disp.addPropertyChangeListener(MapEvent.MAP_VIEW_CHANGED, this);
-		disp.addPropertyChangeListener(MapEvent.MAP_VIEW_SELECTED, this);
-		disp.addPropertyChangeListener(MapEvent.MAP_VIEW_CLOSED, this);
+		disp.addPropertyChangeListener(MapEvent.MAP_EVENT_TYPE, this);
 	}
 
 	public void propertyChange(PropertyChangeEvent pce) {
@@ -106,39 +115,44 @@ public final class MapViewTreePanel extends JPanel
 		}
 		this.performProcessing = false;
 
-		if(	pce.getPropertyName().equals(MapEvent.MAP_VIEW_CLOSED)) {
-			updateTree(null);
-		}
-		if(	pce.getPropertyName().equals(MapEvent.MAP_VIEW_SELECTED)) {
-			MapView mapView = (MapView )pce.getSource();
-			if(this.model == null 
-					|| this.mapView == null 
-					|| !this.mapView.equals(mapView)) {
+		if(pce.getPropertyName().equals(MapEvent.MAP_EVENT_TYPE)) {
+			MapEvent mapEvent = (MapEvent )pce;
+			String mapEventType = mapEvent.getMapEventType();
+
+			if(	mapEventType.equals(MapEvent.MAP_VIEW_CLOSED)) {
+				updateTree(null);
+			}
+			if(	mapEventType.equals(MapEvent.MAP_VIEW_SELECTED)) {
+				MapView mapView = (MapView )pce.getNewValue();
+				if(this.model == null 
+						|| this.mapView == null 
+						|| !this.mapView.equals(mapView)) {
+					updateTree(mapView);
+				}
+			}
+			if(	mapEventType.equals(MapEvent.MAP_CHANGED)) {
+				updateTree(this.mapView);
+			}
+			if(	mapEventType.equals(MapEvent.MAP_SELECTED)) {
+				updateTree(this.mapView);
+			}
+			if(	mapEventType.equals(MapEvent.LIBRARY_SET_CHANGED)) {
+				updateTree(this.mapView);
+			}
+			if(	mapEventType.equals(MapEvent.MAP_VIEW_CHANGED)) {
+				MapView mapView = (MapView )pce.getNewValue();
 				updateTree(mapView);
 			}
-		}
-		if(	pce.getPropertyName().equals(MapEvent.MAP_CHANGED)) {
-			updateTree(this.mapView);
-		}
-		if(	pce.getPropertyName().equals(MapEvent.MAP_SELECTED)) {
-			updateTree(this.mapView);
-		}
-		if(	pce.getPropertyName().equals(MapEvent.LIBRARY_SET_CHANGED)) {
-			updateTree(this.mapView);
-		}
-		if(	pce.getPropertyName().equals(MapEvent.MAP_VIEW_CHANGED)) {
-			MapView mapView = (MapView )pce.getSource();
-			updateTree(mapView);
-		}
-		if(pce.getPropertyName().equals(MapEvent.SELECTION_CHANGED)) {
-			Collection selection = (Collection )pce.getNewValue();
-			Collection items = this.model.findNodes(this.root, selection);
-			this.tree.getSelectionModel().clearSelection();
-			for(Iterator iter = items.iterator(); iter.hasNext();) {
-				Item node = (Item )iter.next();
-				TreePath path = new TreePath(this.treeModel.getPathToRoot(node));
-				this.tree.getSelectionModel().addSelectionPath(path);
-				this.tree.scrollPathToVisible(path);
+			if(mapEventType.equals(MapEvent.SELECTION_CHANGED)) {
+				Collection selection = (Collection )pce.getNewValue();
+				Collection items = this.model.findNodes(this.root, selection);
+				this.tree.getSelectionModel().clearSelection();
+				for(Iterator iter = items.iterator(); iter.hasNext();) {
+					Item node = (Item )iter.next();
+					TreePath path = new TreePath(this.treeModel.getPathToRoot(node));
+					this.tree.getSelectionModel().addSelectionPath(path);
+					this.tree.scrollPathToVisible(path);
+				}
 			}
 		}
 
