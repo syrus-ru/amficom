d5a88eec5679bcdd8d86985b0f70e3e5eefdd874
Arseniy Tashoyan <tashoyan@gmail.com>
2005-08-18 13:50:27 +0400

    ADD: 2 jars to EXTCLASSPATH

--- AMFICOM/v1/mshserver/mscharserver.sh
+++ AMFICOM/v1/mshserver/mscharserver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "*** Mscharserver v1.0.0 ``Bobruysk''"
+
 
 JAVA=$JAVA_HOME/bin/java
 LIB=$HOME/lib
@@ -7,11 +9,12 @@ EXTLIB=$HOME/extlib
 
 ORACLECLASSPATH=$ORACLE_HOME/jdbc/lib/ojdbc14_g.jar:$ORACLE_HOME/jdbc/lib/orai18n.jar:$ORACLE_HOME/jdbc/lib/classes12.jar:$ORACLE_HOME/jdbc/lib/nls_charset12.jar
 
-EXTCLASSPATH=$EXTLIB/trove.jar:$EXTLIB/jdom.jar:$EXTLIB/micsys.jar:$EXTLIB/miutil.jar:$EXTLIB/mxj.jar:$EXTLIB/commons-logging.jar:$EXTLIB/mxjgeom.jar:$EXTLIB/miappletsup.jar:$EXTLIB/xercesImpl.jar:$EXTLIB/mxjtabdp.jar:$EXTLIB/xbean.jar:$EXTLIB/mistyles.jar
+EXTCLASSPATH=$EXTLIB/trove.jar:$EXTLIB/jdom.jar:$EXTLIB/micsys.jar:$EXTLIB/miutil.jar:$EXTLIB/mxj.jar:$EXTLIB/commons-logging.jar:$EXTLIB/mxjgeom.jar:$EXTLIB/miappletsup.jar:$EXTLIB/xercesImpl.jar:$EXTLIB/mxjtabdp.jar:$EXTLIB/xbean.jar:$EXTLIB/mistyles.jar:$EXTLIB/dom4j-1.6.jar:$EXTLIB/jaxen-1.1-beta-6.jar
 
 APPCLASSPATH=$LIB/filter.jar:$LIB/mscharserver.jar:$LIB/general.jar:$LIB/administration.jar:$LIB/configuration.jar:$LIB/csbridge.jar:$LIB/leserver_interface.jar:$LIB/map.jar:$LIB/scheme.jar:$LIB/util.jar:$LIB/resource.jar:$LIB/mapview.jar
 
 
+JAVA="$JAVA -agentlib:jdwp=transport=dt_socket,address=8004,server=y,suspend=n"
 $JAVA -Xmx256m -server -ea -classpath $APPCLASSPATH:$ORACLECLASSPATH:$EXTCLASSPATH com.syrus.AMFICOM.mscharserver.MapSchemeAdministrationResourceServer &
 # Expands to the process ID of the most recently executed background (asynchronous) command.
-echo $! > mscharserver.pid
+echo $! >`dirname $0`/mscharserver.pid
