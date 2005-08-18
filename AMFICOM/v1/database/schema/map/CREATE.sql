c7d470cb1001858a9de9d6b8e2247f3784be9ac5
Arseniy Tashoyan <tashoyan@gmail.com>
2005-08-18 13:33:26 +0400

    BUGFIX: Path to scripts

--- AMFICOM/v1/database/schema/map/CREATE.sql
+++ AMFICOM/v1/database/schema/map/CREATE.sql
@@ -1,7 +1,7 @@
--- $Id: CREATE.sql,v 1.6 2005/08/04 15:22:26 max Exp $
+-- $Id: CREATE.sql,v 1.7 2005/08/18 09:33:26 arseniy Exp $
 
 REM types
-@@type/CREATE;
+@@map/type/CREATE;
 
 REM objects
-@@object/CREATE;
+@@map/object/CREATE;
--- AMFICOM/v1/database/schema/map/DROP.sql
+++ AMFICOM/v1/database/schema/map/DROP.sql
@@ -1,4 +1,4 @@
--- $Id: DROP.sql,v 1.6 2005/08/04 15:22:26 max Exp $
+-- $Id: DROP.sql,v 1.7 2005/08/18 09:33:26 arseniy Exp $
 
-@@object/DROP;
-@@type/DROP;
+@@map/object/DROP;
+@@map/type/DROP;
