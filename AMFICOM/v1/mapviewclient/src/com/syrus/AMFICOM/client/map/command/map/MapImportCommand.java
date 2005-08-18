88df1cf2275ebedbb665ccb30cf40c571ae59ba5
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-18 18:08:53 +0400

    ADD: set user.dir system property for make images being imported to imported objects ADD: "Wait please" blocking message with progress bar

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/command/map/MapImportCommand.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/command/map/MapImportCommand.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MapImportCommand.java,v 1.38 2005/08/17 14:14:18 arseniy Exp $
+ * $Id: MapImportCommand.java,v 1.39 2005/08/18 14:08:53 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -23,6 +23,7 @@ import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
 
+import com.syrus.AMFICOM.client.UI.CommonUIUtilities;
 import com.syrus.AMFICOM.client.event.Dispatcher;
 import com.syrus.AMFICOM.client.event.MapEvent;
 import com.syrus.AMFICOM.client.event.StatusMessageEvent;
@@ -36,6 +37,7 @@ import com.syrus.AMFICOM.client.map.controllers.NodeTypeController;
 import com.syrus.AMFICOM.client.map.ui.MapFrame;
 import com.syrus.AMFICOM.client.model.ApplicationContext;
 import com.syrus.AMFICOM.client.model.Command;
+import com.syrus.AMFICOM.client.resource.LangModelMap;
 import com.syrus.AMFICOM.general.ClonedIdsPool;
 import com.syrus.AMFICOM.general.CreateObjectException;
 import com.syrus.AMFICOM.general.DatabaseException;
@@ -53,8 +55,8 @@ import com.syrus.util.Log;
  * самого окна карты. При этом в азголовке окна отображается информация о том,
  * что активной карты нет, и карта центрируется по умолчанию
  * 
- * @author $Author: arseniy $
- * @version $Revision: 1.38 $, $Date: 2005/08/17 14:14:18 $
+ * @author $Author: krupenn $
+ * @version $Revision: 1.39 $, $Date: 2005/08/18 14:08:53 $
  * @module mapviewclient
  */
 public class MapImportCommand extends ImportCommand {
@@ -80,68 +82,81 @@ public class MapImportCommand extends ImportCommand {
 		if(this.mapFrame == null)
 			return;
 
-		try {
-			Map map = null;
-			Log.debugMessage("Importing map", Level.INFO);
+		Log.debugMessage("Importing map", Level.INFO);
 
-			String fileName = ImportCommand
-					.openFileForReading(MapPropertiesManager.getLastDirectory());
-			if(fileName == null)
-				return;
+		final String fileName = ImportCommand
+				.openFileForReading(MapPropertiesManager.getLastDirectory());
+		if(fileName == null)
+			return;
 
-			File file = new File(fileName);
-			MapPropertiesManager.setLastDirectory(file.getParent());
+		File file = new File(fileName);
+		
+		MapPropertiesManager.setLastDirectory(file.getParent());
 
-			String ext = file.getAbsolutePath().substring(
-					file.getAbsolutePath().lastIndexOf("."));
+		String ext = file.getAbsolutePath().substring(
+				file.getAbsolutePath().lastIndexOf("."));
 
-			if(ext == null) {
-				ext = ".xml";
-			}
-
-			if(ext.equals(".xml")) {
-				map = loadXML(fileName);
-			}
-
-			if(map == null)
-				return;
-
-			StorableObjectPool.putStorableObject(map);
-
-			MapView mv = this.mapFrame.getMapView();
-			this.mapFrame.getMapViewer().getLogicalNetLayer()
-					.getMapViewController().removeSchemes();
-			mv.setMap(map);
-			this.mapFrame.setMapView(mv);
-
-			Dispatcher disp = this.mapFrame.getContext().getDispatcher();
-			if(disp != null) {
-				disp.firePropertyChange(new MapEvent(
-						mv,
-						MapEvent.MAP_VIEW_CHANGED));
-				disp.firePropertyChange(new MapEvent(
-						mv,
-						MapEvent.MAP_VIEW_SELECTED));
-			}
-
-			setResult(Command.RESULT_OK);
-		} catch(MapException e) {
-			this.mapFrame.getContext().getDispatcher().firePropertyChange(new StatusMessageEvent(this, StatusMessageEvent.STATUS_MESSAGE, "Ошибка соединения с картографическими данными"));
-			e.printStackTrace();
-			setResult(Command.RESULT_NO);
-		} catch(DatabaseException e) {
-			e.printStackTrace();
-			setResult(Command.RESULT_NO);
-		} catch(IllegalObjectEntityException e) {
-			e.printStackTrace();
-			setResult(Command.RESULT_NO);
-		} catch(XmlException e) {
-			e.printStackTrace();
-			setResult(Command.RESULT_NO);
-		} catch(IOException e) {
-			e.printStackTrace();
-			setResult(Command.RESULT_NO);
+		if(ext == null) {
+			ext = ".xml";
 		}
+		
+		final String extension = ext;
+
+		CommonUIUtilities.invokeAsynchronously(new Runnable() {
+
+			@SuppressWarnings("synthetic-access")
+			public void run() {
+				try {
+					Map map = null;
+					MapImportCommand.this.aContext.getDispatcher().firePropertyChange(new StatusMessageEvent(this, StatusMessageEvent.STATUS_PROGRESS_BAR, true));
+					if(extension.equals(".xml")) {
+						map = loadXML(fileName);
+					}
+					MapImportCommand.this.aContext.getDispatcher().firePropertyChange(new StatusMessageEvent(this, StatusMessageEvent.STATUS_PROGRESS_BAR, false));
+		
+					if(map == null)
+						return;
+		
+					StorableObjectPool.putStorableObject(map);
+		
+					MapView mv = MapImportCommand.this.mapFrame.getMapView();
+					MapImportCommand.this.mapFrame.getMapViewer().getLogicalNetLayer()
+							.getMapViewController().removeSchemes();
+					mv.setMap(map);
+					MapImportCommand.this.mapFrame.setMapView(mv);
+		
+					Dispatcher disp = MapImportCommand.this.mapFrame.getContext().getDispatcher();
+					if(disp != null) {
+						disp.firePropertyChange(new MapEvent(
+								this,
+								MapEvent.MAP_VIEW_CHANGED,
+								mv));
+						disp.firePropertyChange(new MapEvent(
+								this,
+								MapEvent.MAP_VIEW_SELECTED,
+								mv));
+					}
+		
+					setResult(Command.RESULT_OK);
+				} catch(MapException e) {
+					MapImportCommand.this.mapFrame.getContext().getDispatcher().firePropertyChange(new StatusMessageEvent(this, StatusMessageEvent.STATUS_MESSAGE, "Ошибка соединения с картографическими данными"));
+					e.printStackTrace();
+					setResult(Command.RESULT_NO);
+				} catch(DatabaseException e) {
+					e.printStackTrace();
+					setResult(Command.RESULT_NO);
+				} catch(IllegalObjectEntityException e) {
+					e.printStackTrace();
+					setResult(Command.RESULT_NO);
+				} catch(XmlException e) {
+					e.printStackTrace();
+					setResult(Command.RESULT_NO);
+				} catch(IOException e) {
+					e.printStackTrace();
+					setResult(Command.RESULT_NO);
+				}
+			}
+		}, LangModelMap.getString("Message.Information.ImportingPlsWait"));	
 	}
 
 	protected Map loadXML(String fileName)
@@ -166,6 +181,9 @@ public class MapImportCommand extends ImportCommand {
 		LinkTypeController.getTopologicalLinkTypes();
 		NodeTypeController.getTopologicalNodeTypes();
 
+		String user_dir = System.getProperty("user.dir");
+		System.setProperty("user.dir",  xmlfile.getParent());
+
 		com.syrus.amficom.map.xml.Maps xmlMaps = doc.getMaps();
 		com.syrus.amficom.map.xml.Map[] xmlMapsArray = xmlMaps.getMapArray();
 		for(int i = 0; i < xmlMapsArray.length; i++) {
@@ -188,6 +206,7 @@ public class MapImportCommand extends ImportCommand {
 			// only one map imported
 			break;
 		}
+		System.setProperty("user.dir",  user_dir);
 		return map;
 	}
 
