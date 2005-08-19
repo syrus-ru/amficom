c0fbe66afbed3e02523e96ee4faa17ea937a5124
Arseniy Tashoyan <tashoyan@gmail.com>
2005-08-19 19:55:21 +0400

    misc

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/setup/TestInitialSetup.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/setup/TestInitialSetup.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestInitialSetup.java,v 1.4 2005/06/22 14:45:47 arseniy Exp $
+ * $Id: TestInitialSetup.java,v 1.5 2005/08/19 15:55:21 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -17,12 +17,26 @@ import com.syrus.AMFICOM.administration.TestMCM;
 import com.syrus.AMFICOM.administration.TestServer;
 import com.syrus.AMFICOM.administration.TestServerProcess;
 import com.syrus.AMFICOM.administration.TestSystemUser;
+import com.syrus.AMFICOM.configuration.TestEquipment;
+import com.syrus.AMFICOM.configuration.TestEquipmentType;
+import com.syrus.AMFICOM.configuration.TestKIS;
+import com.syrus.AMFICOM.configuration.TestMeasurementPort;
+import com.syrus.AMFICOM.configuration.TestMeasurementPortType;
+import com.syrus.AMFICOM.configuration.TestMonitoredElement;
+import com.syrus.AMFICOM.configuration.TestPort;
+import com.syrus.AMFICOM.configuration.TestPortType;
+import com.syrus.AMFICOM.configuration.TestTransmissionPath;
+import com.syrus.AMFICOM.configuration.TestTransmissionPathType;
 import com.syrus.AMFICOM.general.DatabaseCommonTest;
 import com.syrus.AMFICOM.general.SQLCommonTest;
 import com.syrus.AMFICOM.general.TestDataType;
+import com.syrus.AMFICOM.general.TestMeasurementUnit;
+import com.syrus.AMFICOM.general.TestParameterTypeEnum;
+import com.syrus.AMFICOM.measurement.TestAnalysisType;
+import com.syrus.AMFICOM.measurement.TestMeasurementType;
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/06/22 14:45:47 $
+ * @version $Revision: 1.5 $, $Date: 2005/08/19 15:55:21 $
  * @author $Author: arseniy $
  * @module test
  */
@@ -41,6 +55,12 @@ public final class TestInitialSetup extends TestCase {
 		//-2.Create data types
 		sqlCommonTest.addTest(new TestDataType("testCreate"));
 
+		//-3.Create measurement units
+		sqlCommonTest.addTest(new TestMeasurementUnit("testCreate"));
+
+		//-4.Create parameter types
+		sqlCommonTest.addTest(new TestParameterTypeEnum("testCreate"));
+
 		final DatabaseCommonTest databaseCommonTest = new DatabaseCommonTest();
 
 		//-3. Create default domain and server
@@ -54,6 +74,21 @@ public final class TestInitialSetup extends TestCase {
 		//-5. Create users for MCM and MCMs itself.
 		databaseCommonTest.addTest(new TestSystemUser("testCreateMCMUsers"));
 		databaseCommonTest.addTest(new TestMCM("testCreateInstance"));
+
+		databaseCommonTest.addTest(new TestEquipmentType("testCreateInstance"));
+		databaseCommonTest.addTest(new TestPortType("testCreateInstance"));
+		databaseCommonTest.addTest(new TestTransmissionPathType("testCreateInstance"));
+		databaseCommonTest.addTest(new TestMeasurementPortType("testCreateInstance"));
+
+		databaseCommonTest.addTest(new TestEquipment("testCreateInstance"));
+		databaseCommonTest.addTest(new TestPort("testCreateInstance"));
+		databaseCommonTest.addTest(new TestTransmissionPath("testCreateInstance"));
+		databaseCommonTest.addTest(new TestKIS("testCreateInstance"));
+		databaseCommonTest.addTest(new TestMeasurementPort("testCreateInstance"));
+		databaseCommonTest.addTest(new TestMonitoredElement("testCreateInstance"));
+
+		databaseCommonTest.addTestSuite(TestMeasurementType.class);
+		databaseCommonTest.addTestSuite(TestAnalysisType.class);
 
 		//- Create test suite contains all operations
 		TestSuite testSuite = new TestSuite();
