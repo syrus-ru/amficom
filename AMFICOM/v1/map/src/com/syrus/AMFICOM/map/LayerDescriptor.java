9303b2b83eb1c3f8bb5904f15471f4399938a315
Maxim Selivanov <max.selivanov@gmail.com>
2005-08-22 10:17:27 +0400

    COSMETIC: renamed field pathName to filePathName

--- AMFICOM/v1/map/src/com/syrus/AMFICOM/map/LayerDescriptor.java
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/map/LayerDescriptor.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: LayerDescriptor.java,v 1.2 2005/08/08 11:35:11 arseniy Exp $
+ * $Id: LayerDescriptor.java,v 1.3 2005/08/22 06:17:27 max Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -12,34 +12,34 @@ import com.syrus.AMFICOM.map.corba.IdlLayerDescriptor;
 /**
  * 
  * @author Maxim Selivanov
- * @author $Author: arseniy $
- * @version $Revision: 1.2 $, $Date: 2005/08/08 11:35:11 $
+ * @author $Author: max $
+ * @version $Revision: 1.3 $, $Date: 2005/08/22 06:17:27 $
  * @module map
  */
 
-public class LayerDescriptor {
+public class LayerDescriptor implements MapFileDescriptor {
 	
 	private String fileName;
-	private String pathName;
+	private String filePathName;
 	private long length;
 	private long lastModified;
 	
 	public LayerDescriptor(String fileName, String pathName, long sizeOfFile, long lastModified) {
 		this.fileName = fileName;
-		this.pathName = pathName;
+		this.filePathName = pathName;
 		this.length = sizeOfFile;
 		this.lastModified = lastModified;
 	}
 	
 	public LayerDescriptor(IdlLayerDescriptor descriptor) {
 		this.fileName = descriptor.fileName;
-		this.pathName = descriptor.pathName;
+		this.filePathName = descriptor.pathName;
 		this.length = descriptor.length;
 		this.lastModified = descriptor.lastModified;
 	}
 
 	public IdlLayerDescriptor getTransferable() {
-		return new IdlLayerDescriptor(this.fileName, this.pathName, this.length, this.lastModified);
+		return new IdlLayerDescriptor(this.fileName, this.filePathName, this.length, this.lastModified);
 	}
 
 	public String getFileName() {
@@ -50,8 +50,8 @@ public class LayerDescriptor {
 		return this.lastModified;
 	}
 
-	public String getPathName() {
-		return this.pathName;
+	public String getFilePathName() {
+		return this.filePathName;
 	}
 
 	public long getLength() {
