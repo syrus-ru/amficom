cee1c6a7318eeb2c59986f8b508257d81a98b4fd
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-22 20:03:03 +0400

    condition wrapper

---
+++ AMFICOM/v1/mapclient/src/com/syrus/AMFICOM/client/map/CollectorConditionWrapper.java
@@ -0,0 +1,37 @@
+/**
+ * $Id: CollectorConditionWrapper.java,v 1.1 2005/08/22 16:03:03 krupenn Exp $
+ *
+ * Syrus Systems
+ * Научно-технический центр
+ * Проект: АМФИКОМ
+ */
+package com.syrus.AMFICOM.client.map;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.syrus.AMFICOM.general.ConditionWrapper;
+import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.StorableObjectWrapper;
+import com.syrus.AMFICOM.newFilter.ConditionKey;
+
+public class CollectorConditionWrapper implements ConditionWrapper {
+	
+	private final static short entityCode = ObjectEntities.COLLECTOR_CODE;
+	
+	private final static String NAME = "Entity name";
+		
+	private static Collection<ConditionKey> keys = new ArrayList<ConditionKey>();
+	
+	static {
+		keys.add(new ConditionKey(StorableObjectWrapper.COLUMN_NAME, NAME, ConditionWrapper.STRING));
+	}
+	
+	public Collection<ConditionKey> getKeys() {
+		return keys;
+	}
+	
+	public short getEntityCode() {
+		return entityCode;
+	}
+}
---
+++ AMFICOM/v1/mapclient/src/com/syrus/AMFICOM/client/map/SiteNodeConditionWrapper.java
@@ -0,0 +1,37 @@
+/**
+ * $Id: SiteNodeConditionWrapper.java,v 1.1 2005/08/22 16:03:03 krupenn Exp $
+ *
+ * Syrus Systems
+ * Научно-технический центр
+ * Проект: АМФИКОМ
+ */
+package com.syrus.AMFICOM.client.map;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.syrus.AMFICOM.general.ConditionWrapper;
+import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.StorableObjectWrapper;
+import com.syrus.AMFICOM.newFilter.ConditionKey;
+
+public class SiteNodeConditionWrapper implements ConditionWrapper {
+	
+	private final static short entityCode = ObjectEntities.SITENODE_CODE;
+	
+	private final static String NAME = "Entity name";
+		
+	private static Collection<ConditionKey> keys = new ArrayList<ConditionKey>();
+	
+	static {
+		keys.add(new ConditionKey(StorableObjectWrapper.COLUMN_NAME, NAME, ConditionWrapper.STRING));
+	}
+	
+	public Collection<ConditionKey> getKeys() {
+		return keys;
+	}
+	
+	public short getEntityCode() {
+		return entityCode;
+	}
+}
