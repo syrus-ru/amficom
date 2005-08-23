8427bd6f79fcfdefe603baa466aa43cfc7189deb
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-23 14:06:05 +0400

    ADD: find spatial objects by layer/subtext and layer/visibleBounds

--- AMFICOM/v1/spatialfx/src/com/syrus/AMFICOM/client/map/objectfx/OfxImageLoader.java
+++ AMFICOM/v1/spatialfx/src/com/syrus/AMFICOM/client/map/objectfx/OfxImageLoader.java
@@ -1,5 +1,5 @@
 /**
- * $Id: OfxImageLoader.java,v 1.2 2005/06/20 15:38:43 krupenn Exp $
+ * $Id: OfxImageLoader.java,v 1.3 2005/08/23 10:06:05 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -11,18 +11,20 @@
 package com.syrus.AMFICOM.client.map.objectfx;
 
 import java.awt.Image;
+import java.awt.geom.Rectangle2D;
 import java.util.Enumeration;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Vector;
 
+import com.ofx.geometry.SxRectangle;
 import com.ofx.query.SxQueryResultInterface;
 import com.ofx.repository.SxSpatialObject;
 import com.syrus.AMFICOM.client.map.MapConnection;
 import com.syrus.AMFICOM.client.map.MapConnectionException;
 import com.syrus.AMFICOM.client.map.MapDataException;
 import com.syrus.AMFICOM.client.map.MapImageLoader;
+import com.syrus.AMFICOM.client.map.SpatialLayer;
+import com.syrus.AMFICOM.client.map.SpatialObject;
 import com.syrus.AMFICOM.map.TopologicalImageQuery;
 
 /**
@@ -32,89 +34,72 @@ import com.syrus.AMFICOM.map.TopologicalImageQuery;
  * 
  * 
  * 
- * @version $Revision: 1.2 $, $Date: 2005/06/20 15:38:43 $
+ * @version $Revision: 1.3 $, $Date: 2005/08/23 10:06:05 $
  * @author $Author: krupenn $
  * @module spatialfx_v1
  */
-public class OfxImageLoader implements MapImageLoader 
-{
+public class OfxImageLoader implements MapImageLoader {
 	
 	private final OfxConnection mapConnection;
 
 	public OfxImageLoader(OfxConnection mapConnection) {
 		this.mapConnection = mapConnection;
 	}
-
-	public List findSpatialObjects(String searchText)
-	{
+/*
+	public List findSpatialObjects(String searchText) {
 		List found = new LinkedList();
 
 		Vector vector;
-		SxQueryResultInterface objects;
 		String spatialClassName;
 		
 		String searchTextLowCase = searchText.toLowerCase();
-		String sampleLowCase;
 		
-		try
-		{
+		try {
 			vector = this.mapConnection.getSxMapViewer().getForegroundClasses();
-			for(Iterator it = vector.iterator(); it.hasNext();)
-			{
+			for(Iterator it = vector.iterator(); it.hasNext();) {
 				spatialClassName = (String )it.next();
-
-				objects = this.mapConnection.getSxMapViewer().getQuery().getObjects(spatialClassName);
-				for(Enumeration en = objects.elements(); en.hasMoreElements();)
-				{
-					SxSpatialObject obj = (SxSpatialObject )en.nextElement();
-					sampleLowCase = obj.getLabel().toLowerCase();
-					if(sampleLowCase.indexOf(searchTextLowCase) != -1)
-					{
-						System.out.println("Label " + obj.getLabel());
-						found.add(new OfxSpatialObject(obj, obj.getLabel()));
-					}
-/*
-					else
-					if(obj.getGeometry().toString().indexOf(searchText) != -1)
-					{
-						System.out.println("Geometry " + obj.getGeometry().toString());
-						vec.add(new MySpatialObject(obj, obj.getGeometry().toString()));
-					}
-*/
-				}
+				findInLayer(spatialClassName, searchTextLowCase, found);
 			}
 
 			vector = this.mapConnection.getSxMapViewer().getBackgroundClasses();
-			for(Iterator it = vector.iterator(); it.hasNext();)
-			{
+			for(Iterator it = vector.iterator(); it.hasNext();) {
 				spatialClassName = (String )it.next();
-				objects = this.mapConnection.getSxMapViewer().getQuery().getObjects(spatialClassName);
-				for(Enumeration en = objects.elements(); en.hasMoreElements();)
-				{
-					SxSpatialObject obj = (SxSpatialObject)en.nextElement();
-					sampleLowCase = obj.getLabel().toLowerCase();
-					if(sampleLowCase.indexOf(searchTextLowCase) != -1)
-					{
-						System.out.println("Label " + obj.getLabel());
-						found.add(new OfxSpatialObject(obj, obj.getLabel()));
-					}
-/*
-					else
-					if(obj.getGeometry().toString().indexOf(searchText) != -1)
-					{
-						System.out.println("Geometry " + obj.getGeometry().toString());
-						vec.add(new MySpatialObject(obj, obj.getGeometry().toString()));
-					}
-*/
-				}
+				findInLayer(spatialClassName, searchTextLowCase, found);
 			}
-		}
-		catch(Exception ex)
-		{
-				ex.printStackTrace();
+		} catch(Exception ex) {
+			ex.printStackTrace();
 		}
 
 		return found;
+	}
+*/
+	/**
+	 * @param layerName
+	 * @param searchText
+	 * @param found
+	 */
+	private void findInLayer(String layerName, String searchText, List found) {
+		String sampleLowCase;
+		SxQueryResultInterface objects = this.mapConnection.getSxMapViewer().getQuery().getObjects(layerName);
+		for(Enumeration en = objects.elements(); en.hasMoreElements();) {
+			SxSpatialObject obj = (SxSpatialObject)en.nextElement();
+			sampleLowCase = obj.getLabel().toLowerCase();
+			if(sampleLowCase.indexOf(searchText) != -1) {
+//				System.out.println("Label " + obj.getLabel());
+				found.add(new OfxSpatialObject(obj, obj.getLabel()));
+			}
+		}
+	}
+
+	private void findInLayer(String layerName, SxRectangle bounds, List found) {
+		SxQueryResultInterface objects = this.mapConnection.getSxMapViewer().getQuery().getObjects(layerName);
+		for(Enumeration en = objects.elements(); en.hasMoreElements();) {
+			SxSpatialObject obj = (SxSpatialObject)en.nextElement();
+			if(obj.getBounds().intersects(bounds)) {
+//				System.out.println("Label " + obj.getLabel());
+				found.add(new OfxSpatialObject(obj, obj.getLabel()));
+			}
+		}
 	}
 
 	public Image renderMapImage(TopologicalImageQuery request) throws MapConnectionException, MapDataException {
@@ -128,6 +113,27 @@ public class OfxImageLoader implements MapImageLoader
 	
 	public MapConnection getMapConnection() throws MapConnectionException {
 		return this.mapConnection;
+	}
+
+	public List<SpatialObject> findSpatialObjects(SpatialLayer layer, String searchText) throws MapConnectionException, MapDataException {
+		List<SpatialObject> found = new LinkedList<SpatialObject>();
+		OfxSpatialLayer ofxSpatialLayer = (OfxSpatialLayer)layer;
+		findInLayer(ofxSpatialLayer.className, searchText, found);
+		return found;
+	}
+
+	public List<SpatialObject> findSpatialObjects(SpatialLayer layer, Rectangle2D.Double bounds) throws MapConnectionException, MapDataException {
+		List<SpatialObject> found = new LinkedList<SpatialObject>();
+		OfxSpatialLayer ofxSpatialLayer = (OfxSpatialLayer)layer;
+		findInLayer(
+				ofxSpatialLayer.className, 
+				new SxRectangle(
+						bounds.getX(),
+						bounds.getY(),
+						bounds.getWidth(),
+						bounds.getHeight()), 
+				found);
+		return found;
 	}
 
 
