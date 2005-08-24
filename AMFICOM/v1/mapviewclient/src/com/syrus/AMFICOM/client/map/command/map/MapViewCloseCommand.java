ca76308c8ee4dec800f068575e9661525d4d22dc
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-24 14:20:59 +0400

    ADD: revert closed object

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/command/map/MapViewCloseCommand.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/command/map/MapViewCloseCommand.java
@@ -1,26 +1,27 @@
 /*
- * $Id: MapViewCloseCommand.java,v 1.14 2005/08/17 14:14:18 arseniy Exp $
+ * $Id: MapViewCloseCommand.java,v 1.15 2005/08/24 10:20:59 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
  * Проект: АМФИКОМ
-*/
+ */
 
 package com.syrus.AMFICOM.client.map.command.map;
 
 import com.syrus.AMFICOM.client.model.AbstractCommand;
 import com.syrus.AMFICOM.client.model.Command;
+import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectPool;
 import com.syrus.AMFICOM.mapview.MapView;
 
 /**
- * Класс $RCSfile: MapViewCloseCommand.java,v $ используется для закрытия 
- * карты при сохранении на экране
- * самого окна карты. При этом в азголовке окна отображается информация о том,
- * что активной карты нет, и карта центрируется по умолчанию
+ * Класс $RCSfile: MapViewCloseCommand.java,v $ используется для закрытия карты
+ * при сохранении на экране самого окна карты. При этом в азголовке окна
+ * отображается информация о том, что активной карты нет, и карта центрируется
+ * по умолчанию
  * 
- * @author $Author: arseniy $
- * @version $Revision: 1.14 $, $Date: 2005/08/17 14:14:18 $
+ * @author $Author: krupenn $
+ * @version $Revision: 1.15 $, $Date: 2005/08/24 10:20:59 $
  * @module mapviewclient
  */
 public class MapViewCloseCommand extends AbstractCommand {
@@ -35,11 +36,12 @@ public class MapViewCloseCommand extends AbstractCommand {
 
 	@Override
 	public void execute() {
-		//		mapFrame.saveConfig();
+		// mapFrame.saveConfig();
 
-		if(this.mapView != null)
-			// TODO should be 'remove', not 'delete'
-			StorableObjectPool.delete(this.mapView.getId());
+		if(this.mapView != null) {
+			// TODO should clean only changes in this.mapview
+			StorableObjectPool.cleanChangedStorableObjects(ObjectEntities.MAPVIEW_CODE);
+		}
 
 		setResult(Command.RESULT_OK);
 	}
