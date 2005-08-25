cfe255819ea576c541ab31a2fa947ecf5c4e53a8
Maxim Selivanov <max.selivanov@gmail.com>
2005-08-25 14:52:46 +0400

    Major: Great changes

--- AMFICOM/v1/filter/src/com/syrus/AMFICOM/newFilter/Filter.java
+++ AMFICOM/v1/filter/src/com/syrus/AMFICOM/newFilter/Filter.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Filter.java,v 1.15 2005/08/09 22:00:24 arseniy Exp $
+ * $Id: Filter.java,v 1.16 2005/08/25 10:52:06 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,115 +7,125 @@
  */
 package com.syrus.AMFICOM.newFilter;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.syrus.AMFICOM.general.ConditionWrapper;
-import com.syrus.AMFICOM.general.LinkedConditionLoader;
+import com.syrus.AMFICOM.general.IllegalDataException;
 import com.syrus.AMFICOM.general.StorableObjectCondition;
 
 /**
- * @version $Revision: 1.15 $, $Date: 2005/08/09 22:00:24 $
- * @author $Author: arseniy $
- * @module filter
+ * @version $Revision: 1.16 $, $Date: 2005/08/25 10:52:06 $
+ * @author $Author: max $
+ * @module misc
  */
 public class Filter {
-	public static final String WRONG_NUMBER_MESSAGE = "you have intered wrong number type in field ";
-	public static final String WRONG_STRING_MESSAGE = "please, fill the field ";
-	public static final String WRONG_LIST_MESSAGE = "Select, from list ";
-	public static final String EQUALS_AND_FROM_SIMULTENIOUSLY = "Fill \"Equals\" or \"From\"";
-	public static final String EQUALS_AND_TO_SIMULTENIOUSLY = "Fill \"Equals\" or \"To\"";
-	public static final String NO_CONDITIONS_CREATED = "You have to create condition(s) first";
-	public static final String WRONG_DATE_MESSAGE = "Please, set the date";
+	
+	
+	//TODO: move it to controller or view
+	public static final String	WRONG_NUMBER_MESSAGE	= "you have intered wrong number type in field ";
+	public static final String	WRONG_STRING_MESSAGE	= "please, fill the field ";
+	public static final String	WRONG_LIST_MESSAGE		= "Select, from list ";
+	public static final String	EQUALS_AND_FROM_SIMULTENIOUSLY	= "Fill \"Equals\" or \"From\"";
+	public static final String	EQUALS_AND_TO_SIMULTENIOUSLY	= "Fill \"Equals\" or \"To\"";
+	public static final String	NO_CONDITIONS_CREATED	= "You have to create condition(s) first";
+	public static final String	WRONG_DATE_MESSAGE	= "Please, set the date";
 
-	private List<String> conditionNames = new LinkedList<String>();
+	private List<ConditionKey> createdConditionKeys = new LinkedList<ConditionKey>();
+	private LogicalScheme logicalScheme = new LogicalScheme();
+	private Collection<FilterView> filterViews = new LinkedList<FilterView>();
 	private List<ConditionKey> keys;
-	private String[] keyNames;
-
-	private Collection<FilterView> filterViews;
-
 	private short entityCode;
-
-	private LogicalScheme logicalScheme;
-	private LinkedConditionLoader linkedConditionLoader;
-
-	public Filter(final ConditionWrapper wrapper, final LinkedConditionLoader linkedConditionLoader) {
-		this.keys = new ArrayList<ConditionKey>(wrapper.getKeys());
+	
+	public Filter(final ConditionWrapper wrapper) {
+		this.keys = wrapper.getKeys();
 		this.entityCode = wrapper.getEntityCode();
-		this.filterViews = new LinkedList<FilterView>();
-		this.linkedConditionLoader = linkedConditionLoader;
-		this.keyNames = new String[this.keys.size()];
-		int i = 0;
-		for (final Iterator<ConditionKey> it = this.keys.iterator(); it.hasNext(); i++) {
-			final ConditionKey conditionKey = it.next();
-			this.keyNames[i] = conditionKey.getName();
-		}
-		this.logicalScheme = new LogicalScheme();
 	}
-
+	
 	public void addView(final FilterView view) {
 		this.filterViews.add(view);
 	}
-
+	
 	public void removeView(final FilterView view) {
 		this.filterViews.remove(view);
 	}
-
-	public void addCondition(final StorableObjectCondition condition, final ConditionKey key) {
+	
+	public void addCondition(final StorableObjectCondition condition, final ConditionKey key) throws IllegalDataException {
+		if (!this.keys.contains(key)) {
+			throw new IllegalDataException("Filter.addCondition | Wrong key (you must select it from appropriate condition wrapper)");
+		}
+		addCondition0(condition, key);
+	}
+	
+	void addCondition0(final StorableObjectCondition condition, final ConditionKey key) {
 		final String conditionName = key.getName();
-		this.conditionNames.add(conditionName);
+		if(!this.createdConditionKeys.contains(key)) {
+			this.createdConditionKeys.add(key);
+		}
 		this.logicalScheme.addCondition(conditionName, condition);
-		refreshCreatedConditions();
+		refresh();
+	}
+	
+	public void removeCondition(final ConditionKey key) {
+		this.createdConditionKeys.remove(key);
+		this.logicalScheme.removeCondition(key.getName());
+		refresh();
 	}
 
-	public void removeCondition(final String name) {
-		this.conditionNames.remove(name);
-		this.logicalScheme.removeCondition(name);
-		refreshCreatedConditions();
-	}
-
-	public void refreshCreatedConditions() {
-		for (final FilterView view : this.filterViews) {
-			view.refreshCreatedConditions(this.conditionNames.toArray());
-			view.refreshResultConditionString(this.logicalScheme.getStringCondition());
+	private void refresh() {
+		for (FilterView view : this.filterViews) {
+			view.refresh();
 		}
 	}
-
-	public String[] getKeyNames() {
-		return this.keyNames;
-	}
-
-	public short getEntityCode() {
+	
+	short getEntityCode() {
 		return this.entityCode;
 	}
-
-	public List<ConditionKey> getKeys() {
+	
+	List<ConditionKey> getKeys() {
 		return this.keys;
 	}
 
-	public boolean hasCondition() {
-		if (this.conditionNames.size() == 0) {
+	boolean hasCondition() {
+		if (this.createdConditionKeys.size() == 0)
 			return false;
-		}
 		return true;
 	}
-
-	public List<String> getConditionNames() {
-		return this.conditionNames;
+	
+	List<String> getConditionNames() {
+		final List<String> names = new LinkedList<String>();
+		for(ConditionKey key : this.createdConditionKeys) {
+			names.add(key.getName());
+		}
+		return names;
 	}
-
-	public LinkedConditionLoader getLinkedConditionLoader() {
-		return this.linkedConditionLoader;
-	}
-
-	public LogicalScheme getLogicalScheme() {
+	
+	LogicalScheme getLogicalScheme() {
 		return this.logicalScheme;
 	}
-
+	
 	public StorableObjectCondition getCondition() {
 		return this.logicalScheme.getResultCondition();
+	}
+
+	ConditionKey getCreatedConditionKey(int index) {
+		return this.createdConditionKeys.get(index);
+	}
+	
+	int getKeyIndex(final ConditionKey key) {
+		return this.keys.indexOf(key);
+	}
+	
+	int getCreatedConditionKeyIndex(final ConditionKey key) {
+		return this.createdConditionKeys.indexOf(key);
+	}
+
+	List<ConditionKey> getCreatedConditionKeys() {
+		return this.createdConditionKeys;	
+	}
+	
+	StorableObjectCondition getCreatedCondition(final ConditionKey key) {
+		return this.logicalScheme.getCondition(key.getName());
 	}
 }
