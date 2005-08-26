1ee4ae8bed0126540c56020d95f504a19746670c
Maxim Selivanov <max.selivanov@gmail.com>
2005-08-26 17:24:52 +0400

    COSMETIC

--- AMFICOM/v1/map/src/com/syrus/AMFICOM/map/DatabaseTypicalConditionImpl.java
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/map/DatabaseTypicalConditionImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DatabaseTypicalConditionImpl.java,v 1.15 2005/08/26 12:11:16 max Exp $
+ * $Id: DatabaseTypicalConditionImpl.java,v 1.16 2005/08/26 13:24:52 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -15,7 +15,7 @@ import com.syrus.AMFICOM.general.StorableObjectWrapper;
 import com.syrus.AMFICOM.general.TypicalCondition;
 
 /**
- * @version $Revision: 1.15 $, $Date: 2005/08/26 12:11:16 $
+ * @version $Revision: 1.16 $, $Date: 2005/08/26 13:24:52 $
  * @author $Author: max $
  * @module map
  */
@@ -30,42 +30,43 @@ final class DatabaseTypicalConditionImpl extends AbstractDatabaseTypicalConditio
 	protected String getColumnName() throws IllegalObjectEntityException {
 		String columnName = null;
 		/* check key support */
+		String key = this.condition.getKey();
 		switch (super.condition.getEntityCode().shortValue()) {
 			case ObjectEntities.PHYSICALLINK_CODE:
-				if (this.condition.getKey().equals(StorableObjectWrapper.COLUMN_CODENAME)) {
+				if (key.equals(StorableObjectWrapper.COLUMN_CODENAME)) {
 					return StorableObjectWrapper.COLUMN_CODENAME;
 				}
-				if (this.condition.getKey().equals(StorableObjectWrapper.COLUMN_NAME)) {
+				if (key.equals(StorableObjectWrapper.COLUMN_NAME)) {
 					return StorableObjectWrapper.COLUMN_NAME;
 				}
-				if (this.condition.getKey().equals(PhysicalLinkWrapper.COLUMN_STREET)) {
+				if (key.equals(PhysicalLinkWrapper.COLUMN_STREET)) {
 					return PhysicalLinkWrapper.COLUMN_STREET;
 				}
 				break;
 			case ObjectEntities.SITENODE_TYPE_CODE:
-				if (this.condition.getKey().equals(StorableObjectWrapper.COLUMN_CODENAME)) {
+				if (key.equals(StorableObjectWrapper.COLUMN_CODENAME)) {
 					return StorableObjectWrapper.COLUMN_CODENAME;
 				}
-				if (this.condition.getKey().equals(StorableObjectWrapper.COLUMN_NAME)) {
+				if (key.equals(StorableObjectWrapper.COLUMN_NAME)) {
 					return StorableObjectWrapper.COLUMN_NAME;
 				}
-				if (this.condition.getKey().equals(SiteNodeWrapper.COLUMN_STREET)) {
+				if (key.equals(SiteNodeWrapper.COLUMN_STREET)) {
 					return SiteNodeWrapper.COLUMN_STREET;
 				}
 				break;
 			case ObjectEntities.PHYSICALLINK_TYPE_CODE:
-				if (this.condition.getKey().equals(StorableObjectWrapper.COLUMN_CODENAME)) {
+				if (key.equals(StorableObjectWrapper.COLUMN_CODENAME)) {
 					return StorableObjectWrapper.COLUMN_CODENAME;
 				}
 				break;
 			case ObjectEntities.MAPLIBRARY_CODE:
-				if (this.condition.getKey().equals(StorableObjectWrapper.COLUMN_CODENAME)) {
+				if (key.equals(StorableObjectWrapper.COLUMN_CODENAME)) {
 					return StorableObjectWrapper.COLUMN_CODENAME;
 				}
 				break;
 			default:
 				throw new IllegalObjectEntityException("Entity '" + ObjectEntities.codeToString(this.condition.getEntityCode())
-						+ "' and key '" + this.condition.getKey() + "' are not supported.",
+						+ "' and key '" + key + "' are not supported.",
 						IllegalObjectEntityException.ENTITY_NOT_REGISTERED_CODE);
 		}
 		return columnName;
