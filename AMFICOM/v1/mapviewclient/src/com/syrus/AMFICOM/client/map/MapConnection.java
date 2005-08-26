2d5a059f8a5c4eab66d0296b0e7c8ce6e2e93706
Peter Peskovsky <peskovsky@gmail.com>
2005-08-26 12:13:05 +0400

    TRIVIAL: Added method searchIsAvailableForlayer(). At the moment it is using the values FROM STATIC MAP INSIDE MapConnection. In future it's supposed to get values from administration module.

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/MapConnection.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/MapConnection.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapConnection.java,v 1.13 2005/08/22 15:11:04 krupenn Exp $
+ * $Id: MapConnection.java,v 1.14 2005/08/26 08:13:05 peskovsky Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -7,20 +7,46 @@
  */
 package com.syrus.AMFICOM.client.map;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.13 $, $Date: 2005/08/22 15:11:04 $
- * @author $Author: krupenn $
+ * @version $Revision: 1.14 $, $Date: 2005/08/26 08:13:05 $
+ * @author $Author: peskovsky $
  * @module mapviewclient
  */
 public abstract class MapConnection {
 	
 	protected List<MapConnectionListener> listeners = new LinkedList<MapConnectionListener>();
+	
+	private static Map<String,Boolean> LAYER_SEARCHABILITIES = new HashMap<String,Boolean>();
+	static{
+		LAYER_SEARCHABILITIES.put("Msk_Bridges_a",true);
+		LAYER_SEARCHABILITIES.put("Msk_Cemetry_a",true);		
+		LAYER_SEARCHABILITIES.put("Msk_house_a",false);
+		LAYER_SEARCHABILITIES.put("Msk_house_p",false);
+		LAYER_SEARCHABILITIES.put("Msk_hydrography_la",true);
+		LAYER_SEARCHABILITIES.put("Msk_metro_title_l",true);
+		LAYER_SEARCHABILITIES.put("Msk_metro_title_p",true);
+		LAYER_SEARCHABILITIES.put("Msk_municip_a",true);
+		LAYER_SEARCHABILITIES.put("Msk_quarter_a",true);
+		LAYER_SEARCHABILITIES.put("Msk_railway_l",true);
+		LAYER_SEARCHABILITIES.put("Msk_railway_p",true);
+		LAYER_SEARCHABILITIES.put("Msk_region_a",true);
+		LAYER_SEARCHABILITIES.put("Msk_road_a",false);
+		LAYER_SEARCHABILITIES.put("Msk_square_a",true);
+		LAYER_SEARCHABILITIES.put("Msk_Street_l",true);
+		LAYER_SEARCHABILITIES.put("Msk_Street_l_1",true);
+		LAYER_SEARCHABILITIES.put("Msk_vegetation_a",true);
+		LAYER_SEARCHABILITIES.put("Msk_Bridges_a",true);
+		LAYER_SEARCHABILITIES.put("Msk_Bridges_a",true);
+		LAYER_SEARCHABILITIES.put("Msk_Bridges_a",true);
+	}
 
 	public abstract boolean connect() throws MapConnectionException;
 
@@ -49,6 +75,19 @@ public abstract class MapConnection {
 	 * @return список слоев &lt;{@link SpatialLayer}&gt;
 	 */
 	public abstract List<SpatialLayer> getLayers() throws MapDataException;
+
+	/**
+	 * @return true, если доступен поиск по слою;
+	 */
+	public boolean searchIsAvailableForLayer(SpatialLayer layer) throws MapDataException{
+		boolean returnValue = false;
+		
+		Boolean tableValue = LAYER_SEARCHABILITIES.get(layer.getName());
+		if (tableValue != null)
+			returnValue = tableValue.booleanValue();
+		
+		return returnValue;
+	}
 	
 	/**
 	 * Получить список названий доступных видов.
