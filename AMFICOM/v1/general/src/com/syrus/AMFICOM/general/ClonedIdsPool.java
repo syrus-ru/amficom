85fd9b25527df246ca73ab43d9f4f4294252a3a5
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-08-26 14:05:07 +0400

    ADD: #setExistingId(...) (patch from Mousebuster)

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ClonedIdsPool.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ClonedIdsPool.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ClonedIdsPool.java,v 1.4 2005/08/24 15:00:29 bass Exp $
+ * $Id: ClonedIdsPool.java,v 1.5 2005/08/26 10:05:07 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -12,15 +12,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/08/24 15:00:29 $
+ * @version $Revision: 1.5 $, $Date: 2005/08/26 10:05:07 $
  * @author $Author: bass $
  * @module general
  */
-public class ClonedIdsPool {
-
+public final class ClonedIdsPool {
 	private Map<String, Identifier> clonedIds = new HashMap<String, Identifier>();
 
-	public final Identifier getClonedId(final short entityCode, final String id) throws IdentifierGenerationException {
+	public Identifier getClonedId(final short entityCode, final String id) throws IdentifierGenerationException {
 		Identifier clonedId = this.clonedIds.get(id);
 		if (clonedId == null) {
 			clonedId = this.cloneId(entityCode, id);
@@ -28,9 +27,13 @@ public class ClonedIdsPool {
 		return clonedId;
 	}
 
-	public final Identifier cloneId(final short entityCode, final String id) throws IdentifierGenerationException {
+	public Identifier cloneId(final short entityCode, final String id) throws IdentifierGenerationException {
 		final Identifier clonedId = IdentifierPool.getGeneratedIdentifier(entityCode);
 		this.clonedIds.put(id, clonedId);
 		return clonedId;
+	}
+
+	public final void setExistingId(final String id, final Identifier clonedId) {
+		this.clonedIds.put(id, clonedId);
 	}
 }
