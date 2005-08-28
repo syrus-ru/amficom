eec3c7378ca01e82c60257381514481b08dd0a32
Arseniy Tashoyan <tashoyan@gmail.com>
2005-08-28 19:13:01 +0400

    ADD: Names of helper tables -- not names of any entity

---
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/TableNames.java
@@ -0,0 +1,37 @@
+/*-
+ * $Id: TableNames.java,v 1.1 2005/08/28 15:13:01 arseniy Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.AMFICOM.general;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/08/28 15:13:01 $
+ * @author $Author: arseniy $
+ * @module general
+ */
+public final class TableNames {
+	public static final String DATA_TYPE = "DataType";
+	public static final String MEASUREMENT_UNIT = "MeasurementUnit";
+	public static final String PARAMETER_TYPE = "ParameterType";
+
+	public static final String MEASUREMENT_TYPE = "MeasurementType";
+	public static final String ANALYSIS_TYPE = "AnalysisType";
+	public static final String MODELING_TYPE = "ModelingType";
+
+	public static final String MNT_TYP_PAR_TYP_LINK = "MntTypParTypLink";
+	public static final String ANA_TYP_PAR_TYP_LINK = "AnaTypParTypLink";
+	public static final String MOD_TYP_PAR_TYP_LINK = "ModTypParTypLink";
+
+	public static final String MNT_TYP_ANA_TYP_LINK = "MntTypAnaTypLink";
+	public static final String MNTPORTTYPMNTTYPLINK = "MntPortTypMntTypLink";
+	public static final String PARAMETERSETMELINK = "ParameterSetMELink";
+	public static final String MEASUREMENTSETUP_ME_LINK = "MeasurementSetupMELink";
+	public static final String MEASUREMENTSETUP_MT_LINK = "MeasurementSetupMTLink";
+	public static final String MEASUREMENTSETUP_TEST_LINK = "MeasurementSetupTestLink";
+	public static final String EQUIPMENT_ME_LINK = "EquipmentMELink";
+	public static final String TRANSMISSIONPATH_ME_LINK = "TransmissionPathMELink";
+
+}
