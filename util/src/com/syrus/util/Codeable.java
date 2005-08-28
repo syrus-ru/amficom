2ff81241cbbd8a6880140ac2518a511038a30a41
Arseniy Tashoyan <tashoyan@gmail.com>
2005-08-28 17:45:41 +0400

    ADD: Moved from general

---
+++ util/src/com/syrus/util/Codeable.java
@@ -0,0 +1,17 @@
+/*-
+ * $Id: Codeable.java,v 1.1 2005/08/28 13:45:41 arseniy Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.util;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/08/28 13:45:41 $
+ * @author $Author: arseniy $
+ * @module util
+ */
+public interface Codeable {
+	int getCode();
+}
