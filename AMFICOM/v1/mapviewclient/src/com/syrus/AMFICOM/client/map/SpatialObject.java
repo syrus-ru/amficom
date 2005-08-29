68e67c3dd2e602c47196635d94e198e9968826df
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-29 16:30:24 +0400

    ADD: constructor

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/SpatialObject.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/SpatialObject.java
@@ -1,5 +1,5 @@
 /**
- * $Id: SpatialObject.java,v 1.8 2005/08/29 12:17:46 peskovsky Exp $
+ * $Id: SpatialObject.java,v 1.9 2005/08/29 12:30:24 krupenn Exp $
  *
  * Syrus Systems
  * Научно-технический центр
@@ -15,28 +15,33 @@ package com.syrus.AMFICOM.client.map;
  * Географический объект. Реализация принадлежит используемой системе ГИС.
  * текущее использование класса - в списке найденных по строковому шаблону
  * графических объектов.
- * @author $Author: peskovsky $
- * @version $Revision: 1.8 $, $Date: 2005/08/29 12:17:46 $
+ * 
+ * @author $Author: krupenn $
+ * @version $Revision: 1.9 $, $Date: 2005/08/29 12:30:24 $
  * @module mapviewclient
  * @see com.syrus.AMFICOM.client.map.operations.SpatialSearchPanel
  */
-public abstract class SpatialObject implements Comparable 
-{
-	protected String label = null;
-	
+public abstract class SpatialObject implements Comparable {
+	protected String label;
+
+	public SpatialObject(String label) {
+		this.label = label;
+	}
+
 	/**
 	 * Возвращает имя географического объекта.
+	 * 
 	 * @return имя объекта
 	 */
 	public String getLabel() {
 		return this.label;
 	}
-	
+
 	public int compareTo(Object o) {
-		if (!(o instanceof SpatialObject))
+		if(!(o instanceof SpatialObject))
 			throw new ClassCastException();
-		
-		SpatialObject spatialObject = (SpatialObject)o;
+
+		SpatialObject spatialObject = (SpatialObject) o;
 		return (this.label.compareTo(spatialObject.label));
 	}
 }
