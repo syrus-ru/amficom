5b678eb01d5b7a253d177821812874d7ed18ca25
Arseniy Tashoyan <tashoyan@gmail.com>
2005-08-29 21:56:33 +0400

    ADD: Class to control all OTAUs. It has to functions: 1) on startup initialize all available OTAUs and COM ports; 2) On demand from RTUTransceiver before start a measurement switch the specified OTAU and run a thread to hold the OTAU in switched state

---
+++ hw/r7/OTAUController.h
@@ -0,0 +1,70 @@
+// OTAUController.h: interface for the OTAUController class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_OTAUCONTROLLER_H__7EEB79A6_E914_4858_A43D_998DB8E48756__INCLUDED_)
+#define AFX_OTAUCONTROLLER_H__7EEB79A6_E914_4858_A43D_998DB8E48756__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <windows.h>
+#include "pthread.h"
+#include "ByteArray.h"
+
+
+#define COM_PORT_READ_TIMEOUT (unsigned long) 10000
+#define COM_PORT_REPLY_LENGTH (unsigned int) 4000
+#define MAX_POSSIBLE_OTAUS (unsigned int) 100
+#define MAX_POSSIBLE_OTAU_PORTS (unsigned int) 100
+#define OTAU_COMMAND_INIT ";INIT-SYS::ALL:WXYZ1::0;"
+#define OTAU_COMMAND_HDR ";RTRV-HDR:::ABCD;"
+#define OTAU_COMMAND_DISCONNECT "DISC-TACC:1:1:1;"
+#define OTAU_COMMAND_CONNECT1 "CONN-TACC-OTAU:OTAU"
+#define OTAU_COMMAND_CONNECT2 ":ABCD:1;"
+#define OTAU_COMMAND_CONNECT_LENGTH 50
+#define OTAU_COMMAND_REANIMATE_CONNECTION ";REPT-STAT:::ABCD;"
+#define OTAU_HOLD_PORT_TIMEOUT (unsigned int) 50 //sec
+
+class OTAUController {
+public:
+	OTAUController(const unsigned int timewait, unsigned short com_port_number);
+	virtual ~OTAUController();
+
+	int start(ByteArray* local_address);
+	void shutdown();
+
+	pthread_t get_thread() const;
+
+private:
+	static void* run(void* args);
+	void initialize_OTAUs();
+	int set_COM_port_properties(const HANDLE com_port_handle);
+	void send_switch_command(const HANDLE com_port_handle,
+		const char* command,
+		const unsigned int command_size,
+		char*& reply,
+		unsigned int& reply_size);
+	unsigned int search_number_of_OTAUs(const char* string, unsigned int str_size);
+	int parse_local_address(const char* local_address, unsigned int la_length);
+	int is_local_address_valid() const;
+//	void check_last_used_com_port();
+//	void disconnect_last_used_com_port();
+	int switch_OTAU();
+
+	unsigned short com_port_number;
+	HANDLE* com_ports;
+	unsigned int* otau_numbers;
+//	HANDLE last_used_com_port;
+
+	unsigned short com_port;
+	unsigned short otau_id;
+	unsigned short otau_port;
+
+	pthread_t thread;
+	unsigned int timewait;
+	int running;
+};
+
+#endif // !defined(AFX_OTAUCONTROLLER_H__7EEB79A6_E914_4858_A43D_998DB8E48756__INCLUDED_)
