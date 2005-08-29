95262df212854753d4afae91c5735b8afa01e024
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-29 16:24:59 +0400

    COMPATIBILITY: SpatialObject became an abstract class

--- AMFICOM/v1/spatialfx/src/com/syrus/AMFICOM/client/map/objectfx/OfxSpatialObject.java
+++ AMFICOM/v1/spatialfx/src/com/syrus/AMFICOM/client/map/objectfx/OfxSpatialObject.java
@@ -3,24 +3,15 @@ package com.syrus.AMFICOM.client.map.objectfx;
 import com.ofx.repository.SxSpatialObject;
 import com.syrus.AMFICOM.client.map.SpatialObject;
 
-public class OfxSpatialObject implements SpatialObject
-{
+public class OfxSpatialObject extends SpatialObject {
 	protected SxSpatialObject so;
-	protected String label;
 		
-	public OfxSpatialObject(SxSpatialObject so, String label)
-	{
+	public OfxSpatialObject(SxSpatialObject so, String label) {
+		super(label);
 		this.so = so;
-		this.label = label;
 	}
 	
-	public String getLabel()
-	{
-		return this.label;
-	}
-	
-	public SxSpatialObject getSxSpatialObject()
-	{
+	public SxSpatialObject getSxSpatialObject() {
 		return this.so;
 	}
 }
