7c516f2d08804e6704abf1c80b957d1889841da0
Arseniy Tashoyan <tashoyan@gmail.com>
2005-08-29 15:05:43 +0400

    ADD: New test method

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestMeasurementPortType.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestMeasurementPortType.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestMeasurementPortType.java,v 1.2 2005/08/28 16:43:51 arseniy Exp $
+ * $Id: TestMeasurementPortType.java,v 1.3 2005/08/29 11:05:43 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,13 +8,17 @@
 package com.syrus.AMFICOM.measurement;
 
 import java.util.EnumSet;
+import java.util.Set;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.DatabaseCommonTest;
+import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectPool;
+import com.syrus.AMFICOM.general.TypicalCondition;
+import com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlTypicalConditionPackage.OperationSort;
 
 public final class TestMeasurementPortType extends TestCase {
 
@@ -24,7 +28,7 @@ public final class TestMeasurementPortType extends TestCase {
 
 	public static Test suite() {
 		final DatabaseCommonTest commonTest = new DatabaseCommonTest();
-		commonTest.addTestSuite(TestMeasurementPortType.class);
+		commonTest.addTest(new TestMeasurementPortType("testGetByCondition"));
 		return commonTest.createTestSetup();
 	}
 
@@ -36,4 +40,18 @@ public final class TestMeasurementPortType extends TestCase {
 				EnumSet.of(MeasurementType.REFLECTOMETRY));
 		StorableObjectPool.flush(measurementPortType, DatabaseCommonTest.getSysUser().getId(), false);
 	}
+
+	public void testGetByCondition() throws ApplicationException {
+		final TypicalCondition tc = new TypicalCondition(MeasurementType.REFLECTOMETRY,
+				OperationSort.OPERATION_IN,
+				ObjectEntities.MEASUREMENTPORT_TYPE_CODE,
+				MeasurementPortTypeWrapper.LINK_COLUMN_MEASUREMENT_TYPE_CODE);
+		final Set<MeasurementPortType> mpts = StorableObjectPool.getStorableObjectsByCondition(tc, true);
+		for (final MeasurementPortType measurementPortType : mpts) {
+			System.out.println("MeasurementPortType: '" + measurementPortType.getId()
+					+ "', codename: '" + measurementPortType.getCodename()
+					+ "', description: '" + measurementPortType.getDescription() + "'");
+		}
+	}
+
 }
