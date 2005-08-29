83e92e9a48c93b1cf658176a30e2b234b1d5416c
Peter Peskovsky <peskovsky@gmail.com>
2005-08-29 16:14:13 +0400

    TRIVIAL: Comparator implementing is moved to super CLASS Spatialobject.

--- AMFICOM/v1/mapinfo/src/com/syrus/AMFICOM/client/map/mapinfo/MapInfoSpatialObject.java
+++ AMFICOM/v1/mapinfo/src/com/syrus/AMFICOM/client/map/mapinfo/MapInfoSpatialObject.java
@@ -4,26 +4,20 @@ import com.syrus.AMFICOM.client.map.SpatialObject;
 import com.syrus.AMFICOM.resource.DoublePoint;
 import com.syrus.util.HashCodeGenerator;
 
-public class MapInfoSpatialObject implements SpatialObject,Comparable {
+public class MapInfoSpatialObject extends SpatialObject {
 	private DoublePoint centre = null;
-
-	private String label = null;
-
+	
 	public MapInfoSpatialObject(final DoublePoint centre, final String featureLabel) {
 		this.centre = centre;
 		this.label = featureLabel;
 	}
 
-	public String getLabel() {
-		return this.label;
-	}
-
 	public DoublePoint getCenter() {
 		return this.centre;
 	}
-	
+
 	public boolean equals(MapInfoSpatialObject object){
-		if (	object.label.equals(this.label)
+		if (	object.getLabel().equals(this.getLabel())
 			&&	object.centre.equals(this.centre))
 			return true;
 		return false;
@@ -35,13 +29,5 @@ public class MapInfoSpatialObject implements SpatialObject,Comparable {
 		generator.addDouble(this.centre.getX());
 		generator.addDouble(this.centre.getY());
 		return generator.getResult();
-	}
-
-	public int compareTo(Object o) {
-		if (!(o instanceof MapInfoSpatialObject))
-			throw new ClassCastException();
-		
-		MapInfoSpatialObject spatialObject = (MapInfoSpatialObject)o;
-		return (this.label.compareTo(spatialObject.label));
 	}
 }
