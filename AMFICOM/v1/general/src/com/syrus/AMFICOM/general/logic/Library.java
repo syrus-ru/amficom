3538cec6d25d020ffedcedc0667ccc292238d273
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-08-31 09:20:38 +0400

    COMPAT: moved from module map to module general

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/logic/Library.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/logic/Library.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: Library.java,v 1.5 2005/08/03 14:33:02 max Exp $
+ * $Id: Library.java,v 1.1 2005/08/31 05:20:38 bass Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -10,14 +10,16 @@ package com.syrus.AMFICOM.general.logic;
 
 import java.util.Set;
 
+import com.syrus.util.Shitlet;
+
 /**
  * @author Andrei Kroupennikov
- * @author $Author: max $
- * @version $Revision: 1.5 $, $Date: 2005/08/03 14:33:02 $
- * @module map
+ * @author $Author: bass $
+ * @version $Revision: 1.1 $, $Date: 2005/08/31 05:20:38 $
+ * @module general
  */
+@Shitlet
 public interface Library extends LibraryEntry {
-
 	Set<LibraryEntry> getChildren();
 	void addChild(final LibraryEntry libraryEntry);
 	void removeChild(final LibraryEntry libraryEntry);
--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/logic/LibraryEntry.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/logic/LibraryEntry.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: LibraryEntry.java,v 1.2 2005/08/02 07:15:11 krupenn Exp $
+ * $Id: LibraryEntry.java,v 1.1 2005/08/31 05:20:38 bass Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,14 +8,16 @@
 
 package com.syrus.AMFICOM.general.logic;
 
+import com.syrus.util.Shitlet;
+
 /**
  * @author Andrei Kroupennikov
- * @author $Author: krupenn $
- * @version $Revision: 1.2 $, $Date: 2005/08/02 07:15:11 $
- * @module map
+ * @author $Author: bass $
+ * @version $Revision: 1.1 $, $Date: 2005/08/31 05:20:38 $
+ * @module general
  */
+@Shitlet
 public interface LibraryEntry {
-
 	void setParent(final Library library);
 	Library getParent();
 }
