5cdbfc8d3bec31bfd302734a9d68f3bfa6019030
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-08-31 17:16:49 +0400

    ADD: menu item for adding attached site node

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/popup/SitePopupMenu.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/popup/SitePopupMenu.java
@@ -6,11 +6,17 @@ import java.awt.event.ActionListener;
 import javax.swing.JMenuItem;
 
 import com.syrus.AMFICOM.client.event.MapEvent;
+import com.syrus.AMFICOM.client.map.MapConnectionException;
+import com.syrus.AMFICOM.client.map.MapDataException;
+import com.syrus.AMFICOM.client.map.MapException;
 import com.syrus.AMFICOM.client.resource.LangModelMap;
 import com.syrus.AMFICOM.map.SiteNode;
+import com.syrus.AMFICOM.map.SiteNodeType;
+import com.syrus.AMFICOM.map.corba.IdlSiteNodeTypePackage.SiteNodeTypeSort;
 
 public final class SitePopupMenu extends MapPopupMenu {
 	private JMenuItem removeMenuItem = new JMenuItem();
+	private JMenuItem attachCableInletMenuItem = new JMenuItem();
 
 	private JMenuItem propertiesMenuItem = new JMenuItem();
 
@@ -34,9 +40,23 @@ public final class SitePopupMenu extends MapPopupMenu {
 	@Override
 	public void setElement(Object me) {
 		this.site = (SiteNode )me;
+		SiteNodeTypeSort sort = this.site.getType().getSort();
+		this.attachCableInletMenuItem.setVisible(
+				sort.value() == SiteNodeTypeSort._ATS
+				|| sort.value() == SiteNodeTypeSort._BUILDING);
 	}
 
 	private void jbInit() {
+		this.attachCableInletMenuItem.setText(LangModelMap.getString("AttachCableInlet"));
+		this.attachCableInletMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					attachCableInlet();
+				} catch(MapException ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
 		this.removeMenuItem.setText(LangModelMap.getString("Delete"));
 		this.removeMenuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -49,9 +69,19 @@ public final class SitePopupMenu extends MapPopupMenu {
 				showProperties();
 			}
 		});
+		this.add(this.attachCableInletMenuItem);
 		this.add(this.removeMenuItem);
 		// this.addSeparator();
 		// this.add(this.propertiesMenuItem);
+	}
+
+	protected void attachCableInlet() throws MapConnectionException, MapDataException {
+		SiteNodeType siteNodeType = super.selectAttachedSiteNodeType();
+
+		if(siteNodeType != null) {
+			super.createAttachedSiteNode(this.site, siteNodeType);
+			this.netMapViewer.getLogicalNetLayer().sendMapEvent(MapEvent.MAP_CHANGED);
+		}
 	}
 
 	void showProperties() {
