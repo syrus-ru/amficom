088cfe2edb45dd68b8670f2af9d8a2c821762f1b
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-09-04 18:57:32 +0400

    BUGFIX: string keys

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ClonedIdsPool.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ClonedIdsPool.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ClonedIdsPool.java,v 1.6 2005/08/31 13:24:21 bass Exp $
+ * $Id: ClonedIdsPool.java,v 1.7 2005/09/04 14:57:32 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -15,18 +15,18 @@ import com.syrus.AMFICOM.general.xml.XmlIdentifier;
 import com.syrus.util.Shitlet;
 
 /**
- * @version $Revision: 1.6 $, $Date: 2005/08/31 13:24:21 $
+ * @version $Revision: 1.7 $, $Date: 2005/09/04 14:57:32 $
  * @author $Author: bass $
  * @module general
  */
 @Shitlet
 public final class ClonedIdsPool {
-	private Map<XmlIdentifier, Identifier> clonedIds = new HashMap<XmlIdentifier, Identifier>();
+	private Map<String, Identifier> clonedIds = new HashMap<String, Identifier>();
 	
 	public Identifier getClonedId(final short entityCode,
 			final XmlIdentifier id)
 	throws IdentifierGenerationException {
-		final Identifier clonedId = this.clonedIds.get(id);
+		final Identifier clonedId = this.clonedIds.get(id.getStringValue());
 		return clonedId == null ? this.cloneId(entityCode, id) : clonedId;
 	}
 
@@ -34,12 +34,12 @@ public final class ClonedIdsPool {
 			final XmlIdentifier id)
 	throws IdentifierGenerationException {
 		final Identifier clonedId = IdentifierPool.getGeneratedIdentifier(entityCode);
-		this.clonedIds.put(id, clonedId);
+		this.clonedIds.put(id.getStringValue(), clonedId);
 		return clonedId;
 	}
 
 	public void setExistingId(final XmlIdentifier id,
 			final Identifier clonedId) {
-		this.clonedIds.put(id, clonedId);
+		this.clonedIds.put(id.getStringValue(), clonedId);
 	}
 }
