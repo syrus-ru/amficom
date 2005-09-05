6ac4b1f06fb6da0dee1cdd6245675782f49b61b6
Maxim Selivanov <max.selivanov@gmail.com>
2005-09-05 14:36:41 +0400

    COMP: from objects to identifiers in NodeLink

--- AMFICOM/v1/map/src/com/syrus/AMFICOM/map/NodeLinkDatabase.java
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/map/NodeLinkDatabase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: NodeLinkDatabase.java,v 1.35 2005/08/31 05:50:36 bass Exp $
+ * $Id: NodeLinkDatabase.java,v 1.36 2005/09/05 10:36:41 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -13,19 +13,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.DatabaseIdentifier;
 import com.syrus.AMFICOM.general.IllegalDataException;
 import com.syrus.AMFICOM.general.RetrieveObjectException;
 import com.syrus.AMFICOM.general.StorableObjectDatabase;
-import com.syrus.AMFICOM.general.StorableObjectPool;
 import com.syrus.AMFICOM.general.StorableObjectVersion;
 import com.syrus.AMFICOM.general.StorableObjectWrapper;
 import com.syrus.util.database.DatabaseDate;
 
 /**
- * @version $Revision: 1.35 $, $Date: 2005/08/31 05:50:36 $
- * @author $Author: bass $
+ * @version $Revision: 1.36 $, $Date: 2005/09/05 10:36:41 $
+ * @author $Author: max $
  * @module map
  */
 public final class NodeLinkDatabase extends StorableObjectDatabase<NodeLink> {
@@ -65,18 +63,18 @@ public final class NodeLinkDatabase extends StorableObjectDatabase<NodeLink> {
 	protected int setEntityForPreparedStatementTmpl(final NodeLink storableObject,
 			final PreparedStatement preparedStatement,
 			int startParameterNumber) throws IllegalDataException, SQLException {
-		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getPhysicalLink().getId());
-		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getStartNode().getId());
-		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getEndNode().getId());
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getPhysicalLinkId());
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getStartNodeId());
+		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getEndNodeId());
 		preparedStatement.setDouble(++startParameterNumber, storableObject.getLength());
 		return startParameterNumber;
 	}
 	
 	@Override
 	protected String getUpdateSingleSQLValuesTmpl(final NodeLink storableObject) throws IllegalDataException {
-		final String values = DatabaseIdentifier.toSQLString(storableObject.getPhysicalLink().getId()) + COMMA
-			+ DatabaseIdentifier.toSQLString(storableObject.getStartNode().getId()) + COMMA
-			+ DatabaseIdentifier.toSQLString(storableObject.getEndNode().getId()) + COMMA
+		final String values = DatabaseIdentifier.toSQLString(storableObject.getPhysicalLinkId()) + COMMA
+			+ DatabaseIdentifier.toSQLString(storableObject.getStartNodeId()) + COMMA
+			+ DatabaseIdentifier.toSQLString(storableObject.getEndNodeId()) + COMMA
 			+ storableObject.getLength();
 		return values;
 	}
@@ -97,33 +95,14 @@ public final class NodeLinkDatabase extends StorableObjectDatabase<NodeLink> {
 						0.0)
 					: storableObject;
 
-		PhysicalLink physicalLink;
-		AbstractNode startNode;
-		AbstractNode endNode;
-
-		try {
-			physicalLink = StorableObjectPool.getStorableObject(DatabaseIdentifier.getIdentifier(resultSet,
-					NodeLinkWrapper.COLUMN_PHYSICAL_LINK_ID), true);
-			startNode = StorableObjectPool.getStorableObject(DatabaseIdentifier.getIdentifier(resultSet,
-					NodeLinkWrapper.COLUMN_START_NODE_ID), true);
-			endNode = StorableObjectPool.getStorableObject(DatabaseIdentifier.getIdentifier(resultSet,
-					NodeLinkWrapper.COLUMN_END_NODE_ID), true);
-		} catch (ApplicationException ae) {
-			String msg = this.getEntityName() + "Database.updateEntityFromResultSet | Error " + ae.getMessage();
-			throw new RetrieveObjectException(msg, ae);
-		} catch (SQLException sqle) {
-			String msg = this.getEntityName() + "Database.updateEntityFromResultSet | Error " + sqle.getMessage();
-			throw new RetrieveObjectException(msg, sqle);
-		}
-
 		nodeLink.setAttributes(DatabaseDate.fromQuerySubString(resultSet, StorableObjectWrapper.COLUMN_CREATED),
 				DatabaseDate.fromQuerySubString(resultSet, StorableObjectWrapper.COLUMN_MODIFIED),
 				DatabaseIdentifier.getIdentifier(resultSet, StorableObjectWrapper.COLUMN_CREATOR_ID),
 				DatabaseIdentifier.getIdentifier(resultSet, StorableObjectWrapper.COLUMN_MODIFIER_ID),
 				new StorableObjectVersion(resultSet.getLong(StorableObjectWrapper.COLUMN_VERSION)),
-				physicalLink,
-				startNode,
-				endNode,
+				DatabaseIdentifier.getIdentifier(resultSet, NodeLinkWrapper.COLUMN_PHYSICAL_LINK_ID),
+				DatabaseIdentifier.getIdentifier(resultSet, NodeLinkWrapper.COLUMN_START_NODE_ID),
+				DatabaseIdentifier.getIdentifier(resultSet, NodeLinkWrapper.COLUMN_END_NODE_ID),
 				resultSet.getDouble(NodeLinkWrapper.COLUMN_LENGTH));
 		return nodeLink;
 	}
