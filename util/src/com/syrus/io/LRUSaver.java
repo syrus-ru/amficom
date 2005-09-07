d85a3c4443eb28fdda7b8dfaa3cee0f101c90364
Arseniy Tashoyan <tashoyan@gmail.com>
2005-09-07 18:07:07 +0400

    COSMETIC

--- util/src/com/syrus/io/LRUSaver.java
+++ util/src/com/syrus/io/LRUSaver.java
@@ -1,5 +1,5 @@
 /*-
-* $Id: LRUSaver.java,v 1.1 2005/09/07 13:00:29 bob Exp $
+* $Id: LRUSaver.java,v 1.2 2005/09/07 14:07:07 arseniy Exp $
 *
 * Copyright ¿ 2005 Syrus Systems.
 * Dept. of Science & Technology.
@@ -14,17 +14,15 @@ import com.syrus.util.LRUMap;
 
 
 /**
- * @version $Revision: 1.1 $, $Date: 2005/09/07 13:00:29 $
- * @author $Author: bob $
+ * @version $Revision: 1.2 $, $Date: 2005/09/07 14:07:07 $
+ * @author $Author: arseniy $
  * @author Vladimir Dolzhenko
  * @module util
  */
 public interface LRUSaver<K, V> {
 
-	void save(final LRUMap<K, V> lruMap, 
-	          final String objectEntityName, 
-	          final boolean cleanLRUMap);
-	
+	void save(final LRUMap<K, V> lruMap, final String objectEntityName, final boolean cleanLRUMap);
+
 	Set<V> load(final String objectEntityName);
 }
 
