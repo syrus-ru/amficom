b79d78e28b5f703bc8f8dd3d39242490e768a0ed
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-09-07 17:05:39 +0400

    COMPAT due to to change PoolContext, add stub XMLStubLRUSaver

--- AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/XMLPoolContext.java
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/XMLPoolContext.java
@@ -1,5 +1,5 @@
 /*
- * $Id: XMLPoolContext.java,v 1.6 2005/08/12 10:09:32 bob Exp $
+ * $Id: XMLPoolContext.java,v 1.7 2005/09/07 13:05:39 bob Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,21 +8,26 @@
 package com.syrus.AMFICOM.general;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.Set;
 
+import com.syrus.io.LRUSaver;
 import com.syrus.util.ApplicationProperties;
+import com.syrus.util.LRUMap;
 
 /**
- * @version $Revision: 1.6 $, $Date: 2005/08/12 10:09:32 $
+ * @version $Revision: 1.7 $, $Date: 2005/09/07 13:05:39 $
  * @author $Author: bob $
  * @module commonclient
  */
-public final class XMLPoolContext extends PoolContext {
+public final class XMLPoolContext implements PoolContext {
 	private static final String KEY_CACHE_PATH = "CachePath";
 	private static final String CACHE_PATH = "cache";
 
 	private XMLObjectLoader objectLoader;
 	
-	@Override
+	private static XMLStubLRUSaver instance;
+	
 	public void init() {
 		final File cachePath = new File(ApplicationProperties.getString(KEY_CACHE_PATH, CACHE_PATH));
 		final Class cacheClass = StorableObjectResizableLRUMap.class;
@@ -41,8 +46,31 @@ public final class XMLPoolContext extends PoolContext {
 		//StorableObjectPool.addObjectPoolGroup(ObjectGroupEntities.MAPVIEW_GROUP_CODE, 1000);
 	}
 
+	public LRUSaver<Identifier, StorableObject> getLRUSaver() {
+		if (instance == null) {
+			synchronized (XMLStubLRUSaver.class) {
+				if (instance == null) {
+					instance = new XMLStubLRUSaver();
+				}
+			}
+		}
+		return instance;
+	}
 	
 	final XMLObjectLoader getObjectLoader() {
 		return this.objectLoader;
+	}
+	
+	private class XMLStubLRUSaver implements LRUSaver<Identifier, StorableObject> {
+
+		public Set<StorableObject> load(String objectEntityName) {
+			return Collections.emptySet();
+		}
+		
+		public void save(	LRUMap<Identifier, StorableObject> lruMap,
+							String objectEntityName,
+							boolean cleanLRUMap) {
+			// nothing yopta			
+		}
 	}
 }
