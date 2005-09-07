41599be31c565ed7a1a2ef96abe4b4ae53d2ded4
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-09-07 17:04:25 +0400

    ADD client impl of lru map store

---
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/ClientLRUMapSaver.java
@@ -0,0 +1,65 @@
+/*-
+* $Id: ClientLRUMapSaver.java,v 1.1 2005/09/07 13:04:25 bob Exp $
+*
+* Copyright ¿ 2005 Syrus Systems.
+* Dept. of Science & Technology.
+* Project: AMFICOM.
+*/
+
+package com.syrus.AMFICOM.general;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.syrus.AMFICOM.general.corba.IdlStorableObject;
+import com.syrus.util.LRUMap;
+import com.syrus.util.Log;
+
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/09/07 13:04:25 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module general
+ */
+final class ClientLRUMapSaver<V extends StorableObject> extends AbstractLRUMapSaver<V> {
+
+	private static ClientLRUMapSaver instance;
+
+	private ClientLRUMapSaver() {
+		super("SOLRUMap.serialized");
+	}
+	
+	public static final ClientLRUMapSaver getInstance() {
+		if (instance == null) {
+			synchronized (ClientLRUMapSaver.class) {
+				if (instance == null) {
+					instance = new ClientLRUMapSaver();
+				}
+			}
+		}
+		return instance;
+	}
+
+	@Override
+	protected Set<V> loading(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+		try {
+			return StorableObjectPool.<V>fromTransferables((IdlStorableObject[]) in.readObject(), false);
+		} catch (final ApplicationException ae) {
+			Log.errorMessage("ClientLRUMapSaver.load | Error: " + ae.getMessage());
+		}
+		return null;
+	}
+	
+	@Override
+	protected Object saving(LRUMap<Identifier, V> lruMap) {
+		final Set<Object> keys = new HashSet<Object>();
+		for(final V v : lruMap) {
+			keys.add(v.getTransferable(ClientSessionEnvironment.getInstance().getConnectionManager().getCORBAServer().getOrb()));
+		}
+		return keys.toArray(new IdlStorableObject[keys.size()]);
+	}
+	
+}
