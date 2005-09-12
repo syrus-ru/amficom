1cab87a39ec2179ec0ef76d81983120ca05dc32f
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-09-12 11:19:08 +0400

    FIX de-iconify frame

--- AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/client/model/ShowWindowCommand.java
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/client/model/ShowWindowCommand.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ShowWindowCommand.java,v 1.5 2005/09/08 14:25:57 bob Exp $
+ * $Id: ShowWindowCommand.java,v 1.6 2005/09/12 07:19:08 bob Exp $
  *
  * Copyright © 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -14,7 +14,7 @@ import java.beans.PropertyVetoException;
 import javax.swing.JInternalFrame;
 
 /**
- * @version $Revision: 1.5 $, $Date: 2005/09/08 14:25:57 $
+ * @version $Revision: 1.6 $, $Date: 2005/09/12 07:19:08 $
  * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module commonclient
@@ -30,11 +30,13 @@ public class ShowWindowCommand extends AbstractCommand {
 	@Override
 	public void execute() {
 		if (this.internalFrame != null) {
+			
 			if (!this.internalFrame.isVisible()) {
 				this.internalFrame.setVisible(true);
 			}
 			this.internalFrame.toFront();
 			try {
+				this.internalFrame.setIcon(false);
 				this.internalFrame.setSelected(true);
 			} catch (PropertyVetoException pve) {
 				// nothing
