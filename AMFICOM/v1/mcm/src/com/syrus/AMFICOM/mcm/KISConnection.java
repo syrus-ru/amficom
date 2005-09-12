042344c62eed8cab9ffe1001d9f03104432c214e
Arseniy Tashoyan <tashoyan@gmail.com>
2005-09-12 20:31:53 +0400

    COSMETIC: Head

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISConnection.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISConnection.java
@@ -1,9 +1,21 @@
+/*-
+ * $Id: KISConnection.java,v 1.7 2005/09/12 16:31:53 arseniy Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
 package com.syrus.AMFICOM.mcm;
 
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.CommunicationException;
 import com.syrus.AMFICOM.measurement.Measurement;
 
+/**
+ * @author $Author: arseniy $
+ * @version $Revision: 1.7 $, $Date: 2005/09/12 16:31:53 $
+ * @module mcm
+ */
 public interface KISConnection {
 
 	boolean isEstablished();
