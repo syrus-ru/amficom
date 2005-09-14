846aa9af9350c5658b29313d1d654e909787d4cf
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-09-14 14:39:43 +0400

    resource keys

---
+++ AMFICOM/v1/mapclient/src/com/syrus/AMFICOM/client/resource/MapEditorResourceKeys.java
@@ -0,0 +1,16 @@
+/**
+ * $Id: MapEditorResourceKeys.java,v 1.1 2005/09/14 10:39:43 krupenn Exp $
+ *
+ * Syrus Systems
+ * Научно-технический центр
+ * Проект: АМФИКОМ
+ */
+package com.syrus.AMFICOM.client.resource;
+
+import com.syrus.AMFICOM.client.resource.ResourceKeys;
+
+public interface MapEditorResourceKeys extends ResourceKeys {
+
+	String ICON_CATALOG = "icon.catalog";
+
+}
