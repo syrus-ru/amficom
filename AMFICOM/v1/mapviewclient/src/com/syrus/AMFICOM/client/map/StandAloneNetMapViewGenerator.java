1dc384ab62b5516c7acb37233d653e2675e819f1
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-09-16 12:18:33 +0400

    standalone generator of images for selected map element

---
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/StandAloneNetMapViewGenerator.java
@@ -0,0 +1,80 @@
+/**
+ * $Id: StandAloneNetMapViewGenerator.java,v 1.1 2005/09/16 08:18:33 krupenn Exp $
+ *
+ * Syrus Systems
+ * Научно-технический центр
+ * Проект: АМФИКОМ
+ */
+package com.syrus.AMFICOM.client.map;
+
+import java.awt.Dimension;
+import java.awt.Image;
+
+import javax.swing.JOptionPane;
+
+import com.syrus.AMFICOM.client.map.command.editor.ViewMapChooserCommand;
+import com.syrus.AMFICOM.client.map.controllers.MapViewController;
+import com.syrus.AMFICOM.client.model.ApplicationContext;
+import com.syrus.AMFICOM.client.model.MapReportApplicationModelFactory;
+import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.map.MapElement;
+import com.syrus.AMFICOM.mapview.MapView;
+
+public class StandAloneNetMapViewGenerator {
+
+	private static NetMapViewer netMapViewer;
+	private static MapConnection mapConnection;
+
+	public static Image getMapShot(MapView mapView, MapElement mapElement, Dimension dimension) throws MapConnectionException, MapDataException, ApplicationException {
+		if(StandAloneNetMapViewGenerator.netMapViewer == null) {
+			initialize();
+		}
+
+		StandAloneNetMapViewGenerator.netMapViewer.getLogicalNetLayer().setMapView(mapView);
+		StandAloneNetMapViewGenerator.netMapViewer.centerAndScale(mapElement, dimension);
+		StandAloneNetMapViewGenerator.netMapViewer.repaint(true);
+		return StandAloneNetMapViewGenerator.netMapViewer.getMapShot();
+	}
+
+	private static void initialize() throws MapConnectionException, MapDataException, ApplicationException {
+		mapConnection = MapConnection.create(MapPropertiesManager.getConnectionClassName());
+		mapConnection.setPath(MapPropertiesManager.getDataBasePath());
+		mapConnection.setView(MapPropertiesManager.getDataBaseView());
+		mapConnection.setURL(MapPropertiesManager.getDataBaseURL());
+			try {
+				mapConnection.connect();
+			} catch(MapConnectionException e) {
+				int result = ViewMapChooserCommand.chooseMap(mapConnection);
+				if(result == JOptionPane.OK_OPTION) {
+//					this.mapConnection.connect();
+				}
+				else {
+					throw e;
+				}
+			}
+
+		final MapImageLoader loader = mapConnection.createImageLoader();
+		final MapContext mapContext = mapConnection.createMapContext();
+		final MapCoordinatesConverter converter = mapConnection.createCoordinatesConverter();
+
+		final MapImageRenderer renderer = MapImageRendererFactory.create(MapPropertiesManager.getMapImageRendererClassName(),
+				converter,
+				mapContext,
+				loader);
+
+		ApplicationContext aContext = new ApplicationContext();
+		aContext.setApplicationModel(new MapReportApplicationModelFactory().create());
+		
+		final LogicalNetLayer logicalNetLayer = new LogicalNetLayer(aContext, converter, mapContext);
+
+		netMapViewer = NetMapViewer.create(MapPropertiesManager.getNetMapViewerClassName(), logicalNetLayer, mapContext, renderer);
+
+		netMapViewer.init();
+
+		logicalNetLayer.setMapViewController(MapViewController.createInstance(netMapViewer));
+
+		netMapViewer.setScale(MapPropertiesManager.getZoom());
+		netMapViewer.setCenter(MapPropertiesManager.getCenter());
+	}
+	
+}
