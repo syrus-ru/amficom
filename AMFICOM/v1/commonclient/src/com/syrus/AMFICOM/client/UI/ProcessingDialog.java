bc8217524228b6fe9d8aeeb0464ebbbaf2b0c5ca
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-09-19 11:56:30 +0400

    FIX getting and locking object from runnableTasks

--- AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/client/UI/ProcessingDialog.java
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/client/UI/ProcessingDialog.java
@@ -1,5 +1,5 @@
 /*-
-* $Id: ProcessingDialog.java,v 1.2 2005/09/18 14:43:42 bob Exp $
+* $Id: ProcessingDialog.java,v 1.3 2005/09/19 07:56:30 bob Exp $
 *
 * Copyright ¿ 2005 Syrus Systems.
 * Dept. of Science & Technology.
@@ -22,6 +22,7 @@ import javax.swing.JDialog;
 import javax.swing.JProgressBar;
 import javax.swing.WindowConstants;
 
+import com.syrus.AMFICOM.client.launcher.Launcher;
 import com.syrus.AMFICOM.client.model.Environment;
 import com.syrus.util.Log;
 
@@ -30,7 +31,7 @@ import com.syrus.util.Log;
  * 
  * Using as blocking (modal) dialog processing task 
  * 
- * @version $Revision: 1.2 $, $Date: 2005/09/18 14:43:42 $
+ * @version $Revision: 1.3 $, $Date: 2005/09/19 07:56:30 $
  * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module commonclient
@@ -50,9 +51,9 @@ public final class ProcessingDialog {
 			Log.DEBUGLEVEL10);
 		synchronized (LOCK) {
 			assert Log.debugMessage("ProcessingDialog.ProcessingDialog | LOCK " + new Date() + " " + title + '[' + threadName + ']',
-				Log.DEBUGLEVEL10);
-			runnableTaskNames.put(runnable, title);
+				Log.DEBUGLEVEL10);			
 			runnableTasks.add(runnable);
+			runnableTaskNames.put(runnable, title);
 		}
 		assert Log.debugMessage("ProcessingDialog.ProcessingDialog | after LOCK " + new Date() + " " + title + '[' + threadName + ']',
 			Log.DEBUGLEVEL10);
@@ -78,28 +79,37 @@ public final class ProcessingDialog {
 		modalDialog.setSize(screenSize.width / 3, modalDialog.getHeight());
 		modalDialog.setLocation((screenSize.width - modalDialog.getWidth())/2,
 				(screenSize.height - modalDialog.getHeight())/2);
+		
 		modalDialog.addComponentListener(new ComponentAdapter() {
 			@Override
 			public void componentShown(ComponentEvent e) {
 				new Thread() {
 					@Override
 					public void run() {
-						for(int i = 0; i < runnableTasks.size(); i++) {
-							final Runnable runnable = runnableTasks.get(i);
-							final String title = runnableTaskNames.get(runnable);
+						
+						final String threadName = currentThread().getName();
+						
+						while(!runnableTasks.isEmpty()) {
+							final Runnable runnable;
+							final String title;							
+							assert Log.debugMessage(".run | before LOCK " + new Date()  + '[' + threadName + ']' , Log.DEBUGLEVEL10);
+							synchronized (LOCK) {
+								assert Log.debugMessage(".run | LOCK " + new Date() + '[' + threadName + ']', Log.DEBUGLEVEL10);
+								runnable = runnableTasks.remove(0);
+								title = runnableTaskNames.remove(runnable);
+							}
+							assert Log.debugMessage(".run | after LOCK " + new Date() + '[' + threadName + ']', Log.DEBUGLEVEL10);
 							modalDialog.setTitle(title);
 							progressBar.setString(title);
-							runnable.run();
-						}
-						final String threadName = currentThread().getName();
-						assert Log.debugMessage(".run | before LOCK " + new Date()  + '[' + threadName + ']' , Log.DEBUGLEVEL10);
-						synchronized (LOCK) {
-							assert Log.debugMessage(".run | LOCK " + new Date() + '[' + threadName + ']', Log.DEBUGLEVEL10);
-							runnableTasks.clear();
-							runnableTaskNames.clear();							
+							try {
+								runnable.run();
+							} catch(final Throwable throwable) {
+								// too unlikely
+								new Launcher.DefaultThrowableHandler().handle(throwable);
+							}
 						}
 						modalDialog.dispose();
-						assert Log.debugMessage(".run | after LOCK " + new Date() + '[' + threadName + ']', Log.DEBUGLEVEL10);
+						
 					}
 				}.start();								
 			}
