b4dae37d630441da35fc34cb3669f9436ccc1817
Arseniy Tashoyan <tashoyan@gmail.com>
2005-09-20 15:16:39 +0400

    MINOR

--- AMFICOM/v1/leserver/leserver.sh
+++ AMFICOM/v1/leserver/leserver.sh
@@ -5,8 +5,16 @@ JAVA=java
 LIB=$HOME/lib
 EXTLIB=$HOME/extlib
 
-ORACLECLASSPATH=$ORACLE_HOME/jdbc/lib/ojdbc14_g.jar:$ORACLE_HOME/jdbc/lib/orai18n.jar:$ORACLE_HOME/jdbc/lib/classes12.jar:$ORACLE_HOME/jdbc/lib/nls_charset12.jar
+ORACLECLASSPATH=$ORACLE_HOME/jdbc/lib/ojdbc14_g.jar:\
+$ORACLE_HOME/jdbc/lib/orai18n.jar:\
+$ORACLE_HOME/jdbc/lib/classes12.jar:\
+$ORACLE_HOME/jdbc/lib/nls_charset12.jar
+
 TROVECLASSPATH=$EXTLIB/trove.jar
+
+XMLCLASSPATH=$EXTLIB/xbean.jar:\
+$LIB/generalxml.jar
+
 APPCLASSPATH=\
 $LIB/leserver.jar:\
 $LIB/general.jar:\
@@ -15,5 +23,5 @@ $LIB/event.jar:\
 $LIB/csbridge.jar:\
 $LIB/util.jar
 
-$JAVA -server -ea -classpath $APPCLASSPATH:$ORACLECLASSPATH:$TROVECLASSPATH com.syrus.AMFICOM.leserver.LoginEventServer &
+$JAVA -server -ea -classpath $APPCLASSPATH:$XMLCLASSPATH:$ORACLECLASSPATH:$TROVECLASSPATH com.syrus.AMFICOM.leserver.LoginEventServer &
 echo $! > `dirname $0`/leserver.pid
