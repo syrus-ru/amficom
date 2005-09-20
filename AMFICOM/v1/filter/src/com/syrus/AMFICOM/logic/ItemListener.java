596b2b83aec29e4e2be1b458172363a5332fcc81
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-09-20 16:31:36 +0400

    COSMETICAL code style

--- AMFICOM/v1/filter/src/com/syrus/AMFICOM/logic/ItemListener.java
+++ AMFICOM/v1/filter/src/com/syrus/AMFICOM/logic/ItemListener.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ItemListener.java,v 1.5 2005/08/08 11:37:22 arseniy Exp $
+ * $Id: ItemListener.java,v 1.6 2005/09/20 12:31:36 bob Exp $
  *
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,8 +9,8 @@
 package com.syrus.AMFICOM.logic;
 
 /**
- * @version $Revision: 1.5 $, $Date: 2005/08/08 11:37:22 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.6 $, $Date: 2005/09/20 12:31:36 $
+ * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module filter
  */
@@ -18,5 +18,5 @@ public interface ItemListener {
 
 	void setParentPerformed(final Item item, final Item oldParent, final Item newParent);
 	
-	void setObjectNameChanged(Item item, String oldName, String newName);
+	void setObjectNameChanged(final Item item, final String oldName, final String newName);
 }
