3962931d4306cbfe38720394ad5956bd24f5f548
Arseniy Tashoyan <tashoyan@gmail.com>
2005-09-21 03:18:44 +0400

    ADD: Handle case of CommunicationException during result creation

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISReport.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISReport.java
@@ -1,5 +1,5 @@
 /*
- * $Id: KISReport.java,v 1.53 2005/09/20 16:29:21 arseniy Exp $
+ * $Id: KISReport.java,v 1.54 2005/09/20 23:18:44 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -9,6 +9,7 @@
 package com.syrus.AMFICOM.mcm;
 
 import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.CommunicationException;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.IllegalObjectEntityException;
 import com.syrus.AMFICOM.general.LoginManager;
@@ -19,7 +20,7 @@ import com.syrus.AMFICOM.measurement.Parameter;
 import com.syrus.AMFICOM.measurement.Result;
 
 /**
- * @version $Revision: 1.53 $, $Date: 2005/09/20 16:29:21 $
+ * @version $Revision: 1.54 $, $Date: 2005/09/20 23:18:44 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module mcm
@@ -52,6 +53,9 @@ final class KISReport {
 		catch (ApplicationException ae) {
 			if (ae.getCause() instanceof IllegalObjectEntityException) {
 				throw new MeasurementException(ae.getMessage(), MeasurementException.IDENTIFIER_GENERATION_FAILED_CODE, ae);
+			}
+			if (ae instanceof CommunicationException) {
+				throw new MeasurementException(ae.getMessage(), MeasurementException.COMMUNICATION_FAILED_CODE, ae);
 			}
 			throw new MeasurementException(ae.getMessage(), MeasurementException.DATABASE_CALL_FAILED_CODE, ae);
 		}
