221fc08fb32b33afb6beebbc9584b29ad804677a
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-09-21 17:42:21 +0400

    COSMETIC:

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/Characterizable.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/Characterizable.java
@@ -1,9 +1,9 @@
-/*
- * $Id: Characterizable.java,v 1.11 2005/09/14 18:51:55 arseniy Exp $
+/*-
+ * $Id: Characterizable.java,v 1.12 2005/09/21 13:42:21 bass Exp $
  *
- * Copyright © 2004 Syrus Systems.
- * Научно-технический центр.
- * Проект: АМФИКОМ.
+ * Copyright © 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
  */
 
 package com.syrus.AMFICOM.general;
@@ -11,14 +11,16 @@ package com.syrus.AMFICOM.general;
 import java.util.Set;
 
 /**
- * @version $Revision: 1.11 $, $Date: 2005/09/14 18:51:55 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.12 $, $Date: 2005/09/21 13:42:21 $
+ * @author $Author: bass $
  * @author Tashoyan Arseniy Feliksovich
  * @module general
  */
-
 public interface Characterizable extends Identifiable {
-
-	Set<Characteristic> getCharacteristics(final boolean usePool) throws ApplicationException;
-
+	/**
+	 * @param usePool
+	 * @throws ApplicationException
+	 */
+	Set<Characteristic> getCharacteristics(final boolean usePool)
+	throws ApplicationException;
 }
