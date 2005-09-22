2507a22634f11a7bfc4a6f497cce74e27cf43ed3
Arseniy Tashoyan <tashoyan@gmail.com>
2005-09-22 16:30:18 +0400

    ADD: New test

---
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestTest.java
@@ -0,0 +1,52 @@
+/*-
+ * $Id: TestTest.java,v 1.1 2005/09/22 12:30:18 arseniy Exp $
+ *
+ * Copyright © 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.AMFICOM.measurement;
+
+import java.util.Date;
+import java.util.SortedMap;
+
+import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.DatabaseCommonTest;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.StorableObjectPool;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/09/22 12:30:18 $
+ * @author $Author: arseniy $
+ * @author Tashoyan Arseniy Feliksovich
+ * @module test
+ */
+public final class TestTest extends TestCase {
+
+	public TestTest(final String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		final DatabaseCommonTest commonTest = new DatabaseCommonTest();
+		commonTest.addTestSuite(TestTest.class);
+		return commonTest.createTestSetup();
+	}
+
+	public void testAddStops() throws ApplicationException {
+		final Identifier testId = new Identifier("Test_321");//146085512912830785
+		final com.syrus.AMFICOM.measurement.Test test = StorableObjectPool.getStorableObject(testId, true);
+		System.out.println("Loaded: " + test.toString() + ", status: " + test.getStatus().value());
+		System.out.println("Stops: " + test.getStoppingMap());
+
+		test.addStopping("Надо есчо");
+
+		System.out.println("Stops: " + test.getStoppingMap());
+
+		StorableObjectPool.flush(test, DatabaseCommonTest.getSysUser().getId(), false);
+	}
+
+}
