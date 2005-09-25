2d0e63b7a0b8857ff659eb19c775fa711ab29322
Arseniy Tashoyan <tashoyan@gmail.com>
2005-09-25 14:54:40 +0400

    PERFOMANCE: Method 'createResult' now caches result, so it is renamed to 'getResult'. Throws ApplicationException.

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISReport.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISReport.java
@@ -1,5 +1,5 @@
 /*
- * $Id: KISReport.java,v 1.55 2005/09/21 15:21:51 arseniy Exp $
+ * $Id: KISReport.java,v 1.56 2005/09/25 10:54:40 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -9,9 +9,7 @@
 package com.syrus.AMFICOM.mcm;
 
 import com.syrus.AMFICOM.general.ApplicationException;
-import com.syrus.AMFICOM.general.CommunicationException;
 import com.syrus.AMFICOM.general.Identifier;
-import com.syrus.AMFICOM.general.IllegalObjectEntityException;
 import com.syrus.AMFICOM.general.LoginManager;
 import com.syrus.AMFICOM.general.ParameterType;
 import com.syrus.AMFICOM.general.StorableObjectPool;
@@ -20,7 +18,7 @@ import com.syrus.AMFICOM.measurement.Parameter;
 import com.syrus.AMFICOM.measurement.Result;
 
 /**
- * @version $Revision: 1.55 $, $Date: 2005/09/21 15:21:51 $
+ * @version $Revision: 1.56 $, $Date: 2005/09/25 10:54:40 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module mcm
@@ -31,33 +29,30 @@ final class KISReport {
 	private String[] parameterCodenames;
 	private byte[][] parameterValues;
 
+	private Result result;
+
 	public KISReport(final String measurementIdStr, final String[] parameterCodenames, final byte[][] parameterValues) {
 		this.measurementId = new Identifier(measurementIdStr);
 		this.parameterCodenames = parameterCodenames;
 		this.parameterValues = parameterValues;
+		
+		this.result = null;
 	}
 
-	Result createResult() throws MeasurementException {
-		try {
-			final Measurement measurement = (Measurement) StorableObjectPool.getStorableObject(this.measurementId, true);
-
-			final Parameter[] parameters = new Parameter[this.parameterCodenames.length];
-			for (int i = 0; i < parameters.length; i++) {
-				parameters[i] = Parameter.createInstance(ParameterType.REFLECTOGRAMMA, this.parameterValues[i]);
-			}
-
-			final Result result = measurement.createResult(LoginManager.getUserId(), parameters);
-			StorableObjectPool.flush(result, LoginManager.getUserId(), false);
-			return result;
-		} catch (ApplicationException ae) {
-			if (ae.getCause() instanceof IllegalObjectEntityException) {
-				throw new MeasurementException(ae.getMessage(), MeasurementException.IDENTIFIER_GENERATION_FAILED_CODE, ae);
-			}
-			if (ae instanceof CommunicationException) {
-				throw new MeasurementException(ae.getMessage(), MeasurementException.COMMUNICATION_FAILED_CODE, ae);
-			}
-			throw new MeasurementException(ae.getMessage(), MeasurementException.DATABASE_CALL_FAILED_CODE, ae);
+	Result getResult() throws ApplicationException {
+		if (this.result != null) {
+			return this.result;
 		}
+
+		final Measurement measurement = StorableObjectPool.getStorableObject(this.measurementId, true);
+
+		final Parameter[] parameters = new Parameter[this.parameterCodenames.length];
+		for (int i = 0; i < parameters.length; i++) {
+			parameters[i] = Parameter.createInstance(ParameterType.REFLECTOGRAMMA, this.parameterValues[i]);
+		}
+
+		this.result = measurement.createResult(LoginManager.getUserId(), parameters);
+		return this.result;
 	}
 
 	public Identifier getMeasurementId() {
