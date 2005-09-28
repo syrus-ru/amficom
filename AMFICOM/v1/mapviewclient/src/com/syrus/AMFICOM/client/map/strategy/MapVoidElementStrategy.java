eab4b207bdc69068e1119762bcdfd7643ec4afdd
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-09-28 19:11:03 +0400

    MAJOR: selectElementsInRect() takes into account only visible elements from logicalNetLayer COSMETIC: formatting

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/strategy/MapVoidElementStrategy.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/strategy/MapVoidElementStrategy.java
@@ -1,19 +1,19 @@
 /**
- * $Id: MapVoidElementStrategy.java,v 1.34 2005/09/16 14:53:36 krupenn Exp $
+ * $$Id: MapVoidElementStrategy.java,v 1.35 2005/09/28 15:11:03 krupenn Exp $$
  *
- * Syrus Systems
- * Научно-технический центр
- * Проект: АМФИКОМ Автоматизированный МногоФункциональный
- *         Интеллектуальный Комплекс Объектного Мониторинга
- *
- * Платформа: java 1.4.1
+ * Copyright 2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
  */
 
 package com.syrus.AMFICOM.client.map.strategy;
 
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 
@@ -28,16 +28,17 @@ import com.syrus.AMFICOM.map.PhysicalLink;
 import com.syrus.AMFICOM.mapview.MapView;
 import com.syrus.AMFICOM.mapview.Selection;
 import com.syrus.AMFICOM.mapview.VoidElement;
+import com.syrus.AMFICOM.resource.DoublePoint;
 import com.syrus.util.Log;
 
 /**
  * Стратегия управления элементами, когда нет выбранных элементов.
+ * 
  * @author $Author: krupenn $
- * @version $Revision: 1.34 $, $Date: 2005/09/16 14:53:36 $
+ * @version $Revision: 1.35 $, $Date: 2005/09/28 15:11:03 $
  * @module mapviewclient
  */
-public final class MapVoidElementStrategy extends AbstractMapStrategy 
-{
+public final class MapVoidElementStrategy extends AbstractMapStrategy {
 	/**
 	 * Топологическая схема.
 	 */
@@ -55,26 +56,25 @@ public final class MapVoidElementStrategy extends AbstractMapStrategy
 	/**
 	 * Private constructor.
 	 */
-	private MapVoidElementStrategy()
-	{//empty
+	private MapVoidElementStrategy() {
+		// empty
 	}
 
 	/**
 	 * Get instance.
+	 * 
 	 * @return instance
 	 */
-	public static MapVoidElementStrategy getInstance()
-	{
+	public static MapVoidElementStrategy getInstance() {
 		return instance;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void setMapElement(MapElement me)
-	{
-		this.mapView = ((VoidElement)me).getMapView();
+	public void setMapElement(MapElement me) {
+		this.mapView = ((VoidElement) me).getMapView();
 		this.map = this.mapView.getMap();
 	}
 
@@ -83,15 +83,12 @@ public final class MapVoidElementStrategy extends AbstractMapStrategy
 	 */
 	@Override
 	protected void leftMousePressed(MapState mapState, Point point)
-		throws MapConnectionException, MapDataException
-	{
+			throws MapConnectionException, MapDataException {
 		int actionMode = mapState.getActionMode();
 
-		if (actionMode == MapState.NULL_ACTION_MODE)
-		{
+		if(actionMode == MapState.NULL_ACTION_MODE) {
 			super.logicalNetLayer.deselectAll();
-//			this.netMapViewer.getLogicalNetLayer().sendSelectionChangeEvent();
-		}//MapState.NULL_ACTION_MODE
+		}// MapState.NULL_ACTION_MODE
 	}
 
 	/**
@@ -99,16 +96,14 @@ public final class MapVoidElementStrategy extends AbstractMapStrategy
 	 */
 	@Override
 	protected void leftMouseDragged(MapState mapState, Point point)
-		throws MapConnectionException, MapDataException
-	{
+			throws MapConnectionException, MapDataException {
 		int actionMode = mapState.getActionMode();
 		int operationMode = mapState.getOperationMode();
 
-		if (actionMode == MapState.NULL_ACTION_MODE &&
-			operationMode == MapState.NO_OPERATION)
-		{
+		if(actionMode == MapState.NULL_ACTION_MODE
+				&& operationMode == MapState.NO_OPERATION) {
 			mapState.setActionMode(MapState.SELECT_MARKER_ACTION_MODE);
-		}//MapState.NULL_ACTION_MODE && MapState.NO_OPERATION
+		}// MapState.NULL_ACTION_MODE && MapState.NO_OPERATION
 	}
 
 	/**
@@ -116,14 +111,12 @@ public final class MapVoidElementStrategy extends AbstractMapStrategy
 	 */
 	@Override
 	protected void leftMouseReleased(MapState mapState, Point point)
-		throws MapConnectionException, MapDataException
-	{
+			throws MapConnectionException, MapDataException {
 		int actionMode = mapState.getActionMode();
 		int operationMode = mapState.getOperationMode();
 
-		if (actionMode == MapState.SELECT_MARKER_ACTION_MODE &&
-			operationMode == MapState.NO_OPERATION)
-		{
+		if(actionMode == MapState.SELECT_MARKER_ACTION_MODE
+				&& operationMode == MapState.NO_OPERATION) {
 			int startX = super.logicalNetLayer.getStartPoint().x;
 			int startY = super.logicalNetLayer.getStartPoint().y;
 			int endX = point.x;
@@ -136,100 +129,93 @@ public final class MapVoidElementStrategy extends AbstractMapStrategy
 			selectElementsInRect(selectionRect);
 
 			mapState.setActionMode(MapState.NULL_ACTION_MODE);
-		}//MapState.SELECT_MARKER_ACTION_MODE && MapState.NO_OPERATION
+		}// MapState.SELECT_MARKER_ACTION_MODE && MapState.NO_OPERATION
 	}
 
 	/**
 	 * Выделить все элементы, попадающие в область.
+	 * 
 	 * @param selectionRect область выборки в экранных координатах
 	 */
-	protected  void selectElementsInRect(Rectangle selectionRect)
-		throws MapConnectionException, MapDataException
-	{
+	protected void selectElementsInRect(Rectangle selectionRect)
+			throws MapConnectionException, MapDataException {
 		long t1 = System.nanoTime();
-		//Здесь просто проверяется что элемент содержится в прямоугольной области
-		Iterator e = this.map.getNodes().iterator();
-		
-		//Пробегаем и смотрим вхотит ли в область MapNodeElement
-		while (e.hasNext())
-		{
-			AbstractNode node = (AbstractNode)e.next();
-			Point p = super.logicalNetLayer.getConverter().convertMapToScreen(node.getLocation());
+		// Здесь просто проверяется что элемент содержится в прямоугольной
+		// области
+		Point topleft = new Point((int)selectionRect.getMinX(), (int)selectionRect.getMinY());
+		Point bottomright = new Point((int)selectionRect.getMaxX(), (int)selectionRect.getMaxY());
+		DoublePoint tl = super.logicalNetLayer.getConverter().convertScreenToMap(topleft);
+		DoublePoint br = super.logicalNetLayer.getConverter().convertScreenToMap(bottomright);
+		Rectangle2D.Double visibleBounds = new Rectangle2D.Double(
+				Math.min(tl.getX(), br.getX()),
+				Math.min(tl.getY(), br.getY()),
+				Math.abs(br.getX() - tl.getX()),
+				Math.abs(br.getY() - tl.getY()));
 
-			if (selectionRect.contains(p))
-			{
-				this.map.setSelected(node, true);
-			}
-			else
-			{
-				this.map.setSelected(node, false);
-			}
+		this.map.clearSelection();
+
+		for(AbstractNode abstractNode : this.logicalNetLayer.getVisibleNodes(visibleBounds)) {
+			this.map.setSelected(abstractNode, true);
 		}
 
-		e = this.map.getAllNodeLinks().iterator();
-
-		if(super.logicalNetLayer.getMapState().getShowMode() == MapState.SHOW_NODE_LINK)
-		{
-			//Пробегаем и смотрим вхотит ли в область nodeLink
-			while (e.hasNext() )
-			{
-				NodeLink nodeLink = (NodeLink)e.next();
-				if (
-					selectionRect.contains(
-						super.logicalNetLayer.getConverter().convertMapToScreen(
-							nodeLink.getStartNode().getLocation())) 
-					&& selectionRect.contains(
-						super.logicalNetLayer.getConverter().convertMapToScreen(
-							nodeLink.getEndNode().getLocation())))
-				{
-					this.map.setSelected(nodeLink, true);
-				}
-				else
-				{
-					this.map.setSelected(nodeLink, false);
-				}
-			}
-		}
-		else
-		if(super.logicalNetLayer.getMapState().getShowMode() == MapState.SHOW_PHYSICAL_LINK)
-		{
-			for(Iterator it = this.map.getAllPhysicalLinks().iterator(); it.hasNext();)
-			{
-				PhysicalLink link = (PhysicalLink)it.next();
-				boolean select = true;
-				for(Iterator it2 = link.getNodeLinks().iterator(); it2.hasNext();)
-				{
-					NodeLink nodeLink = (NodeLink)it2.next();
-					if (! (
-						selectionRect.contains(
-							super.logicalNetLayer.getConverter().convertMapToScreen(
-								nodeLink.getStartNode().getLocation())) 
-						&& selectionRect.contains(
-							super.logicalNetLayer.getConverter().convertMapToScreen(
-								nodeLink.getEndNode().getLocation()))))
-					{ 
-						select = false;
+		int showMode = super.logicalNetLayer.getMapState().getShowMode();
+		List<PhysicalLink> checkedPhysicalLinks = null;
+		for(NodeLink nodeLink : this.logicalNetLayer.getVisibleNodeLinks(visibleBounds)) {
+			switch(showMode) {
+				case MapState.SHOW_NODE_LINK: {
+					// Пробегаем и смотрим вхотит ли в область nodeLink
+					DoublePoint startNodeLocation = nodeLink.getStartNode().getLocation();
+					DoublePoint endNodeLocation = nodeLink.getEndNode().getLocation();
+					if(visibleBounds.contains(
+								startNodeLocation.getX(),
+								startNodeLocation.getY())
+							&& visibleBounds.contains(
+									endNodeLocation.getX(),
+									endNodeLocation.getY())) {
+						this.map.setSelected(nodeLink, true);
 					}
 				}
-				this.map.setSelected(link, select);
+				case MapState.SHOW_PHYSICAL_LINK: {
+					if(checkedPhysicalLinks == null) {
+						checkedPhysicalLinks = new LinkedList<PhysicalLink>();
+					}
+					PhysicalLink link = nodeLink.getPhysicalLink();
+					if(!checkedPhysicalLinks.contains(link)) {
+						boolean select = true;
+						for(NodeLink tmpNodeLink : link.getNodeLinks()) {
+							DoublePoint startNodeLocation = tmpNodeLink.getStartNode().getLocation();
+							DoublePoint endNodeLocation = tmpNodeLink.getEndNode().getLocation();
+							if(!visibleBounds.contains(
+									startNodeLocation.getX(),
+									startNodeLocation.getY())
+								|| !visibleBounds.contains(
+										endNodeLocation.getX(),
+										endNodeLocation.getY())) {
+								select = false;
+							}
+						}
+						this.map.setSelected(link, select);
+						checkedPhysicalLinks.add(link);
+					}
+				}
+				default:
+					//nothing
 			}
 		}
-		
+
 		Set selection = super.logicalNetLayer.getSelectedElements();
-		if(selection.size() == 1)
-		{
-			MapElement me = (MapElement)selection.iterator().next();
+		if(selection.size() == 1) {
+			MapElement me = (MapElement) selection.iterator().next();
 			super.logicalNetLayer.setCurrentMapElement(me);
-		}
-		else
-		if(selection.size() > 1)
-		{
+		} else if(selection.size() > 1) {
 			Selection sel = new Selection();
 			sel.addAll(selection);
 			super.logicalNetLayer.setCurrentMapElement(sel);
 		}
 		long t2 = System.nanoTime();
-		Log.debugMessage("MapVoidElementStrategy.selectElementsInRect | " + (t2 - t1) + " ns ", Level.FINE); //$NON-NLS-1$ //$NON-NLS-2$
+		Log.debugMessage(
+			"MapVoidElementStrategy.selectElementsInRect | " //$NON-NLS-1$ 
+				+ (t2 - t1) + " ns ", //$NON-NLS-1$ 
+			Level.FINE);
 	}
 }
-
