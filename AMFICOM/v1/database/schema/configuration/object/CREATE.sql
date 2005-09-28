a81c69ce512a6a6519b329248e6de94af6b7ef4a
Arseniy Tashoyan <tashoyan@gmail.com>
2005-09-28 15:03:01 +0400

    ADD: New entity ProtoEquipment

--- AMFICOM/v1/database/schema/configuration/object/CREATE.sql
+++ AMFICOM/v1/database/schema/configuration/object/CREATE.sql
@@ -1,7 +1,8 @@
--- $Id: CREATE.sql,v 1.16 2005/08/28 14:29:24 arseniy Exp $
+-- $Id: CREATE.sql,v 1.17 2005/09/28 11:03:01 arseniy Exp $
 
 @@link;
 @@cablelink;
+@@protoequipment;
 @@equipment;
 @@port;
 @@transmissionpath;
--- AMFICOM/v1/database/schema/configuration/object/DROP.sql
+++ AMFICOM/v1/database/schema/configuration/object/DROP.sql
@@ -1,13 +1,15 @@
--- $Id: DROP.sql,v 1.16 2005/08/28 14:29:24 arseniy Exp $
+-- $Id: DROP.sql,v 1.17 2005/09/28 11:03:01 arseniy Exp $
 
 DROP TABLE Transmissionpath;
 DROP TABLE Port;
 DROP TABLE Equipment;
+DROP TABLE ProtoEquipment;
 DROP TABLE CableLink;
 DROP TABLE Link;
 
 DROP SEQUENCE TransmissionPath_seq;
 DROP SEQUENCE Port_seq;
 DROP SEQUENCE Equipment_seq;
+DROP SEQUENCE ProtoEquipment_seq;
 DROP SEQUENCE CableLink_Seq;
 DROP SEQUENCE Link_Seq;
