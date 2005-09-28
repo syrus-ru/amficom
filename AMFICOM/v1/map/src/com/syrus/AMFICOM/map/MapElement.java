aff87a2b59567f2aecd0868c20e69e7bd7039b98
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-09-28 23:15:23 +0400

    COSMETIC:

--- AMFICOM/v1/map/src/com/syrus/AMFICOM/map/MapElement.java
+++ AMFICOM/v1/map/src/com/syrus/AMFICOM/map/MapElement.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: MapElement.java,v 1.24 2005/09/28 14:48:14 krupenn Exp $
+ * $Id: MapElement.java,v 1.25 2005/09/28 19:15:23 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,10 +8,6 @@
 
 package com.syrus.AMFICOM.map;
 
-import java.util.Set;
-
-import com.syrus.AMFICOM.general.ApplicationException;
-import com.syrus.AMFICOM.general.Characteristic;
 import com.syrus.AMFICOM.general.Characterizable;
 import com.syrus.AMFICOM.general.Identifiable;
 import com.syrus.AMFICOM.general.Namable;
@@ -23,8 +19,8 @@ import com.syrus.AMFICOM.resource.DoublePoint;
  *
  *
  *
- * @author $Author: krupenn $
- * @version $Revision: 1.24 $, $Date: 2005/09/28 14:48:14 $
+ * @author $Author: bass $
+ * @version $Revision: 1.25 $, $Date: 2005/09/28 19:15:23 $
  * @module map
  */
 public interface MapElement extends Identifiable, Namable {
