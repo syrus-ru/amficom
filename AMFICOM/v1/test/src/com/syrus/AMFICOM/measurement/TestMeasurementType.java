562e753fe6c69f51377284a7ba4a70dd86948cad
Arseniy Tashoyan <tashoyan@gmail.com>
2005-09-29 12:03:21 +0400

    COSMETIC: Code style

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestMeasurementType.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestMeasurementType.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestMeasurementType.java,v 1.10 2005/08/29 10:00:32 arseniy Exp $
+ * $Id: TestMeasurementType.java,v 1.11 2005/09/29 08:03:21 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -14,13 +14,13 @@ import com.syrus.AMFICOM.general.CreateObjectException;
 import com.syrus.AMFICOM.general.SQLCommonTest;
 
 /**
- * @version $Revision: 1.10 $, $Date: 2005/08/29 10:00:32 $
+ * @version $Revision: 1.11 $, $Date: 2005/09/29 08:03:21 $
  * @author $Author: arseniy $
  * @module test
  */
 public class TestMeasurementType extends TestCase {
 
-	public TestMeasurementType(String name) {
+	public TestMeasurementType(final String name) {
 		super(name);
 	}
 
