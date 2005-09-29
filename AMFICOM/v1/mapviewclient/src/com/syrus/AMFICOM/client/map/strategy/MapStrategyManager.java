d08805b1482d4658f1afa87f13717b83ee70d718
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-09-29 15:39:40 +0400

    COSMETIC: header, formatting, parametrized java.util.Map, implicit imports

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/strategy/MapStrategyManager.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/strategy/MapStrategyManager.java
@@ -1,81 +1,78 @@
-/**
- * $Id: MapStrategyManager.java,v 1.14 2005/08/11 12:43:32 arseniy Exp $
+/*-
+ * $Id: MapStrategyManager.java,v 1.15 2005/09/29 11:39:40 krupenn Exp $
  *
- * Syrus Systems
- * Научно-технический центр
- * Проект: АМФИКОМ Автоматизированный МногоФункциональный
- *         Интеллектуальный Комплекс Объектного Мониторинга
- *
- * Платформа: java 1.4.1
+ * Copyright ї 2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
  */
 
 package com.syrus.AMFICOM.client.map.strategy;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import com.syrus.AMFICOM.map.MapElement;
+import com.syrus.AMFICOM.map.Mark;
+import com.syrus.AMFICOM.map.NodeLink;
+import com.syrus.AMFICOM.map.PhysicalLink;
+import com.syrus.AMFICOM.map.SiteNode;
+import com.syrus.AMFICOM.map.TopologicalNode;
+import com.syrus.AMFICOM.mapview.AlarmMarker;
+import com.syrus.AMFICOM.mapview.CablePath;
+import com.syrus.AMFICOM.mapview.EventMarker;
+import com.syrus.AMFICOM.mapview.Marker;
+import com.syrus.AMFICOM.mapview.MeasurementPath;
+import com.syrus.AMFICOM.mapview.Selection;
+import com.syrus.AMFICOM.mapview.UnboundNode;
+import com.syrus.AMFICOM.mapview.VoidElement;
 
 /**
  * Хранилище стратегий для элементов карты.
- * @author $Author: arseniy $
- * @version $Revision: 1.14 $, $Date: 2005/08/11 12:43:32 $
+ * 
+ * @author $Author: krupenn $
+ * @author Andrei Kroupennikov
+ * @version $Revision: 1.15 $, $Date: 2005/09/29 11:39:40 $
  * @module mapviewclient
  */
-public final class MapStrategyManager 
-{
+public final class MapStrategyManager {
 	/**
 	 * Конструктор закрыт, так как в классе используются только статические
 	 * методы.
 	 */
-	private MapStrategyManager()
-	{//empty
+	private MapStrategyManager() {
+		// empty
 	}
 
 	/**
 	 * Таблица стратегий.
 	 */
-	private static java.util.Map strategyMap = new HashMap();
+	private static Map<Class, MapStrategy> strategyMap = new HashMap<Class, MapStrategy>();
 
-	static
-	{
-		strategyMap.put(com.syrus.AMFICOM.mapview.AlarmMarker.class,
-			MapAlarmMarkerStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.mapview.CablePath.class,
-			MapCablePathElementStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.mapview.EventMarker.class,
-			MapEventMarkerStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.map.Mark.class,
-			MapMarkElementStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.map.NodeLink.class,
-			MapNodeLinkElementStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.mapview.Marker.class,
-			MapMarkerStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.mapview.MeasurementPath.class,
-			MapPathElementStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.map.PhysicalLink.class,
-			MapPhysicalLinkElementStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.map.TopologicalNode.class,
-			MapPhysicalNodeElementStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.map.SiteNode.class,
-			MapSiteNodeElementStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.mapview.UnboundNode.class,
-			MapUnboundNodeElementStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.mapview.VoidElement.class,
-			MapVoidElementStrategy.getInstance());
-		strategyMap.put(com.syrus.AMFICOM.mapview.Selection.class,
-			MapSelectionElementStrategy.getInstance());
+	static {
+		strategyMap.put(AlarmMarker.class, MapAlarmMarkerStrategy.getInstance());
+		strategyMap.put(CablePath.class, MapCablePathElementStrategy.getInstance());
+		strategyMap.put(EventMarker.class, MapEventMarkerStrategy.getInstance());
+		strategyMap.put(Mark.class, MapMarkElementStrategy.getInstance());
+		strategyMap.put(NodeLink.class, MapNodeLinkElementStrategy.getInstance());
+		strategyMap.put(Marker.class, MapMarkerStrategy.getInstance());
+		strategyMap.put(MeasurementPath.class, MapPathElementStrategy.getInstance());
+		strategyMap.put(PhysicalLink.class, MapPhysicalLinkElementStrategy.getInstance());
+		strategyMap.put(TopologicalNode.class, MapPhysicalNodeElementStrategy.getInstance());
+		strategyMap.put(SiteNode.class, MapSiteNodeElementStrategy.getInstance());
+		strategyMap.put(UnboundNode.class, MapUnboundNodeElementStrategy.getInstance());
+		strategyMap.put(VoidElement.class, MapVoidElementStrategy.getInstance());
+		strategyMap.put(Selection.class, MapSelectionElementStrategy.getInstance());
 	}
-	
+
 	/**
 	 * Получить стратегию действий с элементом карты.
+	 * 
 	 * @param mapElement элемент карты
 	 * @return стратегия
 	 */
-	public static MapStrategy getStrategy(MapElement mapElement)
-	{
-		MapStrategy strategy = (MapStrategy )strategyMap.get(mapElement.getClass());
-		if(strategy != null)
-		{
+	public static MapStrategy getStrategy(MapElement mapElement) {
+		MapStrategy strategy = strategyMap.get(mapElement.getClass());
+		if(strategy != null) {
 			strategy.setMapElement(mapElement);
 		}
 		return strategy;
