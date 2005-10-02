2e1124d8efb1a733f39be56e75cfe90d8f285602
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-10-02 16:33:00 +0400

    PERFORMANCE: do not use Vector COSMETIC

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/LineComboBox.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/ui/LineComboBox.java
@@ -1,5 +1,5 @@
 /*-
- * $$Id: LineComboBox.java,v 1.6 2005/09/30 16:08:41 krupenn Exp $$
+ * $$Id: LineComboBox.java,v 1.7 2005/10/02 12:33:00 krupenn Exp $$
  *
  * Copyright 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -20,7 +20,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
-import java.util.Vector;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
@@ -32,58 +31,59 @@ import javax.swing.ListCellRenderer;
 import com.syrus.AMFICOM.client.UI.AComboBox;
 
 /**
- * @version $Revision: 1.6 $, $Date: 2005/09/30 16:08:41 $
+ * @version $Revision: 1.7 $, $Date: 2005/10/02 12:33:00 $
  * @author $Author: krupenn $
  * @author Andrei Kroupennikov
  * @module mapviewclient
  */
-public class LineComboBox extends AComboBox
-{
-	public static Vector vec = new Vector();
+public class LineComboBox extends AComboBox {
+	public static LineStyleHolder[] presetLines;
 	static int lineWidth = 3;
 
 	public static boolean do_init = true;
 
 	{
-		if(do_init)
-		{
-			vec.addElement( new MyLine(new BasicStroke(lineWidth), "Solid line") ); //$NON-NLS-1$
-			vec.addElement( new MyLine(
-				new BasicStroke(
-					lineWidth,
-					BasicStroke.CAP_BUTT,
-					BasicStroke.JOIN_BEVEL,
-					(float)0.0,
-					new float[] {10, 3},
-					(float)0.0), 
-				"Dash line 1") ); //$NON-NLS-1$
-			vec.addElement( new MyLine(
-				new BasicStroke(
-					lineWidth,
-					BasicStroke.CAP_BUTT,
-					BasicStroke.JOIN_BEVEL,
-					(float)0.0,
-					new float[] {3, 10},
-					(float)0.0),
-				"Dash line 2") ); //$NON-NLS-1$
-			vec.addElement( new MyLine(
-				new BasicStroke(
-					lineWidth,
-					BasicStroke.CAP_BUTT,
-					BasicStroke.JOIN_BEVEL,
-					(float)0.0,
-					new float[] {10, 3, 10},
-					(float)0.0),
-				"Dash line 3") ); //$NON-NLS-1$
-			vec.addElement( new MyLine(
-				new BasicStroke(
-					lineWidth,
-					BasicStroke.CAP_BUTT,
-					BasicStroke.JOIN_BEVEL,
-					(float)0.0,
-					new float[] {10, 10, 3, 10},
-					(float)0.0),
-				"Dash line 4") ); //$NON-NLS-1$
+		if(do_init) {
+			presetLines = new LineStyleHolder[5];
+			presetLines[0] = new LineStyleHolder(
+							new BasicStroke(lineWidth),
+							"Solid line"); //$NON-NLS-1$
+			presetLines[1] = new LineStyleHolder(
+					new BasicStroke(
+							lineWidth,
+							BasicStroke.CAP_BUTT,
+							BasicStroke.JOIN_BEVEL,
+							(float) 0.0,
+							new float[] { 10, 3 },
+							(float) 0.0), 
+					"Dash line 1"); //$NON-NLS-1$
+			presetLines[2] = new LineStyleHolder(
+					new BasicStroke(
+							lineWidth,
+							BasicStroke.CAP_BUTT,
+							BasicStroke.JOIN_BEVEL,
+							(float) 0.0,
+							new float[] { 3, 10 },
+							(float) 0.0), 
+					"Dash line 2"); //$NON-NLS-1$
+			presetLines[3] = new LineStyleHolder(
+					new BasicStroke(
+							lineWidth,
+							BasicStroke.CAP_BUTT,
+							BasicStroke.JOIN_BEVEL,
+							(float) 0.0,
+							new float[] { 10, 3, 10 },
+							(float) 0.0), 
+					"Dash line 3"); //$NON-NLS-1$
+			presetLines[4] = new LineStyleHolder(
+					new BasicStroke(
+							lineWidth,
+							BasicStroke.CAP_BUTT,
+							BasicStroke.JOIN_BEVEL,
+							(float) 0.0,
+							new float[] { 10, 10, 3, 10 },
+							(float) 0.0), 
+					"Dash line 4"); //$NON-NLS-1$
 			do_init = false;
 		}
 	}
@@ -91,114 +91,101 @@ public class LineComboBox extends AComboBox
 	public BasicStroke returnStroke;
 	int prevComboBox = 0;
 
-	public MyLine returnMyLine = new MyLine();
-	MyLine[] lines;
+	public LineStyleHolder returnLineStyleHolder = new LineStyleHolder();
+	LineStyleHolder[] lines;
 
-	public class MyLine
-	{
+	public class LineStyleHolder {
 		public BasicStroke basicStroke;
 		public String text;
 
-		public MyLine (BasicStroke col, String tx)
-		{
+		public LineStyleHolder(BasicStroke col, String tx) {
 			this.basicStroke = col;
 			this.text = tx;
 		}
 
-		public MyLine ()
-		{
+		public LineStyleHolder() {
 			this.basicStroke = new BasicStroke(2);
 			this.text = "Solid line"; //$NON-NLS-1$
 		}
 
-		public void setMyLine (BasicStroke col, String tx)
-		{
+		public void setLineStyleHolder(BasicStroke col, String tx) {
 			this.basicStroke = col;
 			this.text = tx;
 		}
 
 		@Override
-		public String toString()
-		{
+		public String toString() {
 			return this.text;
 		}
 	}
-	
-    class ComboBoxRenderer extends JPanel implements ListCellRenderer
-    {
-        public JLabel lbl = new JLabel();
-        public Drawfield a1 = new Drawfield();
 
-        JComboBox jComboBox = new JComboBox();
+	class ComboBoxRenderer extends JPanel implements ListCellRenderer {
+		public JLabel lbl = new JLabel();
+		public Drawfield a1 = new Drawfield();
 
-        public ComboBoxRenderer(JComboBox Box)
-        {
-            setOpaque(true);
-            this.jComboBox = Box;
+		JComboBox jComboBox = new JComboBox();
+
+		public ComboBoxRenderer(JComboBox Box) {
+			setOpaque(true);
+			this.jComboBox = Box;
 			this.lbl.setFont(Box.getFont());
-        }
+		}
 
-		public Component getListCellRendererComponent( 
+		public Component getListCellRendererComponent(
 				JList list,
-                Object value,
-                int index,
-                boolean isSelected,
-                boolean cellHasFocus)
-        {
-            if (isSelected)
-            {
-                setBackground(list.getSelectionBackground());
-                setForeground(list.getSelectionForeground());
-            }
-            else
-            {
-                setBackground(list.getBackground());
-                setForeground(list.getForeground());
-            }
-            MyLine myLine = (MyLine )value;
-            LineComboBox.this.returnMyLine.setMyLine( ((MyLine)value).basicStroke, ((MyLine)value).text );
-
-			if (isSelected)
-			{
-				this.lbl.setForeground(Color.white);
+				Object value,
+				int index,
+				boolean isSelected,
+				boolean cellHasFocus) {
+			if(isSelected) {
+				setBackground(list.getSelectionBackground());
+				setForeground(list.getSelectionForeground());
+			} else {
+				setBackground(list.getBackground());
+				setForeground(list.getForeground());
 			}
-			else
-			{
+			LineStyleHolder myLine = (LineStyleHolder) value;
+			LineComboBox.this.returnLineStyleHolder.setLineStyleHolder(
+					((LineStyleHolder) value).basicStroke,
+					((LineStyleHolder) value).text);
+
+			if(isSelected) {
+				this.lbl.setForeground(Color.white);
+			} else {
 				this.lbl.setForeground(Color.black);
 			}
 
 			this.a1.initDrawfield(
-					0, 
+					0,
 					this.jComboBox.getHeight() / 2,
-                    this.jComboBox.getWidth() / 2, 
+					this.jComboBox.getWidth() / 2,
 					this.jComboBox.getHeight() / 2,
-                    Color.red, 
-					myLine.basicStroke );
+					Color.red,
+					myLine.basicStroke);
 
-			this.a1.setBounds(new Rectangle( 
-					0, 
-					0, 
-					this.jComboBox.getWidth() / 2, 
-					this.jComboBox.getHeight() ));
+			this.a1.setBounds(new Rectangle(
+					0,
+					0,
+					this.jComboBox.getWidth() / 2,
+					this.jComboBox.getHeight()));
 
-			this.lbl.setBounds(new Rectangle( 
-					this.jComboBox.getWidth() / 2, 
-					0, 
-					this.jComboBox.getWidth(), 
-					this.jComboBox.getHeight() ));
+			this.lbl.setBounds(new Rectangle(
+					this.jComboBox.getWidth() / 2,
+					0,
+					this.jComboBox.getWidth(),
+					this.jComboBox.getHeight()));
 
-            this.setLayout(null);
+			this.setLayout(null);
 
-            LineComboBox.this.returnStroke = myLine.basicStroke;
-            this.lbl.setText(myLine.text);
-            this.add(this.a1);
-            this.add(this.lbl);
+			LineComboBox.this.returnStroke = myLine.basicStroke;
+			this.lbl.setText(myLine.text);
+			this.add(this.a1);
+			this.add(this.lbl);
 
-            return this;
-        }
+			return this;
+		}
 
-		class Drawfield extends JLabel
-		{
+		class Drawfield extends JLabel {
 			int startX;
 			int startY;
 			int endX;
@@ -206,13 +193,17 @@ public class LineComboBox extends AComboBox
 			Color color;
 			BasicStroke basicStroke;
 
-			public Drawfield()
-			{
+			public Drawfield() {
 				super();
 			}
 
-			public void initDrawfield(int startx, int starty, int endx, int endy, Color col, BasicStroke bas)
-			{
+			public void initDrawfield(
+					int startx,
+					int starty,
+					int endx,
+					int endy,
+					Color col,
+					BasicStroke bas) {
 				this.startX = startx;
 				this.startY = starty;
 				this.endX = endx;
@@ -222,9 +213,8 @@ public class LineComboBox extends AComboBox
 			}
 
 			@Override
-			public void paint(Graphics g)
-			{
-				Graphics2D p = ( Graphics2D)g;
+			public void paint(Graphics g) {
+				Graphics2D p = (Graphics2D) g;
 
 				Stroke bs = p.getStroke();
 				p.setStroke(this.basicStroke);
@@ -234,156 +224,105 @@ public class LineComboBox extends AComboBox
 				p.setStroke(bs);
 			}
 		}
-    }
+	}
 
-	public LineComboBox()
-	{
-		super(AComboBox.SMALL_FONT);
+	public LineComboBox() {
+		// todo set font size
+		super();
 
-        this.returnMyLine.setMyLine( ((MyLine)vec.get(0)).basicStroke, ((MyLine)vec.get(0)).text );
-		this.returnStroke = ((MyLine)vec.get(0)).basicStroke;
+		this.returnLineStyleHolder.setLineStyleHolder(
+				presetLines[0].basicStroke,
+				presetLines[0].text);
+		this.returnStroke = presetLines[0].basicStroke;
 
-        setModel(new DefaultComboBoxModel(vec));
+		setModel(new DefaultComboBoxModel(presetLines));
 
 		this.setBounds(new Rectangle(0, 0, 20, 20));
 
-		this.addItemListener(new ItemListener()
-		{
-			public void itemStateChanged(ItemEvent e)
-			{
+		this.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
 				this_itemStateChanged(e);
 			}
 		});
-/*
-		this.addActionListener(new java.awt.event.ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				this_actionPerformed(e);
-			}
-		});
-*/
-        ComboBoxRenderer renderer = new ComboBoxRenderer(this);
-        renderer.setPreferredSize(new Dimension(this.getWidth(), this.getHeight()));
-        this.setRenderer(renderer);
-        this.setMaximumRowCount(5);
-/*
-		renderer.lbl.addMouseListener(new java.awt.event.MouseAdapter()
-		{
-			public void mousePressed(MouseEvent e)
-			{
-				this_mousePressed(e);
-			}
-			public void mouseReleased(MouseEvent e)
-			{
-				this_mousePressed(e);
-			}
-		});
-		renderer.a1.addMouseListener(new java.awt.event.MouseAdapter()
-		{
-			public void mousePressed(MouseEvent e)
-			{
-				this_mousePressed(e);
-			}
-			public void mouseReleased(MouseEvent e)
-			{
-				this_mousePressed(e);
-			}
-		});
-*/
+		ComboBoxRenderer renderer = new ComboBoxRenderer(this);
+		renderer.setPreferredSize(new Dimension(this.getWidth(), this
+				.getHeight()));
+		this.setRenderer(renderer);
+		this.setMaximumRowCount(5);
 	}
 
-	void this_itemStateChanged(ItemEvent e)
-	{
+	void this_itemStateChanged(ItemEvent e) {
 		if(e.getStateChange() == ItemEvent.SELECTED)
-        if (this.getSelectedItem() instanceof MyLine)
-        {
-			MyLine ml = (MyLine )this.getSelectedItem();
-			this.returnMyLine.setMyLine( 
-					ml.basicStroke,
-					ml.text );
-			this.returnStroke = ml.basicStroke;
-		}
+			if(this.getSelectedItem() instanceof LineStyleHolder) {
+				LineStyleHolder ml = (LineStyleHolder) this.getSelectedItem();
+				this.returnLineStyleHolder.setLineStyleHolder(ml.basicStroke, ml.text);
+				this.returnStroke = ml.basicStroke;
+			}
 	}
 
-	void this_actionPerformed(ActionEvent e)
-	{
-		this.returnMyLine.setMyLine( 
-				((MyLine)this.getSelectedItem()).basicStroke,
-				((MyLine)this.getSelectedItem()).text );
-		this.returnStroke = ((MyLine)this.getSelectedItem()).basicStroke;
+	void this_actionPerformed(ActionEvent e) {
+		this.returnLineStyleHolder.setLineStyleHolder(
+				((LineStyleHolder) this.getSelectedItem()).basicStroke,
+				((LineStyleHolder) this.getSelectedItem()).text);
+		this.returnStroke = ((LineStyleHolder) this.getSelectedItem()).basicStroke;
 	}
 
-	void this_mousePressed(MouseEvent e)
-	{
-		this.returnMyLine.setMyLine( 
-				((MyLine)this.getSelectedItem()).basicStroke,
-				((MyLine)this.getSelectedItem()).text );
-		this.returnStroke = ((MyLine)this.getSelectedItem()).basicStroke;
+	void this_mousePressed(MouseEvent e) {
+		this.returnLineStyleHolder.setLineStyleHolder(
+				((LineStyleHolder) this.getSelectedItem()).basicStroke,
+				((LineStyleHolder) this.getSelectedItem()).text);
+		this.returnStroke = ((LineStyleHolder) this.getSelectedItem()).basicStroke;
 	}
 
-	public BasicStroke getSelectedStroke()
-	{
-		this.returnStroke = ((MyLine)this.getSelectedItem()).basicStroke;
+	public BasicStroke getSelectedStroke() {
+		this.returnStroke = ((LineStyleHolder) this.getSelectedItem()).basicStroke;
 		return this.returnStroke;
 	}
 
-	public void setLineSize(int i)
-	{
+	public void setLineSize(int i) {
 		lineWidth = i;
 	}
 
-	public int getLineSize()
-	{
+	public int getLineSize() {
 		return lineWidth;
 	}
 
-	public MyLine getReturnMyLine()
-	{
-		this.returnMyLine.setMyLine( 
-				((MyLine)this.getSelectedItem()).basicStroke,
-				((MyLine)this.getSelectedItem()).text );
-		this.returnStroke = ((MyLine)this.getSelectedItem()).basicStroke;
-		return this.returnMyLine;
+	public LineStyleHolder getReturnLineStyleHolder() {
+		this.returnLineStyleHolder.setLineStyleHolder(
+				((LineStyleHolder) this.getSelectedItem()).basicStroke,
+				((LineStyleHolder) this.getSelectedItem()).text);
+		this.returnStroke = ((LineStyleHolder) this.getSelectedItem()).basicStroke;
+		return this.returnLineStyleHolder;
 	}
 
-	public void setMyLine( MyLine myLine)
-	{
-		this.returnMyLine = myLine;
+	public void setMyLine(LineStyleHolder myLine) {
+		this.returnLineStyleHolder = myLine;
 	}
 
-	public Object getSelected()
-	{
-		return getReturnMyLine();//.text;
-//		return getSelectedStroke();
+	public Object getSelected() {
+		return getReturnLineStyleHolder();// .text;
+		// return getSelectedStroke();
 	}
 
-	public void setSelected(Object obj)
-	{
-		String str = (String )obj;
-		for(int i = 0; i < vec.size(); i++)
-		{
-			MyLine line = (MyLine)vec.get(i);
-			if(line.text.equals(str))
-			{
+	public void setSelected(Object obj) {
+		String str = (String) obj;
+		for(int i = 0; i < presetLines.length; i++) {
+			LineStyleHolder line = presetLines[i];
+			if(line.text.equals(str)) {
 				setSelectedItem(line);
 				return;
 			}
 		}
 	}
 
-	public static BasicStroke getStrokeByType(String str)
-	{
-		for(int i = 0; i < vec.size(); i++)
-		{
-			MyLine line = (MyLine)vec.get(i);
-			if(line.text.equals(str))
-			{
+	public static BasicStroke getStrokeByType(String str) {
+		for(int i = 0; i < presetLines.length; i++) {
+			LineStyleHolder line = presetLines[i];
+			if(line.text.equals(str)) {
 				return line.basicStroke;
 			}
 		}
 		return new BasicStroke(2);
 	}
-
 
 }
