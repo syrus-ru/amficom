3ac5468264fd719499a4275d4a62d82027067d19
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-10-07 19:49:54 +0400

    TYPO:

--- AMFICOM/v1/event/src/com/syrus/AMFICOM/eventv2/LineMismatchEvent.java
+++ AMFICOM/v1/event/src/com/syrus/AMFICOM/eventv2/LineMismatchEvent.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: LineMismatchEvent.java,v 1.1 2005/10/06 14:34:29 bass Exp $
+ * $Id: LineMismatchEvent.java,v 1.2 2005/10/07 15:49:54 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,12 +9,12 @@
 package com.syrus.AMFICOM.eventv2;
 
 /**
- * Generation of this one may be triggered upon receip of a
+ * Generation of this one may be triggered upon receipt of a
  * {@link ReflectogramMismatchEvent}.
  * 
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.1 $, $Date: 2005/10/06 14:34:29 $
+ * @version $Revision: 1.2 $, $Date: 2005/10/07 15:49:54 $
  * @module event
  */
 public interface LineMismatchEvent extends Event {
