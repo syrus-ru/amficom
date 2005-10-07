0725c272c0100523bf33da7b3bfe967a4a96abd4
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-10-07 18:28:57 +0400

    MINOR: disable 'save as' items

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/model/DefaultMapEditorApplicationModelFactory.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/model/DefaultMapEditorApplicationModelFactory.java
@@ -1,5 +1,5 @@
 /*-
- * $$Id: DefaultMapEditorApplicationModelFactory.java,v 1.6 2005/09/30 16:08:42 krupenn Exp $$
+ * $$Id: DefaultMapEditorApplicationModelFactory.java,v 1.7 2005/10/07 14:28:57 krupenn Exp $$
  *
  * Copyright 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -12,7 +12,7 @@ package com.syrus.AMFICOM.client.model;
  * Модель приложения по умолчанию - описывает, какие функции по умолчанию
  * доступны или не достпны пользователю
  * 
- * @version $Revision: 1.6 $, $Date: 2005/09/30 16:08:42 $
+ * @version $Revision: 1.7 $, $Date: 2005/10/07 14:28:57 $
  * @author $Author: krupenn $
  * @author Andrei Kroupennikov
  * @module mapviewclient
@@ -22,6 +22,11 @@ public class DefaultMapEditorApplicationModelFactory extends
 	@Override
 	public ApplicationModel create() {
 		ApplicationModel aModel = super.create();
+
+		// TODO disabled until map, mapview, maplibrary cloning will be tested
+		aModel.setVisible(MapEditorApplicationModel.ITEM_MAP_SAVE_AS, false);
+		aModel.setVisible(MapEditorApplicationModel.ITEM_MAP_VIEW_SAVE_AS, false);
+		aModel.setVisible(MapEditorApplicationModel.ITEM_MAP_LIBRARY_SAVE_AS, false);
 
 		return aModel;
 	}
