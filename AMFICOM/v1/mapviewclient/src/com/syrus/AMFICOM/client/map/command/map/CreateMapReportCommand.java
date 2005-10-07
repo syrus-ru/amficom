3c80648c449f8cbfcd1a39cce1264f633ac07e57
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-10-07 18:20:41 +0400

    MAJOR: real implementation

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/command/map/CreateMapReportCommand.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/command/map/CreateMapReportCommand.java
@@ -1,5 +1,5 @@
 /*-
- * $$Id: CreateMapReportCommand.java,v 1.11 2005/10/03 10:35:01 krupenn Exp $$
+ * $$Id: CreateMapReportCommand.java,v 1.12 2005/10/07 14:20:41 krupenn Exp $$
  *
  * Copyright 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,74 +8,79 @@
 
 package com.syrus.AMFICOM.client.map.command.map;
 
+import java.util.HashMap;
+import java.util.Set;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JDialog;
+
+import com.syrus.AMFICOM.client.map.NetMapViewer;
+import com.syrus.AMFICOM.client.map.command.MapDesktopCommand;
+import com.syrus.AMFICOM.client.map.report.MapReportModel;
+import com.syrus.AMFICOM.client.map.ui.MapFrame;
 import com.syrus.AMFICOM.client.model.AbstractCommand;
 import com.syrus.AMFICOM.client.model.ApplicationContext;
+import com.syrus.AMFICOM.client.report.CreateReportDialog;
+import com.syrus.AMFICOM.map.MapElement;
+import com.syrus.AMFICOM.mapview.CablePath;
+import com.syrus.AMFICOM.mapview.MapView;
+import com.syrus.AMFICOM.report.DestinationModules;
+import com.syrus.AMFICOM.scheme.SchemeCableLink;
 
 /**
- * @version $Revision: 1.11 $, $Date: 2005/10/03 10:35:01 $
+ * @version $Revision: 1.12 $, $Date: 2005/10/07 14:20:41 $
  * @author $Author: krupenn $
  * @author Andrei Kroupennikov
  * @module mapviewclient
  */
 public class CreateMapReportCommand extends AbstractCommand {
-	ApplicationContext aContext;
-//	AMTReport aReport = new AMTReport();
-//	MapReportModel mrm = new MapReportModel();
+	private final ApplicationContext aContext;
+	private final JDesktopPane desktopPane;
 
-	public CreateMapReportCommand(ApplicationContext aContext) {
+	public CreateMapReportCommand(JDesktopPane desktopPane, ApplicationContext aContext) {
+		this.desktopPane = desktopPane;
 		this.aContext = aContext;
 	}
 
 	@Override
 	public void execute() {
-/*
-		aReport.addRecord(
-			  mrm.getLangForField(MapReportModel.topology),
-			  MapFrame.getMapMainFrame().getMapViewer().getMapShot());
+		MapFrame mapFrame = MapDesktopCommand.findMapFrame(this.desktopPane);
 
-		MapElement element = MapFrame.getMapMainFrame().getMapViewer().getLogicalNetLayer().getCurrentMapElement();
+		if(mapFrame == null) {
+			return;
+		}
 
-		if(element instanceof MapCablePathElement)
-		{
-			aReport.addRecord(
-			  mrm.getLangForField(MapReportModel.cableLayout),
-			  element);
+		MapView mapView = mapFrame.getMapView();
+
+		if(mapView == null) {
+			return;
 		}
-		else
-		if(element instanceof PhysicalLink)
-		{
-			aReport.addRecord(
-			  mrm.getLangForField(MapReportModel.tunnelCableList),
-			  element);
+
+		NetMapViewer netMapViewer = mapFrame.getMapViewer();
+
+		final Set<MapElement> selectedElements = mapView.getMap().getSelectedElements();
+		if(selectedElements.size() != 1) {
+			// more than 1 object selected
+			return;
 		}
-		else
-		if(element instanceof MapMarker)
-		{
-			aReport.addRecord(
-			  mrm.getLangForField(MapReportModel.markerInfo),
-			  element);
-		}
-		else
-		if(element instanceof SiteNode)
-		{
-			aReport.addRecord(
-			  mrm.getLangForField(MapReportModel.shaftInfo),
-			  element);
-		}
-		else
-		if(element instanceof Collector)
-		{
-			aReport.addRecord(
-			  mrm.getLangForField(MapReportModel.collectorInfo),
-			  element);
-		}
+		MapElement mapElement = selectedElements.iterator().next();
 		
-		OpenTypedTemplateCommand ottc = new OpenTypedTemplateCommand(
-				aContext, 
-				ReportTemplate.rtt_Map,
-				aReport);
-		ottc.execute();
-*/
+		java.util.Map<Object,Object> reportData = new HashMap<Object,Object>();
+		if(mapElement instanceof CablePath) {
+			SchemeCableLink schemeCableLink = ((CablePath)mapElement).getSchemeCableLink();
+			reportData.put(MapReportModel.CABLE_LAYOUT, schemeCableLink.getId());
+		}
+		reportData.put(MapReportModel.TOPOLOGY_IMAGE, netMapViewer.getMapShot());
+		reportData.put(MapReportModel.SELECTED_OBJECT_CHARS, mapElement.getId());
+		try {
+			JDialog dialog = new CreateReportDialog(
+					this.aContext,
+					DestinationModules.MAP,
+					reportData);
+			dialog.setVisible(true);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
 
