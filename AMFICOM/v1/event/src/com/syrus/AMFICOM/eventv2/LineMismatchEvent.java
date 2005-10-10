16cd085e9dec752bb51dd83549527c9f771b58a3
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-10-10 17:52:40 +0400

    JAVADOC: fixes by Saa. Thank you, Alexey!

--- AMFICOM/v1/event/src/com/syrus/AMFICOM/eventv2/LineMismatchEvent.java
+++ AMFICOM/v1/event/src/com/syrus/AMFICOM/eventv2/LineMismatchEvent.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: LineMismatchEvent.java,v 1.6 2005/10/10 11:18:44 bass Exp $
+ * $Id: LineMismatchEvent.java,v 1.7 2005/10/10 13:52:40 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -19,69 +19,71 @@ import com.syrus.AMFICOM.reflectometry.ReflectogramMismatch.Severity;
  * 
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.6 $, $Date: 2005/10/10 11:18:44 $
+ * @version $Revision: 1.7 $, $Date: 2005/10/10 13:52:40 $
  * @module event
  */
 public interface LineMismatchEvent extends Event<IdlLineMismatchEvent> {
 	/**
-	 * @return mismatch type; see {@link AlarmType},
+	 * @return mismatch type; see {@link AlarmType AlarmType},
 	 *         {@link com.syrus.AMFICOM.reflectometry.ReflectogramMismatch#getAlarmType()}.
-	 * @see AlarmType
+	 * @see AlarmType AlarmType
 	 * @see com.syrus.AMFICOM.reflectometry.ReflectogramMismatch#getAlarmType()
 	 */
 	AlarmType getAlarmType();
 	
 	/**
-	 * @return problem severity; see {@link Severity},
+	 * @return problem severity; see {@link Severity Severity},
 	 *         {@link com.syrus.AMFICOM.reflectometry.ReflectogramMismatch#getSeverity()}.
-	 * @see Severity
+	 * @see Severity Severity
 	 * @see com.syrus.AMFICOM.reflectometry.ReflectogramMismatch#getSeverity()
 	 */
 	Severity getSeverity();
 
 	/**
-	 * @return {@code true} if <em>warning threshold</em> crossing degree is
-	 *         defined; see
+	 * @return {@code true} if threshold excess factor is defined; see
 	 *         {@link com.syrus.AMFICOM.reflectometry.ReflectogramMismatch#hasMismatch()}.
 	 * @see com.syrus.AMFICOM.reflectometry.ReflectogramMismatch#hasMismatch()
 	 */
 	boolean hasMismatch();
 
 	/**
-	 * @return lower bound of <em>warning threshold</em> crossing degree if
-	 *         one is defined, i. e. {@link #hasMismatch()}<code>&nbsp;==&nbsp;true</code>.
+	 * @return lower bound of threshold excess factor if
+	 *         {@code mismatch} is defined, i.&nbsp;e.
+	 *         {@link #hasMismatch()}<code>&nbsp;==&nbsp;true</code>.
 	 *         In this case, it&apos;s guaranteed that
 	 *         <code>0.0&nbsp;&lt;=&nbsp;</code>{@link #getMinMismatch()
 	 *         minMismatch}<code>&nbsp;&lt;=&nbsp;</code>{@link
 	 *         #getMaxMismatch() maxMismatch}<code>&nbsp;&lt;=&nbsp;1.0</code>.
-	 * @throws IllegalStateException if threshold crossing degree is
-	 *         undefined, i. e. {@link #hasMismatch()}<code>&nbsp;==&nbsp;false</code>.
+	 * @throws IllegalStateException if threshold excess factor is
+	 *         undefined, i.&nbsp;e. {@link #hasMismatch()}<code>&nbsp;==&nbsp;false</code>.
 	 * @see com.syrus.AMFICOM.reflectometry.ReflectogramMismatch#getMinMismatch()
 	 */
 	double getMinMismatch();
 
 	/**
-	 * @return upper bound of <em>warning threshold</em> crossing degree if
-	 *         one is defined, i. e. {@link #hasMismatch()}<code>&nbsp;==&nbsp;true</code>.
+	 * @return upper bound of threshold excess factor if
+	 *         {@code mismatch} is defined, i.&nbsp;e.
+	 *         {@link #hasMismatch()}<code>&nbsp;==&nbsp;true</code>.
 	 *         In this case, it&apos;s guaranteed that
 	 *         <code>0.0&nbsp;&lt;=&nbsp;</code>{@link #getMinMismatch()
 	 *         minMismatch}<code>&nbsp;&lt;=&nbsp;</code>{@link
 	 *         #getMaxMismatch() maxMismatch}<code>&nbsp;&lt;=&nbsp;1.0</code>.
-	 * @throws IllegalStateException if threshold crossing degree is
-	 *         undefined, i. e. {@link #hasMismatch()}<code>&nbsp;==&nbsp;false</code>.
+	 * @throws IllegalStateException if threshold excess factor is
+	 *         undefined, i.&nbsp;e. {@link #hasMismatch()}<code>&nbsp;==&nbsp;false</code>.
 	 * @see com.syrus.AMFICOM.reflectometry.ReflectogramMismatch#getMaxMismatch()
 	 */
 	double getMaxMismatch();
 
 	/**
-	 * @return identifier of the {@code PathElement} affected.
+	 * @return identifier of the {@code PathElement} affected. It&apos;s
+	 *         guaranteed to be both non-{@code null} and non-void.
 	 */
 	Identifier getAffectedPathElementId();
 
 	/**
 	 * @return {@code true} if the {@code PathElement} pointed to by
 	 *         {@link #getAffectedPathElementId() affectedPathElementId}
-	 *         has a {@code length} (i. e. {@code physicalLength},
+	 *         has a {@code length} (i.&nbsp;e. {@code physicalLength},
 	 *         {@code opticalLength}) attribute. Currently, this is true for
 	 *         {@code PathElement}s that enclose either a {@code SchemeLink}
 	 *         or a {@code SchemeCableLink}.
@@ -90,24 +92,24 @@ public interface LineMismatchEvent extends Event<IdlLineMismatchEvent> {
 
 	/**
 	 * @return physical distance in meters from starting point of the
-	 *         {@code PathElement} affected, in case it is spatious, i. e.
-	 *         {@link #isAffectedPathElementSpacious()
+	 *         {@code PathElement} affected, <em>in case it is spatious</em>,
+	 *         i.&nbsp;e. {@link #isAffectedPathElementSpacious()
 	 *         affectedPathElementSpacious}<code>&nbsp;==&nbsp;true</code>.
 	 * @throws IllegalStateException if {@code PathElement} encloses a
-	 *         {@code SchemeElement} and thus is <em>not</em> spatious, i. e.
-	 *         {@link #isAffectedPathElementSpacious()
+	 *         {@code SchemeElement} and thus is <em>not&nbsp;spatious</em>,
+	 *         i.&nbsp;e. {@link #isAffectedPathElementSpacious()
 	 *         affectedPathElementSpacious}<code>&nbsp;==&nbsp;false</code>.
 	 */
 	double getPhysicalDistanceFromStart();
 
 	/**
 	 * @return physical distance in meters from ending point of the
-	 *         {@code PathElement} affected, in case it is spatious, i. e.
-	 *         {@link #isAffectedPathElementSpacious()
+	 *         {@code PathElement} affected, <em>in case it is spatious</em>,
+	 *         i.&nbsp;e. {@link #isAffectedPathElementSpacious()
 	 *         affectedPathElementSpacious}<code>&nbsp;==&nbsp;true</code>.
 	 * @throws IllegalStateException if {@code PathElement} encloses a
-	 *         {@code SchemeElement} and thus is <em>not</em> spatious, i. e.
-	 *         {@link #isAffectedPathElementSpacious()
+	 *         {@code SchemeElement} and thus is <em>not&nbsp;spatious</em>,
+	 *         i.&nbsp;e. {@link #isAffectedPathElementSpacious()
 	 *         affectedPathElementSpacious}<code>&nbsp;==&nbsp;false</code>.
 	 */
 	double getPhysicalDistanceFromEnd();
