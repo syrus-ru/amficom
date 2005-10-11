ab020a4679ed611044cc1cbce8129f796f75b584
Arseniy Tashoyan <tashoyan@gmail.com>
2005-10-11 17:27:26 +0400

    ADD: Register RoleDatabase

--- AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/DatabaseContextSetup.java
+++ AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/DatabaseContextSetup.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DatabaseContextSetup.java,v 1.11 2005/09/14 18:18:39 arseniy Exp $
+ * $Id: DatabaseContextSetup.java,v 1.12 2005/10/11 13:27:26 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -11,6 +11,7 @@ package com.syrus.AMFICOM.leserver;
 import com.syrus.AMFICOM.administration.DomainDatabase;
 import com.syrus.AMFICOM.administration.MCMDatabase;
 import com.syrus.AMFICOM.administration.PermissionAttributesDatabase;
+import com.syrus.AMFICOM.administration.RoleDatabase;
 import com.syrus.AMFICOM.administration.ServerDatabase;
 import com.syrus.AMFICOM.administration.ServerProcessDatabase;
 import com.syrus.AMFICOM.administration.SystemUserDatabase;
@@ -22,7 +23,7 @@ import com.syrus.AMFICOM.general.CharacteristicTypeDatabase;
 import com.syrus.AMFICOM.general.DatabaseContext;
 
 /**
- * @version $Revision: 1.11 $, $Date: 2005/09/14 18:18:39 $
+ * @version $Revision: 1.12 $, $Date: 2005/10/11 13:27:26 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module leserver
@@ -35,12 +36,15 @@ final class DatabaseContextSetup {
 	public static void initDatabaseContext() {
 		DatabaseContext.registerDatabase(new CharacteristicTypeDatabase());
 		DatabaseContext.registerDatabase(new CharacteristicDatabase());
+
 		DatabaseContext.registerDatabase(new SystemUserDatabase());
 		DatabaseContext.registerDatabase(new PermissionAttributesDatabase());
 		DatabaseContext.registerDatabase(new DomainDatabase());
 		DatabaseContext.registerDatabase(new ServerDatabase());
 		DatabaseContext.registerDatabase(new MCMDatabase());
 		DatabaseContext.registerDatabase(new ServerProcessDatabase());
+		DatabaseContext.registerDatabase(new RoleDatabase());
+
 		DatabaseContext.registerDatabase(new EventTypeDatabase());
 		DatabaseContext.registerDatabase(new EventDatabase());
 		DatabaseContext.registerDatabase(new EventSourceDatabase());
