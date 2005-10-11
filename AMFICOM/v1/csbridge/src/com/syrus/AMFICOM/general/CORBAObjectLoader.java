d90f9464ad2bb67fc8ae4793a9cebd23911d4cd8
Arseniy Tashoyan <tashoyan@gmail.com>
2005-10-11 18:28:38 +0400

    MAJOR: Restore of login made in CORBAActionProcessor. MAJOR: Initialization for default and defined CORBAActionProcessor. MAJOR: Removed static members. MINOR: Features.

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/CORBAObjectLoader.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/CORBAObjectLoader.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: CORBAObjectLoader.java,v 1.58 2005/10/10 11:42:00 bob Exp $
+ * $Id: CORBAObjectLoader.java,v 1.59 2005/10/11 14:28:38 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,11 +8,12 @@
 
 package com.syrus.AMFICOM.general;
 
+import static com.syrus.AMFICOM.general.ErrorMessages.NON_NULL_EXPECTED;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
 
 import org.omg.CORBA.ORB;
 
@@ -22,46 +23,86 @@ import com.syrus.AMFICOM.general.corba.IdVersion;
 import com.syrus.AMFICOM.general.corba.IdlIdentifier;
 import com.syrus.AMFICOM.general.corba.IdlStorableObject;
 import com.syrus.AMFICOM.general.corba.IdlStorableObjectCondition;
-import com.syrus.AMFICOM.general.corba.AMFICOMRemoteExceptionPackage.IdlErrorCode;
 import com.syrus.AMFICOM.security.corba.IdlSessionKey;
-import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.58 $, $Date: 2005/10/10 11:42:00 $
- * @author $Author: bob $
+ * @version $Revision: 1.59 $, $Date: 2005/10/11 14:28:38 $
+ * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module csbridge
  */
 public class CORBAObjectLoader implements ObjectLoader {
-	protected ServerConnectionManager serverConnectionManager;
-	
-	protected static CORBAActionProcessor actionProcessor;
+	ServerConnectionManager serverConnectionManager;
+	private CORBAActionProcessor corbaActionProcessor;
 
-	
-	private static abstract class AbstractLoadAction<T extends StorableObject> 
-	implements CORBAAction {
-		protected Set<T> sett;		
-		
-		public Set<T> getResult() {
-			return this.sett;
+	private abstract class LoadCORBAAction<T extends StorableObject> implements CORBAAction {
+		Set<T> loadedObjects;
+
+		Set<T> getLoadedObjects() {
+			return this.loadedObjects;
 		}
 	}
 
-	public CORBAObjectLoader(final ServerConnectionManager serverConnectionManager) {
-		this.serverConnectionManager = serverConnectionManager;
+	private abstract class RemoveVersionCORBAAction implements CORBAAction {
+		Map<Identifier, StorableObjectVersion> versionsMap;
+
+		Map<Identifier, StorableObjectVersion> getVersionsMap() {
+			return this.versionsMap;
+		}
+	}
+
+
+	/**
+	 * Use DefaultCORBAActionProcessor with given loginRestorer as CORBAActionProcessor
+	 * @param serverConnectionManager
+	 * @param loginRestorer
+	 */
+	public CORBAObjectLoader(final ServerConnectionManager serverConnectionManager, final LoginRestorer loginRestorer) {
+		this(serverConnectionManager, new DefaultCORBAActionProcessor(loginRestorer));
 	}
 
 	/**
-	 * Overridden in: MCMObjectLoader
+	 * Use given CORBAActionProcessor
+	 * @param serverConnectionManager
+	 * @param corbaActionProcessor
+	 */
+	public CORBAObjectLoader(final ServerConnectionManager serverConnectionManager, final CORBAActionProcessor corbaActionProcessor) {
+		assert serverConnectionManager != null : NON_NULL_EXPECTED;
+		assert corbaActionProcessor != null : NON_NULL_EXPECTED;
+
+		this.serverConnectionManager = serverConnectionManager;
+		this.corbaActionProcessor = corbaActionProcessor;
+	}
+
+
+	/**
+	 * Overridden in:
+	 * MCMObjectLoader
 	 */
 	public <T extends StorableObject> Set<T> loadStorableObjects(final Set<Identifier> ids) throws ApplicationException {
-		assert ids != null : ErrorMessages.NON_NULL_EXPECTED;
+		assert ids != null : NON_NULL_EXPECTED;
 		if (ids.isEmpty()) {
 			return Collections.emptySet();
 		}
 
-		final CommonServer server = this.serverConnectionManager.getServerReference();
-		return loadStorableObjects(server, ids);
+		final IdlIdentifier[] idsT = Identifier.createTransferables(ids);
+
+		final LoadCORBAAction<T> action = new LoadCORBAAction<T>() {
+			public void perform() throws AMFICOMRemoteException, ApplicationException {
+				final IdlSessionKey sessionKeyT = LoginManager.getSessionKeyTransferable();
+				final CommonServer server = CORBAObjectLoader.this.serverConnectionManager.getServerReference();
+				final IdlStorableObject[] transferables = server.transmitStorableObjects(idsT, sessionKeyT);
+				this.loadedObjects = StorableObject.fromTransferables(transferables);
+			}
+		};
+
+		this.corbaActionProcessor.performAction(action);
+
+		final Set<T> loadedObjects = action.getLoadedObjects();
+		if (loadedObjects != null) {
+			return loadedObjects;
+		}
+		return Collections.emptySet();
 	}
 
 	/**
@@ -70,87 +111,28 @@ public class CORBAObjectLoader implements ObjectLoader {
 	 */
 	public <T extends StorableObject> Set<T> loadStorableObjectsButIdsByCondition(final Set<Identifier> ids,
 			final StorableObjectCondition condition) throws ApplicationException {
-		assert ids != null && condition != null : ErrorMessages.NON_NULL_EXPECTED;
+		assert ids != null : NON_NULL_EXPECTED;
+		assert condition != null : NON_NULL_EXPECTED;
 
-		final CommonServer server = this.serverConnectionManager.getServerReference();
-		return loadStorableObjectsButIdsByCondition(server, ids, condition);
-	}
-	
-	private static synchronized CORBAActionProcessor getCORBAActionProcessor() {
-		if (actionProcessor == null) {
-			actionProcessor = new CORBAActionProcessor() {
-				public void performAction(final CORBAAction action) throws ApplicationException {
-					int numEfforts = 0;
-					while (true) {
-						try {
-							action.performAction();
-							return;
-						} catch (final AMFICOMRemoteException are) {
-							switch (are.errorCode.value()) {
-								case IdlErrorCode._ERROR_NOT_LOGGED_IN:
-									if (++numEfforts == 1) {
-										if (LoginManager.restoreLogin()) {
-											continue;
-										}
-										Log.debugMessage("CORBAObjectLoader.CORBAActionProcessor.performAction | Login not restored", Level.INFO);
-										return;
-									}
-									throw new LoginException(are.message);
-								default:
-									throw new RetrieveObjectException(are.message);
-							}
-						}
-					}
-				}
-			};
-		}
-		
-		return actionProcessor;
-	}
-	
-	public static final void setActionProcessor(final CORBAActionProcessor actionProcessor) {
-		CORBAObjectLoader.actionProcessor = actionProcessor;
-	}
-	
-	private static <T extends StorableObject> Set<T> loading(final AbstractLoadAction<T> loadAction) 
-	throws ApplicationException {
-		getCORBAActionProcessor().performAction(loadAction);
-		
-		final Set<T> result = loadAction.getResult();
-		if (result != null) {
-			return result;
-		}
-		return Collections.emptySet();		
-	}
-	
-	public static final <T extends StorableObject> Set<T> loadStorableObjects(final CommonServer server, final Set<Identifier> ids)
-			throws ApplicationException {
-		final IdlIdentifier[] idsT = Identifier.createTransferables(ids);
-		
-		return loading(new AbstractLoadAction<T>() {			
-			public void performAction() 
-			throws AMFICOMRemoteException {
-				final IdlSessionKey sessionKeyT = LoginManager.getSessionKeyTransferable();
-				final IdlStorableObject[] transferables = server.transmitStorableObjects(idsT, sessionKeyT);
-				this.sett = StorableObject.fromTransferables(transferables);
-			}
-		});
-	}
-
-	public static final <T extends StorableObject> Set<T> loadStorableObjectsButIdsByCondition(final CommonServer server,
-			final Set<Identifier> ids,
-			final StorableObjectCondition condition) throws ApplicationException {
 		final IdlIdentifier[] idsT = Identifier.createTransferables(ids);
 		final IdlStorableObjectCondition conditionT = condition.getTransferable();
-		
-		return loading(new AbstractLoadAction<T>() {
-			public void performAction() 
-			throws AMFICOMRemoteException {
+
+		final LoadCORBAAction<T> action = new LoadCORBAAction<T>() {
+			public void perform() throws AMFICOMRemoteException, ApplicationException {
 				final IdlSessionKey sessionKeyT = LoginManager.getSessionKeyTransferable();
+				final CommonServer server = CORBAObjectLoader.this.serverConnectionManager.getServerReference();
 				final IdlStorableObject[] transferables = server.transmitStorableObjectsButIdsByCondition(idsT, conditionT, sessionKeyT);
-				this.sett = StorableObject.fromTransferables(transferables);
+				this.loadedObjects = StorableObject.fromTransferables(transferables);
 			}
-		});		
+		};
+		
+		this.corbaActionProcessor.performAction(action);
+
+		final Set<T> loadedObjects = action.getLoadedObjects();
+		if (loadedObjects != null) {
+			return loadedObjects;
+		}
+		return Collections.emptySet();
 	}
 
 	/**
@@ -161,34 +143,24 @@ public class CORBAObjectLoader implements ObjectLoader {
 		if (ids.isEmpty()) {
 			return Collections.emptyMap();
 		}
-		
+
 		final IdlIdentifier[] idsT = Identifier.createTransferables(ids);	
-		
-		class RemoveVersionAction implements CORBAAction {
-			
-			private Map<Identifier, StorableObjectVersion> versionsMap;
-			
-			public void performAction() throws AMFICOMRemoteException, ApplicationException {
-				final CommonServer server = CORBAObjectLoader.this.serverConnectionManager.getServerReference();
-				final IdlSessionKey sessionKeyT = LoginManager.getSessionKeyTransferable();				
+
+		final RemoveVersionCORBAAction action = new RemoveVersionCORBAAction() {
+			public void perform() throws AMFICOMRemoteException, ApplicationException {
+				final IdlSessionKey sessionKeyT = LoginManager.getSessionKeyTransferable();
+				final CommonServer server = CORBAObjectLoader.this.serverConnectionManager.getServerReference();				
 				final IdVersion[] idVersions = server.transmitRemoteVersions(idsT, sessionKeyT);
 				this.versionsMap = new HashMap<Identifier, StorableObjectVersion>(idVersions.length);
 				for (int i = 0; i < idVersions.length; i++) {
 					this.versionsMap.put(new Identifier(idVersions[i].id), new StorableObjectVersion(idVersions[i].version));
 				}
 			}
-			
-			public final Map<Identifier, StorableObjectVersion> getVersionsMap() {
-				return this.versionsMap;
-			}
-			
-		}
-		
-		final RemoveVersionAction removeVersionAction = new RemoveVersionAction();
-		
-		getCORBAActionProcessor().performAction(removeVersionAction);
-		
-		return removeVersionAction.getVersionsMap();
+		};
+
+		this.corbaActionProcessor.performAction(action);
+
+		return action.getVersionsMap();
 	}
 
 	/**
@@ -200,16 +172,17 @@ public class CORBAObjectLoader implements ObjectLoader {
 		if (storableObjects.isEmpty()) {
 			return;
 		}
-		
-		getCORBAActionProcessor().performAction(new CORBAAction() {
-			public void performAction() throws AMFICOMRemoteException ,ApplicationException {
-				final CommonServer server = CORBAObjectLoader.this.serverConnectionManager.getServerReference();
+
+		final CORBAAction action = new CORBAAction() {
+			public void perform() throws AMFICOMRemoteException, ApplicationException {
 				final ORB orb = CORBAObjectLoader.this.serverConnectionManager.getCORBAServer().getOrb();
 				final IdlStorableObject[] transferables = StorableObject.createTransferables(storableObjects, orb);
 				final IdlSessionKey sessionKeyT = LoginManager.getSessionKeyTransferable();
+				final CommonServer server = CORBAObjectLoader.this.serverConnectionManager.getServerReference();
 				server.receiveStorableObjects(transferables, sessionKeyT);
 			}
-		});
+		};
+		this.corbaActionProcessor.performAction(action);
 	}
 
 	public final void delete(final Set<? extends Identifiable> identifiables) throws ApplicationException {
@@ -217,12 +190,12 @@ public class CORBAObjectLoader implements ObjectLoader {
 		if (identifiables.isEmpty()) {
 			return;
 		}
-		
-		getCORBAActionProcessor().performAction(new CORBAAction() {
-			public void performAction() throws AMFICOMRemoteException ,ApplicationException {
-				final CommonServer server = CORBAObjectLoader.this.serverConnectionManager.getServerReference();
+
+		this.corbaActionProcessor.performAction(new CORBAAction() {
+			public void perform() throws AMFICOMRemoteException, ApplicationException {
 				final IdlIdentifier[] idsT = Identifier.createTransferables(identifiables);
 				final IdlSessionKey sessionKeyT = LoginManager.getSessionKeyTransferable();
+				final CommonServer server = CORBAObjectLoader.this.serverConnectionManager.getServerReference();
 				server.delete(idsT, sessionKeyT);
 			}
 		});
