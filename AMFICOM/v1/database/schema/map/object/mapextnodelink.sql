98cb4a85394b1eebaabc4d5a130195f4399c7867
Maxim Selivanov <max.selivanov@gmail.com>
2005-10-11 14:50:25 +0400

    BUGFIX: NodeLink -> SiteNode

--- AMFICOM/v1/database/schema/map/object/mapextnodelink.sql
+++ AMFICOM/v1/database/schema/map/object/mapextnodelink.sql
@@ -1,9 +1,9 @@
 CREATE TABLE MapExtNodeLink (
  map_id,
- ext_node_link_id,
+ ext_site_node_id,
 --
  CONSTRAINT mapexnl_map_fk FOREIGN KEY (map_id)
   REFERENCES Map (id) ON DELETE CASCADE,
  CONSTRAINT mapexnl_exnl_fk FOREIGN KEY (ext_node_link_id)
-  REFERENCES NodeLink (id) ON DELETE CASCADE
+  REFERENCES SiteNode (id) ON DELETE CASCADE
 );
