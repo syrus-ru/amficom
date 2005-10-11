38590b2a726020b8c0830950a9203dc200cfe169
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-10-11 17:46:49 +0400

    ADD: #toString() added

--- AMFICOM/v1/event/src/com/syrus/AMFICOM/eventv2/AbstractReflectogramMismatchEvent.java
+++ AMFICOM/v1/event/src/com/syrus/AMFICOM/eventv2/AbstractReflectogramMismatchEvent.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: AbstractReflectogramMismatchEvent.java,v 1.1 2005/10/06 14:34:29 bass Exp $
+ * $Id: AbstractReflectogramMismatchEvent.java,v 1.2 2005/10/11 13:46:49 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -13,12 +13,23 @@ import static com.syrus.AMFICOM.eventv2.EventType.REFLECTORGAM_MISMATCH;
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.1 $, $Date: 2005/10/06 14:34:29 $
+ * @version $Revision: 1.2 $, $Date: 2005/10/11 13:46:49 $
  * @module event
  */
 public abstract class AbstractReflectogramMismatchEvent
 		implements ReflectogramMismatchEvent {
 	public final EventType getType() {
 		return REFLECTORGAM_MISMATCH;
+	}
+
+	protected String paramString() {
+		return "alarmType = " + this.getAlarmType()
+				+ "; severity = " + this.getSeverity()
+				+ "; monitoredElementId = " + this.getMonitoredElementId();
+	}
+
+	@Override
+	public final String toString() {
+		return this.getClass().getName() + "[" + this.paramString() + "]";
 	}
 }
