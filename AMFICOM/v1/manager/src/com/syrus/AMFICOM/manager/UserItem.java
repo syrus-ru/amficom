f8c9f0fa263aa8675d43011125478ed4495b2a00
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-10-11 19:33:49 +0400

    ADD user perspective item

---
+++ AMFICOM/v1/manager/src/com/syrus/AMFICOM/manager/UserItem.java
@@ -0,0 +1,21 @@
+/*-
+* $Id: UserItem.java,v 1.1 2005/10/11 15:33:49 bob Exp $
+*
+* Copyright ¿ 2005 Syrus Systems.
+* Dept. of Science & Technology.
+* Project: AMFICOM.
+*/
+
+package com.syrus.AMFICOM.manager;
+
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/10/11 15:33:49 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module manager
+ */
+public interface UserItem extends ARMItem {
+
+}
+
