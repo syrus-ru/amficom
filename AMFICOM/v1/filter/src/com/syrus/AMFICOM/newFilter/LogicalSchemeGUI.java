1809f854fc29aa1eae66b44ff88ebbb243b70e59
Maxim Selivanov <max.selivanov@gmail.com>
2005-10-18 12:51:15 +0400

    BUGFIX: window size, localization, window close

--- AMFICOM/v1/filter/src/com/syrus/AMFICOM/newFilter/LogicalSchemeGUI.java
+++ AMFICOM/v1/filter/src/com/syrus/AMFICOM/newFilter/LogicalSchemeGUI.java
@@ -1,5 +1,5 @@
 /*
- * $Id: LogicalSchemeGUI.java,v 1.5 2005/08/08 11:37:22 arseniy Exp $
+ * $Id: LogicalSchemeGUI.java,v 1.6 2005/10/18 08:51:15 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,10 +8,14 @@
 package com.syrus.AMFICOM.newFilter;
 
 import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -21,10 +25,11 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.WindowConstants;
 
 /**
- * @version $Revision: 1.5 $, $Date: 2005/08/08 11:37:22 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.6 $, $Date: 2005/10/18 08:51:15 $
+ * @author $Author: max $
  * @module filter
  */
 public class LogicalSchemeGUI implements LogicalSchemeView {
@@ -45,29 +50,36 @@ public class LogicalSchemeGUI implements LogicalSchemeView {
 		JPanel panel = new JPanel(new GridBagLayout());
 		final JScrollPane jScrollPane = new JScrollPane();
 		jScrollPane.getViewport().add(this.controller.getLogicalSchemeUI());
+		
+		GraphicsEnvironment localGraphicsEnvironment = 
+			GraphicsEnvironment.getLocalGraphicsEnvironment();
+		Rectangle maximumWindowBounds = 
+			localGraphicsEnvironment.getMaximumWindowBounds();
+		jScrollPane.setPreferredSize(new Dimension(maximumWindowBounds.width / 3, 
+			maximumWindowBounds.height / 3));
 
-		JButton andButton = new JButton("AND");
+		JButton andButton = new JButton(LangModelFilter.getString("filter.logicalscheme.button.and"));
 		andButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				LogicalSchemeGUI.this.controller.addAndItem();
 			}
 		});
 		
-		JButton orButton = new JButton("OR");
+		JButton orButton = new JButton(LangModelFilter.getString("filter.logicalscheme.button.or"));
 		orButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				LogicalSchemeGUI.this.controller.addOrItem();
 			}
 		});
 
-		JButton deleteButton = new JButton("delete");
+		JButton deleteButton = new JButton(LangModelFilter.getString("filter.logicalscheme.button.delete"));
 		deleteButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				LogicalSchemeGUI.this.controller.deleteItem();				
 			}
 		});
 
-		JButton arrangeButton = new JButton("arrange");
+		JButton arrangeButton = new JButton(LangModelFilter.getString("filter.logicalscheme.button.arrange"));
 		arrangeButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				LogicalSchemeGUI.this.controller.arrange(jScrollPane.getWidth(), jScrollPane.getHeight());
@@ -75,7 +87,7 @@ public class LogicalSchemeGUI implements LogicalSchemeView {
 		});
 
 		
-		JButton okButton = new JButton("Ok");
+		JButton okButton = new JButton(LangModelFilter.getString("filter.logicalscheme.button.ok"));
 		okButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				LogicalSchemeGUI.this.controller.confirm();				
@@ -83,7 +95,7 @@ public class LogicalSchemeGUI implements LogicalSchemeView {
 		});
 
 		// TODO add listener
-		JButton cancelButton = new JButton("Cancel");
+		final JButton cancelButton = new JButton(LangModelFilter.getString("filter.logicalscheme.button.cancel"));
 		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				LogicalSchemeGUI.this.controller.cancel();
@@ -121,7 +133,13 @@ public class LogicalSchemeGUI implements LogicalSchemeView {
 		panel.add(box2, gbc);
 
 		this.frame.getContentPane().add(panel);
-		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+		this.frame.addComponentListener(new ComponentAdapter() {			
+			@Override
+			public void componentHidden(ComponentEvent e) {
+				cancelButton.doClick();
+			}
+		});
 		this.frame.pack();
 		Dimension dimension = new Dimension(this.frame.getSize());
 		panel.setMinimumSize(dimension);
