967e4ca5e764b2b625abc1a4261b585a7d31a456
Alexey Stratonnikov <alexsaa@gmail.com>
2005-10-18 17:25:25 +0400

    MINOR: Q quality parameter semantics changed; COSMETIC: added docs on d parameter

--- AMFICOM/v1/reflectometry/src/com/syrus/AMFICOM/reflectometry/ReflectometryEvaluationOverallResult.java
+++ AMFICOM/v1/reflectometry/src/com/syrus/AMFICOM/reflectometry/ReflectometryEvaluationOverallResult.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ReflectometryEvaluationOverallResult.java,v 1.1 2005/10/11 13:22:40 saa Exp $
+ * $Id: ReflectometryEvaluationOverallResult.java,v 1.2 2005/10/18 13:25:25 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -12,7 +12,7 @@ package com.syrus.AMFICOM.reflectometry;
  * Интерфейс для обмена
  * общими D и Q параметры (для всей рефлектограмы) результата сравнения.
  * @author $Author: saa $
- * @version $Revision: 1.1 $, $Date: 2005/10/11 13:22:40 $
+ * @version $Revision: 1.2 $, $Date: 2005/10/18 13:25:25 $
  * @module reflectometry
  */
 public interface ReflectometryEvaluationOverallResult {
@@ -25,12 +25,19 @@ public interface ReflectometryEvaluationOverallResult {
 	boolean hasDQ();
 	/**
 	 * Возвращает D-параметр результата сравнения.
+	 * Параметр со знаком, измеряется в децибеллах, означает степень отличия
+	 * рефлектограммы в ее конце.
 	 * @return D-параметр результата сравнения.
 	 * @throws IllegalStateException, если <tt>hasDQ() == false</tt>.
 	 */
 	double getD();
 	/**
 	 * Возвращает Q-параметр результата сравнения.
+	 * Параметр беззнаковый, безразмерный, в диапазоне [0..1], означает
+	 * уровень качества:
+	 * <ul>
+	 * <li>Q=1 - соответствие 100% (полное)
+	 * <li>Q=0 - соответствие 0%, отличие превышает предельное значение
 	 * @return Q-параметр результата сравнения.
 	 * @throws IllegalStateException, если <tt>hasDQ() == false</tt>.
 	 */
