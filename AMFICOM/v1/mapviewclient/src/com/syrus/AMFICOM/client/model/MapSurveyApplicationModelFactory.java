44d311eff6dc935f61915af80b0e447a78032189
Andrey Kroupennikov <andrei.kroupennikov@gmail.com>
2005-10-22 17:49:02 +0400

    BUGFIX: user cannot save and edit mapview

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/model/MapSurveyApplicationModelFactory.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/model/MapSurveyApplicationModelFactory.java
@@ -1,5 +1,5 @@
 /*-
- * $$Id: MapSurveyApplicationModelFactory.java,v 1.5 2005/09/30 16:08:42 krupenn Exp $$
+ * $$Id: MapSurveyApplicationModelFactory.java,v 1.6 2005/10/22 13:49:02 krupenn Exp $$
  *
  * Copyright 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -12,7 +12,7 @@ package com.syrus.AMFICOM.client.model;
  * ‘абричный класс создани€ модели приложени€ дл€ работы с картой в модуле
  * "Ќаблюдение".
  * 
- * @version $Revision: 1.5 $, $Date: 2005/09/30 16:08:42 $
+ * @version $Revision: 1.6 $, $Date: 2005/10/22 13:49:02 $
  * @author $Author: krupenn $
  * @author Andrei Kroupennikov
  * @module mapviewclient
@@ -24,8 +24,10 @@ public class MapSurveyApplicationModelFactory extends
 		ApplicationModel aModel = super.create();
 
 		aModel.setUsable(MapApplicationModel.ACTION_EDIT_MAP, false);
+		aModel.setUsable(MapApplicationModel.ACTION_EDIT_MAP_VIEW, false);
 		aModel.setUsable(MapApplicationModel.ACTION_EDIT_PROPERTIES, false);
 		aModel.setUsable(MapApplicationModel.ACTION_SAVE_MAP, false);
+		aModel.setUsable(MapApplicationModel.ACTION_SAVE_MAP_VIEW, false);
 
 		aModel.setUsable(MapApplicationModel.MODE_NODE_LINK, false);
 
