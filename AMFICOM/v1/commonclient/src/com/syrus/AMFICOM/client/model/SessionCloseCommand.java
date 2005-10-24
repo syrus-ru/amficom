18d9f02dec8cf7a426b610433d2f8eb076313f7c
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-10-24 15:54:31 +0400

    FIX i18n

--- AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/client/model/SessionCloseCommand.java
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/client/model/SessionCloseCommand.java
@@ -4,12 +4,13 @@ package com.syrus.AMFICOM.client.model;
 import com.syrus.AMFICOM.client.event.ContextChangeEvent;
 import com.syrus.AMFICOM.client.event.Dispatcher;
 import com.syrus.AMFICOM.client.event.StatusMessageEvent;
+import com.syrus.AMFICOM.client.resource.I18N;
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.ClientSessionEnvironment;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.6 $, $Date: 2005/10/21 08:48:21 $
+ * @version $Revision: 1.7 $, $Date: 2005/10/24 11:54:31 $
  * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module commonclient
@@ -34,19 +35,27 @@ public class SessionCloseCommand extends AbstractCommand {
 
 	@Override
 	public void execute() {
-		this.dispatcher.firePropertyChange(new StatusMessageEvent(this, StatusMessageEvent.STATUS_MESSAGE, "Закрытие сессии..."));
+		this.dispatcher.firePropertyChange(
+			new StatusMessageEvent(this, 
+				StatusMessageEvent.STATUS_MESSAGE, 
+				I18N.getString("Common.Login.LogOut")));
 		try {
-			final ClientSessionEnvironment clientSessionEnvironment = ClientSessionEnvironment.getInstance();
+			final ClientSessionEnvironment clientSessionEnvironment = 
+				ClientSessionEnvironment.getInstance();
 
 			clientSessionEnvironment.logout();
-			this.dispatcher.firePropertyChange(new StatusMessageEvent(this, StatusMessageEvent.STATUS_MESSAGE, "Сессия закрыта"));
-//			this.dispatcher.firePropertyChange(new ContextChangeEvent(this, ContextChangeEvent.SESSION_CLOSED_EVENT));
-			this.dispatcher.firePropertyChange(new ContextChangeEvent(this, ContextChangeEvent.LOGGED_OUT_EVENT));
+			this.dispatcher.firePropertyChange(
+				new StatusMessageEvent(this, 
+					StatusMessageEvent.STATUS_MESSAGE, 
+					I18N.getString("Common.Login.LoggedOut")));
+			this.dispatcher.firePropertyChange(
+				new ContextChangeEvent(this, 
+					ContextChangeEvent.LOGGED_OUT_EVENT));
 		} catch (ApplicationException e) {
 			Log.errorException(e);
 			this.dispatcher.firePropertyChange(new StatusMessageEvent(this,
 					StatusMessageEvent.STATUS_MESSAGE,
-					"Ошибка при закрытии сессии..."));
+					I18N.getString("Error.SessionClose")));
 		}
 	}
 }
