c392a74c9ea7da457149c88c3612d753c6ff978b
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-10-26 19:32:30 +0400

    BUGFIX: compilation problems fixed

--- AMFICOM/v1/observer/src/com/syrus/AMFICOM/client/observer/alarm/Alarm.java
+++ AMFICOM/v1/observer/src/com/syrus/AMFICOM/client/observer/alarm/Alarm.java
@@ -1,22 +1,39 @@
-/**
- * $Id: Alarm.java,v 1.1 2005/10/26 13:51:56 bass Exp $
+/*-
+ * $Id: Alarm.java,v 1.2 2005/10/26 15:32:30 bass Exp $
  *
- * Syrus Systems
- * Научно-технический центр
- * Проект: АМФИКОМ
+ * Copyright ї 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
  */
+
 package com.syrus.AMFICOM.client.observer.alarm;
 
+import java.util.Collections;
 import java.util.Set;
 
 import com.syrus.AMFICOM.general.Identifiable;
 import com.syrus.AMFICOM.general.StorableObject;
+import com.syrus.AMFICOM.general.StorableObjectWrapper;
 
-public class Alarm extends StorableObject {
+/**
+ * @author krupenn
+ * @author $Author: bass $
+ * @version $Revision: 1.2 $, $Date: 2005/10/26 15:32:30 $
+ * @module observer
+ */
+public final class Alarm extends StorableObject<Alarm> {
+	private static final long serialVersionUID = -8320740053940064854L;
 
 	@Override
 	public Set<Identifiable> getDependencies() {
-		return null;
+		return Collections.emptySet();
 	}
 
+	/**
+	 * @see com.syrus.AMFICOM.general.StorableObject#getWrapper()
+	 */
+	@Override
+	protected StorableObjectWrapper<Alarm> getWrapper() {
+		throw new UnsupportedOperationException();
+	}
 }
