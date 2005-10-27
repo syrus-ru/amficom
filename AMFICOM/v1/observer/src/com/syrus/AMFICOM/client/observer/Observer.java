c1ce80daab0de35308159403ebf93889ff6309c7
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-10-27 14:53:57 +0400

    MINOR: application name changed

--- AMFICOM/v1/observer/src/com/syrus/AMFICOM/client/observer/Observer.java
+++ AMFICOM/v1/observer/src/com/syrus/AMFICOM/client/observer/Observer.java
@@ -1,3 +1,11 @@
+/*-
+ * $Id: Observer.java,v 1.2 2005/10/27 10:53:57 bass Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
 package com.syrus.AMFICOM.client.observer;
 
 import java.awt.Toolkit;
@@ -5,11 +13,16 @@ import java.awt.Toolkit;
 import com.syrus.AMFICOM.client.model.AbstractApplication;
 import com.syrus.AMFICOM.client.model.DefaultObserverApplicationModelFactory;
 
-public class Observer extends AbstractApplication {
+/**
+ * @author krupenn
+ * @author $Author: bass $
+ * @version $Revision: 1.2 $, $Date: 2005/10/27 10:53:57 $
+ * @module observer
+ */
+final class Observer extends AbstractApplication {
+	public static final String APPLICATION_NAME = "observer";
 	
-	public static final String APPLICATION_NAME = "observe";
-	
-	public Observer() {
+	Observer() {
 		super(APPLICATION_NAME);
 	}		
 
@@ -18,7 +31,7 @@ public class Observer extends AbstractApplication {
 		super.aContext.setApplicationModel(new DefaultObserverApplicationModelFactory().create());
 		super.startMainFrame(new ObserverMainFrame(this.aContext), Toolkit.getDefaultToolkit().getImage("images/main/observe_mini.gif"));
 	}
-	
+
 	public static void main(String[] args) {
 		new Observer();
 	}
