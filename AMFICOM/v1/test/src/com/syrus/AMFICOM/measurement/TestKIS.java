bbdc4749c5fdb2d9d29c2a8e80be33391757842e
Arseniy Tashoyan <tashoyan@gmail.com>
2005-10-30 00:42:51 +0400

    ADD: Method 'testAdd'

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestKIS.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestKIS.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestKIS.java,v 1.3 2005/08/30 19:58:39 arseniy Exp $
+ * $Id: TestKIS.java,v 1.4 2005/10/29 20:42:51 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -9,6 +9,7 @@ package com.syrus.AMFICOM.measurement;
 
 import static com.syrus.AMFICOM.general.Identifier.SEPARATOR;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -18,11 +19,18 @@ import junit.framework.TestCase;
 import com.syrus.AMFICOM.administration.Domain;
 import com.syrus.AMFICOM.administration.MCM;
 import com.syrus.AMFICOM.configuration.Equipment;
+import com.syrus.AMFICOM.configuration.Port;
+import com.syrus.AMFICOM.configuration.PortType;
+import com.syrus.AMFICOM.configuration.TransmissionPath;
+import com.syrus.AMFICOM.configuration.TransmissionPathType;
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.DatabaseCommonTest;
 import com.syrus.AMFICOM.general.EquivalentCondition;
+import com.syrus.AMFICOM.general.ErrorMessages;
+import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectPool;
+import com.syrus.AMFICOM.measurement.corba.IdlMonitoredElementPackage.MonitoredElementSort;
 
 public final class TestKIS extends TestCase {
 
@@ -32,7 +40,7 @@ public final class TestKIS extends TestCase {
 
 	public static Test suite() {
 		final DatabaseCommonTest commonTest = new DatabaseCommonTest();
-		commonTest.addTest( new TestKIS("testGetByCondition"));
+		commonTest.addTest( new TestKIS("testAdd"));
 		return commonTest.createTestSetup();
 	}
 
@@ -68,11 +76,93 @@ public final class TestKIS extends TestCase {
 		StorableObjectPool.flush(ObjectEntities.KIS_CODE, DatabaseCommonTest.getSysUser().getId(), false);
 	}
 
-	public void testGetByCondition() throws ApplicationException {
+	public void testAdd() throws ApplicationException {
+		final Identifier sysUserId = DatabaseCommonTest.getSysUser().getId();
+
 		final EquivalentCondition ec = new EquivalentCondition(ObjectEntities.KIS_CODE);
 		final Set<KIS> kiss = StorableObjectPool.getStorableObjectsByCondition(ec, true);
 		for (final KIS kis : kiss) {
 			System.out.println("KIS: '" + kis.getId() + "', '" + kis.getDescription() + "'");
 		}
+		assertTrue("Number of KISs: " + kiss.size(), kiss.size() == 1);
+		final KIS kis1 = kiss.iterator().next();
+
+		final Equipment equipment1 = StorableObjectPool.getStorableObject(kis1.getEquipmentId(), true);
+		assert equipment1 != null : ErrorMessages.NON_NULL_EXPECTED;
+
+		final Set<Port> ports = equipment1.getPorts();
+		assertTrue("Number of Ports: " + ports.size(), ports.size() > 0);
+		final PortType portType = ports.iterator().next().getType();
+
+		ec.setEntityCode(ObjectEntities.TRANSMISSIONPATH_CODE);
+		final Set<TransmissionPath> transmissionPaths = StorableObjectPool.getStorableObjectsByCondition(ec, true);
+		for (final TransmissionPath transmissionPath : transmissionPaths) {
+			System.out.println("TransmissionPath: '" + transmissionPath.getId() + "', '" + transmissionPath.getDescription() + "'");
+		}
+		assertTrue("Number of TransmissionPaths: " + transmissionPaths.size(), transmissionPaths.size() > 0);
+		final TransmissionPathType transmissionPathType = transmissionPaths.iterator().next().getType();
+
+		ec.setEntityCode(ObjectEntities.MEASUREMENTPORT_CODE);
+		final Set<MeasurementPort> measurementPorts = StorableObjectPool.getStorableObjectsByCondition(ec, true);
+		for (final MeasurementPort measurementPort : measurementPorts) {
+			System.out.println("MeasurementPort: '" + measurementPort.getId() + "', '" + measurementPort.getDescription() + "'");
+		}
+		assertTrue("Number of MeasurementPorts: " + measurementPorts.size(), measurementPorts.size() > 0);
+		final MeasurementPortType measurementPortType = measurementPorts.iterator().next().getType();
+
+
+		final Equipment equipment2 = Equipment.createInstance(sysUserId,
+				equipment1.getDomainId(),
+				equipment1.getProtoEquipmentId(),
+				equipment1.getName(),
+				"Рефлектометр QP1640MR",
+				equipment1.getImageId(),
+				equipment1.getSupplier(),
+				equipment1.getSupplierCode(),
+				equipment1.getLatitude(),
+				equipment1.getLongitude(),
+				equipment1.getHwSerial(),
+				equipment1.getHwVersion(),
+				equipment1.getSwSerial(),
+				equipment1.getSwVersion(),
+				equipment1.getInventoryNumber());
+
+		final Port startPort2 = Port.createInstance(sysUserId, portType, "start port", equipment2.getId());
+		final Port finishPort2 = Port.createInstance(sysUserId, portType, "finish port", equipment2.getId());
+
+		final TransmissionPath transmissionPath2 = TransmissionPath.createInstance(sysUserId,
+				equipment1.getDomainId(),
+				"TransmissionPath",
+				"TransmissionPath",
+				transmissionPathType,
+				startPort2.getId(),
+				finishPort2.getId());
+
+		final KIS kis2 = KIS.createInstance(sysUserId,
+				kis1.getDomainId(),
+				kis1.getName(),
+				"Рефлектометр QP1640MR",
+				"rtu-1",
+				(short) 7501,
+				equipment2.getId(),
+				kis1.getMCMId());
+
+		final MeasurementPort measurementPort2 = MeasurementPort.createInstance(sysUserId,
+				measurementPortType,
+				"MeasurementPort",
+				"MeasurementPort",
+				kis2.getId(),
+				startPort2.getId());
+
+		final MonitoredElement monitoredElement2 = MonitoredElement.createInstance(sysUserId,
+				kis1.getDomainId(),
+				"MonitoredElement",
+				measurementPort2.getId(),
+				MonitoredElementSort.MONITOREDELEMENT_SORT_TRANSMISSION_PATH,
+				"0:2:1:1",
+				Collections.singleton(transmissionPath2.getId()));
+
+		StorableObjectPool.flush(monitoredElement2, sysUserId, false);
 	}
+
 }
