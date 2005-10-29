06d79951d0b3758f1a60f5569dee2d52bfb8fb89
Arseniy Tashoyan <tashoyan@gmail.com>
2005-10-30 00:38:48 +0400

    MINOR: Renamed REFLECTOMETRY_QP1640 to REFLECTOMETRY_QP1640A

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/MeasurementPortTypeCodename.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/MeasurementPortTypeCodename.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: MeasurementPortTypeCodename.java,v 1.1 2005/10/29 16:57:57 arseniy Exp $
+ * $Id: MeasurementPortTypeCodename.java,v 1.2 2005/10/29 20:38:48 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,13 +8,13 @@
 package com.syrus.AMFICOM.measurement;
 
 /**
- * @version $Revision: 1.1 $, $Date: 2005/10/29 16:57:57 $
+ * @version $Revision: 1.2 $, $Date: 2005/10/29 20:38:48 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module measurement
  */
 public enum MeasurementPortTypeCodename {
-	REFLECTOMETRY_QP1640("reflectometry_qp1640"),
+	REFLECTOMETRY_QP1640A("reflectometry_qp1640a"),
 	REFLECTOMETRY_PK7600("reflectometry_pk7600"),
 
 	UNKNOWN("unknown");
