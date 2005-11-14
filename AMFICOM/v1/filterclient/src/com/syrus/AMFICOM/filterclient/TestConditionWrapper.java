8be2de79a6aaa06450801678b5636cbb9bb40ced
Maxim Selivanov <max.selivanov@gmail.com>
2005-11-14 18:03:27 +0300

    MINOR: name -> description

--- AMFICOM/v1/filterclient/src/com/syrus/AMFICOM/filterclient/TestConditionWrapper.java
+++ AMFICOM/v1/filterclient/src/com/syrus/AMFICOM/filterclient/TestConditionWrapper.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestConditionWrapper.java,v 1.13 2005/08/25 10:56:12 max Exp $
+ * $Id: TestConditionWrapper.java,v 1.14 2005/11/14 15:03:27 max Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -15,9 +15,10 @@ import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectWrapper;
 import com.syrus.AMFICOM.measurement.TestWrapper;
 import com.syrus.AMFICOM.newFilter.ConditionKey;
+import com.syrus.AMFICOM.newFilter.LangModelFilter;
 
 /**
- * @version $Revision: 1.13 $, $Date: 2005/08/25 10:56:12 $
+ * @version $Revision: 1.14 $, $Date: 2005/11/14 15:03:27 $
  * @author $Author: max $
  * @module filterclient
  */
@@ -25,25 +26,19 @@ public class TestConditionWrapper implements ConditionWrapper {
 
 	private static short entityCode = ObjectEntities.TEST_CODE;
 	
-	//private static final String STATUS = "Status";
-	private static final String START_TIME = "Start time";
-	private static final String END_TIME = "End time";
-	private static final String BYNAME = "Name";
-	//private static final String[] testStatusNames = new String[]{"NEW", "SCHEDULED", "PROCESSING", "COMPLETED", "ABORTED" }; 
-		
-	public static ConditionKey START_TIME_CONDITION_KEY = new ConditionKey(TestWrapper.COLUMN_START_TIME, START_TIME, ConditionWrapper.DATE);
-	public static ConditionKey END_TIME_CONDITION_KEY = new ConditionKey(TestWrapper.COLUMN_END_TIME, END_TIME, ConditionWrapper.DATE);
-	public static ConditionKey NAME_CONDITION_KEY = new ConditionKey(StorableObjectWrapper.COLUMN_NAME, BYNAME, ConditionWrapper.STRING);
+	private static final String START_TIME = LangModelFilter.getString("filter.criteria.starttime");
+	private static final String END_TIME = LangModelFilter.getString("filter.criteria.endtime");
+	private static final String NAME = LangModelFilter.getString("filter.criteria.entityname");
+			
+	private static ConditionKey START_TIME_CONDITION_KEY = new ConditionKey(TestWrapper.COLUMN_START_TIME, START_TIME, ConditionWrapper.DATE);
+	private static ConditionKey END_TIME_CONDITION_KEY = new ConditionKey(TestWrapper.COLUMN_END_TIME, END_TIME, ConditionWrapper.DATE);
+	private static ConditionKey NAME_CONDITION_KEY = new ConditionKey(StorableObjectWrapper.COLUMN_DESCRIPTION, NAME, ConditionWrapper.STRING);
 	
 	private static List<ConditionKey> keys = new ArrayList<ConditionKey>();
 	
 	static {
-		
 		keys.add(START_TIME_CONDITION_KEY);
 		keys.add(END_TIME_CONDITION_KEY);
-		//keys.add(new ConditionKey(ObjectEntities.MONITOREDELEMENT_CODE));
-		//keys.add(new ConditionKey(ObjectEntities.MEASUREMENTPORT_CODE));
-		//keys.add(new ConditionKey(ObjectEntities.MCM_CODE));
 		keys.add(NAME_CONDITION_KEY);
 	}
 	
