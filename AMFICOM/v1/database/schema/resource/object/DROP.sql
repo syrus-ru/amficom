3ad71a33a23effb36f475ab7f3076fae44e557b7
Arseniy Tashoyan <tashoyan@gmail.com>
2005-11-14 14:08:53 +0300

    BUGFIX: Order of creation and drop

--- AMFICOM/v1/database/schema/resource/object/DROP.sql
+++ AMFICOM/v1/database/schema/resource/object/DROP.sql
@@ -1,8 +1,8 @@
--- $Id: DROP.sql,v 1.5 2005/11/14 11:00:39 bob Exp $
+-- $Id: DROP.sql,v 1.6 2005/11/14 11:08:53 arseniy Exp $
 
+DROP TABLE LayoutItem;
 DROP TABLE ImportUIDMap;
 DROP TABLE ImageResource;
-DROP TABLE LayoutItem;
 
-DROP SEQUENCE ImageResource_Seq;
 DROP SEQUENCE LayoutItem_Seq;
+DROP SEQUENCE ImageResource_Seq;
