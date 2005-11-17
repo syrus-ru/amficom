c066baafd08600ee45efe8b68769196f2c75047f
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-11-17 19:23:28 +0300

    MAJOR: rewritten to utilize DeliveryAttributes and send e-mail

--- AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/LineMismatchEventProcessor.java
+++ AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/LineMismatchEventProcessor.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: LineMismatchEventProcessor.java,v 1.8 2005/11/16 10:21:46 arseniy Exp $
+ * $Id: LineMismatchEventProcessor.java,v 1.9 2005/11/17 16:23:28 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,28 +8,42 @@
 
 package com.syrus.AMFICOM.leserver;
 
-import static com.syrus.AMFICOM.administration.SystemUserWrapper.COLUMN_SORT;
-import static com.syrus.AMFICOM.administration.corba.IdlSystemUserPackage.SystemUserSort._USER_SORT_REGULAR;
-import static com.syrus.AMFICOM.administration.corba.IdlSystemUserPackage.SystemUserSort._USER_SORT_SYSADMIN;
+import static com.syrus.AMFICOM.administration.SystemUserWrapper.COLUMN_LOGIN;
+import static com.syrus.AMFICOM.administration.SystemUserWrapper.LOGINPROCESSOR_LOGIN;
 import static com.syrus.AMFICOM.eventv2.EventType.LINE_MISMATCH;
+import static com.syrus.AMFICOM.general.CharacteristicTypeCodenames.USER_EMAIL;
+import static com.syrus.AMFICOM.general.ErrorMessages.NON_NULL_EXPECTED;
+import static com.syrus.AMFICOM.general.Identifier.VOID_IDENTIFIER;
+import static com.syrus.AMFICOM.general.ObjectEntities.CHARACTERISTIC_CODE;
+import static com.syrus.AMFICOM.general.ObjectEntities.CHARACTERISTIC_TYPE_CODE;
 import static com.syrus.AMFICOM.general.ObjectEntities.SYSTEMUSER_CODE;
-import static com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlCompoundConditionPackage.CompoundConditionSort.OR;
+import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_CODENAME;
+import static com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlCompoundConditionPackage.CompoundConditionSort.AND;
 import static com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlTypicalConditionPackage.OperationSort.OPERATION_EQUALS;
+import static java.util.logging.Level.FINEST;
 import static java.util.logging.Level.SEVERE;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.omg.CORBA.ORB;
 
 import com.syrus.AMFICOM.administration.SystemUser;
+import com.syrus.AMFICOM.event.DeliveryAttributes;
+import com.syrus.AMFICOM.eventv2.DefaultEmailNotificationEvent;
 import com.syrus.AMFICOM.eventv2.DefaultPopupNotificationEvent;
+import com.syrus.AMFICOM.eventv2.EmailNotificationEvent;
 import com.syrus.AMFICOM.eventv2.Event;
 import com.syrus.AMFICOM.eventv2.EventType;
 import com.syrus.AMFICOM.eventv2.LineMismatchEvent;
 import com.syrus.AMFICOM.eventv2.PopupNotificationEvent;
 import com.syrus.AMFICOM.eventv2.corba.IdlEvent;
 import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.Characteristic;
+import com.syrus.AMFICOM.general.CharacteristicType;
 import com.syrus.AMFICOM.general.CompoundCondition;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.LinkedIdsCondition;
 import com.syrus.AMFICOM.general.StorableObjectCondition;
 import com.syrus.AMFICOM.general.StorableObjectPool;
 import com.syrus.AMFICOM.general.TypicalCondition;
@@ -38,24 +52,21 @@ import com.syrus.util.Log;
 
 /**
  * @author Andrew ``Bass'' Shcheglov
- * @author $Author: arseniy $
- * @version $Revision: 1.8 $, $Date: 2005/11/16 10:21:46 $
+ * @author $Author: bass $
+ * @version $Revision: 1.9 $, $Date: 2005/11/17 16:23:28 $
  * @module leserver
  */
 final class LineMismatchEventProcessor implements EventProcessor {
-	private static final StorableObjectCondition CONDITION =
-			new CompoundCondition(
-					new TypicalCondition(
-							_USER_SORT_SYSADMIN,
-							OPERATION_EQUALS,
-							SYSTEMUSER_CODE,
-							COLUMN_SORT),
-					OR,
-					new TypicalCondition(
-							_USER_SORT_REGULAR,
-							OPERATION_EQUALS,
-							SYSTEMUSER_CODE,
-							COLUMN_SORT));
+	private static Identifier creatorId = null;
+
+	private static Identifier characteristicTypeId = null;
+
+	private static StorableObjectCondition condition = null;
+
+
+
+
+	
 
 	/**
 	 * @see EventProcessor#getEventType()
@@ -74,26 +85,37 @@ final class LineMismatchEventProcessor implements EventProcessor {
 		final LineMismatchEvent lineMismatchEvent = (LineMismatchEvent) event;
 		Log.debugMessage("LineMismatchEvent: "
 				+ lineMismatchEvent + " started being processed",
-				SEVERE);
+				FINEST);
 
 		try {
 			final LEServerServantManager servantManager = LEServerSessionEnvironment.getInstance().getLEServerServantManager();
 			final ORB orb = servantManager.getCORBAServer().getOrb();
 
-			/*@todo Instead of load all client users make real check of delivery attributes*/
-			final Set<SystemUser> systemUsers =
-					StorableObjectPool.getStorableObjectsByCondition(CONDITION, true);
-			final IdlEvent popupNotificationEvents[] = new IdlEvent[systemUsers.size()];
-			int i = 0; 
-			for (final SystemUser systemUser : systemUsers) {
+			final Set<SystemUser> systemUsers = DeliveryAttributes
+					.getInstance(getCreatorId(), lineMismatchEvent.getSeverity())
+					.getSystemUsersRecursively();
+			final Set<Identifier> systemUserIds = Identifier.createIdentifiers(systemUsers);
+
+			final Set<String> addresses = getAddresses(systemUserIds);
+
+			final IdlEvent notificationEvents[] = new IdlEvent[systemUserIds.size() + addresses.size()];
+			int i = 0;
+			for (final Identifier systemUserId : systemUserIds) {
 				final PopupNotificationEvent popupNotificationEvent =
 						DefaultPopupNotificationEvent.valueOf(
 								lineMismatchEvent,
-								systemUser.getId());
-				popupNotificationEvents[i++] = popupNotificationEvent.getTransferable(orb);
+								systemUserId);
+				notificationEvents[i++] = popupNotificationEvent.getTransferable(orb);
+			}
+			for (final String address : addresses) {
+				final EmailNotificationEvent emailNotificationEvent =
+							DefaultEmailNotificationEvent.valueOf(
+									lineMismatchEvent,
+									address);
+				notificationEvents[i++] = emailNotificationEvent.getTransferable(orb);
 			}
 
-			servantManager.getEventServerReference().receiveEvents(popupNotificationEvents);
+			servantManager.getEventServerReference().receiveEvents(notificationEvents);
 		} catch (final ApplicationException ae) {
 			throw new EventProcessingException(ae);
 		} catch (final IdlEventProcessingException epe) {
@@ -105,6 +127,91 @@ final class LineMismatchEventProcessor implements EventProcessor {
 			 *     -- Old Wise Saa.
 			 */
 			throw new EventProcessingException(epe.message, epe);
+		}
+	}
+
+	private Set<String> getAddresses(final Set<Identifier> systemUserIds) throws ApplicationException {
+		final Set<Characteristic> characteristics = StorableObjectPool.getStorableObjectsByCondition(
+				new CompoundCondition(
+						getCondition(),
+						AND,
+						new LinkedIdsCondition(
+								systemUserIds,
+								CHARACTERISTIC_CODE)),
+				true);
+		final Set<String> addresses = new HashSet<String>();
+		for (final Characteristic characteristic : characteristics) {
+			addresses.add(characteristic.getValue());
+		}
+		return addresses;
+	}
+
+	private static StorableObjectCondition getCondition() {
+		synchronized (LineMismatchEventProcessor.class) {
+			if (condition == null) {
+				condition = new LinkedIdsCondition(getCharacteristicTypeId(), CHARACTERISTIC_CODE);
+			}
+			return condition;
+		}
+	}
+
+	private static Identifier getCharacteristicTypeId() {
+		synchronized (LineMismatchEventProcessor.class) {
+			if (characteristicTypeId == null) {
+				try {
+					final Set<CharacteristicType> characteristicTypes = StorableObjectPool.getStorableObjectsByCondition(
+							new TypicalCondition(
+									USER_EMAIL,
+									OPERATION_EQUALS,
+									CHARACTERISTIC_TYPE_CODE,
+									COLUMN_CODENAME),
+							true);
+					assert characteristicTypes != null : NON_NULL_EXPECTED;
+					final int size = characteristicTypes.size();
+					assert size == 1 : size;
+					characteristicTypeId = characteristicTypes.iterator().next().getId();
+				} catch (final ApplicationException ae) {
+					Log.debugMessage(ae, SEVERE);
+					characteristicTypeId = VOID_IDENTIFIER;
+		
+					/*
+					 * Never. But log the exception prior to issuing an
+					 * eror.
+					 */
+					assert false;
+				}
+			}
+			return characteristicTypeId;
+		}
+	}
+
+	private static Identifier getCreatorId() {
+		synchronized (LineMismatchEventProcessor.class) {
+			if (creatorId == null) {
+				try {
+					final Set<SystemUser> systemUsers = StorableObjectPool.getStorableObjectsByCondition(
+							new TypicalCondition(
+									LOGINPROCESSOR_LOGIN,
+									OPERATION_EQUALS,
+									SYSTEMUSER_CODE,
+									COLUMN_LOGIN),
+							true);
+					assert systemUsers != null : NON_NULL_EXPECTED;
+					final int size = systemUsers.size();
+					assert size == 1 : size;
+					creatorId = systemUsers.iterator().next().getId();
+				} catch (final ApplicationException ae) {
+					Log.debugMessage(ae, SEVERE);
+					creatorId = VOID_IDENTIFIER;
+		
+					/*
+					 * Never. But log the exception prior to issuing an
+					 * eror.
+					 */
+					assert false;
+				}
+			}
+			return creatorId;
 		}
 	}
 }
