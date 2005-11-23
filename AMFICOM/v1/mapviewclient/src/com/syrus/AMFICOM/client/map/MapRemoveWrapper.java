41790a5cc9c83767201a73041e6996ce4116e836
Stanislav Kholshin <kholshin@gmail.com>
2005-11-23 16:57:30 +0300

    fix: when delete map unbound linked scheme

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/MapRemoveWrapper.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/MapRemoveWrapper.java
@@ -1,5 +1,5 @@
 /**
- * $Id: MapRemoveWrapper.java,v 1.5 2005/10/26 14:17:34 krupenn Exp $
+ * $Id: MapRemoveWrapper.java,v 1.6 2005/11/23 13:57:30 stas Exp $
  *
  * Copyright 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -18,6 +18,7 @@ import com.syrus.AMFICOM.client.resource.MapEditorResourceKeys;
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.LinkedIdsCondition;
+import com.syrus.AMFICOM.general.LoginManager;
 import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectPool;
 import com.syrus.AMFICOM.map.Map;
@@ -64,16 +65,6 @@ public class MapRemoveWrapper extends DefaultStorableObjectRemoveWrapper {
 						JOptionPane.ERROR_MESSAGE);
 				return false;
 			}
-			condition = new LinkedIdsCondition(map.getId(), ObjectEntities.SCHEME_CODE);
-			Set<Scheme> schemes = StorableObjectPool.getStorableObjectsByCondition(condition, true);
-			if(!schemes.isEmpty()) {
-				JOptionPane.showMessageDialog(
-						Environment.getActiveWindow(), 
-						I18N.getString(MapEditorResourceKeys.ERROR_LINKED_OBJECTS_EXIST_CANNOT_REMOVE) + " - " + schemes.iterator().next().getName(), 
-						I18N.getString(MapEditorResourceKeys.ERROR), 
-						JOptionPane.ERROR_MESSAGE);
-				return false;
-			}
 			condition = new LinkedIdsCondition(Identifier.createIdentifiers(map.getSiteNodes()), ObjectEntities.MAP_CODE);
 			maps = StorableObjectPool.getStorableObjectsByCondition(condition, true);
 			if(!maps.isEmpty()) {
@@ -83,6 +74,21 @@ public class MapRemoveWrapper extends DefaultStorableObjectRemoveWrapper {
 						I18N.getString(MapEditorResourceKeys.ERROR), 
 						JOptionPane.ERROR_MESSAGE);
 				return false;
+			}
+			condition = new LinkedIdsCondition(map.getId(), ObjectEntities.SCHEME_CODE);
+			Set<Scheme> schemes = StorableObjectPool.getStorableObjectsByCondition(condition, true);
+			if(!schemes.isEmpty()) {
+				// отвязываем схемы
+				for (Scheme scheme : schemes) {
+					scheme.setMap(null);
+					StorableObjectPool.flush(scheme, LoginManager.getUserId(), false);
+				}
+//				JOptionPane.showMessageDialog(
+//						Environment.getActiveWindow(), 
+//						I18N.getString(MapEditorResourceKeys.ERROR_LINKED_OBJECTS_EXIST_CANNOT_REMOVE) + " - " + schemes.iterator().next().getName(), 
+//						I18N.getString(MapEditorResourceKeys.ERROR), 
+//						JOptionPane.ERROR_MESSAGE);
+//				return false;
 			}
 			return super.remove(object);
 		} catch(ApplicationException e) {
