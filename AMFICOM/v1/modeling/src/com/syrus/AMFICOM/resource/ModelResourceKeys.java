e82fe7b2d69d5e75258f55145bd7d132d2786fcf
Stanislav Kholshin <kholshin@gmail.com>
2005-11-24 18:48:17 +0300

    add: internationalization fix: Modeling made workable

---
+++ AMFICOM/v1/modeling/src/com/syrus/AMFICOM/resource/ModelResourceKeys.java
@@ -0,0 +1,13 @@
+/*-
+ * $Id: ModelResourceKeys.java,v 1.1 2005/11/24 15:48:17 stas Exp $
+ *
+ * Copyright ¿ 2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
+package com.syrus.AMFICOM.resource;
+
+public interface ModelResourceKeys {
+	String ICON_MODEL_MAIN = "icon.model.main";
+}
