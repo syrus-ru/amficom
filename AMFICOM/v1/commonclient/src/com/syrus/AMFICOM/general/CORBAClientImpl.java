5bfa7bf83821a25968c70576c8c8aafb641b9ecc
Arseniy Tashoyan <tashoyan@gmail.com>
2005-11-28 15:24:55 +0300

    ADD: Method 'getHostName' from interface CommonUser REMOVE: Method 'getSystemUserId'

--- AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/CORBAClientImpl.java
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/CORBAClientImpl.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: CORBAClientImpl.java,v 1.6 2005/10/31 12:30:02 bass Exp $
+ * $Id: CORBAClientImpl.java,v 1.7 2005/11/28 12:24:55 arseniy Exp $
  *
  * Copyright © 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -7,6 +7,8 @@
  */
 package com.syrus.AMFICOM.general;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,23 +18,37 @@ import com.syrus.AMFICOM.client.event.PopupMessageReceiver;
 import com.syrus.AMFICOM.eventv2.Event;
 import com.syrus.AMFICOM.eventv2.corba.IdlEvent;
 import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
-import com.syrus.AMFICOM.general.corba.CORBAClientOperations;
-import com.syrus.AMFICOM.general.corba.IdlIdentifier;
+import com.syrus.AMFICOM.general.corba.CORBAClientPOA;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.6 $, $Date: 2005/10/31 12:30:02 $
- * @author $Author: bass $
+ * @version $Revision: 1.7 $, $Date: 2005/11/28 12:24:55 $
+ * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module commonclient
  */
-final class CORBAClientImpl implements CORBAClientOperations {
+final class CORBAClientImpl extends CORBAClientPOA {
 	private static final long serialVersionUID = -2562740055558726787L;
 
-	final List<PopupMessageReceiver> popupMessageReceivers;
+	private final String hostName;
+	private final List<PopupMessageReceiver> popupMessageReceivers;
 
 	public CORBAClientImpl() {
+		String hostname;
+		try {
+			hostname = InetAddress.getLocalHost().getCanonicalHostName();
+		} catch (UnknownHostException uhe) {
+			hostname = "unknown";
+			//Не бывать тому!
+			Log.errorMessage(uhe);
+		}
+		this.hostName = hostname;
+
 		this.popupMessageReceivers = Collections.synchronizedList(new LinkedList<PopupMessageReceiver>());
+	}
+
+	public String getHostName() {
+		return this.hostName;
 	}
 
 	public void receiveMessages(final IdlEvent idlEvent) throws AMFICOMRemoteException {
@@ -44,14 +60,6 @@ final class CORBAClientImpl implements CORBAClientOperations {
 		}
 	}
 
-	void addPopupMessageReceiver(final PopupMessageReceiver popupMessageReceiver) {
-		this.popupMessageReceivers.add(popupMessageReceiver);
-	}
-
-	void removePopupMessageReceiver(final PopupMessageReceiver popupMessageReceiver) {
-		this.popupMessageReceivers.remove(popupMessageReceiver);
-	}
-
 	public void verify(final byte b) {
 		try {
 			Log.debugMessage("Verify value: " + b, Level.CONFIG);
@@ -60,10 +68,12 @@ final class CORBAClientImpl implements CORBAClientOperations {
 		}
 	}
 
-	/**
-	 * @see CORBAClientOperations#getSystemUserId()
-	 */
-	public IdlIdentifier getSystemUserId() {
-		return LoginManager.getUserId().getTransferable();
+	void addPopupMessageReceiver(final PopupMessageReceiver popupMessageReceiver) {
+		this.popupMessageReceivers.add(popupMessageReceiver);
 	}
+
+	void removePopupMessageReceiver(final PopupMessageReceiver popupMessageReceiver) {
+		this.popupMessageReceivers.remove(popupMessageReceiver);
+	}
+
 }
