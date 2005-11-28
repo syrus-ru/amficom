32fe9fcca105675714379f27058884cfb125d43a
Arseniy Tashoyan <tashoyan@gmail.com>
2005-11-28 15:30:28 +0300

    REMOVE: Method 'eventGeneration' as unused

--- AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/EventServerImplementation.java
+++ AMFICOM/v1/leserver/src/com/syrus/AMFICOM/leserver/EventServerImplementation.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: EventServerImplementation.java,v 1.25 2005/10/31 11:06:34 bass Exp $
+ * $Id: EventServerImplementation.java,v 1.26 2005/11/28 12:30:28 arseniy Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -12,35 +12,20 @@ import static java.util.logging.Level.INFO;
 import static java.util.logging.Level.SEVERE;
 import static java.util.logging.Level.WARNING;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.syrus.AMFICOM.eventv2.Event;
 import com.syrus.AMFICOM.eventv2.corba.IdlEvent;
-import com.syrus.AMFICOM.general.CreateObjectException;
-import com.syrus.AMFICOM.general.Identifier;
-import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
-import com.syrus.AMFICOM.general.corba.AMFICOMRemoteExceptionPackage.IdlCompletionStatus;
-import com.syrus.AMFICOM.general.corba.AMFICOMRemoteExceptionPackage.IdlErrorCode;
 import com.syrus.AMFICOM.leserver.corba.EventServerPOA;
 import com.syrus.AMFICOM.leserver.corba.EventServerPackage.IdlEventProcessingException;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.25 $, $Date: 2005/10/31 11:06:34 $
- * @author $Author: bass $
+ * @version $Revision: 1.26 $, $Date: 2005/11/28 12:30:28 $
+ * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module leserver
  */
 final class EventServerImplementation extends EventServerPOA {
 	private static final long serialVersionUID = 3257569516216398643L;
-
-	/**
-	 * Map of event queues for every user
-	 */
-	private static final Map<Identifier, UserEventNotifier> USER_EVENT_NOTIFIERS_MAP =
-		Collections.synchronizedMap(new HashMap<Identifier, UserEventNotifier>());
 
 	EventServerImplementation() {
 		EventProcessorRegistry.registerEventProcessor(new ReflectogramMismatchEventProcessor());
@@ -48,35 +33,6 @@ final class EventServerImplementation extends EventServerPOA {
 		EventProcessorRegistry.registerEventProcessor(new PopupNotificationEventProcessor());
 		EventProcessorRegistry.registerEventProcessor(new EmailNotificationEventProcessor());
 		EventProcessorRegistry.registerEventProcessor(new SmsNotificationEventProcessor());
-	}
-
-	/**
-	 * @param idlEvent
-	 * @throws AMFICOMRemoteException
-	 * @see com.syrus.AMFICOM.leserver.corba.EventServerOperations#eventGeneration(com.syrus.AMFICOM.event.corba.IdlEvent)
-	 */
-	public void eventGeneration(
-			final com.syrus.AMFICOM.event.corba.IdlEvent idlEvent)
-	throws AMFICOMRemoteException {
-		try {
-			final com.syrus.AMFICOM.event.Event event = new com.syrus.AMFICOM.event.Event(idlEvent);
-			final com.syrus.AMFICOM.event.EventType eventType = event.getType();
-			for (final Identifier userId : eventType.getAlertedUserIds()) {
-				UserEventNotifier userEventNotifier = USER_EVENT_NOTIFIERS_MAP.get(userId);
-				if (userEventNotifier == null) {
-					userEventNotifier = new UserEventNotifier(userId);
-					userEventNotifier.start();
-					USER_EVENT_NOTIFIERS_MAP.put(userId, userEventNotifier);
-				}
-				userEventNotifier.addEvent(event);
-			}
-		} catch (final CreateObjectException coe) {
-			Log.errorMessage(coe);
-			throw new AMFICOMRemoteException(
-					IdlErrorCode.ERROR_ILLEGAL_DATA,
-					IdlCompletionStatus.COMPLETED_NO,
-					"Cannot create event -- " + coe.getMessage());
-		}
 	}
 
 	/**
