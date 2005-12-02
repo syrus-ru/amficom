ba3e41e6507b88c5ef5f8f380f2400eadcb52ce4
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-12-02 14:22:38 +0300

    CONTRACT: a. now implements Comparable; b. ctor is non-public now; c. instances can be obtained via #valueOf(long), which also performs caching.

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectVersion.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/StorableObjectVersion.java
@@ -1,44 +1,48 @@
 /*-
- * $Id: StorableObjectVersion.java,v 1.10 2005/12/02 09:44:26 arseniy Exp $
+ * $Id: StorableObjectVersion.java,v 1.11 2005/12/02 11:22:38 bass Exp $
  *
  * Copyright © 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
  * Project: AMFICOM.
  */
+
 package com.syrus.AMFICOM.general;
 
 import java.io.Serializable;
 
 /**
- * @version $Revision: 1.10 $, $Date: 2005/12/02 09:44:26 $
- * @author $Author: arseniy $
+ * @version $Revision: 1.11 $, $Date: 2005/12/02 11:22:38 $
+ * @author $Author: bass $
  * @author Tashoyan Arseniy Feliksovich
  * @module general
  */
-public final class StorableObjectVersion implements Serializable {
-	private static final long serialVersionUID = 4373418889318876641L;
+public final class StorableObjectVersion implements Serializable, Comparable<StorableObjectVersion> {
+	private static final long serialVersionUID = 5911000938457805521L;
 
-	public transient static final StorableObjectVersion INITIAL_VERSION = new StorableObjectVersion(0L);
-	public transient static final StorableObjectVersion ILLEGAL_VERSION = new StorableObjectVersion(-1L);
+	/**
+	 * Corresponds to {@link #ILLEGAL_VERSION}
+	 */
+	private static final int MIN_CACHED_VERSION = -1;
+
+	private static final int MAX_CACHED_VERSION = 256;
+	
+	public static final StorableObjectVersion INITIAL_VERSION = valueOf(0L);
+	public static final StorableObjectVersion ILLEGAL_VERSION = valueOf(-1L); 
 
 	private final long version;
 
-	public StorableObjectVersion(final long version) {
+	StorableObjectVersion(final long version) {
 		this.version = version;
 	}
 
-	public boolean isNewer(final StorableObjectVersion storableObjectVersion) {
-		if (Math.abs(this.version - storableObjectVersion.version) < (Long.MAX_VALUE >> 1 - Long.MIN_VALUE >> 1)) {
-			return (this.version > storableObjectVersion.version);
-		}
-		return (this.version < storableObjectVersion.version);
+	public boolean isNewer(final StorableObjectVersion that) {
+		return (Math.abs(this.version - that.version) < (Long.MAX_VALUE >> 1 - Long.MIN_VALUE >> 1))
+			? (this.version > that.version)
+			: (this.version < that.version);
 	}
 
-	public final boolean isOlder(final StorableObjectVersion storableObjectVersion) {
-		if (Math.abs(this.version - storableObjectVersion.version) < (Long.MAX_VALUE >> 1 - Long.MIN_VALUE >> 1)) {
-			return (this.version < storableObjectVersion.version);
-		}
-		return (this.version > storableObjectVersion.version);
+	public final boolean isOlder(final StorableObjectVersion that) {
+		return that.isNewer(this);
 	}
 
 	public StorableObjectVersion increment() {
@@ -50,9 +54,9 @@ public final class StorableObjectVersion implements Serializable {
 			if (newVersion == INITIAL_VERSION.longValue()) {
 				newVersion++;
 			}
-			return new StorableObjectVersion(newVersion);
+			return valueOf(newVersion);
 		}
-		return new StorableObjectVersion(Long.MIN_VALUE);
+		return valueOf(Long.MIN_VALUE);
 	}
 
 	public long longValue() {
@@ -61,15 +65,8 @@ public final class StorableObjectVersion implements Serializable {
 
 	@Override
 	public final boolean equals(final Object object) {
-		if (this == object) {
-			return true;
-		}
-
-		if (object instanceof StorableObjectVersion) {
-			final StorableObjectVersion storableObjectVersion = (StorableObjectVersion) object;
-			return this.version == storableObjectVersion.version;
-		}
-		return false;
+		return object == this || (object instanceof StorableObjectVersion
+				&& this.version == ((StorableObjectVersion) object).version);
 	}
 
 	@Override
@@ -77,27 +74,40 @@ public final class StorableObjectVersion implements Serializable {
 		return (int) (this.version ^ (this.version >>> 32));
 	}
 
-	/**
-	 * @deprecated as unnecessary
-	 */
-	@Override
-	@Deprecated
-	public StorableObjectVersion clone() {
-		return new StorableObjectVersion(this.version);
-	}
-
-	
 	@Override
 	public String toString() {
 		return Long.toString(this.version);
 	}
 
-	/**
-	 * @deprecated use direct {@link #INITIAL_VERSION} 
-	 */
-	@Deprecated
-	public static StorableObjectVersion createInitial() {
-		return INITIAL_VERSION;
+	public int compareTo(final StorableObjectVersion that) {
+		return this.isOlder(that)
+				? -1
+				: this.equals(that) ? 0 : 1;
 	}
 
+	public static StorableObjectVersion valueOf(final long l) {
+		return l >= MIN_CACHED_VERSION && l <= MAX_CACHED_VERSION
+			? VersionCache.cache[(int) l - MIN_CACHED_VERSION]
+			: new StorableObjectVersion(l);
+	}
+
+	/**
+	 * @author Andrew ``Bass'' Shcheglov
+	 * @спизжено_у Josh Bloch
+	 * @author $Author: bass $
+	 * @version $Revision: 1.11 $, $Date: 2005/12/02 11:22:38 $
+	 * @module general
+	 */
+	private static class VersionCache {
+		private VersionCache() {
+			assert false;
+		}
+
+		static final StorableObjectVersion cache[] = new StorableObjectVersion[MAX_CACHED_VERSION - MIN_CACHED_VERSION + 1];
+
+		static {
+			for (int i = 0; i < cache.length; i++)
+				cache[i] = new StorableObjectVersion(i + MIN_CACHED_VERSION);
+		}
+	}
 }
