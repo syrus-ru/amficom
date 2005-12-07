db1b8d8d4e25e030a5485405bc70e7e040ec6b51
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-12-07 17:06:05 +0300

    FIX use URL instead of File

--- AMFICOM/v1/extensions/src/com/syrus/AMFICOM/resources/ImageResourceHandler.java
+++ AMFICOM/v1/extensions/src/com/syrus/AMFICOM/resources/ImageResourceHandler.java
@@ -1,5 +1,5 @@
 /*-
-* $Id: ImageResourceHandler.java,v 1.2 2005/11/11 12:46:16 bass Exp $
+* $Id: ImageResourceHandler.java,v 1.3 2005/12/07 14:06:05 bob Exp $
 *
 * Copyright ¿ 2005 Syrus Systems.
 * Dept. of Science & Technology.
@@ -9,7 +9,7 @@
 package com.syrus.AMFICOM.resources;
 
 import java.awt.Toolkit;
-import java.io.File;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.UIDefaults;
@@ -19,16 +19,19 @@ import com.syrus.AMFICOM.extensions.resources.Image;
 
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/11/11 12:46:16 $
- * @author $Author: bass $
+ * @version $Revision: 1.3 $, $Date: 2005/12/07 14:06:05 $
+ * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module extensions
  */
 public class ImageResourceHandler implements ConcreateResourceHandler<Image> {
+	
 	public void load(final Image image) {
 		final String id = image.getId();
 		final String filename = image.getFilename();
-		final File file = new File(filename);
+		
+		final URL resource = ImageResourceHandler.class.getClassLoader().getResource(filename);
+		
 		final boolean useSize = image.isSetSize();
 		final int width = useSize ? image.getSize().getWidth() : -1;
 		final int height = useSize ? image.getSize().getHeight() : -1;
@@ -37,12 +40,11 @@ public class ImageResourceHandler implements ConcreateResourceHandler<Image> {
 			System.out.println(id + " required for scaling to : " + width + "x" + height);
 		}
 		
-		if (file.exists()) {
+		if (resource != null) {
 			UIManager.put(id, new UIDefaults.LazyValue() {
 
 				public Object createValue(UIDefaults table) {
-					final java.awt.Image image2 =
-						Toolkit.getDefaultToolkit().getImage(filename);
+					java.awt.Image image2 = Toolkit.getDefaultToolkit().createImage(resource);
 					final java.awt.Image image3 = useSize ? 
 							image2.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH) : image2;
 					return new ImageIcon(image3);
