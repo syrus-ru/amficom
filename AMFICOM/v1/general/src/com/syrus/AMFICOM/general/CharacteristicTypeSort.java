bd4462e8245890e5a14e18f1532b0b224949b3d0
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2005-12-07 22:22:41 +0300

    CONTRACT: mutable transferable proxy added; enum itself is no longer mutable.

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/CharacteristicTypeSort.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/CharacteristicTypeSort.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: CharacteristicTypeSort.java,v 1.2 2005/12/07 17:16:24 bass Exp $
+ * $Id: CharacteristicTypeSort.java,v 1.3 2005/12/07 19:22:41 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,17 +9,22 @@
 package com.syrus.AMFICOM.general;
 
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.portable.IDLEntity;
 
 import com.syrus.AMFICOM.general.corba.IdlCharacteristicTypePackage.IdlCharacteristicTypeSort;
+import com.syrus.AMFICOM.general.xml.XmlCharacteristicTypeSort;
 import com.syrus.util.transport.idl.IdlTransferableObject;
+import com.syrus.util.transport.idl.IdlTransferableObjectExt;
+import com.syrus.util.transport.xml.XmlConversionException;
+import com.syrus.util.transport.xml.XmlTransferableObject;
 
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.2 $, $Date: 2005/12/07 17:16:24 $
+ * @version $Revision: 1.3 $, $Date: 2005/12/07 19:22:41 $
  * @module general
  */
-public enum CharacteristicTypeSort implements IdlTransferableObject<IdlCharacteristicTypeSort> {
+public enum CharacteristicTypeSort {
 	OPTICAL,
 	ELECTRICAL,
 	OPERATIONAL,
@@ -27,14 +32,6 @@ public enum CharacteristicTypeSort implements IdlTransferableObject<IdlCharacter
 	VISUAL;
 
 	private static final CharacteristicTypeSort VALUES[] = values();
-
-	/**
-	 * @param orb
-	 * @see IdlTransferableObject#getIdlTransferable(ORB)
-	 */
-	public IdlCharacteristicTypeSort getIdlTransferable(final ORB orb) {
-		return IdlCharacteristicTypeSort.from_int(this.ordinal());
-	}
 
 	/**
 	 * @param i
@@ -51,5 +48,93 @@ public enum CharacteristicTypeSort implements IdlTransferableObject<IdlCharacter
 	public static CharacteristicTypeSort valueOf(
 			final IdlCharacteristicTypeSort characteristicTypeSort) {
 		return valueOf(characteristicTypeSort.value());
+	}
+
+	/**
+	 * @param characteristicTypeSort
+	 * @throws ArrayIndexOutOfBoundsException
+	 */
+	public static CharacteristicTypeSort valueOf(
+			final XmlCharacteristicTypeSort characteristicTypeSort) {
+		return valueOf(characteristicTypeSort.enumValue().intValue() - 1);
+	}
+
+	/**
+	 * A mutable holder for immutable enum instances.
+	 *
+	 * @author Andrew ``Bass'' Shcheglov
+	 * @author $Author: bass $
+	 * @version $Revision: 1.3 $, $Date: 2005/12/07 19:22:41 $
+	 * @module general
+	 */
+	public static final class Proxy
+			implements IdlTransferableObjectExt<IdlCharacteristicTypeSort>,
+			XmlTransferableObject<XmlCharacteristicTypeSort> {
+		private static final long serialVersionUID = -2859495371772821057L;
+
+		private CharacteristicTypeSort value;
+
+		/**
+		 * Creates a new uninitialized instance.
+		 */
+		public Proxy() {
+			// empty
+		}
+
+		public Proxy(final CharacteristicTypeSort value) {
+			this.value = value;
+		}
+
+		public CharacteristicTypeSort getValue() {
+			return this.value;
+		}
+
+		public void setValue(final CharacteristicTypeSort value) {
+			this.value = value;
+		}
+
+		/**
+		 * @param orb
+		 * @see IdlTransferableObject#getIdlTransferable(ORB)
+		 */
+		public IdlCharacteristicTypeSort getIdlTransferable(final ORB orb) {
+			return IdlCharacteristicTypeSort.from_int(this.value.ordinal());
+		}
+
+		/**
+		 * @param characteristicTypeSort
+		 * @see IdlTransferableObjectExt#fromIdlTransferable(IDLEntity)
+		 */
+		public void fromIdlTransferable(final IdlCharacteristicTypeSort characteristicTypeSort) {
+			this.value = valueOf(characteristicTypeSort);
+		}
+
+		/**
+		 * @param characteristicTypeSort
+		 * @param importType
+		 * @param usePool
+		 * @throws XmlConversionException
+		 * @see XmlTransferableObject#getXmlTransferable(org.apache.xmlbeans.XmlObject, String, boolean)
+		 */
+		public void getXmlTransferable(
+				final XmlCharacteristicTypeSort characteristicTypeSort,
+				final String importType,
+				final boolean usePool)
+		throws XmlConversionException {
+			characteristicTypeSort.set(XmlCharacteristicTypeSort.Enum.forInt(this.value.ordinal() + 1));
+		}
+
+		/**
+		 * @param characteristicTypeSort
+		 * @param importType
+		 * @throws XmlConversionException
+		 * @see XmlTransferableObject#fromXmlTransferable(org.apache.xmlbeans.XmlObject, String)
+		 */
+		public void fromXmlTransferable(
+				final XmlCharacteristicTypeSort characteristicTypeSort,
+				final String importType)
+		throws XmlConversionException {
+			this.value = valueOf(characteristicTypeSort);
+		}
 	}
 }
