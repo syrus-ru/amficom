dcc7bce4f921dffae5ca868a99ab21cf62905b19
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-12-07 18:40:44 +0300

    base impl

---
+++ AMFICOM/v1/manager/src/com/syrus/AMFICOM/manager/UI/CellBuffer.java
@@ -0,0 +1,56 @@
+/*-
+* $Id: CellBuffer.java,v 1.1 2005/12/07 15:40:44 bob Exp $
+*
+* Copyright ¿ 2005 Syrus Systems.
+* Dept. of Science & Technology.
+* Project: AMFICOM.
+*/
+
+package com.syrus.AMFICOM.manager.UI;
+
+import org.jgraph.graph.GraphModel;
+
+import com.syrus.AMFICOM.manager.graph.ManagerGraphCell;
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/12/07 15:40:44 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module manager
+ */
+final class CellBuffer {
+
+	private final ManagerMainFrame	managerMainFrame;
+
+	private ManagerGraphCell managerGraphCell;
+
+	public CellBuffer(final ManagerMainFrame managerMainFrame) {
+		this.managerMainFrame = managerMainFrame;
+	}
+	
+	public final void putCells(final ManagerGraphCell cell) {
+		this.clear();
+		this.managerGraphCell = cell;
+	}
+	
+	public final boolean isExists() {
+		return this.managerGraphCell != null;
+	}
+	
+	public final boolean isExists(final ManagerGraphCell cell) {
+		return this.managerGraphCell == cell;
+	}
+	
+	public final ManagerGraphCell getCell() {
+		return this.managerGraphCell;
+	}
+	
+	public final void clear() {
+		if (this.managerGraphCell != null) {
+			final GraphModel model = this.managerMainFrame.graph.getModel();
+			model.remove(new Object[] { this.managerGraphCell});				
+		}
+		this.managerGraphCell = null;
+	}
+}
+
