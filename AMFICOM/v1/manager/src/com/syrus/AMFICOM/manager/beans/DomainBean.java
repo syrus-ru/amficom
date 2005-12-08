c0ab9bdea1834da6add64a7a38260d722705409d
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-12-08 19:05:17 +0300

    FIX dispose

--- AMFICOM/v1/manager/src/com/syrus/AMFICOM/manager/beans/DomainBean.java
+++ AMFICOM/v1/manager/src/com/syrus/AMFICOM/manager/beans/DomainBean.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: DomainBean.java,v 1.2 2005/11/28 14:47:05 bob Exp $
+ * $Id: DomainBean.java,v 1.3 2005/12/08 16:05:17 bob Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -12,17 +12,29 @@ import static com.syrus.AMFICOM.manager.beans.DomainBeanWrapper.KEY_DESCRIPTION;
 import static com.syrus.AMFICOM.manager.beans.DomainBeanWrapper.KEY_NAME;
 
 import java.beans.PropertyChangeEvent;
+import java.util.Set;
 
 import com.syrus.AMFICOM.administration.Domain;
+import com.syrus.AMFICOM.administration.DomainMember;
+import com.syrus.AMFICOM.administration.PermissionAttributes;
 import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.CompoundCondition;
+import com.syrus.AMFICOM.general.Identifiable;
 import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.LinkedIdsCondition;
+import com.syrus.AMFICOM.general.LoginManager;
 import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectPool;
+import com.syrus.AMFICOM.general.TypicalCondition;
+import com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlCompoundConditionPackage.CompoundConditionSort;
+import com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlTypicalConditionPackage.OperationSort;
 import com.syrus.AMFICOM.manager.graph.MPort;
+import com.syrus.AMFICOM.resource.LayoutItem;
+import com.syrus.AMFICOM.resource.LayoutItemWrapper;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.2 $, $Date: 2005/11/28 14:47:05 $
+ * @version $Revision: 1.3 $, $Date: 2005/12/08 16:05:17 $
  * @author $Author: bob $
  * @author Vladimir Dolzhenko
  * @module manager
@@ -81,9 +93,71 @@ public final class DomainBean extends Bean {
 
 	@Override
 	public void dispose() throws ApplicationException {
-		Log.debugMessage("DomainBean.dispose | " + this.identifier, Log.DEBUGLEVEL10);
+		assert Log.debugMessage("DomainBean.dispose | " + this.identifier, Log.DEBUGLEVEL03);
+		
+		final CompoundCondition compoundCondition = 
+			new CompoundCondition(new TypicalCondition(
+				this.getId(), 
+				OperationSort.OPERATION_EQUALS,
+				ObjectEntities.LAYOUT_ITEM_CODE,
+				LayoutItemWrapper.COLUMN_LAYOUT_NAME),
+				CompoundConditionSort.AND,
+				new LinkedIdsCondition(
+					LoginManager.getUserId(),
+					ObjectEntities.LAYOUT_ITEM_CODE) {
+					@Override
+					public boolean isNeedMore(Set< ? extends Identifiable> storableObjects) {
+						return storableObjects.isEmpty();
+					}
+				});
+		
+		final Set<LayoutItem> layoutItems = 
+			StorableObjectPool.getStorableObjectsByCondition(compoundCondition, true);	
+		
+		for(final LayoutItem layoutItem : layoutItems) {
+			StorableObjectPool.delete(layoutItem.getCharacteristics(false));
+		}
+		
+		StorableObjectPool.delete(layoutItems);
+		
+		this.disposeDomainMember(ObjectEntities.KIS_CODE);
+		this.disposeDomainMember(ObjectEntities.SERVER_CODE);
+		this.disposeDomainMember(ObjectEntities.MCM_CODE);		
+		this.disposeUsers();
+		
 		StorableObjectPool.delete(this.identifier);		
+		
 		super.disposeLayoutItem();
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void disposeDomainMember(final short domainMemberCode) 
+	throws ApplicationException {
+		final Set<? extends DomainMember> kiss = 
+			StorableObjectPool.getStorableObjectsByCondition(
+				new LinkedIdsCondition(this.identifier, domainMemberCode), 
+				true);
+		for (final DomainMember member : kiss) {
+			if (member.getDomainId().equals(this.identifier)) {
+				StorableObjectPool.delete(member.getId());
+			}
+		}
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void disposeUsers() throws ApplicationException {
+		final Set<PermissionAttributes> permissionAttributes = 
+			StorableObjectPool.getStorableObjectsByCondition(
+				new LinkedIdsCondition(this.identifier, ObjectEntities.PERMATTR_CODE), 
+				true);
+		for (final PermissionAttributes attributes : permissionAttributes) {
+			if (attributes.getDomainId().equals(this.identifier)) {
+				StorableObjectPool.delete(attributes.getParentId());
+				StorableObjectPool.delete(attributes.getId());
+			}
+		}
+		
 	}
 	
 	@Override
