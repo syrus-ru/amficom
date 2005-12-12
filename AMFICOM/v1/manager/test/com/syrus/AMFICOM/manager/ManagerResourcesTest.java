c943df01093c6b330531e0ec33be0edcf3c59a0d
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2005-12-12 11:16:51 +0300

    ADD manager resources test case

---
+++ AMFICOM/v1/manager/test/com/syrus/AMFICOM/manager/ManagerResourcesTest.java
@@ -0,0 +1,83 @@
+/*-
+* $Id: ManagerResourcesTest.java,v 1.1 2005/12/12 08:16:51 bob Exp $
+*
+* Copyright ¿ 2005 Syrus Systems.
+* Dept. of Science & Technology.
+* Project: AMFICOM.
+*/
+
+package com.syrus.AMFICOM.manager;
+
+import javax.swing.UIManager;
+
+import junit.framework.TestCase;
+
+import com.syrus.AMFICOM.administration.PermissionAttributes.Module;
+import com.syrus.AMFICOM.extensions.ExtensionLauncher;
+import com.syrus.AMFICOM.resources.ResourceHandler;
+
+
+/**
+ * @version $Revision: 1.1 $, $Date: 2005/12/12 08:16:51 $
+ * @author $Author: bob $
+ * @author Vladimir Dolzhenko
+ * @module manager
+ */
+public class ManagerResourcesTest extends TestCase {
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(ManagerResourcesTest.class);
+	}
+
+	public void testLoadResources() {
+		final ExtensionLauncher extensionLauncher = 
+			ExtensionLauncher.getInstance();
+		extensionLauncher.addExtensions("xml/resources.xml");		
+		extensionLauncher.getExtensionHandler(ResourceHandler.class.getName());
+		
+		final String[] strings = new String[] {
+				"com.syrus.AMFICOM.manager.resources.rtu",
+				"com.syrus.AMFICOM.manager.resources.icons.rtu",
+				"com.syrus.AMFICOM.manager.resources.domain",
+				"com.syrus.AMFICOM.manager.resources.icons.domain",
+				"com.syrus.AMFICOM.manager.resources.mcm",
+				"com.syrus.AMFICOM.manager.resources.icons.mcm",				
+				"com.syrus.AMFICOM.manager.resources.user",
+				"com.syrus.AMFICOM.manager.resources.icons.user",
+				"com.syrus.AMFICOM.manager.resources.server",
+				"com.syrus.AMFICOM.manager.resources.icons.server",
+				"com.syrus.AMFICOM.manager.resources.workstation",
+				"com.syrus.AMFICOM.manager.resources.icons.workstation",
+				"com.syrus.AMFICOM.manager.resources.network",
+				"com.syrus.AMFICOM.manager.resources.icons.network",
+				"com.syrus.AMFICOM.manager.resources.warning",
+				"com.syrus.AMFICOM.manager.resources.icons.warning",
+				"com.syrus.AMFICOM.manager.resources.error",
+				"com.syrus.AMFICOM.manager.resources.icons.error",
+				"com.syrus.AMFICOM.manager.resources.action.enter",
+				"com.syrus.AMFICOM.manager.resources.action.connecton",
+				"com.syrus.AMFICOM.manager.resources.action.connectoff",
+				"com.syrus.AMFICOM.manager.resources.action.undo",
+				"com.syrus.AMFICOM.manager.resources.action.redo",
+				"org.jgraph.example.resources.action.paste",
+				"org.jgraph.example.resources.action.cut",
+				"org.jgraph.example.resources.action.delete",
+				"org.jgraph.example.resources.action.zoom",
+				"org.jgraph.example.resources.action.zoomin",
+				"org.jgraph.example.resources.action.zoomout"
+				
+		};
+		
+		for (final String string : strings) {
+			assertNotNull(UIManager.get(string));
+		}
+
+		for (final Module module : Module.getValueList()) {
+			if (module.isEnable()) {
+				assertNotNull(UIManager.get("com.syrus.AMFICOM.manager.resources." + module.getCodename()));
+				assertNotNull(UIManager.get("com.syrus.AMFICOM.manager.resources.icons." + module.getCodename()));				
+			}
+		}
+	}
+}
+
