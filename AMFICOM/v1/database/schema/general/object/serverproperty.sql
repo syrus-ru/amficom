4611c82c0d154cf54c931d2f0bcc7e7b976353b8
cvs-fast-export <cvs-fast-export>
2005-12-14 13:10:12 +0000

    Synthetic commit for incomplete tag GENERAL-0_0_2

--- AMFICOM/v1/database/schema/general/object/serverproperty.sql
+++
@@ -1,9 +0,0 @@
--- $Id: serverproperty.sql,v 1.3 2005/06/15 09:40:34 bass Exp $
-
-CREATE TABLE ServerProperty (
- key VARCHAR2(32 CHAR),
- value VARCHAR2(32 CHAR),
---
- CONSTRAINT sp_pk PRIMARY KEY (key)
-);
-
--- AMFICOM/v1/database/schema/measurement/dictionary/CREATE.sql
+++
@@ -1,9 +0,0 @@
--- $Id: CREATE.sql,v 1.1 2005/08/28 14:29:24 arseniy Exp $
-
-@@measurementtype;
-@@analysistype;
-@@modelingtype;
-@@mnttyppartyplink;
-@@anatyppartyplink;
-@@modtyppartyplink;
-@@mnttypanatyplink;
