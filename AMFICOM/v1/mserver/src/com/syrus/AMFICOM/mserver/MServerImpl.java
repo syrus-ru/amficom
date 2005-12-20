3569618b6921df8c9fcde07440f4d31a71ce9e35
Arseniy Tashoyan <tashoyan@gmail.com>
2005-12-20 12:21:25 +0300

    MINOR: Regenerated serialVersionUID

--- AMFICOM/v1/mserver/src/com/syrus/AMFICOM/mserver/MServerImpl.java
+++ AMFICOM/v1/mserver/src/com/syrus/AMFICOM/mserver/MServerImpl.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: MServerImpl.java,v 1.4 2005/11/28 12:35:30 arseniy Exp $
+ * $Id: MServerImpl.java,v 1.5 2005/12/20 09:21:25 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -12,14 +12,13 @@ import com.syrus.AMFICOM.general.IdentifierGeneratorServerCore;
 import com.syrus.AMFICOM.mserver.corba.MServerOperations;
 
 /**
- * @version $Revision: 1.4 $, $Date: 2005/11/28 12:35:30 $
+ * @version $Revision: 1.5 $, $Date: 2005/12/20 09:21:25 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module mserver
  */
 final class MServerImpl extends IdentifierGeneratorServerCore implements MServerOperations {
-
-	private static final long serialVersionUID = 395371850379497709L;
+	private static final long serialVersionUID = -2000155795996275898L;
 
 	MServerImpl(final MServerServantManager mServerServantManager) {
 		super(mServerServantManager, mServerServantManager.getCORBAServer().getOrb());
