a84ef17d9a1293377693c81e1d64fc83218431dd
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2006-02-06 19:24:18 +0300

    MINOR: patch from Stas.

--- AMFICOM/v1/filterclient/src/com/syrus/AMFICOM/filterclient/MeasurementConditionWrapper.java
+++ AMFICOM/v1/filterclient/src/com/syrus/AMFICOM/filterclient/MeasurementConditionWrapper.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MeasurementConditionWrapper.java,v 1.9 2005/10/18 07:36:47 max Exp $
+ * $Id: MeasurementConditionWrapper.java,v 1.10 2006/02/06 16:24:18 bass Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,9 +8,9 @@
 package com.syrus.AMFICOM.filterclient;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import com.syrus.AMFICOM.client.resource.I18N;
 import com.syrus.AMFICOM.general.ConditionWrapper;
 import com.syrus.AMFICOM.general.ObjectEntities;
 import com.syrus.AMFICOM.general.StorableObjectWrapper;
@@ -19,8 +19,8 @@ import com.syrus.AMFICOM.newFilter.ConditionKey;
 import com.syrus.AMFICOM.newFilter.LangModelFilter;
 
 /**
- * @version $Revision: 1.9 $, $Date: 2005/10/18 07:36:47 $
- * @author $Author: max $
+ * @version $Revision: 1.10 $, $Date: 2006/02/06 16:24:18 $
+ * @author $Author: bass $
  * @module filterclient
  */
 public class MeasurementConditionWrapper implements ConditionWrapper {
@@ -30,15 +30,18 @@ public class MeasurementConditionWrapper implements ConditionWrapper {
 	private final static String NAME = LangModelFilter.getString("filter.criteria.entityname");
 	private final static String START_TIME = LangModelFilter.getString("filter.criteria.starttime");
 
-	private static List<ConditionKey> keys = new ArrayList<ConditionKey>();
+	public final static ConditionKey START_TIME_CONDITION_KEY = new ConditionKey(MeasurementWrapper.COLUMN_START_TIME, START_TIME, ConditionWrapper.DATE);
+	public final static ConditionKey NAME_CONDITION_KEY = new ConditionKey(StorableObjectWrapper.COLUMN_DESCRIPTION, NAME, ConditionWrapper.STRING);
+	
+	private final static List<ConditionKey> keys = new ArrayList<ConditionKey>();
 
 	static {
-		keys.add(new ConditionKey(StorableObjectWrapper.COLUMN_NAME, NAME, ConditionWrapper.STRING));
-		keys.add(new ConditionKey(MeasurementWrapper.COLUMN_START_TIME, START_TIME, ConditionWrapper.DATE));
+		keys.add(NAME_CONDITION_KEY);
+		keys.add(START_TIME_CONDITION_KEY);
 	}
 
 	public List<ConditionKey> getKeys() {
-		return keys;
+		return Collections.unmodifiableList(keys);
 	}
 
 	public short getEntityCode() {
