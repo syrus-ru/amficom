3a321514b5870daa7fedec507bd04f2452b8c1ac
Stanislav Kholshin <kholshin@gmail.com>
2006-02-08 18:15:43 +0300

    add: possibility to add topological node by right click on tunnel

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/popup/NodeLinkPopupMenu.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/popup/NodeLinkPopupMenu.java
@@ -1,5 +1,5 @@
 /*-
- * $$Id: NodeLinkPopupMenu.java,v 1.18 2005/10/31 15:29:31 krupenn Exp $$
+ * $$Id: NodeLinkPopupMenu.java,v 1.19 2006/02/08 15:15:43 stas Exp $$
  *
  * Copyright 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -16,20 +16,25 @@ import javax.swing.JMenuItem;
 import com.syrus.AMFICOM.administration.PermissionAttributes.PermissionCodename;
 import com.syrus.AMFICOM.client.event.MapEvent;
 import com.syrus.AMFICOM.client.event.StatusMessageEvent;
+import com.syrus.AMFICOM.client.map.LogicalNetLayer;
 import com.syrus.AMFICOM.client.map.MapPropertiesManager;
+import com.syrus.AMFICOM.client.map.command.action.CreatePhysicalNodeCommandBundle;
 import com.syrus.AMFICOM.client.model.ApplicationContext;
+import com.syrus.AMFICOM.client.model.Command;
 import com.syrus.AMFICOM.client.model.MapApplicationModel;
 import com.syrus.AMFICOM.client.resource.I18N;
 import com.syrus.AMFICOM.client.resource.MapEditorResourceKeys;
 import com.syrus.AMFICOM.map.NodeLink;
+import com.syrus.AMFICOM.map.corba.IdlPhysicalLinkTypePackage.PhysicalLinkTypeSort;
 
 /**
- * @version $Revision: 1.18 $, $Date: 2005/10/31 15:29:31 $
- * @author $Author: krupenn $
+ * @version $Revision: 1.19 $, $Date: 2006/02/08 15:15:43 $
+ * @author $Author: stas $
  * @author Andrei Kroupennikov
  * @module mapviewclient
  */
 public final class NodeLinkPopupMenu extends MapPopupMenu {
+	private JMenuItem addPointItem = new JMenuItem();
 	private JMenuItem removeMenuItem = new JMenuItem();
 
 	private NodeLink link;
@@ -55,6 +60,14 @@ public final class NodeLinkPopupMenu extends MapPopupMenu {
 	}
 
 	private void jbInit() {
+		this.addPointItem.setText(I18N.getString(MapEditorResourceKeys.POPUP_ADD_SITE));
+		this.addPointItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addPoint();
+			}
+		});
+		this.add(this.addPointItem);
+		
 		this.removeMenuItem.setText(I18N.getString(MapEditorResourceKeys.POPUP_DELETE));
 		this.removeMenuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -62,6 +75,20 @@ public final class NodeLinkPopupMenu extends MapPopupMenu {
 			}
 		});
 		this.add(this.removeMenuItem);
+	}
+	
+	void addPoint() {
+		if(this.link.getPhysicalLink().getType().getSort().value() != PhysicalLinkTypeSort._INDOOR) {
+			CreatePhysicalNodeCommandBundle command = new CreatePhysicalNodeCommandBundle(this.link, super.point);
+			command.setNetMapViewer(super.netMapViewer);
+			LogicalNetLayer logicalNetLayer = this.netMapViewer.getLogicalNetLayer();
+			logicalNetLayer.getCommandList().add(command);
+			logicalNetLayer.getCommandList().execute();
+			if(!command.isUndoable()) {
+				logicalNetLayer.getCommandList().flush();
+			}
+			logicalNetLayer.sendMapEvent(MapEvent.MAP_CHANGED);
+		}
 	}
 
 	void removeNodeLink() {
