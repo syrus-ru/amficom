7141935adfa749de36ee68ae608abdd2d3a48559
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2006-02-08 10:10:31 +0300

    FIX

--- AMFICOM/v1/scheduler/src/com/syrus/AMFICOM/Client/Schedule/UI/TestParametersPanel.java
+++ AMFICOM/v1/scheduler/src/com/syrus/AMFICOM/Client/Schedule/UI/TestParametersPanel.java
@@ -10,6 +10,7 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
@@ -255,6 +256,10 @@ final class TestParametersPanel implements PropertyChangeListener {
 				if (checkBox.isSelected()) {
 					refreshMeasurementSetups();
 				} else {
+					final WrapperedListModel<MeasurementSetup> wrapperedListModel = 
+						testSetups.getModel();
+					final Set<MeasurementSetup> emptySet = Collections.emptySet();
+					wrapperedListModel.setElements(emptySet);
 					try {
 						refreshMeasurementSetup();
 					} catch (ApplicationException e1) {
