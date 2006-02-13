3e6236b70b9fda1f69be99890d3d9cd8ebe76cc0
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-13 15:10:07 +0300

    ADD: valuetype IdlActionType

---
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/corba/IdlActionTypeImpl.java
@@ -0,0 +1,26 @@
+/*-
+ * $Id: IdlActionTypeImpl.java,v 1.1.2.1 2006/02/13 12:09:47 arseniy Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.AMFICOM.measurement.corba;
+
+import com.syrus.AMFICOM.general.StorableObject;
+import com.syrus.AMFICOM.general.corba.IdlCreateObjectException;
+
+/**
+ * @version $Revision: 1.1.2.1 $, $Date: 2006/02/13 12:09:47 $
+ * @author $Author: arseniy $
+ * @author Tashoyan Arseniy Feliksovich
+ * @module measurement
+ */
+final class IdlActionTypeImpl extends IdlActionType {
+
+	@Override
+	public StorableObject getNative() throws IdlCreateObjectException {
+		throw new UnsupportedOperationException("This class can not have native pair");
+	}
+
+}
