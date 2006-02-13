2f6ced886af8eee5cd3129ee39174b2ef1da8ee9
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-13 15:11:57 +0300

    ADD: Default constructor

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/corba/IdlActionImpl.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/corba/IdlActionImpl.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: IdlActionImpl.java,v 1.1.2.1 2006/02/11 18:40:46 arseniy Exp $
+ * $Id: IdlActionImpl.java,v 1.1.2.2 2006/02/13 12:11:57 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -11,12 +11,16 @@ import com.syrus.AMFICOM.general.StorableObject;
 import com.syrus.AMFICOM.general.corba.IdlCreateObjectException;
 
 /**
- * @version $Revision: 1.1.2.1 $, $Date: 2006/02/11 18:40:46 $
+ * @version $Revision: 1.1.2.2 $, $Date: 2006/02/13 12:11:57 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module measurement
  */
 final class IdlActionImpl extends IdlAction {
+
+	IdlActionImpl() {
+		// empty
+	}
 
 	@Override
 	public StorableObject getNative() throws IdlCreateObjectException {
