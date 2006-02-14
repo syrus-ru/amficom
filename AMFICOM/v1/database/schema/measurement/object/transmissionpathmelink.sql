dd5a34ab26a518fea32595705cd52cad08541255
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 12:55:28 +0300

    COSMETIC: Constraint names

--- AMFICOM/v1/database/schema/measurement/object/transmissionpathmelink.sql
+++ AMFICOM/v1/database/schema/measurement/object/transmissionpathmelink.sql
@@ -1,13 +1,13 @@
--- $Id: transmissionpathmelink.sql,v 1.1 2005/08/28 14:29:24 arseniy Exp $
+-- $Id: transmissionpathmelink.sql,v 1.1.2.1 2006/02/14 09:55:28 arseniy Exp $
 
 CREATE TABLE TransmissionPathMELink (
  transmission_path_id,
  monitored_element_id,
 --
- CONSTRAINT tpathmelink_tpath_fk FOREIGN KEY (transmission_path_id)
+ CONSTRAINT tpml_tpath_fk FOREIGN KEY (transmission_path_id)
   REFERENCES TransmissionPath (id) ON DELETE CASCADE,
- CONSTRAINT tpathmelink_me_fk FOREIGN KEY (monitored_element_id)
+ CONSTRAINT tpml_me_fk FOREIGN KEY (monitored_element_id)
   REFERENCES MonitoredElement (id) ON DELETE CASCADE,
- CONSTRAINT tpathmelink_uniq UNIQUE (transmission_path_id, monitored_element_id)
+ CONSTRAINT tpml_uniq UNIQUE (transmission_path_id, monitored_element_id)
 );
 
