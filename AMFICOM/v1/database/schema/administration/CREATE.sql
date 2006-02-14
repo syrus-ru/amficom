b16eefb63fc9ceb007360be6ce477cac5706efe3
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 12:45:55 +0300

    REMOVE: Directory type as unused

---
+++ AMFICOM/v1/database/schema/administration/CREATE.sql
@@ -0,0 +1,4 @@
+-- $Id: CREATE.sql,v 1.4.2.1 2006/02/14 09:45:55 arseniy Exp $
+
+REM objects
+@@administration/object/CREATE;
---
+++ AMFICOM/v1/database/schema/administration/DROP.sql
@@ -0,0 +1,3 @@
+-- $Id: DROP.sql,v 1.3.2.1 2006/02/14 09:45:55 arseniy Exp $
+
+@@administration/object/DROP;
