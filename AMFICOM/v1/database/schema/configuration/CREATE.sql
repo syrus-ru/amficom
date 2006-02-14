10d79aaa06db2f55b72a89aca3ef546ad633d81b
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 12:49:28 +0300

    REMOVE: Directory dictionary as unused

---
+++ AMFICOM/v1/database/schema/configuration/CREATE.sql
@@ -0,0 +1,7 @@
+-- $Id: CREATE.sql,v 1.5.2.1 2006/02/14 09:49:28 arseniy Exp $
+
+REM types
+@@configuration/type/CREATE;
+
+REM objects
+@@configuration/object/CREATE;
---
+++ AMFICOM/v1/database/schema/configuration/DROP.sql
@@ -0,0 +1,4 @@
+-- $Id: DROP.sql,v 1.4.2.1 2006/02/14 09:49:28 arseniy Exp $
+
+@@configuration/object/DROP;
+@@configuration/type/DROP;
