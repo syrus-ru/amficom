dc9c03a487a50e9fb595dc1435a221920277e8ed
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 14:10:04 +0300

    MAJOR: Constraint to ParameterType

--- AMFICOM/v1/database/schema/event/type/eventtyppartyplink.sql
+++ AMFICOM/v1/database/schema/event/type/eventtyppartyplink.sql
@@ -1,13 +1,13 @@
--- $Id: eventtyppartyplink.sql,v 1.5 2005/08/21 15:11:08 arseniy Exp $
+-- $Id: eventtyppartyplink.sql,v 1.5.2.1 2006/02/14 11:09:11 arseniy Exp $
 
 CREATE TABLE EventTypParTypLink (
  event_type_id NOT NULL,
- parameter_type_code NOT NULL,
+ parameter_type_id NOT NULL,
 --
- CONSTRAINT evtpartlnk_uniq
-  UNIQUE (event_type_id, parameter_type_code),
- CONSTRAINT evtpartlnk_mntt_fk FOREIGN KEY (event_type_id)
+ CONSTRAINT etptl_uniq
+  UNIQUE (event_type_id, parameter_type_id),
+ CONSTRAINT etptl_et_fk FOREIGN KEY (event_type_id)
   REFERENCES EventType (id) ON DELETE CASCADE,
- CONSTRAINT evtpartlnk_part_fk FOREIGN KEY (parameter_type_code)
-  REFERENCES ParameterType (code) ON DELETE CASCADE
+ CONSTRAINT etptl_pt_fk FOREIGN KEY (parameter_type_id)
+  REFERENCES ParameterType (id) ON DELETE CASCADE
 );
