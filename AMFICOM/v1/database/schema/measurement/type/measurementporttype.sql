fdccc908fc12fca371e3cccbc94ae8d6fccaa247
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 09:53:22 +0300

    MINOR: Constraint names

--- AMFICOM/v1/database/schema/measurement/type/measurementporttype.sql
+++ AMFICOM/v1/database/schema/measurement/type/measurementporttype.sql
@@ -1,4 +1,4 @@
--- $Id: measurementporttype.sql,v 1.3 2005/09/28 10:34:02 arseniy Exp $
+-- $Id: measurementporttype.sql,v 1.4 2006/02/14 06:53:22 arseniy Exp $
 
 CREATE TABLE MeasurementPortType (
  id NUMBER(19),
@@ -13,13 +13,12 @@ CREATE TABLE MeasurementPortType (
 --
  name VARCHAR2(128 CHAR),
 --
- CONSTRAINT mptyp_pk PRIMARY KEY (id),
- CONSTRAINT mptyp_uniq UNIQUE (codename),
- CONSTRAINT mptyp_creator_fk FOREIGN KEY (creator_id)
+ CONSTRAINT mpt_pk PRIMARY KEY (id),
+ CONSTRAINT mpt_uniq UNIQUE (codename),
+ CONSTRAINT mpt_creator_fk FOREIGN KEY (creator_id)
   REFERENCES SystemUser (id) ON DELETE CASCADE,
- CONSTRAINT mptyp_modifier_fk FOREIGN KEY (modifier_id)
+ CONSTRAINT mpt_modifier_fk FOREIGN KEY (modifier_id)
   REFERENCES SystemUser (id) ON DELETE CASCADE
 );
 
 CREATE SEQUENCE MeasurementPortType_seq ORDER;
-
