5d36d13e87efa4ef25407539315936f68fca1bb9
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 12:41:24 +0300

    MAJOR: Renamed directory to systemuser

---
+++ AMFICOM/v1/database/schema/systemuser/CREATE.sql
@@ -0,0 +1,4 @@
+-- $Id: CREATE.sql,v 1.1.2.1 2006/02/14 09:41:24 arseniy Exp $
+
+PROMPT 01. Creating table SystemUser...;
+@@systemuser;
---
+++ AMFICOM/v1/database/schema/systemuser/DROP.sql
@@ -0,0 +1,6 @@
+-- $Id: DROP.sql,v 1.1.2.1 2006/02/14 09:41:24 arseniy Exp $
+
+PROMPT 01. Dropping table SystemUser...;
+DROP TABLE SystemUser;
+DROP SEQUENCE SystemUser_seq;
+
