07e97c88f4a16655dfeb367a88e0329d9f10f8ab
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 03:24:31 +0300

    MAJOR: New table added, old removed

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/TableNames.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/TableNames.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: TableNames.java,v 1.10.2.2 2006/02/13 19:26:02 arseniy Exp $
+ * $Id: TableNames.java,v 1.10.2.3 2006/02/14 00:24:31 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,7 +9,7 @@
 package com.syrus.AMFICOM.general;
 
 /**
- * @version $Revision: 1.10.2.2 $, $Date: 2006/02/13 19:26:02 $
+ * @version $Revision: 1.10.2.3 $, $Date: 2006/02/14 00:24:31 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module general
@@ -17,30 +17,19 @@ package com.syrus.AMFICOM.general;
 public final class TableNames {
 	public static final String DATA_TYPE = "DataType";
 	public static final String MEASUREMENT_UNIT = "MeasurementUnit";
-	public static final String PARAMETER_TYPE = "ParameterType";
 
 	public static final String SYSTEM_USER_ROLE_LINK = "SystemUserRoleLink";
-	
-	public static final String EQUIPMENT_TYPE = "EquipmentType";
-
-	public static final String MEASUREMENT_TYPE = "MeasurementType";
-	public static final String ANALYSIS_TYPE = "AnalysisType";
-	public static final String MODELING_TYPE = "ModelingType";
-
-	public static final String MNT_TYP_PAR_TYP_LINK = "MntTypParTypLink";
-	public static final String ANA_TYP_PAR_TYP_LINK = "AnaTypParTypLink";
-	public static final String MOD_TYP_PAR_TYP_LINK = "ModTypParTypLink";
 
 	public static final String ACT_PAR_TMPL_LINK = "ActParTmplLink";
 	public static final String ME_TMPL_LINK = "METmplLink";
-//---------------
+
+	public static final String TEST_MEASTMPL_LINK = "TestMeasTmplLink";
+	public static final String TEST_ANATMPL_LINK = "TestAnaTmplLink";
+	public static final String TEST_STOP_LINK = "TestStopLink";
+
+	//---------------
 	public static final String MNT_TYP_ANA_TYP_LINK = "MntTypAnaTypLink";
 	public static final String MNTPORTTYPMNTTYPLINK = "MntPortTypMntTypLink";
-	public static final String PARAMETERSETMELINK = "ParameterSetMELink";
-	public static final String MEASUREMENTSETUP_ME_LINK = "MeasurementSetupMELink";
-	public static final String MEASUREMENTSETUP_MT_LINK = "MeasurementSetupMTLink";
-	public static final String TEST_STOP_LINK = "TestStopLink";
-	public static final String MEASUREMENTSETUP_TEST_LINK = "MeasurementSetupTestLink";
 	public static final String EQUIPMENT_ME_LINK = "EquipmentMELink";
 	public static final String TRANSMISSIONPATH_ME_LINK = "TransmissionPathMELink";
 
