0462cf611b825ffe9356141c3878d1e563e24c3d
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 12:54:33 +0300

    MAJOR: Changed list of tables

--- AMFICOM/v1/database/schema/measurement/type/CREATE.sql
+++ AMFICOM/v1/database/schema/measurement/type/CREATE.sql
@@ -1,4 +1,19 @@
--- $Id: CREATE.sql,v 1.7 2005/08/28 14:29:24 arseniy Exp $
+-- $Id: CREATE.sql,v 1.7.2.1 2006/02/14 09:54:33 arseniy Exp $
 
+PROMPT 01. Creating table MeasurementPortType...;
 @@measurementporttype;
-@@mntporttypmnttyplink;
+
+PROMPT 02. Creating table MeasurementType...;
+@@measurementtype;
+
+PROMPT 03. Creating table AnalysisType...;
+@@analysistype;
+
+PROMPT 04. Creating table ModelingType...;
+@@modelingtype;
+
+PROMPT 05. Creating table ActionTypeKind...;
+@@actiontypekind;
+
+PROMPT 06. Creating table ActionParameterTypeBinding...;
+@@actionparametertypebinding;
--- AMFICOM/v1/database/schema/measurement/type/DROP.sql
+++ AMFICOM/v1/database/schema/measurement/type/DROP.sql
@@ -1,6 +1,24 @@
--- $Id: DROP.sql,v 1.7 2005/08/28 14:29:24 arseniy Exp $
+-- $Id: DROP.sql,v 1.7.2.1 2006/02/14 09:54:33 arseniy Exp $
 
-DROP TABLE MntPortTypMntTypLink;
+PROMPT 06. Dropping table ActionParameterTypeBinding...;
+DROP TABLE ActionParameterTypeBinding;
+DROP SEQUENCE ActionParameterTypeBinding_seq;
+
+PROMPT 05. Dropping table ActionTypeKind...;
+DROP TABLE ActionTypeKind;
+
+PROMPT 04. Dropping table ModelingType...;
+DROP TABLE ModelingType;
+DROP SEQUENCE ModelingType_seq;
+
+PROMPT 03. Dropping table AnalysisType...;
+DROP TABLE AnalysisType;
+DROP SEQUENCE AnalysisType_seq;
+
+PROMPT 02. Dropping table MeasurementType...;
+DROP TABLE MeasurementType;
+DROP SEQUENCE MeasurementType_seq;
+
+PROMPT 01. Dropping table MeasurementPortType...;
 DROP TABLE MeasurementPortType;
-
 DROP SEQUENCE MeasurementPortType_seq;
