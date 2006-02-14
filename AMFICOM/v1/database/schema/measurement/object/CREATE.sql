295563aa9b91f00c7fcc604d2f6b12d8799636b4
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 13:02:20 +0300

    MAJOR: Changed list of tables

--- AMFICOM/v1/database/schema/measurement/object/CREATE.sql
+++ AMFICOM/v1/database/schema/measurement/object/CREATE.sql
@@ -1,23 +1,67 @@
--- $Id: CREATE.sql,v 1.11 2005/08/28 14:29:24 arseniy Exp $
+-- $Id: CREATE.sql,v 1.11.2.1 2006/02/14 10:02:20 arseniy Exp $
 
+PROMPT 01. Creating table KIS...;
 @@kis;
+
+PROMPT 02. Creating table MeasurementPort...;
 @@measurementport;
+
+PROMPT 03. Creating table MonitoredElement...;
 @@monitoredelement;
-@@equipmentmelink;
+
+PROMPT 04. Creating table TransmissionPathMELink...;
 @@transmissionpathmelink;
-@@parameterset;
-@@parametersetmelink;
-@@parameter;
-@@measurementsetup;
-@@measurementsetupmelink;
-@@measurementsetupmtlink;
-@@crontemporalpattern;
-@@periodicaltemporalpattern;
+
+
+
+PROMPT 05. Creating table ActionParameter...;
+@@actionparameter
+
+PROMPT 06. Creating table ActionTemplate...;
+@@actiontemplate;
+
+PROMPT 07. Creating table ActParTmplLink...;
+@@actpartmpllink;
+
+PROMPT 08. Creating table METmplLink...;
+@@metmpllink;
+
+
+
+PROMPT 09. Creating table TemporalPattern...;
+@@temporalpattern;
+
+
+
+PROMPT 10. Creating table Test...;
 @@test;
-@@measurementsetuptestlink;
+
+PROMPT 11. Creating table TestMeasTmplLink...;
+@@testmeastmpllink;
+
+PROMPT 12. Creating table TestAnaTmplLink...;
+@@testanatmpllink;
+
+PROMPT 13. Creating table TestStopLink...;
+@@teststoplink;
+
+
+
+PROMPT 14. Creating table Measurement...;
 @@measurement;
+
+PROMPT 15. Creating table Analysis...;
 @@analysis;
+
+PROMPT 16. Creating table Modeling...;
 @@modeling;
-@@result;
-@@resultparameter;
+
+PROMPT 17. Creating table MeasurementResultParameter...;
+@@measurementresultparameter
+
+PROMPT 18. Creating table AnalysisResultParameter...;
+@@analysisresultparameter;
+
+PROMPT 19. Creating table ModelingResultParameter...;
+@@modelingresultparameter;
 
--- AMFICOM/v1/database/schema/measurement/object/DROP.sql
+++ AMFICOM/v1/database/schema/measurement/object/DROP.sql
@@ -1,39 +1,79 @@
--- $Id: DROP.sql,v 1.13 2005/09/28 10:34:02 arseniy Exp $
+-- $Id: DROP.sql,v 1.13.2.1 2006/02/14 10:02:20 arseniy Exp $
 
-DROP TABLE ResultParameter;
-DROP TABLE Result;
+PROMPT 19. Dropping table ModelingResultParameter...;
+DROP TABLE ModelingResultParameter;
+DROP SEQUENCE ModelingResultParameter_seq;
+
+PROMPT 18. Dropping table AnalysisResultParameter...;
+DROP TABLE AnalysisResultParameter;
+DROP SEQUENCE AnalysisResultParameter_seq;
+
+PROMPT 17. Dropping table MeasurementResultParameter...;
+DROP TABLE MeasurementResultParameter;
+DROP SEQUENCE MeasurementResultParameter_seq;
+
+PROMPT 16. Dropping table Modeling...;
 DROP TABLE Modeling;
-DROP TABLE Analysis;
-DROP TABLE Measurement;
-DROP TABLE MeasurementSetupTestLink;
-DROP TABLE TestStopLink;
-DROP TABLE Test;
-DROP TABLE PeriodicalTemporalPattern;
-DROP TABLE CronTemporalPattern;
-DROP TYPE CronStringArray;
-DROP TABLE MeasurementSetupMTLink;
-DROP TABLE MeasurementSetupMELink;
-DROP TABLE MeasurementSetup;
-DROP TABLE Parameter;
-DROP TABLE ParameterSetMELink;
-DROP TABLE ParameterSet;
-DROP TABLE TransmissionPathMELink;
-DROP TABLE EquipmentMELink;
-DROP TABLE MonitoredElement;
-DROP TABLE MeasurementPort;
-DROP TABLE KIS;
-
-DROP SEQUENCE ResultParameter_seq;
-DROP SEQUENCE Result_seq;
-DROP SEQUENCE Test_seq;
-DROP SEQUENCE PeriodicalTemporalPattern_Seq;
-DROP SEQUENCE CronTemporalPattern_Seq;
 DROP SEQUENCE Modeling_seq;
+
+PROMPT 15. Dropping table Analysis...;
+DROP TABLE Analysis;
 DROP SEQUENCE Analysis_seq;
+
+PROMPT 14. Dropping table Measurement...;
+DROP TABLE Measurement;
 DROP SEQUENCE Measurement_seq;
-DROP SEQUENCE MeasurementSetup_seq;
-DROP SEQUENCE Parameter_seq;
-DROP SEQUENCE ParameterSet_seq;
+
+
+
+PROMPT 13. Dropping table TestStopLink...;
+DROP TABLE TestStopLink;
+
+PROMPT 12. Dropping table TestAnaTmplLink...;
+DROP TABLE TestAnaTmplLink;
+
+PROMPT 11. Dropping table TestMeasTmplLink...;
+DROP TABLE TestMeasTmplLink;
+
+PROMPT 10. Dropping table Test...;
+DROP TABLE Test;
+DROP SEQUENCE Test_seq;
+
+
+
+PROMPT 09. Dropping table TemporalPattern...;
+DROP TABLE TemporalPattern;
+DROP SEQUENCE TemporalPattern_seq;
+
+
+
+PROMPT 08. Dropping table METmplLink...;
+DROP TABLE METmplLink;
+
+PROMPT 07. Dropping table ActParTmplLink...;
+DROP TABLE ActParTmplLink;
+
+PROMPT 06. Dropping table ActionTemplate...;
+DROP TABLE ActionTemplate;
+DROP SEQUENCE ActionTemplate_seq;
+
+PROMPT 05. Dropping table ActionParameter...;
+DROP TABLE ActionParameter;
+DROP SEQUENCE ActionParameter_seq;
+
+
+
+PROMPT 04. Dropping table TransmissionPathMELink...;
+DROP TABLE TransmissionPathMELink;
+
+PROMPT 03. Dropping table MonitoredElement...;
+DROP TABLE MonitoredElement;
 DROP SEQUENCE MonitoredElement_seq;
+
+PROMPT 02. Dropping table MeasurementPort...;
+DROP TABLE MeasurementPort;
 DROP SEQUENCE MeasurementPort_seq;
+
+PROMPT 01. Dropping table KIS...;
+DROP TABLE KIS;
 DROP SEQUENCE KIS_seq;
