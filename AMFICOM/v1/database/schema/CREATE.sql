4850d58e1d97ee085ae82c5c738cda07b428dc0b
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-14 13:03:23 +0300

    MINOR: Renamed directory user to systemuser

---
+++ AMFICOM/v1/database/schema/CREATE.sql
@@ -0,0 +1,37 @@
+-- $Id: CREATE.sql,v 1.12.2.1 2006/02/14 10:03:23 arseniy Exp $
+
+REM systemuser
+@@./systemuser/CREATE;
+
+REM general
+@@./general/CREATE;
+
+REM administration
+@@./administration/CREATE;
+
+REM resource
+@@./resource/CREATE;
+
+REM configuration
+@@./configuration/CREATE;
+
+REM measurement
+@@./measurement/CREATE;
+
+REM event
+@@./event/CREATE;
+
+REM map
+@@./map/CREATE;
+
+REM scheme
+@@./scheme/CREATE;
+
+REM mapview
+@@./mapview/CREATE;
+
+REM security
+@@./security/CREATE;
+
+REM report
+@@./report/CREATE;
---
+++ AMFICOM/v1/database/schema/DROP.sql
@@ -0,0 +1,14 @@
+-- $Id: DROP.sql,v 1.11.2.1 2006/02/14 10:03:23 arseniy Exp $
+
+@@./report/DROP;
+@@./security/DROP;
+@@./mapview/DROP;
+@@./scheme/DROP;
+@@./map/DROP;
+@@./event/DROP;
+@@./measurement/DROP;
+@@./configuration/DROP;
+@@./resource/DROP;
+@@./administration/DROP;
+@@./general/DROP;
+@@./systemuser/DROP;
