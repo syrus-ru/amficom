9973711a25c6215c61c94c761f261eed3ab21a7a
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-17 14:33:30 +0300

    MAJOR: Complete rebuild using LoginPerformer

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/LoginManager.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/LoginManager.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: LoginManager.java,v 1.41 2006/02/13 19:28:41 arseniy Exp $
+ * $Id: LoginManager.java,v 1.42 2006/02/17 11:33:30 arseniy Exp $
  *
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,157 +8,64 @@
 
 package com.syrus.AMFICOM.general;
 
-import static com.syrus.AMFICOM.general.ErrorMessages.NON_NULL_EXPECTED;
-import static com.syrus.AMFICOM.general.Identifier.VOID_IDENTIFIER;
-import static com.syrus.AMFICOM.general.ObjectEntities.DOMAIN_CODE;
-import static com.syrus.AMFICOM.general.ObjectEntities.SYSTEMUSER_CODE;
-
-import java.util.Date;
 import java.util.Set;
 
-import org.omg.CORBA.SystemException;
-
 import com.syrus.AMFICOM.administration.Domain;
-import com.syrus.AMFICOM.administration.corba.IdlDomain;
 import com.syrus.AMFICOM.bugs.Crutch366;
-import com.syrus.AMFICOM.general.corba.AMFICOMRemoteException;
-import com.syrus.AMFICOM.general.corba.CommonUser;
-import com.syrus.AMFICOM.general.corba.IdlIdentifier;
-import com.syrus.AMFICOM.general.corba.IdlIdentifierHolder;
-import com.syrus.AMFICOM.general.corba.AMFICOMRemoteExceptionPackage.IdlErrorCode;
-import com.syrus.AMFICOM.leserver.corba.LoginServer;
 import com.syrus.AMFICOM.security.SessionKey;
-import com.syrus.AMFICOM.security.corba.IdlSessionKey;
-import com.syrus.AMFICOM.security.corba.IdlSessionKeyHolder;
 
 /**
- * @version $Revision: 1.41 $, $Date: 2006/02/13 19:28:41 $
+ * @version $Revision: 1.42 $, $Date: 2006/02/17 11:33:30 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module csbridge
  */
 public final class LoginManager {
-	/*-********************************************************************
-	 * Constants.                                                         *
-	 **********************************************************************/
+	/**
+	 * Основные действия по установлению сессии.
+	 * Поведение реализовано в классах, расширяюющих интерфейс.
+	 * Существующие реализации: CORBALoginPerformer, DatabaseLoginPerformer, XMLLoginPerformer.
+	 */
+	private static LoginPerformer loginPerformer;
 
-	private static final IdlSessionKey EMPTY_IDL_SESSION_KEY = new IdlSessionKey("");
-	private static final SessionKey EMPTY_SESSION_KEY = new SessionKey(EMPTY_IDL_SESSION_KEY);
-
-	/*-********************************************************************
-	 * Static immutable data.                                             *
-	 **********************************************************************/
-
-	private static LoginServerConnectionManager loginServerConnectionManager;
+	/**
+	 * Восстановление утерянной сессии.
+	 */
 	private static LoginRestorer loginRestorer;
-
-	/*-********************************************************************
-	 * Stateful data.                                                     *
-	 **********************************************************************/
-
-	private static SessionKey sessionKey;
-	private static IdlSessionKey idlSessionKey;
-	private static Identifier userId;
-	private static Identifier domainId;
-	private static CommonUser commonUser;
 
 	private LoginManager() {
 		assert false;
 	}
 
-	static {
-		resetSessionKey();
-	}
-
-	public static void init(final LoginServerConnectionManager loginServerConnectionManager1,
-			final CommonUser commonUser1,
-			final LoginRestorer loginRestorer1) {
-		loginServerConnectionManager = loginServerConnectionManager1;
-		commonUser = commonUser1;
+	public static void init(final LoginPerformer loginPerformer1, final LoginRestorer loginRestorer1) {
+		loginPerformer = loginPerformer1;
 		loginRestorer = loginRestorer1;
 	}
 
+	public static void setLoginPerformer(final LoginPerformer loginPerformer1) {
+		loginPerformer = loginPerformer1;
+	}
 
-	/*
-	 * @todo Write meaningful processing of all possible error codes
-	 */
+	public static void setLoginRestorer(final LoginRestorer loginRestorer1) {
+		loginRestorer = loginRestorer1;
+	}
+
 	public static Set<Domain> getAvailableDomains() throws CommunicationException, LoginException {
-		final LoginServer loginServer = loginServerConnectionManager.getLoginServerReference();
-		try {
-			final IdlDomain[] domainsT = loginServer.transmitAvailableDomains();
-			final Set<Domain> availableDomains = StorableObjectPool.fromTransferables(domainsT, true);
-			StorableObjectPool.putStorableObjects(availableDomains);
-			return availableDomains;
-		} catch (AMFICOMRemoteException are) {
-			throw new LoginException("Cannot get available domains -- " + are.message);
-		} catch (ApplicationException ae) {
-			throw new LoginException("Cannot get available domains -- " + ae.getMessage(), ae);
-		}
+		return loginPerformer.getAvailableDomains();
 	}
 
-	/*
-	 * @todo Write meaningful processing of all possible error codes
-	 * */
-	public static void login(final String login,
-			final String password,
-			final Identifier loginDomainId) throws CommunicationException, LoginException {
-		assert login != null : NON_NULL_EXPECTED;
-		assert password != null : NON_NULL_EXPECTED;
-		assert loginDomainId != null : NON_NULL_EXPECTED;
-
-		if (isLoggedIn()) {
-			throw new LoginException(I18N.getString("Error.AlreadyLoggedIn"), true);
-		}
-
-		final LoginServer loginServer = loginServerConnectionManager.getLoginServerReference();
-		try {
-			final IdlSessionKeyHolder idlSessionKeyHolder = new IdlSessionKeyHolder();
-			final IdlIdentifierHolder userIdHolder = new IdlIdentifierHolder();
-			loginServer.login(login, password, loginDomainId.getIdlTransferable(), commonUser, idlSessionKeyHolder, userIdHolder);
-
-			idlSessionKey = idlSessionKeyHolder.value;
-			sessionKey = new SessionKey(idlSessionKey);
-			userId = Identifier.valueOf(userIdHolder.value);
-			domainId = loginDomainId;
-		} catch (final AMFICOMRemoteException are) {
-			switch (are.errorCode.value()) {
-				case IdlErrorCode._ERROR_ILLEGAL_LOGIN:
-					throw new LoginException(I18N.getString("Error.IllegalLogin"));
-				case IdlErrorCode._ERROR_ILLEGAL_PASSWORD:
-					throw new LoginException(I18N.getString("Error.IllegalPassword"));
-				case IdlErrorCode._ERROR_ACCESS_VALIDATION:
-					throw new LoginException(I18N.getString("Error.AccessValidation"));
-				case IdlErrorCode._ERROR_ALREADY_LOGGED:
-					throw new LoginException(I18N.getString("Error.AlreadyLoggedIn"));
-				default:
-					throw new LoginException(I18N.getString("Error.CannotLogin") + " -- " + are.message);
-			}
-		} catch (final SystemException se) {
-			throw new LoginException(I18N.getString("Error.CannotLogin") + " -- " + se.getMessage());
-		}
+	public static void login(final String login, final String password, final Identifier loginDomainId)
+			throws CommunicationException,
+				LoginException {
+		loginPerformer.login(login, password, loginDomainId);
 	}
 
-	/*
-	 * @todo Write meaningful processing of all possible error codes
-	 * */
 	public static void logout() throws CommunicationException, LoginException {
-		if (!isLoggedIn()) {
-			throw new LoginException(I18N.getString("Error.AlreadyLoggedOut"));
-		}
+		loginPerformer.logout();
+	}
 
-		final LoginServer loginServer = loginServerConnectionManager.getLoginServerReference();
-		try {
-			loginServer.logout(idlSessionKey);
-		} catch (AMFICOMRemoteException are) {
-			switch (are.errorCode.value()) {
-				case IdlErrorCode._ERROR_NOT_LOGGED_IN:
-					throw new LoginException("Not logged in");
-				default:
-					throw new LoginException("Cannot logout -- " + are.message);
-			}
-		} finally {
-			resetSessionKey();
-		}
+	public static boolean isLoggedIn() {
+		return loginPerformer.isLoggedIn();
 	}
 
 	/**
@@ -169,58 +76,20 @@ public final class LoginManager {
 	 */
 	public static boolean restoreLogin() throws CommunicationException, LoginException {
 		if (loginRestorer != null && loginRestorer.restoreLogin()) {
-			resetSessionKey();
-			login(loginRestorer.getLogin(), loginRestorer.getPassword(), loginRestorer.getDomainId());
+			loginPerformer.restoreLogin(loginRestorer.getLogin(), loginRestorer.getPassword());
 			return true;
 		}
 		return false;
-	}
-
-	private static void resetSessionKey() {
-		idlSessionKey = EMPTY_IDL_SESSION_KEY;
-		sessionKey = EMPTY_SESSION_KEY;
-		userId = VOID_IDENTIFIER;
-		domainId = VOID_IDENTIFIER;
-	}
-
-	/**
-	 * @throws IllegalStateException if not logged in.
-	 */
-	public static SessionKey getSessionKey() {
-		if (isLoggedIn()) {
-			return sessionKey;
-		}
-		throw new IllegalStateException();
-	}
-
-	/**
-	 * @throws IllegalStateException if not logged in.
-	 */
-	public static IdlSessionKey getIdlSessionKey() {
-		if (isLoggedIn()) {
-			return idlSessionKey;
-		}
-		throw new IllegalStateException();
-	}
-
-	/**
-	 * @throws IllegalStateException if not logged in.
-	 */
-	public static Identifier getUserId() {
-		if (isLoggedIn()) {
-			return userId;
-		}
-		throw new IllegalStateException();
 	}
 
 	/**
 	 * Set my own password
 	 * @param password
 	 * @throws CommunicationException
-	 * @throws AMFICOMRemoteException
+	 * @throws LoginException
 	 */
-	public static void setPassword(final String password) throws CommunicationException, AMFICOMRemoteException {
-		loginServerConnectionManager.getLoginServerReference().setPassword(idlSessionKey, userId.getIdlTransferable(), password);
+	public static void setPassword(final String password) throws CommunicationException, LoginException {
+		loginPerformer.setPassword(password);
 	}
 
 	/**
@@ -228,38 +97,24 @@ public final class LoginManager {
 	 * @param systemUserId
 	 * @param password
 	 * @throws CommunicationException
-	 * @throws AMFICOMRemoteException
-	 * @throws IllegalArgumentException if systemUserId is not system user id
+	 * @throws LoginException
 	 */
 	public static void setPassword(final Identifier systemUserId, final String password)
-			throws CommunicationException, AMFICOMRemoteException {
-
-		if (systemUserId.getMajor() != ObjectEntities.SYSTEMUSER_CODE) {
-			throw new IllegalArgumentException("System user identifier expected.");
-		}
-
-		final IdlIdentifier userIdTransferable = systemUserId.getIdlTransferable();
-		loginServerConnectionManager.getLoginServerReference().setPassword(idlSessionKey, userIdTransferable, password);
+			throws CommunicationException,
+				LoginException {
+		loginPerformer.setPassword(systemUserId, password);
 	}
 
-	/**
-	 * @throws IllegalStateException
-	 *         if not logged in.
-	 */
+	public static SessionKey getSessionKey() {
+		return loginPerformer.getSessionKey();
+	}
+
+	public static Identifier getUserId() {
+		return loginPerformer.getUserId();
+	}
+
 	public static Identifier getDomainId() {
-		if (isLoggedIn()) {
-			return domainId;
-		}
-		throw new IllegalStateException();
-	}
-
-	public static boolean isLoggedIn() {
-		final boolean loggedIn = (idlSessionKey != EMPTY_IDL_SESSION_KEY);
-		assert loggedIn
-				? userId.getMajor() == SYSTEMUSER_CODE && domainId.getMajor() == DOMAIN_CODE
-				: userId.isVoid() && domainId.isVoid()
-						: userId + "; " + domainId + "; logged in: " + loggedIn;
-		return loggedIn;
+		return loginPerformer.getDomainId();
 	}
 
 	/**
@@ -274,10 +129,10 @@ public final class LoginManager {
 		assert domainId1 != null : ErrorMessages.NON_NULL_EXPECTED;
 		assert userId1.getMajor() == ObjectEntities.SYSTEMUSER_CODE : ErrorMessages.ILLEGAL_ENTITY_CODE;
 		assert domainId1.getMajor() == ObjectEntities.DOMAIN_CODE : ErrorMessages.ILLEGAL_ENTITY_CODE;
-
-		userId = userId1;
-		domainId = domainId1;
-		idlSessionKey = new IdlSessionKey("XMLStub_" + (new Date()).toString());
-		sessionKey = new SessionKey(idlSessionKey);
+//
+//		userId = userId1;
+//		domainId = domainId1;
+//		idlSessionKey = new IdlSessionKey("XMLStub_" + (new Date()).toString());
+//		sessionKey = new SessionKey(idlSessionKey);
 	}
 }
