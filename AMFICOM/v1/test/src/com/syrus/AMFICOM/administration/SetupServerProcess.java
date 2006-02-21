d7a0064fb6b3ac47f266547cfe2e65f8ff4555fb
Arseniy Tashoyan <tashoyan@gmail.com>
2006-02-21 18:53:40 +0300

    ADD: New classes for setup

---
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/administration/SetupServerProcess.java
@@ -0,0 +1,145 @@
+/*
+ * $Id: SetupServerProcess.java,v 1.1.2.1 2006/02/21 15:53:40 arseniy Exp $
+ * 
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.administration;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.CompoundCondition;
+import com.syrus.AMFICOM.general.DatabaseCommonTest;
+import com.syrus.AMFICOM.general.EquivalentCondition;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.LoginManager;
+import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.StorableObjectPool;
+import com.syrus.AMFICOM.general.TypicalCondition;
+import com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlCompoundConditionPackage.CompoundConditionSort;
+import com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlTypicalConditionPackage.OperationSort;
+import com.syrus.AMFICOM.setup.I18N;
+
+/**
+ * @version $Revision: 1.1.2.1 $, $Date: 2006/02/21 15:53:40 $
+ * @author $Author: arseniy $
+ * @module test
+ */
+public final class SetupServerProcess extends TestCase {
+
+	public SetupServerProcess(final String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		final DatabaseCommonTest databaseCommonTest = new DatabaseCommonTest();
+		databaseCommonTest.addTestSuite(SetupServerProcess.class);
+		return databaseCommonTest.createTestSetup();
+	}
+
+	public void testCreate() throws ApplicationException {
+		final Identifier userId = LoginManager.getUserId();
+
+		final EquivalentCondition ec = new EquivalentCondition(ObjectEntities.SERVER_CODE);
+		final Set<Server> servers = StorableObjectPool.getStorableObjectsByCondition(ec, true);
+		assertTrue("No servers", servers.size() > 0);
+		final Server server = servers.iterator().next();
+		System.out.println("Server '" + server.getId() + "'");
+
+//	login user
+		final TypicalCondition tc = new TypicalCondition(SystemUserWrapper.LOGINPROCESSOR_LOGIN,
+				OperationSort.OPERATION_EQUALS,
+				ObjectEntities.SYSTEMUSER_CODE,
+				SystemUserWrapper.COLUMN_LOGIN);
+
+//	event user
+		TypicalCondition tc1 = new TypicalCondition(SystemUserWrapper.EVENTPROCESSOR_LOGIN,
+				OperationSort.OPERATION_EQUALS,
+				ObjectEntities.SYSTEMUSER_CODE,
+				SystemUserWrapper.COLUMN_LOGIN);
+
+//	Create compound condition
+		final CompoundCondition cc = new CompoundCondition(tc, CompoundConditionSort.OR, tc1);
+
+//	mserver user
+		tc1 = new TypicalCondition(SystemUserWrapper.MSERVER_LOGIN,
+				OperationSort.OPERATION_EQUALS,
+				ObjectEntities.SYSTEMUSER_CODE,
+				SystemUserWrapper.COLUMN_LOGIN);
+		cc.addCondition(tc1);
+
+//	cmserver user
+		tc1 = new TypicalCondition(SystemUserWrapper.CMSERVER_LOGIN,
+				OperationSort.OPERATION_EQUALS,
+				ObjectEntities.SYSTEMUSER_CODE,
+				SystemUserWrapper.COLUMN_LOGIN);
+		cc.addCondition(tc1);
+
+//	mscharserver user
+		tc1 = new TypicalCondition(SystemUserWrapper.MSCHARSERVER_LOGIN,
+				OperationSort.OPERATION_EQUALS,
+				ObjectEntities.SYSTEMUSER_CODE,
+				SystemUserWrapper.COLUMN_LOGIN);
+		cc.addCondition(tc1);
+
+		final Set users = StorableObjectPool.getStorableObjectsByCondition(cc, true);
+		final Map<String, SystemUser> usersMap = new HashMap<String, SystemUser>(users.size());
+		for (Iterator it = users.iterator(); it.hasNext();) {
+			final SystemUser user = (SystemUser) it.next();
+			System.out.println("User: " + user.getId() + ", " + user.getLogin());
+			usersMap.put(user.getLogin(), user);
+		}
+
+		SystemUser serverProcessUser;
+
+		//	login process
+		serverProcessUser = usersMap.get(SystemUserWrapper.LOGINPROCESSOR_LOGIN);
+		ServerProcess.createInstance(userId,
+				ServerProcessWrapper.LOGIN_PROCESS_CODENAME,
+				server.getId(),
+				serverProcessUser.getId(),
+				I18N.getString(SetupSystemUser.KEY_LOGINSERVER_DESCRIPTION));
+
+//	event process
+		serverProcessUser = usersMap.get(SystemUserWrapper.EVENTPROCESSOR_LOGIN);
+		ServerProcess.createInstance(userId,
+				ServerProcessWrapper.EVENT_PROCESS_CODENAME,
+				server.getId(),
+				serverProcessUser.getId(),
+				I18N.getString(SetupSystemUser.KEY_EVENTSERVER_DESCRIPTION));
+
+//	mserver process
+		serverProcessUser = usersMap.get(SystemUserWrapper.MSERVER_LOGIN);
+		ServerProcess.createInstance(userId,
+				ServerProcessWrapper.MSERVER_PROCESS_CODENAME,
+				server.getId(),
+				serverProcessUser.getId(),
+				I18N.getString(SetupSystemUser.KEY_MSERVER_DESCRIPTION));
+
+//	cmserver process
+		serverProcessUser = usersMap.get(SystemUserWrapper.CMSERVER_LOGIN);
+		ServerProcess.createInstance(userId,
+				ServerProcessWrapper.CMSERVER_PROCESS_CODENAME,
+				server.getId(),
+				serverProcessUser.getId(),
+				I18N.getString(SetupSystemUser.KEY_CMSERVER_DESCRIPTION));
+
+//	mscharserver process
+		serverProcessUser = usersMap.get(SystemUserWrapper.MSCHARSERVER_LOGIN);
+		ServerProcess.createInstance(userId,
+				ServerProcessWrapper.MSCHARSERVER_PROCESS_CODENAME,
+				server.getId(),
+				serverProcessUser.getId(),
+				I18N.getString(SetupSystemUser.KEY_MSCHARSERVER_DESCRIPTION));
+
+		StorableObjectPool.flush(ObjectEntities.SERVERPROCESS_CODE, userId, true);
+	}
+}
---
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/configuration/SetupEquipmentType.java
@@ -0,0 +1,54 @@
+/*-
+ * $Id: SetupEquipmentType.java,v 1.1.2.1 2006/02/21 15:53:40 arseniy Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.AMFICOM.configuration;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.LoginManager;
+import com.syrus.AMFICOM.general.SQLCommonTest;
+import com.syrus.AMFICOM.general.StorableObjectPool;
+import com.syrus.AMFICOM.setup.I18N;
+
+/**
+ * @version $Revision: 1.1.2.1 $, $Date: 2006/02/21 15:53:40 $
+ * @author $Author: arseniy $
+ * @module test
+ */
+public final class SetupEquipmentType extends TestCase {
+	private static String KEY_DESCRIPTION_ROOT = "Description.EquipmentType.";
+
+	public SetupEquipmentType(final String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		final SQLCommonTest commonTest = new SQLCommonTest();
+		commonTest.addTestSuite(SetupEquipmentType.class);
+		return commonTest.createTestSetup();
+	}
+
+	public void testCreate() throws ApplicationException {
+		final Identifier creatorId = LoginManager.getUserId();
+
+		final Set<EquipmentType> equipmentTypes = new HashSet<EquipmentType>();
+		for (final EquipmentTypeCodename equipmentTypeCodename : EquipmentTypeCodename.values()) {
+			final String codename = equipmentTypeCodename.stringValue();
+			equipmentTypes.add(EquipmentType.createInstance(creatorId,
+					codename,
+					I18N.getString(KEY_DESCRIPTION_ROOT + codename)));
+		}
+
+		StorableObjectPool.flush(equipmentTypes, creatorId, false);
+	}
+}
