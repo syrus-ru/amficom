0035cbee54c4f6d6dc97803a38da7a92264a66cb
cvs-fast-export <cvs-fast-export>
2006-03-01 16:27:04 +0000

    Synthetic commit for incomplete tag SAA_BEFORE_ROLLBACK_ENDTIME

--- AMFICOM/v1/database/schema/administration/CREATE.sql
+++
@@ -1,7 +0,0 @@
--- $Id: CREATE.sql,v 1.4 2005/06/15 07:50:17 bass Exp $
-
-REM types
-@@administration/type/CREATE;
-
-REM objects
-@@administration/object/CREATE;
--- AMFICOM/v1/database/schema/resource/object/DROP.sql
+++
@@ -1,8 +0,0 @@
--- $Id: DROP.sql,v 1.6 2005/11/14 11:08:53 arseniy Exp $
-
-DROP TABLE LayoutItem;
-DROP TABLE ImportUIDMap;
-DROP TABLE ImageResource;
-
-DROP SEQUENCE LayoutItem_Seq;
-DROP SEQUENCE ImageResource_Seq;
