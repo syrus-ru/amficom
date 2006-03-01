0d4bb059962ed16face08ab1645c9230de64460c
Arseniy Tashoyan <tashoyan@gmail.com>
2006-03-01 14:41:46 +0300

    COMPATIBILITY: Method Parameter#stringValue()

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestActionParameter.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestActionParameter.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: TestActionParameter.java,v 1.1.2.1 2006/02/28 15:45:48 arseniy Exp $
+ * $Id: TestActionParameter.java,v 1.1.2.2 2006/03/01 11:41:46 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -16,11 +16,11 @@ import junit.framework.TestCase;
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.CORBACommonTest;
 import com.syrus.AMFICOM.general.ParameterType;
+import com.syrus.AMFICOM.measurement.ActionParameterTypeBinding.ParameterValueKind;
 import com.syrus.AMFICOM.reflectometry.ReflectometryParameterTypeCodename;
-import com.syrus.util.ByteArray;
 
 /**
- * @version $Revision: 1.1.2.1 $, $Date: 2006/02/28 15:45:48 $
+ * @version $Revision: 1.1.2.2 $, $Date: 2006/03/01 11:41:46 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module test
@@ -43,11 +43,12 @@ public final class TestActionParameter extends TestCase {
 		final MeasurementPortType measurementPortType = MeasurementPortType.valueOf(MeasurementPortTypeCodename.REFLECTOMETRY_PK7600.stringValue());
 
 		final ActionParameterTypeBinding actionParameterTypeBinding = ActionParameterTypeBinding.valueOf(parameterType, measurementType, measurementPortType);
+		final ParameterValueKind parameterValueKind = actionParameterTypeBinding.getParameterValueKind();
+		System.out.println("Parameter value kind: " + parameterValueKind);
 
 		final Set<ActionParameter> actionParameters = ActionParameter.getValues(actionParameterTypeBinding);
 		for (final ActionParameter actionParameter : actionParameters) {
-			final int value = (new ByteArray(actionParameter.getValue())).toInt();
-			System.out.println("Codename: '" + actionParameter.getTypeCodename() + "', value: " + value);
+			System.out.println("Codename: '" + actionParameter.getTypeCodename() + "', value: " + actionParameter.stringValue());
 		}
 	}
 }
