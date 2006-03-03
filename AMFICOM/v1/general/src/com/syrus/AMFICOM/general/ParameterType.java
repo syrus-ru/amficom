a81c6eb070157adda21a4e1758e6d697822d4c52
Arseniy Tashoyan <tashoyan@gmail.com>
2006-03-03 12:49:26 +0300

    ADD: Metod getCodenameIdentifierMap

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ParameterType.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/ParameterType.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ParameterType.java,v 1.74.2.5 2006/02/28 15:20:02 arseniy Exp $
+ * $Id: ParameterType.java,v 1.74.2.6 2006/03/03 09:49:26 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -14,10 +14,14 @@ import static com.syrus.AMFICOM.general.ErrorMessages.ONLY_ONE_EXPECTED;
 import static com.syrus.AMFICOM.general.ObjectEntities.PARAMETER_TYPE_CODE;
 import static com.syrus.AMFICOM.general.StorableObjectVersion.INITIAL_VERSION;
 import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_CODENAME;
+import static com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlCompoundConditionPackage.CompoundConditionSort.OR;
 import static com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlTypicalConditionPackage.OperationSort.OPERATION_EQUALS;
 
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.omg.CORBA.ORB;
@@ -27,7 +31,7 @@ import com.syrus.AMFICOM.general.corba.IdlParameterTypeHelper;
 import com.syrus.AMFICOM.general.corba.IdlStorableObject;
 
 /**
- * @version $Revision: 1.74.2.5 $, $Date: 2006/02/28 15:20:02 $
+ * @version $Revision: 1.74.2.6 $, $Date: 2006/03/03 09:49:26 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module general
@@ -172,5 +176,55 @@ public final class ParameterType extends StorableObjectType<ParameterType> {
 		}
 		assert parameterTypes.size() == 1 : ONLY_ONE_EXPECTED;
 		return parameterTypes.iterator().next();
+	}
+
+	/**
+	 * Create Map codename-identifier.
+	 * @param codenames
+	 * @return Unmodifiable Map<String codename, Identifier id>
+	 * @throws ApplicationException
+	 */
+	public static Map<String, Identifier> getCodenameIdentifierMap(final String[] codenames) throws ApplicationException {
+		assert codenames != null : NON_NULL_EXPECTED;
+
+		if (codenames.length == 0) {
+			return Collections.emptyMap();
+		}
+
+		//Slightly optimized case
+		if (codenames.length == 1) {
+			final String codename = codenames[0];
+			final TypicalCondition condition = new TypicalCondition(codename,
+					OPERATION_EQUALS,
+					PARAMETER_TYPE_CODE,
+					COLUMN_CODENAME);
+			final Set<Identifier> parameterTypeIds = StorableObjectPool.getIdentifiersByCondition(condition, true);
+			if (parameterTypeIds.isEmpty()) {
+				return Collections.emptyMap();
+			}
+
+			assert parameterTypeIds.size() == 1 : ONLY_ONE_EXPECTED;
+			final Identifier parameterTypeId = parameterTypeIds.iterator().next();
+			final Map<String, Identifier> codenameIdentifierMap = new HashMap<String, Identifier>();
+			codenameIdentifierMap.put(codename, parameterTypeId);
+			return Collections.unmodifiableMap(codenameIdentifierMap);
+
+		}
+
+		final Set<TypicalCondition> codenameConditions = new HashSet<TypicalCondition>();
+		for (final String codename : codenames) {
+			codenameConditions.add(new TypicalCondition(codename, OPERATION_EQUALS, PARAMETER_TYPE_CODE, COLUMN_CODENAME));
+		}
+		final CompoundCondition condition = new CompoundCondition(codenameConditions, OR);
+		final Set<ParameterType> parameterTypes = StorableObjectPool.getStorableObjectsByCondition(condition, true);
+		if (parameterTypes.isEmpty()) {
+			return Collections.emptyMap();
+		}
+
+		final Map<String, Identifier> codenameIdentifierMap = new HashMap<String, Identifier>();
+		for (final ParameterType parameterType : parameterTypes) {
+			codenameIdentifierMap.put(parameterType.getCodename(), parameterType.getId());
+		}
+		return Collections.unmodifiableMap(codenameIdentifierMap);
 	}
 }
