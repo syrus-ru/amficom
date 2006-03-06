4e63108aef636852652b8a4615e0af9d5bbe63a4
Arseniy Tashoyan <tashoyan@gmail.com>
2006-03-06 16:57:50 +0300

    COMPATIBILITY: New contract

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestAnalysisResult.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestAnalysisResult.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestAnalysisResult.java,v 1.5 2006/01/18 15:06:51 saa Exp $
+ * $Id: TestAnalysisResult.java,v 1.5.2.1 2006/03/06 13:57:50 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,26 +7,25 @@
  */
 package com.syrus.AMFICOM.measurement;
 
+import static com.syrus.AMFICOM.general.ObjectEntities.ANALYSISRESULTPARAMETER_CODE;
+import static com.syrus.AMFICOM.reflectometry.ReflectometryParameterTypeCodename.DADARA_ALARMS;
+
 import java.util.Set;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 
-import com.syrus.io.DataFormatException;
 import com.syrus.AMFICOM.analysis.dadara.ReflectogramMismatchImpl;
 import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.DatabaseCommonTest;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.LinkedIdsCondition;
-import com.syrus.AMFICOM.general.ObjectEntities;
-import com.syrus.AMFICOM.general.ParameterType;
 import com.syrus.AMFICOM.general.StorableObjectPool;
-import com.syrus.AMFICOM.reflectometry.ReflectogramMismatch;
 import com.syrus.io.DataFormatException;
 
 /**
- * @version $Revision: 1.5 $, $Date: 2006/01/18 15:06:51 $
- * @author $Author: saa $
+ * @version $Revision: 1.5.2.1 $, $Date: 2006/03/06 13:57:50 $
+ * @author $Author: arseniy $
  * @module test
  */
 public final class TestAnalysisResult extends TestCase {
@@ -46,35 +45,27 @@ public final class TestAnalysisResult extends TestCase {
 //	}
 
 	public void testLoadResult() throws ApplicationException, DataFormatException {
-		long t0 = System.nanoTime();
-		Identifier analysisId = new Identifier("Analysis_63");
-		//Analysis analysis = (Analysis) StorableObjectPool.getStorableObject(analysisId, true);
-		LinkedIdsCondition lic =
-			new LinkedIdsCondition(analysisId, ObjectEntities.RESULT_CODE);
-		long t1 = System.nanoTime();
-		Set set = StorableObjectPool.getStorableObjectsByCondition(lic, true);
-		long t2 = System.nanoTime();
-		Result result = (Result) set.iterator().next();
-		long t3 = System.nanoTime();
-		System.out.println("Loaded: " + result.getId());
+		final long t0 = System.nanoTime();
+		final Identifier analysisId = new Identifier("Analysis_63");
+		//Analysis analysis = StorableObjectPool.getStorableObject(analysisId, true);
+		final LinkedIdsCondition lic = new LinkedIdsCondition(analysisId, ANALYSISRESULTPARAMETER_CODE);
+		final long t1 = System.nanoTime();
+		final Set<AnalysisResultParameter> analysisResultParameters = StorableObjectPool.getStorableObjectsByCondition(lic, true);
+		final long t2 = System.nanoTime();
 
-		final Parameter[] parameters = result.getParameters();
-		System.out.println("Result has " + parameters.length + " parameters");
-		for (int i = 0; i < parameters.length; i++) {
-			final ParameterType parameterType = parameters[i].getType();
-			String codename = parameterType.getCodename();
-			System.out.println("Parameter [" + i + "]: codename " + codename);
-			if (parameterType.equals(ParameterType.DADARA_ALARMS)) {
-				ReflectogramMismatchImpl[] alarms =
-					ReflectogramMismatchImpl.alarmsFromByteArray(parameters[i].getValue());
+		System.out.println("Result has " + analysisResultParameters.size() + " parameters");
+		for (final AnalysisResultParameter analysisResultParameter : analysisResultParameters) {
+			final String codename = analysisResultParameter.getTypeCodename();
+			System.out.println("Parameter codename: " + codename);
+			if (codename.equals(DADARA_ALARMS.stringValue())) {
+				ReflectogramMismatchImpl[] alarms = ReflectogramMismatchImpl.alarmsFromByteArray(analysisResultParameter.getValue());
 				System.out.println("\t" + "Number of alarms: " + alarms.length);
-				for (int j = 0 ; j < alarms.length; j++) {
+				for (int j = 0; j < alarms.length; j++) {
 					System.out.println("\t\t" + "alarm [" + j + "] == " + alarms[j]);
 				}
 			}
 		}
 		System.out.println("id & lic: " + (t1 - t0) / 1e6 + " ms");
 		System.out.println("getSOByC: " + (t2 - t1) / 1e6 + " ms");
-		System.out.println("iterator: " + (t3 - t2) / 1e6 + " ms");
 	}
 }
