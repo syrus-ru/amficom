c89d5bc43471ed7607ed634ea5d9fae130d3c823
Stanislav Kholshin <kholshin@gmail.com>
2006-03-09 16:17:00 +0300

    fix: new algoritm of dererming nearest NodeLink

--- AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/strategy/MapMarkerStrategy.java
+++ AMFICOM/v1/mapviewclient/src/com/syrus/AMFICOM/client/map/strategy/MapMarkerStrategy.java
@@ -1,5 +1,5 @@
 /*-
- * $$Id: MapMarkerStrategy.java,v 1.34 2006/02/15 12:54:38 stas Exp $$
+ * $$Id: MapMarkerStrategy.java,v 1.35 2006/03/09 13:17:00 stas Exp $$
  *
  * Copyright 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -15,8 +15,8 @@ import com.syrus.AMFICOM.client.map.MapCoordinatesConverter;
 import com.syrus.AMFICOM.client.map.MapDataException;
 import com.syrus.AMFICOM.client.map.MapState;
 import com.syrus.AMFICOM.client.map.controllers.MarkerController;
-import com.syrus.AMFICOM.client.map.ui.MotionDescriptor;
 import com.syrus.AMFICOM.client.model.MapApplicationModel;
+import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.map.AbstractNode;
 import com.syrus.AMFICOM.map.MapElement;
 import com.syrus.AMFICOM.map.NodeLink;
@@ -28,7 +28,7 @@ import com.syrus.util.Log;
 /**
  * Стратегия управления маркером.
  * 
- * @version $Revision: 1.34 $, $Date: 2006/02/15 12:54:38 $
+ * @version $Revision: 1.35 $, $Date: 2006/03/09 13:17:00 $
  * @author $Author: stas $
  * @author Andrei Kroupennikov
  * @module mapviewclient
@@ -110,80 +110,52 @@ public final class MapMarkerStrategy extends AbstractMapStrategy
 				+ " " + this.marker.getStartNode().getId() //$NON-NLS-1$
 				+ " " + this.marker.getEndNode().getId() //$NON-NLS-1$
 				+ " " + this.marker.getCablePath().getId(), Log.DEBUGLEVEL09); //$NON-NLS-1$
-		//Проверка того что маркер можно перемещать и его перемещение
-		if (super.logicalNetLayer.getContext().getApplicationModel().isEnabled(MapApplicationModel.ACTION_USE_MARKER))
-		{
-			NodeLink nodeLink = this.marker.getNodeLink();
-			AbstractNode startNode = this.marker.getStartNode();
-			AbstractNode endNode = this.marker.getEndNode();
-			Point anchorPoint = converter.convertMapToScreen(this.marker.getLocation());
+
+//	Проверка того что маркер можно перемещать и его перемещение
+		if (super.logicalNetLayer.getContext().getApplicationModel().isEnabled(MapApplicationModel.ACTION_USE_MARKER)) {
+			// все NodeLink'и по которым может перемещаться маркер
+//			List<NodeLink> nodeLinks = this.marker.getMeasurementPath().getSortedNodeLinks();
+			
+			NodeLink nearestNodeLink = null;
+			CablePath path = null;
+			double shortestDistance = Integer.MAX_VALUE;
+			
+			for(CablePath cablePath : this.marker.getMeasurementPath().getSortedCablePaths()) {
+				for(NodeLink nodeLink : cablePath.getSortedNodeLinks()) {
+					double dist = distance(point, nodeLink);
+					if (dist < shortestDistance) {
+						shortestDistance = dist;
+						nearestNodeLink = nodeLink;
+						path = cablePath;
+					}		
+				}
+			}
+
+			AbstractNode startNode = nearestNodeLink.getStartNode();
+			AbstractNode endNode = nearestNodeLink.getEndNode();
 			Point startPoint = converter.convertMapToScreen(startNode.getLocation());
 			Point endPoint = converter.convertMapToScreen(endNode.getLocation());
-			double lengthFromStartNode;
-			MotionDescriptor motionDescriptor = new MotionDescriptor(startPoint, endPoint, anchorPoint, point);
-			lengthFromStartNode = motionDescriptor.lengthFromStartNode;
-			while (lengthFromStartNode > motionDescriptor.nodeLinkLength)
-			{
-				nodeLink = this.marker.nextNodeLink();
-				if (nodeLink == null)
-					lengthFromStartNode = motionDescriptor.nodeLinkLength;
-				else
-				{
-					if(this.marker.getCablePath().getStartNode().equals(endNode)
-							|| this.marker.getCablePath().getEndNode().equals(endNode)) {
-						CablePath nextCablePath = this.marker.nextCablePath();
-						if(nextCablePath != null) {
-							this.marker.setCablePath(nextCablePath);
-						}
-					}
-
-					startNode = endNode;
-					endNode = nodeLink.getOtherNode(startNode);
-					this.marker.setNodeLink(nodeLink);
-					this.marker.setStartNode(startNode);
-					this.marker.setEndNode(endNode);
-					startPoint = converter.convertMapToScreen(startNode.getLocation());
-					endPoint = converter.convertMapToScreen(endNode.getLocation());
-					motionDescriptor = new MotionDescriptor(startPoint, endPoint, anchorPoint, point);
-					lengthFromStartNode = motionDescriptor.lengthFromStartNode;
-					if (lengthFromStartNode < 0)
-					{
-						lengthFromStartNode = 0;
-						break;
-					}
-				}
+			Point anchorPoint = converter.convertMapToScreen(this.marker.getLocation());
+			
+			double lengthFromStartNode = getLenghtFromStart(startPoint, endPoint, anchorPoint, point);
+			double nodeLinkLength = distance(startPoint, endPoint);
+			if (lengthFromStartNode < 0) {
+				lengthFromStartNode = 0;
+			} else if (lengthFromStartNode > nodeLinkLength) {
+				lengthFromStartNode = nodeLinkLength;
 			}
-			while (lengthFromStartNode < 0)
-			{
-				nodeLink = this.marker.previousNodeLink();
-				if (nodeLink == null)
-					lengthFromStartNode = 0;
-				else
-				{
-					if(this.marker.getCablePath().getStartNode().equals(startNode)
-							|| this.marker.getCablePath().getEndNode().equals(startNode)) {
-						CablePath previousCablePath = this.marker.previousCablePath();
-						if(previousCablePath != null) {
-							this.marker.setCablePath(previousCablePath);
-						}
-					}
-
-					endNode = startNode;
-					startNode = nodeLink.getOtherNode(endNode);
-					this.marker.setNodeLink(nodeLink);
-					this.marker.setStartNode(startNode);
-					this.marker.setEndNode(endNode);
-					startPoint = converter.convertMapToScreen(startNode.getLocation());
-					endPoint = converter.convertMapToScreen(endNode.getLocation());
-					motionDescriptor = new MotionDescriptor(startPoint, endPoint, anchorPoint, point);
-					lengthFromStartNode = motionDescriptor.lengthFromStartNode;
-					if (lengthFromStartNode > motionDescriptor.nodeLinkLength)
-					{
-						lengthFromStartNode = motionDescriptor.nodeLinkLength;
-						break;
-					}
-				}
+			
+			this.marker.setNodeLink(nearestNodeLink);
+			this.marker.setCablePath(path);
+			this.marker.setStartNode(startNode);
+			this.marker.setEndNode(endNode);
+			try {
+				final double fromStartLengthLf = mc.getFromStartLengthLf(this.marker);
+				this.marker.setPhysicalDistance(fromStartLengthLf);
+			} catch (ApplicationException e) {
+				Log.errorMessage(e);
 			}
+			 
 			mc.adjustPosition(this.marker, lengthFromStartNode);
 			mc.notifyMarkerMoved(this.marker);
 		}
@@ -201,6 +173,54 @@ public final class MapMarkerStrategy extends AbstractMapStrategy
 		if (actionMode == MapState.MOVE_ACTION_MODE)
 		{//empty
 		}
+	}
+	
+	private double distance(Point b, NodeLink link) throws MapConnectionException, MapDataException {
+		final MapCoordinatesConverter converter = this.logicalNetLayer.getConverter();
+		final Point a = converter.convertMapToScreen(link.getStartNode().getLocation());
+		final Point c = converter.convertMapToScreen(link.getEndNode().getLocation());
+		
+		double scalar1 = scalar(a, c, a, b);
+		if (scalar1 < 0) { // угол между AC и AB тупой 
+			return distance(a, b);
+		}
+		double scalar2 = scalar(c, a, c, b);
+		if (scalar2 < 0) { // угол между CA и CB тупой 
+			return distance(c, b);
+		}
+		double l1 = distance(a, b);
+		double l2 = distance(b, c);
+		double l3 = distance(a, c);
+		double p = (l1 + l2 + l3) / 2;
+		double h = (2 * Math.sqrt(p * (p - l1) * (p - l2) * (p - l3))) / l3;
+		return h;
+	}
+	
+	private double distance(Point p1, Point p2) {
+		return Math.sqrt((long)(p1.x - p2.x) * (long)(p1.x - p2.x) + (long)(p1.y - p2.y) * (long)(p1.y - p2.y));
+	}
+	
+	private double scalar(Point p1, Point p2, Point p3, Point p4) {
+		return (p2.x - p1.x) * (p4.x - p3.x) + (p2.y - p1.y) * (p4.y - p3.y);
+	}
+	
+	private double getLenghtFromStart(Point startPoint, Point endPoint, Point anchorPoint, Point point) {
+		double nodeLinkLength = distance(startPoint, endPoint);
+		double lengthFromStartNode = distance(startPoint, anchorPoint);
+		double lengthThisToMousePoint = distance(point, anchorPoint);
+		double cosA = (lengthThisToMousePoint == 0 ) ? 0.0 :
+				(	(endPoint.x - startPoint.x) * (point.x - anchorPoint.x) + 
+					(endPoint.y - startPoint.y) * (point.y - anchorPoint.y) ) /
+					( nodeLinkLength * lengthThisToMousePoint );
+		
+		// скалярное произведение векторов фрагмента и lengthFromStartNode
+		double scalar = scalar(startPoint, endPoint, startPoint, anchorPoint);
+
+		if(scalar < 0) {
+			lengthFromStartNode = -lengthFromStartNode;
+		}
+		lengthFromStartNode = lengthFromStartNode + cosA * lengthThisToMousePoint;
+		return lengthFromStartNode;
 	}
 }
 
