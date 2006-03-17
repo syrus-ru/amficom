298d3042650334df205509109541249bcb15654e
cvs-fast-export <cvs-fast-export>
2006-03-17 16:34:40 +0000

    Synthetic commit for incomplete tag HARD-ALARM-FOR-REDMASKS

--- AMFICOM/v1/database/schema/map/DROP.sql
+++
@@ -1,4 +0,0 @@
--- $Id: DROP.sql,v 1.7 2005/08/18 09:33:26 arseniy Exp $
-
-@@map/object/DROP;
-@@map/type/DROP;
--- AMFICOM/v2/database/schema/configuration/object/DROP.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE monitoredelement;
-DROP TABLE equipment;
-
-DROP SEQUENCE monitoredelement_seq;
-DROP SEQUENCE equipment_seq;
-
