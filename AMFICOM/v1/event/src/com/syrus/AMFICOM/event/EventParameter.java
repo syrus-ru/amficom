a42a9ad826f0896819acc81a03ef3fc421f41891
cvs-fast-export <cvs-fast-export>
2006-03-17 10:42:07 +0000

    Synthetic commit for incomplete tag INTEGRATION-20060317

--- AMFICOM/v1/event/src/com/syrus/AMFICOM/event/EventParameter.java
+++ AMFICOM/v1/event/src/com/syrus/AMFICOM/event/EventParameter.java
@@ -1,13 +1,11 @@
 /*
- * $Id: EventParameter.java,v 1.33 2006/03/15 15:47:20 arseniy Exp $
+ * $Id: EventParameter.java,v 1.32 2006/02/16 13:34:26 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
  * Проект: АМФИКОМ.
  */
 package com.syrus.AMFICOM.event;
-
-import static com.syrus.AMFICOM.general.ObjectEntities.EVENTPARAMETER_CODE;
 
 import org.omg.CORBA.ORB;
 
@@ -17,41 +15,44 @@ import com.syrus.AMFICOM.general.Identifiable;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.IdentifierGenerationException;
 import com.syrus.AMFICOM.general.IdentifierPool;
+import com.syrus.AMFICOM.general.ObjectEntities;
+import com.syrus.AMFICOM.general.ParameterType;
 import com.syrus.util.transport.idl.IdlTransferableObject;
 
 /**
- * @version $Revision: 1.33 $, $Date: 2006/03/15 15:47:20 $
+ * @version $Revision: 1.32 $, $Date: 2006/02/16 13:34:26 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module event
  */
-public final class EventParameter implements Identifiable, IdlTransferableObject<IdlEventParameter> {
-	private static final long serialVersionUID = -6771402972594829134L;
+public final class EventParameter implements Identifiable,
+		IdlTransferableObject<IdlEventParameter> {
+	private static final long serialVersionUID = 4906660762164733352L;
 
 	private Identifier id;
-	private Identifier typeId;
+	private ParameterType type;
 	private String value;
 
 	public EventParameter(final IdlEventParameter ept) {
 		this.id = new Identifier(ept.id);
-		this.typeId = Identifier.valueOf(ept._typeId);
+		this.type = ParameterType.fromTransferable(ept.type);
 		this.value = ept.value;
 	}
 
-	protected EventParameter(final Identifier id, final Identifier typeId, final String value) {
+	protected EventParameter(final Identifier id, final ParameterType type, final String value) {
 		this.id = id;
-		this.typeId = typeId;
+		this.type = type;
 		this.value = value;
 	}
 
-	public static EventParameter createInstance(final Identifier typeId, final String value) throws CreateObjectException {
-		if (typeId == null || value == null) {
+	public static EventParameter createInstance(final ParameterType type, final String value) throws CreateObjectException {
+		if (type == null || value == null) {
 			throw new IllegalArgumentException("Argument is 'null'");
 		}
 
 		try {
-			final EventParameter eventParameter = new EventParameter(IdentifierPool.getGeneratedIdentifier(EVENTPARAMETER_CODE),
-					typeId,
+			final EventParameter eventParameter = new EventParameter(IdentifierPool.getGeneratedIdentifier(ObjectEntities.EVENTPARAMETER_CODE),
+					type,
 					value);
 			return eventParameter;
 		} catch (IdentifierGenerationException ige) {
@@ -65,7 +66,7 @@ public final class EventParameter implements Identifiable, IdlTransferableObject
 	 */
 	public IdlEventParameter getIdlTransferable(final ORB orb) {
 		return new IdlEventParameter(this.id.getIdlTransferable(),
-				this.typeId.getIdlTransferable(orb),
+				this.type.getIdlTransferable(orb),
 				this.value);
 	}
 
@@ -73,8 +74,8 @@ public final class EventParameter implements Identifiable, IdlTransferableObject
 		return this.id;
 	}
 
-	public Identifier getTypeId() {
-		return this.typeId;
+	public ParameterType getType() {
+		return this.type;
 	}
 
 	public String getValue() {
---
+++ util/src/com/syrus/util/Codeable.java
@@ -0,0 +1,20 @@
+/*-
+ * $Id: Codeable.java,v 1.2.6.1 2006/03/17 08:06:45 bass Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.util;
+
+/**
+ * @version $Revision: 1.2.6.1 $, $Date: 2006/03/17 08:06:45 $
+ * @author $Author: bass $
+ * @author Tashoyan Arseniy Feliksovich
+ * @module util
+ * @deprecated removed in HEAD revision.
+ */
+@Deprecated
+public interface Codeable {
+	int getCode();
+}
