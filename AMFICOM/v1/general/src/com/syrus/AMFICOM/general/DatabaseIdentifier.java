f5d7c714b1f7757ae117e13cde6c892ec919ca4c
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2006-03-21 18:48:17 +0300

    MINOR: updated to use wasNull()/setNull(...) instead of getObject(...)/setObject(...)

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/general/DatabaseIdentifier.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/general/DatabaseIdentifier.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: DatabaseIdentifier.java,v 1.17 2005/09/20 12:52:32 bass Exp $
+ * $Id: DatabaseIdentifier.java,v 1.17.4.1 2006/03/21 15:48:17 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -16,12 +16,13 @@ import static com.syrus.AMFICOM.general.StorableObjectDatabase.SQL_NULL_TRIMMED;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 
 /**
  * DB Identifier wrapper.
  * Main purpose is to hide Identifier implementation and DB representation of it.
  *
- * @version $Revision: 1.17 $, $Date: 2005/09/20 12:52:32 $
+ * @version $Revision: 1.17.4.1 $, $Date: 2006/03/21 15:48:17 $
  * @author $Author: bass $
  * @module general
  */
@@ -34,22 +35,21 @@ public final class DatabaseIdentifier {
 			throws SQLException {
 		assert id != null : NON_NULL_EXPECTED;
 		if (id.isVoid()) {
-			preparedStatement.setObject(parameterIndex, null);
+			preparedStatement.setNull(parameterIndex, Types.BIGINT);
 		} else {
 			preparedStatement.setLong(parameterIndex, id.getIdentifierCode());
 		}
 	}
 
 	public static Identifier getIdentifier(final ResultSet resultSet, final String columnName) throws SQLException {
-		if (resultSet.getObject(columnName) == null) {
+		final long identifierCode = resultSet.getLong(columnName);
+		if (resultSet.wasNull()) {
 			return VOID_IDENTIFIER;
 		}
 
-		final long identifierCode = resultSet.getLong(columnName);
-
 		assert identifierCode != 0 : NON_ZERO_EXPECTED;
 
-		return new Identifier(identifierCode);
+		return Identifier.valueOf(identifierCode);
 	}
 
 	public static String toSQLString(final Identifier id) {
