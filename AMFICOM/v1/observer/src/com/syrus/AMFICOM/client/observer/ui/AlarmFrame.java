f923e9d42028b67aea819c0d36875e3d897b8df6
Stanislav Kholshin <kholshin@gmail.com>
2006-03-23 14:16:13 +0300

    undo last: setValues is correct as mismatches may be deleted by another client

--- AMFICOM/v1/observer/src/com/syrus/AMFICOM/client/observer/ui/AlarmFrame.java
+++ AMFICOM/v1/observer/src/com/syrus/AMFICOM/client/observer/ui/AlarmFrame.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.beans.PropertyChangeEvent;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -404,7 +403,7 @@ public class AlarmFrame extends JInternalFrame {
 		EquivalentCondition condition = new EquivalentCondition(ObjectEntities.LINEMISMATCHEVENT_CODE);
 		try {
 			Set<AbstractLineMismatchEvent> limeMismatchEvents = StorableObjectPool.getStorableObjectsByCondition(condition , true);
-			this.model.addObjects(new ArrayList<AbstractLineMismatchEvent>(limeMismatchEvents));
+			this.model.setValues(limeMismatchEvents);
 		} catch (ApplicationException e) {
 			Log.errorMessage(e);
 		}
