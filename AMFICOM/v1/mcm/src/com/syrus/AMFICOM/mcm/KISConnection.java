848033b11227ea6f60b7f9095a266dc38c7e08b4
Arseniy Tashoyan <tashoyan@gmail.com>
2006-03-24 14:19:03 +0300

    JAVADOC:

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISConnection.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISConnection.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: KISConnection.java,v 1.9.2.1 2006/03/01 15:50:14 arseniy Exp $
+ * $Id: KISConnection.java,v 1.9.2.2 2006/03/24 11:19:03 arseniy Exp $
  *
  * Copyright © 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -14,24 +14,73 @@ import com.syrus.AMFICOM.measurement.Measurement;
 import com.syrus.util.LRUMap.Retainable;
 
 /**
- * @version $Revision: 1.9.2.1 $, $Date: 2006/03/01 15:50:14 $
+ * @version $Revision: 1.9.2.2 $, $Date: 2006/03/24 11:19:03 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module mcm
  */
 public interface KISConnection extends Retainable {
 
+	/**
+	 * Проверить, установлено ли данное соединение.
+	 * 
+	 * @return <code>true</code>, если соединение установлено,
+	 *         <code>false</code>, если нет.
+	 */
 	boolean isEstablished();
 
+	/**
+	 * Установить соединение с КИС. Если соединение уже установлено - сбросить
+	 * его и установить заново.
+	 * 
+	 * @param kisConnectionTimeout
+	 *        Время, отведённое на попытки установить соединение.
+	 * @throws CommunicationException
+	 */
 	void establish(final long kisConnectionTimeout) throws CommunicationException;
 
+	/**
+	 * Установить соединение с КИС.
+	 * 
+	 * @param kisConnectionTimeout
+	 *        Время, отведённое на попытки установить соединение.
+	 * @param dropIfAlreadyEstablished
+	 *        Если соединение уже установлено - сбрасывать его и создавать
+	 *        заново, или же использовать существующее?
+	 * @throws CommunicationException
+	 */
 	void establish(final long kisConnectionTimeout, final boolean dropIfAlreadyEstablished) throws CommunicationException;
 
+	/**
+	 * Сбросить соединение.
+	 */
 	void drop();
 
+	/**
+	 * Передать данные на КИС через установленное соединение.
+	 * 
+	 * @param measurement
+	 *        Идентификатор измерения.
+	 * @param timewait
+	 *        Время ожидания при записи.
+	 * @throws ApplicationException
+	 */
 	void transmitMeasurement(final Measurement measurement, final long timewait) throws ApplicationException;
 
+	/**
+	 * Получить данные с КИС.
+	 * 
+	 * @param timewait
+	 *        Время ожидания данных.
+	 * @return Данные, полученные с КИС. Если никаких данных не получено - то
+	 *         <code>null</code>.
+	 * @throws CommunicationException
+	 */
 	KISReport receiveKISReport(final long timewait) throws CommunicationException;
 
+	/**
+	 * Получить идентификатор КИС, с которым должно устанавливаться данное
+	 * соединение.
+	 */
 	Identifier getKISId();
 }
--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/TCPKISConnection.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/TCPKISConnection.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: TCPKISConnection.java,v 1.27.2.3 2006/03/16 12:00:15 arseniy Exp $
+ * $Id: TCPKISConnection.java,v 1.27.2.4 2006/03/24 11:19:03 arseniy Exp $
  *
  * Copyright © 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -24,20 +24,46 @@ import com.syrus.util.ApplicationProperties;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.27.2.3 $, $Date: 2006/03/16 12:00:15 $
+ * @version $Revision: 1.27.2.4 $, $Date: 2006/03/24 11:19:03 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module mcm
  */
 final class TCPKISConnection implements KISConnection {
-	
+
+	/**
+	 * Особое значение для {@link #kisTCPSocket}, соответствующее разорванному
+	 * соединению с КИС.
+	 * 
+	 * @see #isEstablished()
+	 */
 	private static final int KIS_TCP_SOCKET_DISCONNECTED = -1;
 
+	/**
+	 * Идентификатор КИС, с которым должно устанавливаться данное соединение.
+	 */
 	private Identifier kisId;
+
+	/**
+	 * Имя машины КИС.
+	 */
 	private String kisHostName;
+
+	/**
+	 * Номер TCP-порта КИС.
+	 */
 	private short kisTCPPort;
+
+	/**
+	 * Описатель соединения с КИС. Если соединение не установлено, должен быть
+	 * равен {@link #KIS_TCP_SOCKET_DISCONNECTED}.
+	 */
 	private int kisTCPSocket;
-	
+
+	/**
+	 * Данные, полученные с КИС после завершения измерения. Это поле выставляет
+	 * метод {@link #receiveKISReportFromSocket(long)}.
+	 */
 	private KISReport kisReport;
 
 	static {
@@ -60,14 +86,23 @@ final class TCPKISConnection implements KISConnection {
 		this.kisTCPSocket = KIS_TCP_SOCKET_DISCONNECTED;
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public boolean isEstablished() {
 		return (this.kisTCPSocket != KIS_TCP_SOCKET_DISCONNECTED);
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public synchronized void establish(final long kisConnectionTimeout) throws CommunicationException {
 		this.establish(kisConnectionTimeout, true);
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public synchronized void establish(final long kisConnectionTimeout, final boolean dropIfAlreadyEstablished)
 			throws CommunicationException {
 		Log.debugMessage("Connecting to KIS '" + this.kisId + "' on host '" + this.kisHostName + "', port " + this.kisTCPPort,
@@ -107,6 +142,9 @@ final class TCPKISConnection implements KISConnection {
 		}
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public synchronized void drop() {
 		if (this.kisTCPSocket != KIS_TCP_SOCKET_DISCONNECTED) {
 			Log.debugMessage("Closing socket: " + this.kisTCPSocket, Log.DEBUGLEVEL09);
@@ -115,6 +153,9 @@ final class TCPKISConnection implements KISConnection {
 		}
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public synchronized void transmitMeasurement(final Measurement measurement, final long timewait) throws ApplicationException {
 		final Identifier measurementId  = measurement.getId();
 
@@ -144,6 +185,9 @@ final class TCPKISConnection implements KISConnection {
 		}
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public synchronized KISReport receiveKISReport(final long timewait) throws CommunicationException {
 		this.kisReport = null;
 		if (this.receiveKISReportFromSocket(timewait)) {
@@ -155,40 +199,59 @@ final class TCPKISConnection implements KISConnection {
 		throw new CommunicationException("TCPKISConnection.receiveKISReport | Cannot receive report");
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public Identifier getKISId() {
 		return this.kisId;
 	}
 
 	/**
-	 * Retain in LRUMap if this connection is established.
+	 * Реализация {@link com.syrus.util.LRUMap.Retainable#retain()}. Нужна для
+	 * хранения объектов соединения в {@link com.syrus.util.LRUMap}.
+	 * 
 	 * @see com.syrus.util.LRUMap.Retainable#retain()
+	 * @return <code>true</code>, если соединение установлено,
+	 *         <code>false</code> - в противном случае.
 	 */
 	public boolean retain() {
 		return this.isEstablished();
 	}
 
 	/**
-	 * Establishes connection with remote KIS.
-	 * NOTE: this method is responsible for correct work of method isEstablished() after its invocation;
-	 * in this TCP implementation it returns KIS_TCP_SOCKET_DISCONNECTED on error.
-	 * @see #isEstablished
-	 * @return socket file descriptor.
+	 * Установить соединение с КИС. Этот метод возвращает файловый описатель
+	 * соединения и, следовательно, отвечает за правильную работу метода
+	 * {@link #isEstablished()}. В случае невозмжности установить соединение,
+	 * он должен вернуть {@link #KIS_TCP_SOCKET_DISCONNECTED}.
+	 * 
+	 * @return Файловый описатель соединения при успешном выполнении, либо
+	 *         {@link #KIS_TCP_SOCKET_DISCONNECTED} при ошибке.
 	 */
 	public native int establishSocketConnection();
 
 	/**
-	 * Drop connection. ParameterSet field kisTCPSocket to KIS_TCP_SOCKET_DISCONNECTED.
+	 * Сбросить соединение. Этот метод должен выставить поле
+	 * {@link #kisTCPSocket} в {@link #KIS_TCP_SOCKET_DISCONNECTED},
+	 * обеспечивая тем самым правильную работу метода {@link #isEstablished()}.
 	 */
 	public native void dropSocketConnection();
 
 	/**
-	 * Transmits data to socket.
+	 * Передать данные на КИС через установленное соединение.
+	 * 
 	 * @param measurementId
+	 *        Идентификатор измерения.
 	 * @param measurementTypeCodename
+	 *        Кодовое имя типа измерения.
 	 * @param localAddress
+	 *        Строка локальной адресации КИС.
 	 * @param parameterTypeCodenames
+	 *        Кодовые имена типов параметров измерения.
 	 * @param parameterValues
-	 * @return true on success, false on failure.
+	 *        Величины параметров измерения.
+	 * @param timewait
+	 *        Время ожидания при записи.
+	 * @return
 	 */
 	public native boolean transmitMeasurementBySocket(String measurementId,
 			String measurementTypeCodename,
@@ -198,12 +261,14 @@ final class TCPKISConnection implements KISConnection {
 			long timewait);
 
 	/**
-	 * Receive report from socket. On success save the report into field kisReport
-	 * NOTE: if nothing received, this method also returns true, but saves null
-	 * into field kisReport
+	 * Получить данные с КИС. В случае успеха выставляет поле {@link #kisReport}
+	 * в значение, полученное с КИС. Если никаких данных не получено, выставляет
+	 * {@link #kisReport} в <code>null</code>.
 	 * 
 	 * @param timewait
-	 * @return true on success, false on failure.
+	 *        Время ожидания данных.
+	 * @return В случае успешного приёма - <code>true</code>, при ошибке -
+	 *         <code>false</code>.
 	 */
 	public native boolean receiveKISReportFromSocket(long timewait);
 }
