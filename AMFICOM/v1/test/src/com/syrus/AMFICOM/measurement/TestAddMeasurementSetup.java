132ca90a561a189d86de109a1e512a50f4c9cb93
Arseniy Tashoyan <tashoyan@gmail.com>
2006-03-27 13:39:55 +0400

    ADD: Parameter type FLAG_SMOOTH_FILTER MINOR: Search the first MeasurementPort on the PK7600 KIS MINOR: Default average count is 100

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestAddMeasurementSetup.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/measurement/TestAddMeasurementSetup.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: TestAddMeasurementSetup.java,v 1.1.2.2 2006/03/23 15:27:54 arseniy Exp $
+ * $Id: TestAddMeasurementSetup.java,v 1.1.2.3 2006/03/27 09:39:55 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -10,13 +10,15 @@ package com.syrus.AMFICOM.measurement;
 import static com.syrus.AMFICOM.general.ErrorMessages.ONLY_ONE_EXPECTED;
 import static com.syrus.AMFICOM.general.Identifier.VOID_IDENTIFIER;
 import static com.syrus.AMFICOM.general.ObjectEntities.MEASUREMENTPORT_CODE;
+import static com.syrus.AMFICOM.general.ObjectEntities.MEASUREMENTSETUP_CODE;
 import static com.syrus.AMFICOM.general.ObjectEntities.MONITOREDELEMENT_CODE;
 import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_DESCRIPTION;
 import static com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlCompoundConditionPackage.CompoundConditionSort.AND;
-import static com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlTypicalConditionPackage.OperationSort.OPERATION_EQUALS;
+import static com.syrus.AMFICOM.general.corba.IdlStorableObjectConditionPackage.IdlTypicalConditionPackage.OperationSort.OPERATION_SUBSTRING;
 import static com.syrus.AMFICOM.measurement.MeasurementPortTypeCodename.REFLECTOMETRY_PK7600;
 import static com.syrus.AMFICOM.measurement.MeasurementTypeCodename.REFLECTOMETRY;
 import static com.syrus.AMFICOM.reflectometry.ReflectometryParameterTypeCodename.AVERAGE_COUNT;
+import static com.syrus.AMFICOM.reflectometry.ReflectometryParameterTypeCodename.FLAG_SMOOTH_FILTER;
 import static com.syrus.AMFICOM.reflectometry.ReflectometryParameterTypeCodename.INDEX_OF_REFRACTION;
 import static com.syrus.AMFICOM.reflectometry.ReflectometryParameterTypeCodename.PULSE_WIDTH_M;
 import static com.syrus.AMFICOM.reflectometry.ReflectometryParameterTypeCodename.RESOLUTION;
@@ -46,7 +48,7 @@ import com.syrus.AMFICOM.measurement.ActionParameterTypeBinding.ParameterValueKi
 import com.syrus.util.ByteArray;
 
 /**
- * @version $Revision: 1.1.2.2 $, $Date: 2006/03/23 15:27:54 $
+ * @version $Revision: 1.1.2.3 $, $Date: 2006/03/27 09:39:55 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module test
@@ -59,7 +61,8 @@ public final class TestAddMeasurementSetup extends TestCase {
 
 	public static Test suite() {
 		final CORBACommonTest commonTest = new CORBACommonTest();
-		commonTest.addTestSuite(TestAddMeasurementSetup.class);
+		//commonTest.addTestSuite(TestAddMeasurementSetup.class);
+		commonTest.addTest(new TestAddMeasurementSetup("testRetrieve"));
 		return commonTest.createTestSetup();
 	}
 
@@ -69,17 +72,17 @@ public final class TestAddMeasurementSetup extends TestCase {
 		final MeasurementType measurementType = MeasurementType.valueOf(REFLECTOMETRY.stringValue());
 		final MeasurementPortType measurementPortType = MeasurementPortType.valueOf(REFLECTOMETRY_PK7600.stringValue());
 
-		final String measurementPortDescription = "MeasurementPort_1_KIS_1_Equipment_1";
+		final String measurementPortDescriptionSubstring = "MeasurementPort_1_";
 		final StorableObjectCondition measurementPortCondition = new CompoundCondition(new LinkedIdsCondition(measurementPortType,
 						MEASUREMENTPORT_CODE),
 				AND,
-				new TypicalCondition(measurementPortDescription,
-						OPERATION_EQUALS,
+				new TypicalCondition(measurementPortDescriptionSubstring,
+						OPERATION_SUBSTRING,
 						MEASUREMENTPORT_CODE,
 						COLUMN_DESCRIPTION));
 		final Set<MeasurementPort> measurementPorts = StorableObjectPool.getStorableObjectsByCondition(measurementPortCondition, true);
 		if (measurementPorts.isEmpty()) {
-			throw new ObjectNotFoundException("MeasurementPort " + measurementPortDescription + " of type " + measurementPortType.getCodename() + " not found");
+			throw new ObjectNotFoundException("MeasurementPort like " + measurementPortDescriptionSubstring + " of type " + measurementPortType.getCodename() + " not found");
 		}
 		assertTrue(ONLY_ONE_EXPECTED + ": " + measurementPorts, measurementPorts.size() == 1);
 		final MeasurementPort measurementPort = measurementPorts.iterator().next();
@@ -87,7 +90,7 @@ public final class TestAddMeasurementSetup extends TestCase {
 		final StorableObjectCondition monitoredElementCondition = new LinkedIdsCondition(measurementPort, MONITOREDELEMENT_CODE);
 		final Set<MonitoredElement> monitoredElements = StorableObjectPool.getStorableObjectsByCondition(monitoredElementCondition, true);
 		if (monitoredElements.isEmpty()) {
-			throw new ObjectNotFoundException("MonitoredElement for MeasurementPort " + measurementPortDescription + " not found");
+			throw new ObjectNotFoundException("MonitoredElement for MeasurementPort " + measurementPort.getDescription() + " not found");
 		}
 		assertTrue(ONLY_ONE_EXPECTED + ": " + monitoredElements, monitoredElements.size() == 1);
 		final MonitoredElement monitoredElement = monitoredElements.iterator().next();
@@ -118,22 +121,25 @@ public final class TestAddMeasurementSetup extends TestCase {
 							actionParameterIds.add(actionParameter.getId());
 						} else if (parameterTypeCodename.equals(INDEX_OF_REFRACTION.stringValue()) && actionParameterStringValue.equals("1.4682")) {
 							actionParameterIds.add(actionParameter.getId());
+						} else if (parameterTypeCodename.equals(FLAG_SMOOTH_FILTER.stringValue()) && actionParameterStringValue.equals("false")) {
+							actionParameterIds.add(actionParameter.getId());
 						}
 					}
 					break;
 				case CONTINUOUS:
+					final int averageCount = 100;
 					if (parameterTypeCodename.equals(AVERAGE_COUNT.stringValue())) {
 						stringBuffer.append(", existing values: ");
 						Identifier scansActionParameterId = null;
 						for (final ActionParameter actionParameter : actionParameters) {
 							final String actionParameterStringValue = actionParameter.stringValue();
 							stringBuffer.append(actionParameterStringValue + ", id: " + actionParameter.getId().getIdentifierCode());
-							if (actionParameterStringValue.equals("4000")) {
+							if (actionParameterStringValue.equals(Integer.toString(averageCount))) {
 								scansActionParameterId = actionParameter.getId();
 							}
 						}
 						if (scansActionParameterId == null) {
-							scansActionParameterId = ActionParameter.createInstance(creatorId, ByteArray.toByteArray(4000), actionParameterTypeBinding.getId()).getId();
+							scansActionParameterId = ActionParameter.createInstance(creatorId, ByteArray.toByteArray(averageCount), actionParameterTypeBinding.getId()).getId();
 						}
 						actionParameterIds.add(scansActionParameterId);
 					}
@@ -158,5 +164,48 @@ public final class TestAddMeasurementSetup extends TestCase {
 				actionTemplate.getMonitoredElementIds());
 
 		StorableObjectPool.flush(measurementSetup, creatorId, false);
+	}
+
+	public void testRetrieve() throws ApplicationException, IOException {
+		final MeasurementPortType measurementPortType = MeasurementPortType.valueOf(REFLECTOMETRY_PK7600.stringValue());
+
+		final String measurementPortDescriptionSubstring = "MeasurementPort_1_";
+		final StorableObjectCondition measurementPortCondition = new CompoundCondition(new LinkedIdsCondition(measurementPortType,
+						MEASUREMENTPORT_CODE),
+				AND,
+				new TypicalCondition(measurementPortDescriptionSubstring,
+						OPERATION_SUBSTRING,
+						MEASUREMENTPORT_CODE,
+						COLUMN_DESCRIPTION));
+		final Set<MeasurementPort> measurementPorts = StorableObjectPool.getStorableObjectsByCondition(measurementPortCondition, true);
+		if (measurementPorts.isEmpty()) {
+			throw new ObjectNotFoundException("MeasurementPort like " + measurementPortDescriptionSubstring + " of type " + measurementPortType.getCodename() + " not found");
+		}
+		assertTrue(ONLY_ONE_EXPECTED + ": " + measurementPorts, measurementPorts.size() == 1);
+		final MeasurementPort measurementPort = measurementPorts.iterator().next();
+
+		final StorableObjectCondition monitoredElementCondition = new LinkedIdsCondition(measurementPort, MONITOREDELEMENT_CODE);
+		final Set<MonitoredElement> monitoredElements = StorableObjectPool.getStorableObjectsByCondition(monitoredElementCondition, true);
+		if (monitoredElements.isEmpty()) {
+			throw new ObjectNotFoundException("MonitoredElement for MeasurementPort " + measurementPort.getDescription() + " not found");
+		}
+		assertTrue(ONLY_ONE_EXPECTED + ": " + monitoredElements, monitoredElements.size() == 1);
+		final MonitoredElement monitoredElement = monitoredElements.iterator().next();
+		System.out.println("MonitoredElement: " + monitoredElement.getName() + ", local address: " + monitoredElement.getLocalAddress());
+
+		final StorableObjectCondition measurementSetupCondition = new LinkedIdsCondition(monitoredElement, MEASUREMENTSETUP_CODE);
+		final Set<MeasurementSetup> measurementSetups = StorableObjectPool.getStorableObjectsByCondition(measurementSetupCondition, true);
+		if (measurementSetups.isEmpty()) {
+			throw new ObjectNotFoundException("MeasurementSetups for MonitoredElement " + monitoredElement.getName() + " not found");
+		}
+
+		for (final MeasurementSetup measurementSetup : measurementSetups) {
+			System.out.println("MeasurementSetup: " + measurementSetup.getDescription());
+			final ActionTemplate measurementTemplate = measurementSetup.getMeasurementTemplate();
+			final Set<ActionParameter> measurementParameters = measurementTemplate.getActionParameters();
+			for (final ActionParameter actionParameter : measurementParameters) {
+				System.out.println("Id: " + actionParameter.getId().getIdentifierCode() + ", '" + actionParameter.getTypeCodename() + "' == " + actionParameter.stringValue());
+			}
+		}
 	}
 }
