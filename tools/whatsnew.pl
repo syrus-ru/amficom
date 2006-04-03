36c9f48a3c527bd37d049b542d54035d9a5cefd2
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-03 19:49:36 +0400

    ADD: Perl script to do cvs -n up

---
+++ tools/whatsnew.pl
@@ -0,0 +1,148 @@
+#!/usr/bin/perl
+use strict;
+
+# Executes cvs -q -n up and prints (via cvs log and CVS/Entries)
+# authors and cvs log messages that are related to 'U' and 'P' records.
+#
+# (F) saa, 2005, 2006
+
+my $optA;
+my $cvsoptsStarted;
+my $cvsOptions;
+
+foreach (@ARGV) {
+    if ($cvsoptsStarted) {
+    	$cvsOptions .= " $_";
+    	next;
+    }
+    if ($_ eq '-A') { $optA=1; next; }
+    if ($_ eq '--') {
+        $cvsoptsStarted=1; next;
+    }
+    die "unknown option: '$_'";
+}
+
+$cvsOptions .= ' -A' if $optA;
+
+print "using CVS options: $cvsOptions\n" if $cvsOptions ne '';
+
+my $fileCount; # counter of modified files
+my $revCount;
+my %authors;
+my %messages;
+my @messages;
+
+sub wider($$$)
+{
+my $line = $_[0];
+my $len = $_[1] + 0;
+my $at = $_[2] + 0;
+while (length($line) < $len)
+{
+    my $at2 = $at;
+    if ($at2 == 2) { $at2 = length($line) % 2; }
+    if ($at2) { $line = ' ' . $line }
+    else { $line = $line . ' ' }
+}
+return $line;
+}
+
+my @cvsUpOut;
+foreach (`cvs -q -n up -d $cvsOptions`)
+{
+chomp;
+next if m#^[?] #; # such files are likely to be out of interest at all
+print "$_\n";
+next unless m#^[UP] (.+)#; # ignore C, R, M, A records
+push @cvsUpOut,$1;
+}
+
+# show progress bar on console
+if (scalar @cvsUpOut)
+{
+   print ' ', (' ' x @cvsUpOut), "]\r[";
+}
+
+foreach (@cvsUpOut)
+{
+print '.';
+$fileCount++;
+my $ffn = $_;
+my $n = $_;
+my $d = '.';
+if ($ffn =~ m#(.+)/(.+)#) {$n = $2; $d = $1;}
+
+# print "'$d' / '$n'\n";
+
+my $rev ='';
+if (!open FTMP, "< $d/CVS/Entries")
+{
+	print STDERR "\nWarning: Failed to open $d/CVS/Entries: $!\n";
+	next;
+}
+while(<FTMP>)
+{
+    next unless m#^/(.+)?/([\d.]+)/#;
+    next unless $1 eq $n;
+    $rev = $2;
+}
+close FTMP;
+# if rev eq '', it means that the file is new
+# die "Error: no Entries record found for $ffn in $d" if $rev eq '';
+# print "rev = $rev\n";
+
+my $revSpec = $rev eq '' ? '' : "-r$rev\::";
+
+open FCVSLOG, "cvs log -N $revSpec $ffn |";
+
+while (<FCVSLOG>)
+{
+    chomp;
+    last if $_ eq 'description:';
+}
+
+my $revSeparator = '----------------------------';
+my $lastSeparator = '=============================================================================';
+$_ = <FCVSLOG>;
+chomp;
+die "'$_' is ne '$revSeparator'" unless $_ eq $revSeparator or $_ eq $lastSeparator;
+
+while (<FCVSLOG>)
+{
+    chomp;
+    next if !m /^revision [\d.]+/;
+    $_ = <FCVSLOG>;
+    chomp;
+    next if !m /author: (\S+);/;
+    my $author = $1;
+    $authors{$author}++;
+    $revCount++;
+    while (<FCVSLOG>)
+    {
+        chomp;
+        last if $_ eq $revSeparator;
+        last if $_ eq $lastSeparator;
+        $_ = wider("$author ", 11, 0) .$_;
+        next if $messages{$_}++;
+        push @messages, $_;
+    }
+}
+
+close FCVSLOG;
+
+}
+print "\n" if $fileCount;
+
+if ($revCount)
+{
+#print "-----\n";
+print "Updated files: $fileCount\n";
+print "New revisions: $revCount\n";
+print "Authors: ", join(", ", sort keys %authors), "\n";
+print "Messages #/author/text:\n";
+print map {wider($messages{$_}, 2, 1) . " $_\n"} @messages;
+}
+else
+{
+print "No updates\n";
+}
