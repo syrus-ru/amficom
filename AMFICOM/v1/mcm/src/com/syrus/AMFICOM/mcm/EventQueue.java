21dd6d6601825f43e4633766ae269f5476c19a65
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-04 14:33:24 +0400

    ROLLBACK: (merged from SOD-1_207-COMPATIBILITY-PATCHES)

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/EventQueue.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/EventQueue.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: EventQueue.java,v 1.8 2006/03/19 13:10:58 arseniy Exp $
+ * $Id: EventQueue.java,v 1.9 2006/04/04 10:33:24 arseniy Exp $
  *
  * Copyright © 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,13 +8,14 @@
 
 package com.syrus.AMFICOM.mcm;
 
-import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.FINEST;
+import static java.util.logging.Level.*;
 
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.SystemException;
 
 import com.syrus.AMFICOM.eventv2.Event;
 import com.syrus.AMFICOM.eventv2.corba.IdlEvent;
@@ -22,12 +23,11 @@ import com.syrus.AMFICOM.general.BaseConnectionManager;
 import com.syrus.AMFICOM.general.CommunicationException;
 import com.syrus.AMFICOM.general.SleepButWorkThread;
 import com.syrus.AMFICOM.leserver.corba.EventServer;
-import com.syrus.AMFICOM.leserver.corba.EventServerPackage.IdlEventProcessingException;
 import com.syrus.util.ApplicationProperties;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.8 $, $Date: 2006/03/19 13:10:58 $
+ * @version $Revision: 1.9 $, $Date: 2006/04/04 10:33:24 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module mcm
@@ -35,9 +35,8 @@ import com.syrus.util.Log;
 final class EventQueue extends SleepButWorkThread {
 	/*	Error codes for method processFall()	*/
 	private static final int FALL_CODE_ESTABLISH_CONNECTION = 1;
-	private static final int FALL_CODE_TRANSMIT_EVENTS = 2;
 
-	private List<Event> eventEqueue;
+	private List<Event<?>> eventQueue;
 	private volatile boolean running;
 
 	public EventQueue() {
@@ -46,15 +45,16 @@ final class EventQueue extends SleepButWorkThread {
 
 		super.setName("EventQueue");
 
-		this.eventEqueue = Collections.synchronizedList(new LinkedList<Event>());
+		this.eventQueue = new LinkedList<Event<?>>();
 		this.running = true;
 	}
 
-	@SuppressWarnings("unused")
-	synchronized void addEvent(final Event event) throws EventQueueFullException {
-		Log.debugMessage("Event: " + event + " added to outbox", INFO);
-		this.eventEqueue.add(event);
-		this.notifyAll();
+	void addEvent(final Event<?> event) {
+		Log.debugMessage("Event: " + event + " is being added to outbox", FINEST);
+		synchronized (this.eventQueue) {
+			this.eventQueue.add(event);
+			this.eventQueue.notifyAll();
+		}
 	}
 
 	@Override
@@ -64,32 +64,43 @@ final class EventQueue extends SleepButWorkThread {
 
 		while (this.running) {
 
-			synchronized (this) {
-				while (this.eventEqueue.isEmpty()) {
+			synchronized (this.eventQueue) {
+				while (this.eventQueue.isEmpty() && this.running) {
 					try {
-						this.wait(10000);
-					} catch (InterruptedException ie) {
+						this.eventQueue.wait(10000);
+					} catch (final InterruptedException ie) {
 						Log.debugMessage(this.getName() + " -- interrupted", Log.DEBUGLEVEL07);
 					}
 				}
 			}
 
+			if (this.eventQueue.isEmpty()) {
+				continue;
+			}
+
+			IdlEvent idlEvents[] = null;
 			try {
 				final EventServer eventServer = connectionManager.getEventServerReference();
 
-				final IdlEvent[] idlEvents = this.createIdlEventArray(connectionManager);
+				idlEvents = this.createIdlEventArray(connectionManager);
+				final int length = idlEvents.length;
+				Log.debugMessage("Sending " + length + " event(s) to the event server", FINEST);
 				eventServer.receiveEvents(idlEvents);
-				this.eventEqueue.clear();
+				Log.debugMessage("Done sending " + length + " event(s) to the event server", FINEST);
 			} catch (final CommunicationException ce) {
-				Log.errorMessage(ce);
+				Log.debugMessage(ce, SEVERE);
 				super.fallCode = FALL_CODE_ESTABLISH_CONNECTION;
 				super.sleepCauseOfFall();
-			} catch (final IdlEventProcessingException epe) {
-				Log.errorMessage("Cannot transmit events -- " + epe.message);
-				super.fallCode = FALL_CODE_TRANSMIT_EVENTS;
+			} catch (final SystemException se) {
+				if (idlEvents != null) {
+					Log.debugMessage(idlEvents.length
+							+ " event(s) has(ve) just been dropped for the following reason:",
+							SEVERE);
+				}
+				Log.debugMessage(se, SEVERE);
+				super.fallCode = FALL_CODE_ESTABLISH_CONNECTION;
 				super.sleepCauseOfFall();
 			}
-
 		}
 	}
 
@@ -101,9 +112,6 @@ final class EventQueue extends SleepButWorkThread {
 			case FALL_CODE_ESTABLISH_CONNECTION:
 				Log.errorMessage("ERROR: Many errors during establishing connection. Чё делать - ума не приложу.");
 				break;
-			case FALL_CODE_TRANSMIT_EVENTS:
-				Log.errorMessage("ERROR: Many errors during transmit event. Чё делать - ума не приложу.");
-				break;
 			default:
 				Log.errorMessage("ERROR: Unknown error code: " + super.fallCode);
 		}
@@ -112,18 +120,21 @@ final class EventQueue extends SleepButWorkThread {
 	private IdlEvent[] createIdlEventArray(final BaseConnectionManager connectionManager) {
 		final ORB orb = connectionManager.getCORBAServer().getOrb();
 		final IdlEvent[] idlEvents;
-		synchronized (this.eventEqueue) {
-			idlEvents = new IdlEvent[this.eventEqueue.size()];
+		synchronized (this.eventQueue) {
+			idlEvents = new IdlEvent[this.eventQueue.size()];
 			int i = 0;
-			for (final Event<IdlEvent> event : this.eventEqueue) {
+			for (final Event<?> event : this.eventQueue) {
 				idlEvents[i++] = event.getIdlTransferable(orb);
 			}
+			this.eventQueue.clear();
 		}
 		return idlEvents;
 	}
 
-	synchronized void shutdown() {
+	void shutdown() {
 		this.running = false;
-		this.notifyAll();
+		synchronized (this.eventQueue) {
+			this.eventQueue.notifyAll();
+		}
 	}
 }
