5ec4952829f9d22ef3b4be89e15e5c196bacd19b
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-05 16:17:01 +0400

    MINOR: Narrowed visibility of method #getActionTemplate0() to default - there is no Actions outside of measurement package

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/Action.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/Action.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Action.java,v 1.43.2.13 2006/04/05 12:00:14 arseniy Exp $
+ * $Id: Action.java,v 1.43.2.14 2006/04/05 12:17:01 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -31,7 +31,7 @@ import com.syrus.AMFICOM.measurement.corba.IdlActionStatus;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.43.2.13 $, $Date: 2006/04/05 12:00:14 $
+ * @version $Revision: 1.43.2.14 $, $Date: 2006/04/05 12:17:01 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module measurement
@@ -159,7 +159,7 @@ public abstract class Action<R extends ActionResultParameter> extends StorableOb
 
 	public abstract ActionTemplate getActionTemplate() throws ApplicationException;
 
-	protected final <T extends Action> ActionTemplate<T> getActionTemplate0() throws ApplicationException {
+	final <T extends Action> ActionTemplate<T> getActionTemplate0() throws ApplicationException {
 		return StorableObjectPool.getStorableObject(this.actionTemplateId, true);
 	}
 
