591a8199f8003308373e0f94468825420907deb7
Alexey Stratonnikov <alexsaa@gmail.com>
2006-04-06 13:20:14 +0400

    FIXes

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/reflectometry/ReflectometryAnalysisCriteria.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/reflectometry/ReflectometryAnalysisCriteria.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ReflectometryAnalysisCriteria.java,v 1.1.2.4 2006/04/05 14:02:34 arseniy Exp $
+ * $Id: ReflectometryAnalysisCriteria.java,v 1.1.2.5 2006/04/06 09:20:14 saa Exp $
  * 
  * Copyright © 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -18,24 +18,36 @@ import com.syrus.io.DataFormatException;
 import com.syrus.util.Log;
 
 /**
- * Параметры анализа при рефлектометрических измерениях.
+ * Параметры анализа и эталона при рефлектометрических измерениях.
  * Состоит из:
  * <ul>
+ * <li> Параметров анализа (есть обязательно):
+ * <ul>
  * <li> {@link #getDadaraCriteria()} параметров анализа dadara
+ * </ul>
+ * <li> Эталона (может не быть, надо убедиться в его наличии
+ * методом {@link #hasEtalon()}):
+ * <ul>
+ * <li> {@link #getDadaraEtalon()} объекта-эталона (используется в dadara)
+ * <li> {@link #getReflectogrammaEtalon()} эталонной рефлектограммы (BellcoreStructure)
+ * </ul>
  * </ul>
  * <p>
  * На данный момент modifier-методы не поддерживаются.
  * <p>
+ * @todo Переименовать, т.к. теперь содержит и эталон
+ * 
  * @author saa
- * @author $Author: arseniy $
- * @version $Revision: 1.1.2.4 $, $Date: 2006/04/05 14:02:34 $
+ * @author $Author: saa $
+ * @version $Revision: 1.1.2.5 $, $Date: 2006/04/06 09:20:14 $
  * @module
  */
-public class ReflectometryAnalysisCriteria implements ReflectometryEtalon {
+public class ReflectometryAnalysisCriteria {
 	private ActionTemplate<Analysis> analysisTemplate;
-	private byte[] dadaraCriteria;
-	private byte[] dadaraEtalon;
-	private byte[] reflectogrammaEtalon;
+	private byte[] dadaraCriteria = null;
+	private boolean hasEtalon = false;
+	private byte[] dadaraEtalon = null;
+	private byte[] reflectogrammaEtalon = null;
 
 	/**
 	 * @param analysisTemplate
@@ -64,14 +76,21 @@ public class ReflectometryAnalysisCriteria implements ReflectometryEtalon {
 				Log.errorMessage("Unknown codename: " + parameterTypeCodename);
 			}
 		}
+
 		if (this.dadaraCriteria == null) {
 			throw new DataFormatException("No criteria"); // no criteria found
 		}
-		if (this.dadaraEtalon == null) {
-			throw new DataFormatException("No dadara etalon"); // no dadara etalon
-		}
-		if (this.reflectogrammaEtalon == null) {
-			throw new DataFormatException("No reflectogramma etalon"); // no reflectogramma etalon
+		this.hasEtalon = this.dadaraEtalon != null || this.reflectogrammaEtalon != null;
+		if (this.hasEtalon) {
+			if (this.dadaraEtalon == null) {
+				throw new DataFormatException("No dadara etalon"); // no dadara etalon
+			}
+			if (this.reflectogrammaEtalon == null) {
+				throw new DataFormatException("No reflectogramma etalon"); // no reflectogramma etalon
+			}
+		} else {
+			this.dadaraEtalon = null;
+			this.reflectogrammaEtalon = null;
 		}
 	}
 
@@ -91,9 +110,20 @@ public class ReflectometryAnalysisCriteria implements ReflectometryEtalon {
 	}
 
 	/**
+	 * @return true, если в этих параметра есть эталон
+	 */
+	public boolean hasEtalon() {
+		return this.hasEtalon;
+	}
+
+	/**
 	 * @return массив байт для восстановления объекта-эталона dadara
+	 * @throws IllegalStateException эталона нет (hasEtalon() == false)
 	 */
 	public byte[] getDadaraEtalon() {
+		if (!hasEtalon()) {
+			throw new IllegalStateException();
+		}
 		return this.dadaraEtalon.clone();
 	}
 
@@ -107,8 +137,12 @@ public class ReflectometryAnalysisCriteria implements ReflectometryEtalon {
 
 	/**
 	 * @return массив байт для восстановления рефлектограммы BellcoreStructure
+	 * @throws IllegalStateException эталона нет (hasEtalon() == false)
 	 */
 	public byte[] getReflectogrammaEtalon() {
+		if (!hasEtalon()) {
+			throw new IllegalStateException();
+		}
 		return this.reflectogrammaEtalon.clone();
 	}
 
@@ -116,7 +150,7 @@ public class ReflectometryAnalysisCriteria implements ReflectometryEtalon {
 	 * не реализован
 	 */
 	public void setReflectogrammaEtalon(byte[] reflectogrammaEtalon) {
-		this.reflectogrammaEtalon = reflectogrammaEtalon;
+//		this.reflectogrammaEtalon = reflectogrammaEtalon;
 		throw new UnsupportedOperationException(); // @todo: implement
 	}
 }
