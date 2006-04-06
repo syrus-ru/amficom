49bb7074961649ca58b3ecdcb8ae8ed05abdb754
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-06 12:08:31 +0400

    ADD: serialVersionUID

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISException.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/KISException.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: KISException.java,v 1.1.2.1 2006/04/06 08:06:13 arseniy Exp $
+ * $Id: KISException.java,v 1.1.2.2 2006/04/06 08:08:31 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,12 +8,13 @@
 package com.syrus.AMFICOM.mcm;
 
 /**
- * @version $Revision: 1.1.2.1 $, $Date: 2006/04/06 08:06:13 $
+ * @version $Revision: 1.1.2.2 $, $Date: 2006/04/06 08:08:31 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module mcm
  */
 final class KISException extends Exception {
+	private static final long serialVersionUID = 5629070237704703388L;
 
 	public KISException(final String message) {
 		super(message);
