1dcc796b287f563c02031ad345a019360faaea94
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-07 14:55:23 +0400

    ADD: Method #getProperKISs()

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/ActionType.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/ActionType.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ActionType.java,v 1.26.2.6 2006/04/07 09:07:34 arseniy Exp $
+ * $Id: ActionType.java,v 1.26.2.7 2006/04/07 10:55:23 arseniy Exp $
  *
  * Copyright © 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,20 +8,28 @@
 package com.syrus.AMFICOM.measurement;
 
 import static com.syrus.AMFICOM.general.ErrorMessages.NOT_IMPLEMENTED;
+import static com.syrus.AMFICOM.general.ObjectEntities.ACTIONPARAMETERTYPEBINDING_CODE;
+import static com.syrus.AMFICOM.general.ObjectEntities.KIS_CODE;
+import static com.syrus.AMFICOM.general.ObjectEntities.MEASUREMENTPORT_CODE;
 
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
+import com.syrus.AMFICOM.general.ApplicationException;
 import com.syrus.AMFICOM.general.Describable;
 import com.syrus.AMFICOM.general.Identifiable;
 import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.LinkedIdsCondition;
+import com.syrus.AMFICOM.general.StorableObjectCondition;
+import com.syrus.AMFICOM.general.StorableObjectPool;
 import com.syrus.AMFICOM.general.StorableObjectType;
 import com.syrus.AMFICOM.general.StorableObjectVersion;
 import com.syrus.AMFICOM.measurement.corba.IdlActionType;
 
 /**
- * @version $Revision: 1.26.2.6 $, $Date: 2006/04/07 09:07:34 $
+ * @version $Revision: 1.26.2.7 $, $Date: 2006/04/07 10:55:23 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module measurement
@@ -69,6 +77,28 @@ public abstract class ActionType extends StorableObjectType implements Describab
 
 	public final void setName(final String name) {
 		throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+	}
+
+	/**
+	 * Найти все КИС, на которых можно проводить действие данного типа.
+	 * 
+	 * @return Список подходящих КИС.
+	 * @throws ApplicationException
+	 */
+	public final Set<KIS> getProperKISs() throws ApplicationException {
+		final StorableObjectCondition bindingCondition = new LinkedIdsCondition(super.id, ACTIONPARAMETERTYPEBINDING_CODE);
+		final Set<ActionParameterTypeBinding> bindings = StorableObjectPool.getStorableObjectsByCondition(bindingCondition, true);
+		final Set<MeasurementPortType> measurementPortTypes = new HashSet<MeasurementPortType>();
+		for (final ActionParameterTypeBinding binding : bindings) {
+			measurementPortTypes.add(binding.getMeasurementPortType());
+		}
+
+		final StorableObjectCondition measurementPortCondition = new LinkedIdsCondition(measurementPortTypes, MEASUREMENTPORT_CODE);
+		final Set<MeasurementPort> measurementPorts = StorableObjectPool.getStorableObjectsByCondition(measurementPortCondition, true);
+
+		final StorableObjectCondition kisCondition = new LinkedIdsCondition(measurementPorts, KIS_CODE);
+		final Set<KIS> kiss = StorableObjectPool.getStorableObjectsByCondition(kisCondition, true);
+		return kiss;
 	}
 
 	/**
