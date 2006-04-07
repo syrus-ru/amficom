c3d89340e2f88ea3c25f009e7f9973f8d2ed6e3d
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2006-04-07 17:33:53 +0400

    MINOR: deps fixed, dependency on kunststoff removed.

--- AMFICOM/v1/scheduler/scheduler.sh
+++ AMFICOM/v1/scheduler/scheduler.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
 
-JAVA=$JAVA_HOME/bin/java
+JAVA=${JAVA_HOME}/bin/java
 LIB=../lib
 EXTLIB=../../extlib
 
-EXTCLASSPATH=$EXTLIB/kunststoff.jar:$EXTLIB/trove.jar
+if [ "${TROVE4J_HOME}" ]
+then
+	EXTCLASSPATH="${TROVE4J_HOME}/lib/trove.jar"
+else
+	EXTCLASSPATH=${EXTLIB}/trove.jar
+fi
 
-XMLCLASSPATH=$LIB/generalxml.jar:\
-$XMLBEANS_HOME/lib/xbean.jar:\
-$XMLBEANS_HOME/lib/jsr173_api.jar
+XMLCLASSPATH=${LIB}/generalxml.jar:\
+${XMLBEANS_HOME}/lib/xbean.jar:\
+${XMLBEANS_HOME}/lib/jsr173_api.jar
 
-APPCLASSPATH=$LIB/scheduler.jar:\
-$LIB/general.jar:\
-$LIB/administration.jar:\
-$LIB/configuration.jar:\
-$LIB/measurement.jar:\
-$LIB/event.jar:\
-$LIB/resource.jar:\
-$LIB/map.jar:\
-$LIB/scheme.jar:\
-$LIB/mapview.jar:\
-$LIB/filter.jar:\
-$LIB/reflectometry.jar:\
-$LIB/csbridge.jar:\
-$LIB/commonclient.jar:\
-$LIB/filterclient.jar:\
-$LIB/leserver_interface.jar:\
-$LIB/util.jar:\
-$LIB/extensions.jar:\
-$LIB/extensions_schemas.jar:\
-$LIB/resources_schemas.jar
+APPCLASSPATH=${LIB}/scheduler.jar:\
+${LIB}/scheduler_schemas.jar:\
+${LIB}/general.jar:\
+${LIB}/administration.jar:\
+${LIB}/configuration.jar:\
+${LIB}/measurement.jar:\
+${LIB}/event.jar:\
+${LIB}/resource.jar:\
+${LIB}/map.jar:\
+${LIB}/scheme.jar:\
+${LIB}/mapview.jar:\
+${LIB}/filter.jar:\
+${LIB}/reflectometry.jar:\
+${LIB}/csbridge.jar:\
+${LIB}/commonclient.jar:\
+${LIB}/filterclient.jar:\
+${LIB}/leserver_interface.jar:\
+${LIB}/util.jar:\
+${LIB}/extensions.jar:\
+${LIB}/extensions_schemas.jar:\
+${LIB}/resources_schemas.jar
 
-$JAVA -Xmx256m -ea -client -classpath $APPCLASSPATH:${XMLCLASSPATH}:$EXTCLASSPATH com.syrus.AMFICOM.Client.Schedule.Schedule
-
+$JAVA -Xmx256m -ea -client -classpath ${APPCLASSPATH}:${XMLCLASSPATH}:${EXTCLASSPATH} com.syrus.AMFICOM.Client.Schedule.Schedule
