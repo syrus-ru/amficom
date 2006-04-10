83e560d674fa26ff6e1f08c841cc8f20a153a6ff
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-10 14:14:53 +0400

    BUGFIX: description can be null

--- AMFICOM/v1/database/schema/report/object/reporttemplate.sql
+++ AMFICOM/v1/database/schema/report/object/reporttemplate.sql
@@ -1,4 +1,4 @@
---$Id: reporttemplate.sql,v 1.5 2005/10/06 11:32:35 max Exp $
+--$Id: reporttemplate.sql,v 1.6 2006/04/10 10:14:53 arseniy Exp $
 
 CREATE TABLE ReportTemplate (
  id NUMBER(19),
@@ -8,12 +8,12 @@ CREATE TABLE ReportTemplate (
  modifier_id NOT NULL,
  version NUMBER(19) NOT NULL,
 --
- name 					VARCHAR2(32 CHAR) 	NOT NULL,
- description 			VARCHAR2(256 CHAR)	NOT NULL,
- sheet_size				NUMBER(3) 			NOT NULL,
- orientation			NUMBER(1)			NOT NULL,
- margin_size			NUMBER(10)			NOT NULL,
- destination_module		VARCHAR2(64 CHAR)	NOT NULL,
+ name VARCHAR2(32 CHAR) NOT NULL,
+ description VARCHAR2(256 CHAR),
+ sheet_size NUMBER(3) NOT NULL,
+ orientation NUMBER(1) NOT NULL,
+ margin_size NUMBER(10) NOT NULL,
+ destination_module VARCHAR2(64 CHAR) NOT NULL,
 --
  CONSTRAINT reporttemplate_pk PRIMARY KEY (id),
  CONSTRAINT reporttemplate_creator_fk FOREIGN KEY (creator_id)
@@ -22,6 +22,6 @@ CREATE TABLE ReportTemplate (
   REFERENCES SystemUser (id) ON DELETE CASCADE
 );
 
-COMMENT ON TABLE ReportTemplate IS '$Id: reporttemplate.sql,v 1.5 2005/10/06 11:32:35 max Exp $';
+COMMENT ON TABLE ReportTemplate IS '$Id: reporttemplate.sql,v 1.6 2006/04/10 10:14:53 arseniy Exp $';
 
 CREATE SEQUENCE ReportTemplate_Seq ORDER;
