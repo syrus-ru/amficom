8db25e710963f77a0d66b1018884e60745107b38
Stanislav Kholshin <kholshin@gmail.com>
2006-04-11 15:08:55 +0400

    add: constants

--- AMFICOM/v1/observer/src/com/syrus/AMFICOM/resource/ObserverResourceKeys.java
+++ AMFICOM/v1/observer/src/com/syrus/AMFICOM/resource/ObserverResourceKeys.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ObserverResourceKeys.java,v 1.1 2005/10/26 13:51:56 bass Exp $
+ * $Id: ObserverResourceKeys.java,v 1.2 2006/04/11 11:08:55 stas Exp $
  *
  * Copyright ¿ 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,27 +9,31 @@
 package com.syrus.AMFICOM.resource;
 
 /**
- * @author $Author: bass $
- * @version $Revision: 1.1 $, $Date: 2005/10/26 13:51:56 $
+ * @author $Author: stas $
+ * @version $Revision: 1.2 $, $Date: 2006/04/11 11:08:55 $
  * @module surveyclient_v1
  */
 
 public interface ObserverResourceKeys {
-	public static final String APPLICATION_TITLE = "application.title"; //$NON-NLS-1$
+	String APPLICATION_TITLE = "application.title"; //$NON-NLS-1$
 	
-	public static final String ROOT = "observer.root"; //$NON-NLS-1$
+	String ROOT = "observer.root"; //$NON-NLS-1$
 	
-	public static final String ICON_OBSERVE = "images/main/observe_mini.gif";
-	public static final String ICON_MAP_OPEN = "images/main/map_mini.gif";
-	public static final String ICON_SCHEME_OPEN = "images/main/schematics_mini.gif";
-	public static final String ICON_ARCHIVE = "images/archive_mini.gif";
-	public static final String ICON_ALARM = "images/alarm_bell_red.gif";
-	public static final String ICON_RESULT = "images/result.gif";
-	public static final String ICON_SCHEDULER = "images/main/scheduling_mini.gif";
-	public static final String ICON_ANALYSIS = "images/main/analyse_mini.gif";
-	public static final String ICON_SURVEY = "images/main/survey_mini.gif";
-	public static final String ICON_EVALUATION = "images/main/evaluation_mini.gif";
-	public static final String ICON_PREDICTION = "images/main/prognosis_mini.gif";
+	String ICON_OBSERVE = "images/main/observe_mini.gif";
+	String ICON_MAP_OPEN = "images/main/map_mini.gif";
+	String ICON_SCHEME_OPEN = "images/main/schematics_mini.gif";
+	String ICON_ARCHIVE = "images/archive_mini.gif";
+	String ICON_ALARM = "images/alarm_bell_red.gif";
+	String ICON_RESULT = "images/result.gif";
+	String ICON_SCHEDULER = "images/main/scheduling_mini.gif";
+	String ICON_ANALYSIS = "images/main/analyse_mini.gif";
+	String ICON_SURVEY = "images/main/survey_mini.gif";
+	String ICON_EVALUATION = "images/main/evaluation_mini.gif";
+	String ICON_PREDICTION = "images/main/prognosis_mini.gif";
 	
-	
+	String	FRAME_ALARM	= "Window.alarmFrame"; //$NON-NLS-1$
+	String	FRAME_ALARM_POPUP	= "Window.alarmPopupFrame"; //$NON-NLS-1$
+	String	FRAME_TREE = "Window.treeFrame"; //$NON-NLS-1$
+	String	FRAME_RESULT = "Window.resultFrame"; //$NON-NLS-1$
+	String	FRAME_RESULT_PARAMETERS = "Window.resultFrame.parameters"; //$NON-NLS-1$
 }
