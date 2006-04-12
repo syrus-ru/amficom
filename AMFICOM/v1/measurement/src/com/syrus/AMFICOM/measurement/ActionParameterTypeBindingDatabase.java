d1a302a30596886685c07fae1b7547e85268c116
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-12 17:03:18 +0400

    MAJOR: Moved helper enum ActionTypeKind to class ActionType MAJOR: Use helper classes ActionTypeKindDatabase and ActionTypeKindWrapper

--- AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/ActionParameterTypeBindingDatabase.java
+++ AMFICOM/v1/measurement/src/com/syrus/AMFICOM/measurement/ActionParameterTypeBindingDatabase.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ActionParameterTypeBindingDatabase.java,v 1.1.2.5 2006/03/06 12:19:59 arseniy Exp $
+ * $Id: ActionParameterTypeBindingDatabase.java,v 1.1.2.6 2006/04/12 13:03:18 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -7,11 +7,7 @@
  */
 package com.syrus.AMFICOM.measurement;
 
-import static com.syrus.AMFICOM.general.Identifier.VOID_IDENTIFIER;
 import static com.syrus.AMFICOM.general.ObjectEntities.ACTIONPARAMETERTYPEBINDING_CODE;
-import static com.syrus.AMFICOM.general.ObjectEntities.ANALYSIS_TYPE_CODE;
-import static com.syrus.AMFICOM.general.ObjectEntities.MEASUREMENT_TYPE_CODE;
-import static com.syrus.AMFICOM.general.ObjectEntities.MODELING_TYPE_CODE;
 import static com.syrus.AMFICOM.general.StorableObjectVersion.ILLEGAL_VERSION;
 import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_CREATED;
 import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_CREATOR_ID;
@@ -19,11 +15,7 @@ import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_ID;
 import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_MODIFIED;
 import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_MODIFIER_ID;
 import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_VERSION;
-import static com.syrus.AMFICOM.measurement.ActionParameterTypeBindingWrapper.COLUMN_ACTION_TYPE_KIND_CODE;
-import static com.syrus.AMFICOM.measurement.ActionParameterTypeBindingWrapper.COLUMN_ANALYSIS_TYPE_ID;
 import static com.syrus.AMFICOM.measurement.ActionParameterTypeBindingWrapper.COLUMN_MEASUREMENT_PORT_TYPE_ID;
-import static com.syrus.AMFICOM.measurement.ActionParameterTypeBindingWrapper.COLUMN_MEASUREMENT_TYPE_ID;
-import static com.syrus.AMFICOM.measurement.ActionParameterTypeBindingWrapper.COLUMN_MODELING_TYPE_ID;
 import static com.syrus.AMFICOM.measurement.ActionParameterTypeBindingWrapper.COLUMN_PARAMETER_TYPE_ID;
 import static com.syrus.AMFICOM.measurement.ActionParameterTypeBindingWrapper.COLUMN_PARAMETER_VALUE_KIND;
 
@@ -38,45 +30,16 @@ import com.syrus.AMFICOM.general.RetrieveObjectException;
 import com.syrus.AMFICOM.general.StorableObjectDatabase;
 import com.syrus.AMFICOM.general.StorableObjectVersion;
 import com.syrus.AMFICOM.measurement.ActionParameterTypeBinding.ParameterValueKind;
+import com.syrus.AMFICOM.measurement.ActionTypeDatabase.ActionTypeKindDatabase;
 import com.syrus.util.database.DatabaseDate;
 
 /**
- * @version $Revision: 1.1.2.5 $, $Date: 2006/03/06 12:19:59 $
+ * @version $Revision: 1.1.2.6 $, $Date: 2006/04/12 13:03:18 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module measurement
  */
 public final class ActionParameterTypeBindingDatabase extends StorableObjectDatabase<ActionParameterTypeBinding> {
-	private static enum ActionTypeKind {
-		MEASUREMENT_TYPE,
-		ANALYSIS_TYPE,
-		MODELING_TYPE;
-
-		private static final ActionTypeKind VALUES[] = values();
-
-		public static ActionTypeKind valueOf(final int code) {
-			return VALUES[code];
-		}
-
-		public static ActionTypeKind valueOf(final Identifier actionTypeId) {
-			switch (actionTypeId.getMajor()) {
-				case MEASUREMENT_TYPE_CODE:
-					return MEASUREMENT_TYPE;
-				case ANALYSIS_TYPE_CODE:
-					return ANALYSIS_TYPE;
-				case MODELING_TYPE_CODE:
-					return MODELING_TYPE;
-				default:
-					throw new IllegalArgumentException("Illegal identifier: " + actionTypeId);
-			}
-		}
-
-		@Override
-		public String toString() {
-			return this.name() + "(" + Integer.toString(this.ordinal()) + ")";
-		}
-	}
-
 	private static String columns;
 	private static String updateMultipleSQLValues;
 
@@ -90,10 +53,7 @@ public final class ActionParameterTypeBindingDatabase extends StorableObjectData
 		if (columns == null) {
 			columns = COLUMN_PARAMETER_VALUE_KIND + COMMA
 				+ COLUMN_PARAMETER_TYPE_ID + COMMA
-				+ COLUMN_ACTION_TYPE_KIND_CODE + COMMA
-				+ COLUMN_MEASUREMENT_TYPE_ID + COMMA
-				+ COLUMN_ANALYSIS_TYPE_ID + COMMA
-				+ COLUMN_MODELING_TYPE_ID + COMMA
+				+ ActionTypeKindDatabase.getInstance().getColumns() + COMMA
 				+ COLUMN_MEASUREMENT_PORT_TYPE_ID;
 		}
 		return columns;
@@ -104,10 +64,7 @@ public final class ActionParameterTypeBindingDatabase extends StorableObjectData
 		if (updateMultipleSQLValues == null) {
 			updateMultipleSQLValues = QUESTION + COMMA
 				+ QUESTION + COMMA
-				+ QUESTION + COMMA
-				+ QUESTION + COMMA
-				+ QUESTION + COMMA
-				+ QUESTION + COMMA
+				+ ActionTypeKindDatabase.getInstance().getUpdateMultipleSQLValues() + COMMA
 				+ QUESTION;
     	}
 		return updateMultipleSQLValues;
@@ -115,31 +72,9 @@ public final class ActionParameterTypeBindingDatabase extends StorableObjectData
 
 	@Override
 	protected String getUpdateSingleSQLValuesTmpl(final ActionParameterTypeBinding storableObject) throws IllegalDataException {
-		final ActionTypeKind actionTypeKind = ActionTypeKind.valueOf(storableObject.getActionTypeId());
-		String actionTypeSql;
-		switch (actionTypeKind) {
-			case MEASUREMENT_TYPE:
-				actionTypeSql = DatabaseIdentifier.toSQLString(storableObject.getActionTypeId()) + COMMA
-						+ SQL_NULL + COMMA
-						+ SQL_NULL;
-				break;
-			case ANALYSIS_TYPE:
-				actionTypeSql = SQL_NULL + COMMA
-						+ DatabaseIdentifier.toSQLString(storableObject.getActionTypeId()) + COMMA
-						+ SQL_NULL;
-				break;
-			case MODELING_TYPE:
-				actionTypeSql = SQL_NULL + COMMA
-						+ SQL_NULL + COMMA
-						+ DatabaseIdentifier.toSQLString(storableObject.getActionTypeId());
-				break;
-			default:
-				throw new IllegalArgumentException("Unknown action type kind: " + actionTypeKind);
-		}
 		final String sql = Integer.toString(storableObject.getParameterValueKind().ordinal()) + COMMA
 				+ DatabaseIdentifier.toSQLString(storableObject.getParameterTypeId()) + COMMA
-				+ Integer.toString(actionTypeKind.ordinal()) + COMMA
-				+ actionTypeSql + COMMA
+				+ ActionTypeKindDatabase.getInstance().getUpdateSingleSQLValues(storableObject.getActionTypeId()) + COMMA
 				+ DatabaseIdentifier.toSQLString(storableObject.getMeasurementPortTypeId());
 		return sql;
 	}
@@ -150,32 +85,10 @@ public final class ActionParameterTypeBindingDatabase extends StorableObjectData
 			int startParameterNumber) throws IllegalDataException, SQLException {
 		preparedStatement.setInt(++startParameterNumber, storableObject.getParameterValueKind().ordinal());
 		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getParameterTypeId());
-
-		final Identifier actionTypeId = storableObject.getActionTypeId();
-		final ActionTypeKind actionTypeKind = ActionTypeKind.valueOf(actionTypeId);
-		preparedStatement.setInt(++startParameterNumber, actionTypeKind.ordinal());
-		switch (actionTypeKind) {
-			case MEASUREMENT_TYPE:
-				DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, actionTypeId);
-				DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, VOID_IDENTIFIER);
-				DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, VOID_IDENTIFIER);
-				break;
-			case ANALYSIS_TYPE:
-				DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, VOID_IDENTIFIER);
-				DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, actionTypeId);
-				DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, VOID_IDENTIFIER);
-				break;
-			case MODELING_TYPE:
-				DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, VOID_IDENTIFIER);
-				DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, VOID_IDENTIFIER);
-				DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, actionTypeId);
-				break;
-			default:
-				throw new IllegalArgumentException("Unknown action type kind: " + actionTypeKind);
-		}
-
+		startParameterNumber = ActionTypeKindDatabase.getInstance().setEntityForPreparedStatement(storableObject.getActionTypeId(),
+				preparedStatement,
+				startParameterNumber);
 		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, storableObject.getMeasurementPortTypeId());
-
 		return startParameterNumber;
 	}
 
@@ -192,22 +105,7 @@ public final class ActionParameterTypeBindingDatabase extends StorableObjectData
 						null)
 					: storableObject;
 
-
-		final ActionTypeKind actionTypeKind = ActionTypeKind.valueOf(resultSet.getInt(COLUMN_ACTION_TYPE_KIND_CODE));
-		Identifier actionTypeId;
-		switch (actionTypeKind) {
-			case MEASUREMENT_TYPE:
-				actionTypeId = DatabaseIdentifier.getIdentifier(resultSet, COLUMN_MEASUREMENT_TYPE_ID);
-				break;
-			case ANALYSIS_TYPE:
-				actionTypeId = DatabaseIdentifier.getIdentifier(resultSet, COLUMN_ANALYSIS_TYPE_ID);
-				break;
-			case MODELING_TYPE:
-				actionTypeId = DatabaseIdentifier.getIdentifier(resultSet, COLUMN_MODELING_TYPE_ID);
-				break;
-			default:
-				throw new IllegalArgumentException("Unknown action type kind: " + actionTypeKind);
-		}
+		final Identifier actionTypeId = ActionTypeKindDatabase.getInstance().getActionTypeId(resultSet);
 		actionParameterTypeBinding.setAttributes(DatabaseDate.fromQuerySubString(resultSet, COLUMN_CREATED),
 				DatabaseDate.fromQuerySubString(resultSet, COLUMN_MODIFIED),
 				DatabaseIdentifier.getIdentifier(resultSet, COLUMN_CREATOR_ID),
@@ -219,5 +117,4 @@ public final class ActionParameterTypeBindingDatabase extends StorableObjectData
 				DatabaseIdentifier.getIdentifier(resultSet, COLUMN_MEASUREMENT_PORT_TYPE_ID));
 		return actionParameterTypeBinding;
 	}
-
 }
