74d1d534b0b079f943eee3a6d033dfc89985df8b
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-12 17:50:39 +0400

    ADD: Field measurement_port_type_id

--- AMFICOM/v1/database/schema/measurement/object/measurementsetup.sql
+++ AMFICOM/v1/database/schema/measurement/object/measurementsetup.sql
@@ -1,4 +1,4 @@
--- $Id: measurementsetup.sql,v 1.9.2.3 2006/03/22 17:50:12 arseniy Exp $
+-- $Id: measurementsetup.sql,v 1.9.2.4 2006/04/12 13:50:39 arseniy Exp $
 
 CREATE TABLE MeasurementSetup (
  id NUMBER(19),
@@ -8,6 +8,7 @@ CREATE TABLE MeasurementSetup (
  modifier_id NOT NULL,
  version NUMBER(19) NOT NULL,
 --
+ measurement_port_type_id NOT NULL,
  measurement_template_id NOT NULL,
  analysis_template_id,
  description VARCHAR2(256 CHAR),
@@ -18,6 +19,8 @@ CREATE TABLE MeasurementSetup (
  CONSTRAINT ms_modifier_fk FOREIGN KEY (modifier_id)
   REFERENCES SystemUser (id) ON DELETE CASCADE,
 --
+ CONSTRAINT ms_mpt_fk FOREIGN KEY (measurement_port_type_id)
+  REFERENCES MeasurementPortType (id) ON DELETE CASCADE,
  CONSTRAINT ms_mtlp_fk FOREIGN KEY (measurement_template_id)
   REFERENCES ActionTemplate (id) ON DELETE CASCADE,
  CONSTRAINT ms_atlp_fk FOREIGN KEY (analysis_template_id)
