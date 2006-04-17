6dae688e8e3ba9238aee91f97f05a8ae72fb2dcd
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-17 13:42:40 +0400

    MAJOR: Constraints only on pulse width in nanoseconds

--- AMFICOM/v1/reflectometry/src/com/syrus/AMFICOM/reflectometry/ReflectometryCharacteristicTypeCodename.java
+++ AMFICOM/v1/reflectometry/src/com/syrus/AMFICOM/reflectometry/ReflectometryCharacteristicTypeCodename.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: ReflectometryCharacteristicTypeCodename.java,v 1.1.2.1 2006/04/14 14:45:45 arseniy Exp $
+ * $Id: ReflectometryCharacteristicTypeCodename.java,v 1.1.2.2 2006/04/17 09:42:40 arseniy Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -10,15 +10,15 @@ package com.syrus.AMFICOM.reflectometry;
 import com.syrus.util.Codename;
 
 /**
- * @version $Revision: 1.1.2.1 $, $Date: 2006/04/14 14:45:45 $
+ * @version $Revision: 1.1.2.2 $, $Date: 2006/04/17 09:42:40 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module reflectometry
  */
 public enum ReflectometryCharacteristicTypeCodename implements Codename {
 	POINTS_MAX_NUMBER("points_max_number"),
-	LOW_RES_MIN_PULSE_WIDTH("low_res_min_pulse_width"),
-	HIGH_RES_MAX_PULSE_WIDTH("high_res_max_pulse_width");
+	LOW_RES_MIN_PULSE_WIDTH_NS("low_res_min_pulse_width_ns"),
+	HIGH_RES_MAX_PULSE_WIDTH_NS("high_res_max_pulse_width_ns");
 
 	private String codename;
 
