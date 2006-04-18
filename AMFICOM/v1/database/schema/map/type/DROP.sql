56e35b2fd121a97b75eb7c307d62ec806f810fe2
cvs-fast-export <cvs-fast-export>
2006-04-18 10:37:03 +0000

    Synthetic commit for incomplete tag BEFORE-CHANGES-2006-04

--- AMFICOM/v1/database/schema/map/type/DROP.sql
+++
@@ -1,9 +0,0 @@
--- $Id: DROP.sql,v 1.5 2005/11/07 20:34:04 bass Exp $
-
-DROP TABLE SiteNodeType;
-DROP TABLE PhysicalLinkType;
-DROP TABLE MapLibrary;
-
-DROP SEQUENCE SiteNodeType_Seq;
-DROP SEQUENCE PhysicalLinkType_Seq;
-DROP SEQUENCE MapLibrary_Seq;
