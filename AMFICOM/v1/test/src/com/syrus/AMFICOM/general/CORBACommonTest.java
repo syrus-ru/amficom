5499b138b69717d9a0df14288d5a9b6bec6eaaf5
Alexey Stratonnikov <alexsaa@gmail.com>
2006-04-18 10:42:57 +0400

    FIX: use Assert.fail() instead of System.exit()

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/general/CORBACommonTest.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/general/CORBACommonTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CORBACommonTest.java,v 1.7 2006/02/17 09:09:58 bob Exp $
+ * $Id: CORBACommonTest.java,v 1.8 2006/04/18 06:42:57 saa Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,13 +7,15 @@
  */
 package com.syrus.AMFICOM.general;
 
+import junit.framework.Assert;
+
 import com.syrus.AMFICOM.general.ClientSessionEnvironment.SessionKind;
 import com.syrus.util.Log;
 
 
 /**
- * @version $Revision: 1.7 $, $Date: 2006/02/17 09:09:58 $
- * @author $Author: bob $
+ * @version $Revision: 1.8 $, $Date: 2006/04/18 06:42:57 $
+ * @author $Author: saa $
  * @module test
  */
 public class CORBACommonTest extends CommonTest {
@@ -36,7 +38,7 @@ public class CORBACommonTest extends CommonTest {
 			ClientSessionEnvironment.getInstance().login("sys", "sys", new Identifier("Domain_1"));
 		} catch (ApplicationException ae) {
 			Log.errorMessage(ae);
-			System.exit(0);
+			Assert.fail(ae.getMessage());
 		}
 	}
 
@@ -45,7 +47,7 @@ public class CORBACommonTest extends CommonTest {
 			ClientSessionEnvironment.getInstance().logout();
 		} catch (ApplicationException ae) {
 			Log.errorMessage(ae);
-			System.exit(0);
+			Assert.fail(ae.getMessage());
 		}
 	}
 }
