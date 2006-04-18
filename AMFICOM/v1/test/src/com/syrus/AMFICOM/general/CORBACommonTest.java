2973e165bd90431772f45575afc9b9305bda39fd
Alexey Stratonnikov <alexsaa@gmail.com>
2006-04-18 14:29:54 +0400

    COSMETIC: import

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/general/CORBACommonTest.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/general/CORBACommonTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CORBACommonTest.java,v 1.6.2.2 2006/04/18 06:40:22 saa Exp $
+ * $Id: CORBACommonTest.java,v 1.6.2.3 2006/04/18 10:29:54 saa Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -8,14 +8,13 @@
 package com.syrus.AMFICOM.general;
 
 import junit.framework.Assert;
-import junit.framework.TestCase;
 
 import com.syrus.AMFICOM.general.ClientSessionEnvironment.SessionKind;
 import com.syrus.util.Log;
 
 
 /**
- * @version $Revision: 1.6.2.2 $, $Date: 2006/04/18 06:40:22 $
+ * @version $Revision: 1.6.2.3 $, $Date: 2006/04/18 10:29:54 $
  * @author $Author: saa $
  * @module test
  */
