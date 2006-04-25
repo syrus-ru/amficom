b2fb279ee9718d420ccfdce11991cbbcf409f4d0
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-25 11:36:28 +0400

    MAJOR: Added equipmenttype.sql COSMETIC: Debug output

--- AMFICOM/v1/database/schema/configuration/type/CREATE.sql
+++ AMFICOM/v1/database/schema/configuration/type/CREATE.sql
@@ -1,7 +1,19 @@
--- $Id: CREATE.sql,v 1.14 2005/09/29 09:04:00 arseniy Exp $
+-- $Id: CREATE.sql,v 1.15 2006/04/25 07:36:28 arseniy Exp $
 
+PROMPT 01. Creating table EquipmentType...;
+@@equipmenttype;
+
+PROMPT 02. Creating table PortType...;
 @@porttype;
+
+PROMPT 03. Creating table LinkType...;
 @@linktype;
+
+PROMPT 04. Creating table CableLinkType...;
 @@cablelinktype;
+
+PROMPT 05. Creating table CableThreadType...;
 @@cablethreadtype;
+
+PROMPT 06. Creating table TransmissionPathType...;
 @@transmissionpathtype;
--- AMFICOM/v1/database/schema/configuration/type/DROP.sql
+++ AMFICOM/v1/database/schema/configuration/type/DROP.sql
@@ -1,13 +1,25 @@
--- $Id: DROP.sql,v 1.12 2005/09/29 09:04:00 arseniy Exp $
+-- $Id: DROP.sql,v 1.13 2006/04/25 07:36:28 arseniy Exp $
 
-DROP TABLE transmissionpathtype;
-DROP TABLE cablethreadtype;
-DROP TABLE cablelinktype;
-DROP TABLE linktype;
-DROP TABLE porttype;
+PROMPT 06. Dropping table TransmissionPathType...;
+DROP TABLE TransmissionPathType;
+DROP SEQUENCE TransmissionPathType_seq;
 
-DROP SEQUENCE transmissionpathtype_seq;
-DROP SEQUENCE cablethreadtype_seq;
-DROP SEQUENCE cablelinktype_seq;
-DROP SEQUENCE linktype_seq;
-DROP SEQUENCE porttype_seq;
+PROMPT 05. Dropping table CableThreadType...;
+DROP TABLE CableThreadType;
+DROP SEQUENCE CableThreadType_seq;
+
+PROMPT 04. Dropping table CableLinkType...;
+DROP TABLE CableLinkType;
+DROP SEQUENCE CableLinkType_seq;
+
+PROMPT 03. Dropping table LinkType...;
+DROP TABLE LinkType;
+DROP SEQUENCE LinkType_seq;
+
+PROMPT 02. Dropping table PortType...;
+DROP TABLE PortType;
+DROP SEQUENCE PortType_seq;
+
+PROMPT 01. Dropping table EquipmentType...;
+DROP TABLE EquipmentType;
+DROP SEQUENCE EquipmentType_seq;
