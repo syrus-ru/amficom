e00f3cc821b1f8f6b1bfeba125c42dc7b3cc3478
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-25 11:35:02 +0400

    MINOR: Debug output

--- AMFICOM/v1/database/schema/configuration/object/DROP.sql
+++ AMFICOM/v1/database/schema/configuration/object/DROP.sql
@@ -1,15 +1,25 @@
--- $Id: DROP.sql,v 1.17 2005/09/28 11:03:01 arseniy Exp $
+-- $Id: DROP.sql,v 1.18 2006/04/25 07:35:02 arseniy Exp $
 
-DROP TABLE Transmissionpath;
-DROP TABLE Port;
-DROP TABLE Equipment;
-DROP TABLE ProtoEquipment;
-DROP TABLE CableLink;
-DROP TABLE Link;
-
+PROMPT 06. Dropping table TransmissionPath...;
+DROP TABLE TransmissionPath;
 DROP SEQUENCE TransmissionPath_seq;
+
+PROMPT 05. Dropping table Port...;
+DROP TABLE Port;
 DROP SEQUENCE Port_seq;
+
+PROMPT 04. Dropping table Equipment...;
+DROP TABLE Equipment;
 DROP SEQUENCE Equipment_seq;
+
+PROMPT 03. Dropping table ProtoEquipment...;
+DROP TABLE ProtoEquipment;
 DROP SEQUENCE ProtoEquipment_seq;
+
+PROMPT 02. Dropping table CableLink...;
+DROP TABLE CableLink;
 DROP SEQUENCE CableLink_Seq;
+
+PROMPT 01. Dropping table Link...;
+DROP TABLE Link;
 DROP SEQUENCE Link_Seq;
