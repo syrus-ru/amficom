917ca18d9c19c904816cf1cc025b185c53db6118
Arseniy Tashoyan <tashoyan@gmail.com>
2006-04-25 13:58:46 +0400

    MAJOR: Narrowed visibility of #oneTimeSetUp() and #oneTimeTearDown()

--- AMFICOM/v1/test/src/com/syrus/AMFICOM/general/CORBACommonTest.java
+++ AMFICOM/v1/test/src/com/syrus/AMFICOM/general/CORBACommonTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CORBACommonTest.java,v 1.8 2006/04/18 06:42:57 saa Exp $
+ * $Id: CORBACommonTest.java,v 1.9 2006/04/25 09:58:46 arseniy Exp $
  * 
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -14,28 +14,29 @@ import com.syrus.util.Log;
 
 
 /**
- * @version $Revision: 1.8 $, $Date: 2006/04/18 06:42:57 $
- * @author $Author: saa $
+ * @version $Revision: 1.9 $, $Date: 2006/04/25 09:58:46 $
+ * @author $Author: arseniy $
  * @module test
  */
 public class CORBACommonTest extends CommonTest {
 
 	@Override
-	public void oneTimeSetUp() {
+	void oneTimeSetUp() {
 		super.oneTimeSetUp();
 		login();
 	}
 
 	@Override
-	public void oneTimeTearDown() {
+	void oneTimeTearDown() {
 		logout();
 		super.oneTimeTearDown();
 	}
 
 	private static void login() {
 		try {
-			ClientSessionEnvironment.createInstance(SessionKind.ALL, null);
-			ClientSessionEnvironment.getInstance().login("sys", "sys", new Identifier("Domain_1"));
+			ClientSessionEnvironment.createInstance(SessionKind.MEASUREMENT, null);
+			final Identifier domainId = LoginManager.getAvailableDomains().iterator().next().getId();
+			ClientSessionEnvironment.getInstance().login("sys", "sys", domainId);
 		} catch (ApplicationException ae) {
 			Log.errorMessage(ae);
 			Assert.fail(ae.getMessage());
