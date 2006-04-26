92717286e3f3ff11752ca96c92d6ff22aadffb91
Stanislav Kholshin <kholshin@gmail.com>
2006-04-26 17:00:20 +0400

    fix: more precise sheetSizes

--- AMFICOM/v1/report/src/com/syrus/AMFICOM/report/SheetSize.java
+++ AMFICOM/v1/report/src/com/syrus/AMFICOM/report/SheetSize.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: SheetSize.java,v 1.2 2006/04/25 10:58:08 stas Exp $
+ * $Id: SheetSize.java,v 1.3 2006/04/26 13:00:20 stas Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -12,16 +12,16 @@ import com.syrus.AMFICOM.resource.IntDimension;
 /**
  * @author Maxim Selivanov
  * @author $Author: stas $
- * @version $Revision: 1.2 $, $Date: 2006/04/25 10:58:08 $
+ * @version $Revision: 1.3 $, $Date: 2006/04/26 13:00:20 $
  * @module report
- * A4 is 11?" x 8?" multiply it on 96 dpi resolution
+ * A4 is 297mm x 210mm multiply it on 96 dpi resolution and 2.54 mm per inch
  */
 public enum SheetSize {
-	A0(new IntDimension (3168, 4512)),
-	A1(new IntDimension (3168, 2256)),
-	A2(new IntDimension (1584, 2256)),
-	A3(new IntDimension (1584, 1128)),
-	A4(new IntDimension (792, 1128));
+	A0(new IntDimension (3168, 4488)),
+	A1(new IntDimension (3168, 2244)),
+	A2(new IntDimension (1584, 2244)),
+	A3(new IntDimension (1584, 1122)),
+	A4(new IntDimension (792, 1122));
 	
 	private IntDimension size;
 	
