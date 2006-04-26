9a1de63721a39b1abc3e85b371064ec1460c5eea
Alexey Stratonnikov <alexsaa@gmail.com>
2006-04-26 16:47:55 +0400

    FIX: processing of target, now accepts -A and -r rev options; ADD: -i option for immediate print

--- tools/whatsnew.pl
+++ tools/whatsnew.pl
@@ -6,25 +6,16 @@ use strict;
 #
 # (F) saa, 2005, 2006
 
-my $optA;
-my $cvsoptsStarted;
-my $cvsOptions;
-
+my $targetTag = '';
+my $interactive = 0;
 foreach (@ARGV) {
-    if ($cvsoptsStarted) {
-    	$cvsOptions .= " $_";
-    	next;
-    }
-    if ($_ eq '-A') { $optA=1; next; }
-    if ($_ eq '--') {
-        $cvsoptsStarted=1; next;
-    }
+    if ($_ eq '-i') { $interactive=1; next; }
+    if ($_ eq '-A') { $targetTag='HEAD'; next; }
+    if ($_ =~ /^\-r(\w+)$/) { $targetTag=$1; next; }
     die "unknown option: '$_'";
 }
 
-$cvsOptions .= ' -A' if $optA;
-
-print "using CVS options: $cvsOptions\n" if $cvsOptions ne '';
+#print "using CVS options: $cvsOptions\n" if $cvsOptions ne '';
 
 my $fileCount; # counter of modified files
 my $revCount;
@@ -34,96 +25,105 @@ my @messages;
 
 sub wider($$$)
 {
-my $line = $_[0];
-my $len = $_[1] + 0;
-my $at = $_[2] + 0;
-while (length($line) < $len)
-{
-    my $at2 = $at;
-    if ($at2 == 2) { $at2 = length($line) % 2; }
-    if ($at2) { $line = ' ' . $line }
-    else { $line = $line . ' ' }
-}
-return $line;
+	my $line = $_[0];
+	my $len = $_[1] + 0;
+	my $at = $_[2] + 0;
+	while (length($line) < $len)
+	{
+		my $at2 = $at;
+		if ($at2 == 2) { $at2 = length($line) % 2; }
+		if ($at2) { $line = ' ' . $line }
+		else { $line = $line . ' ' }
+	}
+	return $line;
 }
 
+my $targetOpt = $targetTag eq '' ? '' : "-r $targetTag";
 my @cvsUpOut;
-foreach (`cvs -q -n up -d $cvsOptions`)
+foreach (`cvs -q -n up -d $targetOpt`)
 {
-chomp;
-next if m#^[?] #; # such files are likely to be out of interest at all
-print "$_\n";
-next unless m#^[UP] (.+)#; # ignore C, R, M, A records
-push @cvsUpOut,$1;
+	chomp;
+	next if m#^[?] #; # such files are likely to be out of interest at all
+	print "$_\n";
+	next unless m#^[UP] (.+)#; # ignore C, R, M, A records
+	push @cvsUpOut,$1;
 }
 
-# show progress bar on console
-if (scalar @cvsUpOut)
+if ($interactive)
 {
-   print ' ', (' ' x @cvsUpOut), "]\r[";
+	print "-----\n";
+} else
+{
+	# show progress bar on console
+	if (scalar @cvsUpOut)
+	{
+	   print ' ', (' ' x @cvsUpOut), "]\r[";
+	}
 }
 
 foreach (@cvsUpOut)
 {
-print '.';
-$fileCount++;
-my $ffn = $_;
-my $n = $_;
-my $d = '.';
-if ($ffn =~ m#(.+)/(.+)#) {$n = $2; $d = $1;}
+	print '.' unless $interactive;
+	$fileCount++;
+	my $ffn = $_;
+	my $n = $_;
+	my $d = '.';
+	if ($ffn =~ m#(.+)/(.+)#) {$n = $2; $d = $1;}
 
-# print "'$d' / '$n'\n";
+	# print "'$d' / '$n'\n";
 
-my $workingRev = '';
-my $repositRev = '';
+	my $workingRev = '';
+	my $repositRev = '';
+	open FTMP, "cvs status $ffn 2>nul |";
+	while(<FTMP>)
+	{
+		$workingRev = $1 if m/^\s*Working revision\:\s*([\d.]+)/;
+		$repositRev = $1 if m/^\s*Repository revision\:\s*([\d.]+)/;
+	}
+	close FTMP;
 
-open FTMP, "cvs status $ffn |";
-while(<FTMP>)
-{
-    $workingRev = $1 if m/^\s*Working revision\:\s*([\d.]+)/;
-    $repositRev = $1 if m/^\s*Repository revision\:\s*([\d.]+)/;
-}
-close FTMP;
+	$repositRev = $targetTag if $targetTag ne '';
 
-my $revSpec = $workingRev eq '' ? '' : "-r$workingRev\::$repositRev";
-# print "revSpec = $revSpec\n";
+	my $revSpec = $workingRev eq '' ? '' : "-r$workingRev::$repositRev";
+	# print "revSpec = $revSpec\n";
 
-open FCVSLOG, "cvs log -N $revSpec $ffn |";
+	open FCVSLOG, "cvs log -N $revSpec $ffn |";
 
-while (<FCVSLOG>)
-{
-    chomp;
-    last if $_ eq 'description:';
-}
+	while (<FCVSLOG>)
+	{
+		chomp;
+		last if $_ eq 'description:';
+	}
 
-my $revSeparator = '----------------------------';
-my $lastSeparator = '=============================================================================';
-$_ = <FCVSLOG>;
-chomp;
-die "'$_' is ne '$revSeparator'" unless $_ eq $revSeparator or $_ eq $lastSeparator;
+	my $revSeparator = '----------------------------';
+	my $lastSeparator = '=============================================================================';
+	$_ = <FCVSLOG>;
+	chomp;
+	die "'$_' is ne '$revSeparator'" unless $_ eq $revSeparator or $_ eq $lastSeparator;
 
-while (<FCVSLOG>)
-{
-    chomp;
-    next if !m /^revision [\d.]+/;
-    $_ = <FCVSLOG>;
-    chomp;
-    next if !m /author: (\S+);/;
-    my $author = $1;
-    $authors{$author}++;
-    $revCount++;
-    while (<FCVSLOG>)
-    {
-        chomp;
-        last if $_ eq $revSeparator;
-        last if $_ eq $lastSeparator;
-        $_ = wider("$author ", 11, 0) .$_;
-        next if $messages{$_}++;
-        push @messages, $_;
-    }
-}
+	while (<FCVSLOG>)
+	{
+		chomp;
+		next if !m /^revision [\d.]+/;
+		$_ = <FCVSLOG>;
+		chomp;
+		next if !m /author: (\S+);/;
+		my $author = $1;
+		$authors{$author}++;
+		$revCount++;
+		while (<FCVSLOG>)
+		{
+			chomp;
+			last if $_ eq $revSeparator;
+			last if $_ eq $lastSeparator;
+			$_ = wider("$author ", 11, 0) .$_;
+			next if $messages{$_}++;
+			push @messages, $_;
+			print " * $_\n" if $interactive;
+		}
+	}
 
-close FCVSLOG;
+	close FCVSLOG;
 
 }
 print "\n" if $fileCount;
