f5525e67ed5957ba2a8fbf1c9b01fa2ff130a321
Alexey Stratonnikov <alexsaa@gmail.com>
2006-05-24 16:07:37 +0400

    *** empty log message ***

---
+++ util/test/com/syrus/util/logging/TestLoggerPerformance.java
@@ -0,0 +1,68 @@
+/*-
+ * $Id: TestLoggerPerformance.java,v 1.1 2006/05/24 12:07:37 saa Exp $
+ * 
+ * Copyright ¿ 2006 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
+package com.syrus.util.logging;
+
+import java.util.logging.Level;
+
+import junit.framework.TestCase;
+
+import com.syrus.util.DefaultLogger;
+import com.syrus.util.Log;
+
+/**
+ * This is not an actual test. This is a measuring tool only.
+ * 
+ * @author saa
+ * @author $Author: saa $
+ * @version $Revision: 1.1 $, $Date: 2006/05/24 12:07:37 $
+ * @module util/test
+ */
+public class TestLoggerPerformance extends TestCase {
+	private long measureDtNanos() {
+		long t0 = System.nanoTime();
+		int count = 0;
+		while (System.nanoTime() - t0 < 100000000L && count < 100000) {
+			Log.debugMessage("Hello, log", Level.INFO);
+			count++;
+		}
+		return (System.nanoTime() - t0) / count;
+	}
+
+	private double measureDtMillis() {
+		return measureDtNanos() / 1000 / 1e3;
+	}
+
+	private String measureAverages() {
+		return "dt: " + measureDtMillis() + ", " + measureDtMillis()
+				+ ", " + measureDtMillis() + ", " + measureDtMillis()
+				+ ", " + measureDtMillis() + ", " + measureDtMillis() + " ms";
+	}
+
+	private void measureAtConditions(String allowLevel, String stackTrace) {
+		System.setProperty("amficom.logging.AllowLevelOutput", allowLevel);
+		System.setProperty("amficom.logging.StackTraceDataSource", stackTrace);
+		Log.setLogger(new DefaultLogger());
+		Log.debugMessage("Hello", Level.INFO);
+		System.out.printf("allowLevel: %6s;  STDS: %10s | %s%n",
+				allowLevel,
+				stackTrace,
+				measureAverages());
+	}
+
+	public void test() {
+		System.setProperty("amficom.logging.LogDebugLevel", "10");
+		System.setProperty("amficom.logging.EchoDebug", "false");
+		measureAtConditions("false", "none");
+		measureAtConditions("true", "none");
+		measureAtConditions("false", "throwable");
+		measureAtConditions("false", "thread");
+		System.out.println("... Sorry, I wrote a lot of logs, " +
+				"Please delete that junk logfiles.");
+	}
+}
