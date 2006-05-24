1209b2ac6a20b4279418e122f79390a57f37d047
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2006-05-24 14:43:52 +0400

    MINOR: +#setLevel(int)

--- util/src/com/syrus/util/Logger.java
+++ util/src/com/syrus/util/Logger.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: Logger.java,v 1.7 2005/11/10 15:47:46 bass Exp $
+ * $Id: Logger.java,v 1.8 2006/05/24 10:43:52 bass Exp $
  *
  * Copyright ¿ 2004-2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -11,9 +11,10 @@ package com.syrus.util;
 import java.util.logging.Level;
 
 /**
- * @version $Revision: 1.7 $, $Date: 2005/11/10 15:47:46 $
- * @author $Author: bass $
  * @author Tashoyan Arseniy Feliksovich
+ * @author Andrew ``Bass'' Shcheglov
+ * @author $Author: bass $
+ * @version $Revision: 1.8 $, $Date: 2006/05/24 10:43:52 $
  * @module util
  */
 public interface Logger {
@@ -39,4 +40,16 @@ public interface Logger {
 	 * @see java.util.logging.Logger#setLevel(Level)
 	 */
 	void setLevel(final Level newLevel);
+
+	/**
+	 * @param reverseIntValue AMFICOM-standard debug level, ranging from 1
+	 *        to 10. Higher <code>reverseIntValue</code>s mean more verbose
+	 *        output. This behaviour is opposite to that of
+	 *        {@link Level Level}<code>.</code>{@link Level#intValue() intValue()},
+	 *        that's the reason why parameter is named
+	 *        <code>reverseIntValue</code>.
+	 * @see #setLevel(Level)
+	 * @see java.util.logging.Logger#setLevel(Level)
+	 */
+	void setLevel(final int reverseIntValue);
 }
