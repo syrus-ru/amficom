0d87dd0f15a02e5e4aac5a9e64fe2d2057eaecc2
Arseniy Tashoyan <tashoyan@gmail.com>
2006-06-05 17:49:42 +0400

    ADD: Method #valueOf() with cachiing MAJOR: Removed costructor of IdlSessionKey

--- AMFICOM/v1/general/src/com/syrus/AMFICOM/security/SessionKey.java
+++ AMFICOM/v1/general/src/com/syrus/AMFICOM/security/SessionKey.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: SessionKey.java,v 1.21 2006/04/26 12:31:18 bass Exp $
+ * $Id: SessionKey.java,v 1.22 2006/06/05 13:49:42 arseniy Exp $
  *
  * Copyright © 2004-2006 Syrus Systems.
  * Научно-технический центр.
@@ -13,16 +13,18 @@ import java.io.Serializable;
 import org.omg.CORBA.ORB;
 
 import com.syrus.AMFICOM.security.corba.IdlSessionKey;
+import com.syrus.util.LRUMap;
+import com.syrus.util.LRUMap.Retainable;
 import com.syrus.util.transport.idl.IdlTransferableObject;
 
 /**
- * @version $Revision: 1.21 $, $Date: 2006/04/26 12:31:18 $
- * @author $Author: bass $
+ * @version $Revision: 1.22 $, $Date: 2006/06/05 13:49:42 $
+ * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module general
  */
-public final class SessionKey implements IdlTransferableObject<IdlSessionKey>, Serializable, SessionData {
-	private static final long serialVersionUID = -2395582378323499911L;
+public final class SessionKey implements IdlTransferableObject<IdlSessionKey>, Serializable, SessionData, Retainable {
+	private static final long serialVersionUID = 8663656100029518421L;
 
 	public static final SessionKey VOID_SESSION_KEY = new SessionKey("");
 
@@ -30,16 +32,22 @@ public final class SessionKey implements IdlTransferableObject<IdlSessionKey>, S
 
 	private IdlSessionKey idlSessionKey;
 
+
 	/**
-	 * Only for generator and database driver
+	 * Закэшированные ключи сессий.
+	 * 
+	 * @see #valueOf(IdlSessionKey)
+	 */
+	private static final LRUMap<String, SessionKey> sessionKeyCache = new LRUMap<String, SessionKey>(20);
+
+
+	/**
+	 * Только для создания новых ключей и восстановления существующих из БД.
+	 * 
 	 * @param sessionCode
 	 */
 	SessionKey(final String sessionCode) {
 		this.sessionCode = sessionCode;
-	}
-
-	public SessionKey(final IdlSessionKey skT) {
-		this.sessionCode = skT.sessionCode;
 	}
 
 	/**
@@ -68,6 +76,29 @@ public final class SessionKey implements IdlTransferableObject<IdlSessionKey>, S
 
 	public SessionKey getSessionKey() {
 		return this;
+	}
+
+	/**
+	 * Использует объекты, закэшированные в {@link #sessionKeyCache}.
+	 * 
+	 * @param idlSessionKey
+	 * @return Объект {@link SessionKey}, соответствующий данному
+	 *         {@code idlSessionKey}.
+	 */
+	public static SessionKey valueOf(final IdlSessionKey idlSessionKey) {
+		final String sessionCode = idlSessionKey.sessionCode;
+
+		SessionKey sessionKey = sessionKeyCache.get(sessionCode);
+		if (sessionKey == null) {
+			sessionKey = new SessionKey(sessionCode);
+			sessionKeyCache.put(sessionCode, sessionKey);
+		}
+		
+		return sessionKey;
+	}
+
+	public boolean retain() {
+		return false;
 	}
 
 	/*-********************************************************************
