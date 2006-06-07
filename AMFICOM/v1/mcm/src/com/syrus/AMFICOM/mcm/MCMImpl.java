914158b287670169371b1f28131ae448635f41ad
Arseniy Tashoyan <tashoyan@gmail.com>
2006-06-07 14:28:35 +0400

    COMPATIBILITY: Method SessionKey#valueOf()

--- AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/MCMImpl.java
+++ AMFICOM/v1/mcm/src/com/syrus/AMFICOM/mcm/MCMImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MCMImpl.java,v 1.12.2.1 2006/03/16 12:04:35 arseniy Exp $
+ * $Id: MCMImpl.java,v 1.12.2.2 2006/06/07 10:28:35 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -21,7 +21,7 @@ import com.syrus.AMFICOM.security.corba.IdlSessionKey;
 import com.syrus.util.Log;
 
 /**
- * @version $Revision: 1.12.2.1 $, $Date: 2006/03/16 12:04:35 $
+ * @version $Revision: 1.12.2.2 $, $Date: 2006/06/07 10:28:35 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module mcm
@@ -38,7 +38,7 @@ final class MCMImpl extends ServerCore implements MCMOperations {
 		final int length = testIdlIds.length;
 		assert length != 0 : ErrorMessages.NON_EMPTY_EXPECTED;
 
-		super.validateLogin(new SessionKey(idlSessionKey));
+		super.validateLogin(SessionKey.valueOf(idlSessionKey));
 
 		final Set<Identifier> testIds = Identifier.fromTransferables(testIdlIds);
 		Log.debugMessage("Request to start " + testIdlIds.length + " test(s): " + Identifier.toString(testIds), Log.DEBUGLEVEL07);
@@ -52,7 +52,7 @@ final class MCMImpl extends ServerCore implements MCMOperations {
 		final int length = testIdlIds.length;
 		assert length != 0 : ErrorMessages.NON_EMPTY_EXPECTED;
 
-		super.validateLogin(new SessionKey(idlSessionKey));
+		super.validateLogin(SessionKey.valueOf(idlSessionKey));
 
 		final Set<Identifier> testIds = Identifier.fromTransferables(testIdlIds);
 		Log.debugMessage("Request to stop " + testIdlIds.length + " test(s): " + Identifier.toString(testIds), Log.DEBUGLEVEL07);
