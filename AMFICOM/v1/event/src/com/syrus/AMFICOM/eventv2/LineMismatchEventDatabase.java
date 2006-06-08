52c1e605a8b3d53edd3690be322681bd9ecbac85
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2006-06-08 22:57:57 +0400

    COMPAT: reflected model changes

--- AMFICOM/v1/event/src/com/syrus/AMFICOM/eventv2/LineMismatchEventDatabase.java
+++ AMFICOM/v1/event/src/com/syrus/AMFICOM/eventv2/LineMismatchEventDatabase.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: LineMismatchEventDatabase.java,v 1.3 2006/05/18 19:37:22 bass Exp $
+ * $Id: LineMismatchEventDatabase.java,v 1.4 2006/06/08 18:57:57 bass Exp $
  *
  * Copyright ¿ 2004-2006 Syrus Systems.
  * Dept. of Science & Technology.
@@ -9,13 +9,16 @@
 package com.syrus.AMFICOM.eventv2;
 
 import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_AFFECTED_PATH_ELEMENT_ID;
+import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_ALARM_STATUS;
 import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_MISMATCH_OPTICAL_DISTANCE;
 import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_MISMATCH_PHYSICAL_DISTANCE;
+import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_PARENT_LINE_MISMATCH_EVENT_ID;
 import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_PHYSICAL_DISTANCE_TO_END;
 import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_PHYSICAL_DISTANCE_TO_START;
 import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_PLAIN_TEXT_MESSAGE;
 import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_REFLECTOGRAM_MISMATCH_EVENT_ID;
 import static com.syrus.AMFICOM.eventv2.LineMismatchEventWrapper.COLUMN_RICH_TEXT_MESSAGE;
+import static com.syrus.AMFICOM.general.Identifier.VOID_IDENTIFIER;
 import static com.syrus.AMFICOM.general.ObjectEntities.LINEMISMATCHEVENT_CODE;
 import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_CREATED;
 import static com.syrus.AMFICOM.general.StorableObjectWrapper.COLUMN_CREATOR_ID;
@@ -29,6 +32,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
+import com.syrus.AMFICOM.eventv2.LineMismatchEvent.AlarmStatus;
 import com.syrus.AMFICOM.general.DatabaseIdentifier;
 import com.syrus.AMFICOM.general.Identifier;
 import com.syrus.AMFICOM.general.IllegalDataException;
@@ -41,7 +45,7 @@ import com.syrus.util.database.DatabaseString;
 /**
  * @author Andrew ``Bass'' Shcheglov
  * @author $Author: bass $
- * @version $Revision: 1.3 $, $Date: 2006/05/18 19:37:22 $
+ * @version $Revision: 1.4 $, $Date: 2006/06/08 18:57:57 $
  * @module event
  */
 public final class LineMismatchEventDatabase
@@ -75,7 +79,9 @@ public final class LineMismatchEventDatabase
 						+ COLUMN_MISMATCH_PHYSICAL_DISTANCE + COMMA
 						+ COLUMN_PLAIN_TEXT_MESSAGE + COMMA
 						+ COLUMN_RICH_TEXT_MESSAGE + COMMA
-						+ COLUMN_REFLECTOGRAM_MISMATCH_EVENT_ID
+						+ COLUMN_REFLECTOGRAM_MISMATCH_EVENT_ID + COMMA
+						+ COLUMN_ALARM_STATUS + COMMA
+						+ COLUMN_PARENT_LINE_MISMATCH_EVENT_ID
 				: columns;
 	}
 
@@ -86,6 +92,8 @@ public final class LineMismatchEventDatabase
 	protected String getUpdateMultipleSQLValuesTmpl() {
 		return updateMultipleSQLValues == null
 				? updateMultipleSQLValues = QUESTION + COMMA
+						+ QUESTION + COMMA
+						+ QUESTION + COMMA
 						+ QUESTION + COMMA
 						+ QUESTION + COMMA
 						+ QUESTION + COMMA
@@ -105,6 +113,8 @@ public final class LineMismatchEventDatabase
 	protected String getUpdateSingleSQLValuesTmpl(
 			final DefaultLineMismatchEvent lineMismatchEvent)
 	throws IllegalDataException {
+		final AlarmStatus alarmStatus = lineMismatchEvent.getAlarmStatus();
+
 		return DatabaseIdentifier.toSQLString(lineMismatchEvent.getAffectedPathElementId()) + COMMA
 				+ (lineMismatchEvent.isAffectedPathElementSpacious()
 						? Double.toString(lineMismatchEvent.getPhysicalDistanceToStart()) + COMMA
@@ -115,7 +125,12 @@ public final class LineMismatchEventDatabase
 				+ Double.toString(lineMismatchEvent.getMismatchPhysicalDistance()) + COMMA
 				+ APOSTROPHE + DatabaseString. toQuerySubString(lineMismatchEvent.getPlainTextMessage(), SIZE_PLAIN_TEXT_MESSAGE_COLUMN) + APOSTROPHE + COMMA
 				+ APOSTROPHE + DatabaseString. toQuerySubString(lineMismatchEvent.getRichTextMessage(), SIZE_RICH_TEXT_MESSAGE_COLUMN) + APOSTROPHE + COMMA
-				+ DatabaseIdentifier.toSQLString(lineMismatchEvent.getReflectogramMismatchEventId());
+				+ DatabaseIdentifier.toSQLString(lineMismatchEvent.getReflectogramMismatchEventId()) + COMMA
+				+ (alarmStatus == null
+						? SQL_NULL + COMMA
+								+ DatabaseIdentifier.toSQLString(lineMismatchEvent.getParentLineMismatchEventId())
+						: Integer.toString(alarmStatus.ordinal()) + COMMA
+								+ DatabaseIdentifier.toSQLString(VOID_IDENTIFIER));
 	}
 
 	/**
@@ -132,6 +147,8 @@ public final class LineMismatchEventDatabase
 			final PreparedStatement preparedStatement,
 			int startParameterNumber)
 	throws IllegalDataException, SQLException {
+		final AlarmStatus alarmStatus = lineMismatchEvent.getAlarmStatus();
+
 		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, lineMismatchEvent.getAffectedPathElementId());
 		if (lineMismatchEvent.isAffectedPathElementSpacious()) {
 			preparedStatement.setDouble(++startParameterNumber, lineMismatchEvent.getPhysicalDistanceToStart());
@@ -145,6 +162,13 @@ public final class LineMismatchEventDatabase
 		DatabaseString.setString(preparedStatement, ++startParameterNumber, lineMismatchEvent.getPlainTextMessage(), SIZE_PLAIN_TEXT_MESSAGE_COLUMN);
 		DatabaseString.setString(preparedStatement, ++startParameterNumber, lineMismatchEvent.getRichTextMessage(), SIZE_RICH_TEXT_MESSAGE_COLUMN);
 		DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, lineMismatchEvent.getReflectogramMismatchEventId());
+		if (alarmStatus == null) {
+			preparedStatement.setNull(++startParameterNumber, Types.INTEGER);
+			DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, lineMismatchEvent.getParentLineMismatchEventId());
+		} else {
+			preparedStatement.setInt(++startParameterNumber, alarmStatus.ordinal());
+			DatabaseIdentifier.setIdentifier(preparedStatement, ++startParameterNumber, VOID_IDENTIFIER);
+		}
 		return startParameterNumber;
 	}
 
@@ -190,6 +214,25 @@ public final class LineMismatchEventDatabase
 		}
 
 
+		final AlarmStatus alarmStatus;
+		final Identifier parentLineMismatchEventId;
+		{
+			/*
+			 * Do not inline!
+			 */
+			final int ordinal = resultSet.getInt(COLUMN_ALARM_STATUS);
+			alarmStatus = resultSet.wasNull()
+					? null
+					: AlarmStatus.valueOf(ordinal);
+			parentLineMismatchEventId = DatabaseIdentifier.getIdentifier(resultSet, COLUMN_PARENT_LINE_MISMATCH_EVENT_ID);
+			if (alarmStatus != null ^ parentLineMismatchEventId.isVoid()) {
+				throw new RetrieveObjectException("id: " + id
+						+ "; alarmStatus: " + alarmStatus
+						+ "; parentLineMismatchEventId: " + parentLineMismatchEventId);
+			}
+		}
+
+
 		lineMismatchEvent.setAttributes(DatabaseDate.fromQuerySubString(resultSet, COLUMN_CREATED),
 				DatabaseDate.fromQuerySubString(resultSet, COLUMN_MODIFIED),
 				DatabaseIdentifier.getIdentifier(resultSet, COLUMN_CREATOR_ID),
@@ -203,7 +246,9 @@ public final class LineMismatchEventDatabase
 				resultSet.getDouble(COLUMN_MISMATCH_PHYSICAL_DISTANCE),
 				DatabaseString.fromQuerySubString(resultSet.getString(COLUMN_PLAIN_TEXT_MESSAGE)),
 				DatabaseString.fromQuerySubString(resultSet.getString(COLUMN_RICH_TEXT_MESSAGE)),
-				DatabaseIdentifier.getIdentifier(resultSet, COLUMN_REFLECTOGRAM_MISMATCH_EVENT_ID));
+				DatabaseIdentifier.getIdentifier(resultSet, COLUMN_REFLECTOGRAM_MISMATCH_EVENT_ID),
+				alarmStatus,
+				parentLineMismatchEventId);
 		return lineMismatchEvent;
 	}
 }
