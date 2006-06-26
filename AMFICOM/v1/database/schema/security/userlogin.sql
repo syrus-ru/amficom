c6b694e81c8360d4daf05c23d8c2aca429239063
Arseniy Tashoyan <tashoyan@gmail.com>
2006-06-26 13:27:27 +0400

    MAJOR: Renamed field user_id to system_user_id

--- AMFICOM/v1/database/schema/security/userlogin.sql
+++ AMFICOM/v1/database/schema/security/userlogin.sql
@@ -1,15 +1,15 @@
--- $Id: userlogin.sql,v 1.9 2005/12/14 11:31:09 arseniy Exp $
+-- $Id: userlogin.sql,v 1.9.4.1 2006/06/26 09:27:27 arseniy Exp $
 
 CREATE TABLE UserLogin (
  session_key VARCHAR2(128 CHAR) NOT NULL,
- user_id NOT NULL,
+ system_user_id NOT NULL,
  domain_id,
  user_ior VARCHAR2(1000 BYTE) NOT NULL,
  login_date DATE NOT NULL,
  last_activity_date DATE NOT NULL,
 --
  CONSTRAINT ul_pk PRIMARY KEY (session_key),
- CONSTRAINT ul_user_fk FOREIGN KEY (user_id)
+ CONSTRAINT ul_user_fk FOREIGN KEY (system_user_id)
   REFERENCES SystemUser (id) ON DELETE CASCADE,
  CONSTRAINT ul_domain_fk FOREIGN KEY (domain_id)
   REFERENCES Domain (id) ON DELETE CASCADE
