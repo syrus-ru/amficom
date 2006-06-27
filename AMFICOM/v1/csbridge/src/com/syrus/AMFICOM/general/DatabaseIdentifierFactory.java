abb3d78a7df7d92dd82f5d387dc5f12989435cf8
Arseniy Tashoyan <tashoyan@gmail.com>
2006-06-27 19:51:17 +0400

    ADD: Database implementation of IdentifierFactory

---
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/DatabaseIdentifierFactory.java
@@ -0,0 +1,34 @@
+/*-
+ * $Id: DatabaseIdentifierFactory.java,v 1.1.2.1 2006/06/27 15:51:17 arseniy Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.AMFICOM.general;
+
+import java.util.Set;
+
+/**
+ * @version $Revision: 1.1.2.1 $, $Date: 2006/06/27 15:51:17 $
+ * @author $Author: arseniy $
+ * @author Tashoyan Arseniy Feliksovich
+ * @module csbridge
+ */
+public final class DatabaseIdentifierFactory implements IdentifierFactory {
+
+	public Identifier getGeneratedIdentifier(final short entityCode)
+			throws CommunicationException,
+				IdentifierGenerationException,
+				IllegalObjectEntityException {
+		return IdentifierGenerator.generateIdentifier(entityCode);
+	}
+
+	public Set<Identifier> getGeneratedIdentifierRange(final short entityCode, final int size)
+			throws CommunicationException,
+				IdentifierGenerationException,
+				IllegalObjectEntityException {
+		return IdentifierGenerator.generateIdentifierRange(entityCode, size);
+	}
+
+}
