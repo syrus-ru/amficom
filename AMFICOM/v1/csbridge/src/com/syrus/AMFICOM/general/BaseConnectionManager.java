6377d93da5a430cad8488518a487c11a15e5e03a
Arseniy Tashoyan <tashoyan@gmail.com>
2006-06-27 19:58:39 +0400

    COMPATIBILITY: Use IdentifierGeneratorServerConnectionManager as source for CORBA service IdentifierGeneratorServer

---
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/BaseConnectionManager.java
@@ -0,0 +1,24 @@
+/*
+ * $Id: BaseConnectionManager.java,v 1.8.2.1 2006/06/27 15:58:39 arseniy Exp $
+ *
+ * Copyright © 2004 Syrus Systems.
+ * Научно-технический центр.
+ * Проект: АМФИКОМ.
+ */
+package com.syrus.AMFICOM.general;
+
+/**
+ * The sum of all extended interfaces
+ * @version $Revision: 1.8.2.1 $, $Date: 2006/06/27 15:58:39 $
+ * @author $Author: arseniy $
+ * @author Tashoyan Arseniy Feliksovich
+ * @module csbridge
+ */
+public interface BaseConnectionManager
+		extends LoginServerConnectionManager, EventServerConnectionManager, IdentifierGeneratorServerConnectionManager {
+	/**
+	 * All known implementations are subclasses of VerifiedConnectionManager,
+	 * thus automatically implement this method.
+	 */
+	CORBAServer getCORBAServer();
+}
