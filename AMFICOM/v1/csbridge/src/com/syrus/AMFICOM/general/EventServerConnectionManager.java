6c831d79bfc85bedc76a6047807ff2d5e88f6449
Arseniy Tashoyan <tashoyan@gmail.com>
2006-06-27 19:53:56 +0400

    ADD: Interface to obtain reference on CORBA service EventServer

--- AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/EventServerConnectionManager.java
+++ AMFICOM/v1/csbridge/src/com/syrus/AMFICOM/general/EventServerConnectionManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: EventServerConnectionManager.java,v 1.5 2005/09/14 18:21:32 arseniy Exp $
+ * $Id: EventServerConnectionManager.java,v 1.5.2.1 2006/06/27 15:53:56 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Научно-технический центр.
@@ -7,10 +7,10 @@
  */
 package com.syrus.AMFICOM.general;
 
-import com.syrus.AMFICOM.leserver.corba.EventServer;
+import com.syrus.AMFICOM.systemserver.corba.EventServer;
 
 /**
- * @version $Revision: 1.5 $, $Date: 2005/09/14 18:21:32 $
+ * @version $Revision: 1.5.2.1 $, $Date: 2006/06/27 15:53:56 $
  * @author $Author: arseniy $
  * @author Tashoyan Arseniy Feliksovich
  * @module csbridge
