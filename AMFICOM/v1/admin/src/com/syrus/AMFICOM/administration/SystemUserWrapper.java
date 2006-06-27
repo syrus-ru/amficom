e1f2a7621dc7bb91191f191a49c9f58b2f5f7a15
Arseniy Tashoyan <tashoyan@gmail.com>
2006-06-27 19:37:05 +0400

    REMOVE: Logins for old server processes ADD: Login for System Server

---
+++ AMFICOM/v1/admin/src/com/syrus/AMFICOM/administration/SystemUserWrapper.java
@@ -0,0 +1,155 @@
+/*
+ * $Id: SystemUserWrapper.java,v 1.19.4.1 2006/06/27 15:37:05 arseniy Exp $
+ *
+ * Copyright ¿ 2004 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
+package com.syrus.AMFICOM.administration;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import com.syrus.AMFICOM.administration.corba.IdlSystemUserPackage.SystemUserSort;
+import com.syrus.AMFICOM.general.ApplicationException;
+import com.syrus.AMFICOM.general.Identifier;
+import com.syrus.AMFICOM.general.StorableObjectWrapper;
+import com.syrus.util.Log;
+
+/**
+ * @version $Revision: 1.19.4.1 $, $Date: 2006/06/27 15:37:05 $
+ * @author $Author: arseniy $
+ * @author Tashoyan Arseniy Feliksovich
+ * @module administration
+ */
+public final class SystemUserWrapper extends StorableObjectWrapper<SystemUser> {
+	public static final String SYS_LOGIN = "sys";
+	public static final String SYSTEMSERVER_LOGIN = "systemserver";
+	public static final String MCM_LOGIN = "mcm";
+
+	public static final String COLUMN_LOGIN = "login";
+	public static final String COLUMN_SORT = "sort";
+
+	public static final String LINK_COLUMN_ROLE_IDS = "role_ids";
+
+	private static SystemUserWrapper instance;
+
+	private List<String> keys;
+
+	private SystemUserWrapper() {
+		this.keys = Collections.unmodifiableList(Arrays.asList(new String[] { COLUMN_DESCRIPTION,
+				COLUMN_LOGIN,
+				COLUMN_NAME,
+				COLUMN_SORT,
+				LINK_COLUMN_ROLE_IDS }));
+	}
+
+	public static SystemUserWrapper getInstance() {
+		if (instance == null) {
+			instance = new SystemUserWrapper();
+		}
+		return instance;
+	}
+
+	public List<String> getKeys() {
+		return this.keys;
+	}
+
+	public String getName(final String key) {
+		/* there is no reason to rename it */
+		return key;
+	}
+
+	@Override
+	public Object getValue(final SystemUser user, final String key) {
+		final Object value = super.getValue(user, key);
+		if (value == null && user != null) {
+			if (key.equals(COLUMN_DESCRIPTION)) {
+				return user.getDescription();
+			}
+			if (key.equals(COLUMN_LOGIN)) {
+				return user.getLogin();
+			}
+			if (key.equals(COLUMN_NAME)) {
+				return user.getName();
+			}
+			if (key.equals(COLUMN_SORT)) {
+				return new Integer(user.getSort().value());
+			}
+			if (key.equals(LINK_COLUMN_ROLE_IDS)) {
+				try {
+					return user.getRoleIds();
+				} catch (ApplicationException ae) {
+					Log.errorMessage(ae);
+					return Collections.emptySet();
+				}
+			}
+		}
+		return value;
+	}
+
+	public boolean isEditable(final String key) {
+		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void setValue(final SystemUser user, final String key, final Object value) {
+		if (user != null) {
+			if (key.equals(COLUMN_DESCRIPTION)) {
+				user.setDescription((String) value);
+			} else if (key.equals(COLUMN_LOGIN)) {
+				user.setLogin((String) value);
+			} else if (key.equals(COLUMN_NAME)) {
+				user.setName((String) value);
+			} else if (key.equals(COLUMN_SORT)) {
+				user.setSort(SystemUserSort.from_int(((Integer) value).intValue()));
+			} else if (key.equals(LINK_COLUMN_ROLE_IDS)) {
+				try {
+					user.setRoleIds((Set<Identifier>) value);
+				} catch (ApplicationException ae) {
+					Log.errorMessage(ae);
+				}
+			}
+		}
+	}
+
+	public String getKey(final int index) {
+		return this.keys.get(index);
+	}
+
+	public Object getPropertyValue(final String key) {
+		/* there is no properties */
+		return null;
+	}
+
+	public void setPropertyValue(final String key, final Object objectKey, final Object objectValue) {
+		/* there is no properties */
+	}
+
+	@Override
+	public Class<?> getPropertyClass(final String key) {
+		final Class<?> clazz = super.getPropertyClass(key); 
+		if (clazz != null) {
+			return clazz;
+		}
+		if (key.equals(COLUMN_DESCRIPTION)
+				|| key.equals(COLUMN_LOGIN)
+				|| key.equals(COLUMN_NAME)) {
+			return String.class;
+		}
+		if (key.equals(COLUMN_SORT)) {
+			return Integer.class;
+		}
+		
+		if (key.equals(LINK_COLUMN_ROLE_IDS)) {
+			return Set.class;
+		}
+		
+		return null;
+	}
+
+}
