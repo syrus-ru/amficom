95536cffa90f2c28c6ef11c17e57a99d7ec054d6
Arseniy Tashoyan <tashoyan@gmail.com>
2006-06-27 15:48:21 +0400

    MAJOR: Reimplemented connection procedure

--- util/src/com/syrus/util/database/DatabaseConnection.java
+++ util/src/com/syrus/util/database/DatabaseConnection.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: DatabaseConnection.java,v 1.26 2006/06/26 17:22:34 bass Exp $
+ * $Id: DatabaseConnection.java,v 1.27 2006/06/27 11:48:21 arseniy Exp $
  *
  * Copyright © 2004 Syrus Systems.
  * Dept. of Science & Technology.
@@ -8,25 +8,25 @@
 
 package com.syrus.util.database;
 
+import static com.syrus.util.Log.DEBUGLEVEL07;
+import static com.syrus.util.Log.DEBUGLEVEL10;
 import static java.util.logging.Level.SEVERE;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import oracle.jdbc.OracleDriver;
 import oracle.jdbc.pool.OracleDataSource;
 
 import com.syrus.util.Log;
 
 /**
- * @author $Author: bass $
- * @version $Revision: 1.26 $, $Date: 2006/06/26 17:22:34 $
+ * @author $Author: arseniy $
+ * @version $Revision: 1.27 $, $Date: 2006/06/27 11:48:21 $
  * @author Tashoyan Arseniy Feliksovich
  * @module util
  */
 public class DatabaseConnection {
-	private static final String JDBCDRIVER = OracleDriver.class.getName();
-	private static final String URLPREFIX = "jdbc:oracle:thin:@";
+	private static final String JDBC_DRIVER_TYPE = "thin";
 	private static final int DBPORT = 1521;
 	public static final String USERNAME = "amficom";
 	private static final String PASSWORD = "amficom";
@@ -49,79 +49,24 @@ public class DatabaseConnection {
 			final long dbConnTimeout,
 			final String dbLoginName) throws SQLException {
 		if (dataSource == null) {
-			final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			try {
-				if (classLoader == null) {
-					Class.forName(JDBCDRIVER);
-				} else {
-					Class.forName(JDBCDRIVER, true, classLoader);
-				}
-			} catch (final ClassNotFoundException cnfe) {
-				throw new SQLException("Cannot locate driver: " + JDBCDRIVER + ", " + cnfe.getMessage());
-			}
-			
-			final String url = URLPREFIX + dbHostname + ":" + Integer.toString(DBPORT) + ":" + dbSID;
-			final long deadtime = System.currentTimeMillis() + dbConnTimeout;
-			boolean connected = false;
-			for (connected = false; System.currentTimeMillis() < deadtime && !connected;) {
-				Log.debugMessage("Attempting to connect to database: " + url + " as " + dbLoginName, Log.DEBUGLEVEL07);
-				try {
-					dataSource = new OracleDataSource();
-					
-					dataSource.setURL(url);
-					dataSource.setUser(dbLoginName);
-					dataSource.setPassword(PASSWORD);
+			Log.debugMessage("Attempting to connect to database '" + dbSID
+					+ "' on host '" + dbHostname
+					+ "' as user '" + dbLoginName + "'",
+					DEBUGLEVEL07);
 
-					dataSource.setConnectionCachingEnabled(true);
+			dataSource = new OracleDataSource();
+			dataSource.setDriverType(JDBC_DRIVER_TYPE);
+			dataSource.setServerName(dbHostname);
+			dataSource.setPortNumber(DBPORT);
+			dataSource.setDatabaseName(dbSID);
+			dataSource.setUser(dbLoginName);
+			dataSource.setPassword(PASSWORD);
+			dataSource.setLoginTimeout((int) (dbConnTimeout / 1000));
+			dataSource.setConnectionCachingEnabled(true);
 
-					connected = true;
-				} catch (SQLException e) {
-					Log.debugMessage("Cannot connect to database: " + url + ", " + e.getMessage(), Log.DEBUGLEVEL07);
-					final Object obj = new Object();
-					try {
-						synchronized (obj) {
-							obj.wait(5 * 1000);
-						}
-					} catch (InterruptedException ex) {
-						Log.errorMessage(ex);
-					}
-				}
-			}
-			if (!connected) {
-				throw new SQLException("Unable to connect to database: " + url);
-			}
-			Log.debugMessage("Connected!", Log.DEBUGLEVEL03);
+			Log.debugMessage("Connected", DEBUGLEVEL07);
 		}
 	}
-
-	public static void closeConnection() {
-		if (dataSource != null) {
-			Log.debugMessage("Disconnecting from database...", Log.DEBUGLEVEL07);
-			try {
-				dataSource.close();
-				dataSource = null;
-				if (openConnections != 0) {
-					Log.debugMessage("There remains " + openConnections + " connection(s) open", SEVERE);
-				}
-				Log.debugMessage("Disconnected from database", Log.DEBUGLEVEL07);
-			} catch (Exception e) {
-				Log.errorMessage(e);
-			}
-		}
-	}
-
-	public static void releaseConnection(final Connection connection) {
-		if (connection != null) {
-			try {
-				connection.close();
-				openConnections--;
-				Log.debugMessage("releaseConnection(Connection)", Log.DEBUGLEVEL10);
-			} catch (Exception e) {
-				Log.errorMessage(e);
-			}
-		}
-	}
-
 
 	public static Connection getConnection() throws SQLException {
 		if (dataSource != null) {
@@ -131,5 +76,37 @@ public class DatabaseConnection {
 			return connection;
 		}
 		throw new SQLException("Connection with database closed");
+	}
+
+	public static void releaseConnection(final Connection connection) {
+		if (connection != null) {
+			try {
+				connection.close();
+				openConnections--;
+				Log.debugMessage("releaseConnection(Connection)", DEBUGLEVEL10);
+			} catch (Exception e) {
+				Log.errorMessage(e);
+			}
+		}
+	}
+
+	/**
+	 * @deprecated Этот метод не нужен.
+	 */
+	@Deprecated
+	public static void closeConnection() {
+		if (dataSource != null) {
+			Log.debugMessage("Disconnecting from database...", DEBUGLEVEL07);
+			try {
+				dataSource.close();
+				dataSource = null;
+				if (openConnections != 0) {
+					Log.debugMessage("There remains " + openConnections + " connection(s) open", SEVERE);
+				}
+				Log.debugMessage("Disconnected from database", DEBUGLEVEL07);
+			} catch (Exception e) {
+				Log.errorMessage(e);
+			}
+		}
 	}
 }
