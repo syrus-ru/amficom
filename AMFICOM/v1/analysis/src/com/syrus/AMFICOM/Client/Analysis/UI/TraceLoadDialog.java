d5552da1e3886961e1cbc428b6c5d8ed7d232239
Stanislav Kholshin <kholshin@gmail.com>
2006-06-29 14:07:18 +0400

    add: progress bar

--- AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/Analysis/UI/TraceLoadDialog.java
+++ AMFICOM/v1/analysis/src/com/syrus/AMFICOM/Client/Analysis/UI/TraceLoadDialog.java
@@ -1,5 +1,5 @@
 /*-
- * $Id: TraceLoadDialog.java,v 1.13 2005/11/18 14:37:42 stas Exp $
+ * $Id: TraceLoadDialog.java,v 1.14 2006/06/29 10:07:18 stas Exp $
  *
  * Copyright ї 2005 Syrus Systems.
  * Dept. of Science & Technology.
@@ -13,6 +13,7 @@ import java.awt.event.ActionListener;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -26,6 +27,7 @@ import javax.swing.tree.TreeSelectionModel;
 import com.syrus.AMFICOM.Client.General.Lang.LangModelAnalyse;
 import com.syrus.AMFICOM.client.UI.tree.IconedTreeUI;
 import com.syrus.AMFICOM.client.resource.I18N;
+import com.syrus.AMFICOM.client.util.SynchronousWorker;
 import com.syrus.AMFICOM.filter.UI.FilterPanel;
 import com.syrus.AMFICOM.filter.UI.TreeFilterUI;
 import com.syrus.AMFICOM.general.ApplicationException;
@@ -43,15 +45,15 @@ import com.syrus.util.Log;
 
 /**
  * @author $Author: stas $
- * @version $Revision: 1.13 $, $Date: 2005/11/18 14:37:42 $
+ * @version $Revision: 1.14 $, $Date: 2006/06/29 10:07:18 $
  * @module analysis
  */
 
 public class TraceLoadDialog {
 	private static TreeFilterUI treeFilterUI;
 	static IconedTreeUI treeUI;
-	private static Set<Result> selectedResults;
-	private static Set<Identifier> selectedMeasurementIds;
+	static Set<Result> selectedResults;
+	static Set<Identifier> selectedMeasurementIds;
 
 	static JOptionPane optionPane;
 	static JDialog dialog;
@@ -133,23 +135,34 @@ public class TraceLoadDialog {
 					}
 				}
 			}
-			// XXX: performance: seemes to be a longest part
-			// 0.5 - 1 sec: present in local (client's) cache
-			// 3 - 4 sec: absent in local (client's) cache, seems to be presentin server's
-			// ~146 sec: seems to be absent in server cache (first request) (Arseniy says servers gets these results from agent)
-			try {
-				if (!selectedMeasurementIds.isEmpty()) {
-					StorableObjectCondition condition2 = new LinkedIdsCondition(selectedMeasurementIds, ObjectEntities.RESULT_CODE);
-					Set<StorableObject> resultSet = StorableObjectPool.getStorableObjectsByCondition(condition2, true);
-					for (Iterator<StorableObject> iter = resultSet.iterator(); iter.hasNext();) {
-						Result res = (Result) iter.next();
-						if (res.getSort().value() == ResultSort.RESULT_SORT_MEASUREMENT.value()) {
-							selectedResults.add(res);	
+			if (!selectedMeasurementIds.isEmpty()) {
+				final SynchronousWorker<Boolean> worker = new SynchronousWorker<Boolean>(null,
+						I18N.getString("Message.Information.please_wait"), 
+						I18N.getString("Message.Information.loading_data"), true) {
+					@Override
+					public Boolean construct() throws Exception {
+//						final LinkedIdsCondition condition2 = new LinkedIdsCondition(selectedMeasurementIds, ObjectEntities.RESULT_CODE);
+//					final TypicalCondition condition3 = new TypicalCondition(ResultSort.RESULT_SORT_MEASUREMENT.value(),
+//							OperationSort.OPERATION_EQUALS, ObjectEntities.RESULT_CODE, ResultWrapper.COLUMN_SORT);
+//					selectedResults = StorableObjectPool.getStorableObjectsByCondition(
+//							new CompoundCondition(condition2, CompoundConditionSort.AND, condition3),
+						
+						StorableObjectCondition condition2 = new LinkedIdsCondition(selectedMeasurementIds, ObjectEntities.RESULT_CODE);
+						Set<Result> results = StorableObjectPool.getStorableObjectsByCondition(condition2, true);
+						for (Result res : results) {
+							if (res.getSort().value() == ResultSort.RESULT_SORT_MEASUREMENT.value()) {
+								selectedResults.add(res);	
+							}
 						}
+						return Boolean.valueOf(true);
 					}
+				};
+				
+				try {
+					worker.execute();
+				} catch (ExecutionException e) {
+					Log.errorMessage(e);
 				}
-			} catch (ApplicationException e) {
-				Log.errorMessage(e);
 			}
 			if (selectedResults.isEmpty()) {
 				// XXX: error processing: выдать сообщение об ошибке, что нет результатов
