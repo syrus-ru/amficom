8c196263fb8e5ff72da8737358671a0d142cc5b6
Alexey Stratonnikov <alexsaa@gmail.com>
2006-06-29 11:19:09 +0400

    *** empty log message ***

---
+++ AMFICOM/v1/commonclient/src/com/syrus/AMFICOM/general/Nameable.java
@@ -0,0 +1,22 @@
+/*-
+ * $Id: Nameable.java,v 1.1 2006/06/29 07:19:09 saa Exp $
+ * 
+ * Copyright © 2006 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+
+package com.syrus.AMFICOM.general;
+
+/**
+ * Интерфейс не вносит ничего нового по отношению к
+ * {@link Namable}, а служит лишь для того, чтобы
+ * частично исправить ошибку в его названии.
+ * 
+ * @author $Author: saa $
+ * @version $Revision: 1.1 $, $Date: 2006/06/29 07:19:09 $
+ * @module
+ */
+public interface Nameable extends Namable {
+	// no new functionality
+}
