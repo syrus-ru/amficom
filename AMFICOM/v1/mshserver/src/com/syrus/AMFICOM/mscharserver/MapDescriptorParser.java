977a90281fd94186b06b40bb972c71eed77ec7ce
cvs-fast-export <cvs-fast-export>
2006-07-05 02:23:17 +0000

    Synthetic commit for incomplete tag BASS_TAKES_OFF_FOREVER

---
+++ AMFICOM/v1/mshserver/src/com/syrus/AMFICOM/mscharserver/MapDescriptorParser.java
@@ -0,0 +1,70 @@
+/*-
+ * $Id: MapDescriptorParser.java,v 1.7 2005/10/31 12:30:11 bass Exp $
+ *
+ * Copyright ¿ 2004-2005 Syrus Systems.
+ * Dept. of Science & Technology.
+ * Project: AMFICOM.
+ */
+package com.syrus.AMFICOM.mscharserver;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import com.syrus.AMFICOM.map.MapDescriptor;
+import com.syrus.util.ApplicationProperties;
+import com.syrus.util.Log;
+
+/**
+ * @author max
+ * @author $Author: bass $
+ * @version $Revision: 1.7 $, $Date: 2005/10/31 12:30:11 $
+ * @module mshserver_v1
+ */
+
+final class MapDescriptorParser {
+	private static final String	MAP_DESCRIPTOR_FILE_KEY = "MapDescriptor";
+	private static final String	DEFAULT_MAP_DESCRIPTOR_FILE	= "map_descriptor.xml";
+
+	List<MapDescriptor> getMapDescriptors() {
+		final List<MapDescriptor> mapDescriptors = new LinkedList<MapDescriptor>();
+		final String mapDescriptorFile = ApplicationProperties.getString(MAP_DESCRIPTOR_FILE_KEY, DEFAULT_MAP_DESCRIPTOR_FILE);
+		final File descFile = new File(mapDescriptorFile);
+		final SAXReader reader = new SAXReader();
+		Document document;
+		try {
+			document = reader.read(descFile);
+		} catch (DocumentException e) {
+			Log.errorMessage(e);
+			return mapDescriptors;
+		}
+		final List<Node> mapNodeList = document.selectNodes("//map");
+		for (Node node : mapNodeList) {
+			final Node nameNode = node.selectSingleNode("name");
+			final Node mdfNode = node.selectSingleNode("mdf");
+			if (nameNode == null || mdfNode == null) {
+				Log.errorMessage("Wrong xml format in file " + mapDescriptorFile);
+				continue;
+			}
+			final String name = nameNode.getText();
+			final String mdf = mdfNode.getText();
+			if (name == null || name.equals("") || mdf == null) {
+				Log.errorMessage("Wrong xml content in file " + mapDescriptorFile);
+				continue;
+			}
+			final File mdfFile = new File(mdf);
+			if (!mdfFile.isFile()) {
+				Log.errorMessage("Couldn't locate mdf: + " + mdfFile.getAbsolutePath());
+				continue;
+			}
+			mapDescriptors.add(new MapDescriptor(name, mdfFile.getName(), mdfFile.getPath(), mdfFile.length(), mdfFile.lastModified()));
+		}
+		return mapDescriptors;
+	}
+}
+
